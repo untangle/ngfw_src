eefd7248787c1f57c09c669ad94a1c36f7bbe813
Robert Scott <rbscott@untangle.com>
2005-03-29 04:18:27 +0000

    No need
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/alpine@74 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/firewall/pattern-build/getPatterns.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p ./patterns
-
-cd ./patterns
-http://l7-filter.sourceforge.net/layer7-protocols/protocols/
-wget -l2 -nd -r --accept="*.pat" http://l7-filter.sourceforge.net/layer7-protocols/protocols/
-wget -l2 -nd -r --accept="*.pat" http://l7-filter.sourceforge.net/layer7-protocols/malware/
-
