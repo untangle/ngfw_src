1d956f642750d5cfc6546709f375538986e44866
John Irwin <jdi@untangle.com>
2005-03-31 23:54:56 +0000

    forgot to add this in 115
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/alpine@121 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ gui/main/com/metavize/gui/widgets/IconLabel.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2003, 2004, 2005 Metavize Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Metavize Inc. ("Confidential Information").  You shall
+ * not disclose such Confidential Information.
+ *
+ *  $Id$
+ */
+
+/*
+ * MPasswordField.java
+ *
+ * Created on December 14, 2004, 7:00 PM
+ */
+
+package com.metavize.gui.widgets;
+
+import java.awt.*;
+import javax.swing.*;
+
+
+public class IconLabel extends JLabel {
+
+    public IconLabel()
+    {
+        super();
+    }
+    
+    public IconLabel(String a, ImageIcon b, int c)
+    {
+        super(a, b, c);
+    }
+    
+    public void paintComponent(Graphics g)
+    {
+        ImageIcon icon = (ImageIcon) getIcon();
+        if (icon == null || icon.getImage() == null) {
+            // Don't have an icon (yet), just draw the background.
+            super.paintComponent(g);
+            return;
+        }
+
+        Rectangle clipRect = g.getClipBounds();
+        int clipRight = clipRect.x + clipRect.width;
+        int clipBottom = clipRect.y + clipRect.height;
+
+        int iconWidth = icon.getIconWidth();
+        int iconHeight = icon.getIconHeight();
+        if (iconWidth == 0 || iconHeight == 0) {
+            System.err.println("Ack -- icon width or height is zero");
+            return;
+        }
+        int startx = (clipRect.x / iconWidth) * iconWidth;
+        int starty = (clipRect.y / iconHeight) * iconHeight;
+        for (int y = starty; y < clipBottom; y = y + iconHeight) {
+            for (int x = startx; x < clipRight; x = x + iconWidth) {
+                // g.drawImage(icon, x, y, this);
+                g.drawImage(icon.getImage(), x, y, this);
+            }
+        }
+    }
+}
