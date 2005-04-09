d62e4f5ad03253aa5ec47b7596afdd32b794d790
Aaron Read <amread@untangle.com>
2005-04-09 08:34:48 +0000

    refactored the token stuff. combined Parser/Unparser with ParserSession and UnparserSession (since they are 1:1).
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/alpine@256 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ mvvm/main/com/metavize/tran/token/AbstractParser.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2005 Metavize Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Metavize Inc. ("Confidential Information").  You shall
+ * not disclose such Confidential Information.
+ *
+ * $Id$
+ */
+
+package com.metavize.tran.token;
+
+import com.metavize.mvvm.tapi.TCPSession;
+
+public abstract class AbstractParser implements Parser
+{
+    protected final TCPSession session;
+    protected final boolean clientSide;
+
+    // constructors -----------------------------------------------------------
+
+    protected AbstractParser(TCPSession session, boolean clientSide)
+    {
+        this.session = session;
+        this.clientSide = clientSide;;
+    }
+
+    // session manipulation ---------------------------------------------------
+
+    public void lineBuffering(boolean oneLine)
+    {
+        if (clientSide) {
+            session.clientLineBuffering(oneLine);
+        } else {
+            session.serverLineBuffering(oneLine);
+        }
+    }
+
+    public int readLimit()
+    {
+        if (clientSide) {
+            return session.clientReadLimit();
+        } else {
+            return session.serverReadLimit();
+        }
+    }
+
+    public void readLimit(int limit)
+    {
+        if (clientSide) {
+            session.clientReadLimit(limit);
+        } else {
+            session.serverReadLimit(limit);
+        }
+    }
+
+    public void scheduleTimer(long delay)
+    {
+        session.scheduleTimer(delay);
+    }
+
+    public void cancelTimer()
+    {
+        session.cancelTimer();
+    }
+}
---
+++ mvvm/main/com/metavize/tran/token/AbstractUnparser.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2005 Metavize Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Metavize Inc. ("Confidential Information").  You shall
+ * not disclose such Confidential Information.
+ *
+ * $Id$
+ */
+
+package com.metavize.tran.token;
+
+import com.metavize.mvvm.tapi.TCPSession;
+
+public abstract class AbstractUnparser implements Unparser
+{
+    protected final TCPSession session;
+    protected final boolean clientSide;
+
+    protected AbstractUnparser(TCPSession session, boolean clientSide)
+    {
+        this.session = session;
+        this.clientSide = clientSide;
+    }
+}
