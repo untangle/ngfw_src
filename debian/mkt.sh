e3e491bdbb5dea89e7b8de25e8df9d03efa29ff8
Dirk Morris <dmorris@untangle.com>
2005-04-20 20:45:18 +0000

    build fix
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/alpine@335 8ebf8b4e-06f3-0310-961f-89f74618be54

--- debian/mkt.sh
+++ debian/mkt.sh
@@ -1,13 +1,31 @@
 #!/bin/sh
 
-for i in `ls *-transform.version | grep -v template` ; do 
+files="`ls *-transform.version | grep -v template`"
+
+for i in $files ; do 
     tran="`echo $i | awk -F\. '{print $1}' `"
     tname="`echo $tran | awk -F- '{print $1}' `"
 
     echo "Making files for" $tname " ... " 
 
-    for templatefile in `ls template*` ; do 
+    for templatefile in `ls template-transform*` ; do 
         newfile="`echo $templatefile | sed -s \"s/template-transform/$tran/\" `"
+        echo $newfile
+        cat $templatefile | sed -s "s/template-transform/$tran/" | sed -s "s/template/$tname/" > $newfile
+    done
+done
+
+
+files="`ls *-casing.version | grep -v template`"
+
+for i in $files ; do 
+    tran="`echo $i | awk -F\. '{print $1}' `"
+    tname="`echo $tran | awk -F- '{print $1}' `"
+
+    echo "Making files for" $tname " ... " 
+
+    for templatefile in `ls template-casing*` ; do 
+        newfile="`echo $templatefile | sed -s \"s/template-casing/$tran/\" `"
         echo $newfile
         cat $templatefile | sed -s "s/template-transform/$tran/" | sed -s "s/template/$tname/" > $newfile
     done
