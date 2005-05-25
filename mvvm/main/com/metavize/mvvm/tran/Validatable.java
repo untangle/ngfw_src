14dcc2ee72079e7797e4e20669adb5fe400dc704
Ian Nieves <inieves@untangle.com>
2005-05-25 20:50:00 +0000

    added support for an interface to validate data objects
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/alpine@802 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ mvvm/main/com/metavize/mvvm/tran/Validatable.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2005 Metavize Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Metavize Inc. ("Confidential Information").  You shall
+ * not disclose such Confidential Information.
+ *
+ * $Id: Validatable.java 229 2005-05-25 22:25:00Z inieves $
+ */
+
+package com.metavize.mvvm.tran;
+
+public interface Validatable {
+
+    /**
+     * Allows a data object to be checked for internal and external consistancy.
+     *
+     * @author <a href="mailto:inieves@metavize.com">Ian Morris Nieves</a>
+     * @version 1.0
+     */
+    public void validate() throws Exception;
+
+}
