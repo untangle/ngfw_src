202472ecbe1e8c26820d5703f864dcf427d2a1fa
Aaron Read <amread@untangle.com>
2005-06-16 20:49:19 +0000

    missing semicolon
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/alpine@1057 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ tran/httpblocker/hier/usr/share/metavize/schema/httpblocker-transform/convert-1.sql
@@ -0,0 +1,5 @@
+-- We forgot a semi-colon in schema-0
+
+ALTER TABLE tr_httpblk_extensions ADD CONSTRAINT FKBC81FBBB871AAD3E FOREIGN KEY (rule_id) REFERENCES string_rule;
+
+ALTER TABLE tr_httpblk_extensions ADD CONSTRAINT FKBC81FBBB1CAE658A FOREIGN KEY (setting_id) REFERENCES tr_httpblk_settings;
