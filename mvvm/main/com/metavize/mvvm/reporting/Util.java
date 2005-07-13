28e6f2f0964bc2d739d6e74b3647f6837b6a5aea
Ian Nieves <inieves@untangle.com>
2005-07-13 19:47:58 +0000

    fix to definition of midnight... i had accidentally committed midnight = 24, which i used for testing purposes
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/alpine@1345 8ebf8b4e-06f3-0310-961f-89f74618be54

--- mvvm/main/com/metavize/mvvm/reporting/Util.java
+++ mvvm/main/com/metavize/mvvm/reporting/Util.java
@@ -21,7 +21,7 @@ public class Util {
     static {
 	// INITIALIZE TIME CONSTANTS
         Calendar c = Calendar.getInstance();
-        c.set(Calendar.HOUR_OF_DAY, 20);
+        c.set(Calendar.HOUR_OF_DAY, 0);
         c.set(Calendar.MINUTE, 0);
         c.set(Calendar.SECOND, 0);
         c.set(Calendar.MILLISECOND, 0);
