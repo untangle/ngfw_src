56b5765733814c131711c4f327d4b4a2f8f5af33
Ian Nieves <inieves@untangle.com>
2005-07-15 02:02:25 +0000

    fix to make text readout instead of a button to click on, if you are running locally
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/alpine@1416 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/reporting/main/com/metavize/tran/reporting/gui/BrowserLaunchJPanel.java
+++ tran/reporting/main/com/metavize/tran/reporting/gui/BrowserLaunchJPanel.java
@@ -14,30 +14,42 @@ import java.net.URL;
 
 public class BrowserLaunchJPanel extends JPanel implements ActionListener {
 
-    private JButton launchJButton;
-    private JLabel launchJLabel;
-    private GridBagConstraints launchJButtonConstraints = new GridBagConstraints(0, 0, 1, 1, 0d, 0d, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0,0,0,0), 0, 0);
-    private GridBagConstraints launchJLabelConstraints = new GridBagConstraints(0, 1, 1, 1, 0d, 0d, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0,0,0,0), 0, 0);
+    
+    private GridBagConstraints noteJLabelConstraints = new GridBagConstraints(0, 0, 1, 1, 0d, 0d, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0,0,0,0), 0, 0);
+    private GridBagConstraints launchJButtonConstraints = new GridBagConstraints(0, 1, 1, 1, 0d, 0d, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(10,0,10,0), 0, 0);
+    private GridBagConstraints launchJLabelConstraints = new GridBagConstraints(0, 2, 1, 1, 0d, 0d, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0,0,0,0), 0, 0);
 
     public BrowserLaunchJPanel(){
 	this.setLayout(new GridBagLayout());
+        
+        JButton launchJButton;
+        launchJButton = new JButton("Launch Web Browser");
+        launchJButton.setFocusPainted(false);
+        launchJButton.setFont(new java.awt.Font("Arial", 0, 11));
+        launchJButton.setPreferredSize(new Dimension(225, 25));
+        launchJButton.setMaximumSize(new Dimension(225, 25));
+        launchJButton.setSize(new Dimension(225, 25));
+        launchJButton.addActionListener(this);
+        this.add(launchJButton, launchJButtonConstraints);
 
-	launchJButton = new JButton("Launch Web Browser");
-	launchJButton.setFocusPainted(false);
-	launchJButton.setFont(new java.awt.Font("Arial", 0, 11));
-	launchJButton.setPreferredSize(new Dimension(225, 25));
-	launchJButton.setMaximumSize(new Dimension(225, 25));
-	launchJButton.setSize(new Dimension(225, 25));
-	launchJButton.addActionListener(this);
-	this.add(launchJButton, launchJButtonConstraints);
-
+        if( Util.isLocal() ){
+            launchJButton.setEnabled(false);
+            JLabel noteJLabel = new JLabel();
+            noteJLabel.setFont(new java.awt.Font("Arial", 0, 11));
+	    noteJLabel.setHorizontalAlignment(SwingConstants.CENTER);
+	    noteJLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+	    noteJLabel.setText("<html><center><b>Note:</b> No web browser is installed on this machine for security reasons.<br>Please re-connect from an extrernal machine.</center></html>");
+            this.add(noteJLabel, noteJLabelConstraints);
+        }
+        
 	try{
+            JLabel launchJLabel;
 	    URL newURL = new URL( Util.getServerCodeBase(), "../reports");
 	    launchJLabel = new JLabel();
 	    launchJLabel.setFont(new java.awt.Font("Arial", 0, 11));
 	    launchJLabel.setHorizontalAlignment(SwingConstants.CENTER);
 	    launchJLabel.setHorizontalTextPosition(SwingConstants.CENTER);
-	    launchJLabel.setText("<html><center><br>To view reports from another browser, go to:<br>" + newURL.toString() + "</center></html>");
+	    launchJLabel.setText("<html><center><br>To view reports a web browser, go to:<br>" + newURL.toString() + "</center></html>");
 	    this.add(launchJLabel, launchJLabelConstraints);
 	}
 	catch(Exception f){
