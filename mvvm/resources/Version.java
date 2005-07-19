e24280df454247f45e33f44271ddd3e9c3a6b438
Dirk Morris <dmorris@untangle.com>
2005-07-19 18:07:04 +0000

    added Version magic to mvvm.util.Version
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/alpine@1490 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ mvvm/resources/Version.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2003,2004 Metavize Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Metavize Inc. ("Confidential Information").  You shall
+ * not disclose such Confidential Information.
+ *
+ * $Id$
+ */
+
+package com.metavize.mvvm.util;
+
+/**
+ *
+ * @author  Dirk Morris
+ */
+public class Version {
+    private static String version = "VERSION"; 
+
+    public static String getVersion()
+    {
+        return version;
+    }
+}
