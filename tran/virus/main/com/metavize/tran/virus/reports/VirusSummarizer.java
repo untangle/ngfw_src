623942090101bd44a7c307392bddd6aafa0c0428
Ian Nieves <inieves@untangle.com>
2005-07-20 02:30:15 +0000

    added the final email summarization bits to the general summarizer moved the summarizer to its own reports directory updated the report-files of the transforms that depend on virus
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/alpine@1513 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/virus/main/com/metavize/tran/virus/reports/VirusSummarizer.java
+++ tran/virus/main/com/metavize/tran/virus/reports/VirusSummarizer.java
@@ -9,7 +9,7 @@
  * $Id$
  */
 
-package com.metavize.tran.virus;
+package com.metavize.tran.virus.reports;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -38,11 +38,15 @@ public class VirusSummarizer extends BaseSummarizer {
 	int emailBlocked = 0;
 
         try {
-            String sql = "SELECT count(*) FROM tr_virus_evt_http WHERE time_stamp >= ? AND time_stamp < ?";
-            PreparedStatement ps = conn.prepareStatement(sql);
+            String sql;
+	    PreparedStatement ps;
+	    ResultSet rs;
+
+	    sql = "SELECT count(*) FROM tr_virus_evt_http WHERE time_stamp >= ? AND time_stamp < ?";
+            ps = conn.prepareStatement(sql);
             ps.setTimestamp(1, startDate);
             ps.setTimestamp(2, endDate);
-            ResultSet rs = ps.executeQuery();
+            rs = ps.executeQuery();
             rs.first();
             httpScanned = rs.getInt(1);
             rs.close();
@@ -57,7 +61,6 @@ public class VirusSummarizer extends BaseSummarizer {
             httpBlocked = rs.getInt(1);
             rs.close();
             ps.close();
-
 
             sql = "SELECT count(*) FROM tr_virus_evt WHERE time_stamp >= ? AND time_stamp < ?";
             ps = conn.prepareStatement(sql);
@@ -79,10 +82,45 @@ public class VirusSummarizer extends BaseSummarizer {
             rs.close();
             ps.close();
 
-	    /////// INSERT SCANNED QUERY HERE
+            sql = "SELECT count(*) FROM tr_virus_evt_mail WHERE time_stamp >= ? AND time_stamp < ? AND clean = false";
+            ps = conn.prepareStatement(sql);
+            ps.setTimestamp(1, startDate);
+            ps.setTimestamp(2, endDate);
+            rs = ps.executeQuery();
+            rs.first();
+            emailBlocked = rs.getInt(1);
+            rs.close();
+            ps.close();
 
-	    /////// INSERT BLOCKED QUERY HERE
+            sql = "SELECT count(*) FROM tr_virus_evt_smtp WHERE time_stamp >= ? AND time_stamp < ? AND clean = false";
+            ps = conn.prepareStatement(sql);
+            ps.setTimestamp(1, startDate);
+            ps.setTimestamp(2, endDate);
+            rs = ps.executeQuery();
+            rs.first();
+            emailBlocked += rs.getInt(1);
+            rs.close();
+            ps.close();
 
+            sql = "SELECT count(*) FROM tr_virus_evt_mail WHERE time_stamp >= ? AND time_stamp < ?";
+            ps = conn.prepareStatement(sql);
+            ps.setTimestamp(1, startDate);
+            ps.setTimestamp(2, endDate);
+            rs = ps.executeQuery();
+            rs.first();
+            emailScanned = rs.getInt(1);
+            rs.close();
+            ps.close();
+
+            sql = "SELECT count(*) FROM tr_virus_evt_smtp WHERE time_stamp >= ? AND time_stamp < ?";
+            ps = conn.prepareStatement(sql);
+            ps.setTimestamp(1, startDate);
+            ps.setTimestamp(2, endDate);
+            rs = ps.executeQuery();
+            rs.first();
+            emailScanned += rs.getInt(1);
+            rs.close();
+            ps.close();
 
             rs.close();
             ps.close();
@@ -92,14 +130,20 @@ public class VirusSummarizer extends BaseSummarizer {
         }
 
         addEntry("Scanned Web downloads", Util.trimNumber("",httpScanned));
-        addEntry("&nbsp;&nbsp;&nbsp;Blocked (infected)", Util.trimNumber("",httpBlocked));
-        addEntry("&nbsp;&nbsp;&nbsp;Passed", Util.trimNumber("",httpScanned - httpBlocked));
+        addEntry("&nbsp;&nbsp;&nbsp;Infected & Blocked", Util.trimNumber("",httpBlocked));
+        addEntry("&nbsp;&nbsp;&nbsp;Clean & Passed", Util.trimNumber("",httpScanned - httpBlocked));
+
+        addEntry("&nbsp;","&nbsp;");
+
         addEntry("Scanned FTP downloads", Util.trimNumber("",ftpScanned));
-        addEntry("&nbsp;&nbsp;&nbsp;Blocked (infected)", Util.trimNumber("",ftpBlocked));
-        addEntry("&nbsp;&nbsp;&nbsp;Passed", Util.trimNumber("",ftpScanned - ftpBlocked));
-        addEntry("Scanned Email downloads", Util.trimNumber("XXXX",ftpScanned));
-        addEntry("&nbsp;&nbsp;&nbsp;Blocked (infected)", Util.trimNumber("XXXX",emailBlocked));
-        addEntry("&nbsp;&nbsp;&nbsp;Passed", Util.trimNumber("XXXX",emailScanned - emailBlocked));
+        addEntry("&nbsp;&nbsp;&nbsp;Infected & Blocked", Util.trimNumber("",ftpBlocked));
+        addEntry("&nbsp;&nbsp;&nbsp;Clean & Passed", Util.trimNumber("",ftpScanned - ftpBlocked));
+
+        addEntry("&nbsp;","&nbsp;");
+
+        addEntry("Scanned Email downloads", Util.trimNumber("",emailScanned));
+        addEntry("&nbsp;&nbsp;&nbsp;Infected & Blocked", Util.trimNumber("",emailBlocked));
+        addEntry("&nbsp;&nbsp;&nbsp;Clean & Passed", Util.trimNumber("",emailScanned - emailBlocked));
 
         // XXXX
         String tranName = "Virus";
