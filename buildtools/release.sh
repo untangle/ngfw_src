96530586a480e571d5bb5526307263e8d531a027
Dirk Morris <dmorris@untangle.com>
2005-07-25 21:45:09 +0000

    no more update.log
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/alpine@1611 8ebf8b4e-06f3-0310-961f-89f74618be54

--- buildtools/release.sh
+++ buildtools/release.sh
@@ -19,7 +19,7 @@ echo "------------------------"
 echo -e "Releasing:\n`ls ../*.deb | egrep \"$pkg\"`"
 sudo ../pkgs/scripts/deb-add.sh /var/www/`whoami`/ `ls ../*.deb | egrep "$pkg"`
 echo "------------------------"
-sudo ../pkgs/scripts/deb-scan.sh /var/www/`whoami`/ &> ../update.log
+sudo ../pkgs/scripts/deb-scan.sh /var/www/`whoami`/ 
 
 rm -f debian/release_list
 svn commit -m"release" debian/*.version debian/changelog
