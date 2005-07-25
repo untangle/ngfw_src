00e393362aa988fa1909698aeba3ce70826b2124
Cho Ng <cng@untangle.com>
2005-07-25 17:22:31 +0000

    mail casing - pop prototype
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/alpine@1603 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/spam/main/com/metavize/tran/spam/SpamPopHandler.java
+++ tran/spam/main/com/metavize/tran/spam/SpamPopHandler.java
@@ -11,25 +11,48 @@
 
 package com.metavize.tran.spam;
 
-
+import com.metavize.mvvm.argon.IntfConverter;
 import com.metavize.mvvm.tapi.TCPSession;
 import com.metavize.tran.mail.PopStateMachine;
+import com.metavize.tran.token.TokenException;
+import com.metavize.tran.token.TokenResult;
 import org.apache.log4j.Logger;
 
 class SpamPopHandler extends PopStateMachine
 {
     private final Logger logger = Logger.getLogger(SpamPopHandler.class);
 
-    private final SpamImpl transform;
+    private final SpamScanner zScanner;
+
+    private final SpamMessageAction zMsgAction;
+    private final boolean bScan;
 
     // constructors -----------------------------------------------------------
 
     SpamPopHandler(TCPSession session, SpamImpl transform)
     {
         super(session);
-        this.transform = transform;
+
+        zScanner = transform.getScanner();
+
+        SpamPOPConfig zConfig;
+        if (IntfConverter.INSIDE == session.clientIntf())
+        {
+            zConfig = transform.getSpamSettings().getPOPInbound();
+        }
+        else
+        {
+            zConfig = transform.getSpamSettings().getPOPOutbound();
+        }
+        bScan = zConfig.getScan();
+        zMsgAction = zConfig.getMsgAction();
     }
 
     // PopStateMachine methods -----------------------------------------------
 
+    protected TokenResult scanMessage() throws TokenException
+    {
+//XXXX
+        return null;
+    }
 }
