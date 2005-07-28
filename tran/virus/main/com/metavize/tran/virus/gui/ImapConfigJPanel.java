0b6fd4be0c0de80dc9953b2e554153b39ff54cae
Ian Nieves <inieves@untangle.com>
2005-07-28 22:55:52 +0000

    fixed column widths
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/alpine@1676 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/virus/main/com/metavize/tran/virus/gui/ImapConfigJPanel.java
+++ tran/virus/main/com/metavize/tran/virus/gui/ImapConfigJPanel.java
@@ -45,7 +45,7 @@ class ImapTableModel extends MSortedTableModel{
     private static final int C1_MW = Util.LINENO_MIN_WIDTH; /* # - invisible */
     private static final int C2_MW = 100; /* source */
     private static final int C3_MW = 55; /* scan */
-    private static final int C4_MW = 125; /* action if SPAM detected */
+    private static final int C4_MW = 140; /* action if SPAM detected */
     private static final int C5_MW = Util.chooseMax(T_TW - (C1_MW + C2_MW + C3_MW + C4_MW), 120); /* description */
 
     public TableColumnModel getTableColumnModel(){
--- tran/virus/main/com/metavize/tran/virus/gui/SmtpConfigJPanel.java
+++ tran/virus/main/com/metavize/tran/virus/gui/SmtpConfigJPanel.java
@@ -45,7 +45,7 @@ class SmtpTableModel extends MSortedTableModel{
     private static final int C1_MW = Util.LINENO_MIN_WIDTH; /* # - invisible */
     private static final int C2_MW = 100; /* source */
     private static final int C3_MW = 55; /* scan */
-    private static final int C4_MW = 125; /* action if SPAM detected */
+    private static final int C4_MW = 140; /* action if SPAM detected */
     private static final int C5_MW = 190; /* notification if SPAM detected */
     private static final int C6_MW = Util.chooseMax(T_TW - (C1_MW + C2_MW + C3_MW + C4_MW + C5_MW), 120); /* description */
 
