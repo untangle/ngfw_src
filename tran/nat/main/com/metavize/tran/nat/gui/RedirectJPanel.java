da9675eab18ca7502b764f585c057be36728de15
Ian Nieves <inieves@untangle.com>
2005-07-28 22:54:41 +0000

    update to make logging its own column
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/alpine@1674 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/nat/main/com/metavize/tran/nat/gui/RedirectJPanel.java
+++ tran/nat/main/com/metavize/tran/nat/gui/RedirectJPanel.java
@@ -51,8 +51,8 @@ class RedirectTableModel extends MSortedTableModel{
     private static final int  T_TW = Util.TABLE_TOTAL_WIDTH;
     private static final int  C0_MW = Util.STATUS_MIN_WIDTH; /* status */
     private static final int  C1_MW = Util.LINENO_MIN_WIDTH; /* # */
-    private static final int  C2_MW = 65;  /* enable action */
-    private static final int  C3_MW = 120; /* action */
+    private static final int  C2_MW = 75;  /* redirect */
+    private static final int  C3_MW = 55;  /* log */
     private static final int  C4_MW = 100; /* traffic type */
     private static final int  C5_MW = 160; /* direction */
     private static final int  C6_MW = 130;  /* source address */
@@ -69,7 +69,6 @@ class RedirectTableModel extends MSortedTableModel{
     
     private ComboBoxModel protocolModel = super.generateComboBoxModel( ProtocolMatcher.getProtocolEnumeration(), ProtocolMatcher.getProtocolDefault() );
     private ComboBoxModel directionModel = super.generateComboBoxModel( TrafficRule.getDirectionEnumeration(), TrafficRule.getDirectionDefault() );
-    private ComboBoxModel actionModel = super.generateComboBoxModel( RedirectRule.getActionEnumeration(), RedirectRule.getActionDefault() );
     
 
     protected boolean getSortable(){ return false; }
@@ -80,8 +79,8 @@ class RedirectTableModel extends MSortedTableModel{
         //                                 #   min    rsz    edit   remv   desc   typ            def
         addTableColumn( tableColumnModel,  0,  C0_MW, false, false, false, false, String.class,  null, sc.TITLE_STATUS );
         addTableColumn( tableColumnModel,  1,  C1_MW, false, false, false, false, Integer.class, null, sc.TITLE_INDEX );
-        addTableColumn( tableColumnModel,  2,  C2_MW, false, true,  false, false, Boolean.class, "false", sc.bold("enable<br>action") );
-        addTableColumn( tableColumnModel,  3,  C3_MW, false, true,  false, false, ComboBoxModel.class, actionModel,  sc.bold("action"));
+        addTableColumn( tableColumnModel,  2,  C2_MW, false, true,  false, false, Boolean.class, "false", sc.bold("redirect") );
+        addTableColumn( tableColumnModel,  3,  C3_MW, false, true,  false, false, Boolean.class, "false",  sc.bold("log"));
         addTableColumn( tableColumnModel,  4,  C4_MW, false, true,  false, false, ComboBoxModel.class, protocolModel, sc.html("traffic<br>type") );
         addTableColumn( tableColumnModel,  5,  C5_MW, false, true,  false, false, ComboBoxModel.class, directionModel, "direction" );
         addTableColumn( tableColumnModel,  6,  C6_MW, true,  true,  false, false, String.class, "1.2.3.4", sc.html("source<br>address") );
@@ -103,7 +102,7 @@ class RedirectTableModel extends MSortedTableModel{
             
             RedirectRule redirectRule = new RedirectRule();
             redirectRule.setLive( (Boolean) rowVector.elementAt(2) );
-            redirectRule.setAction( ((ComboBoxModel) rowVector.elementAt(3)).getSelectedItem().toString() );
+            redirectRule.setLog( (Boolean) rowVector.elementAt(3) );
             redirectRule.setProtocol( ProtocolMatcher.parse(((ComboBoxModel) rowVector.elementAt(4)).getSelectedItem().toString()) );
             redirectRule.setDirection( ((ComboBoxModel) rowVector.elementAt(5)).getSelectedItem().toString() );
             try{ redirectRule.setSrcAddress( IPMatcher.parse((String) rowVector.elementAt(6)) ); }
@@ -145,7 +144,7 @@ class RedirectTableModel extends MSortedTableModel{
 	    rowVector.add(super.ROW_SAVED);
 	    rowVector.add(new Integer(index));
 	    rowVector.add(redirectRule.isLive());
-	    rowVector.add(super.generateComboBoxModel( RedirectRule.getActionEnumeration(), redirectRule.getAction().toString() ));
+	    rowVector.add(redirectRule.getLog());
 	    rowVector.add(super.generateComboBoxModel( ProtocolMatcher.getProtocolEnumeration(), redirectRule.getProtocol().toString() ));
 	    rowVector.add(super.generateComboBoxModel( TrafficRule.getDirectionEnumeration(), redirectRule.getDirection().toString() ));
 	    rowVector.add(redirectRule.getSrcAddress().toString());
