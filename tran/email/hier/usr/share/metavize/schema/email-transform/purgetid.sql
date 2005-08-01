c736e010e551b05a03f7cde93f17598ae1df3b59
Aaron Read <amread@untangle.com>
2005-08-01 20:21:59 +0000

    purge scripts for email
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/alpine@1719 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ tran/email/hier/usr/share/metavize/schema/email-transform/purgetid.sql
@@ -0,0 +1,13 @@
+-- email is no more, so just purgetran
+
+DROP TABLE tr_email_ml_definition;
+DROP TABLE tr_email_settings;
+DROP TABLE tr_email_ssctl_definition;
+DROP TABLE tr_email_vsctl_definition;
+DROP TABLE tr_email_ctl_definition;
+DROP TABLE tr_email_szrelay_evt;
+DROP TABLE tr_email_spam_evt;
+DROP TABLE tr_email_virus_evt;
+DROP TABLE tr_email_custom_evt;
+DROP TABLE tr_email_message_info;
+DROP TABLE tr_email_handler_info;
