01df12483265aa481c0e1fbb57cf5f76dcdeac5f
Ian Nieves <inieves@untangle.com>
2005-08-06 00:02:23 +0000

    fix for bug 611 dealing with WCF no longer needing 3 seperate categories for blocking... now just one single category on to aaron for possible official backend support
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/alpine/release-2.5/alpine@1910 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/httpblocker/main/com/metavize/tran/httpblocker/gui/BlockedCategoriesConfigJPanel.java
+++ tran/httpblocker/main/com/metavize/tran/httpblocker/gui/BlockedCategoriesConfigJPanel.java
@@ -46,12 +46,10 @@ class CategoryTableModel extends MSortedTableModel
     private static final int T_TW = Util.TABLE_TOTAL_WIDTH;
     private static final int C0_MW = Util.STATUS_MIN_WIDTH; /* status */
     private static final int C1_MW = Util.LINENO_MIN_WIDTH; /* # */
-    private static final int C2_MW = 150; /* category */
+    private static final int C2_MW = 316; /* category */
     private static final int C3_MW = 100; /* block domains */
-    private static final int C4_MW = 100; /* block URLs */
-    private static final int C5_MW = 100; /* block expressions */
-    private static final int C6_MW = 120; /* description */
-    private static final int C7_MW = Util.chooseMax(T_TW - (C0_MW + C1_MW + C2_MW + C3_MW + C4_MW + C5_MW + C6_MW), 55); /* original name */
+    private static final int C4_MW = 120; /* description */
+    private static final int C5_MW = Util.chooseMax(T_TW - (C0_MW + C1_MW + C2_MW + C3_MW + C4_MW), 55); /* original name */
 
     
     public TableColumnModel getTableColumnModel(){
@@ -61,12 +59,10 @@ class CategoryTableModel extends MSortedTableModel
         addTableColumn( tableColumnModel,  0, C0_MW, false, false, false, false, String.class,  null, sc.TITLE_STATUS);
         addTableColumn( tableColumnModel,  1, C1_MW, false, false, true,  false, Integer.class, null, sc.TITLE_INDEX);
         addTableColumn( tableColumnModel,  2, C2_MW, true,  false, false, false, String.class,  null, sc.TITLE_CATEGORY);
-        addTableColumn( tableColumnModel,  3, C3_MW, false, true,  false, false, Boolean.class, null, sc.bold("block<br>domains"));
-        addTableColumn( tableColumnModel,  4, C4_MW, false, true,  false, false, Boolean.class, null, sc.bold("block<br>URLs"));
-        addTableColumn( tableColumnModel,  5, C5_MW, false, true,  false, false, Boolean.class, null, sc.bold("block<br>expressions"));
-        addTableColumn( tableColumnModel,  6, C6_MW, true,  true,  false, true,  String.class,  null, sc.TITLE_DESCRIPTION);
-        addTableColumn( tableColumnModel,  7, C7_MW, false, false, true,  false, String.class,  null, "original name");
-        addTableColumn( tableColumnModel,  8, 10,    false, false, true,  false, BlacklistCategory.class, null, "");
+        addTableColumn( tableColumnModel,  3, C3_MW, false, true,  false, false, Boolean.class, null, sc.bold("block"));
+        addTableColumn( tableColumnModel,  4, C4_MW, true,  true,  false, true,  String.class,  null, sc.TITLE_DESCRIPTION);
+        addTableColumn( tableColumnModel,  5, C5_MW, false, false, true,  false, String.class,  null, "original name");
+        addTableColumn( tableColumnModel,  6, 10,    false, false, true,  false, BlacklistCategory.class, null, "");
         return tableColumnModel;
     }
     
@@ -75,13 +71,20 @@ class CategoryTableModel extends MSortedTableModel
         List elemList = new ArrayList();
 	for( Vector rowVector : (Vector<Vector>) this.getDataVector() ){
             
-            BlacklistCategory newElem = (BlacklistCategory) rowVector.elementAt(8);
+            BlacklistCategory newElem = (BlacklistCategory) rowVector.elementAt(6);
             newElem.setDisplayName( (String) rowVector.elementAt(2) );
-            newElem.setBlockDomains( ((Boolean) rowVector.elementAt(3)).booleanValue() );
-            newElem.setBlockUrls( ((Boolean) rowVector.elementAt(4)).booleanValue() );
-            newElem.setBlockExpressions( ((Boolean) rowVector.elementAt(5)).booleanValue() );
-            newElem.setDescription( (String) rowVector.elementAt(6) );
-            newElem.setName( (String) rowVector.elementAt(7) );
+	    if( ((Boolean)rowVector.elementAt(3)).booleanValue() ){
+		newElem.setBlockDomains( true );
+		newElem.setBlockUrls( true );
+		newElem.setBlockExpressions( true );
+	    }
+	    else{
+		newElem.setBlockDomains( false );
+		newElem.setBlockUrls( false );
+		newElem.setBlockExpressions( false );
+	    }
+            newElem.setDescription( (String) rowVector.elementAt(4) );
+            newElem.setName( (String) rowVector.elementAt(5) );
             elemList.add(newElem);
         }
         
@@ -104,9 +107,7 @@ class CategoryTableModel extends MSortedTableModel
             row.add(super.ROW_SAVED);
             row.add(new Integer(counter));
             row.add(newElem.getDisplayName());
-            row.add(Boolean.valueOf(newElem.getBlockDomains()) );
-            row.add(Boolean.valueOf(newElem.getBlockUrls()) );
-            row.add(Boolean.valueOf(newElem.getBlockExpressions()) );
+            row.add(Boolean.valueOf(newElem.getBlockDomains()));
             row.add(newElem.getDescription());
             row.add(newElem.getName());
 	    row.add(newElem);
