ab4a7ba05b865c703145daa4665ebf668aff4cd8
Aaron Read <amread@untangle.com>
2005-08-10 22:57:22 +0000

    added mail stuff to virus rollups
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/alpine/release-2.5/alpine@2019 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/virus/hier/usr/share/metavize/schema/virus-base/rollup.sql
+++ tran/virus/hier/usr/share/metavize/schema/virus-base/rollup.sql
@@ -1,8 +1,14 @@
-CREATE INDEX tr_virus_evt_ts_idx ON tr_virus_evt (time_stamp);
-CREATE INDEX tr_virus_http_ts_idx ON tr_virus_evt_http (time_stamp);
+CREATE INDEX tr_virus_rollup_evt_idx ON tr_virus_evt (time_stamp);
+CREATE INDEX tr_virus_rollup_http_idx ON tr_virus_evt_http (time_stamp);
+CREATE INDEX tr_virus_rollup_smtp_idx ON tr_virus_evt_smtp (time_stamp);
+CREATE INDEX tr_virus_rollup_mail_idx ON tr_virus_evt_mail (time_stamp);
 
 DELETE FROM tr_virus_evt WHERE time_stamp < (:cutoff)::timestamp;
 DELETE FROM tr_virus_evt_http WHERE time_stamp < (:cutoff)::timestamp;
+DELETE FROM tr_virus_evt_smtp WHERE time_stamp < (:cutoff)::timestamp;
+DELETE FROM tr_virus_evt_mail WHERE time_stamp < (:cutoff)::timestamp;
 
-DROP INDEX tr_virus_evt_ts_idx;
-DROP INDEX tr_virus_http_ts_idx;
+DROP INDEX tr_virus_rollup_evt_idx;
+DROP INDEX tr_virus_rollup_http_idx;
+DROP INDEX tr_virus_rollup_smtp_idx;
+DROP INDEX tr_virus_rollup_mail_idx;
