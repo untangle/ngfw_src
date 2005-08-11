3f41b9ea8f035ff9926f114a800b7c7303732f72
Aaron Read <amread@untangle.com>
2005-08-11 03:02:30 +0000

    added blacklist rollup
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/alpine/release-2.5/alpine@2039 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/spyware/hier/usr/share/metavize/schema/spyware-transform/rollup.sql
+++ tran/spyware/hier/usr/share/metavize/schema/spyware-transform/rollup.sql
@@ -1,12 +1,15 @@
 CREATE INDEX tr_spyware_ax_ts_idx ON tr_spyware_evt_activex (time_stamp);
 CREATE INDEX tr_spyware_ac_ts_idx ON tr_spyware_evt_access (time_stamp);
 CREATE INDEX tr_spyware_co_ts_idx ON tr_spyware_evt_cookie (time_stamp);
+CREATE INDEX tr_spyware_bl_ts_idx ON tr_spyware_evt_blacklist (time_stamp);
 
 DELETE FROM tr_spyware_evt_activex WHERE time_stamp < (:cutoff)::timestamp;
 DELETE FROM tr_spyware_evt_access WHERE time_stamp < (:cutoff)::timestamp;
 DELETE FROM tr_spyware_evt_cookie WHERE time_stamp < (:cutoff)::timestamp;
+DELETE FROM tr_spyware_evt_blacklist WHERE time_stamp < (:cutoff)::timestamp;
 
 DROP INDEX tr_spyware_ax_ts_idx;
 DROP INDEX tr_spyware_ac_ts_idx;
 DROP INDEX tr_spyware_co_ts_idx;
+DROP INDEX tr_spyware_bl_ts_idx;
 
