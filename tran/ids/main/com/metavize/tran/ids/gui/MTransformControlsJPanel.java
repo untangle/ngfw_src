71458ebaaca7a8a4a0e358df7eee2658c68d8eea
Ian Nieves <inieves@untangle.com>
2005-08-13 01:03:55 +0000

    the first whack at an ids gui
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@2102 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ tran/ids/main/com/metavize/tran/ids/gui/MTransformControlsJPanel.java
@@ -0,0 +1,25 @@
+package com.metavize.tran.ids.gui;
+
+import com.metavize.gui.transform.*;
+import com.metavize.gui.pipeline.MPipelineJPanel;
+import com.metavize.gui.widgets.editTable.*;
+import com.metavize.gui.util.*;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.table.*;
+import java.util.Vector;
+import javax.swing.event.*;
+
+public class MTransformControlsJPanel extends com.metavize.gui.transform.MTransformControlsJPanel{
+    
+    public MTransformControlsJPanel(MTransformJPanel mTransformJPanel) {
+        super(mTransformJPanel);
+    }
+
+    protected void generateGui(){
+    }
+    
+}
+
+
