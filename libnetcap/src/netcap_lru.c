abc6184a6f6e2bbb87bbe716a45bb912d8980d71
Robert Scott <rbscott@untangle.com>
2005-09-23 05:51:14 +0000

    1. The new lru.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@2666 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ libnetcap/src/netcap_lru.c
@@ -0,0 +1,266 @@
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <pthread.h>
+
+#include <time.h>
+#include <sys/time.h>
+#include <mvutil/debug.h>
+#include <mvutil/errlog.h>
+#include <mvutil/list.h>
+
+#include "netcap_lru.h"
+
+#define _LRU_MIN_LENGTH 4
+
+#define _LRU_DEBUG_HIGH 11
+#define _LRU_DEBUG_LOW  7
+
+static int _lru_remove( netcap_lru_t* lru, netcap_lru_node_t* node );
+
+static __inline__ int _validate_args( int high_water, int low_water, int sieve_size )
+{
+    if (( high_water < 0 ) || ( low_water < 0 )) return errlogargs();
+
+    if ( sieve_size < 0 ) return errlogargs();
+    
+    if ( high_water < low_water ) return errlogargs();
+
+    return 0;
+}
+
+static __inline__ int _validate_lru( netcap_lru_t* lru )
+{
+    if ( _validate_args( lru->high_water, lru->low_water, lru->sieve_size ) < 0 ) {
+        return errlog( ERR_CRITICAL, "_validate_args\n" );
+    }
+
+    if ( lru->remove == NULL ) return errlogargs();
+
+    return 0;
+}
+
+
+int netcap_lru_init( netcap_lru_t* lru, int high_water, int low_water, int sieve_size, 
+                     netcap_lru_check_t* is_deletable, netcap_lru_remove_t* remove )
+{
+    if ( lru == NULL || remove == NULL ) return errlogargs();
+
+    if ( is_deletable == NULL ) errlog( ERR_WARNING, "NULL is_deletable, unable to sift old nodes\n" );
+
+    if ( _validate_args( high_water, low_water, sieve_size ) < 0 ) {
+        return errlog( ERR_CRITICAL, "_validate_args\n" );
+    }
+
+    /* Initialize the two lists maintained by the LRU */
+    if (( list_init( &lru->lru_list, 0 ) < 0 ) || ( list_init( &lru->undeletable_list, 0 ) < 0 )) {
+        return errlog( ERR_CRITICAL, "list_init\n" );
+    }
+
+    /* Initialize the LRU mutex */
+    if ( pthread_mutex_init( &lru->mutex, NULL ) < 0 ) return perrlog( "pthread_mutex_init" );
+
+    lru->length       = 0;
+    lru->high_water   = high_water;
+    lru->low_water    = low_water;
+    lru->sieve_size   = sieve_size;
+    lru->is_deletable = is_deletable;
+    lru->remove       = remove;
+         
+    return 0;
+}
+
+int netcap_lru_config( netcap_lru_t* lru, int high_water, int low_water, int sieve_size )
+{
+    if ( lru == NULL ) return errlogargs();
+    
+    if ( _validate_args( high_water, low_water, sieve_size ) < 0 ) {
+        return errlog( ERR_CRITICAL, "_validate_args\n" );
+    }
+
+    /* Get the mutex lock */
+    if ( pthread_mutex_lock ( &lru->mutex ) < 0 ) return perrlog( "pthread_mutex_lock" );
+    
+    lru->low_water  = low_water;
+    lru->high_water = high_water;
+    lru->sieve_size = sieve_size;
+
+    if ( pthread_mutex_unlock ( &lru->mutex ) < 0 ) return perrlog ( "pthread_mutex_unlock" );
+    
+    return 0;
+}
+
+
+
+/* Add a node to the front of the LRU, node is updated to contain the necessary information for the LRU */
+int netcap_lru_add( netcap_lru_t* lru, netcap_lru_node_t* node, void* data )
+{
+    if ( lru == NULL || node == NULL || data == NULL ) return errlogargs();
+
+    _validate_lru( lru );
+    
+    int _critical_section( void ) {
+        node->state     = _LRU_READY;
+        node->data      = data;
+        if (( node->list_node = list_add_head( &lru->lru_list, node )) == NULL ) {
+            return errlog( ERR_CRITICAL, "list_add_head\n" );
+        }
+        return 0;
+    }
+
+    int ret;
+
+    if ( pthread_mutex_lock( &lru->mutex ) < 0 ) return perrlog( "pthread_mutex_lock\n" );
+    ret = _critical_section();
+    if ( pthread_mutex_unlock( &lru->mutex ) < 0 ) perrlog( "pthread_mutex_unlock\n" );
+    
+    return ret;
+}
+
+
+/* Move a node to the front of the LRU, node should be a value returned from a previous
+ * execution of lru_add */
+int netcap_lru_move_front( netcap_lru_t* lru, netcap_lru_node_t* node )
+{
+    if ( lru == NULL || node == NULL ) return errlogargs();
+
+    _validate_lru( lru );
+    
+    int _critical_section( void ) {
+        /* Node is not ready, can't use it yet anymore */
+        if ( node->state != _LRU_READY ) {
+            debug( _LRU_DEBUG_HIGH, "LRU: Node has already been removed\n" );
+            return 0;
+        }
+        
+        /* Move the node to the front of the list */
+        if ( list_move_head( &lru->lru_list, &node->list_node, node ) < 0 ) {
+            return perrlog( "list_move_head\n" );
+        }
+        return 0;
+    }
+
+    int ret = 0;
+
+    /* Node is not ready, can't use it yet anymore */
+    if ( node->state != _LRU_READY ) {
+        debug( _LRU_DEBUG_HIGH, "LRU: Node has already been removed\n" );
+        return 0;
+    }
+    
+    if ( pthread_mutex_lock( &lru->mutex ) < 0 ) return perrlog( "pthread_mutex_lock\n" );
+    ret = _critical_section();
+    if ( pthread_mutex_unlock( &lru->mutex ) < 0 ) perrlog( "pthread_mutex_unlock\n" );
+    
+    return ret;
+}
+
+
+/* Cut any excessive nodes, and place the results into the node_array */
+int netcap_lru_cut( netcap_lru_t* lru, netcap_lru_node_t** node_array, int node_array_size )
+{
+    if ( lru == NULL || node_array_size < 0 ) return errlogargs();
+    
+    int _critical_section( void ) {
+        int c;
+        int length = 0;
+        int node_count = 0;
+
+        if (( length = list_length ( &lru->lru_list )) < 0 ) return errlog(ERR_CRITICAL,"list_length\n");
+
+        /* Make sure the LRU doesn't get too short */
+        if ( length < _LRU_MIN_LENGTH ) return 0;
+        
+        if ( length < lru->high_water ) {
+            /* No check function, no way to sift nodes */
+            if ( lru->is_deletable == NULL ) {
+                errlog( ERR_WARNING, "NULL is_deletable, unable to sift old nodes\n" );
+                return 0;
+            }
+            
+            /* Leave at most _LRU_MIN_LENGTH nodes on the LRU */
+            c = length - _LRU_MIN_LENGTH;
+            
+            /* Max it out at the size of the sieve */
+            c = ( c < lru->sieve_size ) ? c : lru->sieve_size;
+            debug( _LRU_DEBUG_LOW, "LRU: Update - sifting %d items\n", c );
+            
+            for ( ; ( c-- > 0 ) && ( node_count < node_array_size ) ; ) {
+                int is_deletable;
+                netcap_lru_node_t* node;
+
+                if (( node = list_tail_val( &lru->lru_list )) == NULL ) {
+                    return errlog( ERR_CRITICAL, "list_tail\n" );
+                }
+                
+                if (( is_deletable = lru->is_deletable( node->data )) < 0 ) {
+                    return errlog( ERR_CRITICAL, "lru->is_deletable\n" );
+                }
+                
+                if ( NC_LRU_DONT_DELETE == is_deletable ) {
+                    debug( _LRU_DEBUG_LOW, "LRU: Non-deletable node, continuing to sift\n" );
+                } else {
+                    if ( _lru_remove( lru, node ) < 0 ) return errlog( ERR_CRITICAL, "_lru_remove\n" );
+                    if ( node_array != NULL ) node_array[node_count++] = node;
+                }
+            }
+        } else {
+            /* XXX May need to check for is_deletable */
+            c = length - lru->low_water;
+            debug ( _LRU_DEBUG_LOW, "TRIE: LRU Update - cutting %d items\n", c );
+            
+            for (  ; ( c-- > 0 ) && ( node_count < node_array_size ) ; ) {
+                netcap_lru_node_t* node;
+                
+                if (( node = list_tail_val( &lru->lru_list )) == NULL ) {
+                    return errlog( ERR_CRITICAL, "list_tail_val\n" ); 
+                }
+                
+                if ( _lru_remove( lru, node ) < 0 ) return errlog( ERR_CRITICAL, "_lru_remove\n" );
+                
+                if ( node_array != NULL ) node_array[node_count++] = node;
+            }
+        }
+
+        return node_count;
+    }
+
+    if ( _validate_lru( lru ) < 0 ) return errlog( ERR_CRITICAL, "_validate_lru\n" );
+    
+    if ( node_array != NULL ) bzero( node_array, node_array_size );
+    
+    /* convert from bytes to words */
+    node_array_size = node_array_size / sizeof( netcap_lru_node_t* );
+
+    int ret = 0;
+    
+    if ( pthread_mutex_lock( &lru->mutex ) < 0 ) return perrlog( "pthread_mutex_lock\n" );
+    ret = _critical_section();
+    if ( pthread_mutex_unlock( &lru->mutex ) < 0 ) perrlog( "pthread_mutex_unlock\n" );
+    
+    return ret;
+}
+
+static int _lru_remove( netcap_lru_t* lru, netcap_lru_node_t* node )
+{
+    list_node_t* list_node = node->list_node;
+    
+    if ( NULL == list_node || node->state != _LRU_READY ) {
+        return errlog( ERR_CRITICAL, "Item is not on LRU or LRU is corrupted\n" );
+    }
+
+    /* indicate that the state is removed */
+    node->state = _LRU_REMOVED;
+
+    /* Call the removal function on the data */
+    if ( lru->remove != NULL && lru->remove( node->data ) < 0 ) errlog( ERR_CRITICAL, "lru->remove\n" );
+    
+    /* Actually remove the node from the list */
+    if ( list_remove( &lru->lru_list, list_node ) < 0 ) perrlog( "list_remove\n" );
+
+    /* Null out the list node */
+    list_node = NULL;
+
+    return 0;
+}
---
+++ libnetcap/src/netcap_lru.h
@@ -0,0 +1,71 @@
+#ifndef _NETCAP_LRU_H_
+#define _NETCAP_LRU_H_
+
+#define NC_LRU_DONT_DELETE 0x202A5D72
+
+typedef int  (netcap_lru_check_t)  ( void* data );
+typedef int  (netcap_lru_remove_t) ( void* data );
+
+typedef struct
+{
+    /* Length of the LRU */
+    int length;
+
+    /* List for the LRU */
+    list_t lru_list;
+    
+    /* List of nodes that are permanently non-deletable */
+    list_t undeletable_list;
+
+    /* High water for the LRU */
+    int high_water;
+
+    /* Low water for the LRU */
+    int low_water;
+
+    /* Number of the items to search for one deletable item */
+    int sieve_size;
+    
+    /* A function that determines whether or not an item is deletable */
+    netcap_lru_check_t* is_deletable;
+
+    /* A function to call !before removing the node from the LRU */
+    netcap_lru_remove_t* remove;
+
+    /* Retrieve this when moving a node to the front of the LRU or delete a node */
+    pthread_mutex_t mutex;
+} netcap_lru_t;
+
+
+typedef struct
+{
+    /* Position of the node inside of the LRU */
+    list_node_t* list_node;
+    
+    /* Must have the lock in order to change the state */
+    enum
+    {
+        _LRU_READY   =  0x7E5873CB,  /* Node can be moved to the front of the LRU */
+        _LRU_REMOVED =  0x5D436B9B   /* Once a node is removed, it cannot be put back onto the LRU */
+    } state;
+
+    /* Data associated with this node */
+    void* data;
+} netcap_lru_node_t;
+
+int netcap_lru_init( netcap_lru_t* lru, int high_water, int low_water, int sieve_size, 
+                     netcap_lru_check_t* is_deletable, netcap_lru_remove_t* remove );
+
+int netcap_lru_config( netcap_lru_t* lru, int high_water, int low_water, int sieve_size );
+
+/* Add a node to the front of the LRU, node is updated to contain the necessary information for the LRU */
+int netcap_lru_add( netcap_lru_t* lru, netcap_lru_node_t* node, void* data );
+
+/* Move a node to the front of the LRU, node should be a value returned from a previous
+ * execution of lru_add */
+int netcap_lru_move_front( netcap_lru_t* lru, netcap_lru_node_t* node );
+
+/* Cut any excessive nodes, and place the results into the node_array if it is not null */
+int netcap_lru_cut( netcap_lru_t* lru, netcap_lru_node_t** node_array, int node_array_size );
+
+#endif // _NETCAP_LRU_H_
