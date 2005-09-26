8cc3e9700de0991e59769c778380876d235e901d
John Irwin <jdi@untangle.com>
2005-09-26 18:10:28 +0000

    remove Interface, fixup Direction
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@2694 8ebf8b4e-06f3-0310-961f-89f74618be54

--- mvvm/main/com/metavize/mvvm/tran/Direction.java
+++ mvvm/main/com/metavize/mvvm/tran/Direction.java
@@ -13,61 +13,29 @@ package com.metavize.mvvm.tran;
 
 import java.io.Serializable;
 
+/**
+ * This class represents <b>Data</b> direction, not session direction!
+ *
+ * @author <a href="mailto:jdi@metavize.com">John Irwin</a>
+ * @version 1.0
+ */
 public class Direction implements Serializable
 {
-    private static final long serialVersionUID = 9115074195671091726L;
+    private static final long serialVersionUID = 9115074195671019726L;
 
-    private static final Direction OUTGOING
-        = new Direction("outgoing", Interface.INSIDE, Interface.OUTSIDE);
-    private static final Direction INCOMING
-        = new Direction("incoming", Interface.OUTSIDE, Interface.INSIDE);
+    public static final Direction OUTGOING = new Direction("outgoing");
+    public static final Direction INCOMING = new Direction("incoming");
 
     private final String directionName;
-    private final Interface clientIface;
-    private final Interface serverIface;
 
     // constructors -----------------------------------------------------------
 
-    private Direction(String directionName, Interface clientIface,
-                      Interface serverIface)
+    public Direction(String directionName)
     {
         this.directionName = directionName;
-        this.clientIface = clientIface;
-        this.serverIface = serverIface;
-    }
-
-    // factories --------------------------------------------------------------
-
-    public static final Direction getDirection(Interface clientIface,
-                                               Interface serverIface) {
-        if (Interface.INSIDE == clientIface
-            && Interface.OUTSIDE == serverIface) {
-            return OUTGOING;
-        } else if (Interface.OUTSIDE == clientIface
-                   && Interface.INSIDE == serverIface) {
-            return INCOMING;
-        } else {
-            return new Direction("other", clientIface, serverIface);
-        }
-    }
-
-    public static final Direction getDirection(byte clientIface,
-                                               byte serverIface) {
-        return getDirection(Interface.getInstance(clientIface),
-                            Interface.getInstance(serverIface));
     }
 
     // public methods ---------------------------------------------------------
-
-    public Interface getClientIface()
-    {
-        return clientIface;
-    }
-
-    public Interface getServerIface()
-    {
-        return serverIface;
-    }
 
     public String getDirectionName()
     {
@@ -85,15 +53,12 @@ public class Direction implements Serializable
 
         Direction d = (Direction)o;
 
-        return clientIface == d.clientIface && serverIface == d.serverIface;
+        return directionName.equals(d.directionName);
     }
 
     @Override
     public int hashCode()
     {
-        int result = 17;
-        result = 37 * result + clientIface.hashCode();
-        result = 37 * result + serverIface.hashCode();
-        return result;
+        return directionName.hashCode();
     }
 }
