3b00cecf925da040c5ee0fc888d64196c77fa4c7
Aaron Read <amread@untangle.com>
2005-10-04 02:43:28 +0000

    show a block page when spyware is blocked.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@2818 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/spyware/main/com/metavize/tran/spyware/SpywareHttpHandler.java
+++ tran/spyware/main/com/metavize/tran/spyware/SpywareHttpHandler.java
@@ -11,40 +11,52 @@
 
 package com.metavize.tran.spyware;
 
+import java.net.URI;
 import java.nio.ByteBuffer;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import com.metavize.mvvm.MvvmContextFactory;
 import com.metavize.mvvm.tapi.TCPSession;
 import com.metavize.mvvm.tran.StringRule;
-import com.metavize.tran.http.HttpStateMachine;
+import com.metavize.tran.http.BlockingHttpStateMachine;
 import com.metavize.tran.http.RequestLine;
 import com.metavize.tran.http.StatusLine;
 import com.metavize.tran.token.Chunk;
 import com.metavize.tran.token.EndMarker;
 import com.metavize.tran.token.Header;
 import com.metavize.tran.token.Token;
-import com.metavize.tran.token.TokenResult;
 import com.metavize.tran.util.AsciiCharBuffer;
 import org.apache.log4j.Logger;
 
-public class SpywareHttpHandler extends HttpStateMachine
+public class SpywareHttpHandler extends BlockingHttpStateMachine
 {
     private static final Pattern OBJECT_PATTERN
         = Pattern.compile("<object", Pattern.CASE_INSENSITIVE);
     private static final Pattern CLSID_PATTERN
         = Pattern.compile("clsid:([0-9\\-]*)", Pattern.CASE_INSENSITIVE);
 
+    // XXX someone, make this pretty
+    private static final String BLOCK_TEMPLATE
+        = "<HTML><HEAD>"
+        + "<TITLE>403 Forbidden</TITLE>"
+        + "</HEAD><BODY>"
+        + "<center><b>Metavize Spyware Blocker</b></center>"
+        + "<p>This site blocked because it may be a spyware site.</p>"
+        + "<p>Host: %s</p>"
+        + "<p>URI: %s</p>"
+        + "<p>Please contact your network administrator.</p>"
+        + "<HR>"
+        + "<ADDRESS>Metavize EdgeGuard</ADDRESS>"
+        + "</BODY></HTML>";
+
     private final TCPSession session;
 
     private final List cookieQueue = new LinkedList();
-    private final Queue reqQueue = new LinkedList();
     private final List reqHostQueue = new LinkedList();
 
     private final Logger logger = Logger.getLogger(getClass());
@@ -55,9 +67,6 @@ public class SpywareHttpHandler extends HttpStateMachine
 
     private String extension = "";
     private String mimeType = "";
-    private RequestLine requestRequest;
-    private RequestLine responseRequest;
-    private StatusLine statusLine;
 
     // constructors -----------------------------------------------------------
 
@@ -72,117 +81,124 @@ public class SpywareHttpHandler extends HttpStateMachine
     // HttpStateMachine methods -----------------------------------------------
 
     @Override
-    protected TokenResult doRequestLine(RequestLine requestLine)
+    protected RequestLine doRequestLine(RequestLine requestLine)
     {
         logger.debug("got request line");
-
-        this.requestRequest = requestLine;
-        reqQueue.offer(requestLine);
 
         String path = requestLine.getRequestUri().getPath();
         int i = path.lastIndexOf('.');
         extension = (0 <= i && path.length() - 1 > i)
             ? path.substring(i + 1) : null;
 
-        return new TokenResult(null, new Token[] { requestLine });
+        return requestLine;
     }
 
     @Override
-    protected TokenResult doRequestHeader(Header requestHeader)
+    protected Header doRequestHeader(Header requestHeader)
     {
         logger.debug("got request header");
 
         // XXX we could check the request-uri for an absolute address too...
+        RequestLine requestLine = getRequestLine();
         String host = requestHeader.getValue("host");
-        if (transform.isBlacklistDomain(host, requestRequest.getRequestUri())) {
+        URI uri = requestLine.getRequestUri();
+        if (transform.isBlacklistDomain(host, uri)) {
             SpywareBlacklistEvent evt = new SpywareBlacklistEvent
-                (getSession().id(), requestRequest);
+                (getSession().id(), requestLine);
             eventLogger.info(evt);
             // XXX we could send a page back instead, this isn't really right
             logger.debug("detected spyware, shutting down");
-            session.shutdownServer();
-            session.shutdownClient();
-            session.release();
-            return TokenResult.NONE;
+
+            blockRequest(generateResponse(host, uri.toString(),
+                                          isRequestPersistent()));
+        } else {
+            releaseRequest();
         }
 
-        Header h = clientCookie(requestHeader);
-        return new TokenResult(null, new Token[] { h });
+        return clientCookie(requestLine, requestHeader);
     }
 
     @Override
-    protected TokenResult doRequestBody(Chunk chunk)
+    protected Chunk doRequestBody(Chunk chunk)
     {
-        logger.debug("got request body");
-        return new TokenResult(null, new Token[] { chunk });
+        return chunk;
     }
 
     @Override
-    protected TokenResult doRequestBodyEnd(EndMarker endMarker)
+    protected void doRequestBodyEnd() { }
+
+    @Override
+    protected StatusLine doStatusLine(StatusLine statusLine)
     {
-        logger.debug("got request body end");
-        return new TokenResult(null, new Token[] { endMarker });
+        releaseResponse();
+        return statusLine;
     }
 
     @Override
-    protected TokenResult doStatusLine(StatusLine statusLine)
-    {
-        this.statusLine = statusLine;
-
-        logger.debug("got status line");
-
-        if (100 != statusLine.getStatusCode()) {
-            responseRequest = (RequestLine)reqQueue.remove();
-        }
-
-        return new TokenResult(new Token[] { statusLine }, null);
-    }
-
-    @Override
-    protected TokenResult doResponseHeader(Header header)
+    protected Header doResponseHeader(Header header)
     {
         logger.debug("got response header");
         mimeType = header.getValue("content-type");
 
-        Header h;
-        if (100 != statusLine.getStatusCode()) {
-            h = serverCookie(header);
-            h  = addCookieKillers( h );
-        } else {
-            h = header;
+        if (100 != getStatusLine().getStatusCode()) {
+            header = serverCookie(getResponseRequest(), header);
+            header  = addCookieKillers(header);
         }
 
-        return new TokenResult(new Token[] { h }, null);
+        return header;
     }
 
     @Override
-    protected TokenResult doResponseBody(Chunk chunk)
+    protected Chunk doResponseBody(Chunk chunk)
     {
         logger.debug("got response body");
         if (null != mimeType && mimeType.equalsIgnoreCase("text/html")) {
-            chunk = activeXChunk(chunk);
+            chunk = activeXChunk(getResponseRequest(), chunk);
         }
-        return new TokenResult(new Token[] { chunk }, null);
+
+        return chunk;
     }
 
     @Override
-    protected TokenResult doResponseBodyEnd(EndMarker endMarker)
+    protected void doResponseBodyEnd()
     {
         logger.debug("got response body end");
-        return new TokenResult(new Token[] { endMarker }, null);
     }
 
-    // TokenHandler methods ---------------------------------------------------
+    // private methods --------------------------------------------------------
 
-    @Override
-    public TokenResult releaseFlush()
+    private Token[] generateResponse(String host, String uri,
+                                     boolean persistent)
     {
-        return TokenResult.NONE;
+        String replacement = String.format(BLOCK_TEMPLATE, host, uri);
+
+        Token response[] = new Token[4];
+
+        // XXX make canned responses in constructor
+        // XXX Do template replacement
+        ByteBuffer buf = ByteBuffer.allocate(replacement.length());
+        buf.put(replacement.getBytes()).flip();
+
+        StatusLine sl = new StatusLine("HTTP/1.1", 403, "Forbidden");
+        response[0] = sl;
+
+        Header h = new Header();
+        h.addField("Content-Length", Integer.toString(buf.remaining()));
+        h.addField("Content-Type", "text/html");
+        h.addField("Connection", persistent ? "Keep-Alive" : "Close");
+        response[1] = h;
+
+        Chunk c = new Chunk(buf);
+        response[2] = c;
+
+        response[3] = EndMarker.MARKER;
+
+        return response;
     }
 
     // cookie stuff -----------------------------------------------------------
 
-    private Header clientCookie(Header h)
+    private Header clientCookie(RequestLine requestLine, Header h)
     {
         logger.debug("checking client cookie");
 
@@ -217,7 +233,7 @@ public class SpywareHttpHandler extends HttpStateMachine
                 logger.debug("blocking cookie: " + domain);
                 transform.incrementCount(Spyware.BLOCK);
 
-                eventLogger.info(new SpywareCookieEvent(getSession().id(), requestRequest, domain, true));
+                eventLogger.info(new SpywareCookieEvent(getSession().id(), requestLine, domain, true));
                 i.remove();
                 logger.debug("making cookieKiller: " + domain);
                 cookieKillers.addAll(makeCookieKillers(cookie, host));
@@ -227,7 +243,7 @@ public class SpywareHttpHandler extends HttpStateMachine
         return h;
     }
 
-    private Header serverCookie(Header h)
+    private Header serverCookie(RequestLine rl, Header h)
     {
         logger.debug("checking server cookie");
         // XXX if deferred 0ttl cookie, send it and nullify
@@ -262,7 +278,7 @@ public class SpywareHttpHandler extends HttpStateMachine
             if (badDomain) {
                 logger.debug("cookie deleted: " + domain);
                 transform.incrementCount(Spyware.BLOCK);
-                eventLogger.info(new SpywareCookieEvent(getSession().id(), responseRequest, domain, false));
+                eventLogger.info(new SpywareCookieEvent(getSession().id(), rl, domain, false));
                 i.remove();
             } else {
                 logger.debug("cookie not deleted: " + domain);
@@ -337,7 +353,7 @@ public class SpywareHttpHandler extends HttpStateMachine
 
     // ActiveX stuff ----------------------------------------------------------
 
-    private Chunk activeXChunk(Chunk c)
+    private Chunk activeXChunk(RequestLine rl, Chunk c)
     {
         logger.debug("scanning activeX chunk");
 
@@ -383,7 +399,7 @@ public class SpywareHttpHandler extends HttpStateMachine
             if (block) {
                 logger.debug("blocking activeX");
                 transform.incrementCount(Spyware.BLOCK);
-                eventLogger.info(new SpywareActiveXEvent(getSession().id(), responseRequest, ident));
+                eventLogger.info(new SpywareActiveXEvent(getSession().id(), rl, ident));
                 int len = findEnd(cb, os);
                 if (-1 == len) {
                     logger.warn("chunk does not contain entire tag");
