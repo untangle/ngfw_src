92c19ab565d010ac673d463bb1af68a98d0774ca
Robert Scott <rbscott@untangle.com>
2005-10-27 19:31:39 +0000

    necessary rename
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@3171 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ tran/openvpn/main/com/metavize/tran/vpn/EventHandler.java
@@ -0,0 +1,340 @@
+/*
+ * Copyright (c) 2003, 2004, 2005 Metavize Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Metavize Inc. ("Confidential Information").  You shall
+ * not disclose such Confidential Information.
+ *
+ * $Id$
+ */
+package com.metavize.tran.test;
+
+import java.io.*;
+import java.nio.*;
+
+import com.metavize.mvvm.tapi.*;
+import com.metavize.mvvm.tapi.event.*;
+import com.metavize.mvvm.util.MetaEnv;
+import com.metavize.mvvm.util.SessionUtil;
+import org.apache.log4j.Logger;
+import com.metavize.mvvm.tran.Transform;
+
+public class EventHandler extends AbstractEventHandler
+{
+    private static final long HALF_OPEN_TIMEOUT = 10000; // 10 seconds
+
+    private static final Logger logger
+        = Logger.getLogger(EventHandler.class);
+
+    private TestSettings settings;
+
+    public EventHandler(Transform transform, TestSettings settings)
+    {
+        super(transform);
+
+        if (settings == null) {
+            throw new IllegalArgumentException("No settings");
+        }
+
+        this.settings = settings;
+    }
+
+    void setSettings(TestSettings settings)
+    {
+        this.settings = settings;
+    }
+
+    private int testBufferSize(TCPSession sess)
+    {
+        if (settings.getRandomBufferSizes()) {
+            return MetaEnv.rng()
+                .nextInt(settings.getMaxRandomBufferSize()
+                         - settings.getMinRandomBufferSize() + 1)
+                + settings.getMinRandomBufferSize();
+        } else {
+            return sess.clientReadBufferSize();
+        }
+    }
+
+    private Mode getLocalMode(boolean requestTime)
+    {
+        Mode[] modes = new Mode[10];
+        int i=0;
+
+        if (settings.isNormal()) {
+            modes[i++] = Mode.NORMAL;
+        }
+
+        if (settings.isBuffered()) {
+            modes[i++] = Mode.BUFFERED;
+        }
+
+        if (requestTime && settings.getRelease()) {
+            modes[i++] = Mode.RELEASE;
+        }
+
+        i = MetaEnv.rng().nextInt(i);
+        return modes[i];
+    }
+
+    public void handleTCPNewSessionRequest (TCPNewSessionRequestEvent event)
+    {
+        TCPNewSessionRequest sessReq = event.sessionRequest();
+
+        if (!settings.isQuiet())
+            logger.debug("New TCP Session Request " +
+                               sessReq.clientAddr().getHostAddress() + ":" + sessReq.clientPort() + " -> " +
+                               sessReq.serverAddr().getHostAddress() + ":" + sessReq.serverPort());
+
+        Mode localMode = getLocalMode(true);
+
+        if (Mode.RELEASE == localMode) {
+            logger.debug("Releasing New TCP Session ");
+            sessReq.release();
+            return;
+        }
+    }
+
+    public void handleTCPNewSession (TCPSessionEvent event)
+    {
+        TCPSession sess = event.session();
+        Mode localMode = getLocalMode(false);
+        // Pick session parameters at random to allow testing more codepaths.
+        int crbs = testBufferSize(sess);
+        int srbs = testBufferSize(sess);
+        sess.clientReadBufferSize(crbs);
+        sess.serverReadBufferSize(srbs);
+        if (Mode.BUFFERED == localMode) {
+            if (!settings.isQuiet())
+                logger.debug(" Type: Line Buffered");
+            sess.clientLineBuffering(true);
+            sess.serverLineBuffering(true);
+        }
+        TestSessionState sss = new TestSessionState();
+        sss.localMode = localMode;
+        sess.attach(sss);
+        if (!settings.isQuiet())
+            logger.debug("New TCP Session " +
+                               sess.clientAddr().getHostAddress() + ":" + sess.clientPort() + " -> " +
+                               sess.serverAddr().getHostAddress() + ":" + sess.serverPort());
+    }
+
+    public void handleUDPNewSessionRequest (UDPNewSessionRequestEvent event)
+    {
+        UDPNewSessionRequest sessReq = event.sessionRequest();
+
+        if (!settings.isQuiet())
+            logger.debug("New UDP Session Request " +
+                               sessReq.clientAddr().getHostAddress() + ":" + sessReq.clientPort() + " -> " +
+                               sessReq.serverAddr().getHostAddress() + ":" + sessReq.serverPort());
+
+        Mode localMode = getLocalMode(true);
+
+        if (Mode.RELEASE == localMode) {
+            logger.debug("Releasing New UDP Session ");
+            sessReq.release();
+            return;
+        }
+    }
+
+    public void handleUDPNewSession (UDPSessionEvent event)
+    {
+        UDPSession sess = event.session();
+        Mode localMode = getLocalMode(false);
+
+        int maxPacketSize = 65536;
+        sess.clientMaxPacketSize(maxPacketSize);
+        sess.serverMaxPacketSize(maxPacketSize);
+
+        TestSessionState sss = new TestSessionState();
+        sss.localMode = localMode;
+        sess.attach(sss);
+        if (!settings.isQuiet())
+            logger.debug("New UDP Session " +
+                               sess.clientAddr().getHostAddress() + ":" + sess.clientPort() + " -> " +
+                               sess.serverAddr().getHostAddress() + ":" + sess.serverPort());
+    }
+
+    public void handleUDPClientExpired(UDPSessionEvent event)
+        throws MPipeException
+    {
+        UDPSession sess = event.session();
+        if (!settings.isQuiet())
+            logger.debug("UDP Client Expired " +
+                               sess.clientAddr().getHostAddress() + ":" + sess.clientPort() + " -> " +
+                               sess.serverAddr().getHostAddress() + ":" + sess.serverPort());
+        super.handleUDPClientExpired(event);
+    }
+
+    public void handleUDPServerExpired(UDPSessionEvent event)
+        throws MPipeException
+    {
+        UDPSession sess = event.session();
+        if (!settings.isQuiet())
+            logger.debug("UDP Server Expired " +
+                               sess.clientAddr().getHostAddress() + ":" + sess.clientPort() + " -> " +
+                               sess.serverAddr().getHostAddress() + ":" + sess.serverPort());
+        super.handleUDPServerExpired(event);
+    }
+
+    public void handleTCPClientRST(TCPSessionEvent event)
+        throws MPipeException
+    {
+        TCPSession sess = event.session();
+        if (!settings.isQuiet())
+            logger.warn("TCP Client RST " +
+                               sess.clientAddr().getHostAddress() + ":" + sess.clientPort() + " -> " +
+                               sess.serverAddr().getHostAddress() + ":" + sess.serverPort());
+        super.handleTCPClientRST(event);
+    }
+
+    public void handleTCPServerRST(TCPSessionEvent event)
+        throws MPipeException
+    {
+        TCPSession sess = event.session();
+        if (!settings.isQuiet())
+            logger.warn("TCP Server RST " +
+                               sess.clientAddr().getHostAddress() + ":" + sess.clientPort() + " -> " +
+                               sess.serverAddr().getHostAddress() + ":" + sess.serverPort());
+        super.handleTCPServerRST(event);
+    }
+
+    public void handleTCPClientFIN(TCPSessionEvent event)
+        throws MPipeException
+    {
+        TCPSession sess = event.session();
+        if (!settings.isQuiet())
+            logger.debug("TCP Client Input Shutdown " +
+                               sess.clientAddr().getHostAddress() + ":" + sess.clientPort() + " -> " +
+                               sess.serverAddr().getHostAddress() + ":" + sess.serverPort());
+        super.handleTCPClientFIN(event);
+        sess.scheduleTimer(HALF_OPEN_TIMEOUT);
+    }
+
+    public void handleTCPServerFIN(TCPSessionEvent event)
+        throws MPipeException
+    {
+        TCPSession sess = event.session();
+        if (!settings.isQuiet())
+            logger.debug("TCP Server Input Shutdown " +
+                               sess.clientAddr().getHostAddress() + ":" + sess.clientPort() + " -> " +
+                               sess.serverAddr().getHostAddress() + ":" + sess.serverPort());
+        super.handleTCPServerFIN(event);
+        sess.scheduleTimer(HALF_OPEN_TIMEOUT);
+    }
+
+    public void handleTCPFinalized(TCPSessionEvent event)
+        throws MPipeException
+    {
+        TCPSession sess = event.session();
+        if (!settings.isQuiet())
+            logger.debug("TCP Finalized " +
+                               sess.clientAddr().getHostAddress() + ":" + sess.clientPort() + " -> " +
+                               sess.serverAddr().getHostAddress() + ":" + sess.serverPort());
+        super.handleTCPFinalized(event);
+    }
+
+    public void handleUDPFinalized(UDPSessionEvent event)
+        throws MPipeException
+    {
+        UDPSession sess = event.session();
+        if (!settings.isQuiet())
+            logger.debug("UDP Finalized " +
+                               sess.clientAddr().getHostAddress() + ":" + sess.clientPort() + " -> " +
+                               sess.serverAddr().getHostAddress() + ":" + sess.serverPort());
+        super.handleUDPFinalized(event);
+    }
+
+    public void handleTimer(IPSessionEvent event)
+    {
+        IPSession sess = event.ipsession();
+        logger.debug("Timing out session in client state " +
+                           SessionUtil.prettyState(sess.clientState()) + " server state " +
+                           SessionUtil.prettyState(sess.serverState()) + " after " +
+                           HALF_OPEN_TIMEOUT + "ms of inactivity");
+        // XXXX
+        // sess.close();
+    }
+
+    public IPDataResult handleTCPClientChunk(TCPChunkEvent event)
+    {
+        TCPSession sess = event.session();
+        ByteBuffer buff = event.chunk();
+        if (sess.clientState() == TCPSessionDesc.HALF_OPEN_OUTPUT) {
+            // Reset the timer.
+            sess.scheduleTimer(HALF_OPEN_TIMEOUT);
+            if (!settings.isQuiet())
+                logger.debug("Resetting half open timer");
+        }
+        if (!settings.isQuiet())
+            logger.debug("Passing chunk size " + buff.remaining() + " bytes to Server");
+        return IPDataResult.PASS_THROUGH;
+        // if (sess.doubleBuffered()) {
+        // copy it
+        // return new IPDataResult(new ByteBuffer[] { copy_of_chunk });
+    }
+
+    public IPDataResult handleTCPServerChunk(TCPChunkEvent event)
+    {
+        TCPSession sess = event.session();
+        ByteBuffer buff = event.chunk();
+        if (sess.serverState() == TCPSessionDesc.HALF_OPEN_OUTPUT) {
+            // Reset the timer.
+            sess.scheduleTimer(HALF_OPEN_TIMEOUT);
+            if (!settings.isQuiet())
+                logger.debug("Resetting half open timer");
+        }
+        if (!settings.isQuiet())
+            logger.debug("Passing chunk size " + buff.remaining() + " bytes to Client");
+        return IPDataResult.PASS_THROUGH;
+        // if (sess.doubleBuffered()) {
+        // copy it
+        // return new IPDataResult(new ByteBuffer[] { copy_of_chunk });
+    }
+
+    public void handleUDPClientPacket(UDPPacketEvent event)
+        throws MPipeException
+    {
+        UDPSession sess = event.session();
+        ByteBuffer packet = event.packet();
+        if (!settings.isQuiet())
+            logger.debug("Passing packet size " + packet.remaining() + " bytes to server");
+        // if (sess.doubleBuffered()) {
+        // copy it
+        // return new IPDataResult(new ByteBuffer[] { copy_of_packet });
+
+    // Send it through;
+    super.handleUDPClientPacket(event);
+    }
+
+    public void handleUDPServerPacket(UDPPacketEvent event)
+        throws MPipeException
+    {
+        UDPSession sess = event.session();
+        ByteBuffer packet = event.packet();
+        if (!settings.isQuiet())
+            logger.debug("Passing packet size " + packet.remaining() + " bytes to client");
+        // if (sess.doubleBuffered()) {
+        // copy it
+        // return new IPDataResult(new ByteBuffer[] { copy_of_packet });
+
+    // Send it through;
+    super.handleUDPServerPacket(event);
+    }
+
+    static class TestSessionState {
+
+        public static final int NO_LEFTOVER = -1;
+
+        // For double-buffered sessions, if we couldn't write the whole read
+        // buffer to the write buffer then we left off at this position.  Set
+        // to NO_LEFTOVER.
+        int serverReadPosition = NO_LEFTOVER;
+
+        int clientReadPosition = NO_LEFTOVER;
+
+        Mode localMode;
+    }
+}
+
---
+++ tran/openvpn/main/com/metavize/tran/vpn/TestSettings.java
@@ -0,0 +1,226 @@
+/*
+ * Copyright (c) 2004, 2005 Metavize Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Metavize Inc. ("Confidential Information").  You shall
+ * not disclose such Confidential Information.
+ *
+ * $Id$
+ */
+
+package com.metavize.tran.test;
+
+import java.io.Serializable;
+
+import com.metavize.mvvm.security.Tid;
+
+/**
+ * Settings for the Test Transform.
+ *
+ * @author <a href="mailto:amread@metavize.com">Aaron Read</a>
+ * @version 1.0
+ * @hibernate.class
+ * table="TR_TEST_SETTINGS"
+ */
+public class TestSettings implements Serializable
+{
+    private static final long serialVersionUID = 4143567998376955882L;
+
+    private Long id;
+    private Tid tid;
+    private Mode mode = Mode.NORMAL;
+    // private boolean doubleEnded = true;
+    private boolean buffered = true;
+    private boolean normal = true;
+    private boolean release = true;
+    private boolean quiet = false;
+    private boolean randomBufferSizes = false;
+    private int minRandomBufferSize = 63;
+    private int maxRandomBufferSize = 16500;
+
+    public TestSettings () { }
+
+    public TestSettings( Tid tid)
+    {
+    this.tid = tid;
+    }
+
+    public void resetSettings()
+    {
+        release = false;
+        normal = false;
+        // doubleEnded = false;
+        buffered = false;
+    }
+
+    /**
+     * @hibernate.id
+     * column="ID"
+     * generator-class="native"
+     */
+    private Long getId()
+    {
+        return id;
+    }
+
+    private void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    /**
+     * Transform id for these settings.
+     *
+     * @return tid for these settings
+     * @hibernate.many-to-one
+     * column="TID"
+     * not-null="true"
+     */
+    public Tid getTid()
+    {
+        return tid;
+    }
+
+    public void setTid(Tid tid)
+    {
+        this.tid = tid;
+    }
+
+    /**
+     * Mode to operate in.
+     *
+     * @return mode.
+     * @hibernate.property
+     * type="com.metavize.tran.test.ModeUserType"
+     * column="MODE"
+     */
+    public Mode getMode()
+    {
+        return mode;
+    }
+
+    public void setMode(Mode mode)
+    {
+        this.mode = mode;
+    }
+
+    /**
+     * Buffered?
+     *
+     * @return true if buffered.
+     * @hibernate.property
+     * column="BUFFERED"
+     */
+    public boolean isBuffered()
+    {
+        return buffered;
+    }
+
+   public void setBuffered(boolean buffered)
+    {
+        this.buffered = buffered;
+    }
+
+    /**
+     * Normal?
+     *
+     * @return a <code>boolean</code> value
+     * @hibernate.property
+     * column="NORMAL"
+     */
+    public boolean isNormal()
+    {
+        return normal;
+    }
+
+    public void setNormal(boolean normal)
+    {
+        this.normal = normal;
+    }
+
+    /**
+     * Release sessions, true by default.
+     *
+     * @return true if sessions are released.
+     * @hibernate.property
+     * column="RELEASE"
+     */
+    public boolean getRelease()
+    {
+        return release;
+    }
+
+    public void setRelease(boolean release)
+    {
+        this.release = release;
+    }
+
+    /**
+     * By default, logs lots of info, quiet suppresses this.
+     *
+     * @return true
+     * @hibernate.property
+     * column="QUIET"
+     */
+    public boolean isQuiet()
+    {
+        return quiet;
+    }
+
+    public void setQuiet(boolean quiet)
+    {
+        this.quiet = quiet;
+    }
+
+    /**
+     * Use random buffer sizes, false by default.
+     *
+     * @return true for random buffer sizes.
+     */
+    public boolean getRandomBufferSizes()
+    {
+        return randomBufferSizes;
+    }
+
+    public void setRandomBufferSizes(boolean randomBufferSizes)
+    {
+        this.randomBufferSizes = randomBufferSizes;
+    }
+
+    /**
+     * Minimum buffer size when using random buffer sizes. Default is
+     * 63.
+     *
+     * @return minimum buffer size.
+     * @hibernate.property
+     * column="MIN_RANDOM_BUFFER_SIZE"
+     */
+    public int getMinRandomBufferSize()
+    {
+        return minRandomBufferSize;
+    }
+
+    public void setMinRandomBufferSize(int minRandomBufferSize)
+    {
+        this.minRandomBufferSize = minRandomBufferSize;
+    }
+
+    /**
+     * Maximum buffer size when using random buffer sizes. Default is
+     * 16500;
+     *
+     * @return maximum buffer size.
+     * @hibernate.property
+     * column="MAX_RANDOM_BUFFER_SIZE"
+     */
+    public int getMaxRandomBufferSize()
+    {
+        return maxRandomBufferSize;
+    }
+
+    public void setMaxRandomBufferSize(int maxRandomBufferSize)
+    {
+        this.maxRandomBufferSize = maxRandomBufferSize;
+    }
+}
