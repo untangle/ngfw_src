54067af113560fe5b819bcd341916a224a00815f
Aaron Read <amread@untangle.com>
2005-11-14 21:48:11 +0000

    new threading model
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@3396 8ebf8b4e-06f3-0310-961f-89f74618be54

--- mvvm/main/com/metavize/mvvm/logging/EventLogger.java
+++ mvvm/main/com/metavize/mvvm/logging/EventLogger.java
@@ -12,14 +12,17 @@
 package com.metavize.mvvm.logging;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 import com.metavize.mvvm.MvvmContextFactory;
+import com.metavize.mvvm.security.Tid;
 import com.metavize.mvvm.tran.TransformContext;
 import com.metavize.mvvm.util.TransactionWork;
 import org.apache.log4j.Logger;
@@ -29,37 +32,29 @@ public class EventLogger<E extends LogEvent> implements EventManager<E>
 {
     private static final int QUEUE_SIZE = 10000;
     private static final int BATCH_SIZE = QUEUE_SIZE;
-    private static final int SYNC_TIME = 300000;
+    private static final int SYNC_TIME = 600000;
 
+    private static final Map<Tid, Worker> WORKERS = new HashMap<Tid, Worker>();
     private static final Object LOG_LOCK = new Object();
 
     private final List<EventCache<E>> caches = new LinkedList<EventCache<E>>();
     private final TransformContext transformContext;
-    private final String tag;
-
-    private final BlockingQueue<E> inputQueue
-        = new LinkedBlockingQueue<E>(QUEUE_SIZE);
-    private final Worker worker = new Worker();
-    private final SyslogManager syslogManager = MvvmContextFactory.context()
-        .syslogManager();
 
     private final Logger logger = Logger.getLogger(getClass());
 
     private volatile int limit = 100;
+    private BlockingQueue<EventDesc> inputQueue;
 
     // constructors -----------------------------------------------------------
 
     public EventLogger()
     {
         this.transformContext = null;
-        this.tag = "mvvm[0]: ";
     }
 
     public EventLogger(TransformContext transformContext)
     {
         this.transformContext = transformContext;
-        this.tag = transformContext.getTransformDesc().getName()
-            + "[" + transformContext.getTid().getId() + "]: ";
     }
 
     // EventManager methods ---------------------------------------------------
@@ -120,19 +115,37 @@ public class EventLogger<E extends LogEvent> implements EventManager<E>
 
     public void log(E e)
     {
-        if (!inputQueue.offer(e)) {
+        if (!inputQueue.offer(new EventDesc(this, e))) {
             logger.warn("dropping logevent: " + e);
         }
     }
 
     public void start()
     {
-        new Thread(worker).start();
+        Tid tid = null == transformContext ? null : transformContext.getTid();
+        synchronized (WORKERS) {
+            Worker w = WORKERS.get(tid);
+            if (null == w) {
+                w = new Worker(transformContext);
+                WORKERS.put(tid, w);
+                w.start();
+            }
+
+            this.inputQueue = w.getInputQueue();
+        }
     }
 
     public void stop()
     {
-        worker.stop();
+        Tid tid = null == transformContext ? null : transformContext.getTid();
+        synchronized (WORKERS) {
+            inputQueue = null;
+            Worker w = WORKERS.get(tid);
+            w.stop();
+            if (!w.isLive()) {
+                WORKERS.remove(tid);
+            }
+        }
     }
 
     // package protected methods ----------------------------------------------
@@ -142,13 +155,44 @@ public class EventLogger<E extends LogEvent> implements EventManager<E>
         return transformContext;
     }
 
+    private void doLog(LogEvent e)
+    {
+        for (EventCache<E> ec : caches) {
+            ec.log((E)e);
+        }
+    }
+
     // private classes --------------------------------------------------------
 
-    private class Worker implements Runnable
+    private static class Worker implements Runnable
     {
+        private final List<LogEvent> logQueue = new LinkedList<LogEvent>();
+        private final TransformContext transformContext;
+        private final BlockingQueue<EventDesc> inputQueue
+            = new LinkedBlockingQueue<EventDesc>();
+        private final String tag;
+
+        private final SyslogManager syslogManager = MvvmContextFactory
+            .context().syslogManager();
+
+        private final Logger logger = Logger.getLogger(getClass());
+
+        private int clientCount = 0;
+
         private volatile Thread thread;
 
-        List<E> logQueue = new LinkedList<E>();
+        // constructors -------------------------------------------------------
+
+        Worker(TransformContext transformContext)
+        {
+            this.transformContext = transformContext;
+            if (null == transformContext) {
+                this.tag = "mvvm[0]: ";
+            } else {
+                this.tag = transformContext.getTransformDesc().getName()
+                    + "[" + transformContext.getTid().getId() + "]: ";
+            }
+        }
 
         // Runnable methods ---------------------------------------------------
 
@@ -163,16 +207,18 @@ public class EventLogger<E extends LogEvent> implements EventManager<E>
                 long t = System.currentTimeMillis();
 
                 if (t < nextSync) {
-                    E e;
+                    EventDesc ed;
                     try {
-                        e = inputQueue.poll(nextSync - t, TimeUnit.MILLISECONDS);
+                        ed = inputQueue.poll(nextSync - t, TimeUnit.MILLISECONDS);
                     } catch (InterruptedException exn) {
                         continue;
                     }
 
-                    if (null == e) {
+                    if (null == ed) {
                         continue;
                     }
+
+                    LogEvent e = ed.getLogEvent();
 
                     logQueue.add(e);
 
@@ -182,9 +228,7 @@ public class EventLogger<E extends LogEvent> implements EventManager<E>
                         logger.warn("failed to send syslog", exn);
                     }
 
-                    for (EventCache<E> ec : caches) {
-                        ec.log(e);
-                    }
+                    ed.getEventLogger().doLog(e);
                 }
 
                 if (logQueue.size() >= BATCH_SIZE || t >= nextSync) {
@@ -210,9 +254,9 @@ public class EventLogger<E extends LogEvent> implements EventManager<E>
                 {
                     public boolean doWork(Session s)
                     {
-                        for (Iterator<E> i = logQueue.iterator();
+                        for (Iterator<LogEvent> i = logQueue.iterator();
                              i.hasNext(); ) {
-                            E e = i.next();
+                            LogEvent e = i.next();
                             s.save(e);
                             i.remove();
                         }
@@ -230,13 +274,58 @@ public class EventLogger<E extends LogEvent> implements EventManager<E>
             }
         }
 
-        // public methods -----------------------------------------------------
+        // package protected methods ------------------------------------------
 
-        public void stop()
+        void start()
         {
-            Thread t = thread;
-            thread = null;
-            t.interrupt();
+            if (0 == clientCount) {
+                new Thread(this).start();
+            }
+
+            clientCount++;
+        }
+
+        void stop()
+        {
+            clientCount--;
+
+            if (0 == clientCount) {
+                Thread t = thread;
+                thread = null;
+                t.interrupt();
+            }
+        }
+
+        public boolean isLive()
+        {
+            return null != thread;
+        }
+
+        public BlockingQueue<EventDesc> getInputQueue()
+        {
+            return inputQueue;
+        }
+    }
+
+    private static class EventDesc
+    {
+        private final EventLogger eventLogger;
+        private final LogEvent logEvent;
+
+        EventDesc(EventLogger eventLogger, LogEvent logEvent)
+        {
+            this.eventLogger = eventLogger;
+            this.logEvent = logEvent;
+        }
+
+        EventLogger getEventLogger()
+        {
+            return eventLogger;
+        }
+
+        LogEvent getLogEvent()
+        {
+            return logEvent;
         }
     }
 }
