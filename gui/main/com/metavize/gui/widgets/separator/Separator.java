3996f098d0c4749454c38343d6c1bcd94f825d43
Ian Nieves <inieves@untangle.com>
2005-12-06 05:03:54 +0000

    added new IPS icon added arrow in rack separator
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@3782 8ebf8b4e-06f3-0310-961f-89f74618be54

--- gui/main/com/metavize/gui/widgets/separator/Separator.java
+++ gui/main/com/metavize/gui/widgets/separator/Separator.java
@@ -17,6 +17,7 @@ import java.awt.*;
 public class Separator extends JLabel {
 
     private static ImageIcon backgroundImageIcon;
+    private static ImageIcon arrowImageIcon;
     private String foregroundText = "";
     private static Color foregroundTextColor;
     private static Font foregroundTextFont;
@@ -28,8 +29,9 @@ public class Separator extends JLabel {
     private void init(){
 	if( backgroundImageIcon == null ){
 	    backgroundImageIcon = new ImageIcon( getClass().getResource("/com/metavize/gui/widgets/separator/PlainSeparator688x50.png") );
+	    arrowImageIcon = new ImageIcon( getClass().getResource("/com/metavize/gui/widgets/separator/SeparatorArrow.png") );
 	    foregroundTextColor = new Color(129,129,129);
-	    foregroundTextFont = new Font("Arial",Font.PLAIN, 30);
+	    foregroundTextFont = new Font("Arial",Font.PLAIN, 20);
 	}
 	super.setOpaque(false);
 	super.setIcon(backgroundImageIcon);
@@ -43,12 +45,14 @@ public class Separator extends JLabel {
     public void paint(Graphics g){
 	super.paint(g);
 	if( foregroundText.length() > 0 ){
+	    g.drawImage(arrowImageIcon.getImage(), 67, 20, (java.awt.image.ImageObserver)null);
 	    Graphics2D g2 = (Graphics2D) g;
 	    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
 	    g.setColor(foregroundTextColor);
 	    g.setFont(foregroundTextFont);
-	    g.drawString(foregroundText, 51, 35);
+	    // g.drawString(foregroundText, 51, 32);
+	    g.drawString(foregroundText, 88, 32);
 	}
     }
 }
