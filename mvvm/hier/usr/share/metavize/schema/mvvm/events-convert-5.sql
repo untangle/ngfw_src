b7b513b764b3fe1bce5c266fbcc194698648d4f4
Aaron Read <amread@untangle.com>
2005-12-13 18:54:17 +0000

    oopies
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@3949 8ebf8b4e-06f3-0310-961f-89f74618be54

--- mvvm/hier/usr/share/metavize/schema/mvvm/events-convert-5.sql
+++ mvvm/hier/usr/share/metavize/schema/mvvm/events-convert-5.sql
@@ -7,7 +7,7 @@
 DROP TABLE events.mvvm_tmp;
 
 CREATE TABLE events.mvvm_tmp AS
-    SELECT evt.event_id, CAST (evt.time_stamp AS timestamp without timezone), endp.event_id AS pl_endp_id,
+    SELECT evt.event_id, CAST (evt.time_stamp AS timestamp without time zone), endp.event_id AS pl_endp_id,
            raze_date, c2p_bytes, s2p_bytes, p2c_bytes, p2s_bytes,
            c2p_chunks, s2p_chunks, p2c_chunks, p2s_chunks
     FROM events.pl_stats evt JOIN events.pl_endp endp USING (session_id);
