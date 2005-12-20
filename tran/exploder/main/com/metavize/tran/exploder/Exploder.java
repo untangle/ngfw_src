43c42b302825be4aee05b0ef6c18fd5665b679b4
Aaron Read <amread@untangle.com>
2005-12-20 21:11:31 +0000

    kaka
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@4018 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ tran/exploder/main/com/metavize/tran/exploder/Exploder.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2005 Metavize Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Metavize Inc. ("Confidential Information").  You shall
+ * not disclose such Confidential Information.
+ *
+ * $Id$
+ */
+
+package com.metavize.tran.exploder;
+
+public interface Exploder
+{
+}
