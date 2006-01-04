da403b028dd1f333c2dd599add468c8837e73f2d
Aaron Read <amread@untangle.com>
2006-01-04 21:08:12 +0000

    reduce wasted memory
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@4075 8ebf8b4e-06f3-0310-961f-89f74618be54

--- mvvm/main/com/metavize/tran/util/AsciiString.java
+++ mvvm/main/com/metavize/tran/util/AsciiString.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005 Metavize Inc.
+ * Copyright (c) 2005, 2006 Metavize Inc.
  * All rights reserved.
  *
  * This software is the confidential and proprietary information of
@@ -14,42 +14,37 @@ package com.metavize.tran.util;
 public class AsciiString implements CharSequence
 {
     private final byte[] val;
-    private final int offset;
-    private final int length;
 
     public AsciiString(byte[] val)
     {
-        this.offset = 0;
-        this.length = val.length;
-        this.val = new byte[length];
+        this.val = new byte[val.length];
         System.arraycopy(val, 0, this.val, 0, val.length);
     }
 
     // for subsequence
     private AsciiString(byte[] val, int offset, int length)
     {
-        this.val = val;
-        this.offset = offset;
-        this.length = length;
+        this.val = new byte[length];
+        System.arraycopy(val, 0, this.val, offset, length);
     }
 
     public char charAt(int i)
     {
-        return (char)val[offset + i];
+        return (char)val[i];
     }
 
     public int length()
     {
-        return length;
+        return val.length;
     }
 
     public CharSequence subSequence(int start, int end)
     {
-        return new AsciiString(val, offset + start, end - start);
+        return new AsciiString(val, start, end - start);
     }
 
     public String toString()
     {
-        return new String(val, offset, length);
+        return new String(val);
     }
 }
