1b8193bdf6efde9d5fb0e53433996bd86e46cd42
Aaron Read <amread@untangle.com>
2006-02-07 19:57:19 +0000

    fixed output
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@4471 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/firewall/main/com/metavize/tran/firewall/gui/LogJPanel.java
+++ tran/firewall/main/com/metavize/tran/firewall/gui/LogJPanel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005 Metavize Inc.
+ * Copyright (c) 2005, 2006 Metavize Inc.
  * All rights reserved.
  *
  * This software is the confidential and proprietary information of
@@ -17,10 +17,10 @@ import javax.swing.event.ChangeListener;
 import javax.swing.table.*;
 
 import com.metavize.gui.transform.*;
-import com.metavize.gui.widgets.editTable.*;
 import com.metavize.gui.util.Util;
-import com.metavize.mvvm.logging.EventRepository;
+import com.metavize.gui.widgets.editTable.*;
 import com.metavize.mvvm.logging.EventManager;
+import com.metavize.mvvm.logging.EventRepository;
 import com.metavize.mvvm.logging.RepositoryDesc;
 import com.metavize.mvvm.tran.PipelineEndpoints;
 import com.metavize.mvvm.tran.Transform;
@@ -86,7 +86,7 @@ public class LogJPanel extends MLogTableJPanel {
                 event.add( null == pe ? "" : (pe.getCClientAddr().getHostAddress() + ":" + Integer.toString(pe.getCClientPort())) );
                 event.add( "rule #" + requestLog.getRuleIndex() );
                 event.add( null == pe ? "" : pe.getDirectionName() );
-                event.add( null == pe ? "" : (pe.getSServerAddr() + ":" + (Integer.toString(pe.getSServerPort()))) );
+                event.add( null == pe ? "" : (pe.getSServerAddr().getHostAddress() + ":" + (Integer.toString(pe.getSServerPort()))) );
                 allEvents.add( event );
             }
 
