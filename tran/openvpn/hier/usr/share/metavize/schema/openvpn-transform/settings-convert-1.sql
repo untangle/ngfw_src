c8465858ce09a6ff7321a9a6f9d19a9f726a78f6
Cho Ng <cng@untangle.com>
2006-02-10 02:12:48 +0000

    fix typo in conversion sql script
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@4551 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/openvpn/hier/usr/share/metavize/schema/openvpn-transform/settings-convert-1.sql
+++ tran/openvpn/hier/usr/share/metavize/schema/openvpn-transform/settings-convert-1.sql
@@ -2,5 +2,5 @@
 
 -- New column by-group (pool) for enabling client-side DNS
 -- Set to false by default
-update table settings.tr_openvpn_group add column use_dns bool;
+alter table settings.tr_openvpn_group add column use_dns bool;
 update settings.tr_openvpn_group set use_dns=false;
