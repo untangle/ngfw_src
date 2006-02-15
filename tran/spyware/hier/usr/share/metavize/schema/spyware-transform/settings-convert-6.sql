c936dbc5c1d674e47b2d00172d698112bcaa6b82
Aaron Read <amread@untangle.com>
2006-02-15 18:37:39 +0000

    force spyware lists to reinitialize
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@4604 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/spyware/hier/usr/share/metavize/schema/spyware-transform/settings-convert-6.sql
+++ tran/spyware/hier/usr/share/metavize/schema/spyware-transform/settings-convert-6.sql
@@ -1,1 +1,6 @@
 -- convert script for release 3.2
+
+-- force spyware lists to reinitialize
+UPDATE tr_spyware_settings set cookie_version = -1;
+UPDATE tr_spyware_settings set activex_version = -1
+UPDATE tr_spyware_settings set subnet_version = -1;
