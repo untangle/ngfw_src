9cfdddbe117f7c3ed930bfed7ac1d1ffdc35be6b
John Irwin <jdi@untangle.com>
2006-02-25 00:45:03 +0000

    missing semicolon!
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@4873 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/spyware/hier/usr/share/metavize/schema/spyware-transform/settings-convert-6.sql
+++ tran/spyware/hier/usr/share/metavize/schema/spyware-transform/settings-convert-6.sql
@@ -2,5 +2,5 @@
 
 -- force spyware lists to reinitialize
 UPDATE tr_spyware_settings set cookie_version = -1;
-UPDATE tr_spyware_settings set activex_version = -1
+UPDATE tr_spyware_settings set activex_version = -1;
 UPDATE tr_spyware_settings set subnet_version = -1;
