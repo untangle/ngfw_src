e2d40f8409296b0d00945f3480fafe6ef3afb0f7
John Irwin <jdi@untangle.com>
2006-03-02 18:32:14 +0000

    add indices for conversion speed
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@4975 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/spyware/hier/usr/share/metavize/schema/spyware-transform/settings-convert-6.sql
+++ tran/spyware/hier/usr/share/metavize/schema/spyware-transform/settings-convert-6.sql
@@ -4,3 +4,12 @@
 UPDATE tr_spyware_settings set cookie_version = -1;
 UPDATE tr_spyware_settings set activex_version = -1;
 UPDATE tr_spyware_settings set subnet_version = -1;
+
+-- indices
+
+CREATE INDEX idx_spyware_rule_ar ON settings.tr_spyware_ar (rule_id);
+
+CREATE INDEX idx_spyware_rule_cr ON settings.tr_spyware_cr (rule_id);
+
+CREATE INDEX idx_spyware_rule_sr ON settings.tr_spyware_sr (rule_id);
+
--- tran/spyware/hier/usr/share/metavize/schema/spyware-transform/settings-schema-6.sql
+++ tran/spyware/hier/usr/share/metavize/schema/spyware-transform/settings-schema-6.sql
@@ -55,6 +55,14 @@ CREATE TABLE settings.tr_spyware_wl (
 -- constraints |
 ----------------
 
+-- indices
+
+CREATE INDEX idx_spyware_rule_ar ON settings.tr_spyware_ar (rule_id);
+
+CREATE INDEX idx_spyware_rule_cr ON settings.tr_spyware_cr (rule_id);
+
+CREATE INDEX idx_spyware_rule_sr ON settings.tr_spyware_sr (rule_id);
+
 -- foreign key constraints
 
 ALTER TABLE settings.tr_spyware_ar
