6d304040295b12aa1f71e2634d981004a3b19044
Ian Nieves <inieves@untangle.com>
2006-03-04 23:58:06 +0000

    update to fix confusing missing // after the protocol
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@5035 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/httpblocker/main/com/metavize/tran/httpblocker/gui/BlockedURLsConfigJPanel.java
+++ tran/httpblocker/main/com/metavize/tran/httpblocker/gui/BlockedURLsConfigJPanel.java
@@ -63,7 +63,7 @@ class BlockedURLTableModel extends MSortedTableModel<Object>{
         addTableColumn( tableColumnModel,  0, C0_MW, false, false, false, false, String.class,  null, sc.TITLE_STATUS);
         addTableColumn( tableColumnModel,  1, C1_MW, false, false, false, false, Integer.class, null, sc.TITLE_INDEX);
         addTableColumn( tableColumnModel,  2, C2_MW, true,  true,  false, false, String.class,  sc.EMPTY_CATEGORY, sc.TITLE_CATEGORY);
-        addTableColumn( tableColumnModel,  3, C3_MW, true,  true,  false, false, String.class,  "http:", "URL");
+        addTableColumn( tableColumnModel,  3, C3_MW, true,  true,  false, false, String.class,  "http://", "URL");
         addTableColumn( tableColumnModel,  4, C4_MW, false, true,  false, false, Boolean.class, "true", sc.bold("block"));
         addTableColumn( tableColumnModel,  5, C5_MW, true,  true,  false, true,  String.class,  sc.EMPTY_DESCRIPTION, sc.TITLE_DESCRIPTION);
         addTableColumn( tableColumnModel,  6, 10,    false, false, true,  false, StringRule.class, null, "");
