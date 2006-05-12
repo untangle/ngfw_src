44a8dab901ebdcebb49e2b4f84d908c25d691d68
Aaron Read <amread@untangle.com>
2006-05-12 02:03:17 +0000

    using mkwap
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@5846 8ebf8b4e-06f3-0310-961f-89f74618be54

--- buildtools/mkwap.sh
+++ buildtools/mkwap.sh
@@ -28,10 +28,12 @@ dest=$4
 
 uriroot="$src/root"
 classroot="$dest/WEB-INF/classes"
+webinf="$dest/WEB-INF"
+weblib="$webinf/lib"
 
 libdirs="$root/alpine/output/usr/share/metavize/lib $root/alpine/output/usr/share/java/mvvm $root/downloads/output/jakarta-tomcat-5.0.28-embed/lib"
 
-classpath="$(find $libdirs -name '*.jar' -printf '%p:')"
+classpath="$JAVA_HOME/lib/tools.jar:$(find $libdirs -name '*.jar' -printf '%p:')"
 
 shift $(($OPTIND - 1))
 
@@ -46,15 +48,17 @@ rm $srclist
 
 (cd "$uriroot" && tar c --exclude '*.svn*' . | tar x -C "$dest")
 
-weblib=$dest/WEB-INF/lib
 mkdir -p "$weblib"
 cat $src/dlinc | copyfiles $root/downloads/output $weblib
 
 webfrag=$(mktemp)
 
-$JAVA_HOME/bin/java -cp "$classpath org.apache.jasper.JspC -d $classroot \
-    -p $name -webinc $webfrag -uriroot "$uriroot"
+$JAVA_HOME/bin/java -cp "$classpath" org.apache.jasper.JspC -s -l -v -compile \
+    -d $classroot -p $name -webinc $webfrag -uriroot "$uriroot"
 
-cat $webfrag
+find $classroot -name '*.java' -exec rm '{}' ';'
+
+sed -i "$webinf/web.xml" -e "/@JSP_PRE_COMPILED_SERVLETS@/r $webfrag" \
+    -e "/@JSP_PRE_COMPILED_SERVLETS@/d"
 
 rm $webfrag
