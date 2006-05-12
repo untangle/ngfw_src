425aaaa218c7e2211417f9d1dfd8fb634883cccc
Aaron Read <amread@untangle.com>
2006-05-12 00:41:54 +0000

    some changes
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@5844 8ebf8b4e-06f3-0310-961f-89f74618be54

--- buildtools/mkwap.sh
+++ buildtools/mkwap.sh
@@ -13,33 +13,48 @@ function copyfiles()
 
 # main ------------------------------------------------------------------------
 
-while getopts :r: KEY
+while getopts :r:n: KEY
   do
   case ${KEY} in
-      r) root="$OPTARG"
+      r) root="$OPTARG";;
+      n) name="$OPTARG";;
   esac
 done
 
-libdirs="$root/alpine/output/usr/share/metavize/lib $root/alpine/output/usr/share/java/mvvm"
-classpath=$(find $libdirs -name '*.jar' -printf "%p:")
+root=$1
+name=$2
+src=$3
+dest=$4
+
+uriroot="$src/root"
+classroot="$dest/WEB-INF/classes"
+
+libdirs="$root/alpine/output/usr/share/metavize/lib $root/alpine/output/usr/share/java/mvvm $root/downloads/output/jakarta-tomcat-5.0.28-embed/lib"
+
+classpath="$(find $libdirs -name '*.jar' -printf '%p:')"
 
 shift $(($OPTIND - 1))
 
-src=$1
-dest=$2
-
-classbuild="$dest/WEB-INF/classes"
-mkdir -p "$classbuild"
+mkdir -p "$classroot"
 
 srclist=$(mktemp)
 find "$src/src" -name '*.java' >$srclist
 
-$JAVA_HOME/bin/javac -cp "$classpath" -sourcepath "$src/src" -d "$classbuild" \
+$JAVA_HOME/bin/javac -cp "$classpath" -sourcepath "$src/src" -d "$classroot" \
     @$srclist
 rm $srclist
 
-(cd "$src/root" && tar c --exclude '*.svn*' . | tar x -C "$dest")
+(cd "$uriroot" && tar c --exclude '*.svn*' . | tar x -C "$dest")
 
 weblib=$dest/WEB-INF/lib
-mkdir $weblib
+mkdir -p "$weblib"
 cat $src/dlinc | copyfiles $root/downloads/output $weblib
+
+webfrag=$(mktemp)
+
+$JAVA_HOME/bin/java -cp "$classpath org.apache.jasper.JspC -d $classroot \
+    -p $name -webinc $webfrag -uriroot "$uriroot"
+
+cat $webfrag
+
+rm $webfrag
