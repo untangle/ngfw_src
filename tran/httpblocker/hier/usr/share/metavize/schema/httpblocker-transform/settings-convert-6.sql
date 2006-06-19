6ba23e41253a5b336c97bc8e5fc61887cb7ad098
Aaron Read <amread@untangle.com>
2006-06-19 22:37:39 +0000

    set log_only to false
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@6168 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/httpblocker/hier/usr/share/metavize/schema/httpblocker-transform/settings-convert-6.sql
+++ tran/httpblocker/hier/usr/share/metavize/schema/httpblocker-transform/settings-convert-6.sql
@@ -5,7 +5,7 @@ UPDATE settings.tr_httpblk_settings SET fascist_mode = false;
 ALTER TABLE settings.tr_httpblk_settings ALTER COLUMN fascist_mode SET NOT NULL;
 
 ALTER TABLE settings.tr_httpblk_blcat ADD COLUMN log_only bool;
-UPDATE settings.tr_httpblk_blcat SET log_only = true;
+UPDATE settings.tr_httpblk_blcat SET log_only = false;
 ALTER TABLE settings.tr_httpblk_blcat ALTER COLUMN log_only SET NOT NULL;
 
 ALTER TABLE settings.tr_httpblk_blcat ALTER COLUMN block_domains SET NOT NULL;
