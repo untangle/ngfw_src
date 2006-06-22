0a4c0bf2f89367f1214f51e0948e15f28f47fae9
John Irwin <jdi@untangle.com>
2006-06-22 00:21:18 +0000

    first version of mvvm monitor (1604)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@6192 8ebf8b4e-06f3-0310-961f-89f74618be54

--- mvvm/hier/usr/bin/mvvm.sh
+++ mvvm/hier/usr/bin/mvvm.sh
@@ -2,16 +2,24 @@
 # $Id: mvvm.sh,v 1.8 2005/03/22 18:46:43 cng Exp $
 
 MVVM_CONSOLE_LOG=${MVVM_CONSOLE_LOG:-"@PREFIX@/var/log/mvvm/console.log"}
-MVVM_MVVM_LOG=${MVVM_CONSOLE_LOG:-"@PREFIX@/var/log/mvvm/mvvm.log"}
-MVVM_GC_LOG=${MVVM_CONSOLE_LOG:-"@PREFIX@/var/log/mvvm/gc.log"}
+MVVM_MVVM_LOG=${MVVM_MVVM_LOG:-"@PREFIX@/var/log/mvvm/mvvm.log"}
+MVVM_GC_LOG=${MVVM_GC_LOG:-"@PREFIX@/var/log/mvvm/gc.log"}
 MVVM_WRAPPER_LOG=${MVVM_WRAPPER_LOG:-"@PREFIX@/var/log/mvvm/wrapper.log"}
 MVVM_LAUNCH=${MVVM_LAUNCH:-"@PREFIX@/usr/bin/bunnicula"}
 
-reapChildHardest() {
+# Short enough to restart mvvm promptly
+SLEEP_TIME=2
+
+# Used to kill a child with extreme prejudice
+nukeIt() {
     echo "mvvm.sh: Killing -9 all bunnicula \"$pid\" (`date`)" >> $MVVM_WRAPPER_LOG
     kill -3 $pid
     kill -9 $pid
     kill -9 `ps awwx | grep java | grep bunnicula | awk '{print $1}'` 2>/dev/null
+}
+
+reapChildHardest() {
+    nukeIt
     flushIptables ; exit
 }
 
@@ -76,6 +84,41 @@ raiseFdLimit() {
     ulimit -n 1024000
 }
 
+# Return true (0) when we need to reap and restart the mvvm.
+needToRestart() {
+    cheaphigh=`head -3 /tmp/m | tail -1 | awk '{ high=split($1, arr, "-"); print arr[2]; }'`
+    if [ -z $cheaphigh ]; then
+# not fatal, process has probably just died, which we'll catch soon.
+        echo "*** no heap size ($cheaphigh) on `date` in `pwd` ***" >> $MVVM_WRAPPER_LOG
+    else
+        bignibble=${cheaphigh:0:1}
+        case $bignibble in
+            0 | 1)
+# less than 384Meg native heap
+                ;;
+            2)
+# 384Meg < native heap < 640Meg 
+                if [ `date +%H` -eq 1 ]; then
+                    echo "*** bunnicula heap soft limit on `date` in `pwd` ***" >> $MVVM_WRAPPER_LOG
+                    return 0;
+                fi
+                ;;
+            3 | 4 | 5 | 6 | 7 | 8 | 9)
+# native heap > 640Meg 
+                echo "*** bunnicula heap hard limit ($bignibble) on `date` in `pwd` ***" >> $MVVM_WRAPPER_LOG
+                return 0;
+                ;;
+            *)
+                echo "*** unexpected heap size ($bignibble) on `date` in `pwd` ***" >> $MVVM_WRAPPER_LOG
+                ;;
+        esac
+    fi
+
+# just return the status of this one.
+    tail -50 $MVVM_GC_LOG | grep -i "concurrent mode failure"
+}
+
+                
 trap reapChildHardest 6
 trap reapChildHarder 15
 trap reapChild 2
@@ -98,17 +141,40 @@ while true; do
 
     pid=$!
     echo "Bunnicula launched. (pid:$pid) (`date`)" >> $MVVM_WRAPPER_LOG
-    wait $pid
 
+# Instead of waiting, we now monitor.
+    while true; do
+        sleep $SLEEP_TIME
+        if [ ! -d /proc/$pid ] ; then
+            echo "*** restarting missing bunnicula $? on `date` ***" >> $MVVM_WRAPPER_LOG
+            break
+        fi
+        if needToRestart; then
+            echo "*** need to restart bunnicula $? on `date` ***" >> $MVVM_WRAPPER_LOG
+            nukeIt
+            break
+        fi
+    done
+
+# Clean up the zombie.  Risky? XXX    
+#    wait $pid
+
+# Crash/Kill        
     flushIptables
-    echo "*** bunnicula returned $? on `date` in `pwd` ***" >> $MVVM_WRAPPER_LOG
-
+    echo "*** bunnicula exited on `date` in `pwd` ***" >> $MVVM_WRAPPER_LOG
     echo "*** copied $MVVM_CONSOLE_LOG to $MVVM_CONSOLE_LOG.crash ***" >> $MVVM_WRAPPER_LOG
     echo "*** copied $MVVM_MVVM_LOG to $MVVM_MVVM_LOG.crash ***" >> $MVVM_WRAPPER_LOG
     echo "*** copied $MVVM_GC_LOG to $MVVM_GC_LOG.crash ***" >> $MVVM_WRAPPER_LOG
+    cp -f $MVVM_CONSOLE_LOG.crash.1 $MVVM_CONSOLE_LOG.crash.2
+    cp -f $MVVM_CONSOLE_LOG.crash $MVVM_CONSOLE_LOG.crash.1
     cp -f $MVVM_CONSOLE_LOG $MVVM_CONSOLE_LOG.crash
+    cp -f $MVVM_MVVM_LOG.crash.1 $MVVM_MVVM_LOG.crash.2
+    cp -f $MVVM_MVVM_LOG.crash $MVVM_MVVM_LOG.crash.1
     cp -f $MVVM_MVVM_LOG $MVVM_MVVM_LOG.crash
+    cp -f $MVVM_GC_LOG.crash.1 $MVVM_GC_LOG.crash.2
+    cp -f $MVVM_GC_LOG.crash $MVVM_GC_LOG.crash.1
     cp -f $MVVM_GC_LOG $MVVM_GC_LOG.crash
 
     sleep 2
+    echo "*** restarting on `date` ***" >> $MVVM_WRAPPER_LOG
 done
