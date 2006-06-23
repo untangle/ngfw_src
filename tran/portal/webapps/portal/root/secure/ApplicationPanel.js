28f04466a687eafc7bcfe76eac9537d4c60ea82e
Aaron Read <amread@untangle.com>
2006-06-23 01:16:14 +0000

    got rid of extra buttons
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@6259 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/portal/webapps/portal/root/secure/ApplicationPanel.js
+++ tran/portal/webapps/portal/root/secure/ApplicationPanel.js
@@ -54,29 +54,16 @@ ApplicationPanel.prototype._makeToolbar = function() {
    b.setToolTipContent("Display latest contents");
    b.addSelectionListener(new AjxListener(this, this.refresh));
 
-   b = new DwtButton(toolbar, DwtButton.ALIGN_CENTER);
-   b.setText("New Bookmark");
-   b.setToolTipContent("Add a new bookmark");
-   this.addBookmarkButton = b;
-
-   b = new DwtButton(toolbar, DwtButton.ALIGN_CENTER);
-   b.setText("Delete");
-   b.setToolTipContent("Delete selected files");
-   this.deleteBookmarkButton = b;
-
    return toolbar;
 }
 
 ApplicationPanel.prototype._layout = function()
 {
    var size = this.getSize();
-   DBG.println("SIZE: (" + size.x + ", " + size.y + ")");
 
    var x = 0;
-   DBG.println("TOOLBAR AT: (0, 0)");;
    this._toolbar.setLocation(0, 0);
    x += this._toolbar.getSize().x;
-   DBG.println("BOOKMARK LIST AT: (" + x + ", 0, ", + size.x - x + ", " + size.y + ")");
    this._applicationList.setBounds(x, 0, size.x - x, size.y);
 }
 
