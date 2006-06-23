4667880ad4dad530d244cc8d81f2f74f092b1fdf
Aaron Read <amread@untangle.com>
2006-06-23 23:07:26 +0000

    bookmark property
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@6290 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ tran/portal/webapps/portal/root/secure/BookmarkProperty.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2006 Metavize Inc.
+// All rights reserved.
+
+function BookmarkProperty(values, def, valFn)
+{
+   this.values = values;
+   this.def = def;
+   this.valFn = valFn;
+}
