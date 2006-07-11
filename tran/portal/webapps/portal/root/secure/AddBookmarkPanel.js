879c76ffd960746fe00a33173aafb466c02a0be3
Aaron Read <amread@untangle.com>
2006-07-11 01:47:30 +0000

    rdp bookmark working?
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@6394 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/portal/webapps/portal/root/secure/AddBookmarkPanel.js
+++ tran/portal/webapps/portal/root/secure/AddBookmarkPanel.js
@@ -3,20 +3,20 @@
 
 function AddBookmarkPanel(parent, apps)
 {
-   if (0 == arguments.length) {
-      return;
-   }
+  if (0 == arguments.length) {
+    return;
+  }
 
-   DwtComposite.call(this, parent);
+  DwtComposite.call(this, parent);
 
-   this._bookmarkWidgets = [ ];
+  this._bookmarkWidgets = [ ];
 
-   this._apps = [];
-   for (var i = 0; i < apps.length; i++) {
-      this._apps.push(new DwtSelectOption(apps[i], false, apps[i].name));
-   }
+  this._apps = [];
+  for (var i = 0; i < apps.length; i++) {
+    this._apps.push(new DwtSelectOption(apps[i], false, apps[i].name));
+  }
 
-   this._init();
+  this._init();
 }
 
 AddBookmarkPanel.prototype = new DwtComposite();
@@ -26,92 +26,94 @@ AddBookmarkPanel.prototype.constructor = AddBookmarkPanel;
 
 AddBookmarkPanel.prototype.getBookmark = function()
 {
-   var app = this._appField.getValue();
-   var fn = app.getBookmarkFunction();
+  var app = this._appField.getValue();
+  var fn = app.getBookmarkFunction();
 
-   var target;
-   if (fn) {
-      target = fn(this._properties);
-   } else {
-      target = this._targetField.getValue();
-   }
+  var target;
+  if (fn) {
+    DBG.println("using bookmark fn");
+    target = fn(this._properties);
+  } else {
+    DBG.println("NOT using bookmark fn");
+    target = this._targetField.getValue();
+  }
 
-   return new Bookmark(null, this._nameField.getValue(), app.name, target);
+  return new Bookmark(null, this._nameField.getValue(), app.name, target);
 }
 
-AddBookmarkPanel.prototype.focus = function()
+  AddBookmarkPanel.prototype.focus = function()
 {
-   this._fields[0].focus();
+  this._fields[0].focus();
 }
 
-// private methods ------------------------------------------------------------
+  // private methods ------------------------------------------------------------
 
-AddBookmarkPanel.prototype._init = function()
+    AddBookmarkPanel.prototype._init = function()
 {
-   this._fields = new Array();
+  this._fields = new Array();
 
-   var label = new DwtLabel(this);
-   label.setText("Application:");
-   this._appField = new DwtSelect(this, this._apps);
-   var l = new AjxListener(this, this._showFields);
-   this._appField.addChangeListener(l);
-   this._fields.push(this._appField);
+  var label = new DwtLabel(this);
+  label.setText("Application:");
+  this._appField = new DwtSelect(this, this._apps);
+  var l = new AjxListener(this, this._showFields);
+  this._appField.addChangeListener(l);
+  this._fields.push(this._appField);
 
-   this._showFields();
+  this._showFields();
 }
 
-AddBookmarkPanel.prototype._showFields = function()
+      AddBookmarkPanel.prototype._showFields = function()
 {
-   this._properties = { };
+  this._properties = { };
 
-   var app = this._appField.getValue();
-   var props = app.getBookmarkProperties();
-   if (props) {
-      this._showPropFields(props);
-   } else {
-      this._showDefaultFields();
-   }
+  var app = this._appField.getValue();
+  var props = app.getBookmarkProperties();
+  if (props) {
+    this._showPropFields(props);
+  } else {
+    this._showDefaultFields();
+  }
 }
 
-AddBookmarkPanel.prototype._showDefaultFields = function()
+        AddBookmarkPanel.prototype._showDefaultFields = function()
 {
-   this._fields = this._fields.splice(0, 1);
+  this._fields = this._fields.splice(0, 1);
 
-   for (var i = 0; i < this._bookmarkWidgets.length; i++) {
-      this.removeChild(this._bookmarkWidgets[i]);
-   }
+  for (var i = 0; i < this._bookmarkWidgets.length; i++) {
+    this.removeChild(this._bookmarkWidgets[i]);
+  }
 
-   var label = new DwtLabel(this);
-   this._bookmarkWidgets.push(label);
-   label.setText("Name:");
-   this._nameField = new DwtInputField({ parent: this });
-   this._bookmarkWidgets.push(this._nameField);
-   this._fields.push(this._nameField);
+  var label = new DwtLabel(this);
+  this._bookmarkWidgets.push(label);
+  label.setText("Name:");
+  this._nameField = new DwtInputField({ parent: this });
+  this._bookmarkWidgets.push(this._nameField);
+  this._fields.push(this._nameField);
 
-   label = new DwtLabel(this);
-   this._bookmarkWidgets.push(label);
-   label.setText("Target:");
-   this._targetField = new DwtInputField({ parent: this });
-   this._bookmarkWidgets.push(this._targetField);
-   this._fields.push(this._targetField);
+  label = new DwtLabel(this);
+  this._bookmarkWidgets.push(label);
+  label.setText("Target:");
+  this._targetField = new DwtInputField({ parent: this });
+  this._bookmarkWidgets.push(this._targetField);
+  this._fields.push(this._targetField);
 }
 
-AddBookmarkPanel.prototype._showPropFields = function(props)
+          AddBookmarkPanel.prototype._showPropFields = function(props)
 {
-   this._fields = this._fields.splice(0, 1);
+  this._fields = this._fields.splice(0, 1);
 
-   for (var i = 0; i < this._bookmarkWidgets.length; i++) {
-      this.removeChild(this._bookmarkWidgets[i]);
-   }
+  for (var i = 0; i < this._bookmarkWidgets.length; i++) {
+    this.removeChild(this._bookmarkWidgets[i]);
+  }
 
-   for (var f in props) {
-      var prop = props[f];
-      var label = prop.getLabel(this);
-      this._bookmarkWidgets.push(label);
-      var field = prop.getField(this);
-      this._fields.push(field);
-      this._bookmarkWidgets.push(field);
-      this._properties[prop.id] = field;
-      DBG.println("ADDED PROP: " + prop.id);
-   }
+  for (var f in props) {
+    var prop = props[f];
+    var label = prop.getLabel(this);
+    this._bookmarkWidgets.push(label);
+    var field = prop.getField(this);
+    this._fields.push(field);
+    this._bookmarkWidgets.push(field);
+    this._properties[prop.id] = field;
+    DBG.println("ADDED PROP: " + prop.id);
+  }
 }
--- tran/portal/webapps/portal/root/secure/Portal.js
+++ tran/portal/webapps/portal/root/secure/Portal.js
@@ -2,46 +2,46 @@
 // All rights reserved.
 
 function Portal(shell) {
-   if (0 == arguments.length) {
-      return;
-   }
+  if (0 == arguments.length) {
+    return;
+  }
 
-   this._shell = shell;
+  this._shell = shell;
 
-   this._shell.addControlListener(new AjxListener(this, this._shellListener));
+  this._shell.addControlListener(new AjxListener(this, this._shellListener));
 
-   DwtComposite.call(this, this._shell, "Portal");
+  DwtComposite.call(this, this._shell, "Portal");
 
-   this._loadApps();
+  this._loadApps();
 
-   this._navBar = new NavigationBar(this);
-   this._navBar.zShow(true);
-   var l = new AjxListener(this, this._homeButtonListener);
-   this._navBar.addHomeButtonListener(l);
-   l = new AjxListener(this, this._logoutButtonListener);
-   this._navBar.addLogoutButtonListener(l);
+  this._navBar = new NavigationBar(this);
+  this._navBar.zShow(true);
+  var l = new AjxListener(this, this._homeButtonListener);
+  this._navBar.addHomeButtonListener(l);
+  l = new AjxListener(this, this._logoutButtonListener);
+  this._navBar.addLogoutButtonListener(l);
 
-   this._portalPanel = new PortalPanel(this._shell);
-   this._portalPanel.zShow(true);
+  this._portalPanel = new PortalPanel(this._shell);
+  this._portalPanel.zShow(true);
 
-   l = new AjxListener(this, this._bookmarkSelectionListener);
-   this._portalPanel.bookmarkPanel.addSelectionListener(l);
+  l = new AjxListener(this, this._bookmarkSelectionListener);
+  this._portalPanel.bookmarkPanel.addSelectionListener(l);
 
-   l = new AjxListener(this, this._applicationSelectionListner);
-   this._portalPanel.applicationPanel.addSelectionListener(l);
+  l = new AjxListener(this, this._applicationSelectionListner);
+  this._portalPanel.applicationPanel.addSelectionListener(l);
 
-   l = new AjxListener(this, this._addBookmarkButtonListener);
-   this._portalPanel.bookmarkPanel.addBookmarkButton.addSelectionListener(l);
-   l = new AjxListener(this, this._deleteButtonListener)
-   this._portalPanel.bookmarkPanel.deleteBookmarkButton.addSelectionListener(l);
+  l = new AjxListener(this, this._addBookmarkButtonListener);
+  this._portalPanel.bookmarkPanel.addBookmarkButton.addSelectionListener(l);
+  l = new AjxListener(this, this._deleteButtonListener);
+  this._portalPanel.bookmarkPanel.deleteBookmarkButton.addSelectionListener(l);
 
-   this.showPortal();
+  this.showPortal();
 
-   this.refresh();
-   this.addControlListener(new AjxListener(this, this._controlListener));
+  this.refresh();
+  this.addControlListener(new AjxListener(this, this._controlListener));
 
-   this.layout();
-   this.zShow(true);
+  this.layout();
+  this.zShow(true);
 }
 
 Portal.prototype = new DwtComposite();
@@ -51,273 +51,276 @@ Portal.prototype.constructor = Portal;
 
 Portal.prototype.showApplicationUrl = function(url, application, bookmark)
 {
-   if (this._mainPanel) {
-      Dwt.setVisible(this._mainPanel.getHtmlElement(), false);
-      if (this._mainPanel != this._portalPanel) {
-         this._mainPanel.dispose();
-      }
-   }
+  if (this._mainPanel) {
+    Dwt.setVisible(this._mainPanel.getHtmlElement(), false);
+    if (this._mainPanel != this._portalPanel) {
+      this._mainPanel.dispose();
+    }
+  }
 
-   this._mainPanel = new ApplicationIframe(this._shell);
-   this._mainPanel.loadUrl(url);
-   Dwt.setVisible(this._mainPanel.getHtmlElement(), true);
-   this._mainPanel.zShow(true);
+  this._mainPanel = new ApplicationIframe(this._shell);
+  this._mainPanel.loadUrl(url);
+  Dwt.setVisible(this._mainPanel.getHtmlElement(), true);
+  this._mainPanel.zShow(true);
 
-   this._navBar.applicationMode(application, bookmark);
+  this._navBar.applicationMode(application, bookmark);
 
-   this.layout();
+  this.layout();
 }
 
-Portal.prototype.splitUrl = function(url)
+  Portal.prototype.splitUrl = function(url)
 {
-   var o = new Object();
+  var o = new Object();
 
-   if (0 == url.indexOf("//")) {
+  if (0 == url.indexOf("//")) {
+    o.proto = location.protocol;
+    if (":" == o.proto[o.proto.length - 1]) {
+      o.proto = o.proto.substring(0, o.proto.length - 1);
+    }
+    var i = url.indexOf("/", 2);
+    o.host = url.substring(2, i);
+    o.path = url.substring(i);
+  } else if (0 == url.indexOf("/")) {
+    o.proto = location.protocol;
+    if (":" == o.proto[o.proto.length - 1]) {
+      o.proto = o.proto.substring(0, o.proto.length - 1);
+    }
+    o.host = location.host;
+    o.path = url;
+  } else {
+    var i = url.indexOf(":");
+    if (0 > i) {
       o.proto = location.protocol;
       if (":" == o.proto[o.proto.length - 1]) {
-         o.proto = o.proto.substring(0, o.proto.length - 1);
+        o.proto = o.proto.substring(0, o.proto.length - 1);
       }
-      var i = url.indexOf("/", 2);
-      o.host = url.substring(2, i);
-      o.path = url.substring(i);
-   } else if (0 == url.indexOf("/")) {
-      o.proto = location.protocol;
-      if (":" == o.proto[o.proto.length - 1]) {
-         o.proto = o.proto.substring(0, o.proto.length - 1);
+      o.host = location.hostname;
+      var p = location.pathname;
+      for (var k = p.length - 1; 0 <= k; k--) {
+        if ("/" == p[k]) {
+          p = p.substring(0, k + 1);
+          break;
+        }
       }
-      o.host = location.host;
-      o.path = url;
-   } else {
-      var i = url.indexOf(":");
-      if (0 > i) {
-         o.proto = location.protocol;
-         if (":" == o.proto[o.proto.length - 1]) {
-            o.proto = o.proto.substring(0, o.proto.length - 1);
-         }
-         o.host = location.hostname;
-         var p = location.pathname;
-         for (var k = p.length - 1; 0 <= k; k--) {
-            if ("/" == p[k]) {
-               p = p.substring(0, k + 1);
-               break;
-            }
-         }
-         o.path = p + url;
-      } else {
-         o.proto = url.substring(0, i);
-         i = i + 3;
-         var j = url.indexOf('/', i);
-         o.host = url.substring(i, j);
-         o.path = url.substring(j);
-      }
-   }
+      o.path = p + url;
+    } else {
+      o.proto = url.substring(0, i);
+      i = i + 3;
+      var j = url.indexOf('/', i);
+      o.host = url.substring(i, j);
+      o.path = url.substring(j);
+    }
+  }
 
-   return o;
+  return o;
 }
 
 // public methods -------------------------------------------------------------
 
 Portal.prototype.showPortal = function()
 {
-   if (this._mainPanel && this._mainPanel != this._portalPanel) {
-      Dwt.setVisible(this._mainPanel.getHtmlElement(), false);
-      this._mainPanel.dispose();
-   }
-   this._mainPanel = this._portalPanel;
-   Dwt.setVisible(this._portalPanel.getHtmlElement(), true);
-   this._navBar.portalMode();
+  if (this._mainPanel && this._mainPanel != this._portalPanel) {
+    Dwt.setVisible(this._mainPanel.getHtmlElement(), false);
+    this._mainPanel.dispose();
+  }
+  this._mainPanel = this._portalPanel;
+  Dwt.setVisible(this._portalPanel.getHtmlElement(), true);
+  this._navBar.portalMode();
 }
 
 Portal.prototype.refresh = function()
 {
-   this._refreshPageInfo()
-   this._loadApps();
-   this._portalPanel.refresh();
+  this._refreshPageInfo()
+  this._loadApps();
+  this._portalPanel.refresh();
 }
 
 Portal.prototype.layout = function()
 {
-   DBG.println("Portal.layout");
+  DBG.println("Portal.layout");
 
-   var size = this._shell.getSize();
-   var width = size.x;
-   var height = size.y;
+  var size = this._shell.getSize();
+  var width = size.x;
+  var height = size.y;
 
-   var x = 0;
-   var y = 0;
+  var x = 0;
+  var y = 0;
 
-   this._navBar.setLocation(x, y);
-   var size = this._navBar.getSize();
-   y += size.y;
+  this._navBar.setLocation(x, y);
+  var size = this._navBar.getSize();
+  y += size.y;
 
-   DBG.println("SET BOUNDS(" + x + ", " + y + ", " + width + ", " + (height - y) + ")");
-   this._mainPanel.setBounds(x, y, width, height - y);
+  DBG.println("SET BOUNDS(" + x + ", " + y + ", " + width + ", " + (height - y) + ")");
+  this._mainPanel.setBounds(x, y, width, height - y);
 }
 
 // private methods ------------------------------------------------------------
 
 Portal.prototype._refreshPageInfo = function()
 {
-   AjxRpc.invoke(null, "secure/portal?command=info", null,
-                 new AjxCallback(this, this._refreshPageInfoCallback,
-                                 new Object()), true);
+  AjxRpc.invoke(null, "secure/portal?command=info", null,
+                new AjxCallback(this, this._refreshPageInfoCallback,
+                                new Object()), true);
 }
 
 // init -----------------------------------------------------------------------
 
 Portal.prototype._loadApps = function()
 {
-   AjxRpc.invoke(null, "secure/application?command=ls", null,
-                 new AjxCallback(this, this._refreshAppsCallback,
-                                 new Object()), true);
+  AjxRpc.invoke(null, "secure/application?command=ls", null,
+                new AjxCallback(this, this._refreshAppsCallback,
+                                new Object()), true);
 }
 
 // shell ----------------------------------------------------------------------
 
 Portal.prototype._shellListener = function(ev)
 {
-   if (ev.oldWidth != ev.newWidth || ev.oldHeight != ev.newHeight) {
-      this.layout();
-   }
+  if (ev.oldWidth != ev.newWidth || ev.oldHeight != ev.newHeight) {
+    this.layout();
+  }
 }
 
 // util -----------------------------------------------------------------------
 
 Portal._mkSrcDestCommand = function(command, src, dest)
 {
-   var url = "exec?command=" + command;
+  var url = "exec?command=" + command;
 
-   for (var i = 0; i < src.length; i++) {
-      url += "&src=" + src[i].url; // XXX does this get escaped ?
-   }
+  for (var i = 0; i < src.length; i++) {
+    url += "&src=" + src[i].url; // XXX does this get escaped ?
+  }
 
-   url += "&dest=" + dest.url; // XXX does this get escaped ?
+  url += "&dest=" + dest.url; // XXX does this get escaped ?
 
-   return url;
+  return url;
 }
 
 // callbacks ------------------------------------------------------------------
 
 Portal.prototype._controlListener = function()
 {
-   this.layout();
+  this.layout();
 }
 
-Portal.prototype._bookmarkSelectionListener = function(ev) {
-   switch (ev.detail) {
-      case DwtListView.ITEM_DBL_CLICKED:
-      var item = ev.item;
-      var app = this._appMap[item.app];
-      // XXX if null?
-      app.openBookmark(this, item);
-      break;
-   }
+Portal.prototype._bookmarkSelectionListener = function(ev)
+{
+  switch (ev.detail) {
+  case DwtListView.ITEM_DBL_CLICKED:
+  var item = ev.item;
+  var app = this._appMap[item.app];
+  // XXX if null?
+  app.openBookmark(this, item);
+  break;
+  }
 }
 
-Portal.prototype._applicationSelectionListner = function(ev) {
-   switch (ev.detail) {
-      case DwtListView.ITEM_DBL_CLICKED:
-      var item = ev.item;
-      var app = this._appMap[item.name];
-      // XXX if null?
-      app.openApplication(this);
-      break;
-   }
+Portal.prototype._applicationSelectionListner = function(ev)
+{
+  switch (ev.detail) {
+  case DwtListView.ITEM_DBL_CLICKED:
+  var item = ev.item;
+  var app = this._appMap[item.name];
+  // XXX if null?
+  app.openApplication(this);
+  break;
+  }
 }
 
-Portal.prototype._refreshAppsCallback = function(obj, results) {
-   var root = results.xml.getElementsByTagName("applications")[0];
+Portal.prototype._refreshAppsCallback = function(obj, results)
+{
+  var root = results.xml.getElementsByTagName("applications")[0];
 
-   var children = root.childNodes;
+  var children = root.childNodes;
 
-   var launchableApps = new AjxVector();
+  var launchableApps = new AjxVector();
 
-   this._apps = [ ];
-   this._appMap = { };
+  this._apps = [ ];
+  this._appMap = { };
 
-   for (var i = 0; i < children.length; i++) {
-      var child = children[i];
+  for (var i = 0; i < children.length; i++) {
+    var child = children[i];
 
-      if ("application" == child.tagName) {
-         var name = child.getAttribute("name");
-         var description = child.getAttribute("description");
-         var isHostService = "true" == child.getAttribute("isHostService");
-         var appJs = child.getElementsByTagName("appJs")[0].firstChild.data;
-         var app = new Application(name, description, isHostService, appJs);
-         this._apps.push(app);
-         this._appMap[name] = app;
-         if (!isHostService) {
-            launchableApps.add(app);
-         }
-         DBG.println("this._appMap[" + name + "] = " + app);
+    if ("application" == child.tagName) {
+      var name = child.getAttribute("name");
+      var description = child.getAttribute("description");
+      var isHostService = "true" == child.getAttribute("isHostService");
+      var appJs = child.getElementsByTagName("appJs")[0].firstChild.data;
+      var app = new Application(name, description, isHostService, appJs);
+      this._apps.push(app);
+      this._appMap[name] = app;
+      if (!isHostService) {
+        launchableApps.add(app);
       }
-   }
+      DBG.println("this._appMap[" + name + "] = " + app);
+    }
+  }
 
-   this._portalPanel.applicationPanel.set(launchableApps);
+  this._portalPanel.applicationPanel.set(launchableApps);
 }
 
-Portal.prototype._refreshPageInfoCallback = function(obj, results) {
-   var root = results.xml.getElementsByTagName("page-info")[0];
-   document.title = root.getAttribute("title");
-   var motd = root.getAttribute("motd");
-   this._portalPanel.setMotd(motd);
+Portal.prototype._refreshPageInfoCallback = function(obj, results)
+{
+  var root = results.xml.getElementsByTagName("page-info")[0];
+  document.title = root.getAttribute("title");
+  var motd = root.getAttribute("motd");
+  this._portalPanel.setMotd(motd);
 }
-
 
 Portal.prototype._homeButtonListener = function()
 {
-   this.showPortal();
+  this.showPortal();
 }
 
 Portal.prototype._logoutButtonListener = function()
 {
-   var cb = new AjxCallback(this, this._logoutCallback);
-   AjxRpc.invoke(null, "logout", null, cb, { }, true);
+  var cb = new AjxCallback(this, this._logoutCallback);
+  AjxRpc.invoke(null, "logout", null, cb, { }, true);
 }
 
 Portal.prototype._logoutCallback = function()
 {
-   window.location.reload();
+  window.location.reload();
 }
 
 Portal.prototype._addBookmarkButtonListener = function(ev)
 {
-   var dialog = new AddBookmarkDialog(DwtShell.getShell(window), this._apps);
+  var dialog = new AddBookmarkDialog(DwtShell.getShell(window), this._apps);
 
-   var cb = function() {
-      var bm = dialog.getBookmark();
-      DBG.println("SENDING BOOKMARK: " + bm);
-      var url = "secure/bookmark?command=add&name=" + escape(bm.name)
-         + "&app=" + escape(bm.app) + "&target=" + escape(bm.target);
+  var cb = function() {
+    var bm = dialog.getBookmark();
+    DBG.println("SENDING BOOKMARK: " + bm);
+    var url = "secure/bookmark?command=add&name=" + escape(bm.name)
+    + "&app=" + escape(bm.app) + "&target=" + escape(bm.target);
 
-      var cb = function(obj, results) {
-         this.refresh();
-         dialog.popdown();
-      }
+    var cb = function(obj, results) {
+      this.refresh();
+      dialog.popdown();
+    }
 
-      AjxRpc.invoke(null, url, null, new AjxCallback(this, cb, {}), true);
-   }
+    AjxRpc.invoke(null, url, null, new AjxCallback(this, cb, {}), true);
+  }
 
-   var l = new AjxListener(this, cb);
-   dialog.setButtonListener(DwtDialog.OK_BUTTON, l);
-   dialog.addListener(DwtEvent.ENTER, l);
+  var l = new AjxListener(this, cb);
+  dialog.setButtonListener(DwtDialog.OK_BUTTON, l);
+  dialog.addListener(DwtEvent.ENTER, l);
 
-   dialog.popup();
+  dialog.popup();
 }
 
 Portal.prototype._deleteButtonListener = function(ev)
 {
-   var sel = this._portalPanel.bookmarkPanel.getSelection();
-   if (0 == sel.length) {
-      return;
-   }
+  var sel = this._portalPanel.bookmarkPanel.getSelection();
+  if (0 == sel.length) {
+    return;
+  }
 
-   var url = "secure/bookmark?command=rm";
+  var url = "secure/bookmark?command=rm";
 
-   for (var i = 0; i < sel.length; i++) {
-      url += "&id=" + sel[i].id;
-   }
+  for (var i = 0; i < sel.length; i++) {
+    url += "&id=" + sel[i].id;
+  }
 
-   AjxRpc.invoke(null, url, null, new AjxCallback(this, this.refresh, { }),
-                 true);
+  AjxRpc.invoke(null, url, null, new AjxCallback(this, this.refresh, { }),
+                true);
 }
