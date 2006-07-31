98a9e2c331dbdc69de7c2ae4ce4467f673ec9faa
Dirk Morris <dmorris@untangle.com>
2006-07-31 17:14:58 +0000

    text renames
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@6644 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/portal/main/com/metavize/tran/portal/gui/UserHomeSettingsJPanel.java
+++ tran/portal/main/com/metavize/tran/portal/gui/UserHomeSettingsJPanel.java
@@ -373,7 +373,7 @@ public class UserHomeSettingsJPanel extends javax.swing.JPanel implements Savabl
                 restrictIPJPanel.setLayout(new java.awt.GridBagLayout());
 
                 explorerJLabel.setFont(new java.awt.Font("Dialog", 0, 12));
-                explorerJLabel.setText("Show File Explorer: ");
+                explorerJLabel.setText("Show Network File Browser: ");
                 gridBagConstraints = new java.awt.GridBagConstraints();
                 gridBagConstraints.gridx = 0;
                 gridBagConstraints.gridy = 0;
