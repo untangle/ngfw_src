27b039909645f70e76555af878c8051ddf78343b
Aaron Read <amread@untangle.com>
2006-09-29 17:38:58 +0000

    web.xml
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@7319 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ tran/httpblocker/servlets/httpblocker/root/httpblocker.js
@@ -0,0 +1,36 @@
+// Copyright (c) 2006 Metavize Inc.
+// All rights reserved.
+
+function unblockSite(forAll)
+{
+    alert("TID: " + window.tid);
+
+    var req = false;
+
+    if (window.XMLHttpRequest) {
+        req = new XMLHttpRequest();
+        if (req.overrideMimeType) {
+            req.overrideMimeType('text/xml');
+        }
+    } else if (window.ActiveXObject) {
+        try {
+            req = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (exn) {
+            try {
+                req = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (exn) {}
+        }
+    }
+
+    req.onreadystatechange = function() {
+        if (req.readyState == 4) {
+            window.location.reload();
+        }
+    };
+
+    var hostname = window.location.hostname;
+    req.open('POST', "unblock", true);
+    req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    req.send("site=" + hostname + "&global=" + forAll);
+};
+
---
+++ tran/httpblocker/servlets/httpblocker/src/com/metavize/tran/httpblocker/UnblockerServlet.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2006 Metavize Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Metavize Inc. ("Confidential Information").  You shall
+ * not disclose such Confidential Information.
+ *
+ * $Id$
+ */
+
+package com.metavize.tran.httpblocker;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UnblockerServlet extends HttpServlet
+{
+    // HttpServlet methods ----------------------------------------------------
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException
+    {
+        resp.setContentType("text/xml");
+        resp.addHeader("Cache-Control", "no-cache");
+
+        String site = req.getParameter("site");
+        String global = req.getParameter("global");
+
+        System.out.println("SITE: " + site + " GLOBAL: " + global);
+
+        try {
+            resp.getOutputStream().println("<success/>");
+        } catch (IOException exn) {
+            throw new ServletException(exn);
+        }
+    }
+}
+
