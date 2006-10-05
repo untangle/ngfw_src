f9678065a8db5d7da04c0d05779749b85c77a879
Aaron Read <amread@untangle.com>
2006-10-05 21:54:00 +0000

    fixed prj.el
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@7396 8ebf8b4e-06f3-0310-961f-89f74618be54

--- prj.el
+++ prj.el
@@ -26,8 +26,8 @@
               "commons-fileupload-1.1/commons-fileupload-1.1.jar"
               "commons-httpclient-3.0/commons-httpclient-3.0.jar"
               "hibernate-3.2/hibernate3.jar"
-              "hibernate-annotations-3.2.0.CR2/hibernate-annotations.jar"
-              "hibernate-annotations-3.2.0.CR2/lib/ejb3-persistence.jar"
+              "hibernate-annotations-3.2.0.CR3/hibernate-annotations.jar"
+              "hibernate-annotations-3.2.0.CR3/lib/ejb3-persistence.jar"
               "htmlparser1_6_20060319/htmlparser1_6/lib/htmllexer.jar"
               "htmlparser1_6_20060319/htmlparser1_6/lib/htmlparser.jar"
               "apache-tomcat-5.5.17-embed/lib/catalina.jar"
@@ -39,7 +39,8 @@
               "junit3.8.1/junit.jar"
               "logging-log4j-1.2.9/dist/lib/log4j-1.2.9.jar"
               "bcel-5.1/bcel-5.1.jar"))
-    (mapcar
-     (lambda (a)
-       (concat "./staging/grabbag/" a))
-     (directory-files (concat prj-dir "./staging/grabbag") nil ".*\.jar")))))
+    (if (file-exists-p (concat prj-dir "./staging/grabbag"))
+        (mapcar
+         (lambda (a)
+           (concat "./staging/grabbag/" a))
+         (directory-files (concat prj-dir "./staging/grabbag") nil ".*\.jar"))))))
