436316aadfb38972be3bdbc45b7d6bee330d6ec1
Aaron Read <amread@untangle.com>
2006-10-17 20:02:34 +0000

    GA version of hibernate 3.2
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@7473 8ebf8b4e-06f3-0310-961f-89f74618be54

--- util/package.rb
+++ util/package.rb
@@ -2,4 +2,10 @@
 
 buildutil = Package["buildutil"]
 
-JarTarget.buildTarget(buildutil, [ Jars::Becl , Jars::Reporting], 'impl', [ 'util/impl' ] )
+jt = JarTarget.buildTarget(buildutil, [ Jars::Becl , Jars::Reporting], 'impl', [ 'util/impl' ] )
+$InstallTarget.installJars(jt, "#{buildutil.distDirectory}/usr/share/metavize/lib")
+$InstallTarget.installJars(Jars::Becl, "#{buildutil.distDirectory}/usr/share/java/mvvm")
+
+ms = MoveSpec.new('util/hier', FileList['util/hier/**/*'], buildutil.distDirectory)
+cf = CopyFiles.new(buildutil, ms, 'hier', $BuildEnv.filterset)
+buildutil.registerTarget('hier', cf)
