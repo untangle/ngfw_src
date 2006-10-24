df72bd4acf6235db01dddc41133ba134df248b2b
Aaron Read <amread@untangle.com>
2006-10-24 05:26:59 +0000

    metavize -> untangle
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@7664 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/firewall/impl/com/metavize/tran/firewall/EventHandler.java
+++ tran/firewall/impl/com/metavize/tran/firewall/EventHandler.java
@@ -9,17 +9,6 @@
  * $Id$
  */
 
-/*
- * copyright (c) 2003, 2004, 2005, 2006 Metavize Inc.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * Untangle Networks, Inc. ("Confidential Information"). You shall
- * not disclose such Confidential Information.
- *
- * $Id$
- */
-
 package com.metavize.tran.firewall;
 
 import java.util.Iterator;
