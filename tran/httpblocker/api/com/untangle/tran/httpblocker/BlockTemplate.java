c7ccc8d5172886f4efa3c75681eb1af3b141ae47
Ian Nieves <inieves@untangle.com>
2006-11-02 20:23:42 +0000

    naming update
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@7824 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/httpblocker/api/com/untangle/tran/httpblocker/BlockTemplate.java
+++ tran/httpblocker/api/com/untangle/tran/httpblocker/BlockTemplate.java
@@ -50,7 +50,7 @@ public class BlockTemplate implements Serializable
     private static String BLOCK_TEMPLATE;
 
     private Long id;
-    private String header = "Untangle Networks Content Filter";
+    private String header = "Untangle Networks Content Filter" ;
     private String contact = "your network administrator";
 
     // constructor ------------------------------------------------------------
