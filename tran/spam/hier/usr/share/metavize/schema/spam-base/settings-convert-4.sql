c3c4dc99808aaf160a658c09058ef9b2efe1ec1c
John Irwin <jdi@untangle.com>
2006-11-06 23:53:32 +0000

    turn off throttle.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@7886 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/spam/hier/usr/share/metavize/schema/spam-base/settings-convert-4.sql
+++ tran/spam/hier/usr/share/metavize/schema/spam-base/settings-convert-4.sql
@@ -1,5 +1,8 @@
 -- settings convert for release 4.1
 
+-- turn off throttle
+UPDATE tr_spam_smtp_config SET throttle = false;
+
 -- change spam strength definitions
 UPDATE tr_spam_smtp_config SET strength = 33 WHERE strength = 35;
 UPDATE tr_spam_smtp_config SET strength = 35 WHERE strength = 43;
