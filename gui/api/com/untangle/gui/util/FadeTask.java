76e7002281423187108e6f9e7015834023895982
Ian Nieves <inieves@untangle.com>
2006-12-07 04:29:27 +0000

    svn merge -r 8344:8345 svn://chef/branch/release-4.1/alpine
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@8348 8ebf8b4e-06f3-0310-961f-89f74618be54

--- gui/api/com/untangle/gui/util/FadeTask.java
+++ gui/api/com/untangle/gui/util/FadeTask.java
@@ -23,13 +23,15 @@ public class FadeTask implements ActionListener {
     private static final float FADE_DECREMENT = .05f;
     private static final int FADE_CONSTANT_COUNTS = 6;
     /////////////////////////////////
-    
+	int redBack;
+	int greenBack;
+	int blueBack;  
     private int fadeConstantCount = 0;
     private float fadeLeft = 1f;
     private javax.swing.Timer fadeTimer;
-    private int redInit;
-    private int greenInit;
-    private int blueInit;
+    private int redInit   = 104;
+    private int greenInit = 189;
+    private int blueInit  = 73;
     private int alpha;
     private JComponent jComponent;
     private Color backgroundColor;
@@ -38,18 +40,28 @@ public class FadeTask implements ActionListener {
 	fadeTimer = new javax.swing.Timer(FADE_DELAY_MILLIS, this);
 	backgroundColor = jComponent.getBackground();
 	this.hasBackground = hasBackground;
-	redInit = backgroundColor.getRed();
-	greenInit = backgroundColor.getGreen();
-	blueInit = backgroundColor.getBlue();
+	redBack = backgroundColor.getRed();
+	greenBack = backgroundColor.getGreen();
+	blueBack = backgroundColor.getBlue();
 	this.jComponent = jComponent;
 	fadeTimer.start();
     }
     public void actionPerformed(ActionEvent evt){
         if ( fadeLeft > 0f ) {
-	    jComponent.setBackground(new Color( (int)(((float)redInit)*(1f-fadeLeft)), 
-						(int)(((float)greenInit)*(1f-fadeLeft)), 
-						blueInit + (int)((255f-(float)blueInit)*fadeLeft),
-						(int) (hasBackground ? 200f*fadeLeft : 255 ) ));
+            if( hasBackground ){
+                jComponent.setBackground(new Color(
+                                                   redInit, 
+                                                   greenInit, 
+                                                   blueInit, 
+                                                   (int)(255f*fadeLeft)));
+            }
+            else{
+                jComponent.setBackground(new Color(
+                                                   (int)(((float)redInit)+((float)(redBack-redInit)*(1f-fadeLeft))), 
+                                                   (int)(((float)greenInit)+((float)(greenBack-greenInit)*(1f-fadeLeft))), 
+                                                   (int)(((float)blueInit)+((float)(blueBack-blueInit)*(1f-fadeLeft))), 
+                                                   (int)255));
+            }
 	    if( hasBackground )
 		jComponent.getParent().repaint();
 	    else
