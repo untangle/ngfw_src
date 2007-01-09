7a3e395916e594cab133269b809f612a587afecd
Cho Ng <cng@untangle.com>
2007-01-09 00:09:00 +0000

    update keyname
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@8550 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/mail/api/com/untangle/tran/mail/papi/quarantine/WebConstants.java
+++ tran/mail/api/com/untangle/tran/mail/papi/quarantine/WebConstants.java
@@ -21,7 +21,7 @@ public class WebConstants {
   public static final String SAFELIST_ADD_RV = "sladd";
   public static final String SAFELIST_REMOVE_RV = "slremove";
   public static final String VIEW_INBOX_RV = "viewibx";
-  public static final String MAIL_ID_RP = "mid";
+  public static final String MAIL_ID_RP = "mailId";
   //When the "action" is "sladd" or "slremove", this is the
   //key for the value of the (base64 encoded) email address
   public static final String SAFELIST_TARGET_ADDR_RP = "sladdr";
