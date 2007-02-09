3e89d6a72139e055c38c0bb6187c02ce9762f54c
Robert Scott <rbscott@untangle.com>
2007-02-09 00:03:00 +0000

    Fix for bug 2342
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@8802 8ebf8b4e-06f3-0310-961f-89f74618be54

--- mvvm/impl/com/untangle/mvvm/type/IPaddrUserType.java
+++ mvvm/impl/com/untangle/mvvm/type/IPaddrUserType.java
@@ -17,6 +17,11 @@ import java.net.UnknownHostException;
 
 public class IPaddrUserType extends StringBasedUserType
 {
+    private static final IPaddr EMPTY_IPADDR = new IPaddr( null );
+
+    /* special string used to represent an empty ip addr */
+    private static final String EMPTY_STRING = "0.0.0.0/31";
+    
     public Class returnedClass()
     {
         return IPaddr.class;
@@ -24,11 +29,33 @@ public class IPaddrUserType extends StringBasedUserType
 
     protected String userTypeToString( Object v )
     {
+        /* null (don't actually have to worry about null because of the StringBasedUserType.) */
+        if ( v == null ) return null;
+        
+        IPaddr i = (IPaddr)v;
+        
+        if ( i.isEmpty()) return EMPTY_STRING;
+        
         return ((IPaddr)v).toString();
     }
 
     public Object createUserType( String val ) throws Exception
     {
+        /* the following are the conditions which should use the empty ip addr */
+
+        /* null (don't actually have to worry about null because of the StringBasedUserType.) */
+        if ( val == null ) return EMPTY_IPADDR;
+
+        /* empty string */
+        val = val.trim();
+        if ( val.length() == 0 ) return EMPTY_IPADDR;
+
+        /* This is a special string that is used to store an empty IP
+         * address in the database.  this is because 0.0.0.0 (an
+         * address of 0.0.0.0) differs from IPaddr( null ) (no address
+         * at all). */
+        if ( EMPTY_STRING.equals( val )) return EMPTY_IPADDR;
+       
         return IPaddr.parse( val );
     }
 }
