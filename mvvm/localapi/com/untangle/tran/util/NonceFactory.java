b3cac9763b05fca5f89ad1e7262787312c15035e
Aaron Read <amread@untangle.com>
2007-02-23 23:34:41 +0000

    progress
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@8969 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ mvvm/localapi/com/untangle/tran/util/NonceFactory.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2003-2007 Untangle, Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Untangle, Inc. ("Confidential Information"). You shall
+ * not disclose such Confidential Information.
+ *
+ * $Id: SpywareHttpHandler.java 8668 2007-01-29 19:17:09Z amread $
+ */
+
+package com.untangle.tran.util;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class NonceFactory<T>
+{
+    private final Map<String, T> nonces = new HashMap<String, T>();
+    private final Random random = new Random();
+
+    private Timer timer = null;
+
+    public NonceFactory() { }
+
+    String generateNonce(T o)
+    {
+        String nonce;
+
+        synchronized (this) {
+            while (nonces.containsKey(nonce = Long.toHexString(random.nextLong())));
+
+            nonces.put(nonce, o);
+            if (null == timer) {
+                timer = new Timer(true);
+            }
+
+            timer.schedule(new PurgeTask(nonce), 600000);
+        }
+
+        return nonce;
+    }
+
+    T getNonceData(String nonce)
+    {
+        synchronized (this) {
+            return nonces.get(nonce);
+        }
+    }
+
+    T removeNonce(String nonce)
+    {
+        T data;
+
+        synchronized (this) {
+            data = nonces.remove(nonce);
+            if (0 == nonces.size()) {
+                timer.cancel();
+                timer = null;
+            }
+        }
+
+        return data;
+    }
+
+    private class PurgeTask extends TimerTask
+    {
+        private final String nonce;
+
+        PurgeTask(String nonce)
+        {
+            this.nonce = nonce;
+        }
+
+        public void run()
+        {
+            synchronized (NonceFactory.this) {
+                nonces.remove(nonce);
+                if (0 == nonces.size()) {
+                    timer.cancel();
+                    timer = null;
+                }
+            }
+        }
+    }
+}
