263c08706b74afb06ef34e8df17b46a516843ce9
Ian Nieves <inieves@untangle.com>
2007-02-26 09:26:06 +0000

    this thread was spinning sometimes, added a check for that...
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@8980 8ebf8b4e-06f3-0310-961f-89f74618be54

--- gui/api/com/untangle/gui/util/MessageClientThread.java
+++ gui/api/com/untangle/gui/util/MessageClientThread.java
@@ -41,8 +41,10 @@ public class MessageClientThread extends Thread implements Shutdownable {
     public void run(){
 
 	MessageQueue<ToolboxMessage> toolQ = null;
-
+    int failureCount = 0;
 	while(!stop){
+        if(failureCount>10)
+            break;
 	    try{
 		if( toolQ == null )
 		    toolQ = mvvmContext.toolboxManager().subscribe();
@@ -52,8 +54,9 @@ public class MessageClientThread extends Thread implements Shutdownable {
 		}
 		Thread.sleep(2000);
 	    }
-	    catch(InterruptedException e){ continue; }
+	    catch(InterruptedException e){ failureCount++; continue; }
 	    catch(Exception e){
+            failureCount++;
 		if( !isInterrupted() ){
 		    try{ Util.handleExceptionWithRestart("Error handling messages", e); }
 		    catch(Exception f){
