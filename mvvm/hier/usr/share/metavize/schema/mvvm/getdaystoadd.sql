e5067b239865ee78a6b0d87e7baa39e55f59beae
John Irwin <jdi@untangle.com>
2007-03-09 19:17:04 +0000

    fixed
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@9241 8ebf8b4e-06f3-0310-961f-89f74618be54

--- mvvm/hier/usr/share/metavize/schema/mvvm/getdaystoadd.sql
+++ mvvm/hier/usr/share/metavize/schema/mvvm/getdaystoadd.sql
@@ -1,13 +1,33 @@
+CREATE TABLE day_name_date (
+        day_name text NOT NULL,
+        day_begin date NOT NULL);
+
 DROP TABLE :outtable;
 
 CREATE TABLE :outtable (
         day_name text NOT NULL,
         day_begin date NOT NULL);
 
+-- Note that loop iteration step is -1.
+-- args: start-days, end-days, base-date
+CREATE OR REPLACE FUNCTION make_dates(INTEGER, INTEGER, DATE)
+  RETURNS SETOF day_name_date
+  LANGUAGE 'plpgsql'
+  AS '
+    DECLARE
+      rec day_name_date%ROWTYPE;
+    BEGIN
+      FOR index IN REVERSE $1..$2 LOOP
+        SELECT \'\', $3 + index INTO rec;
+        RETURN NEXT rec;
+      END LOOP;
+      RETURN;
+    END
+  ';
+
 -- Yucky, but these three together get the right output.
 INSERT INTO :outtable
-    SELECT '' as day_name, (:midnight)::date + s.a as day_begin
-      FROM generate_series(-1, -:daystosave, -1) AS s(a);
+   SELECT * FROM make_dates(-1, -:daystosave, (:midnight)::date);
 DELETE FROM :outtable 
   WHERE EXISTS
       (SELECT 1 from :intable intable WHERE :outtable.day_begin = intable.day_begin);
