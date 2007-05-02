6013534f4097ba61f7d8f2637e315e0c3f1db27d
Aaron Read <amread@untangle.com>
2007-05-02 21:04:50 +0000

    add comments
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@9967 8ebf8b4e-06f3-0310-961f-89f74618be54

--- mvvm/bootstrap/com/untangle/mvvm/engine/DataSourceFactory.java
+++ mvvm/bootstrap/com/untangle/mvvm/engine/DataSourceFactory.java
@@ -18,6 +18,12 @@ import java.sql.SQLException;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import com.mchange.v2.c3p0.DataSources;
 
+/**
+ * Provides database connections from the connection pool.
+ *
+ * @author <a href="mailto:amread@untangle.com">Aaron Read</a>
+ * @version 1.0
+ */
 public class DataSourceFactory
 {
     private static final DataSourceFactory FACTORY = new DataSourceFactory();
@@ -52,11 +58,23 @@ public class DataSourceFactory
 
     // public methods ---------------------------------------------------------
 
+    /**
+     * Get new database connection from the pool.
+     *
+     * @return a database connection.
+     * @exception SQLException if cannot get a new connection.
+     */
     public Connection getConnection() throws SQLException
     {
         return dataSource.getConnection();
     }
 
+    /**
+     * Closes the connection, returning it to the pool.
+     *
+     * @param c connection to close.
+     * @exception SQLException if cannot close the connection.
+     */
     public void closeConnection(Connection c) throws SQLException
     {
         c.close();
@@ -64,6 +82,11 @@ public class DataSourceFactory
 
     // package protected methods ----------------------------------------------
 
+    /**
+     * Destroys the connection pool, freeing its resources.
+     *
+     * @exception SQLException if pool cannot be destroyed.
+     */
     void destroy() throws SQLException
     {
         //DataSources.destroy(dataSource);
