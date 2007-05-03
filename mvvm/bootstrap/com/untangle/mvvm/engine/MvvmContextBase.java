a35dc3a6221f7278103748f9c4c214b6739fc6c3
Aaron Read <amread@untangle.com>
2007-05-03 18:21:34 +0000

    added more comments
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@9984 8ebf8b4e-06f3-0310-961f-89f74618be54

--- mvvm/bootstrap/com/untangle/mvvm/engine/MvvmContextBase.java
+++ mvvm/bootstrap/com/untangle/mvvm/engine/MvvmContextBase.java
@@ -14,7 +14,7 @@ package com.untangle.mvvm.engine;
 
 /**
  * This class is for Main to manipulate MvvmContext without resorting
- * to reflection and allowing package protection for sensitive methods.
+ * to reflection and allowing package protection of sensitive methods.
  *
  * @author <a href="mailto:amread@untangle.com">Aaron Read</a>
  * @version 1.0
@@ -23,28 +23,61 @@ public abstract class MvvmContextBase
 {
     protected Main main;
 
+    // abstract methods -------------------------------------------------------
+
+    /**
+     * Initialize the MVVM, starting up base services.
+     */
     protected abstract void init();
-    protected abstract void destroy();
+
+    /**
+     * Do final initialization, begin processing traffic.
+     */
     protected abstract void postInit();
 
+    /**
+     * Destroy the MVVM, stopping all services.
+     */
+    protected abstract void destroy();
+
+    // protected methods ------------------------------------------------------
+
+    /**
+     * Gives MvvmContextImpl access to the Main object.
+     *
+     * @return the Main object given to {@link #doInit(Main)}.
+     */
+    protected Main getMain()
+    {
+        return main;
+    }
+
+    // package protected methods ----------------------------------------------
+
+    /**
+     * Method for bootstrap access to MvvmContext.
+     *
+     * @param main the Main object that started the boot process.
+     */
     void doInit(Main main)
     {
         this.main = main;
         init();
     }
 
+    /**
+     * Method for bootstrap access to MvvmContext.
+     */
     void doPostInit()
     {
         postInit();
     }
 
+    /**
+     * Method for bootstrap access to MvvmContext.
+     */
     void doDestroy()
     {
         destroy();
-    }
-
-    protected Main getMain()
-    {
-        return main;
     }
 }
