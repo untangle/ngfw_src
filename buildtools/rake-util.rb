f95b6c87cd6340d3ab169555f73a55dbd2ca072e
Sebastien Delafond <sdelafond@gmail.com>
2007-05-04 00:12:07 +0000

    Move to Sun's 1.5.0.10 packaged by Debian.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@10001 8ebf8b4e-06f3-0310-961f-89f74618be54

--- buildtools/rake-util.rb
+++ buildtools/rake-util.rb
@@ -100,7 +100,7 @@ class BuildEnv
 
     @servletcommon = "#{Dir.pwd}/servlet/common"
 
-    @javahome = ENV['JAVA_HOME'] || "/opt/java"
+    @javahome = "/usr/lib/jvm/java-1.5.0-sun-1.5.0.10"
 
     ## Flag indicating whether or not this is a development or
     ## package/production build.
@@ -114,7 +114,7 @@ class BuildEnv
 
     {
       /@PREFIX@/ => prefix,
-      /@DEFAULT_JAVA_HOME@/ => '/opt/java',
+      /@DEFAULT_JAVA_HOME@/ => '/usr/lib/jvm/java-1.5.0-sun-1.5.0.10',
       /@USR_BIN@/ => "#{prefix}/usr/bin",
       /@MVVM_HOME@/ => "#{prefix}/usr/share/metavize",
       /@MVVM_DUMP@/ => "#{prefix}/usr/share/metavize/dump",
