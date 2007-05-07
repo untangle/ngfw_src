bcf39c8d715bd8e7b132c1068a54365e5ff13b26
Aaron Read <amread@untangle.com>
2007-05-07 20:22:28 +0000

    more comments
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@10036 8ebf8b4e-06f3-0310-961f-89f74618be54

--- mvvm/bootstrap/com/untangle/mvvm/logging/MvvmLoggingContextFactory.java
+++ mvvm/bootstrap/com/untangle/mvvm/logging/MvvmLoggingContextFactory.java
@@ -11,7 +11,23 @@
 
 package com.untangle.mvvm.logging;
 
+/**
+ * Factory for retrieving the current logging context. Allows for
+ * access to the {@link MvvmLoggingContext} on demand. Returns the
+ * {@link MvvmLoggingContext} in effect at the time the {@link #get()}
+ * method is called. Thus, it is only guaranteed to work if called
+ * syncronously from the thread that accesses {@link #get()}.
+ *
+ * @author <a href="mailto:amread@untangle.com">Aaron Read</a>
+ * @version 1.0
+ */
 public interface MvvmLoggingContextFactory
 {
+    /**
+     * Returns the {@link MvvmLoggingContext} in effect for this
+     * thread at the moment the get() method is called.
+     *
+     * @return the effective {@link MvvmLoggingContext}.
+     */
     MvvmLoggingContext get();
 }
