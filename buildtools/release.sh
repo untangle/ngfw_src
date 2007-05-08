541462d9b2d33445295aa6049cae969f0fd7007b
Dirk Morris <dmorris@untangle.com>
2007-05-08 18:49:10 +0000

    Starting to cleanup the alpine build.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@10048 8ebf8b4e-06f3-0310-961f-89f74618be54

--- buildtools/release.sh
+++ buildtools/release.sh
@@ -20,12 +20,9 @@ fi
 
 echo "Releasing: \"$1\""
 
-
 echo "------------------------"
-echo -e "Releasing:\n`ls ../*.deb | egrep \"$pkg\"`"
-sudo ../pkgs/scripts/deb-add.sh /var/www/$dest/ `ls ../*.deb | egrep "$pkg"`
-echo "------------------------"
-sudo ../pkgs/scripts/deb-scan.sh /var/www/$dest/ 
+echo -e "Releasing:\n`ls ../*.changes | egrep \"$pkg\"`"
+dput -c ../pkgs/scripts/dput.cf mephisto ../`head -n 1 debian/changelog | perl -npe 's/ \((.*)\).*/*$$1/'`*.changes
 
 rm -f debian/release_list
 svn commit -m"release" debian/*.version debian/changelog
