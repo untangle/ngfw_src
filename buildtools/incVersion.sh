bba296c5f784ce98e30cc410027cf8d99e42aa18
Sebastien Delafond <sdelafond@gmail.com>
2007-05-11 01:17:32 +0000

    debhelper compat level set to 4 upgraded Standards-Version to current (3.7.2)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@10135 8ebf8b4e-06f3-0310-961f-89f74618be54

--- buildtools/incVersion.sh
+++ buildtools/incVersion.sh
@@ -6,20 +6,23 @@ distribution=${1}
 
 echo -n "Setting version, setting distribution to \"$distribution\"..."
 
-if [[ ! -d ../pkgs ]] ; then
-  echo "You must check out the pkgs subversion directory" && exit 1
+if [[ -f ../VERSION ]] ; then
+  versionFile=../VERSION
+else
+  versionFile=../../VERSION
 fi
 
 revision=`svn info . | awk '/Revision: / { print $2 }'`
 timestamp=`svn info | awk '/Last Changed Date: / { gsub(/-/, "", $4) ; print $4 }'`
 hasLocalChanges=`svn status | grep -v -E '^\?'`
 
-baseVersion=`cat VERSION`~svn${timestamp}r${revision}
+baseVersion=`cat $versionFile`~svn${timestamp}r${revision}
 
 if [[ -z "$hasLocalChanges" ]] ; then
   upstreamVersion=$baseVersion
 else
   upstreamVersion=${baseVersion}+$USER`date +"%Y%m%dT%H%M"`
+  distribution=$USER
 fi
 
 previousUpstreamVersion=`dpkg-parsechangelog | awk '/Version: / { gsub(/-.*/, "", $2) ; print $2 }'`
