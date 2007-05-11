8eed3cab11f179299e73a24c23462078fea8d382
Aaron Read <amread@untangle.com>
2007-05-11 21:43:20 +0000

    hippie
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@10158 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/package.rb
+++ tran/package.rb
@@ -1,32 +1,31 @@
 # -*-ruby-*-
 
-require "tran/test/package.rb"
-require "tran/reporting/package.rb"
-require "tran/portal/package.rb"
-require "tran/ftp/package.rb"
-require "tran/http/package.rb"
-require "tran/mail/package.rb"
-require "tran/spyware/package.rb"
-require "tran/nat/package.rb"
+require 'tran/test/package.rb'
+require 'tran/reporting/package.rb'
+require 'tran/ftp/package.rb'
+require 'tran/http/package.rb'
+require 'tran/mail/package.rb'
+require 'tran/spyware/package.rb'
+require 'tran/nat/package.rb'
 
-require "tran/airgap/package.rb"
-require "tran/firewall/package.rb"
-require "tran/openvpn/package.rb"
-require "tran/protofilter/package.rb"
-require "tran/boxbackup/package.rb"
-require "tran/sigma/package.rb"
-require "tran/httpblocker/package.rb"
-require "tran/ids/package.rb"
+require 'tran/airgap/package.rb'
+require 'tran/firewall/package.rb'
+require 'tran/openvpn/package.rb'
+require 'tran/protofilter/package.rb'
+require 'tran/boxbackup/package.rb'
+require 'tran/sigma/package.rb'
+require 'tran/httpblocker/package.rb'
+require 'tran/ids/package.rb'
 
 ## Base Transforms
-require "tran/spam/package.rb"
-require "tran/virus/package.rb"
+require 'tran/spam/package.rb'
+require 'tran/virus/package.rb'
 
 ## SPAM based transforms
-require "tran/clamphish/package.rb"
-require "tran/spamassassin/package.rb"
+require 'tran/clamphish/package.rb'
+require 'tran/spamassassin/package.rb'
 
 ## Virus based transforms
-require "tran/clam/package.rb"
-require "tran/hauri/package.rb"
-require "tran/kav/package.rb"
+require 'tran/clam/package.rb'
+require 'tran/hauri/package.rb'
+require 'tran/kav/package.rb'
