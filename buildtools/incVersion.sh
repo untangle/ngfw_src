c0d797770824c0428c7c38e6b9712a92d5bda0cd
Sebastien Delafond <sdelafond@gmail.com>
2007-05-11 22:50:30 +0000

    .
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@10161 8ebf8b4e-06f3-0310-961f-89f74618be54

--- buildtools/incVersion.sh
+++ buildtools/incVersion.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+# usage...
 [[ ! $# -eq 1 ]] && echo "Usage: $0 distribution" && exit 1
 
+# CL args
 distribution=${1}
+case $distribution in
+  # FIXME: somehow only codenames work for uploading, right now
+  testing) distribution=mustang ;;
+esac
 
-echo -n "Setting version, setting distribution to \"$distribution\"..."
+echo -n "Setting version, setting distribution to \"$distribution\""
 
 if [[ -f ../VERSION ]] ; then
   versionFile=../VERSION
@@ -12,8 +18,12 @@ else
   versionFile=../../VERSION
 fi
 
+# get 2 values from SVN: last changed revision & timestamp for the
+# current directory
 revision=`svn info . | awk '/Last Changed Rev: / { print $4 }'`
-timestamp=`svn info | awk '/Last Changed Date: / { gsub(/-/, "", $4) ; print $4 }'`
+timestamp=`svn info . | awk '/Last Changed Date: / { gsub(/-/, "", $4) ; print $4 }'`
+
+# this how we figure out if we're up-to-date or not
 hasLocalChanges=`svn status | grep -v -E '^\?'`
 
 baseVersion=`cat $versionFile`~svn${timestamp}r${revision}
@@ -31,6 +41,7 @@ else
   distribution=$USER
 fi
 
+echo -n ", \"version\" to ${upstreamVersion}-1"
 dch -v ${upstreamVersion}-1 -D ${distribution} "auto build"
 
 echo " done."
