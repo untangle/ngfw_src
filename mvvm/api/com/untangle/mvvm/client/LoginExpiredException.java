3ee78a49e3ee1c7ccef2f8d8ba34def2877628e6
Aaron Read <amread@untangle.com>
2007-06-08 23:07:28 +0000

    javadoc
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/alpine@10637 8ebf8b4e-06f3-0310-961f-89f74618be54

--- mvvm/api/com/untangle/mvvm/client/LoginExpiredException.java
+++ mvvm/api/com/untangle/mvvm/client/LoginExpiredException.java
@@ -11,14 +11,31 @@
 
 package com.untangle.mvvm.client;
 
+/**
+ * Signals that login session has expired.
+ *
+ * @author <a href="mailto:amread@untangle.com">Aaron Read</a>
+ * @version 1.0
+ */
 public class LoginExpiredException extends InvocationException
 {
-    public LoginExpiredException() { super(); }
-    public LoginExpiredException(String message) { super(message); }
+    public LoginExpiredException()
+    {
+        super();
+    }
+
+    public LoginExpiredException(String message)
+    {
+        super(message);
+    }
+
     public LoginExpiredException(String message, Throwable cause)
     {
         super(message, cause);
     }
 
-    public LoginExpiredException(Throwable cause) { super(cause); }
+    public LoginExpiredException(Throwable cause)
+    {
+        super(cause);
+    }
 }
