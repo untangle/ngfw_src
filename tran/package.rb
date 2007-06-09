cc8e93fbb15ff6dbbbe0da708934a2087e7d672e
John Irwin <jdi@untangle.com>
2007-06-09 17:55:07 +0000

    br
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@10666 8ebf8b4e-06f3-0310-961f-89f74618be54

--- tran/package.rb
+++ tran/package.rb
@@ -1,30 +1,30 @@
 # -*-ruby-*-
 
-require "#{ALPINE_HOME}/tran/test/package.rb"
-require "#{ALPINE_HOME}/tran/reporting/package.rb"
-require "#{ALPINE_HOME}/tran/ftp/package.rb"
-require "#{ALPINE_HOME}/tran/http/package.rb"
-require "#{ALPINE_HOME}/tran/mail/package.rb"
-require "#{ALPINE_HOME}/tran/spyware/package.rb"
-require "#{ALPINE_HOME}/tran/nat/package.rb"
+require "#{SRC_HOME}/tran/test/package.rb"
+require "#{SRC_HOME}/tran/reporting/package.rb"
+require "#{SRC_HOME}/tran/ftp/package.rb"
+require "#{SRC_HOME}/tran/http/package.rb"
+require "#{SRC_HOME}/tran/mail/package.rb"
+require "#{SRC_HOME}/tran/spyware/package.rb"
+require "#{SRC_HOME}/tran/nat/package.rb"
 
-require "#{ALPINE_HOME}/tran/airgap/package.rb"
-require "#{ALPINE_HOME}/tran/firewall/package.rb"
-require "#{ALPINE_HOME}/tran/openvpn/package.rb"
-require "#{ALPINE_HOME}/tran/protofilter/package.rb"
-require "#{ALPINE_HOME}/tran/sigma/package.rb"
-require "#{ALPINE_HOME}/tran/httpblocker/package.rb"
-require "#{ALPINE_HOME}/tran/ids/package.rb"
+# require "#{SRC_HOME}/tran/airgap/package.rb"
+require "#{SRC_HOME}/tran/firewall/package.rb"
+require "#{SRC_HOME}/tran/openvpn/package.rb"
+require "#{SRC_HOME}/tran/protofilter/package.rb"
+require "#{SRC_HOME}/tran/sigma/package.rb"
+require "#{SRC_HOME}/tran/httpblocker/package.rb"
+require "#{SRC_HOME}/tran/ids/package.rb"
 
 ## Base Transforms
-require "#{ALPINE_HOME}/tran/spam/package.rb"
-require "#{ALPINE_HOME}/tran/virus/package.rb"
+require "#{SRC_HOME}/tran/spam/package.rb"
+require "#{SRC_HOME}/tran/virus/package.rb"
 
 ## SPAM based transforms
-require "#{ALPINE_HOME}/tran/clamphish/package.rb"
-require "#{ALPINE_HOME}/tran/spamassassin/package.rb"
+require "#{SRC_HOME}/tran/clamphish/package.rb"
+require "#{SRC_HOME}/tran/spamassassin/package.rb"
 
 ## Virus based transforms
-require "#{ALPINE_HOME}/tran/clam/package.rb"
-require "#{ALPINE_HOME}/tran/hauri/package.rb"
-require "#{ALPINE_HOME}/tran/kav/package.rb"
+require "#{SRC_HOME}/tran/clam/package.rb"
+require "#{SRC_HOME}/tran/hauri/package.rb"
+require "#{SRC_HOME}/tran/kav/package.rb"
