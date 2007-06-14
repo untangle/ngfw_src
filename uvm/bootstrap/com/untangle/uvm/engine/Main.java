3c781edeaab39aec38773a7dad8ba7559fe8fdd9
John Irwin <jdi@untangle.com>
2007-06-14 19:03:53 +0000

    brl
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@10940 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/bootstrap/com/untangle/uvm/engine/Main.java
+++ uvm/bootstrap/com/untangle/uvm/engine/Main.java
@@ -1,15 +1,15 @@
 /*
- * $HeadURL$
+ * $HeadURL:$
+ * Copyright (c) 2003-2007 Untangle, Inc. 
  *
- * Copyright (c) 2003-2007 Untangle, Inc.
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2,
  * as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful, but
  * AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
- * See the GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
+ * NONINFRINGEMENT.  See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
