b87b058fddf4959c79ba91d62bb0ab660d7a7cf9
Dirk Morris <dmorris@untangle.com>
2007-06-15 22:24:49 +0000

    fixing mkt.sh script
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@10997 8ebf8b4e-06f3-0310-961f-89f74618be54

--- debian/mkt.sh
+++ debian/mkt.sh
@@ -5,45 +5,45 @@ files="`ls *-node-*.postinst | grep -v template`"
 
 for i in $files ; do 
     node="`echo $i | awk -F\. '{print $1}' `"
-    nodename="`echo $node | awk -F- '{print $2}' `"
+    nodename="`echo $node | awk -F- '{print $3}' `"
 
     echo "Making files for" $nodename " ... " 
 
-    for templatefile in `ls template-node*` ; do 
-        newfile="`echo $templatefile | sed -s \"s/template-node/$node/\" `"
+    for templatefile in `ls untangle-node-template*` ; do 
+        newfile="`echo $templatefile | sed -s \"s/template/$nodename/\" `"
         echo $newfile
-        cat $templatefile | sed -s "s/template-node/$node/" | sed -s "s/template/$nodename/" > $newfile
+        cat $templatefile | sed -s "s/template/$nodename/g" > $newfile
     done
 done
 
-
-files="`ls *-node-*.postinst | grep -v template`"
+files="`ls *-casing-*.postinst | grep -v template`"
 
 for i in $files ; do 
     node="`echo $i | awk -F\. '{print $1}' `"
-    nodename="`echo $node | awk -F- '{print $2}' `"
+    nodename="`echo $node | awk -F- '{print $3}' `"
 
     echo "Making files for" $nodename " ... " 
 
-    for templatefile in `ls template-casing*` ; do 
-        newfile="`echo $templatefile | sed -s \"s/template-casing/$node/\" `"
+    for templatefile in `ls untangle-casing-template*` ; do 
+        newfile="`echo $templatefile | sed -s \"s/template/$nodename/\" `"
         echo $newfile
-        cat $templatefile | sed -s "s/template-node/$node/" | sed -s "s/template/$nodename/" > $newfile
+        cat $templatefile | sed -s "s/template/$nodename/g" > $newfile
     done
 done
 
+exit
 
-files="`ls *-node-*.postinst | grep -v template`"
+files="`ls *-base-*.postinst | grep -v template`"
 
 for i in $files ; do 
     node="`echo $i | awk -F\. '{print $1}' `"
-    nodename="`echo $node | awk -F- '{print $2}' `"
+    nodename="`echo $node | awk -F- '{print $3}' `"
 
     echo "Making files for" $nodename " ... " 
 
-    for templatefile in `ls template-base*` ; do 
-        newfile="`echo $templatefile | sed -s \"s/template-base/$node/\" `"
+    for templatefile in `ls untangle-base-template*` ; do 
+        newfile="`echo $templatefile | sed -s \"s/template/$nodename/\" `"
         echo $newfile
-        cat $templatefile | sed -s "s/template-node/$node/" | sed -s "s/template/$nodename/" > $newfile
+        cat $templatefile | sed -s "s/template/$nodename/g" > $newfile
     done
 done
