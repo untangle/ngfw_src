7b6bf5a0c75a049e689009ecf053b0e2f4196e9f
Dirk Morris <dmorris@untangle.com>
2007-06-15 22:10:30 +0000

    renaming node packages to reflect new node names
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@10994 8ebf8b4e-06f3-0310-961f-89f74618be54

--- debian/mkt.sh
+++ debian/mkt.sh
@@ -1,49 +1,49 @@
 #!/bin/sh
 
-# exclude email-transform, it is custom
-files="`ls *-transform.version | grep -v template`"
+# exclude email-node, it is custom
+files="`ls *-node-*.postinst | grep -v template`"
 
 for i in $files ; do 
-    tran="`echo $i | awk -F\. '{print $1}' `"
-    tname="`echo $tran | awk -F- '{print $1}' `"
+    node="`echo $i | awk -F\. '{print $1}' `"
+    nodename="`echo $node | awk -F- '{print $2}' `"
 
-    echo "Making files for" $tname " ... " 
+    echo "Making files for" $nodename " ... " 
 
-    for templatefile in `ls template-transform*` ; do 
-        newfile="`echo $templatefile | sed -s \"s/template-transform/$tran/\" `"
+    for templatefile in `ls template-node*` ; do 
+        newfile="`echo $templatefile | sed -s \"s/template-node/$node/\" `"
         echo $newfile
-        cat $templatefile | sed -s "s/template-transform/$tran/" | sed -s "s/template/$tname/" > $newfile
+        cat $templatefile | sed -s "s/template-node/$node/" | sed -s "s/template/$nodename/" > $newfile
     done
 done
 
 
-files="`ls *-casing.version | grep -v template`"
+files="`ls *-node-*.postinst | grep -v template`"
 
 for i in $files ; do 
-    tran="`echo $i | awk -F\. '{print $1}' `"
-    tname="`echo $tran | awk -F- '{print $1}' `"
+    node="`echo $i | awk -F\. '{print $1}' `"
+    nodename="`echo $node | awk -F- '{print $2}' `"
 
-    echo "Making files for" $tname " ... " 
+    echo "Making files for" $nodename " ... " 
 
     for templatefile in `ls template-casing*` ; do 
-        newfile="`echo $templatefile | sed -s \"s/template-casing/$tran/\" `"
+        newfile="`echo $templatefile | sed -s \"s/template-casing/$node/\" `"
         echo $newfile
-        cat $templatefile | sed -s "s/template-transform/$tran/" | sed -s "s/template/$tname/" > $newfile
+        cat $templatefile | sed -s "s/template-node/$node/" | sed -s "s/template/$nodename/" > $newfile
     done
 done
 
 
-files="`ls *-base.version | grep -v template`"
+files="`ls *-node-*.postinst | grep -v template`"
 
 for i in $files ; do 
-    tran="`echo $i | awk -F\. '{print $1}' `"
-    tname="`echo $tran | awk -F- '{print $1}' `"
+    node="`echo $i | awk -F\. '{print $1}' `"
+    nodename="`echo $node | awk -F- '{print $2}' `"
 
-    echo "Making files for" $tname " ... " 
+    echo "Making files for" $nodename " ... " 
 
     for templatefile in `ls template-base*` ; do 
-        newfile="`echo $templatefile | sed -s \"s/template-base/$tran/\" `"
+        newfile="`echo $templatefile | sed -s \"s/template-base/$node/\" `"
         echo $newfile
-        cat $templatefile | sed -s "s/template-transform/$tran/" | sed -s "s/template/$tname/" > $newfile
+        cat $templatefile | sed -s "s/template-node/$node/" | sed -s "s/template/$nodename/" > $newfile
     done
 done
