35f73a970e1c8c46e5edb823f8cda17474b003f0
Aaron Read <amread@untangle.com>
2007-06-19 22:54:02 +0000

    rename
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@11210 8ebf8b4e-06f3-0310-961f-89f74618be54

--- gui/api/com/untangle/gui/main/PolicyStateMachine.java
+++ gui/api/com/untangle/gui/main/PolicyStateMachine.java
@@ -1,6 +1,6 @@
 /*
  * $HeadURL$
- * Copyright (c) 2003-2007 Untangle, Inc. 
+ * Copyright (c) 2003-2007 Untangle, Inc.
  *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2,
@@ -522,7 +522,7 @@ public class PolicyStateMachine implements ActionListener, Shutdownable {
                 // FOCUS AND HIGHLIGHT IN CURRENT RACK
                 focusInRack(mNodeJPanel);
                 // AUTO ON
-                if( nodeDesc.getName().startsWith("nat") || nodeDesc.getName().startsWith("openvpn") ){
+                if( nodeDesc.getName().endsWith("nat") || nodeDesc.getName().endsWith("openvpn") ){
                     mNodeJPanel.setPowerOnHintVisible(true);
                     MOneButtonJDialog.factory( Util.getMMainJFrame(), "", nodeDesc.getDisplayName()
                                                + " can not be automatically turned on."
