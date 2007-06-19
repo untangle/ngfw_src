28926fe1ace9a28bbae3834a8f2435cfc6968195
Aaron Read <amread@untangle.com>
2007-06-19 02:35:36 +0000

    javadoc
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@11153 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/api/com/untangle/uvm/node/NodeStartException.java
+++ uvm/api/com/untangle/uvm/node/NodeStartException.java
@@ -1,6 +1,6 @@
 /*
  * $HeadURL$
- * Copyright (c) 2003-2007 Untangle, Inc. 
+ * Copyright (c) 2003-2007 Untangle, Inc.
  *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2,
@@ -33,6 +33,12 @@
 
 package com.untangle.uvm.node;
 
+/**
+ * Signals that a Node could not be started.
+ *
+ * @author <a href="mailto:amread@untangle.com">Aaron Read</a>
+ * @version 1.0
+ */
 public class NodeStartException extends NodeException
 {
     public NodeStartException()
