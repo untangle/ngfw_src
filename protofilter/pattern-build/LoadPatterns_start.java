5e3a01c0c9392c5c4165630516051695b0383856
John Irwin <jdi@untangle.com>
2007-06-22 20:09:10 +0000

    cleanup
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@11428 8ebf8b4e-06f3-0310-961f-89f74618be54

--- protofilter/pattern-build/LoadPatterns_start.java
+++ protofilter/pattern-build/LoadPatterns_start.java
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-package com.untangle.tran.protofilter;
+package com.untangle.node.protofilter;
 
 import java.util.TreeMap;
 
