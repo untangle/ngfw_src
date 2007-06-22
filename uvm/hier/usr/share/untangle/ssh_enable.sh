ee6b7387887bab00cfb0c70c9cb6c85d45d80cca
Sebastien Delafond <sdelafond@gmail.com>
2007-06-22 23:58:06 +0000

    restart, not start
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@11460 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/share/untangle/ssh_enable.sh
+++ uvm/hier/usr/share/untangle/ssh_enable.sh
@@ -6,7 +6,7 @@ for PKG_NAME in untangle-support-agent ssh ; do
   INIT_SCRIPT="/etc/init.d/${PKG_NAME}"
 
   # Start the daemon
-  ${INIT_SCRIPT} start
+  ${INIT_SCRIPT} restart
 
   # Enable startup at boot time
   update-rc.d ${PKG_NAME} defaults > /dev/null
