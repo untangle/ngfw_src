abc76e02d57d77e17d668dbe0faad7aa9f087953
Aaron Read <amread@untangle.com>
2007-10-23 16:43:48 +0000

    mkg
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@12485 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/share/untangle/bin/mkg.rb
+++ uvm/hier/usr/share/untangle/bin/mkg.rb
@@ -1,16 +1,23 @@
 #!/usr/bin/ruby
 
 require 'logger'
-require 'debian'
+require 'fileutils'
+require 'getoptlong'
 
-LOG_FILE = '@UVM_LOG@/apt.log'
-LOG = Logger.new(LOG_FILE, 10, 1048576)
+# this is needed now !
+export DEBIAN_FRONTEND=noninteractive
 
-STDERR.reopen(LOG_FILE, 'a')
+INST_OPTS = " -o DPkg::Options::=--force-confnew --yes --force-yes --fix-broken --purge "
+UPGD_OPTS = " -o DPkg::Options::=--force-confnew --yes --force-yes --fix-broken --purge "
+UPDT_OPTS = " --yes --force-yes --purge "
+REMO_OPTS = " --yes --force-yes --fix-broken --purge "
 
-printusage()
-{
-puts <<STR
+PREFIX = "@PREFIX@"
+
+STDERR.reopen('@UVM_LOG@/apt.log', 'a')
+
+def printusage()
+  puts <<STR
 usage:
   mkg installed
   mkg available
@@ -19,146 +26,225 @@ usage:
   mkg upgrade
   mkg remove
 STR
-  exit(1)
-}
+end
 
-def installed()
-  IO.popen("dpkg --get-selections '*'", 'r') do |input|
-    IO.popen("xargs dpkg-query -W", 'w') do |output|
-      input.each_line do |l|
-        s = l.split
-        output.puts(s[0]) if 'install' == s[1]
+class DevMkg
+  @pkg_list_dir = "#{PREFIX}/tmp"
+
+  def initialize(stamp)
+    @stamp = stamp
+  end
+
+  def installed()
+    each_pkg_list_line { |l| puts "#{l.split[1]} 10.0" if /^Package: / =~ l }
+  end
+
+  def available()
+    each_pkg_list_line do |l|
+      puts l unless /^XB-/ =~ l
+      puts "Version: 10.0" if /^Package: / =~ l
+    end
+  end
+
+  def install(name)
+        aptlog "start $stamp"
+        echo "END PACKAGE LIST" >>$APT_LOG
+        aptlog "done $stamp"
+  end
+
+  def update()
+  end
+
+  def upgrade()
+        aptlog "start $stamp"
+        echo "END PACKAGE LIST" >>$APT_LOG
+        aptlog "done $stamp"
+  end
+
+  def remove(pkg)
+  end
+
+  private
+
+  def each_pkg_list_line()
+    Dir.new(@pkg_list_dir).select { |d| /^pkg-list/ =~ d }.each do |p|
+      File.open(p, 'r') do |io|
+        io.each_line { |l| yield l }
       end
     end
   end
 end
 
-def download(pkg)
+class RealMkg
+  def initialize(stamp)
+    @stamp = stamp
+  end
+
+  def installed()
+    IO.popen("dpkg --get-selections 'untangle*'", 'r') do |input|
+      IO.popen("xargs dpkg-query -W", 'w') do |output|
+        input.each_line do |l|
+          s = l.split
+          output.puts(s[0]) if 'install' == s[1]
+        end
+      end
+    end
+  end
+
+  def available()
+    IO.popen("apt-cache search '^untangle-'") do |input|
+      IO.popen("xargs apt-cache show --no-all-versions", 'w') do |output|
+        input.each_line do |l|
+          output.puts(l.split[0])
+        end
+      end
+    end
+  end
+
+  def install(pkg)
+    STDERR.puts "start #{@stamp}"
+    STDERR.puts "downloading: \"#{pkg}\""
+    pkg_list = get_pkg_list("apt-get install #{INST_OPTS} --print-uris #{pkg}")
+    download(pkg, pkg_list)
+    STDERR.puts "installing: \"#{pkg}\""
+
+    IO.popen("apt-get install #{INST_OPTS} #{pkg}", 'r') do |input|
+      input.each_line { |l| STDERR.puts l }
+    end
+    r = $?
+
+    STDERR.puts "done #{@stamp}"
+
+    r
+  end
+
+  def update()
+    STDERR.puts "start #{@stamp}"
+    STDERR.puts "apt-get update #{UPDT_OPTS}"
+    IO.popen("apt-get update #{UPDT_OPTS}") do |io|
+      io.each_line { |l| STDERR.puts l }
+    end
+
+    r = $?
+
+    STDERR.puts "done #{@stamp}"
+  end
+
+  def upgrade()
+    STDERR.puts "start #{@stamp}"
+    STDERR.puts "apt-get upgrade #{UPGD_OPTS}"
+
+    pkg_list = get_pkg_list("apt-get dist-upgrade #{UPGD_OPTS} --print-uris")
+    download(pkg_list)
+    r = cmd_to_stderr("apt-get dist-upgrade #{UPGD_OPTS}")
+
+    STDERR.puts "done #{@stamp}"
+  end
+
+  def remove(pkg)
+    STDERR.puts "start #{@stamp}"
+    STDERR.puts "apt-get remove #{REMO_OPTS} \"$2\""
+    r = cmd_to_stderr("apt-get remove #{REMO_OPTS}")
+    cmd_to_stderr("apt-get clean")
+    STDERR.puts "done #{@stamp}"
+  end
+
+  private
+
+  def cmd_to_stderr(cmd)
+    IO.popen(cmd) do |io|
+      io.each_line { |l| STDERR.puts l }
+    end
+    $?
+  end
+
+  def get_tempdir()
+    tmp = Tempfile.new('mkg')
+    path = tmp.path
+    tmp.delete
+    Dir.mkdir(path)
+    Dir.new(path)
+  end
+
+  def get_pkg_list(apt_cmd)
+    pkg_list = []
+
+    IO.popen(apt_cmd, 'r') do |io|
+      io.each_line do |l|
+        pkg_list << l if /^'http:\/\// =~ l
+      end
+    end
+
+    pkg_list
+  end
+
+  def download(pkg, pkg_list)
+    pkg_list.each { |p| STDERR.puts p }
+    STDERR.puts "END PACKAGE LIST"
+
+    tmp_dir = get_tempdir()
+    begin
+      pkg_list.each do |p|
+        STDERR.puts "downloading: #{p}"
+
+        cmd_to_stderr("wget --progress=dot -P #{tmp_dir.path} #{p}")
+
+        if $?.success?
+          tmp_dir.each do |f|
+            FileUtils.mv("#{tmp_dir.path}/#{f}", "/var/cache/apt/archives")
+          end
+          STDERR.puts "DOWNLOAD SUCCEEDED: #{pkg}"
+        else
+          STDERR.puts "DOWNLOAD FAILED: #{pkg}"
+        end
+
+      end
+    ensure
+      Dir.delete(tmp_dir.path)
+    end
+  end
+
 end
 
+stamp = nil
 
+opts = GetoptLong.new([ '-k', GetoptLong::REQUIRED_ARGUMENT ])
+opts.each do |opt, arg|
+  if '-k' == opt
+    stamp = arg
+  end
+end
 
-# this is needed now !
-export DEBIAN_FRONTEND=noninteractive
+if ARGV.length < 1
+  printusage()
+  exit(1)
+end
 
+ret = 0
 
+mkg = PREFIX.empty? ? RealMkg.new(stamp) : DevMkg(stamp)
 
-INST_OPTS = " -o DPkg::Options::=--force-confnew --yes --force-yes --fix-broken --purge "
-UPGD_OPTS = " -o DPkg::Options::=--force-confnew --yes --force-yes --fix-broken --purge "
-UPDT_OPTS = " --yes --force-yes --purge "
-REMO_OPTS = " --yes --force-yes --fix-broken --purge "
-DEBUG_OPTS = " -o Debug::pkgDPkgPM=true "
-
-
-RET=0
-
-download()
-{
-    aptlist=$1
-
-    cat $aptlist >>$APT_LOG
-    echo "END PACKAGE LIST" >>$APT_LOG
-
-    for u in $(cat $aptlist | awk '{ print $1 }' | sed "s/'//g"); do
-        echo "downloading: $u" >>$APT_LOG
-        dltmp=$(mktemp -d mkg.XXXXXXXXXX)
-        wget --progress=dot -P "$dltmp" "$u" >>$APT_LOG 2>&1
-        if [ $? -eq 0 ]; then
-            mv $dltmp/* /var/cache/apt/archives
-            echo "DOWNLOAD SUCCEEDED: $u" >>$APT_LOG
-        else
-            echo "DOWNLOAD FAILED: $u" >>$APT_LOG
-        fi
-        rm -r $dltmp
-    done
-}
-
-while getopts k: f; do
-  case $f in
-      k) stamp=$OPTARG
-  esac
-done
-shift $(expr $OPTIND - 1)
-
-if [ $# -lt 1 ] ; then
-    printusage
-fi
-
-if [ -z $prefix ] ; then
-    # invoked in real environment
-    if [ "$1" = "installed" ] ; then
-        my_dpkg_query '*' 2>/dev/null | awk '{ if ($2) printf("%s %s\n",$1,$2) }'
-        #RET=$? return code ignored
-    elif [ "$1" = "available" ] ; then
-        pkgs=$(apt-cache search '.' 2>/dev/null | cut -d ' ' -f 1)
-        for pkg in $pkgs; do
-            apt-cache show --no-all-versions $pkg 2>/dev/null
-            #RET=$? return code ignored
-        done
-    elif [ "$1" = "install" ] ; then
-        aptlog "start $stamp"
-
-        aptlog "downloading: \"$2\""
-
-        # XXX the way i create mackage name is wrong:
-        aptlist=$(mktemp -t mkg.XXXXXXXXXX)
-        apt-get install $INST_OPTS --print-uris "$2" 2>/dev/null \
-            | awk " /^'http:\\/\\// { print \$0 } " >$aptlist
-        download $aptlist
-        rm $aptlist
-
-        aptlog "installing: \"$2\""
-        # XXX the way i create mackage name is wrong:
-        apt-get install $INST_OPTS "$2" >>$APT_LOG 2>&1
-        RET=$?
-
-        aptlog "done $stamp"
-
-    elif [ "$1" = "update" ] ; then
-        aptlog "start $tstamp" "apt-get update $UPDT_OPTS"
-        apt-get update $UPDT_OPTS >>$APT_LOG 2>&1
-        RET=$?
-        aptlog "done $stamp"
-    elif [ "$1" = "upgrade" ] ; then
-        aptlog "start $stamp"  "apt-get upgrade $UPGD_OPTS"
-
-        aptlist=$(mktemp -t mkg.XXXXXXXXXX)
-        apt-get dist-upgrade $UPGD_OPTS --print-uris 2>/dev/null \
-            | awk " /^'http:\\/\\// { print \$0 } " >$aptlist
-        download $aptlist
-        rm $aptlist
-
-        apt-get dist-upgrade $UPGD_OPTS >>$APT_LOG 2>&1
-        RET=$?
-
-        aptlog "done $stamp"
-    elif [ "$1" = "remove" ] ; then
-        aptlog "start $stamp" "apt-get remove $REMO_OPTS \"$2\""
-        apt-get remove $REMO_OPTS "$2" >>$APT_LOG 2>&1
-        apt-get clean >>/dev/null 2>&1
-        RET=$?
-        aptlog "done $stamp"
-    fi
+case ARGV.shift
+when 'installed'
+  mkg.installed()
+when 'available'
+  mkg.available()
+when 'install'
+  if ARGV.empty?
+    printusage()
+  else
+    mkg.install(ARGV.shift)
+  end
+when 'update'
+  mkg.update()
+when 'upgrade'
+  mkg.upgrade()
+when 'remove'
+  if ARGV.empty?
+    printusage()
+  else
+    mkg.remove(ARGV.shift)
+  end
 else
-    # invoked in development environment
-    if [ "$1" = "installed" ] ; then
-        cat $pkglist* | awk ' /^Package: / { print $2 " 10.0" } '
-    elif [ "$1" = "install" ] ; then
-        aptlog "start $stamp"
-        echo "END PACKAGE LIST" >>$APT_LOG
-        aptlog "done $stamp"
-    elif [ "$1" = "update" ] ; then
-        : # XXX
-    elif [ "$1" = "upgrade" ] ; then
-        aptlog "start $stamp"
-        echo "END PACKAGE LIST" >>$APT_LOG
-        aptlog "done $stamp"
-    elif [ "$1" = "remove" ] ; then
-        echo "remove not supported in build environment"
-    elif [ "$1" = "available" ] ; then
-        cat $pkglist* | sed -e 's/^XB-//' \
-            | awk '{ print $0 }; /^Package: / { print "Version: 10.0" }'
-    fi
-fi
 
 exit $RET
