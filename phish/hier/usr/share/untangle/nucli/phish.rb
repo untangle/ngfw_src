555119b26440a6d662a4a6f60f39a98be8c3bb26
Catalin Matei <cmatei@untangle.com>
2007-11-23 16:33:47 +0000

    Phish Blocker FN
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@13033 8ebf8b4e-06f3-0310-961f-89f74618be54

--- phish/hier/usr/share/untangle/nucli/phish.rb
+++ phish/hier/usr/share/untangle/nucli/phish.rb
@@ -16,12 +16,26 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 require 'filternode'
+include_class(['com.untangle.node.spam.SpamMessageAction',
+               'com.untangle.node.spam.SMTPSpamMessageAction'])
 
 class Phish < UVMFilterNode
 
   UVM_NODE_NAME = "untangle-node-phish"
   NODE_NAME = "Phish Blocker"
   MIB_ROOT = UVM_FILTERNODE_MIB_ROOT + ".6"
+
+  @@spamActionValues = {
+    "mark" => SpamMessageAction::MARK,
+    "pass" => SpamMessageAction::PASS
+  }
+
+  @@smtpSpamActionValues = {
+    "mark" => SMTPSpamMessageAction::MARK,
+    "pass" => SMTPSpamMessageAction::PASS,
+    "block" =>  SMTPSpamMessageAction::BLOCK,
+    "quarantine" => SMTPSpamMessageAction::QUARANTINE
+  }
 
   def initialize
     @diag = Diag.new(DEFAULT_DIAG_LEVEL)
@@ -47,9 +61,120 @@ class Phish < UVMFilterNode
   end  
   
   def get_help_text()
-    return <<-HELP
--- To be implemented.
-    HELP
+    return <<HELP
+- phish -- enumerate all phish blocker nodes running on effective #{BRAND} server.
+- phish <#X|TID> web
+    -- Display web anti-phishing protection options for node #X|TID.
+- phish <#X|TID> web [enable:true|false]
+    -- Set web anti-phishing protection options for node #X|TID.
+- phish <#X|TID> [protocol:SMTP|POP|IMAP]
+    -- Display email anti-phishing protection options for node #X|TID and the specified protocol.
+- phish <#X|TID> [protocol:SMTP|POP|IMAP] update [key:scan|action|description] new_value
+    -- Update email anti-phishing protection options with specified settings:
+      scan:true|false
+      action:mark|pass|block|quarantine for SMTP and mark|pass for POP and IMAP
+      description:any string
+- phish <#X|TID> <snmp|stats>
+    -- Display phish blocker #X|TID statistics
+HELP
   end
 
+  #
+  # Command handlers.
+  #
+
+  def cmd_SMTP(tid)
+    display_settings(tid, "SMTP")
+  end
+
+  def cmd_POP(tid)
+    display_settings(tid, "POP")
+  end
+  
+  def cmd_IMAP(tid)
+    display_settings(tid, "IMAP")
+  end
+  
+  def cmd_SMTP_update(tid, key, value)
+    update_protocol_setting(tid, "SMTP", key, value)
+  end
+
+  def cmd_POP_update(tid, key, value)
+    update_protocol_setting(tid, "POP", key, value)
+  end
+  
+  def cmd_IMAP_update(tid, key, value)
+    update_protocol_setting(tid, "IMAP", key, value)
+  end
+  
+  def display_settings(tid, protocol)
+    config = get_protocol_config(tid, protocol)
+    ret = "scan,action,description\n"
+    ret << "#{config.getScan.to_s}"
+    ret << ",#{config.getMsgAction}"
+    ret << ",#{config.getNotes}"
+    ret << "\n"
+    return ret
+  end
+  
+  def update_protocol_setting(tid, protocol, key, new_value)
+    config = get_protocol_config(tid, protocol)
+    begin
+      case key
+      when "scan"
+        validate_bool(new_value, "scan")
+        config.setScan(new_value == "true")
+      when "action"
+        actionValues = protocol == "SMTP" ? @@smtpSpamActionValues : @@spamActionValues
+        validate_list(new_value, actionValues.keys ,"action")
+        config.setMsgAction(actionValues[new_value])
+      when "description"
+        config.setNotes(new_value)
+      else
+        raise "Error: invalid key for '#{protocol}'"
+      end      
+    rescue => ex
+      return ex.to_s
+    end
+    update_phish_settings(tid, protocol, config)
+    msg = "#{key} for #{protocol} updated."
+
+    @diag.if_level(2) { puts! msg }
+    return msg
+  end
+  
+
+  #-- Helper methods
+  def get_phish_settings(tid)
+    @@uvmRemoteContext.nodeManager.nodeContext(tid).node.getPhishSettings
+  end
+  
+  def get_protocol_config(tid, protocol)
+    settings = get_phish_settings(tid)
+    case protocol
+    when "SMTP"
+      settings.getSmtpConfig
+    when "POP"
+      settings.getPopConfig
+    when "IMAP"
+      settings.getImapConfig
+    end
+  end
+  
+  def update_phish_settings(tid, protocol, config)
+    node = @@uvmRemoteContext.nodeManager.nodeContext(tid).node
+    settings = node.getPhishSettings
+
+    case protocol
+    when "SMTP"
+      settings.setSmtpConfig(config)
+    when "POP"
+      settings.setPopConfig(config)
+    when "IMAP"
+      settings.setImapConfig(config)
+    end
+    
+    node.setPhishSettings(settings)
+  end
+  
 end
