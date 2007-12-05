1bf6c7d6562ffb5a9e530c9af809606be931e913
Aaron Read <amread@untangle.com>
2007-12-05 01:28:46 +0000

    hippies
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@13405 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm-lib/servlets/alpaca/src/com/untangle/uvm/servlet/alpaca/RawPostMethod.java
+++ uvm-lib/servlets/alpaca/src/com/untangle/uvm/servlet/alpaca/RawPostMethod.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-package com.untangle.uvm.servlet.store;
+package com.untangle.uvm.servlet.alpaca;
 
 import java.io.IOException;
 import java.io.InputStream;
