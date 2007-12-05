166c4d59fe0610facf7654a33f73eddfdad1175f
Aaron Read <amread@untangle.com>
2007-12-05 01:12:49 +0000

    hippies
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@13403 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm-lib/package.rb
+++ uvm-lib/package.rb
@@ -55,8 +55,8 @@ ServletBuilder.new(uvm_lib, 'com.untangle.uvm.store.jsp',
 ServletBuilder.new(uvm_lib, 'com.untangle.uvm.reports.jsp',
                    "./uvm-lib/servlets/reports")
 
-ServletBuilder.new(uvm_lib, 'com.untangle.uvm.alpaca.jsp',
-                   "./uvm-lib/servlets/alpaca")
+#ServletBuilder.new(uvm_lib, 'com.untangle.uvm.alpaca.jsp',
+#                   "./uvm-lib/servlets/alpaca")
 
 # wmi installer
 ServletBuilder.new(uvm_lib, "com.untangle.uvm.user.servlet",
