d9f68cfa4f82adba03d6e5c48f3a1793e522da9b
Aaron Read <amread@untangle.com>
2007-12-05 02:07:25 +0000

    worky
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@13407 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm-lib/servlets/alpaca/src/com/untangle/uvm/servlet/alpaca/ProxyServlet.java
+++ uvm-lib/servlets/alpaca/src/com/untangle/uvm/servlet/alpaca/ProxyServlet.java
@@ -55,40 +55,19 @@ import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.log4j.Logger;
 
 /**
- * A reverse proxy that makes our online store appear to be hosted on
- * the Untangle appliance. Implemented by replacing all strings with a
- * base URL with the base URL of the appliance.
+ * A reverse proxy for the alpaca.
  *
- * XXX we should reuse WebProxy from the Portal
+ * XXX in the future, put apache in front, and redirect from there.
  *
  * @author <a href="mailto:amread@untangle.com">Aaron Read</a>
  * @version 1.0
  */
 public class ProxyServlet extends HttpServlet
 {
-    private static final String STORE_HOST;
-    private static final String COOKIE_DOMAIN;
-    private static final String URI_BASE;
     private static final String BASE_URL;
-    private static final Map<Character, Integer> OCCURANCE;
-
-    private static final String INST_COOKIE = "instCookie";
 
     static {
-        String s = System.getProperty("uvm.store.host");
-        STORE_HOST = null == s ? "library.untangle.com" : s;
-        COOKIE_DOMAIN = STORE_HOST;
-        s = System.getProperty("uvm.store.uri");
-        URI_BASE = null == s ? "" : s;
-        BASE_URL = "https://" + STORE_HOST + URI_BASE;
-
-        Map<Character, Integer> m = new HashMap<Character, Integer>();
-
-        for (int i = 0; i < BASE_URL.length(); i++) {
-            m.put(BASE_URL.charAt(i), i);
-        }
-
-        OCCURANCE = Collections.unmodifiableMap(m);
+        BASE_URL = "http://localhost:3000/alpaca";
     }
 
     private final HttpClientCache clientCache;
@@ -98,7 +77,7 @@ public class ProxyServlet extends HttpServlet
 
     public ProxyServlet()
     {
-        clientCache = new HttpClientCache(COOKIE_DOMAIN);
+        clientCache = new HttpClientCache("localhost");
     }
 
     // HttpServlet methods ----------------------------------------------------
@@ -140,33 +119,6 @@ public class ProxyServlet extends HttpServlet
         HttpClient httpClient = clientCache.getClient(req, resp);
         HttpState state = httpClient.getState();
 
-        RemoteToolboxManager tool = ctx.toolboxManager();
-        tool.installed();
-        StringBuilder sb = new StringBuilder();
-        for (MackageDesc md : tool.installed()) {
-            MackageDesc.Type type = md.getType();
-            /* ignore packages that don't have an untangle type */
-            if (null == type || MackageDesc.Type.UNKNOWN == type) {
-                continue;
-            }
-            sb.append(md.getName());
-            sb.append("=");
-            sb.append(md.getInstalledVersion());
-            sb.append(",");
-        }
-        if (sb.length() > 0) {
-            sb.deleteCharAt(sb.length() - 1);
-        }
-
-        Cookie instCookie = (Cookie)s.getAttribute(INST_COOKIE);
-        if (null == instCookie) {
-            instCookie = new Cookie(COOKIE_DOMAIN, "installed", sb.toString(), "/", -1, false);
-            state.addCookie(instCookie);
-            s.setAttribute(INST_COOKIE, instCookie);
-        } else {
-            instCookie.setValue(sb.toString());
-        }
-
         InputStream is = null;
         OutputStream os = null;
 
@@ -201,11 +153,7 @@ public class ProxyServlet extends HttpServlet
             }
 
             os = resp.getOutputStream();
-            if (rewriteStream) {
-                rewriteStream(is, os);
-            } else {
-                copyStream(is, os);
-            }
+            copyStream(is, os);
 
         } catch (UnknownHostException exn) {
             logger.warn("unknown host", exn);
@@ -263,67 +211,6 @@ public class ProxyServlet extends HttpServlet
         os.flush();
     }
 
-    private int rewriteStream(InputStream is, OutputStream os)
-        throws IOException
-    {
-        int count = 0;
-
-        // XXX charset!
-        BufferedReader r = new BufferedReader(new InputStreamReader(is));
-        BufferedWriter w = new BufferedWriter(new OutputStreamWriter(os));
-
-        char[] buf = new char[BASE_URL.length()];
-
-        int offset = 0;
-        int i = 0;
-
-        while (true) {
-            for ( ; i < buf.length; i++) {
-                int c = r.read();
-                if (0 > c) {
-                    for (int m = 0; m < i; m++) {
-                        w.write(buf[(offset + m) % buf.length]);
-                    }
-                    w.flush();
-                    return count;
-                } else {
-                    buf[(offset + i) % buf.length] = (char)c;
-                }
-            }
-
-            int j;
-            for (j = buf.length - 1; 0 <= j; j--) {
-                if (buf[(offset + j) % buf.length] != BASE_URL.charAt(j)) {
-                    break;
-                }
-            }
-
-            if (-1 == j) {
-                count++;
-                w.append("."); // XXX not correct in general
-                i = 0;
-            } else {
-                Integer k = OCCURANCE.get(buf[(offset + j) % buf.length]);
-
-                if (null == k) {
-                    for (int m = 0; m < j + 1; m++) {
-                        w.write(buf[(offset + m) % buf.length]);
-                    }
-                    offset = (offset + (j + 1)) % buf.length;
-                    i = buf.length - (j + 1);
-                } else {
-                    int l = Math.max(j - k, 1);
-
-                    for (int m = 0; m < l; m++) {
-                        w.write(buf[(offset + m) % buf.length]);
-                    }
-                    offset = (offset + l) % buf.length;
-                    i = buf.length - l;
-                }
-            }
-        }
-    }
-
     private void copyHeaders(HttpServletRequest req, HttpMethod method)
     {
         for (Enumeration e = req.getHeaderNames(); e.hasMoreElements(); ) {
@@ -333,7 +220,7 @@ public class ProxyServlet extends HttpServlet
                 || k.equalsIgnoreCase("cookie")) {
                 // skip
             } else if (k.equalsIgnoreCase("host")) {
-                method.addRequestHeader("Host", STORE_HOST);
+                method.addRequestHeader("Host", "localhost");
             } else if (k.equalsIgnoreCase("referer")) {
                 method.addRequestHeader("Referer", BASE_URL);
             } else {
@@ -365,48 +252,5 @@ public class ProxyServlet extends HttpServlet
                 resp.setHeader(name, value);
             }
         }
-    }
-
-    // test -------------------------------------------------------------------
-
-    public static void main(String[] args) throws Exception
-    {
-        final int numTimes = 10000;
-
-        final PipedOutputStream os = new PipedOutputStream();
-        PipedInputStream is = new PipedInputStream(os);
-
-        Runnable writer = new Runnable()
-            {
-                public void run()
-                {
-                    try {
-                        Random r = new Random();
-                        for (int i = 0; i < numTimes; i++) {
-                            int l = r.nextInt() % 100;
-                            for (int j = 0; j < l; j++) {
-                                os.write((byte)r.nextInt() % 256);
-                            }
-
-                            os.write(BASE_URL.toString().getBytes());
-                        }
-
-                        os.close();
-                    } catch (Exception exn) {
-                        exn.printStackTrace();
-                    }
-                }
-            };
-
-        FileOutputStream fos = new FileOutputStream("/dev/null");
-
-
-        Thread t = new Thread(writer);
-        t.start();
-
-        ProxyServlet ps = new ProxyServlet();
-        int i = ps.rewriteStream(is, fos);
-
-        System.out.println("numTimes: " + numTimes + " I: " + i);
     }
 }
