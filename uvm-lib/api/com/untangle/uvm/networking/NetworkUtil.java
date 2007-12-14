5b551e84cfa6dfc97130625a47b7e1182e53c309
Ken Hilton <ken@untangle.com>
2007-12-14 22:35:07 +0000

    Revised example host name to one that should be less confusing to users, as per BZ 1862.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@13739 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm-lib/api/com/untangle/uvm/networking/NetworkUtil.java
+++ uvm-lib/api/com/untangle/uvm/networking/NetworkUtil.java
@@ -501,7 +501,7 @@ public class NetworkUtil
 
         try {
             h = HostName.parse( "local.domain" );
-            l = HostName.parse( "untangle.local.domain" );
+            l = HostName.parse( "untangle.example.com" );
         } catch ( ParseException e ) {
             /* This should never happen */
             System.err.println( "Unable to initialize LOCAL_DOMAIN_DEFAULT: " + e );
