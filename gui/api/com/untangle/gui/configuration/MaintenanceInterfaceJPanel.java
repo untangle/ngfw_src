28b9abfd10885c0cce6cf21f25306bd3389cf165
Aaron Read <amread@untangle.com>
2007-12-20 22:06:23 +0000

    remove ping
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@13913 8ebf8b4e-06f3-0310-961f-89f74618be54

--- gui/api/com/untangle/gui/configuration/MaintenanceInterfaceJPanel.java
+++ gui/api/com/untangle/gui/configuration/MaintenanceInterfaceJPanel.java
@@ -1,6 +1,6 @@
 /*
  * $HeadURL$
- * Copyright (c) 2003-2007 Untangle, Inc. 
+ * Copyright (c) 2003-2007 Untangle, Inc.
  *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2,
@@ -79,7 +79,6 @@ public class MaintenanceInterfaceJPanel extends MEditTableJPanel{
         private static final int  C2_MW = 120;  /* network interface */
         private static final int  C3_MW = 230;  /* connection */
         private static final int  C4_MW = 175;  /* mode */
-        private static final int  C5_MW = 65;   /* block ping */
 
 
 
@@ -90,11 +89,10 @@ public class MaintenanceInterfaceJPanel extends MEditTableJPanel{
             addTableColumn( tableColumnModel,  0,  C0_MW, false, false, true, false, String.class,  null, sc.TITLE_STATUS );
             addTableColumn( tableColumnModel,  1,  C1_MW, false, false, true,  false, Integer.class, null, sc.TITLE_INDEX );
             addTableColumn( tableColumnModel,  2,  C2_MW, false, false, false, false, String.class, null, sc.html("network<br>interface") );
-            addTableColumn( tableColumnModel,  3,  C5_MW, false, true,  false, false, Boolean.class, null, sc.html("block<br>ping") );
-            addTableColumn( tableColumnModel,  4,  C3_MW, false, false, false, false, String.class, null, sc.html("connection") );
-            addTableColumn( tableColumnModel,  5,  C4_MW, false, true,  false, false, ComboBoxModel.class, null, sc.html("mode") );
+            addTableColumn( tableColumnModel,  3,  C3_MW, false, false, false, false, String.class, null, sc.html("connection") );
+            addTableColumn( tableColumnModel,  4,  C4_MW, false, true,  false, false, ComboBoxModel.class, null, sc.html("mode") );
 
-            addTableColumn( tableColumnModel,  6,  10,    false, false, true,  false, Interface.class, null, "");
+            addTableColumn( tableColumnModel,  5,  10,    false, false, true,  false, Interface.class, null, "");
             return tableColumnModel;
         }
 
@@ -107,9 +105,8 @@ public class MaintenanceInterfaceJPanel extends MEditTableJPanel{
 
             for( Vector rowVector : tableVector ){
                 rowIndex++;
-                newElem = (Interface) rowVector.elementAt(6);
-                newElem.setIsPingable( !(Boolean) rowVector.elementAt(3) );
-                newElem.setEthernetMedia( (EthernetMedia) ((ComboBoxModel)rowVector.elementAt(5)).getSelectedItem() );
+                newElem = (Interface) rowVector.elementAt(5);
+                newElem.setEthernetMedia( (EthernetMedia) ((ComboBoxModel)rowVector.elementAt(4)).getSelectedItem() );
                 elemList.add(newElem);
             }
 
@@ -130,11 +127,10 @@ public class MaintenanceInterfaceJPanel extends MEditTableJPanel{
 
             for( Interface intf : interfaceList ){
                 rowIndex++;
-                tempRow = new Vector(7);
+                tempRow = new Vector(6);
                 tempRow.add( super.ROW_SAVED );
                 tempRow.add( rowIndex );
                 tempRow.add( intf.getName() );
-                tempRow.add( !intf.getIsPingable() );
                 tempRow.add( intf.getConnectionState() + (intf.getConnectionState().equals("connected")?" @ "+intf.getCurrentMedia():"") );
                 tempRow.add( super.generateComboBoxModel( EthernetMedia.getEnumeration(), intf.getEthernetMedia()) );
                 //tempRow.add( intf.getCurrentMedia() );
