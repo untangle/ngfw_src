f0377aa6339986a7433c455833df430ef23f4c57
Aaron Read <amread@untangle.com>
2008-01-25 07:09:05 +0000

    internal/external to trusted
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@14327 8ebf8b4e-06f3-0310-961f-89f74618be54

--- firewall/hier/usr/share/untangle/schema/untangle-node-firewall/settings-convert-6.rb
+++ firewall/hier/usr/share/untangle/schema/untangle-node-firewall/settings-convert-6.rb
@@ -3,8 +3,8 @@ def update_schema()
    'ALTER TABLE settings.n_firewall_rule ADD COLUMN src_intf_matcher text',
    'ALTER TABLE settings.n_firewall_rule ADD COLUMN dst_intf_matcher text',
    "UPDATE settings.n_firewall_rule SET live = false, src_intf_matcher = 'any', dst_intf_matcher = 'any' WHERE inbound = false AND outbound = false",
-   "UPDATE settings.n_firewall_rule SET src_intf_matcher = 'any', dst_intf_matcher = 'more_external' WHERE inbound = false AND outbound = true",
-   "UPDATE settings.n_firewall_rule SET src_intf_matcher = 'more_internal', dst_intf_matcher = 'any' WHERE inbound = true AND outbound = false",
+   "UPDATE settings.n_firewall_rule SET src_intf_matcher = 'any', dst_intf_matcher = 'less_trusted' WHERE inbound = false AND outbound = true",
+   "UPDATE settings.n_firewall_rule SET src_intf_matcher = 'more_trusted', dst_intf_matcher = 'any' WHERE inbound = true AND outbound = false",
    "UPDATE settings.n_firewall_rule SET src_intf_matcher = 'any', dst_intf_matcher = 'any' WHERE inbound = true AND outbound = true",
    'ALTER TABLE settings.n_firewall_rule DROP COLUMN inbound',
    'ALTER TABLE settings.n_firewall_rule DROP COLUMN outbound'
