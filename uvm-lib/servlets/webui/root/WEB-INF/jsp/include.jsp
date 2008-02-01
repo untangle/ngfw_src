f6f01b8c1228053e0fc38a47122c531171cdcbd5
Catalin Matei <cmatei@untangle.com>
2008-02-01 12:16:24 +0000

    Prototype#3 - add protofilter event log functionality and blingers
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/web-ui/work/src@14410 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm-lib/servlets/webui/root/WEB-INF/jsp/include.jsp
+++
@@ -1,5 +0,0 @@
-<%@ page session="false"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
-<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
-<%@ taglib prefix="uvm" uri="http://www.untangle.com/tags" %>
--- uvm-lib/servlets/webui/root/requests/test.js
+++
@@ -1,7 +0,0 @@
-{
- 'data':[ 
-	  {'category':'Streaming Video','protocol':'RTSP tunneled within HTTP','block':false,'log':false,'description':'RTSP tunneled within HTTP','signature':'^(get[\x09-\x0d -~]* Accept: application/x-rtsp-tunnelled|http/(0\.9|1\.0|1\.1) [1-5][0-9][0-9] [\x09-\x0d -~]*a=control:rtsp://)'},
-	  {'category':'Streaming Video','protocol':'Quicktime HTTP','block':false,'log':false,'description':'Quicktime HTTP','signature':'user-agent: quicktime \(qtver=[0-9].[0-9].[0-9];os=[\x09-\x0d -~]+\)\x0d\x0a'}
-	]
-}
-	
