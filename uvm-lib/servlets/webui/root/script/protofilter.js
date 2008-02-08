9f355249a8c03b8d3b0f653b2e7b733a801b1db0
Vald Dumitrescu <vdumitrescu@untangle.com>
2008-02-08 10:00:46 +0000

    start protofilter with jsonrpc
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/web-ui/work/src@14558 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm-lib/servlets/webui/root/script/protofilter.js
+++ uvm-lib/servlets/webui/root/script/protofilter.js
@@ -106,6 +106,13 @@ Ext.untangle.ProtocolControlSettings = Ext.extend(Ext.untangle.Settings, {
 	    container.dom.insertBefore(el, position);
         this.el = Ext.get(el);
     	
+    	var a = rpc.nodeManager.nodeInstances('untangle-node-protofilter')
+		var nc = rpc.nodeManager.nodeContext(a.list[0])
+		var n = nc.node()
+		var s = n.getProtoFilterSettings()
+		var pl = s.patterns.list.length;
+    	
+    	
 	    // create the data store
 	    this.storePL = new Ext.data.JsonStore({
 	        url: Ext.untangle.ProtocolControlSettings.nodeUrl,
