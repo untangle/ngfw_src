082364c9ead38a655345f48f5241da073ccf1cd6
Sebastien Delafond <sdelafond@gmail.com>
2008-02-08 00:54:31 +0000

    Give up on using return codes; will parse stdout instead (Closes: #3640).
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@14549 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/share/untangle/bin/banner-nanny.rb
+++ uvm/hier/usr/share/untangle/bin/banner-nanny.rb
@@ -3,25 +3,59 @@
 require 'socket'
 require 'timeout'
 
-BYTES_TO_READ = 3
+BYTES_TO_READ = 1
 port, timeout = ARGV
+failures      = 0
+f             = open("/var/log/uvm/banner-nanny.log", "a")
+logMsg        = ""
+s             = nil
+rc            = 0
 
-failures = 0
+logMsg += Time.now.strftime("%Y %m %d %H:%M:%S")
+logMsg += " | port=#{port} | "
 
 begin
-  Timeout::timeout(timeout.to_i) {
-    s = TCPSocket.new('localhost', port.to_i)
-    s.puts "PING SPAMC/1.2"
-    s.read(BYTES_TO_READ)
-  }
-  exit 0
-rescue Timeout::Error
-  failures += 1
-  if failures == 2 then
-    exit 1
-  else
-    retry
+  begin
+    Timeout::timeout(timeout.to_i) {
+      s = TCPSocket.new('localhost', port.to_i)
+      s.puts "PING SPAMC/1.2"
+      s.read(BYTES_TO_READ)
+    }
+    rc = 0
+  rescue Timeout::Error, Errno::ECONNRESET => e
+    failures += 1
+    logMsg += "failure ##{failures} (e.class) | "
+    if failures == 3 then
+      logMsg += "final-failure(3 times) | "
+      rc = 1
+    else
+      s.close()
+      logMsg += "retrying | "
+      retry
+    end
+  rescue Errno::ECONNREFUSED
+    logMsg += "final-failure(ECONNREFUSED) | "
+    rc = 0 # the 1st-gen nannies will handle this
+  ensure
+    logMsg += "in 1st ensure clause | "
+    s.close()
   end
-rescue Errno::ECONNREFUSED
-  exit 0 # the 1st-gen nannies will handle this
+rescue # do nothing
+  logMsg "in main rescue | "
+ensure
+  begin
+    logMsg += "about to exit with rc=#{rc}\n"
+    if rc != 0 then
+      puts "failure"
+      f.write(logMsg)
+      f.flush()
+    else
+      puts "success"
+    end
+    Process.exit!(rc)
+  rescue Exception => e
+    f.write(Time.now.strftime("%Y %m %d %H:%M:%S") + " | whut! | #{e.class} | #{e.message} | #{e.backtrace.inspect}\n")
+    f.flush()
+    Process.exit!(rc)
+  end
 end
