140b100370326e09ff425c304d7286fda8ed3b17
Aaron Read <amread@untangle.com>
2008-03-13 22:37:43 +0000

    updated conversion to 5.0 naming
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@15220 8ebf8b4e-06f3-0310-961f-89f74618be54

--- http-casing/hier/usr/share/untangle/schema/untangle-casing-http/events-convert-9.sql
+++ http-casing/hier/usr/share/untangle/schema/untangle-casing-http/events-convert-9.sql
@@ -17,19 +17,19 @@
 
 -- events convert for release-5.2
 
-DROP TABLE events.new_tr_http_req_line;
+DROP TABLE events.new_n_http_req_line;
 
-CREATE TABLE events.new_tr_http_req_line (
+CREATE TABLE events.new_n_http_req_line (
     request_id int8 NOT NULL,
     pl_endp_id int8,
     method char(1),
     uri text,
     time_stamp timestamp);
 
-INSERT INTO events.new_tr_http_req_line
-  SELECT line.*, reqevt.time_stamp FROM tr_http_req_line line
-  INNER JOIN tr_http_evt_req reqevt ON (reqevt.request_id = line.request_id);
+INSERT INTO events.new_n_http_req_line
+  SELECT line.request_id, line.pl_endp_id, line.method, line.uri, reqevt.time_stamp FROM n_http_req_line line
+  INNER JOIN n_http_evt_req reqevt ON (reqevt.request_id = line.request_id);
 
-DROP TABLE events.tr_http_req_line;
-ALTER TABLE events.new_tr_http_req_line RENAME TO tr_http_req_line;
-ALTER TABLE events.tr_http_req_line ADD PRIMARY KEY (request_id);
+DROP TABLE events.n_http_req_line;
+ALTER TABLE events.new_n_http_req_line RENAME TO n_http_req_line;
+ALTER TABLE events.n_http_req_line ADD PRIMARY KEY (request_id);
