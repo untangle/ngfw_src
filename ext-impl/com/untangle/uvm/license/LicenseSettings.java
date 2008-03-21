8426aa7378627c50461d6bc2d784148cfe7bbed9
Aaron Read <amread@untangle.com>
2008-03-21 22:26:00 +0000

    merge -r15370:15405 svn://chef/hades
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/web-ui/hades/rup@15408 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ ext-impl/com/untangle/uvm/license/LicenseSettings.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2003-2007 Untangle, Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Untangle, Inc. ("Confidential Information"). You shall
+ * not disclose such Confidential Information.
+ *
+ * $Id$
+ */
+
+package com.untangle.uvm.license;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+class LicenseSettings
+{
+    private final List<License> licenses;
+    
+    LicenseSettings( Collection<License> licenses )
+    {
+        this.licenses = Collections.unmodifiableList( new ArrayList( licenses ));
+    }
+
+    /**
+     * This is the current set of product licenses.  Perhap, this
+     * should be a map of product names to the license?  Actually, the
+     * same product may have multiple licenses, why delete the trial
+     * license?  that will prevent it from getting trials twice.
+     *
+     * @return The product licenses.
+     */
+    public List<License> getLicenses()
+    {
+        return this.licenses;
+    }
+}
