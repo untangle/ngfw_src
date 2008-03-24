6abbaf22374fc40f9bd7cb6269dd5bb83cf583de
Robert Scott <rbscott@untangle.com>
2008-03-24 01:28:54 +0000

    fixing a few postinsts.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/rup@15415 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ gui/com/untangle/node/license/gui/EmptyGui.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2003-2007 Untangle, Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Untangle, Inc. ("Confidential Information"). You shall
+ * not disclose such Confidential Information.
+ *
+ * $Id$
+ */
+
+package com.untangle.node.license.gui;
+
+/* just an empty class so the license manager gui jar is created for
+ * the ProfessionalLicense.txt file. */
+class EmptyGui
+{
+}
