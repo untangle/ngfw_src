56c8ec800068ff85fd40c4044de38410de8fc5f6
Vald Dumitrescu <vdumitrescu@untangle.com>
2008-04-01 12:03:44 +0000

    hai
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/web-ui/work/src@15613 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm-lib/servlets/webui/root/script/i18n.js
+++ uvm-lib/servlets/webui/root/script/i18n.js
@@ -1,8 +1,11 @@
+// instances map of i18n objects for nodes 
 Ung.i18nNodeInstances={};
 
+//I18N object class
 Ung.I18N=Ext.extend(Ext.Component, {
+	//map of translations
 	map: null,
-	
+    //initialize I18N component
     initComponent: function(){
         Ung.I18N.superclass.initComponent.call(this);
 		if (this.map == null) {
@@ -21,19 +24,19 @@ Ung.I18N=Ext.extend(Ext.Component, {
 			this.map['timestamp_fmt'] = 'Y-m-d g:i:s a';
 		}
     },
-
+	// translation function
 	_: function (s) {
 		if (this.map!== null && this.map[s]) {
 			return this.map[s];
 		}
 		return s;
 	},
-	
+	// pluralize function
 	pluralise: function (s, p, n) {
 		if (n != 1) return this._(p);
 		return this._(s);
 	},
-	
+	// replaces arguments with values, for a string with arguments
 	sprintf: function (s) {
 		var bits = s.split('%');
 		var out = bits[0];
@@ -51,6 +54,7 @@ Ung.I18N=Ext.extend(Ext.Component, {
 		return out;
 	},
 	
+	//formats a number with local separators
     numberFormat: function(v) {
         v = parseInt(v,10) + '';
         var x = v.split(this.map['decimal_sep']);
@@ -62,11 +66,11 @@ Ung.I18N=Ext.extend(Ext.Component, {
         }
         return x1 + x2;
     }, 
-    
+    // formats a date
     dateFormat: function(v) {
     	return Ext.util.Format.date(v, this.map['date_fmt']);
     },
-    
+    //formats a timestamp
     timestampFormat: function(v) {
     	var date = new Date();
     	date.setTime(v.time);
@@ -75,8 +79,12 @@ Ung.I18N=Ext.extend(Ext.Component, {
 
 });
 
+//node I18N class object
 Ung.NodeI18N=Ext.extend(Ung.I18N, {
+	//node map
 	nodeMap: null,
+	//translation function tryes to find the word in the nodeMap
+	// and if not succesful it tries to find it in the global map
 	_: function (s) {
 		if (this.nodeMap!== null && this.nodeMap[s]) {
 			return this.nodeMap[s];
