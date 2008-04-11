0774a820e507ea94149bcc4c14de169de93dace8
Catalin Matei <cmatei@untangle.com>
2008-04-11 05:43:05 +0000

    Attack Blocker UI - in progress
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/web-ui/work/src@15796 8ebf8b4e-06f3-0310-961f-89f74618be54

--- shield/hier/usr/share/untangle/web/webui/script/untangle-node-shield/settings.js
+++ shield/hier/usr/share/untangle/web/webui/script/untangle-node-shield/settings.js
@@ -17,29 +17,31 @@ Ung.Shield = Ext.extend(Ung.Settings, {
     },
     // Exceptions grid
     buildExceptions: function() {
-    	//blocked is a check column
-    	
+    	//enable is a check column
 	    var enableColumn = new Ext.grid.CheckColumn({
 	       header: this.i18n._("enable"), dataIndex: 'live', fixed:true
 	    });
+	    
+		var deviderData = [[5,5+' '+this.i18n._("users")],[25,25+' '+this.i18n._("users")],[40,50+' '+this.i18n._("users")],[75,100+' '+this.i18n._("users")],[-1,this.i18n._("unlimited")]];
 	    
     	this.gridExceptions=new Ung.EditorGrid({
     		settingsCmp: this,
     		//the total records is set from the base settings shieldNodeRulesLength field
     		totalRecords: this.getBaseSettings().shieldNodeRulesLength,
-//    		emptyRow: {"enable":true,"address":"1.2.3.4","blocked":false,"log":false,"description":i18n._("[no description]"),"definition":"[no signature]"},
+//    		emptyRow: {"enable":true,"address":"1.2.3.4","divider":5,"description":i18n._("[no description]")},
     		title: this.i18n._("Exceptions"),
     		//the column is autoexpanded if the grid width permits
     		autoExpandColumn: 'description',
     		recordJavaClass: "com.untangle.node.shield.ShieldNodeRule",
     		//this is the function used by Ung.RpcProxy to retrive data from the server
     		proxyRpcFn: this.getRpcNode().getShieldNodeRules,
+    		
 			//the list of fields
 			fields: [
 	           {name: 'id'},
 	           {name: 'live'},
 	           {name: 'address'},
-	           {name: 'block'},
+	           {name: 'divider'},
 	           //this field is internationalized so a converter was added
 	           {name: 'description', convert: function(v){ return this.i18n._(v)}.createDelegate(this)},
 			],
@@ -49,23 +51,46 @@ Ung.Shield = Ext.extend(Ung.Settings, {
 				enableColumn,
 				{id:'address',header: this.i18n._("address"), width: 140, dataIndex: 'address',
 					//this is a simple text editor
-					editor: new Ext.form.TextField({allowBlank: false})},
-				{id:'block',header: this.i18n._("block"), width: 100, dataIndex: 'block',
-					editor: new Ext.form.TextField({allowBlank: false})},
+					editor: new Ext.form.TextField({
+						allowBlank: false, 
+						validator: function (fieldValue) {
+							return /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(fieldValue);
+						}})
+				},
+				{id:'divider',header: this.i18n._("user")+"<br>"+this.i18n._("count"), width: 100, dataIndex: 'divider',
+		           editor: new Ext.form.ComboBox({
+					    store: new Ext.data.SimpleStore({
+							fields:['dividerValue', 'dividerName'],
+							data: deviderData
+						}),
+						displayField: 'dividerName',
+						valueField: 'dividerValue',
+					    typeAhead: true,
+					    mode: 'local',
+					    triggerAction: 'all',
+					    listClass: 'x-combo-list-small',
+					    selectOnFocus:true}),
+					renderer: function(value) {
+						for (var i=0;i<deviderData.length;i++){
+							if (deviderData[i][0]==value) {
+								return deviderData[i][1];
+							}
+						}
+						return value;
+					}
+				},
 				{id:'description',header: this.i18n._("description"), width: 120, dataIndex: 'description',
 					editor: new Ext.form.TextField({allowBlank: false})}
 			],
-//			sortField: 'id',
-//			columnsDefaultSortable: true,
-//			plugins: [blockedColumn,logColumn],
+			//sortField: 'address',
+			columnsDefaultSortable: true,
 			plugins: [enableColumn],
 			// the row input lines used by the row editor window
 			rowEditorInputLines: [
 				{name:"enable", label: this.i18n._("Enable"), type:"checkbox"},
-				{name:"address", label: this.i18n._("Sddress"), type:"text", style:"width:200px;"},
-				{name:"block", label: this.i18n._("Block"), type:"text", style:"width:200px;"},
+				{name:"address", label: this.i18n._("Address"), type:"text", style:"width:200px;"},
+				{name:"divider", label: this.i18n._("User Count"), type:"text", style:"width:200px;"},
 				{name:"description", label: this.i18n._("Description"), type:"textarea", style:"width:200px;height:60px;"},
-				{name:"definition", label: this.i18n._("Signature"), type:"textarea", style:"width:200px;height:60px;"}
 			]
     	});
     },
@@ -124,7 +149,7 @@ Ung.Shield = Ext.extend(Ung.Settings, {
 			if(exception) {Ext.MessageBox.alert(i18n._("Failed"),exception.message); return;}
 			//exit settings screen
 			this.node.onCancelClick();
-		}.createDelegate(this),this.gridExtensions.getSaveList());
+		}.createDelegate(this),this.gridExceptions.getSaveList());
 	}
 });
 }
