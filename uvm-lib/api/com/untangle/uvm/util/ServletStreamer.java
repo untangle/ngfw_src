49dd4cbf47c34a43cc2cf12d97235d716c0afc0f
Robert Scott <rbscott@untangle.com>
2008-04-22 23:57:46 +0000

    This is the new store, in order to use this from here on out, you must use development-library.untangle.com
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@15900 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ uvm-lib/api/com/untangle/uvm/util/ServletStreamer.java
@@ -0,0 +1,128 @@
+/*
+ * $HeadURL$
+ * Copyright (c) 2003-2007 Untangle, Inc. 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
+ * NONINFRINGEMENT.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.untangle.uvm.util;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+/* Used to stream data to as the response to a servlet request */
+public class ServletStreamer
+{
+    private static final ServletStreamer INSTANCE = new ServletStreamer();
+
+    private final Logger logger = Logger.getLogger( this.getClass());
+
+    private ServletStreamer()
+    {
+    }
+
+    public void stream( HttpServletRequest request, HttpServletResponse response,
+                        String fileName, String downloadFileName, String type )
+        throws ServletException, IOException
+    {
+        stream( request, response, new File( fileName ), downloadFileName, type );
+    }
+
+    public void stream( HttpServletRequest request, HttpServletResponse response,
+                        File file, String downloadFileName, String type )
+        throws ServletException, IOException
+    {
+        try {
+            stream( request, response, new FileInputStream( file ), downloadFileName, type, file.length());
+        } catch ( FileNotFoundException e ) {
+            logger.info( "The file '" + file + "' does not exist" );
+            /* Indicate that the response was not rejected */
+            response.sendError( HttpServletResponse.SC_FORBIDDEN );
+            return;
+        }
+    }
+    
+    /**
+     * Stream a file instead of sending to a client.
+     * @param request the request from the client.
+     * @param response The Response to the client.
+     * @param inputStream the stream to send.
+     * @param downloadFileName if non-null, send this file as an attachment rather than an inline file.
+     * @param type Mime type of the file.
+     * @param length Length of the file, if greater than zero this is set in the header.
+     */
+    public void stream( HttpServletRequest request, HttpServletResponse response,
+                        InputStream inputStream, String downloadFileName, String type, long length )
+        throws ServletException, IOException
+    {
+        response.setContentType( type );
+
+        if ( downloadFileName != null ) {
+            response.setHeader( "Content-Disposition", "attachment;filename=\"" + downloadFileName + "\"" );
+        }
+        if ( length > 0 ) {
+            response.setHeader( "Content-Length", "" + length );
+        }
+
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
+        OutputStream out = null;
+
+        try {
+            out = response.getOutputStream();
+            bis = new BufferedInputStream( inputStream );
+            bos = new BufferedOutputStream( out );
+            byte[] buff = new byte[2048];
+            int bytesRead;
+            while( -1 != ( bytesRead = bis.read( buff, 0, buff.length ))) bos.write( buff, 0, bytesRead );
+        } catch ( Exception e ) {
+            logger.warn( "Error streaming file.", e );
+        } finally {
+            try {
+                if ( bis != null ) bis.close();
+            } catch ( Exception e ) {
+                logger.warn( "Error closing input stream", e );
+            }
+
+            try {
+                if ( bos != null ) bos.close();
+            } catch ( Exception e ) {
+                logger.warn( "Error closing output stream", e );
+            }
+
+            try {
+                if ( out != null ) out.close();
+            } catch ( Exception e ) {
+                logger.warn( "Error closing output stream", e );
+            }
+        }
+    }
+
+    public static ServletStreamer getInstance()
+    {
+        return INSTANCE;
+    }
+}
---
+++ uvm-lib/servlets/library/src/com/untangle/uvm/library/Launcher.java
@@ -0,0 +1,157 @@
+/*
+ * $HeadURL$
+ * Copyright (c) 2003-2007 Untangle, Inc. 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
+ * NONINFRINGEMENT.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.untangle.uvm.library;
+
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import com.untangle.uvm.LocalUvmContext;
+import com.untangle.uvm.LocalUvmContextFactory;
+import com.untangle.uvm.Version;
+
+/* This is used to guarantee that the client is authenticated when
+ * they retrieve the final install image. */
+public class Launcher extends HttpServlet
+{
+    private static final String FIELD_ACTION  = "action";
+    private static final String FIELD_LIBITEM = "libitem";
+    private static final String FIELD_PROTOCOL = "protocol";
+    private static final String FIELD_HOST = "host";
+    private static final String FIELD_PORT = "port";
+
+    private static final String ACTION_MY_ACCOUNT = "my_account";
+    private static final String ACTION_BROWSE = "browse";
+    private static final String ACTION_WIZARD = "wizard";
+
+    private final Logger logger = Logger.getLogger( this.getClass());
+
+    protected void doGet( HttpServletRequest request,  HttpServletResponse response )
+        throws ServletException, IOException
+    {
+        try {
+
+            URL redirect;
+            String action = request.getParameter( FIELD_ACTION );
+            if ( action == null ) action = "";
+            action = action.trim().toLowerCase();
+
+            if ( action.length() == 0 || ACTION_MY_ACCOUNT.equals( action )) {
+                redirect = getMyAccountURL( request );
+            } else if ( ACTION_BROWSE.equals( action )) {
+                String libitem = request.getParameter( FIELD_LIBITEM );
+                if ( libitem == null ) redirect = getMyAccountURL( request );
+                else redirect = getLibraryURL( request, libitem );
+            } else if ( ACTION_WIZARD.equals( action )) {
+                redirect = getWizardURL( request );
+            } else {
+                redirect = getMyAccountURL( request );
+            }
+                
+            response.sendRedirect( redirect.toString());
+        } catch (  MalformedURLException e ) {
+            throw new ServletException( "Error building redirect string.", e );
+        }
+    }
+
+    private URL getMyAccountURL( HttpServletRequest request ) throws MalformedURLException
+    {
+        return getActionURL( request, "my_account", null );
+    }
+
+    private URL getWizardURL( HttpServletRequest request ) throws MalformedURLException
+        
+    {
+        return getActionURL( request, "wizard", null );
+    }
+
+    private URL getLibraryURL( HttpServletRequest request, String mackageName ) throws MalformedURLException
+    {
+        return getActionURL( request, "browse", mackageName );
+    }
+
+    private URL getActionURL( HttpServletRequest request, String action, String mackageName )
+        throws MalformedURLException
+    {
+        LocalUvmContext context = LocalUvmContextFactory.context();
+        String host = context.toolboxManager().getLibraryHost();
+        
+        String query = getLibraryQuery( request, action );
+        if ( mackageName != null ) query += "&name=" + URLEncoder.encode( mackageName );
+        
+        /* Open is a universal redirector which uses action to determine how it should redirect */
+        return new URL( "https" + "://" + host + "/open.php?" + query );
+    }
+
+    private String getLibraryQuery( HttpServletRequest request, String action )
+    {
+        LocalUvmContext context = LocalUvmContextFactory.context();
+
+        String protocol = request.getParameter( FIELD_PROTOCOL );
+        String host = request.getParameter( FIELD_HOST );
+        
+        Integer port = null;
+
+        /* It is better to use the information from the client,
+         * because the client may have been redirected to this server,
+         * and if this happened, the server doesn't know the
+         * information about the original connection the client
+         * made */
+        try {
+            String v = request.getParameter( FIELD_PORT );
+            if ( v != null ) port = Integer.valueOf( v );
+        } catch ( Exception e ) {
+            logger.warn( "Request is missing the host, using the request[" + port + "]" );
+        }
+
+
+        if ( protocol == null ) {            
+            protocol = request.getScheme();
+            logger.warn( "Request is missing the protocol, using the request[" + protocol + "]" );
+        }
+
+        if ( host == null ) {
+            host = request.getServerName();
+            logger.warn( "Request is missing the host, using the request[" + host + "]" );
+        }
+
+        if ( port == null ) {
+            port = request.getServerPort();
+            logger.warn( "Request is missing the host, using the request[ " + port + "]" );
+        }
+
+        String query = "boxkey=" + URLEncoder.encode( context.getActivationKey());
+        query += "&untangleFullVersion=" + URLEncoder.encode( Version.getFullVersion());
+        query += "&host=" + URLEncoder.encode( host );
+        if ( port != null ) query += "&port=" + port;
+        query += "&protocol=" + URLEncoder.encode( protocol );
+        query += "&action=" + URLEncoder.encode( action );
+        
+        return query;
+    }
+}
