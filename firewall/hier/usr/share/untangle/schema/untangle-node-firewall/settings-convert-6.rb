c48a5fc6d0c5c8ddc22d7b9d867a4685edd5c301
Aaron Read <amread@untangle.com>
2008-04-28 20:54:54 +0000

    merge -r16035:16036 svn://chef/branch/prod/burgundy/work/
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/release-5.2/work/src@16038 8ebf8b4e-06f3-0310-961f-89f74618be54

--- firewall/hier/usr/share/untangle/schema/untangle-node-firewall/settings-convert-6.rb
+++ firewall/hier/usr/share/untangle/schema/untangle-node-firewall/settings-convert-6.rb
@@ -4,7 +4,7 @@ def update_schema()
    'ALTER TABLE settings.n_firewall_rule ADD COLUMN dst_intf_matcher text',
    "UPDATE settings.n_firewall_rule SET live = false, src_intf_matcher = 'any', dst_intf_matcher = 'any' WHERE inbound = false AND outbound = false",
    "UPDATE settings.n_firewall_rule SET src_intf_matcher = 'any', dst_intf_matcher = 'less_trusted' WHERE inbound = false AND outbound = true",
-   "UPDATE settings.n_firewall_rule SET src_intf_matcher = 'more_trusted', dst_intf_matcher = 'any' WHERE inbound = true AND outbound = false",
+   "UPDATE settings.n_firewall_rule SET src_intf_matcher = 'any', dst_intf_matcher = 'more_trusted' WHERE inbound = true AND outbound = false",
    "UPDATE settings.n_firewall_rule SET src_intf_matcher = 'any', dst_intf_matcher = 'any' WHERE inbound = true AND outbound = true",
    'ALTER TABLE settings.n_firewall_rule DROP COLUMN inbound',
    'ALTER TABLE settings.n_firewall_rule DROP COLUMN outbound'
