bcb140f43b4ca06ac7115b6194fa7fe910a3a5a3
Vald Dumitrescu <vdumitrescu@untangle.com>
2008-05-06 09:14:20 +0000

    JsonListReader
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/web-ui/work/src@16108 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm-lib/servlets/webui/root/script/components.js
+++ uvm-lib/servlets/webui/root/script/components.js
@@ -2373,3 +2373,31 @@ Ung.EditorGrid = Ext.extend(Ext.grid.EditorGridPanel, {
 
 });
 
+Ung.JsonListReader = Ext.extend(Ext.data.JsonReader, {
+    readRecords : function(o){
+        var sid = this.meta ? this.meta.id : null;
+    	var recordType = this.recordType, fields = recordType.prototype.fields;
+    	var records = [];
+    	this.getRoot = this.meta.root ? this.getJsonAccessor(this.meta.root) : function(p){return p;};
+    	var root = this.getRoot(o)
+	    for(var i = 0; i < root.length; i++){
+		    var n = root[i];
+	        var values = {};
+	        var id = ((sid || sid === 0) && n[sid] !== undefined && n[sid] !== "" ? n[sid] : null);
+	        for(var j = 0, jlen = fields.length; j < jlen; j++){
+                var f = fields.items[j];
+                var k = f.mapping !== undefined && f.mapping !== null ? f.mapping : j;
+                var v = n[k] !== undefined ? n[k] : f.defaultValue;
+                v = f.convert(v, n);
+                values[f.name] = v;
+            }
+	        var record = new recordType(values, id);
+	        record.json = n;
+	        records[records.length] = record;
+	    }
+	    return {
+	        records : records,
+	        totalRecords : records.length
+	    };
+    }
+});
