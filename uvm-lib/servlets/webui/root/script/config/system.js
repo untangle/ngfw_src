95487843d0d366d86b185a100d9810903c867ba9
Vald Dumitrescu <vdumitrescu@untangle.com>
2008-05-21 10:42:12 +0000

    TimeZone done
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/web-ui/work/src@16378 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm-lib/servlets/webui/root/script/config/system.js
+++ uvm-lib/servlets/webui/root/script/config/system.js
@@ -59,6 +59,12 @@ if (!Ung.hasResource["Ung.System"]) {
             }
             return this.rpc.miscSettings;
         },
+        getTimeZone : function(forceReload) {
+            if (forceReload || this.rpc.timeZone === undefined) {
+                this.rpc.timeZone = rpc.adminManager.getTimeZone();
+            }
+            return this.rpc.timeZone;
+        },
         getTODOPanel : function(title) {
             return new Ext.Panel({
                 title : this.i18n._(title),
@@ -116,7 +122,8 @@ if (!Ung.hasResource["Ung.System"]) {
                     }, {
                         xtype : 'checkbox',
                         name : 'isSupportEnabled',
-                        boxLabel : this.i18n._('<b>Send</b> us data about your server. This will send us status updates and an email if any unexpected problems occur, but will not allow us to login to your server. No personal information about your network traffic will be transmitted.'),
+                        boxLabel : this.i18n
+                                ._('<b>Send</b> us data about your server. This will send us status updates and an email if any unexpected problems occur, but will not allow us to login to your server. No personal information about your network traffic will be transmitted.'),
                         hideLabel : true,
                         checked : this.getMiscSettings().isExceptionReportingEnabled,
                         listeners : {
@@ -148,7 +155,77 @@ if (!Ung.hasResource["Ung.System"]) {
                     fields : ['code', 'name']
                 })
             });
-
+            var timeZonesData = [
+                    ["GMT-12:00", "International Dateline West", "Etc/GMT-12"],
+                    ["GMT-11:00", "Midway Island, Samoa", "Pacific/Midway"],
+                    ["GMT-10:00", "Hawaii", "US/Hawaii"],
+                    ["GMT-09:00", "Alaska", "US/Alaska"],
+                    ["GMT-08:00", "Pacific Time (US & Canada), Tijuana", "US/Pacific"],
+                    ["GMT-07:00", "Chihuahua, La Paz, Mazatlan", "America/Chihuahua"],
+                    ["GMT-07:00", "Mountain Time (US & Canada)", "US/Mountain"],
+                    ["GMT-06:00", "Central Time (US & Canada & Central America)", "US/Central"],
+                    ["GMT-06:00", "Guadalajara, Mexico City, Monterrey", "America/Mexico_City"],
+                    ["GMT-06:00", "Saskatchewan", "Canada/Saskatchewan"],
+                    ["GMT-05:00", "Bogota, Lima, Quito", "America/Bogota"],
+                    ["GMT-05:00", "Eastern Time (US & Canada)", "US/Eastern"],
+                    ["GMT-05:00", "Indiana (East)", "US/East-Indiana"],
+                    ["GMT-04:00", "Atlantic Time (Canada)", "Canada/Atlantic"],
+                    ["GMT-04:00", "Caracas, La Paz", "America/Caracas"],
+                    ["GMT-04:00", "Santiago", "America/Santiago"],
+                    ["GMT-03:30", "Newfoundland", "Canada/Newfoundland"],
+                    ["GMT-03:00", "Buenos Aires, Georgetown, Brasilia, Greenland", "America/Buenos_Aires"],
+                    ["GMT-02:00", "Mid-Atlantic", "Etc/GMT-2"],
+                    ["GMT-01:00", "Azores", "Atlantic/Azores"],
+                    ["GMT-01:00", "Cape Verde Is.", "Atlantic/Cape_Verde"],
+                    ["GMT", "Casablanca, Monrovia", "Africa/Casablanca"],
+                    ["GMT", "Greenwich Mean Time : Dublin, Edinburgh, Lisbon, London", "Etc/Greenwich"],
+                    ["GMT+01:00", "Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna", "Europe/Amsterdam"],
+                    ["GMT+01:00", "Belgrade, Bratislava, Budapest, Ljubljana, Prague", "Europe/Belgrade"],
+                    ["GMT+01:00", "Brussels, Copenhagen, Madrid, Paris", "Europe/Brussels"],
+                    ["GMT+01:00", "Sarajevo, Skopje, Warsaw, Zagreb", "Europe/Sarajevo"],
+                    ["GMT+01:00", "West Central Africa", "Etc/GMT+1"],
+                    ["GMT+02:00", "Athens, Beirut, Istanbul, Minsk", "Europe/Athens"],
+                    ["GMT+02:00", "Bucharest", "Europe/Bucharest"],
+                    ["GMT+02:00", "Cairo, Harare, Pretoria", "Africa/Cairo"],
+                    ["GMT+02:00", "Helsinki, Kyiv, Riga, Sofia, Talinn, Vilnius", "Europe/Helsinki"],
+                    ["GMT+02:00", "Jerusalem", "Asia/Jerusalem"],
+                    ["GMT+03:00", "Baghdad", "Asia/Baghdad"],
+                    ["GMT+03:00", "Kuwait, Riyadh", "Asia/Kuwait"],
+                    ["GMT+03:00", "Moscow, St. Petersburg, Volgograd", "Europe/Moscow"],
+                    ["GMT+03:00", "Nairobi", "Africa/Nairobi"],
+                    ["GMT+03:30", "Tehran", "Asia/Tehran"],
+                    ["GMT+04:00", "Abu Dhabi, Muscat", "Asia/Muscat"],
+                    ["GMT+04:00", "Baku, Tbilsi, Yerevan", "Asia/Baku"],
+                    ["GMT+04:30", "Kabul", "Asia/Kabul"],
+                    ["GMT+05:00", "Ekaterinburg, Islamabad, Karachi, Tashkent", "Asia/Karachi"],
+                    ["GMT+05:30", "Chennai, Kolkata, Mumbai, New Delhi", "Asia/Calcutta"], // NOTICE
+                                                                                            // TWO
+                                                                                            // SPELLINGS
+                                                                                            // OF
+                                                                                            // KOLKATA/CALCUTTA
+                    ["GMT+05:45", "Kathmandu", "Asia/Katmandu"], // NOTICE
+                                                                    // TWO
+                                                                    // SPELLINGS
+                                                                    // OF
+                                                                    // KATMANDU/KATHMANDU
+                    ["GMT+06:00", "Almaty, Novosibirsk", "Asia/Almaty"], ["GMT+06:00", "Astana, Dhaka", "Asia/Dhaka"],
+                    ["GMT+06:30", "Rangoon", "Asia/Rangoon"], ["GMT+07:00", "Bangkok, Hanoi, Jakarta", "Asia/Bangkok"],
+                    ["GMT+07:00", "Krasnoyarsk", "Asia/Krasnoyarsk"],
+                    ["GMT+08:00", "Beijing, Chongqing, Hong Kong, Urumqi", "Asia/Hong_Kong"],
+                    ["GMT+08:00", "Irkutsk, Ulaan Bataar", "Asia/Irkutsk"], ["GMT+08:00", "Kuala Lumpur, Singapore", "Asia/Kuala_Lumpur"],
+                    ["GMT+08:00", "Perth", "Australia/Perth"], ["GMT+08:00", "Taipei", "Asia/Taipei"],
+                    ["GMT+09:00", "Osaka, Sapporo, Tokyo", "Asia/Tokyo"], ["GMT+09:00", "Seoul", "Asia/Seoul"],
+                    ["GMT+09:00", "Yakutsk", "Asia/Yakutsk"], ["GMT+09:30", "Adelaide", "Australia/Adelaide"],
+                    ["GMT+09:30", "Darwin", "Australia/Darwin"], ["GMT+10:00", "Brisbane", "Australia/Brisbane"],
+                    ["GMT+10:00", "Canberra, Melbourne, Sydney", "Australia/Canberra"],
+                    ["GMT+10:00", "Guam, Port Moresby", "Pacific/Guam"], ["GMT+10:00", "Hobart", "Australia/Hobart"],
+                    ["GMT+10:00", "Vladivostok", "Asia/Vladivostok"], ["GMT+11:00", "Magadan, Solomon Is., New Caledonia", "Asia/Magadan"],
+                    ["GMT+12:00", "Auckland, Wellington", "Pacific/Auckland"],
+                    ["GMT+12:00", "Fiji, Kamchatka, Marshall Is.", "Pacific/Fiji"]];
+            var timeZones = [];
+            for (var i = 0; i < timeZonesData.length; i++) {
+                timeZones.push([timeZonesData[i][2], "(" + timeZonesData[i][0] + ") " + timeZonesData[i][1]]);
+            }
             this.panelRegionalSettings = new Ext.Panel({
                 // private fields
                 info : 'panelRegionalSettings',
@@ -178,23 +255,18 @@ if (!Ung.hasResource["Ung.System"]) {
                     items : [{
                         xtype : 'combo',
                         name : 'timezone',
-                        // editable : false,
-                        store : [],
-                        fieldLabel : this.i18n._('Timezone'),
+                        editable : false,
+                        store : timeZones,
+                        width : 350,
+                        hideLabel : true,
                         mode : 'local',
                         triggerAction : 'all',
                         listClass : 'x-combo-list-small',
-                        value : rpc.adminManager.getTimeZone(),
+                        value : this.getTimeZone().ID,
                         listeners : {
                             "change" : {
                                 fn : function(elem, newValue) {
-                                    this.getBaseSettings().smtpConfig.msgAction.name = newValue;
-                                    for (var i = 0; i < this.smtpData.length; i++) {
-                                        if (this.smtpData[i][0] == newValue) {
-                                            this.getBaseSettings().smtpConfig.msgAction.key = this.smtpData[i][2]
-                                            break;
-                                        }
-                                    }
+                                    this.getTimeZone().ID = newValue;
                                 }.createDelegate(this)
                             }
                         }
@@ -295,39 +367,40 @@ if (!Ung.hasResource["Ung.System"]) {
         // save function
         saveAction : function() {
             if (this.validate()) {
-                // disable tabs during save
-                // this.tabs.disable();
-            	this.saveSemaphore=2;
+                this.saveSemaphore = 2;
+                // save language settings
                 rpc.languageManager.setLanguageSettings(function(result, exception) {
-                    // re-enable tabs
-                    // this.tabs.enable();
                     if (exception) {
                         Ext.MessageBox.alert(i18n._("Failed"), exception.message);
                         return;
                     }
-                    // exit settings screen
-                    
                     this.afterSave();
-                    
                 }.createDelegate(this), this.getLanguageSettings());
+                
+                // save network settings
                 rpc.networkManager.setSettings(function(result, exception) {
-                    // re-enable tabs
-                    // this.tabs.enable();
                     if (exception) {
                         Ext.MessageBox.alert(i18n._("Failed"), exception.message);
                         return;
                     }
-                    // exit settings screen
                     this.afterSave();
                 }.createDelegate(this), this.getAccessSettings(), this.getMiscSettings());
-
+                
+                //save timezone
+                rpc.adminManager.setTimeZone(function(result, exception) {
+                    if (exception) {
+                        Ext.MessageBox.alert(i18n._("Failed"), exception.message);
+                        return;
+                    }
+                    this.afterSave();
+                }.createDelegate(this), this.getTimeZone());
             }
         },
-        afterSave : function () {
-        	this.saveSemaphore--;
-        	if(this.saveSemaphore==0) {
-        	   this.cancelAction();
-        	}
+        afterSave : function() {
+            this.saveSemaphore--;
+            if (this.saveSemaphore == 0) {
+                this.cancelAction();
+            }
         }
 
     });
