f504158490624648a33f867307a30ad93e2ff6f4
Catalin Matei <cmatei@untangle.com>
2008-05-21 09:57:04 +0000

    Add a TimeZone serializer (json-rpc)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/web-ui/work/src@16377 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm-lib/servlets/webui/src/com/untangle/uvm/webui/jabsorb/UtJsonRpcServlet.java
+++ uvm-lib/servlets/webui/src/com/untangle/uvm/webui/jabsorb/UtJsonRpcServlet.java
@@ -65,6 +65,7 @@ public class UtJsonRpcServlet extends JSONRPCServlet
                 b.registerSerializer(new IPMaddrSerializer());
                 b.registerSerializer(new IPaddrSerializer());
                 b.registerSerializer(new URLSerializer());
+                b.registerSerializer(new TimeZoneSerializer());
                 b.registerSerializer(new MimeTypeSerializer());
 			} catch (Exception e) {
 				e.printStackTrace();
