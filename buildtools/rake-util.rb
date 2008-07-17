01ea5ff44f2cc95827ef9a8b05b12b97f2124d92
Aaron Read <amread@untangle.com>
2008-07-17 22:58:02 +0000

    asdf
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/web-ui/work/src@17530 8ebf8b4e-06f3-0310-961f-89f74618be54

--- buildtools/rake-util.rb
+++ buildtools/rake-util.rb
@@ -99,7 +99,8 @@ class BuildEnv
 
   # XXX XXX should these live here???
   DOWNLOADS = ["#{SRC_HOME}/downloads/output", '/usr/share/java/uvm'].find { |d| File.exist?(d) }
-  puts "DOWNLOADS: #{DOWNLOADS}"
+  K = "FOO"
+  puts "SRC_HOME #{SRC_HOME}, DOWNLOADS: #{BuildEnv::DOWNLOADS} K: #{K}"
   SERVLET_COMMON = "./servlet/common"
 
   attr_reader :home, :prefix, :staging, :devel, :deb, :isDevel, :grabbag, :downloads, :servletcommon, :include, :installTarget
