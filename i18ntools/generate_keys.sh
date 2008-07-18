603b7fae51c267aa72b7812a05c8bc8c14d4d9b4
Catalin Matei <cmatei@untangle.com>
2008-07-18 08:34:21 +0000

    L10N:  * BlockPages - finalize  * Virus
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/web-ui/work/src@17566 8ebf8b4e-06f3-0310-961f-89f74618be54

--- i18ntools/generate_keys.sh
+++ i18ntools/generate_keys.sh
@@ -8,33 +8,61 @@ fi
 case "$1" in 
 "administration"|"system"|"systemInfo")
     cd ../uvm-lib/servlets/webui/po
-    # TODO change charset=utf-8 for pot file
     echo 'get new keys'
-    xgettext --copyright-holder='Untangle, Inc.' -L Python -k.i18n._ -o tmpkeys.pot ../root/script/config/$1.js
-    msgmerge -U ung_$1.pot tmpkeys.pot
-    rm tmpkeys.pot
+    xgettext --copyright-holder='Untangle, Inc.' -L Python -k.i18n._ -o tmp_keys.pot ../root/script/config/$1.js
+    msgmerge -U ung_$1.pot tmp_keys.pot
+    rm tmp_keys.pot
     echo 'update po files'
     msgmerge -U ro/ung_$1.po ung_$1.pot
     ;;
+"mail_casing")
+    cd ../mail-casing/servlets/quarantine/po/
+    xgettext --copyright-holder='Untangle, Inc.' -L Python -ki18n._ -o tmp_keys.pot ../root/quarantine.js
+    xgettext -j --copyright-holder='Untangle, Inc.' -L Python -ki18n._ -o tmp_keys.pot ../root/remaps.js
+    xgettext -j --copyright-holder='Untangle, Inc.' -L Python -ki18n._ -o tmp_keys.pot ../root/request.js
+    xgettext -j --copyright-holder='Untangle, Inc.' -L Python -ki18n._ -o tmp_keys.pot ../root/safelist.js
+    msgcat tmp_keys.pot jspx_keys.pot -o tmp_keys.pot
+    msgmerge -U ung_$1.pot tmp_keys.pot
+    rm tmp_keys.pot
+    echo 'update po files'
+    msgmerge -U ro/ung_$1.po ung_$1.pot
+    ;;    
 "webfilter")
     cd ../$1/po/
-    # TODO change charset=utf-8 for pot file
     echo 'get new keys'
-    xgettext --copyright-holder='Untangle, Inc.' -L Python -k.i18n._ -o tmpkeys.pot ../hier/usr/share/untangle/web/webui/script/untangle-node-$1/settings.js
-    msgcat tmpkeys.pot db_keys.pot block_page_keys.pot -o tmpkeys.pot
-    msgmerge -U ung_$1.pot tmpkeys.pot
-    rm tmpkeys.pot
+    xgettext --copyright-holder='Untangle, Inc.' -L Python -k.i18n._ -o tmp_keys.pot ../hier/usr/share/untangle/web/webui/script/untangle-node-$1/settings.js
+    msgcat tmp_keys.pot db_keys.pot block_page_keys.pot -o tmp_keys.pot
+    msgmerge -U ung_$1.pot tmp_keys.pot
+    rm tmp_keys.pot
     echo 'update po files'
     msgmerge -U ro/ung_$1.po ung_$1.pot
     ;;
-"phish")
-    cd ../$1/po/
-    # TODO change charset=utf-8 for pot file
+"virus")
+    cd ../virus-base/po/
     echo 'get new keys'
-    xgettext --copyright-holder='Untangle, Inc.' -L Python -k.i18n._ -o tmpkeys.pot ../hier/usr/share/untangle/web/webui/script/untangle-node-$1/settings.js
-    msgcat tmpkeys.pot block_page_keys.pot -o tmpkeys.pot
-    msgmerge -U ung_$1.pot tmpkeys.pot
-    rm tmpkeys.pot
+    xgettext --copyright-holder='Untangle, Inc.' -L Python -k.i18n._ -o tmp_keys.pot ../hier/usr/share/untangle/web/webui/script/untangle-base-virus/settings.js
+    msgcat tmp_keys.pot db_keys.pot block_page_keys.pot -o tmp_keys.pot
+    msgmerge -U ung_$1.pot tmp_keys.pot
+    rm tmp_keys.pot
+    echo 'update po files'
+    msgmerge -U ro/ung_$1.po ung_$1.pot
+    ;;
+"phish"|"spyware")
+    cd ../$1/po/
+    echo 'get new keys'
+    xgettext --copyright-holder='Untangle, Inc.' -L Python -k.i18n._ -o tmp_keys.pot ../hier/usr/share/untangle/web/webui/script/untangle-node-$1/settings.js
+    msgcat tmp_keys.pot block_page_keys.pot -o tmp_keys.pot
+    msgmerge -U ung_$1.pot tmp_keys.pot
+    rm tmp_keys.pot
+    echo 'update po files'
+    msgmerge -U ro/ung_$1.po ung_$1.pot
+    ;;
+"spamassassin")    
+    cd ../$1/po/
+    echo 'get new keys'
+    xgettext --copyright-holder='Untangle, Inc.' -L Python -k.i18n._ -o tmp_keys.pot ../hier/usr/share/untangle/web/webui/script/untangle-node-$1/settings.js
+    msgmerge -U ung_$1.pot tmp_keys.pot
+    rm tmp_keys.pot
     echo 'update po files'
     msgmerge -U ro/ung_$1.po ung_$1.pot
     ;;
