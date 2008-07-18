6ecf1f0ecb46d59f57f40da8d8ed41388ec913c6
Aaron Read <amread@untangle.com>
2008-07-18 04:02:16 +0000

    add ports to apache
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/web-ui/work/src@17557 8ebf8b4e-06f3-0310-961f-89f74618be54

--- mail-casing/package.rb
+++ mail-casing/package.rb
@@ -1,6 +1,7 @@
+
 # -*-ruby-*-
 # $HeadURL$
-# Copyright (c) 2003-2007 Untangle, Inc. 
+# Copyright (c) 2003-2007 Untangle, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2,
