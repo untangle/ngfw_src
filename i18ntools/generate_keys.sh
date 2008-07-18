dfe428f0fd5d9fb9d4578b7df556c4bc5f4ca9a2
Catalin Matei <cmatei@untangle.com>
2008-07-18 05:28:44 +0000

    L10N BlockPages - phish
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/web-ui/work/src@17563 8ebf8b4e-06f3-0310-961f-89f74618be54

--- i18ntools/generate_keys.sh
+++ i18ntools/generate_keys.sh
@@ -17,15 +17,26 @@ case "$1" in
     msgmerge -U ro/ung_$1.po ung_$1.pot
     ;;
 "webfilter")
-    cd ../webfilter/po/
+    cd ../$1/po/
     # TODO change charset=utf-8 for pot file
     echo 'get new keys'
-    xgettext --copyright-holder='Untangle, Inc.' -L Python -k.i18n._ -o tmpkeys.pot ../hier/usr/share/untangle/web/webui/script/untangle-node-webfilter/settings.js
+    xgettext --copyright-holder='Untangle, Inc.' -L Python -k.i18n._ -o tmpkeys.pot ../hier/usr/share/untangle/web/webui/script/untangle-node-$1/settings.js
     msgcat tmpkeys.pot db_keys.pot block_page_keys.pot -o tmpkeys.pot
-    msgmerge -U ung_webfilter.pot tmpkeys.pot
+    msgmerge -U ung_$1.pot tmpkeys.pot
     rm tmpkeys.pot
     echo 'update po files'
-    msgmerge -U ro/ung_webfilter.po ung_webfilter.pot
+    msgmerge -U ro/ung_$1.po ung_$1.pot
+    ;;
+"phish")
+    cd ../$1/po/
+    # TODO change charset=utf-8 for pot file
+    echo 'get new keys'
+    xgettext --copyright-holder='Untangle, Inc.' -L Python -k.i18n._ -o tmpkeys.pot ../hier/usr/share/untangle/web/webui/script/untangle-node-$1/settings.js
+    msgcat tmpkeys.pot block_page_keys.pot -o tmpkeys.pot
+    msgmerge -U ung_$1.pot tmpkeys.pot
+    rm tmpkeys.pot
+    echo 'update po files'
+    msgmerge -U ro/ung_$1.po ung_$1.pot
     ;;
 *)
     echo 1>&2 Module Name \"$1\" is invalid ...
