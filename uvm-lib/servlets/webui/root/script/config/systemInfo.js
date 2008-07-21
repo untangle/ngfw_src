ebd099c3e04c662cd463bc128dbce618becb8891
Catalin Dondera <catalind@untangle.com>
2008-07-21 12:52:47 +0000

    Added validation & save for 'System Info'
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/web-ui/work/src@17625 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm-lib/servlets/webui/root/script/config/systemInfo.js
+++ uvm-lib/servlets/webui/root/script/config/systemInfo.js
@@ -107,7 +107,7 @@ if (!Ung.hasResource["Ung.SystemInfo"]) {
                     autoHeight : true,
                     layout : 'column',
                     items: [{
-	                    columnWidth : .6,
+	                    columnWidth : .4,
 	                    layout : "form",
                         border : false,
                         style : 'text-align: right;',
@@ -119,49 +119,61 @@ if (!Ung.hasResource["Ung.SystemInfo"]) {
 	                    items : [ new Ext.form.TextField({
 	                        name : "Company Name",
 	                        fieldLabel : this.i18n._("Company Name"),
+                            id : 'companyName',
                             allowBlank : false,
                             value : this.getRegistrationInfo().companyName
 	                    }), new Ext.form.TextField({
                             name : "First Name",
                             fieldLabel : this.i18n._("First Name"),
+                            id : 'firstName',
                             allowBlank : false,
                             value : this.getRegistrationInfo().firstName
                         }), new Ext.form.TextField({
                             name : "Last Name",
                             fieldLabel : this.i18n._("Last Name"),
+                            id : 'lastName',
                             allowBlank : false,
                             value : this.getRegistrationInfo().lastName
                         }), new Ext.form.TextField({
                             name : "Address 1",
+                            id : 'address1',
                             fieldLabel : this.i18n._("Address 1"),
                             value : this.getRegistrationInfo().address1
                         }), new Ext.form.TextField({
                             name : "Address 2",
+                            id : 'address2',
                             fieldLabel : this.i18n._("Address 2"),
                             value : this.getRegistrationInfo().address2
                         }), new Ext.form.TextField({
                             name : "City",
+                            id : 'city',
                             fieldLabel : this.i18n._("City"),
                             value : this.getRegistrationInfo().city
                         }), new Ext.form.TextField({
                             name : "State",
+                            id : 'state',
                             fieldLabel : this.i18n._("State"),
                             value : this.getRegistrationInfo().state
                         }), new Ext.form.TextField({
                             name : "Zipcode",
+                            id : 'zipcode',
                             fieldLabel : this.i18n._("Zipcode"),
                             value : this.getRegistrationInfo().zipcode
                         }), new Ext.form.TextField({
                             name : "Phone #",
+                            id : 'phone',
                             fieldLabel : this.i18n._("Phone #"),
                             value : this.getRegistrationInfo().phone
                         }), new Ext.form.TextField({
                             name : "Email",
+                            id : 'emailAddr',
                             fieldLabel : this.i18n._("Email"),
                             allowBlank : false,
-                            value : this.getRegistrationInfo().emailAddr
+                            value : this.getRegistrationInfo().emailAddr,
+                            vtype : 'email'
                         }), new Ext.form.TextField({
                             name : "Number of computers%sprotected by Untangle",
+                            id : 'numSeats',
                             fieldLabel : i18n.sprintf(this.i18n._('Number of computers%sprotected by Untangle'),'<br>'),
                             allowBlank : false,
                             value : this.getRegistrationInfo().numSeats
@@ -190,18 +202,95 @@ if (!Ung.hasResource["Ung.SystemInfo"]) {
                 })]
            });
         },
+        //validate Registration Info
+        validateClient : function() {
+            var companyNameCmp = Ext.getCmp('companyName');
+            if (!companyNameCmp.isValid()) {
+                Ext.MessageBox.alert(this.i18n._('Warning'), this.i18n._('You must fill out the company name.'),
+                    function () {
+                        this.tabs.activate(this.panelRegistration);
+                        companyNameCmp.focus(true);
+                    }.createDelegate(this) 
+                );
+                return false;
+            }
+            this.getRegistrationInfo().companyName = companyNameCmp.getValue();
+
+            var firstNameCmp = Ext.getCmp('firstName');
+            if (!firstNameCmp.isValid()) {
+                Ext.MessageBox.alert(this.i18n._('Warning'), this.i18n._('You must fill out your first name.'),
+                    function () {
+                        this.tabs.activate(this.panelRegistration);
+                        firstNameCmp.focus(true);
+                    }.createDelegate(this) 
+                );
+                return false;
+            }
+            this.getRegistrationInfo().companyName = companyNameCmp.getValue();
+
+            var lastNameCmp = Ext.getCmp('lastName');
+            if (!lastNameCmp.isValid()) {
+                Ext.MessageBox.alert(this.i18n._('Warning'), this.i18n._('You must fill out the last name.'),
+                    function () {
+                        this.tabs.activate(this.panelRegistration);
+                        lastNameCmp.focus(true);
+                    }.createDelegate(this) 
+                );
+                return false;
+            }
+            this.getRegistrationInfo().companyName = companyNameCmp.getValue();
+
+            var emailAddrCmp = Ext.getCmp('emailAddr');
+            if (!emailAddrCmp.isValid()) {
+                Ext.MessageBox.alert(this.i18n._('Warning'), this.i18n._('You must fill out the email address in the format "user@domain.com".'),
+                    function () {
+                        this.tabs.activate(this.panelRegistration);
+                        emailAddrCmp.focus(true);
+                    }.createDelegate(this) 
+                );
+                return false;
+            }
+            this.getRegistrationInfo().companyName = companyNameCmp.getValue();
+
+            var numSeatsCmp = Ext.getCmp('numSeats');
+            if (!numSeatsCmp.isValid()) {
+                Ext.MessageBox.alert(this.i18n._('Warning'), this.i18n._('You must fill out the number of computers protected by Untangle.'),
+                    function () {
+                        this.tabs.activate(this.panelRegistration);
+                        numSeatsCmp.focus(true);
+                    }.createDelegate(this) 
+                );
+                return false;
+            }
+            this.getRegistrationInfo().companyName = companyNameCmp.getValue();
+
+            var address1Cmp = Ext.getCmp('address1');
+            this.getRegistrationInfo().address1 = address1Cmp.getValue();
+            var address2Cmp = Ext.getCmp('address2');
+            this.getRegistrationInfo().address2 = address2Cmp.getValue();
+            var cityCmp = Ext.getCmp('city');
+            this.getRegistrationInfo().city = cityCmp.getValue();
+            var stateCmp = Ext.getCmp('state');
+            this.getRegistrationInfo().state = stateCmp.getValue();
+            var zipcodeCmp = Ext.getCmp('zipcode');
+            this.getRegistrationInfo().zipcode = zipcodeCmp.getValue();
+            var phoneCmp = Ext.getCmp('phone');
+            this.getRegistrationInfo().phone = phoneCmp.getValue();
+            
+            return true;
+        },
         // save function
         saveAction : function() {
             if (this.validate()) {
-//                Ext.MessageBox.wait(i18n._("Saving..."), i18n._("Please wait"));
-//                rpc.brandingManager.setBaseSettings(function(result, exception) {
-//                    Ext.MessageBox.hide();
-//                    if (exception) {
-//                        Ext.MessageBox.alert(i18n._("Failed"), exception.message);
-//                        return;
-//                    }
-//                    this.cancelAction();
-//                }.createDelegate(this), this.getBrandingBaseSettings());
+                Ext.MessageBox.wait(i18n._("Saving..."), i18n._("Please wait"));
+                rpc.adminManager.setRegistrationInfo(function(result, exception) {
+                    Ext.MessageBox.hide();
+                    if (exception) {
+                        Ext.MessageBox.alert(i18n._("Failed"), exception.message);
+                        return;
+                    }
+                    this.cancelAction();
+                }.createDelegate(this), this.getRegistrationInfo());
             }
         }
 
