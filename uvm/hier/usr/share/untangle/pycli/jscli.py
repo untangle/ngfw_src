355e51388ee79056f8b12347f120ec36ca8949e5
Aaron Read <amread@untangle.com>
2008-08-05 20:04:11 +0000

    jscli usage
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/web-ui/work/src@18048 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/share/untangle/pycli/jscli.py
+++ uvm/hier/usr/share/untangle/pycli/jscli.py
@@ -26,22 +26,92 @@ class CurlRequestHandler(object):
         self.__curl.setopt( pycurl.CONNECTTIMEOUT, 10 )
         self.__curl.setopt( pycurl.COOKIEFILE, "" )
         self.__curl.setopt( pycurl.FOLLOWLOCATION, 0 )
-        
+
     def make_request(self, url, postdata, content_type = "text/plain"  ):
         response = StringIO()
-        
+
         self.__curl.setopt( pycurl.URL, url )
         self.__curl.setopt( pycurl.POST, 1 )
         if not content_type == None:
             self.__curl.setopt( pycurl.HTTPHEADER, [ "Content-type: ", content_type ] )
         self.__curl.setopt( pycurl.VERBOSE, False )
-        self.__curl.setopt( pycurl.POSTFIELDS, str( postdata ))            
+        self.__curl.setopt( pycurl.POSTFIELDS, str( postdata ))
         self.__curl.setopt( pycurl.WRITEFUNCTION, response.write )
         self.__curl.perform()
 
         if ( self.__curl.getinfo( pycurl.HTTP_CODE ) != 200 ): raise JSONRPCException("Invalid username or password")
-        
+
         return response.getvalue()
+
+def printUsage(script):
+    print """\
+%s Usage:
+  optional args:
+    -h hostname
+    -u username
+    -w password
+    -t timeout (default 120000)
+    -p policy
+    -v
+  toolbox commands:
+    ucli install mackage-name
+    ucli uninstall mackage-name
+    ucli update
+    ucli upgrade
+    ucli requestInstall mackage-name
+  toolbox lists:
+    ucli available
+    ucli installed
+    ucli uninstalled
+    ucli upgradable
+    ucli uptodate
+  node manager commands:
+    ucli instantiate mackage-name [ args ]
+    ucli start TID
+    ucli stop TID
+    ucli destroy TID
+    ucli neverStarted
+  node manager lists:
+    ucli instances
+  node live sessions:
+    ucli sessions [ TID ]
+  admin manager:
+    ucli who
+    ucli getRegInfo
+    ucli passwd [ -a | -d ] login [ password ]
+  uvm commands:
+    ucli shutdown
+    ucli serverStats
+    ucli gc
+    ucli loadRup
+    ucli setProperty key value
+  policy manager:
+    ucli addPolicy name [notes]
+    ucli listPolicies
+  reporting manager:
+    ucli isReportingEnabled
+    ucli areReportsAvailable
+    ucli prepareReports [ args ]
+    ucli startReports
+    ucli stopReports
+    ucli isReportingRunning
+  logging manager:
+    ucli userLogs tid
+    ucli resetLogs
+    ucli logError [text]
+  combo commands:
+    ucli loadt short-name [ args ]
+    ucli reloadt short-name
+    ucli unloadt short-name
+  apt commands:
+    ucli register mackage-name
+    ucli unregister mackage-name
+  argon commands:
+  nucli server commands:
+    ucli restartCliServer
+  debugging commands:
+    ucli aptTail
+""" % script
 
 handler = CurlRequestHandler()
 try:
@@ -58,6 +128,11 @@ args = copy( sys.argv )
 calledMethod = False
 
 script = args.pop(0)
+
+if len(args) == 0:
+    printUsage(script)
+    sys.exit(1)
+
 method = args.pop(0).lower()
 
 for manager in Manager.managers:
@@ -74,5 +149,7 @@ for manager in Manager.managers:
         print "Unable to make the request: ", e.error
     break
 
-if not calledMethod: print "Unable to find method: ", method
-
+if not calledMethod:
+    print "Unable to find method: ", method
+    printUsage(script)
+    sys.exit(1)
