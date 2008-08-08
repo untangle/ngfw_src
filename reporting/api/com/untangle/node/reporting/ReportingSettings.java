a8a35b9f085671f8c3cf6547bc7b740bb17abb44
Catalin Matei <cmatei@untangle.com>
2008-08-08 08:01:12 +0000

    Untangle Reports - IPMap
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/web-ui/work/src@18092 8ebf8b4e-06f3-0310-961f-89f74618be54

--- reporting/api/com/untangle/node/reporting/ReportingSettings.java
+++ reporting/api/com/untangle/node/reporting/ReportingSettings.java
@@ -62,12 +62,12 @@ public class ReportingSettings implements Serializable
     @Id
     @Column(name="id")
     @GeneratedValue
-    private Long getId()
+    public Long getId()
     {
         return id;
     }
 
-    private void setId(Long id)
+    public void setId(Long id)
     {
         this.id = id;
     }
--- uvm-lib/api/com/untangle/uvm/node/IPMaddrDirectory.java
+++ uvm-lib/api/com/untangle/uvm/node/IPMaddrDirectory.java
@@ -81,12 +81,12 @@ public class IPMaddrDirectory implements Serializable
     @Id
     @Column(name="id")
     @GeneratedValue
-    private Long getId()
+    public Long getId()
     {
         return id;
     }
 
-    private void setId(Long id)
+    public void setId(Long id)
     {
         this.id = id;
     }
