9bfb410f01d4e73ecaee5a0337015ed378cd96dd
Aaron Read <amread@untangle.com>
2008-08-21 22:03:01 +0000

    move
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/web-ui/work/src@18445 8ebf8b4e-06f3-0310-961f-89f74618be54

--- http-casing/localapi/com/untangle/node/http/BlockPageUtil.java
+++ http-casing/localapi/com/untangle/node/http/BlockPageUtil.java
@@ -116,5 +116,4 @@ public class BlockPageUtil
     {
         return INSTANCE;
     }
-
 }
--- uvm-lib/impl/com/untangle/uvm/engine/RemoteLanguageManagerImpl.java
+++ uvm-lib/impl/com/untangle/uvm/engine/RemoteLanguageManagerImpl.java
@@ -36,19 +36,17 @@ import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
-import org.apache.commons.fileupload.FileItem;
-import org.apache.log4j.Logger;
-import org.hibernate.Query;
-import org.hibernate.Session;
-
 import com.untangle.uvm.LanguageInfo;
 import com.untangle.uvm.LanguageSettings;
 import com.untangle.uvm.RemoteLanguageManager;
 import com.untangle.uvm.UvmException;
 import com.untangle.uvm.util.DeletingDataSaver;
 import com.untangle.uvm.util.TransactionWork;
-
 import edu.emory.mathcs.backport.java.util.Collections;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.log4j.Logger;
+import org.hibernate.Query;
+import org.hibernate.Session;
 
 /**
  * Implementation of RemoteLanguageManagerImpl.
@@ -65,10 +63,10 @@ class RemoteLanguageManagerImpl implements RemoteLanguageManager
     private static final String DEFAULT_LANGUAGE = "en";
     private static final String BASENAME_COMMUNITY_PREFIX = "i18n.community";
     private static final String BASENAME_OFFICIAL_PREFIX = "i18n.official";
-    private static final String LANGUAGES_CFG = "lang.cfg";  
+    private static final String LANGUAGES_CFG = "lang.cfg";
     private static final String LC_MESSAGES = "LC_MESSAGES";
     private static final String UNG_PREFIX = "ung_";
-	private static final int BUFFER = 2048; 
+    private static final int BUFFER = 2048;
 
     private final Logger logger = Logger.getLogger(getClass());
 
@@ -76,8 +74,8 @@ class RemoteLanguageManagerImpl implements RemoteLanguageManager
     private LanguageSettings settings;
 
     RemoteLanguageManagerImpl(UvmContextImpl uvmContext) {
-    	this.uvmContext = uvmContext;    	
-    	
+        this.uvmContext = uvmContext;
+
         TransactionWork tw = new TransactionWork()
         {
             public boolean doWork(Session s)
@@ -86,11 +84,11 @@ class RemoteLanguageManagerImpl implements RemoteLanguageManager
                 settings = (LanguageSettings)q.uniqueResult();
 
                 if (null == settings) {
-                	settings = new LanguageSettings();
-                	settings.setLanguage(DEFAULT_LANGUAGE);
+                    settings = new LanguageSettings();
+                    settings.setLanguage(DEFAULT_LANGUAGE);
                     s.save(settings);
                 }
-                
+
                 return true;
             }
         };
@@ -99,220 +97,225 @@ class RemoteLanguageManagerImpl implements RemoteLanguageManager
 
     // public methods ---------------------------------------------------------
 
-	public LanguageSettings getLanguageSettings() {
-		return settings;
-	}
+    public LanguageSettings getLanguageSettings() {
+        return settings;
+    }
 
-	public void setLanguageSettings(LanguageSettings settings) {
-        /* delete whatever is in the db, and just make a fresh settings object */
-		LanguageSettings copy = new LanguageSettings();
+    public void setLanguageSettings(LanguageSettings settings) {
+        /* delete whatever is in the db, and just make a fresh
+         * settings object */
+        LanguageSettings copy = new LanguageSettings();
         settings.copy(copy);
         saveSettings(copy);
         this.settings = copy;
-	}
-	
-    public void uploadLanguagePack(FileItem item) throws UvmException {
-	    try {
-	        BufferedOutputStream dest = null;
-			ZipEntry entry = null;
-			
-			//validate language pack
-		    if (!item.getName().endsWith(".zip")) {
-				throw new UvmException("Invalid Language Pack");
-		    }
-			
-	        // Open the ZIP file
-		    InputStream uploadedStream = item.getInputStream();
-		    ZipInputStream zis = new ZipInputStream(uploadedStream);
-			while ((entry = zis.getNextEntry()) != null) {
-				if (entry.isDirectory()) {
-					File dir = new File(LANGUAGES_COMMUNITY_DIR + File.separator + entry.getName());
-					if (!dir.exists()) {
-						dir.mkdir();
-					}
-				} else {
-					int count;
-					byte data[] = new byte[BUFFER];
-					
-					// write the files to the disk
-					FileOutputStream fos = new FileOutputStream(LANGUAGES_COMMUNITY_DIR + File.separator + entry.getName());
-					dest = new BufferedOutputStream(fos, BUFFER);
-					while ((count = zis.read(data, 0, BUFFER)) != -1) {
-						dest.write(data, 0, count);
-					}
-					dest.flush();
-					dest.close();
-					
-					//compile to .mo file and install it in the appropriate place (LOCALE_DIR)
-					compileMoFile(entry);
-					
-					//compile the java properties version & install it in the classpath (LANGUAGES_DIR)
-					compileResourceBundle(entry);
-				}
-			}
-			zis.close();		    	
-		    uploadedStream.close();
-	    } catch (IOException e) {
-	    	logger.error(e);
-			throw new UvmException("Upload Language Pack Failed");
-	    }
     }
 
-	private void compileResourceBundle(ZipEntry entry) throws IOException,
-			UvmException {
-		boolean success = true;
-		try {
-			String tokens[] = entry.getName().split(File.separator);
-			String lang = tokens[0];
-			String moduleName = tokens[1].substring(0, tokens[1].lastIndexOf("."));
-			
-			String cmd[] = { "msgfmt", "--java2", 
-					"-d", LANGUAGES_DIR,
-					"-r", BASENAME_COMMUNITY_PREFIX + "." + moduleName,
-					"-l", lang,
-					LANGUAGES_COMMUNITY_DIR + File.separator + entry.getName()};
-			Process p = Runtime.getRuntime().exec(cmd);
-			p.waitFor();
-			if (p.exitValue() != 0) {
-				success = false;
-			}
-			
-		} catch (Exception err) {
-			success = false;
-		}
-		if (!success) {
-			throw new UvmException("Error compiling to resource bundle");
-		}
-	}
+    public void uploadLanguagePack(FileItem item) throws UvmException {
+        try {
+            BufferedOutputStream dest = null;
+            ZipEntry entry = null;
 
-	private void compileMoFile(ZipEntry entry) throws IOException, UvmException {
-		boolean success = true;
-		try {
-			String tokens[] = entry.getName().split(File.separator);
-			String lang = tokens[0];
-			String moduleName = tokens[1].substring(0, tokens[1].lastIndexOf("."));
-			
-			String cmd[] = { "msgfmt",
-					"-o", LOCALE_DIR + File.separator + lang + File.separator + LC_MESSAGES + File.separator + moduleName  + ".mo",
-					LANGUAGES_COMMUNITY_DIR + File.separator + entry.getName()};
-			Process p = Runtime.getRuntime().exec(cmd);
-			p.waitFor();
-			if (p.exitValue() != 0) {
-				success = false;
-			}
-			
-		} catch (InterruptedException err) {
-			success = false;
-		}				
-		if (!success) {
-			throw new UvmException("Error compiling to mo file");
-		}
-	}
-	
+            // validate language pack
+            if (!item.getName().endsWith(".zip")) {
+                throw new UvmException("Invalid Language Pack");
+            }
+
+            // Open the ZIP file
+            InputStream uploadedStream = item.getInputStream();
+            ZipInputStream zis = new ZipInputStream(uploadedStream);
+            while ((entry = zis.getNextEntry()) != null) {
+                if (entry.isDirectory()) {
+                    File dir = new File(LANGUAGES_COMMUNITY_DIR + File.separator + entry.getName());
+                    if (!dir.exists()) {
+                        dir.mkdir();
+                    }
+                } else {
+                    int count;
+                    byte data[] = new byte[BUFFER];
+
+                    // write the files to the disk
+                    FileOutputStream fos = new FileOutputStream(LANGUAGES_COMMUNITY_DIR + File.separator + entry.getName());
+                    dest = new BufferedOutputStream(fos, BUFFER);
+                    while ((count = zis.read(data, 0, BUFFER)) != -1) {
+                        dest.write(data, 0, count);
+                    }
+                    dest.flush();
+                    dest.close();
+
+                    // compile to .mo file and install it in the
+                    // appropriate place (LOCALE_DIR)
+                    compileMoFile(entry);
+
+                    // compile the java properties version & install
+                    // it in the classpath (LANGUAGES_DIR)
+                    compileResourceBundle(entry);
+                }
+            }
+            zis.close();
+            uploadedStream.close();
+        } catch (IOException e) {
+            logger.error(e);
+            throw new UvmException("Upload Language Pack Failed");
+        }
+    }
+
+    private void compileResourceBundle(ZipEntry entry) throws IOException,
+            UvmException {
+        boolean success = true;
+        try {
+            String tokens[] = entry.getName().split(File.separator);
+            String lang = tokens[0];
+            String moduleName = tokens[1].substring(0, tokens[1].lastIndexOf("."));
+
+            String cmd[] = { "msgfmt", "--java2",
+                    "-d", LANGUAGES_DIR,
+                    "-r", BASENAME_COMMUNITY_PREFIX + "." + moduleName,
+                    "-l", lang,
+                    LANGUAGES_COMMUNITY_DIR + File.separator + entry.getName()};
+            Process p = Runtime.getRuntime().exec(cmd);
+            p.waitFor();
+            if (p.exitValue() != 0) {
+                success = false;
+            }
+
+        } catch (Exception err) {
+            success = false;
+        }
+        if (!success) {
+            throw new UvmException("Error compiling to resource bundle");
+        }
+    }
+
+    private void compileMoFile(ZipEntry entry) throws IOException, UvmException {
+        boolean success = true;
+        try {
+            String tokens[] = entry.getName().split(File.separator);
+            String lang = tokens[0];
+            String moduleName = tokens[1].substring(0, tokens[1].lastIndexOf("."));
+
+            String cmd[] = { "msgfmt",
+                    "-o", LOCALE_DIR + File.separator + lang + File.separator + LC_MESSAGES + File.separator + moduleName  + ".mo",
+                    LANGUAGES_COMMUNITY_DIR + File.separator + entry.getName()};
+            Process p = Runtime.getRuntime().exec(cmd);
+            p.waitFor();
+            if (p.exitValue() != 0) {
+                success = false;
+            }
+
+        } catch (InterruptedException err) {
+            success = false;
+        }
+        if (!success) {
+            throw new UvmException("Error compiling to mo file");
+        }
+    }
+
     public List<LanguageInfo> getLanguagesList() {
         List<LanguageInfo> languages = new ArrayList<LanguageInfo>();
- 
+
         Set<String> availableLanguages = new HashSet<String>();
-    	//get available official languages
+        // get available official languages
         Collections.addAll(availableLanguages, (new File(LANGUAGES_OFFICIAL_DIR)).list());
-    	//get available community languages
+        // get available community languages
         Collections.addAll(availableLanguages, (new File(LANGUAGES_COMMUNITY_DIR)).list());
-    	
-		// Reading all languages from config file and keep only the one which we have translations for
-		try {
-			BufferedReader in = new BufferedReader(new FileReader(LANGUAGES_DIR + File.separator + LANGUAGES_CFG));
-			String s = new String();
-			while((s = in.readLine())!= null) {
-				if (s.trim().length() > 0){
-					String[] tokens = s.split("\\s");
-					if (tokens.length >= 2) {
-						String langCode = tokens[0];
-						String langName = tokens[1];
-						if (DEFAULT_LANGUAGE.equals(langCode) || 
-								availableLanguages.contains(langCode)){
-							languages.add(new LanguageInfo(tokens[0], tokens[1]));
-						}
-					}
-				}
-			}
-			in.close();
-		} catch (IOException e) {
-//			e.printStackTrace();
-		}
-    		
-    	return languages;
+
+        // Reading all languages from config file and keep only the
+        // one which we have translations for
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(LANGUAGES_DIR + File.separator + LANGUAGES_CFG));
+            String s = new String();
+            while((s = in.readLine())!= null) {
+                if (s.trim().length() > 0){
+                    String[] tokens = s.split("\\s");
+                    if (tokens.length >= 2) {
+                        String langCode = tokens[0];
+                        String langName = tokens[1];
+                        if (DEFAULT_LANGUAGE.equals(langCode) ||
+                                availableLanguages.contains(langCode)){
+                            languages.add(new LanguageInfo(tokens[0], tokens[1]));
+                        }
+                    }
+                }
+            }
+            in.close();
+        } catch (IOException e) {
+//          e.printStackTrace();
+        }
+
+        return languages;
     }
-    
+
     public Map<String, String> getTranslations(String module){
-    	String ungPrefixedModule = UNG_PREFIX + module;
-		Map<String, String> map = new HashMap<String, String>();
-/*		
-    	try {
-    		I18n i18n = I18nFactory.getI18n("i18n."+ungPrefixedModule, ungPrefixedModule, Thread
-    				.currentThread().getContextClassLoader(), new Locale(settings
-    				.getLanguage()), I18nFactory.DEFAULT);
-    		
-    		if (i18n != null) {
-    			for (Enumeration<String> enumeration = i18n.getResources().getKeys(); enumeration
-    					.hasMoreElements();) {
-    				String key = enumeration.nextElement();
-    				map.put(key, i18n.tr(key));
-    			}
-    		}
-		} catch (MissingResourceException e) {
-			// Do nothing - Fall back to a default that returns the passed text if no resource bundle can be located
-			// is done in client side
-		}
-*/		
-	// We will use this approuch instead of the one above, because of a bug in ResouceBundle in jdk 1.5, related with cache:
-	// I18N is using ResourceBundle.getBundle(baseName, locale, loader); to load a resouce bundle which caches the ResourceBundles, 
-	// and if we tried once to load a resource bundle, second time will return not found (MissingResourceException), evean if 
-	// the resource is available now; this prevent dynamic loading, which we need.
-	try {
-		Class clazz = loadResourceBundleClass(BASENAME_COMMUNITY_PREFIX + "." + ungPrefixedModule + "_" + settings.getLanguage());
-		if (clazz == null) {
-			// fall back to official translations
-			clazz = loadResourceBundleClass(BASENAME_OFFICIAL_PREFIX + "." + ungPrefixedModule + "_" + settings.getLanguage());
-		}
-		if (clazz != null) {
-			ResourceBundle resourceBundle = (ResourceBundle)clazz.newInstance();
-			if (resourceBundle != null) {
-				for (Enumeration<String> enumeration = resourceBundle.getKeys(); enumeration.hasMoreElements();) {
-					String key = enumeration.nextElement();
-					map.put(key, resourceBundle.getString(key));
-				}
-			}
-		}
-	} catch (Exception e) {
-		// Do nothing - Fall back to a default that returns the passed text if no resource bundle can be located
-		// is done in client side
-	}
-		
-		
+        String ungPrefixedModule = UNG_PREFIX + module;
+        Map<String, String> map = new HashMap<String, String>();
+/*
+        try {
+            I18n i18n = I18nFactory.getI18n("i18n."+ungPrefixedModule, ungPrefixedModule, Thread
+                    .currentThread().getContextClassLoader(), new Locale(settings
+                    .getLanguage()), I18nFactory.DEFAULT);
 
-		return map;
+            if (i18n != null) {
+                for (Enumeration<String> enumeration = i18n.getResources().getKeys(); enumeration
+                        .hasMoreElements();) {
+                    String key = enumeration.nextElement();
+                    map.put(key, i18n.tr(key));
+                }
+            }
+        } catch (MissingResourceException e) {
+            // Do nothing - Fall back to a default that returns the passed text if no resource bundle can be located
+            // is done in client side
+        }
+*/
+    // We will use this approuch instead of the one above, because of a bug in ResouceBundle in jdk 1.5, related with cache:
+    // I18N is using ResourceBundle.getBundle(baseName, locale, loader); to load a resouce bundle which caches the ResourceBundles,
+    // and if we tried once to load a resource bundle, second time will return not found (MissingResourceException), evean if
+    // the resource is available now; this prevent dynamic loading, which we need.
+    try {
+        Class clazz = loadResourceBundleClass(BASENAME_COMMUNITY_PREFIX + "." + ungPrefixedModule + "_" + settings.getLanguage());
+        if (clazz == null) {
+            // fall back to official translations
+            clazz = loadResourceBundleClass(BASENAME_OFFICIAL_PREFIX + "." + ungPrefixedModule + "_" + settings.getLanguage());
+        }
+        if (clazz != null) {
+            ResourceBundle resourceBundle = (ResourceBundle)clazz.newInstance();
+            if (resourceBundle != null) {
+                for (Enumeration<String> enumeration = resourceBundle.getKeys(); enumeration.hasMoreElements();) {
+                    String key = enumeration.nextElement();
+                    map.put(key, resourceBundle.getString(key));
+                }
+            }
+        }
+    } catch (Exception e) {
+        // Do nothing - Fall back to a default that returns the passed
+        // text if no resource bundle can be located is done in client
+        // side
     }
 
-	private Class loadResourceBundleClass(String name) {
-		Class clazz = null;
-		try {
-			clazz =Thread.currentThread().getContextClassLoader().loadClass(name);
-		} catch (ClassNotFoundException e1) {
-		}
-		return clazz;
-	}
-    
+
+
+        return map;
+    }
+
+    private Class loadResourceBundleClass(String name) {
+        Class clazz = null;
+        try {
+            clazz =Thread.currentThread().getContextClassLoader().loadClass(name);
+        } catch (ClassNotFoundException e1) {
+        }
+        return clazz;
+    }
+
     // private methods --------------------------------------------------------
     private void saveSettings(LanguageSettings settings) {
-        DeletingDataSaver<LanguageSettings> saver = 
+        DeletingDataSaver<LanguageSettings> saver =
             new DeletingDataSaver<LanguageSettings>(uvmContext,"LanguageSettings");
         this.settings = saver.saveData(settings);
     }
-    
+
     static {
-    	LANGUAGES_DIR = System.getProperty("bunnicula.lang.dir"); //place for languages resources files
-    	LANGUAGES_COMMUNITY_DIR = LANGUAGES_DIR + File.separator + "community"; //place for community languages resources files
-    	LANGUAGES_OFFICIAL_DIR = LANGUAGES_DIR + File.separator + "official"; //place for official languages resources files
-    	LOCALE_DIR = "/usr/share/locale"; // place for .mo files
+        LANGUAGES_DIR = System.getProperty("bunnicula.lang.dir"); // place for languages resources files
+        LANGUAGES_COMMUNITY_DIR = LANGUAGES_DIR + File.separator + "community"; // place for community languages resources files
+        LANGUAGES_OFFICIAL_DIR = LANGUAGES_DIR + File.separator + "official"; // place for official languages resources files
+        LOCALE_DIR = "/usr/share/locale"; // place for .mo files
     }
 }
