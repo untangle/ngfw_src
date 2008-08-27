ee3a15bd42a1aa4e196b9ba404f6550454d1e8c7
Aaron Read <amread@untangle.com>
2008-08-27 22:06:50 +0000

    add active stat
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/web-ui/work/src@18532 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/share/untangle/schema/uvm/settings-convert-16.sql
+++ uvm/hier/usr/share/untangle/schema/uvm/settings-convert-16.sql
@@ -41,3 +41,11 @@ CREATE TABLE settings.u_blinger_active (
     position             int4 NOT NULL,
     blinger_name         text NOT NULL,
     PRIMARY KEY          (blinger_settings_id, position));
+
+CREATE TABLE settings.u_active_stat (
+    id                   int8 NOT NULL,
+    settings_id          int8,
+    position             int4,
+    name                 text NOT NULL,
+    interval             text NOT NULL,
+    PRIMARY KEY (id));
