ccf174d778664d57ff7e9386dca63603f5a0b320
Robert Scott <rbscott@untangle.com>
2008-09-11 01:24:50 +0000

    inserting a script for debugging licensing.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/rup@18772 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ unittest/get_license_status.rb
@@ -0,0 +1,26 @@
+## A rush shell for testing the new quarantine API methods.
+
+## (Wherever the rush shell is)
+## To run use: sudo ./dist/usr/bin/rush ./license/unittest/get_license_status.rb 
+
+identifiers = [
+               "untangle-dual-virus-blocker-kav",
+               "untangle-remote-access-portal",
+               "untangle-configuration-backup", 
+               "untangle-pcremote",
+               "untangle-user-directory-integration", 
+               "untangle-user-directory-management", 
+               "untangle-policy-manager", 
+               "untangle-license-manager", 
+               "untangle-branding-manager", 
+               "untangle-pcremote"
+              ]
+
+license_manager = Untangle::RemoteUvmContext.licenseManager()
+
+identifiers.each do |identifier|
+  status = license_manager.getLicenseStatus( identifier )
+  puts "#{identifier}: type #{status["type"]}, #{status["timeRemaining"]}"
+end
+
+
