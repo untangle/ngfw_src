a80d09e254f85a488dc2fcae22d7613f4a457b95
Aaron Read <amread@untangle.com>
2008-11-18 01:48:11 +0000

    get nonce after download
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/rup@20400 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ ext-localapi/com/untangle/uvm/rup/NonceServlet.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2003-2007 Untangle, Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Untangle, Inc. ("Confidential Information"). You shall
+ * not disclose such Confidential Information.
+ *
+ * $Id: PortalServlet.java 18363 2008-08-19 01:07:14Z amread $
+ */
+
+package com.untangle.uvm.rup;
+
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.Random;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.untangle.uvm.LocalUvmContext;
+import com.untangle.uvm.LocalUvmContextFactory;
+import com.untangle.uvm.util.TransactionWork;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+
+public class NonceServlet extends HttpServlet
+{
+    private final Random rand = new SecureRandom();
+    private final Logger logger = Logger.getLogger(getClass());
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException
+    {
+        LocalUvmContext uvm = LocalUvmContextFactory.context();
+
+        final ProxyNonce nonce = new ProxyNonce(Long.toHexString(rand.nextLong()));
+        uvm.runTransaction(new TransactionWork() {
+                public boolean doWork(Session s)
+                {
+                    s.save(nonce);
+                    return true;
+                }
+            });
+
+        try {
+            resp.getWriter().println(nonce.getNonce());
+        } catch (IOException exn) {
+            logger.warn("could not send nonce", exn);
+            throw new ServletException(exn);
+        }
+    }
+}
