d3bebe6b07d0fdda353a506d667188fdce540162
Vald Dumitrescu <vdumitrescu@untangle.com>
2008-12-04 16:33:46 +0000

    refactor Ung.SettingsWin base class for: Ung.NodeWin and Ung.ConfigWin Ung.Settings removed
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@20802 8ebf8b4e-06f3-0310-961f-89f74618be54

--- webfilter/hier/usr/share/untangle/web/webui/script/untangle-node-webfilter/settings.js
+++ webfilter/hier/usr/share/untangle/web/webui/script/untangle-node-webfilter/settings.js
@@ -1,9 +1,9 @@
-Ung.Settings.dependency["untangle-node-webfilter"] = {
+Ung.NodeWin.dependency["untangle-node-webfilter"] = {
     name : "untangle-base-webfilter",
     fn : function() {
         if (!Ung.hasResource["Ung.WebFilter"]) {
             Ung.hasResource["Ung.WebFilter"] = true;
-            Ung.Settings.registerClassName('untangle-node-webfilter', 'Ung.WebFilter');
+            Ung.NodeWin.registerClassName('untangle-node-webfilter', 'Ung.WebFilter');
             Ung.WebFilter = Ext.extend(Ung.BaseWebFilter, {});
         }
     }
