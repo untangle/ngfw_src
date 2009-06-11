3af538a92b287e01c8bb3000a2902392bfda2e39
Dirk Morris <dmorris@untangle.com>
2009-06-11 23:04:27 +0000

    fix off by ten problem in headers
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@23605 8ebf8b4e-06f3-0310-961f-89f74618be54

--- spam-base/impl/com/untangle/node/spam/SpamReport.java
+++ spam-base/impl/com/untangle/node/spam/SpamReport.java
@@ -168,9 +168,9 @@ public class SpamReport
         }
 
         sb.append("score=");
-        sb.append(String.format("%.1f", score / 10.0f));
+        sb.append(String.format("%.1f", score));
         sb.append(" required=");
-        sb.append(String.format("%.1f", threshold / 10.0f));
+        sb.append(String.format("%.1f", threshold));
 
         sb.append(" tests=");
 
