15d8aa6a7a44ce3b478a41198ca0e1a9d76ae24b
Dirk Morris <dmorris@untangle.com>
2009-08-03 19:21:35 +0000

    remove utnice
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@24110 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm-lib/impl/com/untangle/uvm/engine/UvmContextImpl.java
+++ uvm-lib/impl/com/untangle/uvm/engine/UvmContextImpl.java
@@ -473,11 +473,8 @@ public class UvmContextImpl extends UvmContextBase
 
     public Process exec(String[] cmd, String[] envp, File dir) throws IOException
     {
-        String[] newCmd = new String[cmd.length + 1];
-        newCmd[0] = System.getProperty("bunnicula.bin.dir") + "/utnice";
-        System.arraycopy(cmd, 0, newCmd, 1, cmd.length);
         try {
-            return Runtime.getRuntime().exec(newCmd, envp, dir);
+            return Runtime.getRuntime().exec(cmd, envp, dir);
         } catch (IOException x) {
             // Check and see if we've run out of virtual memory.  This is very ugly
             // but there's not another apparent way.  XXXXXXXXXX
