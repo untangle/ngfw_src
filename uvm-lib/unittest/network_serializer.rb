53c9b814df2fa544daacb9f76fa833f5b01f4dd9
Robert Scott <rbscott@untangle.com>
2009-08-14 18:00:24 +0000

    updating the network serializer scripts.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@24243 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm-lib/unittest/network_serializer.rb
+++ uvm-lib/unittest/network_serializer.rb
@@ -3,9 +3,21 @@
 networkManager = Untangle::RemoteUvmContext.networkManager()
 settings = networkManager.getNetworkSettings()
 
+bridge_map = {}
+settings["interfaceList"]["list"].each do |i|
+  bridge = bridge_map[i["networkSpace"]["name"]]
+  bridge = [] if bridge.nil?
+  system_name = i["systemName"]
+  next if system_name == "tun0"
+  bridge << system_name
+
+  bridge_map[i["networkSpace"]["name"]] = bridge
+end
+
 settings["networkSpaceList"]["list"].each do |space|
   space["networkList"]["list"].each do |networkRule|
-    puts "ip network: #{space["name"]} #{networkRule["IPNetwork"]["network"]}/#{networkRule["IPNetwork"]["netmask"]}"
+    i_name = ( bridge_map[space["name"]] or []).join( "," )
+    puts "ip network: #{space["name"]}[#{i_name}] #{networkRule["IPNetwork"]["network"]}/#{networkRule["IPNetwork"]["netmask"]}"
   end
 end
 
