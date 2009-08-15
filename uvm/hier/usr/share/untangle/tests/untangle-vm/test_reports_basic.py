3cde3abeaef2573491a395b26f509a74465f81b9
Robert Scott <rbscott@untangle.com>
2009-08-15 01:24:59 +0000

    Inserting a basic test for reports.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@24252 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ uvm/hier/usr/share/untangle/tests/untangle-vm/test_reports_basic.py
@@ -0,0 +1,90 @@
+from untangle.ats.reports_setup import ReportsSetup
+from untangle.ats.reports_setup import QueryString
+
+import datetime
+
+class TestReportsBasic(ReportsSetup):
+    @classmethod
+    def setup_class( cls ):
+        ReportsSetup.setup_class.im_func(cls)
+        cls.create_session_event()
+        cls.create_session_event(pl_endp = { "c_server_port" : ReportsSetup.DEFAULT_SERVER_PORT + 1 })
+        cls.create_session_event(pl_endp = { "c_server_port" : ReportsSetup.DEFAULT_SERVER_PORT + 2 })
+        cls.create_session_event(pl_endp = { "c_client_addr" : "10.0.0.1" })
+        cls.create_session_event(pl_endp = { "c_client_addr" : "10.0.0.2" })
+        cls.create_session_event(pl_endp = { "s_client_addr" : "10.0.0.2" })
+
+        timestamp = datetime.datetime.now().replace( hour = 3, minute = 10 ) - datetime.timedelta( days = 1 )
+        end_timestamp = timestamp + datetime.timedelta( minutes = 2 )
+        cls.create_session_event(pl_endp = { "time_stamp" : QueryString("TIMESTAMP '%s'" % timestamp )},
+                                 pl_stats = { "time_stamp" : QueryString("TIMESTAMP '%s'" % end_timestamp )})
+        
+        timestamp = timestamp.replace( hour = 3, minute = 11 )
+        end_timestamp = timestamp + datetime.timedelta( minutes = 2 )
+        cls.create_session_event(pl_endp = { "time_stamp" : QueryString("TIMESTAMP '%s'" % timestamp )},
+                                 pl_stats = { "time_stamp" : QueryString("TIMESTAMP '%s'" % end_timestamp )})
+
+        timestamp = timestamp.replace( hour = 3, minute = 11, second = ((timestamp.second + 1 ) % 60))
+        end_timestamp = timestamp + datetime.timedelta( minutes = 2 )
+        cls.create_session_event(pl_endp = { "time_stamp" : QueryString("TIMESTAMP '%s'" % timestamp )},
+                                 pl_stats = { "time_stamp" : QueryString("TIMESTAMP '%s'" % end_timestamp )})
+
+        timestamp = timestamp.replace( hour = 3, minute = 14 )
+        end_timestamp = timestamp + datetime.timedelta( minutes = 2 )
+        cls.create_session_event(pl_endp = { "time_stamp" : QueryString("TIMESTAMP '%s'" % timestamp )},
+                                 pl_stats = { "time_stamp" : QueryString("TIMESTAMP '%s'" % end_timestamp )})
+
+
+        ## This one should not be in todays report
+        timestamp = timestamp.replace( hour = 3, minute = 14 ) - datetime.timedelta( days = 2 )
+        end_timestamp = timestamp + datetime.timedelta( minutes = 2 )
+        cls.create_session_event(pl_endp = { "time_stamp" : QueryString("TIMESTAMP '%s'" % timestamp )},
+                                 pl_stats = { "time_stamp" : QueryString("TIMESTAMP '%s'" % end_timestamp )})
+
+        ## This one should be pruned
+        timestamp = timestamp - datetime.timedelta( days = 40 )
+        end_timestamp = timestamp + datetime.timedelta( minutes = 2 )
+        cls.create_session_event(pl_endp = { "time_stamp" : QueryString("TIMESTAMP '%s'" % timestamp )},
+                                 pl_stats = { "time_stamp" : QueryString("TIMESTAMP '%s'" % end_timestamp )})
+
+        ## This one should not be in this report (it occurs today.)
+        timestamp = datetime.datetime.now()
+        end_timestamp = timestamp + datetime.timedelta( minutes = 2 )
+        cls.create_session_event(pl_endp = { "time_stamp" : QueryString("TIMESTAMP '%s'" % timestamp )},
+                                 pl_stats = { "time_stamp" : QueryString("TIMESTAMP '%s'" % end_timestamp )})
+
+
+        
+        cls.run_reports()
+
+    def test_session_totals_totals( self ):
+        ## 10 sessions created, one 
+        yield self.check_session_totals, "", 11
+        ## Truncate the time
+        timestamp = ReportsSetup.DEFAULT_TIMESTAMP.replace( second = 0, microsecond = 0 )
+        yield self.check_session_totals, "trunc_time=timestamp '%s'" % timestamp, 6
+        timestamp = datetime.datetime.now().replace( hour = 3, minute = 10, second = 0, microsecond = 0 ) - datetime.timedelta( days = 1 )
+        yield self.check_session_totals, "trunc_time=timestamp '%s'" % timestamp, 1
+        timestamp = timestamp.replace( hour = 3, minute = 11 )
+        yield self.check_session_totals, "trunc_time=timestamp '%s'" % timestamp, 2
+        timestamp = timestamp.replace( hour = 3, minute = 14 )
+        yield self.check_session_totals, "trunc_time=timestamp '%s'" % timestamp, 1
+        
+    def check_session_totals( self, params, count ):
+        curs = self.get_conn().cursor()
+        if ( params != None ) and ( len( params ) > 0):
+            params = "WHERE %s" % params
+        else:
+            params = ""
+
+        query = "SELECT SUM(new_sessions) FROM reports.session_totals %s" % params
+        print "execute: %s" % query
+            
+        curs.execute( query )
+
+        num_sessions = curs.fetchone()[0]
+        if ( num_sessions == None ):
+            num_sessions = 0
+            
+        num_sessions = int(num_sessions)
+        assert num_sessions == count
