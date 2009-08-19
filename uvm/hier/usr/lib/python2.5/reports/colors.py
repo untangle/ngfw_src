5b4926c23638abfca07bfd502811b5c132dc558b
Aaron Read <amread@untangle.com>
2009-08-19 23:52:39 +0000

    color changes
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@24287 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/lib/python2.5/reports/colors.py
+++ uvm/hier/usr/lib/python2.5/reports/colors.py
@@ -21,15 +21,15 @@ import reportlab.lib.colors as colors
 from reportlab.lib.colors import HexColor
 
 badness = HexColor(0x3333FF)
+detected=HexColor(0xFF8000)
 goodness = HexColor(0x00DD00)
 blue = HexColor(0x3333FF)
 
-color_palette = [HexColor(0x00CC00), HexColor(0x0066B3), HexColor(0xFF8000),
-                 HexColor(0xFFCC00), HexColor(0x330099), HexColor(0x990099),
-                 HexColor(0xCCFF00), HexColor(0x808080), HexColor(0x008F00),
-                 HexColor(0x00487D), HexColor(0xAA0000), HexColor(0xBEBEBE),
-                 HexColor(0x8FB300), HexColor(0x80FF80), HexColor(0x80C9FF),
-                 HexColor(0xFFC080), HexColor(0xFFE680), HexColor(0xAA80FF),
-                 HexColor(0xEE00CC), HexColor(0xFF8080), HexColor(0x666600),
-                 HexColor(0xFFBFFF), HexColor(0x00FFCC), HexColor(0xCC6699),
-                 HexColor(0x999900)]
+color_palette = [HexColor(0x00CC00), HexColor(0x0066B3), HexColor(0xFFCC00),
+                 HexColor(0x330099), HexColor(0x990099), HexColor(0xCCFF00),
+                 HexColor(0x808080), HexColor(0x008F00), HexColor(0x00487D),
+                 HexColor(0xAA0000), HexColor(0x8FB300), HexColor(0x80FF80),
+                 HexColor(0x80C9FF), HexColor(0xFFC080), HexColor(0xFFE680),
+                 HexColor(0xAA80FF), HexColor(0xEE00CC), HexColor(0xFF8080),
+                 HexColor(0x666600), HexColor(0xFFBFFF), HexColor(0x00FFCC),
+                 HexColor(0xCC6699), HexColor(0x999900)]
