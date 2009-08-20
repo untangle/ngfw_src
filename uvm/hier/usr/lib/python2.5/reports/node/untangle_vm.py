58cf473fed758f743e6464008f5e256e794c953e
Sebastien Delafond <sdelafond@gmail.com>
2009-08-20 16:39:59 +0000

    Run, *then* commit.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@24296 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/lib/python2.5/reports/node/untangle_vm.py
+++ uvm/hier/usr/lib/python2.5/reports/node/untangle_vm.py
@@ -69,9 +69,9 @@ class UvmNode(Node):
                        [Column('hname', 'text'),
                         Column('uid', 'text'),
                         Column('client_intf', 'smallint'),
-                        Column('c_server_port', 'int4')],
-                        Column('c_client_port', 'int4')],                       
-                       [Column('new_sessions', 'bigint', 'count(*)'),
+                        Column('c_server_port', 'int4'),
+                        Column('c_client_port', 'int4'),                       
+                        Column('new_sessions', 'bigint', 'count(*)'),
                         Column('s2c_bytes', 'bigint', 'sum(p2c_bytes)'),
                         Column('c2s_bytes', 'bigint', 'sum(p2s_bytes)')])
         reports.engine.register_fact_table(ft)
