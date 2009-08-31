42027f4530e308435a8c6088bd8f57f1103c4279
Sebastien Delafond <sdelafond@gmail.com>
2009-08-31 12:38:20 +0000

    Restart the reports daemon process when we ship new nodes that have reporting (Closes: #6776).
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@24369 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ uvm/hier/usr/share/untangle/bin/untangle-reports-wrapper.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+RESTART_FILE=/etc/untangle/reports-to-restart
+DAEMON=/usr/lib/python2.5/reports/server.py
+LOGFILE=/var/log/uvm/report.log
+
+pid=foo
+
+while true ; do
+
+  if ! ps -p $pid > /dev/null ; then
+    $DAEMON >> $LOGFILE 2>&1 &
+    pid=$!
+  fi
+
+  sleep 30
+  if [ -f $RESTART_FILE ] ; then
+    rm -f $RESTART_FILE
+    pkill -f $DAEMON
+    exit 0
+  fi
+
+done
