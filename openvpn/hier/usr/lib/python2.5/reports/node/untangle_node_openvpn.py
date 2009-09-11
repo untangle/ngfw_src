5d3c49f4febab66ffa6e7c64f146990fbe8ef3df
Sebastien Delafond <sdelafond@gmail.com>
2009-09-11 16:36:57 +0000

    Closes: #6859.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@24450 8ebf8b4e-06f3-0310-961f-89f74618be54

--- openvpn/hier/usr/lib/python2.5/reports/node/untangle_node_openvpn.py
+++ openvpn/hier/usr/lib/python2.5/reports/node/untangle_node_openvpn.py
@@ -254,6 +254,6 @@ FROM reports.n_openvpn_connect_totals
 WHERE trunc_time >= %s AND trunc_time < %s""" % (DateFromMx(start_date),
                                                  DateFromMx(end_date)))
 
-        return sql + "ORDER BY time_stamp"
+        return sql + "ORDER BY trunc_time"
     
 reports.engine.register_node(OpenVpn())
