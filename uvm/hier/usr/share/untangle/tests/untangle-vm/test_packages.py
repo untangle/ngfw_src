41d48e927a65a64faaf913ec3f00ee407f6c9654
Sebastien Delafond <sdelafond@gmail.com>
2009-10-08 13:58:25 +0000

    Test for the presence of various essential packages.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@24738 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/share/untangle/tests/untangle-vm/test_packages.py
+++ uvm/hier/usr/share/untangle/tests/untangle-vm/test_packages.py
@@ -19,20 +19,24 @@ class AptSetup(object):
       return name.replace('%3a', ':')
 
 class TestApt(AptSetup):
-#   def test_sections(self):
-#     # make sure main, hades & upstream are listed
+  PKGS_MAIN = ("untangle-libitem-opensource-package", "untangle-vm")
+  PKGS_PREMIUM = ("untangle-libitem-professional-package", "untangle-support-agent")
+  PKGS_UPSTREAM = ("spamassassin", "clamav", "ssh")
 
-  PKG_NAME = "untangle-libitem-opensource-package"
-
-  def test_opensource_libitem(self):
-
-    package          = self.cache[self.PKG_NAME]
+  def test_package(self, pkg):
+    package          = self.cache[pkg]
     package._lookupRecord(True)
     record           = package._records.Record
     section          = apt_pkg.ParseSection(record)
     fileName         = self.sanitizeName(section["Filename"])
-    versionedPackage = self.depcache.GetCandidateVer(self.pkgCache[self.PKG_NAME])
+    versionedPackage = self.depcache.GetCandidateVer(self.pkgCache[pkg])
     packageFile      = versionedPackage.FileList[0][0]
     indexFile        = self.cache._list.FindIndex(packageFile)
     url              = indexFile.ArchiveURI(fileName)
-    urllib.urlretrieve(url, "/dev/null") # try to get it
+
+def pytest_generate_tests(metafunc):
+  if "pkg" in metafunc.funcargnames:
+    for pkgs in (TestApt.PKGS_MAIN, TestApt.PKGS_PREMIUM, TestApt.PKGS_UPSTREAM):
+      for pkg in pkgs:
+        metafunc.addcall(funcargs=dict(pkg=pkg))
+
