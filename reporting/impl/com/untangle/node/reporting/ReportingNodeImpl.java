4075832de3aefa80b35c705be54c30910fdffed8
Sebastien Delafond <sdelafond@gmail.com>
2009-11-10 15:20:50 +0000

    Upon installing the reporting node, sign up admin users for emailed reports (Closes: #7024).
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@25114 8ebf8b4e-06f3-0310-961f-89f74618be54

--- reporting/impl/com/untangle/node/reporting/ReportingNodeImpl.java
+++ reporting/impl/com/untangle/node/reporting/ReportingNodeImpl.java
@@ -17,6 +17,9 @@
  */
 package com.untangle.node.reporting;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 import com.untangle.uvm.node.Validator;
 import com.untangle.uvm.util.TransactionWork;
 import com.untangle.uvm.LocalUvmContextFactory;
@@ -76,15 +79,8 @@ public class ReportingNodeImpl extends AbstractNode implements ReportingNode
                     q.setParameter("tid", getTid());
                     settings = (ReportingSettings)q.uniqueResult();
 
-                    boolean bSave = false;
-
                     if (null == settings) {
-                        // should never happen
                         settings = initSettings();
-                        bSave = true;
-                    }
-
-                    if (true == bSave) {
                         s.merge(settings);
                     }
 
@@ -101,11 +97,6 @@ public class ReportingNodeImpl extends AbstractNode implements ReportingNode
         if (this.settings == null) {
             String[] args = {""};
             postInit(args);
-        }
-
-        /* If null, pull these from mail settings. */
-        if ( this.settings.getReportingUsers() == null ) {
-            loadReportingUsers(this.settings);
         }
     }
 
@@ -140,27 +131,43 @@ public class ReportingNodeImpl extends AbstractNode implements ReportingNode
         setReportingSettings((ReportingSettings)settings);
     }
 
+    /* 
+     * Add admin users to the list of reporting users, and set them up for
+     * emailed reports.
+     *
+     * The list is maintained in the DB as a comma-separated string, so we
+     * split it first, deal with the resulting HashSet, and spit out another
+     * comma-separated string at the very end of this method. */
     private void loadReportingUsers(ReportingSettings s)
     {
         RemoteAdminManager adminManager = LocalUvmContextFactory.context().adminManager();
         String reportEmail = adminManager.getMailSettings().getReportEmail();
-        if ( reportEmail == null ) {
-            reportEmail = "";
+	HashSet<String> res = new HashSet();
+        if ((reportEmail != null) && (!reportEmail.isEmpty())) {
+	    reportEmail = reportEmail.trim();
+	    res.addAll(Arrays.asList(reportEmail.split(",")));
+	}
+
+        /* add in all other admins with an email */
+	String email;
+        for (User user : adminManager.getAdminSettings().getUsers()) {
+	    email = user.getEmail();
+	    if ((email != null) && (!email.equals("[no email]")
+				    && (!email.isEmpty()))) {
+		res.add(email);
+	    }
         }
-        
-        reportEmail = reportEmail.trim();
-        
-        /* Add in any other users that have reporting on and write access off. */
-        for ( User user : adminManager.getAdminSettings().getUsers()) {
-            if ( !user.getHasWriteAccess() && user.getHasReportsAccess()) {
-                if ( reportEmail.length() == 0 ) {
-                    reportEmail += ",";
-                }
-                
-                reportEmail += user.getName();
-            }
-        }
-        
+
+	// assemble back the comma-separated string
+	String[] rea = res.toArray(new String[0]);
+	reportEmail = rea[0];
+	for (int i = 1 ; i < rea.length ; i++)
+	    reportEmail += "," + rea[i];
+
+	// modify the passed-in ReportingSettings, so the users we gathered
+	// are now known to the report node
         s.setReportingUsers(reportEmail);
+	// also sign them up for emailed reports
+	adminManager.getMailSettings().setReportEmail(reportEmail);
     }
 }
