6416806558a263713d948d7297bb898d293a49b1
Sebastien Delafond <sdelafond@gmail.com>
2009-11-12 15:06:57 +0000

    During UVM upgrades, automatically redirect /webui-bound clients to an AJAX upgrade page, that tails -f some pre-selected logs.
    Once the upgrade is complete, automatically redirect them (without the need
    for the user to hit "refresh") to the UVM login page.
    
    This was tested with FF3.5, Opera 10.01, and Konqueror 4.3.2.
    
    Closes: #7094.
    
    
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@25137 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ uvm/hier/usr/share/untangle/web/upgrade/uvm-upgrade.js
@@ -0,0 +1,49 @@
+function doIt() {
+    request.open("GET", "uvm-upgrade.log", false);
+    request.send(null);
+    if (request.readyState == 4) {
+	if (request.status != 404 ) {
+	    var lines = request.responseText.split("\n");
+	    var lastLog = "";
+	    for (i=Math.max(0,lines.length-30) ; i < lines.length-1; i++) {
+		lastLog += lines[i] + "<br>";
+	    }
+	    document.getElementById("upgrade").innerHTML = lastLog;
+	} else {
+	    location.href = "/webui";
+	}
+    }
+}
+
+var request = false;
+/*@cc_on @*/
+/*@if (@_jscript_version >= 5)
+// JScript gives us Conditional compilation, we can cope with old IE versions.
+// and security blocked creation of the objects.
+ try {
+  request = new ActiveXObject("Msxml2.REQUEST");
+ } catch (e) {
+  try {
+   request = new ActiveXObject("Microsoft.REQUEST");
+  } catch (E) {
+   request = false;
+  }
+ }
+@end @*/
+
+if (!request && typeof XMLHttpRequest != 'undefined') {
+    try {
+	request = new XMLHttpRequest();
+    } catch (e) {
+	request=false;
+    }
+}
+if (!request && window.createRequest) {
+    try {
+	request = window.createRequest();
+    } catch (e) {
+	request=false;
+    }
+}
+
+setInterval("doIt()", 1000); // every second
