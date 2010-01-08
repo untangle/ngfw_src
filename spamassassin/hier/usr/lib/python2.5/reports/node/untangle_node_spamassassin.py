f444da7b7d98b914d9ea7d275668d7908b8d1113
Dirk Morris <dmorris@untangle.com>
2010-01-08 22:02:05 +0000

    rename ham (Closes: #6976)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@25414 8ebf8b4e-06f3-0310-961f-89f74618be54

--- spamassassin/hier/usr/lib/python2.5/reports/node/untangle_node_spamassassin.py
+++ spamassassin/hier/usr/lib/python2.5/reports/node/untangle_node_spamassassin.py
@@ -20,4 +20,4 @@ import reports.node.untangle_base_spam
 
 _ = reports.i18n_helper.get_translation('untangle-node-spamassassin').lgettext
 
-reports.engine.register_node(reports.node.untangle_base_spam.SpamBaseNode('untangle-node-spamassassin', 'Spam Assassin', 'sa', 'SpamAssassin', _('spam'), _('ham'), _("Hourly Spam Rate"), _("Daily Spam Rate"), _("Top Ten Spammed")))
+reports.engine.register_node(reports.node.untangle_base_spam.SpamBaseNode('untangle-node-spamassassin', 'Spam Assassin', 'sa', 'SpamAssassin', _('spam'), _('clean'), _("Hourly Spam Rate"), _("Daily Spam Rate"), _("Top Ten Spammed")))
