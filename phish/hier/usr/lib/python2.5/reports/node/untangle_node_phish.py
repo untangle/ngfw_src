72ebcf16b71ebae4c065fe5f71fae303e4ac60b3
Dirk Morris <dmorris@untangle.com>
2010-02-03 22:47:56 +0000

    capitalize spam and phish (Comment: #7343)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@25692 8ebf8b4e-06f3-0310-961f-89f74618be54

--- phish/hier/usr/lib/python2.5/reports/node/untangle_node_phish.py
+++ phish/hier/usr/lib/python2.5/reports/node/untangle_node_phish.py
@@ -20,4 +20,4 @@ import reports.node.untangle_base_spam
 
 _ = reports.i18n_helper.get_translation('untangle-node-phish').lgettext
 
-reports.engine.register_node(reports.node.untangle_base_spam.SpamBaseNode('untangle-node-phish', 'Phish', 'phish', 'Clam', _('phish'), _('clean'), _('Hourly Phish Rate'), _('Daily Phish Rate'), _('Top Ten Phishing Victims')))
+reports.engine.register_node(reports.node.untangle_base_spam.SpamBaseNode('untangle-node-phish', 'Phish', 'phish', 'Clam', _('Phish'), _('Clean'), _('Hourly Phish Rate'), _('Daily Phish Rate'), _('Top Ten Phishing Victims')))
