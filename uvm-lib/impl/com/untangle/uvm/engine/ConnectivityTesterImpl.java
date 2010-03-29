b9694327c1fc98c502095dcc80c2236b36336f4c
Marc Runkel <marc@runkel.org>
2010-03-29 17:16:12 +0000

    Updated 64.2.3.42 to new IP address 74.123.28.42 (64.2.3.42 hasn't worked in years, but 64.2.3.0/24 is going away mid-april for good.)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@26137 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm-lib/impl/com/untangle/uvm/engine/ConnectivityTesterImpl.java
+++ uvm-lib/impl/com/untangle/uvm/engine/ConnectivityTesterImpl.java
@@ -45,7 +45,7 @@ class RemoteConnectivityTesterImpl implements RemoteConnectivityTester
     private static final String TEST_HOSTNAME_DOMAIN  = "untangle.com";
 
     /* Backup IP address to use if DNS fails */
-    private static final String BACKUP_ADDRESS_STRING = "64.2.3.42";
+    private static final String BACKUP_ADDRESS_STRING = "74.123.28.42";
 
     /* Address to use if the DNS lookup fails */
     private static final InetAddress BACKUP_ADDRESS;
