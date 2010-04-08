c429e1de49476f3d1efbafbac672b19499572160
Sebastien Delafond <sdelafond@gmail.com>
2010-04-08 15:53:58 +0000

    Re-factoring a bit.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@26207 8ebf8b4e-06f3-0310-961f-89f74618be54

--- gtags.sh
+++ gtags.sh
@@ -2,7 +2,13 @@ echo "Warning: this file needs to be *sourced*"
 
 export GTAGSLIBPATH=../../hades/rup
 
+updateTags() {
+  (cd $1 ; /usr/bin/find . -name *.java -a ! -path "*/dist/*" -a ! -path '*/downloads/*' | gtags -i -f -)
+}
+
 if [ $# -gt 0 ] ; then
-  /usr/bin/find . -name *.java -a ! -path "*/dist/*" -a ! -path '*/downloads/*' | gtags -i -f -
-  (cd $GTAGSLIBPATH ; /usr/bin/find . -name *.java -a ! -path "*/dist/*" -a ! -path '*/downloads/*' | gtags -i -f -)
+  # FIXME: split GTAGSLIBPATH on '.', and call updateTags on every
+  # component
+  updateTags .
+  updateTags $GTAGSLIBPATH
 fi
