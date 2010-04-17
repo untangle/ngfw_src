7ea230164cf7f8e5ed1a1e0ac7dc4a3d1f4103ff
Sebastien Delafond <sdelafond@gmail.com>
2010-04-17 07:53:05 +0000

    Proper GTAGSLIBPATH splitting.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@26333 8ebf8b4e-06f3-0310-961f-89f74618be54

--- gtags.sh
+++ gtags.sh
@@ -1,14 +1,14 @@
 echo "Warning: this file needs to be *sourced*"
 
-export GTAGSLIBPATH=../../hades/rup
+export GTAGSLIBPATH=.:${1:-../../hades/rup}
 
 updateTags() {
   (cd $1 ; /usr/bin/find . -name *.java -a ! -path "*/dist/*" -a ! -path '*/downloads/*' | gtags -i -f -)
 }
 
-if [ $# -gt 0 ] ; then
-  # FIXME: split GTAGSLIBPATH on '.', and call updateTags on every
-  # component
-  updateTags .
-  updateTags $GTAGSLIBPATH
+# FIXME: use getopt
+if [ "$2" = "-u" ] || [ "$1" = "-u" ] ; then
+  echo $GTAGSLIBPATH | perl -pe 's/:/\n/g' | while read path ; do
+    updateTags "$path"
+  done
 fi
