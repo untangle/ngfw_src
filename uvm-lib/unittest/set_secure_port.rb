2692187abba977d30883be6bdb9394231c12fea1
Dirk Morris <dmorris@untangle.com>
2010-04-17 00:07:28 +0000

    new set_secure_port test
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@26331 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm-lib/unittest/set_secure_port.rb
+++ uvm-lib/unittest/set_secure_port.rb
@@ -5,12 +5,6 @@ address_settings =  nm.getAddressSettings()
 puts "Current HTTPS port is #{address_settings["httpsPort"]}"
 address_settings["httpsPort"] = ARGV[0].to_i
 
-if address_settings["publicAddress"] == nil
-   address_settings.delete("publicAddress")
-end
-if address_settings["publicIPaddr"] == nil
-   address_settings.delete("publicIPaddr")
-end
 nm.setAddressSettings( address_settings )
 
 address_settings =  nm.getAddressSettings()
