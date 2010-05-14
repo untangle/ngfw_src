47ee8279b7a4a0432f9ed35bf656c63157361f15
Sebastien Delafond <sdelafond@gmail.com>
2010-05-14 05:20:40 +0000

    Use getopts.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@26612 8ebf8b4e-06f3-0310-961f-89f74618be54

--- gtags.sh
+++ gtags.sh
@@ -1,13 +1,24 @@
 echo "Warning: this file needs to be *sourced*"
 
-export GTAGSLIBPATH=.:${1:-../../hades/rup}
+DEFAULT_RUP=../../hades/rup
+
+while getopts "u" flag ; do
+  if [ "$flag" = "u" ] ; then
+    UPDATE=1
+  fi
+done
+ 
+rup=${1:-$DEFAULT_RUP}
+
+export GTAGSLIBPATH=.:${rup}
 
 updateTags() {
-  (cd $1 ; /usr/bin/find . -name *.java -a ! -path "*/dist/*" -a ! -path '*/downloads/*' | gtags -i -f -)
+  pushd $1
+  /usr/bin/find . -name *.java -a ! -path "*/dist/*" -a ! -path '*/downloads/*' | gtags -i -f -
+  popd
 }
 
-# FIXME: use getopt
-if [ "$2" = "-u" ] || [ "$1" = "-u" ] ; then
+if [ -n "$UPDATE" ] ; then
   echo $GTAGSLIBPATH | perl -pe 's/:/\n/g' | while read path ; do
     updateTags "$path"
   done
