5d3534191908ac8de0fc53e1741694588f393aa7
Sebastien Delafond <sdelafond@gmail.com>
2010-06-22 10:35:59 +0000

    [reports] New implementation of stacked bars charts.
      * use TimeTableXYDataset instead of TimeSeriesCollection to store
        the data.
      * use StackedXYBarRenderer instead of XYBarRenderer to create the
        chart.
      * format the date axis the same way we do TimeSeriesCharts.
      * will need some refactoring later on, as there is now more
        duplicated code from TimeSeriesChart.java
    
    Comment: #7713.
    
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@26967 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm-lib/api/com/untangle/uvm/reports/StackedBarChart.java
+++ uvm-lib/api/com/untangle/uvm/reports/StackedBarChart.java
@@ -42,6 +42,7 @@ import java.io.Reader;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -50,25 +51,35 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.DateAxis;
-// import org.jfree.chart.axis.CategoryAxis;
-// import org.jfree.chart.axis.CategoryLabelPositions;
-// import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.axis.DateTickUnit;
+import org.jfree.chart.axis.DateTickUnitType;
+import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYBarRenderer;
+import org.jfree.chart.renderer.xy.StackedXYBarRenderer;
 import org.jfree.chart.title.TextTitle;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.xy.XYBarDataset;
 import org.jfree.data.io.CSV;
 import org.jfree.data.time.Minute;
+import org.jfree.data.time.SimpleTimePeriod;
+import org.jfree.data.time.TimePeriod;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.time.TimeTableXYDataset;
+
+import com.untangle.uvm.util.DateTruncator;
 
 public class StackedBarChart extends Plot
 {
     private static final DateFormat DF = new SimpleDateFormat("MMM-dd");
 
     private static final DateFormat HF = new SimpleDateFormat("HH");
+
+    // duplicated from com.uvm.engine.ReportingManagerImpl
+    private static final DateFormat DATE_FORMAT =
+        new SimpleDateFormat("yyyy-MM-dd");
 
     private final String xLabel;
     private final String yLabel;
@@ -85,6 +96,90 @@ public class StackedBarChart extends Plot
         this.majorFormatter = majorFormatter;
     }
 
+    private int getInterval(String timeStr) {
+    	try {
+    		DF.parse(timeStr);
+                return 3;
+    	} catch (java.text.ParseException exn) {
+            try{
+                HF.parse(timeStr);
+                return 1;
+            } catch (java.text.ParseException exn2) {
+                logger.warn("Couldn't get time interval for row key: " + timeStr, exn2);
+            }
+    	}
+    	return 1;
+    }
+
+    private void formatDateAxis(DateAxis da, File csvPath) {
+        logger.debug("About to format date axis for: " + csvPath);
+        File dir = csvPath.getParentFile().getParentFile();
+
+        String maxString = dir.getParentFile().getName();
+	Date max = null;
+	try { // 1st, assume this is a generic report
+	    max = DATE_FORMAT.parse(maxString);
+	} catch (java.text.ParseException exn) {
+            try { // try to see if it's a per-{user,host,email} report
+                dir = dir.getParentFile().getParentFile();
+                maxString = dir.getParentFile().getName();
+                max = DATE_FORMAT.parse(maxString);
+            } catch (java.text.ParseException e) {
+                logger.warn("Couldn't parse time for: " + maxString, e);
+                return;
+            }
+	}
+
+        String length = dir.getName();
+        String[] split = length.split("-");
+        int numDays = Integer.parseInt(split[0]);
+
+        Calendar c = Calendar.getInstance();
+        c.setTime(max);
+        c.add(Calendar.DATE, -numDays);
+        Date min = c.getTime();
+
+	// range in seconds
+	long range = (max.getTime() - min.getTime()) / 1000;
+
+	int trunc; // where to truncate the boundaries
+	String dateFormatStr; // the format for X-axix ticks
+	DateTickUnitType tickUnit = null;
+        int tickFrequency = -1;
+	logger.debug(min + " -> " + max + " (" + range + ")");
+	if (range < 45 * 60) { // less than 45min
+	    trunc = Calendar.MINUTE;
+	    dateFormatStr = "mm:ss";
+	} else if (range < 18 * 60 * 60) { // less than 18h
+	    trunc = Calendar.HOUR;
+	    dateFormatStr = "HH:mm";
+	} else if (range <= 24 * 60 * 60) { // less than 24h
+	    trunc = Calendar.DATE;
+	    dateFormatStr = "HH:mm";
+//             tickUnit = DateTickUnitType.HOUR;
+//             tickFrequency = 4;
+        } else if (range <= 7 * 24 * 60 * 60 ) { // less than 7 days
+	    trunc = Calendar.DATE;
+	    dateFormatStr = "MMM-d";
+            tickUnit = DateTickUnitType.DAY;
+            tickFrequency = 2;
+	} else {
+	    trunc = Calendar.DATE;
+	    dateFormatStr = "MMM-d";
+            tickUnit = DateTickUnitType.DAY;
+            tickFrequency = 7;
+        }
+
+	min = DateTruncator.truncateDate(min, trunc, true);
+	max = DateTruncator.truncateDate(max, trunc, true);
+	logger.debug("... adapted to: " + min + " -> " + max + " (tickUnit=" + tickUnit + ", tickFrequency=" + tickFrequency);
+// 	da.setMinimumDate(min);
+// 	da.setMaximumDate(max);
+
+        da.setDateFormatOverride(new SimpleDateFormat(dateFormatStr));
+        if (tickUnit != null) 
+            da.setTickUnit(new DateTickUnit(tickUnit, tickFrequency));
+    }
 
     private Date parseTimeStamp(String timeStr) 
     {
@@ -117,8 +212,7 @@ public class StackedBarChart extends Plot
 
         CategoryDataset cd = csv.readCategoryDataset(r);
 
-        //        DefaultCategoryDataset rotated = new DefaultCategoryDataset();
-        TimeSeriesCollection tsc = new TimeSeriesCollection();
+        TimeTableXYDataset dataset = new TimeTableXYDataset();
 
         List<Color> seriesColors = new ArrayList<Color>();
         for (int i = 0; i < cd.getColumnCount(); i++) {
@@ -133,74 +227,50 @@ public class StackedBarChart extends Plot
                 }
             }
 
-            TimeSeries ds = new TimeSeries(columnKey);
             for (int j = 0; j < cd.getRowCount(); j++) {
                 Comparable rowKey = cd.getRowKey(j);
                 try {
-                	Date d = parseTimeStamp((String)rowKey);
+                    int interval = getInterval((String)rowKey);
+                    Date d = parseTimeStamp((String)rowKey);
                     double v = cd.getValue(rowKey, columnKey).doubleValue();
-                    ds.add(new Minute(d), v);
+
+                    Calendar cal = Calendar.getInstance();
+                    cal.setTime(d);
+                    cal.add(Calendar.HOUR, interval);
+
+                    TimePeriod period = new SimpleTimePeriod(d, cal.getTime());
+
+                    dataset.add(period, v, columnKey);
                 } catch (Exception exn) {
                     logger.warn("Bad row key: " + rowKey, exn);
                 }
             }
-            tsc.addSeries(ds);
         }
 
-//         for (int i = 0; i < cd.getColumnCount(); i++) {
-//             String columnKey = (String)cd.getColumnKey(i);
-
-//             for (int j = 0; j < cd.getRowCount(); j++) {
-//                 Comparable rowKey = cd.getRowKey(j);
-
-//                 rotated.addValue(cd.getValue(rowKey, columnKey),
-//                                  columnKey, rowKey);
-//             }
-//         }
-
         String title = getTitle();
-        // FIXME: compute bar width properly
-        JFreeChart jfChart =
-            ChartFactory.createXYBarChart(title, this.xLabel, true, this.yLabel,
-                                          new XYBarDataset(tsc, 10000000.0),
-                                          PlotOrientation.VERTICAL,
-                                          true, false, false);
 
+        XYBarRenderer renderer = new StackedXYBarRenderer();
+        renderer.setDrawBarOutline(false);
+        renderer.setShadowVisible(false);
+
+        XYPlot plot = new XYPlot(dataset,
+                                 new DateAxis(this.xLabel),
+                                 new NumberAxis(this.yLabel),
+                                 renderer);
+
+        JFreeChart jfChart = new JFreeChart(plot);      
 
         jfChart.setTitle(new TextTitle(title, TITLE_FONT));
 
-//         CategoryPlot p = (CategoryPlot)jfChart.getPlot();
-//         StackedBarRenderer renderer = (StackedBarRenderer)p.getRenderer();
-        XYPlot p = (XYPlot)jfChart.getPlot();
-        XYBarRenderer renderer = (XYBarRenderer)p.getRenderer();
-        renderer.setShadowVisible(false);
         for (int i = 0; i < seriesColors.size(); i++) {
             renderer.setSeriesPaint(i, seriesColors.get(i));
         }
 
-//         for (String key : colors.keySet()) {
-//             int i = rotated.getRowIndex(key);
-//             String colorStr = colors.get(key);
-//             if (null != colorStr) {
-//                 try {
-//                     Color c = Color.decode("0x" + colorStr);
-//                     renderer.setSeriesPaint(i, c);
-//                 } catch (NumberFormatException exn) {
-//                     logger.warn("could not decode color: " + colorStr, exn);
-//                 }
-//             }
-//         }
-
-        p.getRangeAxis().setLabelFont(AXIS_FONT);
-        DateAxis da = (DateAxis)p.getDomainAxis();
+        plot.getRangeAxis().setLabelFont(AXIS_FONT);
+        DateAxis da = (DateAxis)plot.getDomainAxis();
         da.setLabelFont(AXIS_FONT);
 
-	//formatDateAxis(da, f);
-
-//         CategoryAxis domainAxis = p.getDomainAxis();
-//         domainAxis.setCategoryLabelPositions(CategoryLabelPositions.DOWN_45);
-//         p.getRangeAxis().setLabelFont(AXIS_FONT);
-//         p.getDomainAxis().setLabelFont(AXIS_FONT);
+	formatDateAxis(da, f);
 
         ChartUtilities.saveChartAsPNG(new File(reportBase + "/" + imageUrl),
                                       jfChart, CHART_WIDTH, CHART_HEIGHT,
