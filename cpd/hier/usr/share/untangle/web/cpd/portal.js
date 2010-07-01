825baabc2aaf459de100dc17d95d3749a0820a1d
Karthik Ram <kram@untangle.com>
2010-07-01 18:48:24 +0000

    closes: #7723
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@27059 8ebf8b4e-06f3-0310-961f-89f74618be54

--- cpd/hier/usr/share/untangle/web/cpd/portal.js
+++ cpd/hier/usr/share/untangle/web/cpd/portal.js
@@ -154,16 +154,20 @@ function showLogoutPopup(){
     	window.setTimeout(function()
             {
                 if(_hasPopupBlocker(_popup) || popupopened == false){
-                    /* In a future release, this should not be an "alert" - this should instead be a "div based dialog"*/
-                    alert('The logout window was blocked by the browser\'s pop-up blocker.\nTo display the logout window, please adjust the pop-up blocker settings. \nTo proceed to site you wanted to access, click OK.');                    
+                    TINY.box.show('<div>The logout window was blocked by the browser\'s pop-up blocker.<br/><br/><A href="#" onclick="return showLogoutWindowAndRedirect()" style="font-weight:bold">Show Logout Window and Continue to Site</a>.</div');                
                 }else{    
                     _popup.blur();                
                     window.focus();                       
                     _popup.focus();
                 }
-                redirectUser();
             },3000);     
     }
+}
+function showLogoutWindowAndRedirect(){
+    var _top  = 5,_left = screen.width-315;
+    _popup = window.open("logout.php","cpd_logout", "height=90,width=300,status=no,toolbar=no,address=no,menubar=no,location=no,top="+_top+",left="+_left);
+    redirectUser();
+    return false;    
 }
 function redirectUser()
 {
@@ -323,3 +327,171 @@ var BrowserDetect = {
 
 };
 BrowserDetect.init();
+var TINY={};
+
+function T$(i){return document.getElementById(i)}
+TINY.setStyle = function(o){
+    if(o.id=='tinybox'){
+        o.style.position = 'absolute';
+        o.style.display = 'none';
+        o.style.padding = '10px';
+        o.style.background = '#FFF';
+        o.style.border = '10px solid #339900';
+        o.style.zIndex = 2000;
+    }else if(o.id =='tinymask'){
+        o.style.position = 'absolute';
+        o.style.display = 'none';
+        o.style.top = 0;
+        o.style.left = 0;
+        o.style.height = '100%';
+        o.style.width = '100%';
+        o.style.border = '1px solid #555';
+        o.style.background = '#000';
+        o.style.zIndex = 1500;
+    }else if (o.is = 'tinycontent'){
+        o.style.background = '#FFF';
+    }else{
+        Unt.db('id doesn not match');
+    }
+}
+/**
+ *  TINY - Standalone Popup by Michael Leigeber
+ *  Licensed under Creative Commons 3.0 License http://creativecommons.org/licenses/by/3.0/us/ 
+ *  Free for personal and commercial use    
+ **/ 
+TINY.box=function(){
+	var p,m,b,fn,ic,iu,iw,ih,ia,f=0;
+	return{
+		show:function(c,u,w,h,a,t){
+			if(!f){
+				p=document.createElement('div'); p.id='tinybox';
+				TINY.setStyle(p);
+				m=document.createElement('div'); m.id='tinymask';
+				TINY.setStyle(m);
+				b=document.createElement('div'); b.id='tinycontent';
+				TINY.setStyle(b);
+				document.body.appendChild(m); document.body.appendChild(p); p.appendChild(b);
+				m.onclick=TINY.box.hide; window.onresize=TINY.box.resize; f=1
+			}
+			if(!a&&!u){
+				p.style.width=w?w+'px':'auto'; p.style.height=h?h+'px':'auto';
+				p.style.backgroundImage='none'; b.innerHTML=c
+			}else{
+				b.style.display='none'; p.style.width=p.style.height='100px'
+			}
+			this.mask();
+			ic=c; iu=u; iw=w; ih=h; ia=a; this.alpha(m,1,80,3);
+			if(t){setTimeout(function(){TINY.box.hide()},1000*t)}
+		},
+		fill:function(c,u,w,h,a){
+			if(u){
+				p.style.backgroundImage='';
+				var x=window.XMLHttpRequest?new XMLHttpRequest():new ActiveXObject('Microsoft.XMLHTTP');
+				x.onreadystatechange=function(){
+					if(x.readyState==4&&x.status==200){TINY.box.psh(x.responseText,w,h,a)}
+				};
+				x.open('GET',c,1); x.send(null)
+			}else{
+				this.psh(c,w,h,a)
+			}
+		},
+		psh:function(c,w,h,a){
+			if(a){
+				if(!w||!h){
+					var x=p.style.width, y=p.style.height; b.innerHTML=c;
+					p.style.width=w?w+'px':''; p.style.height=h?h+'px':'';
+					b.style.display='';
+					w=parseInt(b.offsetWidth); h=parseInt(b.offsetHeight);
+					b.style.display='none'; p.style.width=x; p.style.height=y;
+				}else{
+					b.innerHTML=c
+				}
+				this.size(p,w,h)
+			}else{
+				p.style.backgroundImage='none'
+			}
+		},
+		destroy : function(){
+		    TINY.box.hide();
+		    Unt.cleanup();
+        },
+        cleanup : function(){
+		    try{
+                document.body.removeChild(p);
+                document.body.removeChild(m);
+            }catch(e){
+            
+            }			        
+        },
+		hide:function(){
+			TINY.box.alpha(p,-1,0,3);
+			Unt.cleanup();
+		},
+		resize:function(){
+			TINY.box.pos(); TINY.box.mask()
+		},
+		mask:function(){
+			m.style.height=TINY.page.total(1)+'px';
+			m.style.width=''; m.style.width=TINY.page.total(0)+'px'
+		},
+		pos:function(){
+			var t=(TINY.page.height()/2)-(p.offsetHeight/2); t=t<10?10:t;
+			p.style.top=(t+TINY.page.top())+'px';
+			p.style.left=(TINY.page.width()/2)-(p.offsetWidth/2)+'px'
+		},
+		alpha:function(e,d,a){
+			clearInterval(e.ai);
+			if(d==1){
+				e.style.opacity=0; e.style.filter='alpha(opacity=0)';
+				e.style.display='block'; this.pos()
+			}
+			e.ai=setInterval(function(){TINY.box.ta(e,a,d)},20)
+		},
+		ta:function(e,a,d){
+			var o=Math.round(e.style.opacity*100);
+			if(o==a){
+				clearInterval(e.ai);
+				if(d==-1){
+					e.style.display='none';
+					e==p?TINY.box.alpha(m,-1,0,2):b.innerHTML=p.style.backgroundImage=''
+				}else{
+					e==m?this.alpha(p,1,100):TINY.box.fill(ic,iu,iw,ih,ia)
+				}
+			}else{
+				var n=Math.ceil((o+((a-o)*.5))); n=n==1?0:n;
+				e.style.opacity=n/100; e.style.filter='alpha(opacity='+n+')'
+			}
+		},
+		size:function(e,w,h){
+			e=typeof e=='object'?e:T$(e); clearInterval(e.si);
+			var ow=e.offsetWidth, oh=e.offsetHeight,
+			wo=ow-parseInt(e.style.width), ho=oh-parseInt(e.style.height);
+			var wd=ow-wo>w?0:1, hd=(oh-ho>h)?0:1;
+			e.si=setInterval(function(){TINY.box.ts(e,w,wo,wd,h,ho,hd)},20)
+		},
+		ts:function(e,w,wo,wd,h,ho,hd){
+			var ow=e.offsetWidth-wo, oh=e.offsetHeight-ho;
+			if(ow==w&&oh==h){
+				clearInterval(e.si); p.style.backgroundImage='none'; b.style.display='block'
+			}else{
+				if(ow!=w){var n=ow+((w-ow)*.5); e.style.width=wd?Math.ceil(n)+'px':Math.floor(n)+'px'}
+				if(oh!=h){var n=oh+((h-oh)*.5); e.style.height=hd?Math.ceil(n)+'px':Math.floor(n)+'px'}
+				this.pos()
+			}
+		}
+	}
+}();
+
+TINY.page=function(){
+	return{
+		top:function(){return document.documentElement.scrollTop||document.body.scrollTop},
+		width:function(){return self.innerWidth||document.documentElement.clientWidth||document.body.clientWidth},
+		height:function(){return self.innerHeight||document.documentElement.clientHeight||document.body.clientHeight},
+		total:function(d){
+			var b=document.body, e=document.documentElement;
+			return d?Math.max(Math.max(b.scrollHeight,e.scrollHeight),Math.max(b.clientHeight,e.clientHeight)):
+			Math.max(Math.max(b.scrollWidth,e.scrollWidth),Math.max(b.clientWidth,e.clientWidth))
+		}
+	}
+}();
+
