0f5ca7484cf7ebae3efce23c1517e53e0b464fdd
Dirk Morris <dmorris@untangle.com>
2010-08-10 19:56:49 +0000

    add skeleton for bandwidth control
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/rup@27243 8ebf8b4e-06f3-0310-961f-89f74618be54

--- package.rb
+++ package.rb
@@ -6,13 +6,10 @@ guiDeps = []
 ## This is the build for the node
 license_manager = HadesBuildEnv['untangle-node-license']
 
-
 NodeBuilder.makeNode(HadesBuildEnv, 'untangle-node-license', 'license', implDeps, guiDeps )
 
 if HadesBuildEnv.isDevel
-  LicenseBuilder::License.new( license_manager, "upm-dev", "untangle-license-manager", 
-                               'untangle-node-license', LicenseBuilder::Developer )
-                               
+  LicenseBuilder::License.new( license_manager, "upm-dev", "untangle-license-manager", 'untangle-node-license', LicenseBuilder::Developer )
 end
 
 uvm_lib = BuildEnv::SRC['untangle-libuvm']
@@ -30,7 +27,5 @@ deps << uvm_lib['impl']
 
 j << JarTarget.build_target( license_manager, deps, "ext-impl", "./license/ext-impl" )
 
-
 ## Install the license manager uvm resource into the lib directory.
-HadesBuildEnv.installTarget.install_jars(j, "#{license_manager.distDirectory}/usr/share/untangle/lib",
-                                         nil, false, true)
+HadesBuildEnv.installTarget.install_jars(j, "#{license_manager.distDirectory}/usr/share/untangle/lib", nil, false, true)
