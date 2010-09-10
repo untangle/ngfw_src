17100edbaa4823d30291627ff06bdc4340c77229
Dirk Morris <dmorris@untangle.com>
2010-09-10 19:19:50 +0000

    need the notrack flag
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@27433 8ebf8b4e-06f3-0310-961f-89f74618be54

--- libnetcap/src/netcap_udp.c
+++ libnetcap/src/netcap_udp.c
@@ -428,9 +428,8 @@ static int _netcap_udp_sendto (int sock, void* data, size_t data_len, int flags,
     int                ret;
     /**
      * mark packet with:  MARK_ANTISUB + whatever packet marks are specified 
-     * MARK_NOTRACK is commented out because it appears to not be needed and it interferes with proper QoS tracking
      */
-    u_int              nfmark = ( MARK_ANTISUB | /* MARK_NOTRACK | */ ( pkt->is_marked ? pkt->nfmark : 0 )); 
+    u_int              nfmark = ( MARK_ANTISUB | MARK_NOTRACK | ( pkt->is_marked ? pkt->nfmark : 0 )); 
     
     /* if the caller uses the force flag, then override the default bits of the mark */
     if ( pkt->is_marked == IS_MARKED_FORCE_FLAG ) nfmark = pkt->nfmark;
