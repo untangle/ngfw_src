48b8d1629c53805df7d8a834765d855a37a1b429
Dirk Morris <dmorris@untangle.com>
2010-11-23 22:35:25 +0000

    update to license manager - work in progress
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/src@27910 8ebf8b4e-06f3-0310-961f-89f74618be54

--- ext-impl/com/untangle/uvm/license/LicenseManagerImpl.java
+++ ext-impl/com/untangle/uvm/license/LicenseManagerImpl.java
@@ -25,73 +25,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
+import org.apache.log4j.Logger;
 
-import com.untangle.node.util.UtLogger;
 import com.untangle.uvm.LocalUvmContextFactory;
 import com.untangle.uvm.util.Pulse;
 import com.untangle.uvm.SettingsManager;
 
 public class LicenseManagerImpl implements LicenseManager
 {
-    private static final int SELF_SIGNED_VERSION = 0x1F;
-
-    private static final String LICENSE_DIRECTORY = System.getProperty("uvm.conf.dir") + "/licenses";
-    private static final String PROPERTY_BASE = "com.untangle.uvm.license";
-    private static final String PROPERTY_PRODUCT_IDENTIFIER = PROPERTY_BASE + ".identifier";
-    private static final String PROPERTY_LICENSE_TYPE = PROPERTY_BASE + ".type";
-    /* The package that this product is in, this is for mcli extraname */
-    private static final String PROPERTY_START = PROPERTY_BASE + ".start";
-    private static final String PROPERTY_END = PROPERTY_BASE + ".end";
-    private static final String PROPERTY_KEY = PROPERTY_BASE + ".key";
-    private static final String PROPERTY_KEY_VERSION = PROPERTY_BASE + ".keyVersion";
-
-    private static final long LICENSE_SUBSCRIPTION_EXTENSION = (1000l * 60 * 60 * 24 * 365 * 8) + 835l;
-
-    /* 30 day keys */
-    private static final long OLD_TRIAL_KEY_DURATION = (1000l * 60 * 60 * 24 * 30) + 539;
-
     private static final String EXPIRED = "expired";
-
-    /* These are the various license types */
-//     /**
-//      * FIXME XXX
-//      * really we're adding random numbers of milliseconds? (278 and such)
-//      * 
-//      * This is just fucking stupid
-//      */
-//     private static enum LicenseType {
-//         TRIAL14("14 Day Trial", 1000l * 60 * 60 * 24 * 15 + 278),
-//         TRIAL("30 Day Trial", 1000l * 60 * 60 * 24 * 31 + 539),
-//         SUBSCRIPTION("Subscription", 1000l * 60 * 60 * 24 * 365 * 2 + 344),
-//         DEVELOPMENT("Untangle Development", 1000l * 60 * 60 * 24 * 60 + 960);
-
-//         private final String name;
-//         private final long duration;
-
-//         private LicenseType(String name, long duration) {
-//             this.name = name;
-//             this.duration = duration;
-//         }
-
-//         public String getName() {
-//             return this.name;
-//         }
-
-//         public long getDuration() {
-//             return this.duration;
-//         }
-//     }
-
-    private static final FileFilter LICENSE_FILE_FILTER = new FileFilter() {
-        /* untangle-license-file */
-        public boolean accept(File file) {
-            /* don't read from pipes or directories */
-            return file.isFile() && file.getName().endsWith(".ulf");
-        }
-    };
-
-    /* One day in milliseconds */
-    private static final long ONE_DAY = 1000 * 60 * 60 * 24;
 
     /* update every 2 hours, leaves an hour window */
     private static final long TIMER_DELAY = 1000 * 60 * 60 * 2;
@@ -103,17 +45,24 @@ public class LicenseManagerImpl implements LicenseManager
     private static final LicenseManagerImpl INSTANCE;
 
     /**
-     * map from the product identifier to the latest valid license available for
-     * this product
+     * Map from the product name to the latest valid license available for this product
+     * This is where the fully evaluated license are stored
+     * This map stores the evaluated (validated) licenses
      */
     private Map<String, License> licenseMap = new HashMap<String, License>();
 
-    /** list of all licenses. */
-    private LicenseSettings licenseSettings;
+    /**
+     * A list of all known licenses
+     * These are fully evaluated (validated) license
+     */
+    private List<License> licenseList = new LinkedList<License>();
 
-    /** configuration */
-    /** frequency of license updates */
-    private final long timerDelay = TIMER_DELAY;
+    /**
+     * The current settings
+     * Contains a list of all known licenses store locally
+     * Note: the licenses in the settings don't have metadata
+     */
+    private LicenseSettings settings;
 
     /**
      * Amount of time to update the license for, if the timer task dies, all
@@ -127,17 +76,12 @@ public class LicenseManagerImpl implements LicenseManager
     /** Pulse that updates the expiration dates, this is a daemon task. */
     private final Pulse pulse = new Pulse("uvm-lmi", true, task);
 
-    private final UtLogger logger = new UtLogger(getClass());
+    private final Logger logger = Logger.getLogger(getClass());
 
     private LicenseManagerImpl()
     {
-        /*
-         * This shouldn't be in properties, it is too easy to override. *
-         * this.timerDelay = Long.getLong(
-         * "com.untangle.uvm.license.timerDelay", TIMER_DELAY );
-         * this.validationPeriod = Long.getLong(
-         * "com.untangle.uvm.license.validationPeriod", VALIDATION_PERIOD );
-         */
+        this.readLicenses();
+        this.mapLicenses();
     }
 
     /**
@@ -152,21 +96,44 @@ public class LicenseManagerImpl implements LicenseManager
     @Override
     public final License getLicense(String identifier)
     {
-        License lic = this.licenseMap.get(identifier);
-        if (lic == null)
-            return new License(identifier, identifier, "Subscription", 0, 0, "invalid", 1, Boolean.FALSE);
-        return lic;
+        License license = this.licenseMap.get(identifier);
+
+        if (license != null)
+            return license;
+        
+        /**
+         * Special for development environment
+         * Assume all licenses are valid
+         * This should be removed if you want to test the licensing in the dev environment
+         */
+        if (LocalUvmContextFactory.context().isDevel()) {
+            logger.warn("Creating development license: " + identifier);
+            license = new License(identifier, identifier, "Development", 0, 9999999999l, "development", 1, Boolean.TRUE);
+            this.licenseMap.put(identifier,license);
+            return license;
+        }
+
+        logger.warn("No license found for: " + identifier + " - Creating invalid license...");
+        license = new License(identifier, identifier, "Subscription", 0, 0, "invalid", 1, Boolean.FALSE);
+        this.licenseMap.put(identifier,license);
+        return license;
     }
 
     @Override
     public final boolean hasPremiumLicense()
     {
-        return this.licenseSettings.getLicenses().size() > 0;
+        return this.settings.getLicenses().size() > 0;
     }
 
-    /* --------------------- PACKAGE --------------------- */
+    public void initializeSettings()
+    {
+        logger.info("Initializing Settings...");
 
-    /* --------------------- PRIVATE --------------------- */
+        List<License> licenses = new LinkedList<License>();
+        this.settings = new LicenseSettings(licenses);
+
+        this._saveSettings(this.settings);
+    }
 
     /**
      * Read the licenses and load them into the current settings object
@@ -178,13 +145,16 @@ public class LicenseManagerImpl implements LicenseManager
         LinkedList<License> licenses;
         
         try {
-            licenses = settingsManager.loadBasePath(LinkedList.class, System.getProperty("uvm.conf.dir"), "licenses", "licenses");
+            this.settings = settingsManager.loadBasePath(LicenseSettings.class, System.getProperty("uvm.conf.dir"), "licenses", "licenses");
         } catch (SettingsManager.SettingsException e) {
             logger.error("Unable to read license file: ", e );
             return;
         }
-        
-        this.licenseSettings = new LicenseSettings(licenses);
+
+        if (this.settings == null)
+            initializeSettings();
+
+        return;
     }
 
     /**
@@ -194,11 +164,25 @@ public class LicenseManagerImpl implements LicenseManager
     {
         /* Create a new map of all of the valid licenses */
         Map<String, License> newMap = new HashMap<String, License>();
+        LinkedList<License> newList = new LinkedList<License>();
 
-        for (License license : this.licenseSettings.getLicenses()) {
-            if (!verifyLicense(license))
-                continue;
+        for (License lic : this.settings.getLicenses()) {
 
+            /**
+             * Create a duplicate - we're about to fill in metadata
+             * But we don't want to mess with the original
+             */
+            License license = new License(lic);
+
+            /**
+             * Complete Meta-data
+             */
+            if (isLicenseValid(license)) {
+                license.setValid(Boolean.TRUE);
+            } else {
+                license.setValid(Boolean.FALSE);
+            }
+            
             String identifier = license.getName();
             License current = newMap.get(identifier);
 
@@ -207,37 +191,15 @@ public class LicenseManagerImpl implements LicenseManager
                 continue;
 
             newMap.put(identifier, license);
+            newList.add(license);
         }
 
         this.licenseMap = Collections.unmodifiableMap(newMap);
-    }
-
-    private String createSelfSignedLicenseKey(License license) throws NoSuchAlgorithmException
-    {
-        if (license.getKeyVersion() != SELF_SIGNED_VERSION)
-            return null;
-
-        MessageDigest digest = MessageDigest.getInstance("MD5");
-        String firstKey = license.getName() + "-license-"
-                + 0xDEC0DED + "-" + license.getType();
-
-        digest.reset();
-        byte[] data = digest.digest(firstKey.getBytes());
-        firstKey = toHex(data) + "\n";
-        String file = firstKey + buildLicenseFile(license);
-
-        digest.reset();
-        data = digest.digest(file.getBytes());
-        return toHex(data);
-    }
-
-    private boolean verifyLicense(License license)
-    {
-        return verifyLicense(license, true);
+        this.licenseList = Collections.unmodifiableList(newList);
     }
 
     @SuppressWarnings("fallthrough")
-    private boolean verifyLicense(License license, boolean verifyExpiration)
+    private boolean isLicenseValid(License license)
     {
         int version = license.getKeyVersion();
 
@@ -249,7 +211,7 @@ public class LicenseManagerImpl implements LicenseManager
             return false;
         }
 
-        if ((license.getEnd() < now) && verifyExpiration) {
+        if ((license.getEnd() < now)) {
             logger.warn( "The license: " + license + " has already expired." );
             return false;
         }
@@ -281,7 +243,7 @@ public class LicenseManagerImpl implements LicenseManager
 //         }
 
         switch (version) {
-        case SELF_SIGNED_VERSION:
+        case 1:
             // XXX
             // since there is no a single fucking line of documentation
             // this will have to do until 8.1
@@ -308,34 +270,6 @@ public class LicenseManagerImpl implements LicenseManager
         }
     }
 
-    private String buildLicenseFile(License license)
-    {
-        StringBuilder sb = new StringBuilder();
-        /*
-         * just a little string to make it slightly harder to guess the
-         * algorithm
-         */
-        int version = license.getKeyVersion();
-        switch (version) {
-        case SELF_SIGNED_VERSION:
-            /* presently the only one supported, fallthrough */
-
-        default:
-            long end = license.getEnd();
-//             if (LicenseType.SUBSCRIPTION.getName().equals(license.getType())) {
-//                 end -= LICENSE_SUBSCRIPTION_EXTENSION;
-//             }
-            sb.append("untangle: " + (0xC0DED + 0xDEC0DED) + "\n");
-            sb.append("version: " + version + "\n");
-            sb.append("type: " + license.getType() + "\n");
-            sb.append("product: " + license.getName() + "\n");
-            sb.append("start: " + license.getStart() + "\n");
-            sb.append("end: " + end + "\n");
-        }
-
-        return sb.toString();
-    }
-
     private String toHex(byte data[])
     {
         String response = "";
@@ -355,7 +289,7 @@ public class LicenseManagerImpl implements LicenseManager
             return EXPIRED;
 
         /* Calculate the number of days remaining */
-        long days = (expiration - now) / ONE_DAY;
+        long days = (expiration - now) / (60 * 60 * 24);
 
         switch ((int) days) {
         case 0:
@@ -385,7 +319,7 @@ public class LicenseManagerImpl implements LicenseManager
      */
     private void scheduleFixedTask()
     {
-        this.pulse.start(this.timerDelay);
+        this.pulse.start(this.TIMER_DELAY);
     }
 
     /* statics */
@@ -394,6 +328,29 @@ public class LicenseManagerImpl implements LicenseManager
         return INSTANCE;
     }
 
+    /**
+     * Set the current settings to new Settings
+     * Also save the settings to disk if save is true
+     */
+    private void _saveSettings(LicenseSettings newSettings)
+    {
+        /**
+         * Save the settings
+         */
+        SettingsManager settingsManager = LocalUvmContextFactory.context().settingsManager();
+        try {
+            settingsManager.saveBasePath(LicenseSettings.class, System.getProperty("uvm.conf.dir"), "licenses", "licenses", newSettings);
+        } catch (SettingsManager.SettingsException e) {
+            logger.warn("Failed to save settings.",e);
+        }
+
+        /**
+         * Change current settings
+         */
+        this.settings = newSettings;
+        try {logger.info("New Settings: \n" + new org.json.JSONObject(this.settings).toString(2));} catch (Exception e) {}
+    }
+    
     private class LicenseUpdateTask implements Runnable
     {
         public void run() {
--- ext-impl/com/untangle/uvm/license/LicenseSettings.java
+++ ext-impl/com/untangle/uvm/license/LicenseSettings.java
@@ -15,14 +15,34 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.LinkedList;
 
-class LicenseSettings
+@SuppressWarnings("serial")
+public class LicenseSettings implements java.io.Serializable
 {
+    private Integer settingsVersion = new Integer(1);
+
     private List<License> licenses;
     
-    LicenseSettings( List<License> licenses )
+    public LicenseSettings( )
+    {
+        this.licenses = new LinkedList<License>();
+    }
+
+    public LicenseSettings( List<License> licenses )
     {
         this.licenses = licenses;
+    }
+
+
+    public Integer getSettingsVersion()
+    {
+        return settingsVersion;
+    }
+
+    public void setSettingsVersion( Integer settingsVersion )
+    {
+        this.settingsVersion = settingsVersion;
     }
 
     public List<License> getLicenses()
