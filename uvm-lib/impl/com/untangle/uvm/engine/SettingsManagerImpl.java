9e53eb0a65db01682f0163ec6bdba466aebed5a2
Dirk Morris <dmorris@untangle.com>
2010-11-30 02:44:59 +0000

    remove old debug code
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@27948 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm-lib/impl/com/untangle/uvm/engine/SettingsManagerImpl.java
+++ uvm-lib/impl/com/untangle/uvm/engine/SettingsManagerImpl.java
@@ -133,34 +133,6 @@ public class SettingsManagerImpl implements SettingsManager
         }
     }
 
-    public String convertStreamToString(InputStream is)
-        throws IOException
-    {
-        /*
-         * To convert the InputStream to String we use the
-         * Reader.read(char[] buffer) method. We iterate until the
-         * Reader return -1 which means there's no more data to
-         * read. We use the StringWriter class to produce the string.
-         */
-        if (is != null) {
-            java.io.Writer writer = new java.io.StringWriter();
- 
-            char[] buffer = new char[1024];
-            try {
-                java.io.Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-                int n;
-                while ((n = reader.read(buffer)) != -1) {
-                    writer.write(buffer, 0, n);
-                }
-            } finally {
-                is.close();
-            }
-            return writer.toString();
-        } else {       
-            return "";
-        }
-    }
-    
     /**
      * Documented in SettingsManager.java
      */
