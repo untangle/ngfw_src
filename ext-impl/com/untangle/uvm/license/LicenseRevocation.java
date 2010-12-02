9bb4540a6a64d750c497ee6ccc3f96dfbd65499d
Dirk Morris <dmorris@untangle.com>
2010-12-02 00:59:41 +0000

    forgot missing file
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/src@27966 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ ext-impl/com/untangle/uvm/license/LicenseRevocation.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2003-2007 Untangle, Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Untangle, Inc. ("Confidential Information"). You shall
+ * not disclose such Confidential Information.
+ *
+ * $Id: License.java 27705 2010-10-15 01:44:20Z dmorris $
+ */
+
+package com.untangle.uvm.license;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.apache.log4j.Logger;
+
+@SuppressWarnings("serial")
+public class LicenseRevocation implements Serializable
+{
+    private final Logger logger = Logger.getLogger(License.class);
+
+    /** Identifier for the product this license revocation is for */
+    private String name;
+
+    /** The UID for this license revocation */
+    private String uid;
+
+    public LicenseRevocation()
+    {
+    }
+
+    public LicenseRevocation( String uid, String name )
+    {
+        this.uid = uid;
+        this.name = name;
+    }
+
+    /**
+     * Returns the unique identifier of the product of this license
+     */
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+
+    /**
+     * Returns the unique identifier of the product of this license
+     */
+    public String getUID()
+    {
+        return this.uid;
+    }
+    
+    public void setUID( String uid )
+    {
+        this.uid = uid;
+    }
+    
+    public String toString()
+    {
+        return "<" + this.uid + "/" + this.name + ">";
+    }
+
+}
