b341c355d265e4091778b6876b92ae7d96d8afba
Dirk Morris <dmorris@untangle.com>
2011-01-28 23:32:29 +0000

    fix warning on 64-bit gcc
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@28354 8ebf8b4e-06f3-0310-961f-89f74618be54

--- libnetcap/src/netcap_sched.c
+++ libnetcap/src/netcap_sched.c
@@ -1,20 +1,4 @@
-/*
- * $HeadURL$
- * Copyright (c) 2003-2007 Untangle, Inc. 
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
- * NONINFRINGEMENT.  See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- */
+/* $HeadURL$ */
 
 #include <unistd.h>
 #include <sys/time.h>
@@ -308,6 +292,7 @@ static int           _sched_run_events  ( int if_exit )
     list_node_t* node;
     list_node_t* next;
     struct timespec now;
+    bzero(&now, sizeof(now));
     
     if ( list_length ( &_sched.queue ) == 0 ) return 0;
 
