57e4d98dc4211d2bbef60aad7a6e9148daa5f6db
Dirk Morris <dmorris@untangle.com>
2011-02-11 21:16:42 +0000

    database convertors for interface changes, also removed unused database tables
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@28476 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ firewall/hier/usr/share/untangle/schema/untangle-node-firewall/settings-convert-7.sql
@@ -0,0 +1,20 @@
+-- settings conversion for release-8.1
+
+-- interface enumeration changes
+UPDATE settings.n_firewall_rule SET src_intf_matcher = '1' WHERE src_intf_matcher = '0';
+UPDATE settings.n_firewall_rule SET src_intf_matcher = '2' WHERE src_intf_matcher = '1';
+UPDATE settings.n_firewall_rule SET src_intf_matcher = '3' WHERE src_intf_matcher = '2';
+UPDATE settings.n_firewall_rule SET src_intf_matcher = '4' WHERE src_intf_matcher = '3';
+UPDATE settings.n_firewall_rule SET src_intf_matcher = '5' WHERE src_intf_matcher = '4';
+UPDATE settings.n_firewall_rule SET src_intf_matcher = '6' WHERE src_intf_matcher = '5';
+UPDATE settings.n_firewall_rule SET src_intf_matcher = '7' WHERE src_intf_matcher = '6';
+UPDATE settings.n_firewall_rule SET src_intf_matcher = '255' WHERE src_intf_matcher = '7';
+
+UPDATE settings.n_firewall_rule SET dst_intf_matcher = '1' WHERE dst_intf_matcher = '0';
+UPDATE settings.n_firewall_rule SET dst_intf_matcher = '2' WHERE dst_intf_matcher = '1';
+UPDATE settings.n_firewall_rule SET dst_intf_matcher = '3' WHERE dst_intf_matcher = '2';
+UPDATE settings.n_firewall_rule SET dst_intf_matcher = '4' WHERE dst_intf_matcher = '3';
+UPDATE settings.n_firewall_rule SET dst_intf_matcher = '5' WHERE dst_intf_matcher = '4';
+UPDATE settings.n_firewall_rule SET dst_intf_matcher = '6' WHERE dst_intf_matcher = '5';
+UPDATE settings.n_firewall_rule SET dst_intf_matcher = '7' WHERE dst_intf_matcher = '6';
+UPDATE settings.n_firewall_rule SET dst_intf_matcher = '255' WHERE dst_intf_matcher = '7';
---
+++ uvm/hier/usr/share/untangle/schema/uvm/settings-convert-22.sql
@@ -0,0 +1,46 @@
+-- settings conversion for release-8.1
+
+-- interface enumeration changes
+UPDATE settings.u_user_policy_rule SET client_intf_matcher = '1' WHERE client_intf_matcher = '0';
+UPDATE settings.u_user_policy_rule SET client_intf_matcher = '2' WHERE client_intf_matcher = '1';
+UPDATE settings.u_user_policy_rule SET client_intf_matcher = '3' WHERE client_intf_matcher = '2';
+UPDATE settings.u_user_policy_rule SET client_intf_matcher = '4' WHERE client_intf_matcher = '3';
+UPDATE settings.u_user_policy_rule SET client_intf_matcher = '5' WHERE client_intf_matcher = '4';
+UPDATE settings.u_user_policy_rule SET client_intf_matcher = '6' WHERE client_intf_matcher = '5';
+UPDATE settings.u_user_policy_rule SET client_intf_matcher = '7' WHERE client_intf_matcher = '6';
+UPDATE settings.u_user_policy_rule SET client_intf_matcher = '255' WHERE client_intf_matcher = '7';
+
+UPDATE settings.u_user_policy_rule SET server_intf_matcher = '1' WHERE server_intf_matcher = '0';
+UPDATE settings.u_user_policy_rule SET server_intf_matcher = '2' WHERE server_intf_matcher = '1';
+UPDATE settings.u_user_policy_rule SET server_intf_matcher = '3' WHERE server_intf_matcher = '2';
+UPDATE settings.u_user_policy_rule SET server_intf_matcher = '4' WHERE server_intf_matcher = '3';
+UPDATE settings.u_user_policy_rule SET server_intf_matcher = '5' WHERE server_intf_matcher = '4';
+UPDATE settings.u_user_policy_rule SET server_intf_matcher = '6' WHERE server_intf_matcher = '5';
+UPDATE settings.u_user_policy_rule SET server_intf_matcher = '7' WHERE server_intf_matcher = '6';
+UPDATE settings.u_user_policy_rule SET server_intf_matcher = '255' WHERE server_intf_matcher = '7';
+
+-- drop obsolete (unused) tables
+DROP TABLE settings.u_ddnsq_settings;
+DROP TABLE settings.u_dhcp_lease_rule;
+DROP TABLE settings.u_dns_static_host_rule;
+DROP TABLE settings.u_network_intf;
+DROP TABLE settings.u_ip_network;
+DROP TABLE settings.u_network_route;
+DROP TABLE settings.u_network_space;
+DROP TABLE settings.u_redirect_rule;
+DROP TABLE settings.u_redirects;
+DROP TABLE settings.u_network_settings;
+DROP TABLE settings.u_network_services;
+DROP TABLE settings.u_dhcp_lease_list;
+DROP TABLE settings.u_dns_host_list;
+DROP TABLE settings.u_pppoe_connection;
+DROP TABLE settings.u_pppoe;
+DROP TABLE settings.u_wmi_settings;
+
+
+
+
+
+
+
+
