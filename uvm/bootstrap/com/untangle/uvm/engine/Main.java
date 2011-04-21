6500a053947bef78226da5b5b99dda5564f1d428
Dirk Morris <dmorris@untangle.com>
2011-04-21 19:53:17 +0000

    new uvm wrapper
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@28783 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/bootstrap/com/untangle/uvm/engine/Main.java
+++ uvm/bootstrap/com/untangle/uvm/engine/Main.java
@@ -81,6 +81,8 @@ public class Main
      */
     public static final void main(String[] args) throws Exception
     {
+        System.out.println("UVM starting...");
+        
         synchronized (Main.class) {
             if (null == MAIN) {
                 MAIN = new Main();
