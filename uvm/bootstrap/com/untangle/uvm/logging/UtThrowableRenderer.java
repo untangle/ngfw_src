4a1092e04d7e0e76e97cba2f642b032c18bccf3b
Sebastien Delafond <sdelafond@gmail.com>
2011-05-13 12:45:04 +0000

    [syslog] Make sure we catch node exception (Closes: #8748)
      * Since log4j doesn't handle syslogtag, we manually prefix each log
        line with the proper tag; infortunately, for multi-line messages
        like stack traces, this only happens on the first line, and
        therefore all the actual trace information is caught by the rsyslog
        rules for that tag. Since those lines are seen by rsyslog as not
        having a tag, they don't match any of our rules and end up in the
        catch-all place, namely /var/log/syslog
    
      * To work around this, we use a (new in 1.2.16) log4j feature and
        extend ThrowableRenderer into our own UtThrowableRenderer that
        prepends the correct tag before *each* line of the stack trace
    
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/release-9.0/work/src@28928 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ uvm/bootstrap/com/untangle/uvm/logging/UtThrowableRenderer.java
@@ -0,0 +1,48 @@
+/*
+ * $HeadURL$
+ * Copyright (c) 2003-2011 Untangle, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
+ * NONINFRINGEMENT.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.untangle.uvm.logging;
+
+import java.util.List;
+import java.util.LinkedList;
+
+import org.apache.log4j.spi.ThrowableRenderer;
+
+/**
+ * 
+ *
+ * @author <a href="mailto:seb@untangle.com">Sebastien Delafond</a>
+ * @version 1.0
+ */
+public class UtThrowableRenderer implements ThrowableRenderer {
+
+    private String prefix;
+
+    public UtThrowableRenderer(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String[] doRender(Throwable t) {
+        List<String> l = new LinkedList<String>();
+        for (StackTraceElement ste: t.getStackTrace()) {
+            l.add(this.prefix + ste.toString());
+        }
+        return l.toArray(new String[0]);
+    }
+
+}
