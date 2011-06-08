c293ae965fe5db994b50529e7f40bfdf69a65e2e
Dirk Morris <dmorris@untangle.com>
2011-06-08 21:16:46 +0000

    handle old interface matcher formats
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@29031 8ebf8b4e-06f3-0310-961f-89f74618be54

--- firewall/hier/usr/share/untangle/schema/untangle-node-firewall/settings-convert-7.sql
+++ firewall/hier/usr/share/untangle/schema/untangle-node-firewall/settings-convert-7.sql
@@ -10,6 +10,11 @@ UPDATE settings.n_firewall_rule SET src_intf_matcher = '3' WHERE src_intf_matche
 UPDATE settings.n_firewall_rule SET src_intf_matcher = '2' WHERE src_intf_matcher = '1';
 UPDATE settings.n_firewall_rule SET src_intf_matcher = '1' WHERE src_intf_matcher = '0';
 
+UPDATE settings.u_firewall_rule SET src_intf_matcher = '1' WHERE src_intf_matcher = 'O';
+UPDATE settings.u_firewall_rule SET src_intf_matcher = '2' WHERE src_intf_matcher = 'I';
+UPDATE settings.u_firewall_rule SET src_intf_matcher = '3' WHERE src_intf_matcher = 'D';
+UPDATE settings.u_firewall_rule SET src_intf_matcher = '250' WHERE src_intf_matcher = 'V';
+
 UPDATE settings.n_firewall_rule SET dst_intf_matcher = '250' WHERE dst_intf_matcher = '7';
 UPDATE settings.n_firewall_rule SET dst_intf_matcher = '7' WHERE dst_intf_matcher = '6';
 UPDATE settings.n_firewall_rule SET dst_intf_matcher = '6' WHERE dst_intf_matcher = '5';
@@ -18,6 +23,11 @@ UPDATE settings.n_firewall_rule SET dst_intf_matcher = '4' WHERE dst_intf_matche
 UPDATE settings.n_firewall_rule SET dst_intf_matcher = '3' WHERE dst_intf_matcher = '2';
 UPDATE settings.n_firewall_rule SET dst_intf_matcher = '2' WHERE dst_intf_matcher = '1';
 UPDATE settings.n_firewall_rule SET dst_intf_matcher = '1' WHERE dst_intf_matcher = '0';
+
+UPDATE settings.u_firewall_rule SET dst_intf_matcher = '1' WHERE dst_intf_matcher = 'O';
+UPDATE settings.u_firewall_rule SET dst_intf_matcher = '2' WHERE dst_intf_matcher = 'I';
+UPDATE settings.u_firewall_rule SET dst_intf_matcher = '3' WHERE dst_intf_matcher = 'D';
+UPDATE settings.u_firewall_rule SET dst_intf_matcher = '250' WHERE dst_intf_matcher = 'V';
 
 UPDATE settings.n_firewall_rule set src_intf_matcher = 'any' WHERE src_intf_matcher = 'more_trusted';
 UPDATE settings.n_firewall_rule set src_intf_matcher = 'any' WHERE src_intf_matcher = 'less_trusted';
--- uvm/hier/usr/share/untangle/schema/uvm/settings-convert-22.sql
+++ uvm/hier/usr/share/untangle/schema/uvm/settings-convert-22.sql
@@ -15,6 +15,11 @@ UPDATE settings.u_user_policy_rule SET client_intf_matcher = '3' WHERE client_in
 UPDATE settings.u_user_policy_rule SET client_intf_matcher = '2' WHERE client_intf_matcher = '1';
 UPDATE settings.u_user_policy_rule SET client_intf_matcher = '1' WHERE client_intf_matcher = '0';
 
+UPDATE settings.u_user_policy_rule SET client_intf_matcher = '1' WHERE client_intf_matcher = 'O';
+UPDATE settings.u_user_policy_rule SET client_intf_matcher = '2' WHERE client_intf_matcher = 'I';
+UPDATE settings.u_user_policy_rule SET client_intf_matcher = '3' WHERE client_intf_matcher = 'D';
+UPDATE settings.u_user_policy_rule SET client_intf_matcher = '250' WHERE client_intf_matcher = 'V';
+
 UPDATE settings.u_user_policy_rule SET server_intf_matcher = '250' WHERE server_intf_matcher = '7';
 UPDATE settings.u_user_policy_rule SET server_intf_matcher = '7' WHERE server_intf_matcher = '6';
 UPDATE settings.u_user_policy_rule SET server_intf_matcher = '6' WHERE server_intf_matcher = '5';
@@ -23,6 +28,11 @@ UPDATE settings.u_user_policy_rule SET server_intf_matcher = '4' WHERE server_in
 UPDATE settings.u_user_policy_rule SET server_intf_matcher = '3' WHERE server_intf_matcher = '2';
 UPDATE settings.u_user_policy_rule SET server_intf_matcher = '2' WHERE server_intf_matcher = '1';
 UPDATE settings.u_user_policy_rule SET server_intf_matcher = '1' WHERE server_intf_matcher = '0';
+
+UPDATE settings.u_user_policy_rule SET server_intf_matcher = '1' WHERE server_intf_matcher = 'O';
+UPDATE settings.u_user_policy_rule SET server_intf_matcher = '2' WHERE server_intf_matcher = 'I';
+UPDATE settings.u_user_policy_rule SET server_intf_matcher = '3' WHERE server_intf_matcher = 'D';
+UPDATE settings.u_user_policy_rule SET server_intf_matcher = '250' WHERE server_intf_matcher = 'V';
 
 UPDATE settings.u_user_policy_rule SET client_intf_matcher = 'any' WHERE client_intf_matcher = 'more_trusted';
 UPDATE settings.u_user_policy_rule SET client_intf_matcher = 'any' WHERE client_intf_matcher = 'less_trusted';
