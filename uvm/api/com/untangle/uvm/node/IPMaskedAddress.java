12a487a07339a658c0b6c58386c0e10035369e0a
Dirk Morris <dmorris@untangle.com>
2011-07-22 00:06:10 +0000

    make IPMaskedAddress constructor more tolerant of other formats
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@29352 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/api/com/untangle/uvm/node/IPMaskedAddress.java
+++ uvm/api/com/untangle/uvm/node/IPMaskedAddress.java
@@ -1,37 +1,6 @@
-
 /*
- * $HeadURL$
- * Copyright (c) 2003-2007 Untangle, Inc. 
- *
- * This library is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2,
- * as published by the Free Software Foundation.
- *
- * This library is distributed in the hope that it will be useful, but
- * AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
- * NONINFRINGEMENT.  See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Linking this library statically or dynamically with other modules is
- * making a combined work based on this library.  Thus, the terms and
- * conditions of the GNU General Public License cover the whole combination.
- *
- * As a special exception, the copyright holders of this library give you
- * permission to link this library with independent modules to produce an
- * executable, regardless of the license terms of these independent modules,
- * and to copy and distribute the resulting executable under terms of your
- * choice, provided that you also meet, for each linked independent module,
- * the terms and conditions of the license of that module.  An independent
- * module is a module which is not derived from or based on this library.
- * If you modify this library, you may extend this exception to your version
- * of the library, but you are not obligated to do so.  If you do not wish
- * to do so, delete this exception statement from your version.
+ * $Id$
  */
-
 package com.untangle.uvm.node;
 
 import java.io.Serializable;
@@ -57,22 +26,17 @@ public class IPMaskedAddress implements Serializable, Comparable<IPMaskedAddress
 {
     private static final Logger logger = Logger.getLogger(IPMaskedAddress.class);
 
-    // This is the canonical printed representation of the ANY_ADDRESS
     public static final String ANY = "any";
     public static final String SPECIFIC_NODE_MASK = "255.255.255.255";
-
     public static final String ANY_ADDRESS = "0.0.0.0";
 
-    // XXX why isn't this all caps, I think this should be ANY and the
-    // above ANY should be ANY_STRING or something like that.
     public static IPMaskedAddress anyAddr = new IPMaskedAddress(ANY_ADDRESS,ANY_ADDRESS);
 
     private final String addr;
     private final String mask;
 
-
     /**
-     * Creates a new <code>IPMaskedAddress</code> for a specific host.
+     * Creates a new <code>IPMaskedAddress</code> for a specific host and/or mask.
      * Assumes values are legal.  Use parse() if unsure.
      *
      * addr     = host-num "." host-num [ "." host-num [ "." host-num ] ]
@@ -82,17 +46,33 @@ public class IPMaskedAddress implements Serializable, Comparable<IPMaskedAddress
      */
     public IPMaskedAddress(String addr)
     {
-        this.addr = addr;
-        this.mask = SPECIFIC_NODE_MASK;
-    }
+        if  (addr.contains("/")) {
+            String[] strs = addr.split("/");
+            String mask = strs[1];
 
+            /* check if the mask is an int */
+            try {
+                int numbits = Integer.parseInt(mask);
+                mask = longToMask(numbits == 0 ? 0 : 0xffffffff << (32 - numbits));;
+            } catch (NumberFormatException x) {
+                // guess it wasn't an INT, so just assume it wask an IP mask
+            }
+
+            this.addr = strs[0];
+            this.mask = mask;
+        } else {
+            this.addr = addr;
+            this.mask = SPECIFIC_NODE_MASK;
+        }
+    }
 
     /**
      * Creates a new <code>IPMaskedAddress</code> which is a copy of the input IPMaskedAddress.
      *
      * @param originalIPMaskedAddress an <code>IPMAddr</code> to be replicated
      */
-    public IPMaskedAddress(IPMaskedAddress originalIPMaskedAddress){
+    public IPMaskedAddress(IPMaskedAddress originalIPMaskedAddress)
+    {
         if(originalIPMaskedAddress == null) {
             this.addr = null;
             this.mask = null;
@@ -500,29 +480,32 @@ public class IPMaskedAddress implements Serializable, Comparable<IPMaskedAddress
             ch = srcb[i++];
             if (Character.isDigit(ch)) {
                 // note that Java byte is signed, so need to convert to int
-                int sum = (dst[cur] & 0xff)*10
-                    + (Character.digit(ch, 10) & 0xff);
+                int sum = (dst[cur] & 0xff)*10 + (Character.digit(ch, 10) & 0xff);
 
-                if (sum > 255)
+                if (sum > 255) {
                     return null;
+                }
 
                 dst[cur] = (byte)(sum & 0xff);
                 if (! saw_digit) {
-                    if (++octets > INADDRSZ)
+                    if (++octets > INADDRSZ) {
                         return null;
+                    }
                     saw_digit = true;
                 }
             } else if (ch == '.' && saw_digit) {
-                if (octets == INADDRSZ)
+                if (octets == INADDRSZ) {
                     return null;
+                }
                 cur++;
                 dst[cur] = 0;
                 saw_digit = false;
             } else
                 return null;
         }
-        if (octets < INADDRSZ)
+        if (octets < INADDRSZ) {
             return null;
+        }
         return dst;
     }
 
