c03c1b64fa8bca264b9022753f7b1f23ff197a02
Dirk Morris <dmorris@untangle.com>
2011-08-02 21:42:13 +0000

    moving more xml files to json
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/src@29409 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ hier/usr/share/untangle/lib/untangle-node-license/nodeDesc.js
@@ -0,0 +1,17 @@
+{
+        "javaClass": "com.untangle.uvm.node.NodeDesc",
+        "className" : "com.untangle.node.license.LicenseManagerNode",
+        "name" : "untangle-node-license",
+        "displayName" : "License Manager",
+        "syslogName" : "License_Manager",
+        "type" : "SERVICE",
+        "viewPosition" : 990,
+        "singleInstance" : true,
+        "uvmResources" : {
+            "javaClass": "java.util.LinkedList",
+            "list": [
+                "untangle-node-license-ext-impl"
+            ]
+        }
+}
+
