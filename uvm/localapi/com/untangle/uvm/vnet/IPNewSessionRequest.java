927984b66f775e9928529043831de3de2cb7ef25
Dirk Morris <dmorris@untangle.com>
2011-08-25 19:04:10 +0000

    better comments
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@29569 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/localapi/com/untangle/uvm/vnet/IPNewSessionRequest.java
+++ uvm/localapi/com/untangle/uvm/vnet/IPNewSessionRequest.java
@@ -1,36 +1,6 @@
 /*
- * $HeadURL$
- * Copyright (c) 2003-2007 Untangle, Inc.
- *
- * This library is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2,
- * as published by the Free Software Foundation.
- *
- * This library is distributed in the hope that it will be useful, but
- * AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
- * NONINFRINGEMENT.  See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Linking this library statically or dynamically with other modules is
- * making a combined work based on this library.  Thus, the terms and
- * conditions of the GNU General Public License cover the whole combination.
- *
- * As a special exception, the copyright holders of this library give you
- * permission to link this library with independent modules to produce an
- * executable, regardless of the license terms of these independent modules,
- * and to copy and distribute the resulting executable under terms of your
- * choice, provided that you also meet, for each linked independent module,
- * the terms and conditions of the license of that module.  An independent
- * module is a module which is not derived from or based on this library.
- * If you modify this library, you may extend this exception to your version
- * of the library, but you are not obligated to do so.  If you do not wish
- * to do so, delete this exception statement from your version.
+ * $Id$
  */
-
 package com.untangle.uvm.vnet;
 
 import java.net.InetAddress;
@@ -40,9 +10,6 @@ import com.untangle.uvm.node.SessionEndpoints;
 
 /**
  * The new IP session request interface
- *
- * @author <a href="mailto:jdi@untangle.com">John Irwin</a>
- * @version 1.0
  */
 public interface IPNewSessionRequest extends NewSessionRequest, SessionEndpoints
 {
@@ -68,12 +35,16 @@ public interface IPNewSessionRequest extends NewSessionRequest, SessionEndpoints
 
     PipelineEndpoints pipelineEndpoints();
 
-    // May only be called before session is established (from
-    // UDPNewSessionRequestEvent handler)
+    /**
+     * May only be called before session is established (from
+     * UDPNewSessionRequestEvent handler)
+     */
     void rejectSilently();
 
-    // May only be called before session is established (from
-    // UDPNewSessionRequestEvent handler)
+    /**
+     * May only be called before session is established (from
+     * UDPNewSessionRequestEvent handler)
+     */
     void rejectSilently(boolean needsFinalization);
 
     // Codes for rejectReturnUnreachable()
@@ -87,12 +58,16 @@ public interface IPNewSessionRequest extends NewSessionRequest, SessionEndpoints
     // static final byte PROHIBITED_HOST = 10;      // By RFC1812, should use PROHIBITED instead
     static final byte PROHIBITED = 13;
 
-    // May only be called before session is established (from
-    // UDPNewSessionRequestEvent handler)
+    /**
+     * May only be called before session is established (from
+     * UDPNewSessionRequestEvent handler)
+     */
     void rejectReturnUnreachable(byte code);
 
-    // May only be called before session is established (from
-    // UDPNewSessionRequestEvent handler)
+    /**
+     * May only be called before session is established (from
+     * UDPNewSessionRequestEvent handler)
+     */
     void rejectReturnUnreachable(byte code, boolean needsFinalization);
 
     /**
