252b689eaedf47f6eddfbbea0a8dc0bf674d2507
Dirk Morris <dmorris@untangle.com>
2011-09-22 17:07:23 +0000

    5 minute delay currently
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@29808 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/servlets/webui/root/script/components.js
+++ uvm/servlets/webui/root/script/components.js
@@ -2542,7 +2542,7 @@ Ung.GridEventLog = Ext.extend(Ext.grid.GridPanel, {
             text : '<div style="width:30px;"></div>'
         }, this.pagingToolbar, {
             xtype : 'tbtext',
-            text : '<i><font color="red">WARNING:</font></i> Event logs are delayed by 2 minutes.'
+            text : '<i><font color="red">WARNING:</font></i> Event logs are delayed by 5 minutes.'
         }];
         Ung.GridEventLog.superclass.initComponent.call(this);
         var columnModel=this.getColumnModel();
