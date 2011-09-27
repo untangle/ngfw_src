aafe860e93ce51be1a03827495a6f15a39a70aeb
Dirk Morris <dmorris@untangle.com>
2011-09-27 19:10:29 +0000

    add support for stdout and stderr redirect to logfile
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@29864 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/lib/python2.5/untangle_tests/client_control.py
+++ uvm/hier/usr/lib/python2.5/untangle_tests/client_control.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 class ClientControl:
 
@@ -6,20 +7,47 @@ class ClientControl:
     hostIP = "192.168.2.100"
     hostUsername = "testshell"
     hostKeyFile = "@PREFIX@/usr/lib/python2.5/untangle_tests/testShell.key"
+    logfile = None
     verbosity = 0
+
+
+    def redirectOutput(self, logfile):
+        self.orig_stdout = sys.stdout
+        self.orig_stderr = sys.stderr
+        sys.stdout = logfile
+        sys.stderr = logfile
+
+    def restoreOutput(self):
+        sys.stdout = self.orig_stdout
+        sys.stderr = self.orig_stderr
 
     # runs a given command
     # returns 0 if the process returned 0, 1 otherwise
     def runCommand(self, command):
-        if (ClientControl.verbosity <= 1):
-            redirectOutput = " >/dev/null 2>&1 "
-        else:
-            redirectOutput = ""
 
-        sshCommand = "ssh -i %s %s@%s \"%s %s\" %s" % (ClientControl.hostKeyFile, ClientControl.hostUsername, ClientControl.hostIP, command, redirectOutput, redirectOutput)
-        if (ClientControl.verbosity > 2):
-            print "Running command on client: \"%s\"" % command
-        result = os.system(sshCommand)
+        if (ClientControl.verbosity <= 0):
+            shellRedirect = " >/dev/null 2>&1 "
+        else:
+            shellRedirect = ""
+
+        if (ClientControl.logfile != None):
+            self.redirectOutput(ClientControl.logfile)
+
+        result = 1
+        try:
+            sshCommand = "ssh -i %s %s@%s \"%s %s\" %s" % (ClientControl.hostKeyFile, ClientControl.hostUsername, ClientControl.hostIP, command, shellRedirect, shellRedirect)
+            if (ClientControl.verbosity > 0):
+                print "\nRunning command on client: \"%s\"" % command
+           #result = os.system(sshCommand)
+            child_stdin, child_stdout, child_stderr = os.popen3(sshCommand)
+            child_stdin.close()
+            print child_stdout.read()
+            print child_stderr.read()
+            result = os.wait()
+        finally:
+            if (ClientControl.logfile != None):
+                self.restoreOutput()
+
         if result == 0:
             return 0
         else:
