cda4a167bdc7d613f826a1d6554ea60efb745673
Dirk Morris <dmorris@untangle.com>
2011-10-03 20:05:22 +0000

    add new web filter tests
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@29894 8ebf8b4e-06f3-0310-961f-89f74618be54

--- webfilter/hier/usr/lib/python2.5/untangle_tests/webfilter_tests.py
+++ webfilter/hier/usr/lib/python2.5/untangle_tests/webfilter_tests.py
@@ -1,5 +1,5 @@
-
 import unittest
+import time
 from jsonrpc import ServiceProxy
 from jsonrpc import JSONRPCException
 from uvm import Manager
@@ -8,32 +8,58 @@ from untangle_tests import ClientControl
 
 uvmContext = Uvm().getUvmContext()
 clientControl = ClientControl()
-
+nodeDesc = None
 node = None
+
+def addBlockedSite(site):
+    global node
+    newUrl =  { "blocked": True, "description": "description", "flagged": True, "javaClass": "com.untangle.uvm.node.GenericRule", "string": site }
+
+    blockedUrls = node.getBlockedUrls()
+    blockedUrls["list"].append(newUrl)
+    node.setBlockedUrls(blockedUrls)
+
 
 class WebFilterTests(unittest.TestCase):
 
+
     def setUp(self):
-        global node
-        if node == None:
+        global nodeDesc, node
+        if nodeDesc == None:
             if (uvmContext.nodeManager().isInstantiated("untangle-node-webfilter")):
                 raise Exception('node webfilter already instantiated')
-            node = uvmContext.nodeManager().instantiateAndStart("untangle-node-webfilter", uvmContext.policyManager().getDefaultPolicy())
+            nodeDesc = uvmContext.nodeManager().instantiateAndStart("untangle-node-webfilter", uvmContext.policyManager().getDefaultPolicy())
+            node = uvmContext.nodeManager().nodeContext(nodeDesc['nodeId']).node()
 
     # verify client is online
     def test_10_clientIsOnline(self):
         result = clientControl.runCommand("wget -o /dev/null http://google.com/")
         assert (result == 0)
 
-    # verify client is online
+    # verify porn site is blocked in default config
     def test_11_defaultPornIsBlocked(self):
         result = clientControl.runCommand("wget http://playboy.com/ 2>&1 | grep -q blockpage")
         assert (result == 0)
 
+    # verify metaloft site is not blocked in default config
+    def test_12_defaultMetaloftIsNotBlocked(self):
+        result = clientControl.runCommand("wget -q -O - http://metaloft.com/test.html 2>&1 | grep -q success")
+        assert (result == 0)
+
+    # verify blocked site url list works
+    def test_13_blockedUrl(self):
+        addBlockedSite("metaloft.com/test.html")
+        # this test URL should now be blocked
+        result = clientControl.runCommand("wget http://metaloft.com/test.html 2>&1 | grep -q blockpage")
+        assert (result == 0)
+        # this test2 URL should NOT be blocked
+        result = clientControl.runCommand("wget http://metaloft.com/test2.html 2>&1 | grep -q blockpage")
+        assert (result != 0)
+
 
     def test_999_finalTearDown(self):
-        global node
-        uvmContext.nodeManager().destroy(node['nodeId']);
+        global nodeDesc
+        uvmContext.nodeManager().destroy(nodeDesc['nodeId']);
         
 
 
