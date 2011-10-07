e5b6203ed94a8b135d2825a5c9d05fef64faa7b1
Sebastien Delafond <sdelafond@gmail.com>
2011-10-07 09:51:29 +0000

    [events] Fix bug where last webfilter/sitefilter event would be duplicated upon re-running reports
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@29944 8ebf8b4e-06f3-0310-961f-89f74618be54

--- http-casing/hier/usr/lib/python2.5/reports/node/untangle_casing_http.py
+++ http-casing/hier/usr/lib/python2.5/reports/node/untangle_casing_http.py
@@ -1,19 +1,3 @@
-# $HeadURL: svn://chef/work/src/buildtools/rake-util.rb $
-# Copyright (c) 2003-2009 Untangle, Inc.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License, version 2,
-# as published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful, but
-# AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
-# NONINFRINGEMENT.  See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
-
 import reports.engine
 import reports.sql_helper as sql_helper
 
@@ -122,7 +106,7 @@ INSERT INTO reports.n_http_events
        c2s_content_length, s2c_content_length, s2c_content_type, hname)
     SELECT
         -- timestamp from request
-        er.time_stamp,
+        ps.time_stamp,
         -- pipeline endpoints
         ps.session_id, ps.client_intf, ps.server_intf,
         ps.c_client_addr, ps.s_client_addr, ps.c_server_addr, ps.s_server_addr,
