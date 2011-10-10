921592045ce270c9c424fdb735df0b8b448a37bc
Dirk Morris <dmorris@untangle.com>
2011-10-10 19:44:46 +0000

    style fixes
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@29954 8ebf8b4e-06f3-0310-961f-89f74618be54

--- spyware/impl/com/untangle/node/spyware/SpywareStatisticManager.java
+++ spyware/impl/com/untangle/node/spyware/SpywareStatisticManager.java
@@ -1,19 +1,5 @@
 /*
- * $HeadURL$
- * Copyright (c) 2003-2007 Untangle, Inc. 
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
- * NONINFRINGEMENT.  See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * $Id$
  */
 package com.untangle.node.spyware;
 
@@ -22,35 +8,42 @@ import com.untangle.uvm.logging.StatisticEvent;
 import com.untangle.uvm.node.StatisticManager;
 import com.untangle.uvm.node.NodeContext;
 
-class SpywareStatisticManager extends StatisticManager {
-
+class SpywareStatisticManager extends StatisticManager
+{
     private SpywareStatisticEvent statisticEvent = new SpywareStatisticEvent();
 
-    public SpywareStatisticManager(NodeContext tctx) {
+    public SpywareStatisticManager(NodeContext tctx)
+    {
         super(EventLoggerFactory.factory().getEventLogger(tctx));
     }
 
-    protected StatisticEvent getInitialStatisticEvent() {
+    protected StatisticEvent getInitialStatisticEvent()
+    {
         return this.statisticEvent;
     }
 
-    protected StatisticEvent getNewStatisticEvent() {
+    protected StatisticEvent getNewStatisticEvent()
+    {
         return (this.statisticEvent = new SpywareStatisticEvent());
     }
 
-    void incrPass() {
+    void incrPass()
+    {
         this.statisticEvent.incrPass();
     }
 
-    void incrCookie() {
+    void incrCookie()
+    {
         this.statisticEvent.incrCookie();
     }
 
-    void incrURL() {
+    void incrURL()
+    {
         this.statisticEvent.incrURL();
     }
 
-    void incrSubnetAccess() {
+    void incrSubnetAccess()
+    {
         this.statisticEvent.incrSubnetAccess();
     }
 }
