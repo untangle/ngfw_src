fa5f495a2f600e40242e5c6b238d4a6ef6f57f4c
Dirk Morris <dmorris@untangle.com>
2011-10-18 16:09:58 +0000

    Try the 5-minute load now. Obviously what we want to know is how busy the server will be during the time the incremental reports are running. No load can tell us the future. The 1-minute load was too quick and on dogfood didn't represent what was coming. Frequently it would have a load of .8 and it would satrt incremental reports and then an AV update would start and the server would go nuts. I tried the 15-minute load to see if that would help (because perhaps it would see the previous AV update) but that was too smoothed and was always at .95. Going to try reading the 5-minute load instead and see how that goes. If this does poorly I think we should switch back to the 1-minute load as it is better than the 15-minute load IMO.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@30048 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/share/untangle/bin/reporting-generate-reports.py
+++ uvm/hier/usr/share/untangle/bin/reporting-generate-reports.py
@@ -132,7 +132,7 @@ if simulate:
      sql_helper.CONNECTION_STRING = simulate
 
 def getLoad():
-     return float(open("/proc/loadavg").read().split(" ")[2])
+     return float(open("/proc/loadavg").read().split(" ")[1])
      
 def get_report_lengths(date):
     lengths = []
