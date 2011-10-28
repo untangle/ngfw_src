6092ccbd068f5d05f9e7cc241fdff65b9dc2247b
Sebastien Delafond <sdelafond@gmail.com>
2011-10-28 14:24:28 +0000

    [events] Cleanup data from the events schema (Comment: #9254)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@30128 8ebf8b4e-06f3-0310-961f-89f74618be54

--- openvpn/hier/usr/lib/python2.5/reports/node/untangle_node_openvpn.py
+++ openvpn/hier/usr/lib/python2.5/reports/node/untangle_node_openvpn.py
@@ -1,19 +1,3 @@
-# $HeadURL: svn://chef/work/src/buildtools/rake-util.rb $
-# Copyright (c) 2003-2009 Untangle, Inc.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License, version 2,
-# as published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful, but
-# AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
-# NONINFRINGEMENT.  See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
-
 import gettext
 import logging
 import mx
@@ -86,6 +70,12 @@ class OpenVpn(Node):
     def reports_cleanup(self, cutoff):
         sql_helper.drop_partitioned_table("n_openvpn_stats", cutoff)
         sql_helper.drop_partitioned_table("n_openvpn_connect_totals", cutoff)
+
+    def events_cleanup(self, cutoff):
+        try:
+            sql_helper.run_sql("""\
+DELETE FROM events.n_openvpn_connect_evt WHERE time_stamp < %s""", (cutoff,))
+        except: pass
 
     @print_timing
     def __create_n_openvpn_stats(self, start_date, end_date):
