e23144c46e5bdb977d1834e2eb1c4d40871be9ff
Dirk Morris <dmorris@untangle.com>
2011-11-28 20:12:23 +0000

    style cleanup
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@30241 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/servlets/webui/src/com/untangle/uvm/webui/jabsorb/UtCallbackController.java
+++ uvm/servlets/webui/src/com/untangle/uvm/webui/jabsorb/UtCallbackController.java
@@ -1,21 +1,6 @@
-/*
- * $HeadURL: svn://chef/branch/prod/jabsorb/work/src/uvm/servlets/alpaca/src/com/untangle/uvm/servlet/alpaca/ProxyServlet.java $
- * Copyright (c) 2003-2007 Untangle, Inc.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
- * NONINFRINGEMENT.  See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+/**
+ * $Id: UtCallbackController.java,v 1.00 2011/11/28 12:04:17 dmorris Exp $
  */
-
 package com.untangle.uvm.webui.jabsorb;
 
 import java.io.Serializable;
@@ -36,15 +21,10 @@ public class UtCallbackController extends CallbackController
     }
 
     @Override
-    public void preInvokeCallback(Object context, Object instance,
-                                  Method method, Object[] arguments)
-    {
-    }
+    public void preInvokeCallback(Object context, Object instance, Method method, Object[] arguments) { }
 
     @Override
-    public void postInvokeCallback(Object context, Object instance,
-                                   Method method, Object result)
-        throws Exception
+    public void postInvokeCallback(Object context, Object instance, Method method, Object result)  throws Exception
     {
         if (result!= null && !(result instanceof Serializable)) {
             bridge.registerCallableReference(result.getClass());
