4469c4ac7ecb45914066dae9267f5f9027d7f3c1
Sebastien Delafond <sdelafond@gmail.com>
2011-12-08 15:27:40 +0000

    [events] Restore cleanup functionality for reports schema (Closes: #9498)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/release-9.1/work/src@30393 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/lib/python2.5/reports/engine.py
+++ uvm/hier/usr/lib/python2.5/reports/engine.py
@@ -1,21 +1,3 @@
-# $HeadURL: svn://chef/work/src/buildtools/rake-util.rb $
-# Copyright (c) 2003-2009 Untangle, Inc.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License, version 2,
-# as published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful, but
-# AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
-# NONINFRINGEMENT.  See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
-#
-# Aaron Read <amread@untangle.com>
-
 import commands
 import logging
 import mx
@@ -363,7 +345,7 @@ def events_cleanup(cutoff):
             logger.debug("** about to clean events for %s" % (name,))            
             node.events_cleanup(cutoff)
         except:
-            logger.warn('count not cleanup events for: %s' % name,
+            logger.warn('could not cleanup events for: %s' % name,
                          exc_info=True)
 
 @print_timing
@@ -376,7 +358,7 @@ def reports_cleanup(cutoff):
             logger.debug("** about to clean data for %s" % (name,))
             node.reports_cleanup(cutoff)
         except:
-            logger.warn('count not cleanup reports for: %s' % name,
+            logger.warn('could not cleanup reports for: %s' % name,
                          exc_info=True)
 
 @print_timing
