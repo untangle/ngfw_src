5dac469da31be4b3714e544990899a0160acce11
Dirk Morris <dmorris@untangle.com>
2011-12-13 21:40:51 +0000

    add classd application category attachment
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@30443 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/impl/com/untangle/uvm/engine/ArgonConnectorComparator.java
+++ uvm/impl/com/untangle/uvm/engine/ArgonConnectorComparator.java
@@ -1,21 +1,6 @@
-/*
- * $HeadURL$
- * Copyright (c) 2003-2007 Untangle, Inc.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
- * NONINFRINGEMENT.  See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+/**
+ * $Id$
  */
-
 package com.untangle.uvm.engine;
 
 import java.util.Comparator;
@@ -26,9 +11,6 @@ import com.untangle.uvm.vnet.SoloPipeSpec;
 
 /**
  * Compares ArgonConnectors with SoloPipeSpecs.
- *
- * @author <a href="mailto:amread@untangle.com">Aaron Read</a>
- * @version 1.0
  */
 class ArgonConnectorComparator implements Comparator<ArgonConnector>
 {
