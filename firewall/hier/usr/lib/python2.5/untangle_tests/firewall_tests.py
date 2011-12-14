67bcd616f67697fbb57fb8cf5cb63fbaa5c99f18
Dirk Morris <dmorris@untangle.com>
2011-12-14 23:10:38 +0000

    more firewall test cases
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@30462 8ebf8b4e-06f3-0310-961f-89f74618be54

--- firewall/hier/usr/lib/python2.5/untangle_tests/firewall_tests.py
+++ firewall/hier/usr/lib/python2.5/untangle_tests/firewall_tests.py
@@ -11,13 +11,16 @@ uvmContext = Uvm().getUvmContext()
 clientControl = ClientControl()
 nodeDesc = None
 node = None
+metaloftIP = "74.123.29.145"
 
-def createDstPortRule(port, blocked=True):
+def createSingleMatcherRule( matcherType, value, blocked=True ):
+    matcherTypeStr = str(matcherType)
+    valueStr = str(value)
     return {
         "javaClass": "com.untangle.node.firewall.FirewallRule", 
         "id": 1, 
         "enabled": True, 
-        "description": "Block Port 80", 
+        "description": "Single Matcher: " + matcherTypeStr + " = " + valueStr, 
         "log": True, 
         "block": blocked, 
         "matchers": {
@@ -26,19 +29,23 @@ def createDstPortRule(port, blocked=True):
                 {
                     "invert": False, 
                     "javaClass": "com.untangle.node.firewall.FirewallRuleMatcher", 
-                    "matcherType": "DST_PORT", 
-                    "value": port
+                    "matcherType": matcherTypeStr, 
+                    "value": valueStr
                     }
                 ]
             }
         };
 
-def createDstAddrRule(addr, blocked=True):
+def createDualMatcherRule( matcherType, value, matcherType2, value2, blocked=True ):
+    matcherTypeStr = str(matcherType)
+    valueStr = str(value)
+    matcherTypeStr2 = str(matcherType2)
+    valueStr2 = str(value2)
     return {
         "javaClass": "com.untangle.node.firewall.FirewallRule", 
         "id": 1, 
         "enabled": True, 
-        "description": "Block Port 80", 
+        "description": "Dual Matcher: " + matcherTypeStr + " = " + valueStr + " && " + matcherTypeStr2 + " = " + valueStr2, 
         "log": True, 
         "block": blocked, 
         "matchers": {
@@ -47,30 +54,16 @@ def createDstAddrRule(addr, blocked=True):
                 {
                     "invert": False, 
                     "javaClass": "com.untangle.node.firewall.FirewallRuleMatcher", 
-                    "matcherType": "DST_ADDR", 
-                    "value": addr
+                    "matcherType": matcherTypeStr, 
+                    "value": valueStr
+                    },
+                {
+                    "invert": False, 
+                    "javaClass": "com.untangle.node.firewall.FirewallRuleMatcher", 
+                    "matcherType": matcherTypeStr2, 
+                    "value": valueStr2
                     }
-                ]
-            }
-        };
 
-def createSrcAddrRule(addr, blocked=True):
-    return {
-        "javaClass": "com.untangle.node.firewall.FirewallRule", 
-        "id": 1, 
-        "enabled": True, 
-        "description": "Block Port 80", 
-        "log": True, 
-        "block": blocked, 
-        "matchers": {
-            "javaClass": "java.util.LinkedList", 
-            "list": [
-                {
-                    "invert": False, 
-                    "javaClass": "com.untangle.node.firewall.FirewallRuleMatcher", 
-                    "matcherType": "SRC_ADDR", 
-                    "value": addr
-                    }
                 ]
             }
         };
@@ -122,88 +115,230 @@ class FirewallTests(unittest.TestCase):
         assert (result == 0)
 
     # verify a block port 80 rule works
-    def test_020_blockPort80(self):
+    def test_020_blockDstPort80(self):
         nukeRules();
-        appendRule(createDstPortRule("80"));
+        appendRule(createSingleMatcherRule("DST_PORT","80"));
         result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
         assert (result != 0)
 
     # verify a block port 79-81 rule works
-    def test_021_blockPort79to81(self):
+    def test_021_blockDstPort79to81(self):
         nukeRules();
-        appendRule(createDstPortRule("79-81"));
+        appendRule(createSingleMatcherRule("DST_PORT","79-81"));
         result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
         assert (result != 0)
 
     # verify a block port 79,80,81 rule works
-    def test_022_blockPort79comma80comma81(self):
+    def test_022_blockDstPort79comma80comma81(self):
         nukeRules();
-        appendRule(createDstPortRule("79,80,81"));
+        appendRule(createSingleMatcherRule("DST_PORT","79,80,81"));
         result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
         assert (result != 0)
 
     # verify a block port 79,80,81 rule works
-    def test_023_blockPort79comma81(self):
+    def test_023_blockDstPort79comma81(self):
         nukeRules();
-        appendRule(createDstPortRule("79,81"));
+        appendRule(createSingleMatcherRule("DST_PORT","79,81"));
         result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
         assert (result == 0)
 
     # verify a block port 79,80,81 rule works
-    def test_024_blockPortList(self):
+    def test_024_blockDstPortList(self):
         nukeRules();
-        appendRule(createDstPortRule("1-5,80,90-100"));
+        appendRule(createSingleMatcherRule("DST_PORT","1- 5,80, 90-100"));
         result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
         assert (result != 0)
 
     # verify a block port any rule works
-    def test_025_blockPortAny(self):
+    def test_025_blockDstPortAny(self):
         nukeRules();
-        appendRule(createDstPortRule("any"));
+        appendRule(createSingleMatcherRule("DST_PORT","any"));
         result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
         assert (result != 0)
 
-    # verify a block src_addr any rule works
-    def test_030_blockSourceAddrAny(self):
+    # verify src addr rule with any works
+    def test_030_blockSrcAddrAny(self):
         nukeRules();
-        appendRule(createSrcAddrRule("any"));
+        appendRule(createSingleMatcherRule("SRC_ADDR","any"));
         result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
         assert (result != 0)
 
-    # verify a block port any rule works
-    def test_031_blockSourceAddrIP(self):
+    # verify src addr rule with IP works
+    def test_031_blockSrcAddrIP(self):
         nukeRules();
-        appendRule(createSrcAddrRule(ClientControl.hostIP));
+        appendRule(createSingleMatcherRule("SRC_ADDR",ClientControl.hostIP));
         result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
         assert (result != 0)
 
-    # verify a block port any rule works
-    def test_032_blockSourceAddrCIDR(self):
+    # verify src addr rule with CIDR works
+    def test_032_blockSrcAddrCIDR(self):
         nukeRules();
-        appendRule(createSrcAddrRule(ClientControl.hostIP+"/24"));
+        appendRule(createSingleMatcherRule("SRC_ADDR",ClientControl.hostIP+"/24"));
         result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
         assert (result != 0)
 
-    # verify a block port any rule works
-    def test_033_blockSourceAddrComma(self):
+    # verify src addr rule with commas works
+    def test_033_blockSrcAddrComma(self):
         nukeRules();
-        appendRule(createSrcAddrRule(ClientControl.hostIP+",1.2.3.4"));
+        appendRule(createSingleMatcherRule("SRC_ADDR","4.3.2.1, "+ ClientControl.hostIP + ",  1.2.3.4/31"));
         result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
         assert (result != 0)
 
-    # verify rules evaluated in order
-    def test_040_ruleOrder(self):
+    # verify dst addr rule with any works
+    def test_040_blockDstAddrAny(self):
+        global metaloftIP;
         nukeRules();
-        appendRule( createSrcAddrRule(ClientControl.hostIP, blocked=False) );
-        appendRule( createDstPortRule("80") );
+        appendRule( createSingleMatcherRule("DST_ADDR","Any", blocked=True) );
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify dst addr rule with IP works
+    def test_041_blockDstAddr(self):
+        global metaloftIP;
+        nukeRules();
+        appendRule( createSingleMatcherRule("DST_ADDR",metaloftIP, blocked=True) );
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify dst addr rule with CIDR works
+    def test_042_blockDstAddrCIDR(self):
+        global metaloftIP;
+        nukeRules();
+        appendRule( createSingleMatcherRule("DST_ADDR",metaloftIP+"/31", blocked=True) );
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify dst addr rule with commas works
+    def test_043_blockDstAddrComma(self):
+        global metaloftIP;
+        nukeRules();
+        appendRule( createSingleMatcherRule("DST_ADDR","1.2.3.4/31," + metaloftIP+",5.6.7.8", blocked=True) );
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify src intf any rule works
+    def test_050_blockDstIntfAny(self):
+        global metaloftIP;
+        nukeRules();
+        appendRule( createSingleMatcherRule( "DST_INTF", "any" ) );
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify dst intf number rule works
+    def test_051_blockDstIntf(self):
+        global metaloftIP;
+        nukeRules();
+        appendRule( createSingleMatcherRule( "DST_INTF", ClientControl.interfaceExternal ) );
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify dst intf number rule doesnt match everythin
+    def test_052_blockDstIntfWrongIntf(self):
+        global metaloftIP;
+        nukeRules();
+        appendRule( createSingleMatcherRule( "DST_INTF", ClientControl.interfaceExternal + 1 ) );
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result == 0)
+
+    # verify dst intf with commas blocks
+    def test_053_blockDstIntfCommas(self):
+        global metaloftIP;
+        nukeRules();
+        appendRule( createSingleMatcherRule( "DST_INTF", "99," + str(ClientControl.interfaceExternal) +  ", 100" ) );
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify dst intf wan is blockde
+    def test_054_blockDstIntfWan(self):
+        global metaloftIP;
+        nukeRules();
+        appendRule( createSingleMatcherRule( "DST_INTF", "wan" ) );
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify dst intf non_wan not blocked
+    def test_055_blockDstIntfNonWan(self):
+        global metaloftIP;
+        nukeRules();
+        appendRule( createSingleMatcherRule( "DST_INTF", "non_wan" ) );
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result == 0)
+
+    # verify src intf any rule works
+    def test_060_blockSrcIntfAny(self):
+        global metaloftIP;
+        nukeRules();
+        appendRule( createSingleMatcherRule( "SRC_INTF", "any" ) );
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify src intf number rule works
+    def test_061_blockSrcIntf(self):
+        global metaloftIP;
+        nukeRules();
+        appendRule( createSingleMatcherRule( "SRC_INTF", ClientControl.interface ) );
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify src intf number rule doesnt match everythin
+    def test_062_blockSrcIntfWrongIntf(self):
+        global metaloftIP;
+        nukeRules();
+        appendRule( createSingleMatcherRule( "SRC_INTF", ClientControl.interface + 1 ) );
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result == 0)
+
+    # verify src intf with commas blocks
+    def test_063_blockSrcIntfCommas(self):
+        global metaloftIP;
+        nukeRules();
+        appendRule( createSingleMatcherRule( "SRC_INTF", "99," + str(ClientControl.interface) +  ", 100" ) );
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify src intf non_wan is blocked
+    def test_064_blockSrcIntfWan(self):
+        global metaloftIP;
+        nukeRules();
+        appendRule( createSingleMatcherRule( "SRC_INTF", "non_wan" ) );
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify src intf wan not blocked
+    def test_065_blockSrcIntfNonWan(self):
+        global metaloftIP;
+        nukeRules();
+        appendRule( createSingleMatcherRule( "SRC_INTF", "wan" ) );
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result == 0)
+
+    # verify rules that a rule with two matching matchers works
+    def test_080_dualMatcherRule(self):
+        nukeRules();
+        appendRule( createDualMatcherRule("SRC_ADDR", ClientControl.hostIP, "DST_PORT", 80) );
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify rules that both MUST match for the session to be blocked
+    def test_080_dualMatcherRuleAnd(self):
+        nukeRules();
+        appendRule( createDualMatcherRule("SRC_ADDR", ClientControl.hostIP, "DST_PORT", 79) );
         result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
         assert (result == 0)
 
     # verify rules evaluated in order
-    def test_041_ruleOrderReveres(self):
+    def test_090_ruleOrder(self):
         nukeRules();
-        appendRule( createDstPortRule("80") );
-        appendRule( createSrcAddrRule(ClientControl.hostIP, blocked=False) );
+        appendRule( createSingleMatcherRule("SRC_ADDR", ClientControl.hostIP, blocked=False) );
+        appendRule( createSingleMatcherRule("DST_PORT", "80") );
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result == 0)
+
+    # verify rules evaluated in order
+    def test_091_ruleOrderReverse(self):
+        nukeRules();
+        appendRule( createSingleMatcherRule("DST_PORT", "80") );
+        appendRule( createSingleMatcherRule("SRC_ADDR", ClientControl.hostIP, blocked=False) );
         result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
         assert (result != 0)
 
