517f1ceeb9cb3a01b91b828514bf63c3a7ad5575
Dirk Morris <dmorris@untangle.com>
2011-12-14 20:39:51 +0000

    new firewall tests
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@30461 8ebf8b4e-06f3-0310-961f-89f74618be54

--- firewall/hier/usr/lib/python2.5/untangle_tests/firewall_tests.py
+++ firewall/hier/usr/lib/python2.5/untangle_tests/firewall_tests.py
@@ -12,25 +12,68 @@ clientControl = ClientControl()
 nodeDesc = None
 node = None
 
-ruleBlockPort80 = {
-    "javaClass": "com.untangle.node.firewall.FirewallRule", 
-    "id": 1, 
-    "enabled": True, 
-    "description": "Block Port 80", 
-    "log": True, 
-    "block": True, 
-    "matchers": {
-        "javaClass": "java.util.LinkedList", 
-        "list": [
-            {
-                "invert": False, 
-                "javaClass": "com.untangle.node.firewall.FirewallRuleMatcher", 
-                "matcherType": "DST_PORT", 
-                "value": "80"
-                }
-            ]
-        }
-    };
+def createDstPortRule(port, blocked=True):
+    return {
+        "javaClass": "com.untangle.node.firewall.FirewallRule", 
+        "id": 1, 
+        "enabled": True, 
+        "description": "Block Port 80", 
+        "log": True, 
+        "block": blocked, 
+        "matchers": {
+            "javaClass": "java.util.LinkedList", 
+            "list": [
+                {
+                    "invert": False, 
+                    "javaClass": "com.untangle.node.firewall.FirewallRuleMatcher", 
+                    "matcherType": "DST_PORT", 
+                    "value": port
+                    }
+                ]
+            }
+        };
+
+def createDstAddrRule(addr, blocked=True):
+    return {
+        "javaClass": "com.untangle.node.firewall.FirewallRule", 
+        "id": 1, 
+        "enabled": True, 
+        "description": "Block Port 80", 
+        "log": True, 
+        "block": blocked, 
+        "matchers": {
+            "javaClass": "java.util.LinkedList", 
+            "list": [
+                {
+                    "invert": False, 
+                    "javaClass": "com.untangle.node.firewall.FirewallRuleMatcher", 
+                    "matcherType": "DST_ADDR", 
+                    "value": addr
+                    }
+                ]
+            }
+        };
+
+def createSrcAddrRule(addr, blocked=True):
+    return {
+        "javaClass": "com.untangle.node.firewall.FirewallRule", 
+        "id": 1, 
+        "enabled": True, 
+        "description": "Block Port 80", 
+        "log": True, 
+        "block": blocked, 
+        "matchers": {
+            "javaClass": "java.util.LinkedList", 
+            "list": [
+                {
+                    "invert": False, 
+                    "javaClass": "com.untangle.node.firewall.FirewallRuleMatcher", 
+                    "matcherType": "SRC_ADDR", 
+                    "value": addr
+                    }
+                ]
+            }
+        };
 
 def flushEvents():
     global uvmContext
@@ -80,12 +123,89 @@ class FirewallTests(unittest.TestCase):
 
     # verify a block port 80 rule works
     def test_020_blockPort80(self):
-        global ruleBlockPort80;
         nukeRules();
-        appendRule(ruleBlockPort80);
+        appendRule(createDstPortRule("80"));
         result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
         assert (result != 0)
 
+    # verify a block port 79-81 rule works
+    def test_021_blockPort79to81(self):
+        nukeRules();
+        appendRule(createDstPortRule("79-81"));
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify a block port 79,80,81 rule works
+    def test_022_blockPort79comma80comma81(self):
+        nukeRules();
+        appendRule(createDstPortRule("79,80,81"));
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify a block port 79,80,81 rule works
+    def test_023_blockPort79comma81(self):
+        nukeRules();
+        appendRule(createDstPortRule("79,81"));
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result == 0)
+
+    # verify a block port 79,80,81 rule works
+    def test_024_blockPortList(self):
+        nukeRules();
+        appendRule(createDstPortRule("1-5,80,90-100"));
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify a block port any rule works
+    def test_025_blockPortAny(self):
+        nukeRules();
+        appendRule(createDstPortRule("any"));
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify a block src_addr any rule works
+    def test_030_blockSourceAddrAny(self):
+        nukeRules();
+        appendRule(createSrcAddrRule("any"));
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify a block port any rule works
+    def test_031_blockSourceAddrIP(self):
+        nukeRules();
+        appendRule(createSrcAddrRule(ClientControl.hostIP));
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify a block port any rule works
+    def test_032_blockSourceAddrCIDR(self):
+        nukeRules();
+        appendRule(createSrcAddrRule(ClientControl.hostIP+"/24"));
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify a block port any rule works
+    def test_033_blockSourceAddrComma(self):
+        nukeRules();
+        appendRule(createSrcAddrRule(ClientControl.hostIP+",1.2.3.4"));
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
+
+    # verify rules evaluated in order
+    def test_040_ruleOrder(self):
+        nukeRules();
+        appendRule( createSrcAddrRule(ClientControl.hostIP, blocked=False) );
+        appendRule( createDstPortRule("80") );
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result == 0)
+
+    # verify rules evaluated in order
+    def test_041_ruleOrderReveres(self):
+        nukeRules();
+        appendRule( createDstPortRule("80") );
+        appendRule( createSrcAddrRule(ClientControl.hostIP, blocked=False) );
+        result = clientControl.runCommand("wget -o /dev/null -t 1 --timeout=3 http://metaloft.com/")
+        assert (result != 0)
 
     def test_999_finalTearDown(self):
         global nodeDesc
