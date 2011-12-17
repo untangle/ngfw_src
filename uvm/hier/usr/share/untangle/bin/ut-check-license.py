04db2ca37d1af90b73750e941f144e76b78518ae
Dirk Morris <dmorris@untangle.com>
2011-12-17 17:55:50 +0000

    svn merge -r30503:30504 svn://chef/work/src/ . (Comment: #9562)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/release-9.1/work/src@30505 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ uvm/hier/usr/share/untangle/bin/ut-check-license.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+#
+# A simple utility to check the validity of a license
+# This checks by reading the licenses.js file directly 
+#
+# If a valid license is found, it exits with a return code of 0
+# Otherwise 1
+#
+#
+
+import simplejson as json
+import sys
+import time
+
+LICENSES_JSON_OBJ = json.loads(open('/usr/share/untangle/conf/licenses/licenses.js', 'r').read())
+
+def checkLicense(node):
+    wans = []
+
+    try:
+        if LICENSES_JSON_OBJ == None:
+            return False;
+        if LICENSES_JSON_OBJ['licenses'] == None:
+            return False;
+        if LICENSES_JSON_OBJ['licenses']['list'] == None:
+            return False;
+
+        for license in LICENSES_JSON_OBJ['licenses']['list']:
+            if license['name'] == None:
+                continue;
+            if license['name'] != node:
+                continue;
+
+            validity = license['valid']
+
+            if validity is None:
+                print "License found: Invalid";
+                return False;
+
+            # if its a string type, convert to boolean
+            if (isinstance(validity, str)):
+                validity = ((validity == 'True') or (validity == 'true')) 
+
+            if validity == True:
+                print "License found: Valid";
+                return True;
+            if validity == False:
+                print "License found: Invalid";
+                return False;
+
+    except Exception, e:
+        print "Exception:", e
+        return False;
+
+    print "License not found for: " + node ;
+    return False;
+
+
+if len(sys.argv) < 2:
+    print "usage: %s node" % sys.argv[0]
+    sys.exit(1)
+
+isValid = checkLicense(sys.argv[1]);
+
+if (isValid):
+    sys.exit(0);
+else:
+    sys.exit(1);
