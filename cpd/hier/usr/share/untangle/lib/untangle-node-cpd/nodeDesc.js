80fc59950f9a6a25dd6f7006e18123274acb6c98
Mike Hotz <mahotz@untangle.com>
2012-02-28 15:57:36 +0000

    Removed old stuff from annotatedClasses.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@31270 8ebf8b4e-06f3-0310-961f-89f74618be54

--- cpd/hier/usr/share/untangle/lib/untangle-node-cpd/nodeDesc.js
+++ cpd/hier/usr/share/untangle/lib/untangle-node-cpd/nodeDesc.js
@@ -11,11 +11,6 @@
         "annotatedClasses" : {
             "javaClass": "java.util.LinkedList",
             "list": [
-                "com.untangle.node.cpd.CPDSettings",
-                "com.untangle.node.cpd.CaptureRule",
-                "com.untangle.node.cpd.PassedAddress",
-                "com.untangle.node.cpd.PassedClient",
-                "com.untangle.node.cpd.PassedServer",
                 "com.untangle.node.cpd.BlockEvent",
                 "com.untangle.node.cpd.CPDLoginEvent",
                 "com.untangle.node.cpd.HostDatabaseEntry"
