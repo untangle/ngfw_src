70c122c2d3ffefd48d126af9b6ec017701157c3d
Dirk Morris <dmorris@untangle.com>
2012-05-10 20:46:01 +0000

    massive simplification of syslog logic
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/src@31912 8ebf8b4e-06f3-0310-961f-89f74618be54

--- hier/usr/share/untangle/lib/untangle-node-license/nodeProperties.js
+++ hier/usr/share/untangle/lib/untangle-node-license/nodeProperties.js
@@ -3,7 +3,6 @@
         "className" : "com.untangle.node.license.LicenseManagerImpl",
         "name" : "untangle-node-license",
         "displayName" : "License Manager",
-        "syslogName" : "License_Manager",
         "type" : "SERVICE",
         "viewPosition" : 990,
         "singleInstance" : true
