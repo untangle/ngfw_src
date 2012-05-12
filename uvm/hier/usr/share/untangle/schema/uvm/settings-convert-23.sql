95c1f37567ef708fc5c5dafb1390b237fba54893
Dirk Morris <dmorris@untangle.com>
2012-05-12 04:37:05 +0000

    remove old conversion files
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@31928 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/share/untangle/schema/uvm/settings-convert-23.sql
+++
@@ -1,7 +0,0 @@
--- settings conversion for release-9.1
-
-ALTER TABLE settings.u_ab_settings
-  DROP COLUMN ab_configuration;
-
-ALTER TABLE settings.u_ab_repository_settings
-  ADD COLUMN enabled bool;
--- uvm/hier/usr/share/untangle/schema/uvm/settings-convert-24.sql
+++
@@ -1,3 +0,0 @@
--- settings conversion for release-9.2
-
-DROP TABLE settings.u_misc_settings;
