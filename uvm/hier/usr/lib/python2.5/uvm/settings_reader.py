ded567455ce9ad097d19a9e584f8afae31d2ee86
Mike Hotz <mahotz@untangle.com>
2012-05-17 18:12:36 +0000

    Added simple get_node_settings python function.  Usage looks something like this:
    from uvm.settings_reader import get_node_settings
    
    settings = get_node_settings('untangle-node-branding')
    print "Company Name = ", settings['companyName']
    
    Will return None if it can't find or load the node settings for any
    reason.  Likely needs cleanup and better error handling but this
    gets the job done for now.
    
    
    
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@31963 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ uvm/hier/usr/lib/python2.5/uvm/settings_reader.py
@@ -0,0 +1,47 @@
+import simplejson as json
+
+#-----------------------------------------------------------------------------
+
+def get_node_settings(nodename):
+
+    listfile = "@PREFIX@/usr/share/untangle/settings/untangle-vm/node_manager.js"
+
+    # find the settings for the argumented node name
+    try:
+
+        # read the node manager settings
+        file = open(listfile, "r")
+        data = file.read()
+        file.close()
+
+        # get the list of active nodes
+        nodeinfo = json.loads(data)
+        nodelist = nodeinfo['nodes']['list']
+
+        nodeid = None
+
+        # look for the target node name and grab the node id
+        for node in nodelist:
+            if (node['nodeName'] != nodename): continue
+            nodeid = node['id']
+            break
+
+        # not found so return empty string
+        if (nodeid == None): return(None)
+
+        # generate the settings file name using the node id we found above
+        nodefile = "@PREFIX@/usr/share/untangle/settings/" + nodename + "/settings_" + str(nodeid) + ".js"
+        file = open(nodefile, "r")
+        data = file.read()
+        file.close()
+
+        settings = json.loads(data)
+
+    # for all exceptions we just return empty
+    except:
+        return(None)
+
+    # return the settings
+    return(settings)
+
+#-----------------------------------------------------------------------------
