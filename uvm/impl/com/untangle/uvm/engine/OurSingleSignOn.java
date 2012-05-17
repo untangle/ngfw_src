094b9bd4d50d1960ef22c9051fe70dfdd834d83f
Dirk Morris <dmorris@untangle.com>
2012-05-17 19:00:27 +0000

    remove log statements that cause build process to fail
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@31964 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/impl/com/untangle/uvm/engine/OurSingleSignOn.java
+++ uvm/impl/com/untangle/uvm/engine/OurSingleSignOn.java
@@ -1,23 +1,7 @@
-/*
- * $HeadURL$
- * Copyright (c) 2003-2007 Untangle, Inc.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
- * NONINFRINGEMENT.  See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+/**
+ * $Id$
  */
-
 package com.untangle.uvm.engine;
-
 
 import java.io.IOException;
 
@@ -34,20 +18,16 @@ import org.apache.catalina.valves.ValveBase;
  * Sits in front of the Tomcat SingleSignOn to allow query string to contain
  * sso id, since we can't get cookies through jnlp.
  */
-public class OurSingleSignOn extends ValveBase {
-
-
+public class OurSingleSignOn extends ValveBase
+{
     // ----------------------------------------------------- Instance Variables
-
 
     public static final String SSO_SESSION_PARAMETER_NAME = "jsessionidsso";
 
     /**
      * Descriptive information about this Valve implementation.
      */
-    protected static String info =
-        "com.untangle.uvm.engine.OurSingleSignOn";
-
+    protected static String info = "com.untangle.uvm.engine.OurSingleSignOn";
 
     // ---------------------------------------------------------- Valve Methods
 
@@ -61,22 +41,23 @@ public class OurSingleSignOn extends ValveBase {
      * @exception ServletException if a servlet error occurs
      */
     public void invoke(Request request, Response response)
-        throws IOException, ServletException {
+        throws IOException, ServletException
+    {
 
         // Has a valid user already been authenticated?
-        if (containerLog.isDebugEnabled())
-            containerLog.debug("Process request for '" + request.getRequestURI() + "'");
+        //if (containerLog.isDebugEnabled())
+        //containerLog.debug("Process request for '" + request.getRequestURI() + "'");
         if (request.getUserPrincipal() != null) {
-            if (containerLog.isDebugEnabled())
-                containerLog.debug(" Principal '" + request.getUserPrincipal().getName() +
-                                   "' has already been authenticated");
+            //if (containerLog.isDebugEnabled())
+            //containerLog.debug(" Principal '" + request.getUserPrincipal().getName() +
+            //"' has already been authenticated");
             getNext().invoke(request, response);
             return;
         }
 
         // Check for the single sign on cookie
-        if (containerLog.isDebugEnabled())
-            containerLog.debug(" Checking for SSO cookie");
+        //if (containerLog.isDebugEnabled())
+        //containerLog.debug(" Checking for SSO cookie");
         Cookie cookie = null;
         String ssoVal = null;
         Cookie cookies[] = request.getCookies();
@@ -105,13 +86,13 @@ public class OurSingleSignOn extends ValveBase {
 
     }
 
-
     // ------------------------------------------------------ Protected Methods
 
     /**
      * Get sso session id from query string.
      */
-    protected String getSsoSessionId(Request req) {
+    protected String getSsoSessionId(Request req)
+    {
         return req.getParameter(SSO_SESSION_PARAMETER_NAME);
     }
 }
