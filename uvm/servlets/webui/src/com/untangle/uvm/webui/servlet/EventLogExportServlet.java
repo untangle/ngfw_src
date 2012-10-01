49e879e1aed7bd99473643d89e51c26e2d3635c6
Dirk Morris <dmorris@untangle.com>
2012-10-01 19:09:11 +0000

    setFetchSize to try and limit memory usage on export
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/release-9.3/work/src@33132 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/servlets/webui/src/com/untangle/uvm/webui/servlet/EventLogExportServlet.java
+++ uvm/servlets/webui/src/com/untangle/uvm/webui/servlet/EventLogExportServlet.java
@@ -1,7 +1,7 @@
 /**
  * $Id: EventLogExportServlet.java,v 1.00 2011/12/18 20:38:21 dmorris Exp $
  */
-package com.untangle.uvm.webui.servlet;
+package com.untangle.uvm.reports.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -24,9 +24,7 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.log4j.Logger;
 
-import com.untangle.uvm.UvmContext;
 import com.untangle.uvm.UvmContextFactory;
-import com.untangle.uvm.util.I18nUtil;
 
 /**
  * A servlet for export event log data
@@ -34,7 +32,6 @@ import com.untangle.uvm.util.I18nUtil;
 @SuppressWarnings({ "serial", "unchecked" })
 public class EventLogExportServlet extends HttpServlet
 {
-    private final int MAX_RESULTS = 10000000;
 	private final Logger logger = Logger.getLogger(getClass());
 
     /** character encoding */
@@ -62,7 +59,7 @@ public class EventLogExportServlet extends HttpServlet
         Long policyId = Long.parseLong(policyIdStr);
         logger.info("Export CSV( name:" + name + " query: " + query + " policyId: " + policyId + " columnList: " + columnListStr + ")");
 
-        ResultSet resultSet = UvmContextFactory.context().getEventsResultSet( query, policyId, MAX_RESULTS );
+        ResultSet resultSet = UvmContextFactory.context().getEventsResultSet( query, policyId, -1 );
         
         // Write content type and also length (determined via byte array).
         resp.setCharacterEncoding(CHARACTER_ENCODING);
@@ -70,6 +67,7 @@ public class EventLogExportServlet extends HttpServlet
         resp.setHeader("Content-Disposition","attachment; filename="+name+".csv");
         // Write the header
         resp.getWriter().write(columnListStr + "\n");
+        resp.getWriter().flush();
 
         if (resultSet == null)
             return;
