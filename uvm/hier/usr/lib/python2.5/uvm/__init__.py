9134289182a2c3669b8fd3abf64eba30d0453392
Dirk Morris <dmorris@untangle.com>
2012-12-10 18:56:13 +0000

    admin_manager doesnt exist anymore
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@33580 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/lib/python2.5/uvm/__init__.py
+++ uvm/hier/usr/lib/python2.5/uvm/__init__.py
@@ -1,6 +1,5 @@
 from uvm.toolbox_manager import ToolboxManager
 from uvm.node_manager import NodeManager
-from uvm.admin_manager import AdminManager
 from uvm.logging_manager import LoggingManager
 from uvm.network_manager import NetworkManager
 from uvm.uvm_manager import UvmManager
