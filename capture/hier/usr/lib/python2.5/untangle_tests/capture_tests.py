8d651d0c28b2ce6038f9dc583ea988afbcf15601
Mike Hotz <mahotz@untangle.com>
2013-01-15 16:19:49 +0000

    Merging the fixes to the test script from the 9.4 branch.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@33789 8ebf8b4e-06f3-0310-961f-89f74618be54

--- capture/hier/usr/lib/python2.5/untangle_tests/capture_tests.py
+++ capture/hier/usr/lib/python2.5/untangle_tests/capture_tests.py
@@ -8,6 +8,7 @@ from jsonrpc import JSONRPCException
 from uvm import Manager
 from uvm import Uvm
 from untangle_tests import ClientControl
+from untangle_tests import TestDict
 
 uvmContext = Uvm().getUvmContext()
 defaultRackId = 1
@@ -19,21 +20,22 @@ node = None
 
 def createCaptureRule():
     return {
-        "alert": False,
         "capture": True,
-        "category": "[no category]",
-        "clientAddress": "any",
-        "clientInterface": "any",
-        "days": "mon,tue,wed,thu,fri,sat,sun",
-        "description": "Any Interface",
-        "endTime": "23:59",
+        "description": "Test Rule - Capture all internal traffic",
+        "enabled": True,
+        "id": 1,
         "javaClass": "com.untangle.node.capture.CaptureRule",
-        "live": True,
-        "log": False,
-        "name": "[no name]",
-        "serverAddress": "any",
-        "startTime": "00:00"
-        };
+        "matchers": {
+            "javaClass": "java.util.LinkedList",
+            "list": [{
+                "invert": False,
+                "javaClass": "com.untangle.node.capture.CaptureRuleMatcher",
+                "matcherType": "SRC_INTF",
+                "value": "2"
+                }]
+            },
+        "ruleId": 1
+    };
 
 class CaptureTests(unittest.TestCase):
 
@@ -51,30 +53,30 @@ class CaptureTests(unittest.TestCase):
             if (uvmContext.nodeManager().isInstantiated(self.nodeName())):
                 print "ERROR: Node %s already installed" % self.nodeName()
                 raise Exception('node %s already instantiated' % self.nodeName())
-            node = uvmContext.nodeManager().instantiate(self.nodeName(), defaultRackId)
+            node = uvmContext.nodeManager().instantiateAndStart(self.nodeName(), defaultRackId)
             nodeData = node.getCaptureSettings()
 
     def test_010_clientIsOnline(self):
         result = clientControl.runCommand("wget -4 -t 2 --timeout=5 -a /tmp/capture_test_010.log -O /tmp/capture_test_010.out http://www.untangle.com/")
         assert (result == 0)
 
-    def test_020_daemonNotRunning_TrafficCheck(self):
+    def test_020_defaultTrafficCheck(self):
         result = clientControl.runCommand("wget -4 -t 2 --timeout=5 -a /tmp/capture_test_020.log -O /tmp/capture_test_020.out http://www.google.com/")
         assert (result == 0)
 
-    def test_021_daemonConfig_and_Startup(self):
+    def test_021_captureTrafficCheck(self):
         global node, nodeData
         nodeData['captureRules']['list'].append(createCaptureRule())
         node.setSettings(nodeData)
-        node.start()
+        result = clientControl.runCommand("wget -4 -t 2 --timeout=5 -a /tmp/capture_test_021.log -O /tmp/capture_test_021.out http://www.google.com/")
         assert (result == 0)
-
-    def test_022_basicCaptureRule(self):
-        result = clientControl.runCommand("wget -4 -t2 --timeout=5 -a /tmp/capture_test_021.log -O - http://www.google.com/ | grep -q captive-portal")
-        assert (result == 0)
+        search = clientControl.runCommand("grep -q 'Captive Portal' /tmp/capture_test_021.out")
+        assert (search == 0)
 
     def test_999_finalTearDown(self):
         global node
         uvmContext.nodeManager().destroy( node.getNodeSettings()["id"] )
         node = None
+
+TestDict.registerNode("capture", CaptureTests)
 
