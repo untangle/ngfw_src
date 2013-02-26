beae2295b571b4a3c726c9a9be2abcc18d07a5d1
John Coffin <jcoffin@gmail.com>
2013-02-26 00:38:49 +0000

    Add AD testing to captive portal tests
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@34016 8ebf8b4e-06f3-0310-961f-89f74618be54

--- capture/hier/usr/lib/python2.5/untangle_tests/capture_tests.py
+++ capture/hier/usr/lib/python2.5/untangle_tests/capture_tests.py
@@ -15,10 +15,13 @@ defaultRackId = 1
 clientControl = ClientControl()
 nodeData = None
 node = None
+nodeDataAD = None
+nodeAD = None
 
+#  /usr/share/untangle/web/webui/script/untangle-node-adconnector/settings.js
 #pdb.set_trace()
 
-def createCaptureRule():
+def createCaptureInternalNicRule():
     return {
         "capture": True,
         "description": "Test Rule - Capture all internal traffic",
@@ -37,6 +40,46 @@ def createCaptureRule():
         "ruleId": 1
     };
 
+
+def createCaptureLoginADSettings():
+    return {
+    "authenticationType": "ACTIVE_DIRECTORY",
+    "basicLoginFooter": "If you have any questions, please contact your network administrator.",
+    "basicLoginMessageText": "Please enter your username and password to connect to the internet.",
+    "basicLoginPageTitle": "Captive Portal",
+    "basicLoginPageWelcome": "Welcome to the Untangle Captive Portal",
+    "basicLoginPassword": "Password:",
+    "basicLoginUsername": "Username:",
+    "pageType": "BASIC_LOGIN",
+    };
+
+def createADSettings():
+    return {
+       "activeDirectorySettings": {
+            "LDAPHost": "10.5.6.48",
+            "LDAPPort": 389,
+            "OUFilter": "",
+            "domain": "adtesting.int",
+            "enabled": True,
+            "javaClass": "com.untangle.node.adconnector.ActiveDirectorySettings",
+            "superuser": "Administrator",
+            "superuserPass": "s90fly29"
+        },
+        "radiusSettings": {
+            "port": 1812, 
+            "enabled": False, 
+            "authenticationMethod": "PAP", 
+            "javaClass": "com.untangle.node.adconnector.RadiusSettings", 
+            "server": "1.2.3.4", 
+            "sharedSecret": "mysharedsecret"
+        }
+    }
+
+def nukeRules():
+    rules = node.getRules()
+    rules["list"] = [];
+    node.setRules(rules);
+
 class CaptureTests(unittest.TestCase):
 
     @staticmethod
@@ -44,17 +87,27 @@ class CaptureTests(unittest.TestCase):
         return "untangle-node-capture"
 
     @staticmethod
+    def nodeNameAD():
+        return "untangle-node-adconnector"
+
+    @staticmethod
     def vendorName():
         return "Untangle"
 
     def setUp(self):
-        global nodeData, node
+        global nodeData, node, nodeDataAD, nodeAD
         if node == None:
             if (uvmContext.nodeManager().isInstantiated(self.nodeName())):
                 print "ERROR: Node %s already installed" % self.nodeName()
                 raise Exception('node %s already instantiated' % self.nodeName())
             node = uvmContext.nodeManager().instantiateAndStart(self.nodeName(), defaultRackId)
             nodeData = node.getCaptureSettings()
+        if nodeAD == None:
+            if (uvmContext.nodeManager().isInstantiated(self.nodeNameAD())):
+                print "ERROR: Node %s already installed" % self.nodeNameAD()
+                raise Exception('node %s already instantiated' % self.nodeNameAD())
+            nodeAD = uvmContext.nodeManager().instantiateAndStart(self.nodeNameAD(), defaultRackId)
+            nodeDataAD = nodeAD.getActiveDirectorySettings()
 
     def test_010_clientIsOnline(self):
         result = clientControl.runCommand("wget -4 -t 2 --timeout=5 -a /tmp/capture_test_010.log -O /tmp/capture_test_010.out http://www.untangle.com/")
@@ -66,17 +119,35 @@ class CaptureTests(unittest.TestCase):
 
     def test_021_captureTrafficCheck(self):
         global node, nodeData
-        nodeData['captureRules']['list'].append(createCaptureRule())
+        nodeData['captureRules']['list'].append(createCaptureInternalNicRule())
         node.setSettings(nodeData)
         result = clientControl.runCommand("wget -4 -t 2 --timeout=5 -a /tmp/capture_test_021.log -O /tmp/capture_test_021.out http://www.google.com/")
         assert (result == 0)
         search = clientControl.runCommand("grep -q 'Captive Portal' /tmp/capture_test_021.out")
         assert (search == 0)
 
+    def test_030_captureADLogin(self):
+        global nodeData, node, nodeDataAD, nodeAD
+        # Configure AD settings
+        testResultString = nodeAD.getActiveDirectoryManager().getActiveDirectoryStatusForSettings(createADSettings())
+        # print 'testResultString %s' % testResultString  # debug line
+        nodeAD.setSettings(createADSettings())
+        assert ("success" in testResultString)
+        # Create Internal NIC capture rule with basic AD login page
+        nodeData['authenticationType']="ACTIVE_DIRECTORY"
+        nodeData['pageType'] = "BASIC_LOGIN"
+        node.setSettings(nodeData)
+        result = clientControl.runCommand("wget -4 -t 2 --timeout=5 -a /tmp/capture_test_021.log -O /tmp/capture_test_021.out http://www.google.com/")
+        assert (result == 0)
+        search = clientControl.runCommand("grep -q 'username and password' /tmp/capture_test_021.out")
+        assert (search == 0)
+
     def test_999_finalTearDown(self):
-        global node
+        global node, nodeAD
         uvmContext.nodeManager().destroy( node.getNodeSettings()["id"] )
+        uvmContext.nodeManager().destroy( nodeAD.getNodeSettings()["id"] )
         node = None
+        nodeAD = None
 
 TestDict.registerNode("capture", CaptureTests)
 
