c9dfe28ce58873032fec7669a49437cbef02ab46
Dirk Morris <dmorris@untangle.com>
2013-02-27 02:42:04 +0000

    remove unused class
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@34032 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/api/com/untangle/uvm/security/RFC2253Name.java
+++ uvm/api/com/untangle/uvm/security/RFC2253Name.java
@@ -1,36 +1,6 @@
-/*
- * $HeadURL$
- * Copyright (c) 2003-2007 Untangle, Inc. 
- *
- * This library is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2,
- * as published by the Free Software Foundation.
- *
- * This library is distributed in the hope that it will be useful, but
- * AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
- * NONINFRINGEMENT.  See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Linking this library statically or dynamically with other modules is
- * making a combined work based on this library.  Thus, the terms and
- * conditions of the GNU General Public License cover the whole combination.
- *
- * As a special exception, the copyright holders of this library give you
- * permission to link this library with independent modules to produce an
- * executable, regardless of the license terms of these independent modules,
- * and to copy and distribute the resulting executable under terms of your
- * choice, provided that you also meet, for each linked independent module,
- * the terms and conditions of the license of that module.  An independent
- * module is a module which is not derived from or based on this library.
- * If you modify this library, you may extend this exception to your version
- * of the library, but you are not obligated to do so.  If you do not wish
- * to do so, delete this exception statement from your version.
+/**
+ * $Id$
  */
-
 package com.untangle.uvm.security;
 import java.util.ArrayList;
 import java.util.List;
@@ -41,17 +11,6 @@ import javax.naming.ldap.Rdn;
 
 import com.untangle.node.util.Pair;
 
-
-//=============================================
-// DEVELOPER NOTES:
-//
-// I got lazy, and wrapped the Java APIs.  If
-// this is ever used in a performance-critical
-// app it should be re-written
-//
-//  - wrs 1/12/05
-//=============================================
-
 /**
  * Convience class for dealing with LDAP-style (RFC2253) Distinguished names.  For
  * now, just wraps the Java APIs (which are a bit cryptic, but take care of encoding
@@ -59,9 +18,18 @@ import com.untangle.node.util.Pair;
  * of naming.
  * <br>
  * Note that instances are not threadsafe.
- */
+ *
+ * DEVELOPER NOTES:
+ *
+ * I got lazy, and wrapped the Java APIs.  If
+ * this is ever used in a performance-critical
+ * app it should be re-written
+ *
+ *  - wrs 1/12/05
+ **/
 @SuppressWarnings("serial")
-public class RFC2253Name implements java.io.Serializable {
+public class RFC2253Name implements java.io.Serializable
+{
 
     private List<Pair<String, String>> m_members;
 
@@ -72,7 +40,8 @@ public class RFC2253Name implements java.io.Serializable {
     /**
      * Copy constructor
      */
-    public RFC2253Name(RFC2253Name copy) {
+    public RFC2253Name(RFC2253Name copy)
+    {
         m_members = new ArrayList<Pair<String, String>>();
         for(Pair<String, String> pair : copy.m_members) {
             m_members.add(new Pair<String, String>(pair.a, pair.b));
@@ -94,7 +63,8 @@ public class RFC2253Name implements java.io.Serializable {
      * @exception InvalidNameException if the type or value
      *            are in an unparsable format.
      */
-    public RFC2253Name(String org, String orgUnit, String city, String state, String country) throws InvalidNameException {
+    public RFC2253Name(String org, String orgUnit, String city, String state, String country) throws InvalidNameException
+    {
         m_members = new ArrayList<Pair<String, String>>();
         add("O", org);
         add("OU", orgUnit);
@@ -128,9 +98,9 @@ public class RFC2253Name implements java.io.Serializable {
      * @exception InvalidNameException if the type or value
      *            are in an unparsable format.
      */
-    public void add(String type, String value)
-        throws InvalidNameException {
-        new Rdn(type, value);//Will throw an exception
+    public void add(String type, String value) throws InvalidNameException
+    {
+        new Rdn(type, value); //Will throw an exception
         m_members.add(new Pair<String, String>(type, value));
     }
 
@@ -145,8 +115,8 @@ public class RFC2253Name implements java.io.Serializable {
      * @exception InvalidNameException if the type or value
      *            are in an unparsable format.
      */
-    public void add(String type, String value, int index)
-        throws InvalidNameException {
+    public void add(String type, String value, int index) throws InvalidNameException
+    {
         new Rdn(type, value);//Will throw an exception
         m_members.add(index, new Pair<String, String>(type, value));
     }
@@ -158,7 +128,8 @@ public class RFC2253Name implements java.io.Serializable {
      *
      * @return the index, or -1 if not found
      */
-    public int indexOf(String type) {
+    public int indexOf(String type)
+    {
         int index = 0;
         for(Pair<String, String> entry : m_members) {
             if(entry.a.equalsIgnoreCase(type)) {
@@ -175,7 +146,8 @@ public class RFC2253Name implements java.io.Serializable {
      *
      * @param index the index
      */
-    public void remove(int index) {
+    public void remove(int index)
+    {
         m_members.remove(index);
     }
 
@@ -210,7 +182,8 @@ public class RFC2253Name implements java.io.Serializable {
      *
      * @return all types.
      */
-    public List<String> listTypes() {
+    public List<String> listTypes()
+    {
         ArrayList<String> ret = new ArrayList<String>();
         for(Pair<String, String> entry : m_members) {
             ret.add(entry.a);
@@ -219,7 +192,8 @@ public class RFC2253Name implements java.io.Serializable {
     }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return toRFC2253String();
     }
 
@@ -232,8 +206,8 @@ public class RFC2253Name implements java.io.Serializable {
      *
      * @exception InvalidNameException if the string is an illegal Distinguished Name
      */
-    public static RFC2253Name parse(String str)
-        throws InvalidNameException {
+    public static RFC2253Name parse(String str) throws InvalidNameException
+    {
 
         List<Pair<String, String>> ret = new ArrayList<Pair<String, String>>();
 
@@ -253,7 +227,8 @@ public class RFC2253Name implements java.io.Serializable {
      *
      * @return the new name
      */
-    public static RFC2253Name create() {
+    public static RFC2253Name create()
+    {
         return new RFC2253Name(new ArrayList<Pair<String, String>>());
     }
 
