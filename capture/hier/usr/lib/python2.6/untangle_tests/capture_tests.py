e0fe3d37763b41ae5a39dc7177d79ee4b6e12f80
John Coffin <jcoffin@gmail.com>
2013-03-13 19:50:38 +0000

    Add logout test for AD capture and RADIUS authentication
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@34241 8ebf8b4e-06f3-0310-961f-89f74618be54

--- capture/hier/usr/lib/python2.6/untangle_tests/capture_tests.py
+++ capture/hier/usr/lib/python2.6/untangle_tests/capture_tests.py
@@ -3,22 +3,30 @@ import time
 import sys
 import pdb
 import os
+import re
+import subprocess
 from jsonrpc import ServiceProxy
 from jsonrpc import JSONRPCException
 from uvm import Manager
 from uvm import Uvm
 from untangle_tests import ClientControl
 from untangle_tests import TestDict
+from untangle_tests import SystemProperties
 
 uvmContext = Uvm().getUvmContext()
 defaultRackId = 1
 clientControl = ClientControl()
+systemProperties = SystemProperties()
 nodeData = None
 node = None
+nodeDataAD = None
+nodeAD = None
+adHost = "10.5.6.48"
+radiusHost = "10.5.6.71"
 
-#pdb.set_trace()
+# pdb.set_trace()
 
-def createCaptureRule():
+def createCaptureInternalNicRule():
     return {
         "capture": True,
         "description": "Test Rule - Capture all internal traffic",
@@ -37,6 +45,60 @@ def createCaptureRule():
         "ruleId": 1
     };
 
+
+def createCaptureLoginADSettings():
+    return {
+    "authenticationType": "ACTIVE_DIRECTORY",
+    "basicLoginFooter": "If you have any questions, please contact your network administrator.",
+    "basicLoginMessageText": "Please enter your username and password to connect to the internet.",
+    "basicLoginPageTitle": "Captive Portal",
+    "basicLoginPageWelcome": "Welcome to the Untangle Captive Portal",
+    "basicLoginPassword": "Password:",
+    "basicLoginUsername": "Username:",
+    "pageType": "BASIC_LOGIN",
+    };
+
+def createADSettings():
+    # Need to send Radius setting even though it's not used in this case.
+    return {
+       "activeDirectorySettings": {
+            "LDAPHost": adHost,
+            "LDAPPort": 389,
+            "OUFilter": "",
+            "domain": "adtesting.int",
+            "enabled": True,
+            "javaClass": "com.untangle.node.adconnector.ActiveDirectorySettings",
+            "superuser": "ATSadmin",
+            "superuserPass": "passwd"},
+        "radiusSettings": {
+            "port": 1812, 
+            "enabled": False, 
+            "authenticationMethod": "PAP", 
+            "javaClass": "com.untangle.node.adconnector.RadiusSettings", 
+            "server": radiusHost, 
+            "sharedSecret": "mysharedsecret"}
+    }
+
+def createRadiusSettings():
+    return {
+        "activeDirectorySettings": {
+            "enabled": False, 
+            "superuserPass": "passwd", 
+            "LDAPPort": "389", 
+            "OUFilter": "", 
+            "domain": "adtest.metaloft.com", 
+            "javaClass": "com.untangle.node.adconnector.ActiveDirectorySettings", 
+            "LDAPHost": adHost, 
+            "superuser": "Administrator"}, 
+        "radiusSettings": {
+            "port": 1812, 
+            "enabled": True, 
+            "authenticationMethod": "PAP", 
+            "javaClass": "com.untangle.node.adconnector.RadiusSettings", 
+            "server": radiusHost, 
+            "sharedSecret": "chakas"}
+        }
+
 class CaptureTests(unittest2.TestCase):
 
     @staticmethod
@@ -44,17 +106,28 @@ class CaptureTests(unittest2.TestCase):
         return "untangle-node-capture"
 
     @staticmethod
+    def nodeNameAD():
+        return "untangle-node-adconnector"
+
+    @staticmethod
     def vendorName():
         return "Untangle"
 
     def setUp(self):
-        global nodeData, node
+        global nodeData, node, nodeDataRD, nodeDataAD, nodeAD
         if node == None:
             if (uvmContext.nodeManager().isInstantiated(self.nodeName())):
                 print "ERROR: Node %s already installed" % self.nodeName()
                 raise Exception('node %s already instantiated' % self.nodeName())
             node = uvmContext.nodeManager().instantiateAndStart(self.nodeName(), defaultRackId)
             nodeData = node.getCaptureSettings()
+        if nodeAD == None:
+            if (uvmContext.nodeManager().isInstantiated(self.nodeNameAD())):
+                print "ERROR: Node %s already installed" % self.nodeNameAD()
+                raise Exception('node %s already instantiated' % self.nodeNameAD())
+            nodeAD = uvmContext.nodeManager().instantiateAndStart(self.nodeNameAD(), defaultRackId)
+            nodeDataAD = nodeAD.getActiveDirectorySettings()
+            nodeDataRD = nodeAD.getRadiusSettings()
 
     def test_010_clientIsOnline(self):
         result = clientControl.runCommand("wget -4 -t 2 --timeout=5 -a /tmp/capture_test_010.log -O /tmp/capture_test_010.out http://www.untangle.com/")
@@ -66,17 +139,90 @@ class CaptureTests(unittest2.TestCase):
 
     def test_021_captureTrafficCheck(self):
         global node, nodeData
-        nodeData['captureRules']['list'].append(createCaptureRule())
+        nodeData['captureRules']['list'].append(createCaptureInternalNicRule())
         node.setSettings(nodeData)
         result = clientControl.runCommand("wget -4 -t 2 --timeout=5 -a /tmp/capture_test_021.log -O /tmp/capture_test_021.out http://www.google.com/")
         assert (result == 0)
         search = clientControl.runCommand("grep -q 'Captive Portal' /tmp/capture_test_021.out")
         assert (search == 0)
 
+    adResult = subprocess.call(["ping","-c","1",adHost],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    @unittest2.skipIf(adResult != 0,  "No AD server available")
+
+    def test_030_captureADLogin(self):
+        global nodeData, node, nodeDataAD, nodeAD
+        # Configure AD settings
+        testResultString = nodeAD.getActiveDirectoryManager().getActiveDirectoryStatusForSettings(createADSettings())
+        # print 'testResultString %s' % testResultString  # debug line
+        nodeAD.setSettings(createADSettings())
+        assert ("success" in testResultString)
+        # Create Internal NIC capture rule with basic AD login page
+        nodeData['authenticationType']="ACTIVE_DIRECTORY"
+        nodeData['pageType'] = "BASIC_LOGIN"
+        node.setSettings(nodeData)
+        # check that basic captive page is shown
+        result = clientControl.runCommand("wget -4 -t 2 --timeout=5 -a /tmp/capture_test_030.log -O /tmp/capture_test_030.out http://www.google.com/")
+        assert (result == 0)
+        search = clientControl.runCommand("grep -q 'username and password' /tmp/capture_test_030.out")
+        assert (search == 0)
+        # print 'Login page found'  # debug line
+        # check if AD login and password 
+        appid = str(node.getNodeSettings()["id"])
+        # print 'appid is %s' % appid  # debug line
+        # get the IP address of the capture page 
+        gatewayIPAddress = systemProperties.internalInterfaceIP()
+        # print 'gatewayIPAddress is %s' % gatewayIPAddress
+        result = clientControl.runCommand("wget -a /tmp/capture_test_030a.log -O /tmp/capture_test_030a.out  \'http://" + gatewayIPAddress + "/capture/handler.py/authpost?username=atsadmin&password=passwd&nonce=9abd7f2eb5ecd82b&method=GET&appid=" + appid + "&host=test.untangle.com&uri=/\'",True)
+        search = clientControl.runCommand("grep -q 'Hi!' /tmp/capture_test_030a.out")
+        assert (search == 0)
+        # logout user to clean up test.
+        # wget http://<internal IP>/capture/logout  
+        result = clientControl.runCommand("wget -4 -t 2 --timeout=5 -a /tmp/capture_test_030c.log -O /tmp/capture_test_030c.out http://" + gatewayIPAddress + "/capture/logout")
+        assert (result == 0)
+        search = clientControl.runCommand("grep -q 'logout' /tmp/capture_test_030c.out")
+        assert (search == 0)
+
+    RadiusResult = subprocess.call(["ping","-c","1",radiusHost],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    @unittest2.skipIf(RadiusResult != 0,  "No RADIUS server available")
+
+    def test_040_captureRadiusLogin(self):
+        global nodeData, node, nodeDataRD, nodeDataAD, nodeAD
+        # Configure RADIUS settings
+        nodeAD.setSettings(createRadiusSettings())
+        testResultString = nodeAD.getRadiusManager().getRadiusStatusForSettings(createRadiusSettings(),"normal","passwd")
+        print 'testResultString %s' % testResultString  # debug line
+        assert ("success" in testResultString)
+        # Create Internal NIC capture rule with basic AD login page
+        nodeData['authenticationType']="RADIUS"
+        nodeData['pageType'] = "BASIC_LOGIN"
+        node.setSettings(nodeData)
+        # check that basic captive page is shown
+        result = clientControl.runCommand("wget -4 -t 2 --timeout=5 -a /tmp/capture_test_040.log -O /tmp/capture_test_040.out http://www.google.com/")
+        assert (result == 0)
+        search = clientControl.runCommand("grep -q 'username and password' /tmp/capture_test_030.out")
+        assert (search == 0)
+        # check if RADIUS login and password 
+        appid = str(node.getNodeSettings()["id"])
+        # print 'appid is %s' % appid  # debug line
+        # get the IP address of the capture page 
+        gatewayIPAddress = systemProperties.internalInterfaceIP()
+        # print 'gatewayIPAddress is %s' % gatewayIPAddress
+        result = clientControl.runCommand("wget -a /tmp/capture_test_040b.log -O /tmp/capture_test_040b.out  \'http://" + gatewayIPAddress + "/capture/handler.py/authpost?username=normal&password=passwd&nonce=9abd7f2eb5ecd82b&method=GET&appid=" + appid + "&host=test.untangle.com&uri=/\'",True)
+        search = clientControl.runCommand("grep -q 'Hi!' /tmp/capture_test_030b.out")
+        assert (search == 0)
+        # logout user to clean up test.
+        # wget http://<internal IP>/capture/logout  
+        result = clientControl.runCommand("wget -4 -t 2 --timeout=5 -a /tmp/capture_test_030c.log -O /tmp/capture_test_030c.out http://" + gatewayIPAddress + "/capture/logout")
+        assert (result == 0)
+        search = clientControl.runCommand("grep -q 'logout' /tmp/capture_test_040c.out")
+        assert (search == 0)
+
+
     def test_999_finalTearDown(self):
-        global node
+        global node, nodeAD
         uvmContext.nodeManager().destroy( node.getNodeSettings()["id"] )
+        uvmContext.nodeManager().destroy( nodeAD.getNodeSettings()["id"] )
         node = None
+        nodeAD = None
 
 TestDict.registerNode("capture", CaptureTests)
-
