b3111bc642ede1ec60c43060d113f891d8c5f724
Sebastien Delafond <sdelafond@gmail.com>
2013-03-20 15:57:27 +0000

    [buildtools]Passing a non-empty BUILDBOT environment variable will try to build the way the buildbot does it
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@34343 8ebf8b4e-06f3-0310-961f-89f74618be54

--- buildtools/buildtools.rb
+++ buildtools/buildtools.rb
@@ -4,14 +4,22 @@
 
 ENV["JAVA_HOME"] = "/usr/lib/jvm/java-6-sun"
 
-POTENTIAL_SRC_HOMES = [ ENV['SRC_HOME'], '../../work/src', '../../src' ]
+$DevelBuild = ARGV.grep(/install/).empty?
+
+POTENTIAL_SRC_HOMES = [  ENV['SRC_HOME'], '../../work/src', '../../src' ]
 POTENTIAL_SRC_HOMES << '.' unless `pwd` =~ /hades/
 SRC_HOME = POTENTIAL_SRC_HOMES.compact.find do |d|
   File.exist?(d)
 end
-puts "SRC_HOME = #{SRC_HOME}"
 
-$DevelBuild = ARGV.grep(/install/).empty?
+if not (ENV['BUILDBOT'].nil? or ENV['BUILDBOT'].empty?) then
+  $DevelBuild = false
+  if `pwd` =~ /hades/ then
+    Object.send(:remove_const, :SRC_HOME)
+    SRC_HOME=""
+  end
+end
+puts "SRC_HOME = #{SRC_HOME}"
 puts "DevelBuild = #{$DevelBuild}"
 
 ## This is how you define where the stamp file will go
