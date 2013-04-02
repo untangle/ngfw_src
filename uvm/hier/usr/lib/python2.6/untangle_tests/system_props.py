35fa1b34b2cd70823bbca8c039e017c87cc296ea
John Coffin <jcoffin@gmail.com>
2013-04-02 02:16:06 +0000

    Add IPv4/IPv6 manipulation library and update system_props to handle new network settings
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@34451 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/lib/python2.6/untangle_tests/system_props.py
+++ uvm/hier/usr/lib/python2.6/untangle_tests/system_props.py
@@ -1,12 +1,18 @@
+# -*- coding: utf-8 -*-
 import os
 import sys
 import subprocess
 import simplejson as json
+import socket
+import ipaddr
 
-class SystemProperties:
+class SystemProperties():
+
+    global network_settings_file
+    network_settings_file = '/usr/share/untangle/settings/untangle-vm/network.js'
 
     def getInterface(self, name):
-        NETCONFIG_JSON_OBJ = json.loads(open('/usr/share/untangle/settings/untangle-vm/network.js', 'r').read())
+        NETCONFIG_JSON_OBJ = json.loads(open(network_settings_file , 'r').read())
         for intf in NETCONFIG_JSON_OBJ['interfaces']['list']:
             if intf['name'] == name:
                 return intf
@@ -26,5 +32,28 @@ class SystemProperties:
         # else:
         #     return "UnknownIP"
 
+    def findInterfaceIPbyIP(self, remoteIP):
+        # finds the first IP address of any interface which is on the same network as the IP passed in
+        NETCONFIG_JSON_OBJ = json.loads(open(network_settings_file , 'r').read())
+        for intf in NETCONFIG_JSON_OBJ['interfaces']['list']:
+            if intf['configType'] == 'ADDRESSED':
+                testIP = intf['v4StaticAddress']
+                testMask = intf['v4StaticNetmask']
+                testRange = testIP + '/' + self.get_net_size(testMask)
+                testNet = ipaddr.IPNetwork(testRange)
+                testAddr = ipaddr.IPAddress(remoteIP)
+                if testAddr in testNet:
+                    return testIP
+        return None
 
-        
+    def get_net_size(self, netmask):
+        netmaskpart = netmask.split('.')
+        binary_str = ''
+        for octet in netmaskpart:
+            binary_str += bin(int(octet))[2:].zfill(8)
+        return str(len(binary_str.rstrip('0')))
+
+
+# debug
+# systemProperties = SystemProperties()
+# print systemProperties.findInterfaceIPbyIP('192.168.10.31')
