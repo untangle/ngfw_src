5fe81d3cfa24966610e0e455a90ef63b0753ed2a
John Coffin <jcoffin@gmail.com>
2013-06-12 05:38:37 +0000

    Find IP address and netmask by device name and handle DHCP cases
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@34970 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/lib/python2.6/untangle_tests/system_props.py
+++ uvm/hier/usr/lib/python2.6/untangle_tests/system_props.py
@@ -5,6 +5,8 @@ import subprocess
 import simplejson as json
 import socket
 import ipaddr
+import fcntl
+import struct
 
 class SystemProperties():
 
@@ -36,8 +38,10 @@ class SystemProperties():
         # finds the first IP address of any interface which is on the same network as the IP passed in
         NETCONFIG_JSON_OBJ = json.loads(open(network_settings_file , 'r').read())
         for intf in NETCONFIG_JSON_OBJ['interfaces']['list']:
+            # is interface configured
             if (intf['configType'] == 'ADDRESSED'):
-                try:
+                if (intf['v4ConfigType'] == 'STATIC'):
+                    # is this a static IP
                     testIP = intf['v4StaticAddress']
                     testMask = intf['v4StaticNetmask']
                     testRange = testIP + '/' + self.get_net_size(testMask)
@@ -45,8 +49,22 @@ class SystemProperties():
                     testAddr = ipaddr.IPAddress(remoteIP)
                     if testAddr in testNet:
                         return testIP
-                except:
-                    pass  # no static ips   
+                    else:
+                        pass
+                elif (intf['v4ConfigType'] == 'AUTO'):
+                    # is this a dynamic IP
+                    nicDevice = str(intf['physicalDev'])
+                    testIP = self.get_ip_address(nicDevice)
+                    testMask =  self.get_netmask(nicDevice)
+                    testRange = testIP + '/' + self.get_net_size(testMask)
+                    testNet = ipaddr.IPNetwork(testRange)
+                    testAddr = ipaddr.IPAddress(remoteIP)
+                    if testAddr in testNet:
+                        return testIP
+                    else:
+                        pass
+                else:
+                    pass
             else:
                 pass
         return None
@@ -58,7 +76,24 @@ class SystemProperties():
             binary_str += bin(int(octet))[2:].zfill(8)
         return str(len(binary_str.rstrip('0')))
 
+    def get_ip_address(self, ifname):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(
+            s.fileno(),
+            0x8915,  # SIOCGIFADDR
+            struct.pack('256s', ifname[:15])
+        )[20:24])
 
+    def get_netmask(self, ifname):
+        s= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(
+            s, 
+            35099,
+            struct.pack('256s', ifname)
+        )[20:24])    
+        
 # debug
 # systemProperties = SystemProperties()
-# print systemProperties.findInterfaceIPbyIP('192.168.10.31')
+# print "return value <%s>" % systemProperties.findInterfaceIPbyIP('10.5.6.60')
+# print "return value <%s>" % systemProperties.get_ip_address('eth0')
+# print "return value <%s>" % systemProperties.get_ip_address('eth4')
