d7ebbeebb3b903f120dbcc436d267bf2dd2c7890
John Coffin <jcoffin@gmail.com>
2013-08-25 05:35:42 +0000

    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@35723 8ebf8b4e-06f3-0310-961f-89f74618be54

--- openvpn/hier/usr/share/untangle/lib/untangle-node-openvpn/installer/openvpn/bin/addtap.bat
+++
@@ -1,3 +0,0 @@
-rem Add a new TAP virtual ethernet adapter
-"C:\Program Files (x86)\OpenVPN\bin\tapinstall.exe" install "C:\Program Files (x86)\OpenVPN\driver\OemWin2k.inf" tap0901
-pause
--- openvpn/hier/usr/share/untangle/lib/untangle-node-openvpn/installer/openvpn/bin/deltapall.bat
+++
@@ -1,4 +0,0 @@
-echo WARNING: this script will delete ALL TAP virtual adapters (use the device manager to delete adapters one at a time)
-pause
-"C:\Program Files (x86)\OpenVPN\bin\tapinstall.exe" remove tap0901
-pause
