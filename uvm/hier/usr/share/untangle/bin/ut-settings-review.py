d393832db303269954dbebc43c0eaa7ace0ed25d
Dirk Morris <dmorris@untangle.com>
2013-09-24 05:45:24 +0000

    settings review utility
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@35939 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ uvm/hier/usr/share/untangle/bin/ut-settings-review.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+import os
+import getopt
+import sys
+import subprocess
+import re
+
+def usage():
+    print """\
+usage: %s filename
+example: %s network.js reviews network.js changes
+""" % (sys.argv[0], sys.argv[0])
+    sys.exit(1)
+
+try:
+     opts, args = getopt.getopt(sys.argv[1:], "", [])
+except getopt.GetoptError, err:
+     print str(err)
+     usage()
+     sys.exit(2)
+
+if len(args) < 1:
+    usage();
+
+filename = args[0]
+
+prefix = "@PREFIX@"
+if prefix == "@PREFIX@": prefix = "" # for running in src
+prev = None
+
+cmd = "/usr/bin/find %s/usr/share/untangle/settings/ -type f -name '%s*' | xargs ls -1tr " % (prefix, filename)
+for file in subprocess.Popen(["/bin/sh","-c",cmd], stdout=subprocess.PIPE).communicate()[0].split('\n'):
+    if file == None or file.strip() == "":
+        continue
+    if prev == None:
+        prev = file
+        continue
+
+    print " prev = %s, file = %s " % (prev, file)
+    m = re.search(r'(\d\d\d\d)-(\d\d)-(\d\d)-(\d\d)(\d\d)', file)
+    if m == None or m.group(1) == None or m.group(2) == None or m.group(3) == None or m.group(4) == None or m.group(5) == None :
+        print "Unable to find date of file: %s" % file
+        continue
+
+    year = m.group(1)
+    month = m.group(2)
+    day = m.group(3)
+    hour = m.group(4)
+    min = m.group(5)
+
+    os.system("/usr/bin/clear")
+    print "= The following changes were made at %s-%s-%s at %s:%s =\n" % (year, month, day, hour, min)
+    os.system("diff -urN %s %s" % (prev, file))
+
+    print "\n\n"
+    raw_input("Press Enter to continue...")
+
+    prev = file
