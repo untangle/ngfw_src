f7aebfa8a359d7d2a66d4fc371e784c96ca478bc
Dirk Morris <dmorris@untangle.com>
2013-11-05 00:27:17 +0000

    remove apt_manager
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@36331 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/lib/python2.6/uvm/__init__.py
+++ uvm/hier/usr/lib/python2.6/uvm/__init__.py
@@ -1,9 +1,7 @@
-from uvm.apt_manager import AptManager
 from uvm.node_manager import NodeManager
 from uvm.logging_manager import LoggingManager
 from uvm.network_manager import NetworkManager
 from uvm.uvm_manager import UvmManager
-from uvm.message_manager import MessageManager
 from uvm.license_manager import LicenseManager
 
 from uvm.manager import Manager
