de10c89162b8c2ec97938cb9e36c6ed44944374d
Dirk Morris <dmorris@untangle.com>
2013-11-19 23:32:09 +0000

    remove copyrights from GPL code
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@36443 8ebf8b4e-06f3-0310-961f-89f74618be54

--- ips/src/com/untangle/node/ips/options/OptionArg.java
+++ ips/src/com/untangle/node/ips/options/OptionArg.java
@@ -1,21 +1,6 @@
-/*
- * $HeadURL: svn://chef/branch/prod/web-ui/work/src/ips/impl/com/untangle/node/ips/options/IpsOption.java $
- * Copyright (c) 2003-2007 Untangle, Inc.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
- * NONINFRINGEMENT.  See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+/**
+ * $Id: OptionArg.java,v 1.00 2013/11/19 15:27:42 dmorris Exp $
  */
-
 package com.untangle.node.ips.options;
 
 import com.untangle.node.ips.IpsDetectionEngine;
