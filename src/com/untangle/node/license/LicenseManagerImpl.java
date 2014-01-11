dfc71cba874910727bed37108e21ac213e72ba0e
Dirk Morris <dmorris@untangle.com>
2014-01-11 01:23:09 +0000

    untangle-node-router and casings always have valid liceneses
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/src@36831 8ebf8b4e-06f3-0310-961f-89f74618be54

--- src/com/untangle/node/license/LicenseManagerImpl.java
+++ src/com/untangle/node/license/LicenseManagerImpl.java
@@ -656,11 +656,17 @@ public class LicenseManagerImpl extends NodeBase implements LicenseManager
         else if ("untangle-node-openvpn".equals(identifier)) return true;
         else if ("untangle-node-phish".equals(identifier)) return true;
         else if ("untangle-node-protofilter".equals(identifier)) return true;
+        else if ("untangle-node-router".equals(identifier)) return true;
         else if ("untangle-node-reporting".equals(identifier)) return true;
         else if ("untangle-node-shield".equals(identifier)) return true;
         else if ("untangle-node-spamassassin".equals(identifier)) return true;
         else if ("untangle-node-webfilter".equals(identifier)) return true;
 
+        if ("untangle-node-license".equals(identifier)) return true;
+        else if ("untangle-casing-http".equals(identifier)) return true;
+        else if ("untangle-casing-ftp".equals(identifier)) return true;
+        else if ("untangle-casing-smtp".equals(identifier)) return true;
+        
         return false;
     }
 
