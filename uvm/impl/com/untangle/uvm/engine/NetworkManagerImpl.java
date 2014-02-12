e8bf26dbdb6fc50d69861693367bde16e7c5fd1c
Dirk Morris <dmorris@untangle.com>
2014-02-12 19:59:34 +0000

    if its a netboot set authorized_keys
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@37170 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/impl/com/untangle/uvm/engine/NetworkManagerImpl.java
+++ uvm/impl/com/untangle/uvm/engine/NetworkManagerImpl.java
@@ -678,6 +678,15 @@ public class NetworkManagerImpl implements NetworkManager
             newSettings.setForwardFilterRules( new LinkedList<FilterRule>() );
             newSettings.setInputFilterRules( defaultInputFilterRules() );
             newSettings.setStaticDhcpEntries( new LinkedList<DhcpStaticEntry>() );
+
+            /**
+             * If this is a netboot (untangle local installation)
+             * Copy the authorized keys to root's ssh
+             */
+            if ( UvmContextFactory.context().isNetBoot() ) {
+                UvmContextFactory.context().execManager().exec( "if [ ! -d /root/.ssh ] ; then mkdir /root/.ssh ; chmod 700 /root/.ssh ; fi" );
+                UvmContextFactory.context().execManager().exec( "if [ ! -f /root/.ssh/authorized_keys2 ] ; then cp -f /usr/share/untangle-support-keyring/authorized_keys2 /root/.ssh/ ; chmod 700 /root/.ssh/authorized_keys2 ; fi " );
+            }
         }
         catch (Exception e) {
             logger.error("Error creating Network Settings",e);
