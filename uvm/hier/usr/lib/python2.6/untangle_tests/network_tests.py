e7c6aafea2547f9d0867d974791de23b81c99b5e
John Coffin <jcoffin@gmail.com>
2014-02-16 20:07:13 +0000

    clean up comments
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@37203 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/lib/python2.6/untangle_tests/network_tests.py
+++ uvm/hier/usr/lib/python2.6/untangle_tests/network_tests.py
@@ -264,21 +264,16 @@ def getUDPSpeed():
     
 def sendUDPPackets():
     # Use mgen to send UDP packets.  Returns number of packets received.
-    # start mgen receiver on client. radius server.
+    # start mgen receiver on client.
     os.system("rm mgen_recv.dat >/dev/null 2>&1")
     clientControl.runCommand("rm mgen_recv.dat >/dev/null 2>&1")
     clientControl.runCommand("mgen output mgen_recv.dat port 5000 >/dev/null 2>&1 &")
-    # os.system("rm mgen_recv.dat >/dev/null 2>&1")
-    # os.system("ssh -o 'StrictHostKeyChecking=no' -i /usr/lib/python2.6/untangle_tests/testShell.key testshell@" + radiusServer + " \"rm mgen_recv.dat >/dev/null 2>&1\"")
-    # os.system("ssh -o 'StrictHostKeyChecking=no' -i /usr/lib/python2.6/untangle_tests/testShell.key testshell@" + radiusServer + " \"/home/fnsadmin/MGEN/mgen output mgen_recv.dat port 5000 >/dev/null 2>&1 &\"")
     # start the UDP generator on the radius server.
     os.system("ssh -o 'StrictHostKeyChecking=no' -i /usr/lib/python2.6/untangle_tests/testShell.key testshell@" + radiusServer + " \"input /home/testshell/udp-load-ats.mgn txlog log mgen_snd.log >/dev/null 2>&1\"")
-    # clientControl.runCommand("mgen input /home/testshell/udp-load-ats.mgn txlog log mgen_snd.log >/dev/null 2>&1")
     # wait for UDP to finish
     time.sleep(70)
     # kill mgen receiver    
     clientControl.runCommand("pkill mgen >/dev/null 2>&1")
-    # os.system("ssh -o 'StrictHostKeyChecking=no' -i /usr/lib/python2.6/untangle_tests/testShell.key testshell@" + radiusServer + " \"pkill mgen >/dev/null 2>&1\"")
     os.system("scp -o 'StrictHostKeyChecking=no' -i /usr/lib/python2.6/untangle_tests/testShell.key testshell@" + ClientControl.hostIP + ":mgen_recv.dat ./ >/dev/null 2>&1")
     wcResults = subprocess.Popen(["wc","-l","mgen_recv.dat"], stdout=subprocess.PIPE).communicate()[0]
     # print "wcResults " + str(wcResults)
