77db8b18a6c2f19907e9358a4768c199cee19cc3
Dirk Morris <dmorris@untangle.com>
2014-02-22 02:59:58 +0000

    no need to restart pyconnector on networking save now (its not used for downloading apps anymore)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@37242 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/impl/com/untangle/uvm/engine/NetworkManagerImpl.java
+++ uvm/impl/com/untangle/uvm/engine/NetworkManagerImpl.java
@@ -184,11 +184,6 @@ public class NetworkManagerImpl implements NetworkManager
         // notify interested parties that the settings have changed
         callNetworkListeners();
 
-        // restart pyconnector
-        // we only do this here and not in a networking hook so that its only
-        // done when new settings are saved, not everytime networking is restarted
-        UvmContextFactory.context().execManager().execResult("/etc/init.d/untangle-pyconnector restart");
-
         if ( errorOccurred ) {
             throw new RuntimeException(errorStr);
         }
