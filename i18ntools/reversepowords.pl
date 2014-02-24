13c9f40e5d5c1795f8cbf198ee2a99b17af91f23
John Coffin <jcoffin@gmail.com>
2014-02-24 22:51:09 +0000

    improved reverse wording for translation testing
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@37251 8ebf8b4e-06f3-0310-961f-89f74618be54

--- i18ntools/reversepowords.pl
+++ i18ntools/reversepowords.pl
@@ -6,7 +6,7 @@ my $PROGRAM = "reversepowords";
 my $VERSION = "0.1";
 # ./reversepowords.pl -dir=/home/jcoffin/translations081110/es 
 #                     -savedir=/home/jcoffin/new 
-#                     -countonly=1
+
 # Input validation bits
 # ./reversepowords -files
 GetOptions("help"=>\$hflag,
@@ -14,7 +14,6 @@ GetOptions("help"=>\$hflag,
             "files=s"=>\$files,
             "dir=s"=>\$dir,
             "savedir=s"=>\$savedir,
-            "countonly=s"=>\$countonly,
             );
             
 usage() if $hflag;
@@ -38,7 +37,6 @@ if (!($files)) {
 if ($files) {
     # print "Process files <$files>\n" if $files;
     @files2process = split / /, $files;
-    $wordcount = 0;
     foreach $file (@files2process) {
         print "processing file <$file>\n";
         # open file
@@ -51,31 +49,28 @@ if ($files) {
         $newfilename =~ s/\.pot$/\.po/;
         # add language to filename
         # $newfilename =~ s/\./-test\./;
-        if (!($countonly)) {
-            open(FILEWRITE, "> $savedir/$newfilename")|| 
-                print("Could not open file $savedir/$newfilename for overwriting !");;
-            print FILEWRITE @newfile;
-            close(FILEWRITE);
-        }
+        open(FILEWRITE, "> $savedir/$newfilename")|| 
+            print("Could not open file $savedir/$newfilename for overwriting !");;
+        print FILEWRITE @newfile;
+        close(FILEWRITE);
     }
-    print "WordCount is <$wordcount>\n";
 }
  
 exit;
         
 sub processDataFile {
-    local (@datafile) = @_;
-    local @newdatafile =();
+    my (@datafile) = @_;
+    my @newdatafile =();
     $wording_loaded = 0;
-    $newline = "msgstr \"";
+    $newline = "msgstr ";
     foreach $line (@datafile) {
         # we completed a word reversal lines, write them out
         if ($line  =~ /^msgstr/) {
             $wording_loaded = 0;
-            $newline =~ s/\"$//;
+            # $newline =~ s/\"$//;
             # print "Write out newline <$newline>\n";
             push @newdatafile, $newline;
-            $newline = "msgstr \"";
+            $newline = "msgstr ";
         } elsif ($line  =~ /^\"Plural-Forms/) {
             push @newdatafile, "\"Plural-Forms: nplurals=0; plural=0;\\n\""; 
         } elsif ($line  =~ /^\"Content-Type/) {
@@ -93,41 +88,41 @@ sub processDataFile {
             $line =~ /\"(.*)\"/;
             # print "Words <$1>\n";
             # print "Words found <$1>\n";
-            @words = split / /, $1;
-            $wordcount += $#words;
-            foreach $word (@words) {
-                if ($word =~/\{|\%|\"/) {
-                    # don't reverse words with special characters
-                    $newline .= $word;
-                } elsif ($word =~/\\\\n/) {
-                    # treat words with double \\ \n in a special way
-                    @specialword = split /\\\\n/, $word;
-                    $i=0;
-                    while ($specialword[$i]) {
-                    	$specialword[$i] = reverse $specialword[$i];
-	                    $i++;
-                    }
-                    $newline .= join("\\\\n",@specialword);                    
-                    $newline .= $word;
-                } elsif ($word =~/\\n/) {
-                    # treat words with \n in a special way
-                    @specialword = split /\\n/, $word;
-                    $i=0;
-                    while ($specialword[$i]) {
-                    	$specialword[$i] = reverse $specialword[$i];
-	                    $i++;
-                    }
-                    $newline .= join("\\n",@specialword);                    
-                    $newline .= $word;
+            my $processline = $1;
+            # print "Line is <" . $processline . ">\n";
+            my @chars = split("", $processline);
+            my $notescaped = 1;
+            @wordstack = ();
+            $newline .= "\"";
+            foreach $wordchar (@chars) {
+                # print "Char is <" . $wordchar . ">\n";
+                # print "escape is <" . $notescaped . ">\n";
+                if (($wordchar =~ m/[a-zA-Z]/) && ($notescaped)){
+                    # print "new char <" . $wordchar . ">\n";
+                    push(@wordstack, $wordchar);
                 } else {
-                    $newline .= reverse $word;
-                    $newline =~ s/n\\\./\.\\n/g;
+                    if ($wordchar =~ m/\\|\%/) {
+                        # ignore next character
+                        $notescaped = 0;
+                    } else {
+                        $notescaped = 1;
+                    }
+                    $rword = "";
+                    if (@wordstack) {
+                        $rword = join('', reverse @wordstack);
+                        @wordstack = ();
+                    }
+                    $newline .= $rword;
+                    $newline .= $wordchar;
                 }
-                $newline .= " ";
             }
-            $newline =~ s/\s+$//;
-            $newline .= "\"\n\"";
-            # print "Newline is <$newline>\n";
+            $rword = "";
+            if (@wordstack) {
+                $rword = join('', reverse @wordstack);
+                @wordstack = ();
+            }
+            $newline .= $rword;
+            $newline .= "\"\n";
         } else {
             push @newdatafile, $line;
         }
