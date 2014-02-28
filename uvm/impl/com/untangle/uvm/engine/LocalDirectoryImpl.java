07fb4bb2a84398c15a5d48383ee1e0b43a01934e
Mike Hotz <mahotz@untangle.com>
2014-02-28 20:28:51 +0000

    Updated LocalDirectory to export username/password info to chap-secrets so it can be used to authenticate L2TP connections.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@37311 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/impl/com/untangle/uvm/engine/LocalDirectoryImpl.java
+++ uvm/impl/com/untangle/uvm/engine/LocalDirectoryImpl.java
@@ -7,6 +7,7 @@ import com.untangle.uvm.LocalDirectory;
 import com.untangle.uvm.LocalDirectoryUser;
 import com.untangle.uvm.SettingsManager;
 import com.untangle.uvm.UvmContextFactory;
+
 import org.apache.commons.codec.binary.Base64;
 import org.apache.log4j.Logger;
 
@@ -15,6 +16,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.io.FileWriter;
 
 /**
  * Local Directory stores a local list of users
@@ -23,34 +25,33 @@ public class LocalDirectoryImpl implements LocalDirectory
 {
     private final Logger logger = Logger.getLogger(getClass());
 
-    private final static String LOCAL_DIRECTORY_SETTINGS_FILE = System.getProperty( "uvm.settings.dir" ) + "/untangle-vm/local_directory.js";
+    private final static String LOCAL_DIRECTORY_SETTINGS_FILE = System.getProperty("uvm.settings.dir") + "/untangle-vm/local_directory.js";
 
     private final static String UNCHANGED_PASSWORD = "***UNCHANGED***";
-    
+
     private LinkedList<LocalDirectoryUser> currentList;
-    
+
     public LocalDirectoryImpl()
     {
         loadUsersList();
     }
-
 
     private boolean accountExpired(LocalDirectoryUser user)
     {
         return user.getExpirationTime() > 0 && System.currentTimeMillis() >= user.getExpirationTime();
     }
 
-    public boolean authenticate( String username, String password )
+    public boolean authenticate(String username, String password)
     {
-        if ( username == null ) {
+        if (username == null) {
             logger.warn("Invalid arguments: username is null");
             return false;
         }
-        if ( password == null ) {
+        if (password == null) {
             logger.warn("Invalid arguments: password is null or empty");
             return false;
         }
-        if ( "".equals(password) ) {
+        if ("".equals(password)) {
             logger.info("Blank passwords not allowed");
             return false;
         }
@@ -85,7 +86,7 @@ public class LocalDirectoryImpl implements LocalDirectory
     public void setUsers(LinkedList<LocalDirectoryUser> users)
     {
         HashSet<String> usersSeen = new HashSet<String>();
-        
+
         /**
          * Remove cleartext password before saving
          */
@@ -100,22 +101,24 @@ public class LocalDirectoryImpl implements LocalDirectory
                 usersSeen.add(user.getUsername());
 
             /**
-             * If password hasn't changed - copy the hashes from the previous settings
-             * We must do this because the UI does not send the same hashes back
+             * If password hasn't changed - copy the hashes from the previous
+             * settings We must do this because the UI does not send the same
+             * hashes back
              */
             if (UNCHANGED_PASSWORD.equals(user.getPassword())) {
                 if (this.currentList != null) {
                     for (LocalDirectoryUser currentUser : this.currentList) {
                         if (currentUser.equals(user)) {
-                            user.setPasswordShaHash( currentUser.getPasswordShaHash());
-                            user.setPasswordMd5Hash( currentUser.getPasswordMd5Hash());
-                            user.setPasswordBase64Hash( currentUser.getPasswordBase64Hash());
+                            user.setPasswordShaHash(currentUser.getPasswordShaHash());
+                            user.setPasswordMd5Hash(currentUser.getPasswordMd5Hash());
+                            user.setPasswordBase64Hash(currentUser.getPasswordBase64Hash());
                         }
                     }
                 }
             }
             /**
-             * Otherwise the password has changed and we must recalculate the hashes
+             * Otherwise the password has changed and we must recalculate the
+             * hashes
              */
             else {
                 user.setPasswordShaHash(calculateShaHash(user.getPassword()));
@@ -151,12 +154,11 @@ public class LocalDirectoryImpl implements LocalDirectory
         return u1.getEmail() != null && u1.getEmail().equals(u2.getEmail());
     }
 
-
     public boolean userExists(LocalDirectoryUser user)
     {
         LinkedList<LocalDirectoryUser> users = this.getUsers();
-        for (LocalDirectoryUser u: users) {
-            if ( userNameMatch(u, user) || emailMatch(u, user)) {
+        for (LocalDirectoryUser u : users) {
+            if (userNameMatch(u, user) || emailMatch(u, user)) {
                 return true;
             }
         }
@@ -167,39 +169,38 @@ public class LocalDirectoryImpl implements LocalDirectory
     {
         LinkedList<LocalDirectoryUser> users = this.getUsers();
         boolean retVal = false;
-        for (LocalDirectoryUser u: users) {
-            if ( userNameMatch(u, user) || emailMatch( u, user)) {
-                if ( user.getPassword() != null && !user.getPassword().equals( UNCHANGED_PASSWORD )) {
+        for (LocalDirectoryUser u : users) {
+            if (userNameMatch(u, user) || emailMatch(u, user)) {
+                if (user.getPassword() != null && !user.getPassword().equals(UNCHANGED_PASSWORD)) {
                     u.setPasswordShaHash(calculateShaHash(user.getPassword()));
                     u.setPasswordMd5Hash(calculateMd5Hash(user.getPassword()));
                     u.setPasswordBase64Hash(calculateBase64Hash(user.getPassword()));
                     u.removeCleartextPassword();
                 }
-                if ( user.getFirstName() != null && user.getFirstName().trim().length() > 0 ) {
-                    u.setFirstName( user.getFirstName());
+                if (user.getFirstName() != null && user.getFirstName().trim().length() > 0) {
+                    u.setFirstName(user.getFirstName());
                 }
-                if ( user.getLastName() != null && user.getLastName().trim().length() > 0 ) {
-                    u.setLastName( user.getLastName());
+                if (user.getLastName() != null && user.getLastName().trim().length() > 0) {
+                    u.setLastName(user.getLastName());
                 }
-                if ( user.getExpirationTime() > 0) {
+                if (user.getExpirationTime() > 0) {
                     u.setExpirationTime(user.getExpirationTime());
                 }
-                retVal=true;
+                retVal = true;
                 break;
             }
         }
-        if ( retVal) {
+        if (retVal) {
             this.saveUsersList(users);
         }
         return retVal;
     }
 
-
     public boolean userExpired(LocalDirectoryUser user)
     {
         LinkedList<LocalDirectoryUser> users = this.getUsers();
-        for (LocalDirectoryUser u: users) {
-            if ( userNameMatch(u, user) || emailMatch( u, user)) {
+        for (LocalDirectoryUser u : users) {
+            if (userNameMatch(u, user) || emailMatch(u, user)) {
                 if (accountExpired(u)) {
                     return true;
                 }
@@ -214,18 +215,16 @@ public class LocalDirectoryImpl implements LocalDirectory
         boolean entriesDeleted = false;
         while (iter.hasNext()) {
             LocalDirectoryUser u = iter.next();
-            if ( accountExpired(u)) {
+            if (accountExpired(u)) {
                 iter.remove();
                 entriesDeleted = true;
             }
         }
-        if ( entriesDeleted) {
-            saveUsersList( this.getUsers());
+        if (entriesDeleted) {
+            saveUsersList(this.getUsers());
         }
     }
 
-
-    
     private synchronized void saveUsersList(LinkedList<LocalDirectoryUser> list)
     {
         SettingsManager settingsManager = UvmContextFactory.context().settingsManager();
@@ -236,24 +235,25 @@ public class LocalDirectoryImpl implements LocalDirectory
         try {
             settingsManager.save(LinkedList.class, LOCAL_DIRECTORY_SETTINGS_FILE, list);
         } catch (SettingsManager.SettingsException e) {
-            logger.warn("Failed to save settings.",e);
+            logger.warn("Failed to save settings.", e);
             return;
         }
 
+        updateChapSecrets(list);
         this.currentList = list;
     }
-        
+
     @SuppressWarnings("unchecked")
     private void loadUsersList()
     {
         SettingsManager settingsManager = UvmContextFactory.context().settingsManager();
         LinkedList<LocalDirectoryUser> users = null;
-        
+
         // Read the settings from file
         try {
             users = (LinkedList<LocalDirectoryUser>) settingsManager.load(LinkedList.class, LOCAL_DIRECTORY_SETTINGS_FILE);
         } catch (SettingsManager.SettingsException e) {
-            logger.warn("Unable to read localDirectory file: ", e );
+            logger.warn("Unable to read localDirectory file: ", e);
         }
 
         // no settings? just initialize new ones
@@ -262,30 +262,69 @@ public class LocalDirectoryImpl implements LocalDirectory
             this.saveUsersList(new LinkedList<LocalDirectoryUser>());
         }
 
+        updateChapSecrets(users);
         this.currentList = users;
     }
 
-    private String calculateShaHash( String password )
+    private void updateChapSecrets(LinkedList<LocalDirectoryUser> list)
     {
-        return calculateHash( password, "SHA" );
+        /**
+         * The IPsec L2TP feature uses LocalDirectory for login credentials so
+         * any time we load or save the list we'll call this function which will
+         * export all of the user/pass info to the chap-secrets file
+         */
+
+        String chapFile = "/tmp/chap-secrets.l2tp";
+
+        try {
+            // put all the username/password pairs into a temp file for L2TP
+            FileWriter chap = new FileWriter(chapFile, false);
+
+            for (LocalDirectoryUser user : list) {
+                byte[] rawPassword = Base64.decodeBase64(user.getPasswordBase64Hash().getBytes());
+                String userPassword = new String(rawPassword);
+                chap.write(user.getUsername() + "\t\t");
+                chap.write("untangle-l2tp\t\t");
+                chap.write(userPassword + "\t\t");
+                chap.write("*\n");
+            }
+
+            chap.flush();
+            chap.close();
+
+            /**
+             * Once we have the temp file we call the magic script to merge them
+             * into the global chap-secrets file
+             */
+            UvmContextFactory.context().execManager().exec("/usr/share/untangle/bin/ut-chap-manager L2TP " + chapFile);
+        }
+
+        catch (Exception exn) {
+            logger.error("Unable to create L2TP chap-secrets file");
+        }
     }
 
-    private String calculateMd5Hash( String password )
+    private String calculateShaHash(String password)
     {
-        return calculateHash( password, "MD5" );
+        return calculateHash(password, "SHA");
     }
 
-    private String calculateBase64Hash( String password )
+    private String calculateMd5Hash(String password)
     {
-            String hashString;
-            byte[] digest = Base64.encodeBase64(password.getBytes());
-            return new String(digest);
+        return calculateHash(password, "MD5");
     }
-    
-    private String calculateHash( String password, String hashAlgo)
+
+    private String calculateBase64Hash(String password)
+    {
+        String hashString;
+        byte[] digest = Base64.encodeBase64(password.getBytes());
+        return new String(digest);
+    }
+
+    private String calculateHash(String password, String hashAlgo)
     {
         try {
-            MessageDigest hasher = MessageDigest.getInstance( hashAlgo );
+            MessageDigest hasher = MessageDigest.getInstance(hashAlgo);
             String hashString;
             hasher.update(password.getBytes());
             byte[] digest = hasher.digest();
@@ -304,5 +343,4 @@ public class LocalDirectoryImpl implements LocalDirectory
 
         return null;
     }
-    
 }
