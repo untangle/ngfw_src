edb14720be63ed205415bda536f62c9a5e52f2fb
Dirk Morris <dmorris@untangle.com>
2014-04-03 18:49:01 +0000

    reintroduce RouterEventHandler for port mangling to avoid port conflicts - still needs work
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@37469 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ router/src/com/untangle/node/router/RouterEventHandler.java
@@ -0,0 +1,114 @@
+/**
+ * $Id: RouterEventHandler.java 36443 2013-11-19 23:32:09Z dmorris $
+ */
+package com.untangle.node.router;
+
+import java.net.InetAddress;
+import java.util.Random;
+import com.untangle.uvm.logging.LogEvent;
+import com.untangle.uvm.vnet.AbstractEventHandler;
+import com.untangle.uvm.vnet.IPNewSessionRequest;
+import com.untangle.uvm.vnet.NodeSession;
+import com.untangle.uvm.vnet.Protocol;
+import com.untangle.uvm.vnet.event.TCPNewSessionRequestEvent;
+import com.untangle.uvm.vnet.event.TCPSessionEvent;
+import com.untangle.uvm.vnet.event.UDPNewSessionRequestEvent;
+import com.untangle.uvm.vnet.event.UDPSessionEvent;
+import org.apache.log4j.Logger;
+
+class RouterEventHandler extends AbstractEventHandler
+{
+    private final Logger logger = Logger.getLogger(RouterEventHandler.class);
+
+    /* Router Node */
+    private final RouterImpl node;
+
+    private int nextPort;
+    
+    /* Setup  */
+    RouterEventHandler(RouterImpl node)
+    {
+        super(node);
+        this.nextPort = (new Random().nextInt(20000)) + 10000;
+        this.node = node;
+    }
+
+    public void handleTCPNewSessionRequest(TCPNewSessionRequestEvent event)
+    {
+        handleNewSessionRequest(event.sessionRequest(), Protocol.TCP);
+    }
+
+    public void handleUDPNewSessionRequest(UDPNewSessionRequestEvent event)
+    {
+        return;
+    }
+
+    private void handleNewSessionRequest(IPNewSessionRequest request, Protocol protocol)
+    {
+        InetAddress origClientAddr = request.getOrigClientAddr();
+        InetAddress newClientAddr = request.getNewClientAddr();
+        InetAddress origServerAddr = request.getOrigServerAddr();
+        InetAddress newServerAddr = request.getNewServerAddr();
+        int origClientPort = request.getOrigClientPort();
+        int newClientPort  = request.getNewClientPort();
+        int origServerPort = request.getOrigServerPort();
+        int newServerPort  = request.getNewServerPort();
+
+        if ( logger.isDebugEnabled()) {
+            logger.debug( "pre-translation : " + origClientAddr + ":" + origClientPort +  " -> " + origServerAddr + ":" + origServerPort );
+            logger.debug( "post-translation: " + newClientAddr + ":" + newClientPort +  " -> " + newServerAddr + ":" + newServerPort );
+        }
+
+        /**
+         * The sole purpose of the router event handler is to rewrite the source port of
+         * TCP connections
+         *
+         * The masquerading is handled by the kernel, however the kernel considers the
+         * following two connections to be unique:
+         *
+         * 1.2.3.4:1234 -> 10.0.0.1:1234
+         * 1.2.3.4:1234 -> 192.168.1.100:1234
+         *
+         * Because the kernel can differentiate the two even while both sessions
+         * use the same port on 1.2.3.4.
+         *
+         * However, because we will be non-local binding we cant have both sessions
+         * bind sockets to 1.2.3.4:1234
+         *
+         * As such, we must use or own port assignment scheme for TCP.
+         * (UDP doesn't matter since we don't bind to sockets)
+         */
+        
+        // if doing NAT, then rewrite the source port
+        if ( !origClientAddr.equals(newClientAddr) ||
+             !origServerAddr.equals(newServerAddr) ||
+             (origClientPort != newClientPort) ||
+             (origServerPort != newServerPort) ){
+
+            if( protocol == Protocol.TCP && !origClientAddr.equals(newClientAddr)){
+                int port = getNextPort();
+
+                if ( logger.isDebugEnabled()) {
+                    logger.debug( "Mangling server-side client port from " + origClientPort + " to " + port );
+                }
+                request.setNewClientPort( port );
+            }
+        }
+    }
+
+
+    private int getNextPort()
+    {
+        /**
+         * FIXME
+         *
+         * This needs to be smarter.
+         * It needs to either check the currently used list, or maintain a currently used list to avoid conflicts
+         */
+        
+        if (nextPort > 50000)
+            nextPort = 10000;
+
+        return nextPort++;
+    }
+}
--- router/src/com/untangle/node/router/RouterImpl.java
+++ router/src/com/untangle/node/router/RouterImpl.java
@@ -17,9 +17,11 @@ import com.untangle.uvm.vnet.SoloPipeSpec;
 
 public class RouterImpl extends NodeBase implements Router
 {
+    private final RouterEventHandler handler;
     private final RouterSessionManager sessionManager;
     private final DhcpMonitor dhcpMonitor;
 
+    private final SoloPipeSpec routerPipeSpec;
     private final SoloPipeSpec routerFtpPipeSpecCtl;
     private final SoloPipeSpec routerFtpPipeSpecData;
 
@@ -31,13 +33,14 @@ public class RouterImpl extends NodeBase implements Router
     {
         super( nodeSettings, nodeProperties );
 
-        //this.handler          = new RouterEventHandler(this);
+        this.handler          = new RouterEventHandler(this);
         this.sessionManager   = new RouterSessionManager(this);
         this.dhcpMonitor      = new DhcpMonitor( this );
 
         /**
          * Have to figure out pipeline ordering, this should always towards the server
          */
+        routerPipeSpec = new SoloPipeSpec("router", this, this.handler, Fitting.OCTET_STREAM, Affinity.SERVER, SoloPipeSpec.MAX_STRENGTH - 1);
 
         /**
          * This subscription has to evaluate after NAT
@@ -45,10 +48,13 @@ public class RouterImpl extends NodeBase implements Router
         routerFtpPipeSpecCtl = new SoloPipeSpec("router-ftp-ctl", this, new TokenAdaptor(this, new RouterFtpFactory(this)), Fitting.FTP_CTL_TOKENS, Affinity.SERVER, 0);
         routerFtpPipeSpecData = new SoloPipeSpec("router-ftp-data", this, new TokenAdaptor(this, new RouterFtpFactory(this)), Fitting.FTP_DATA_TOKENS, Affinity.SERVER, 0);
 
-        pipeSpecs = new SoloPipeSpec[] { routerFtpPipeSpecCtl, routerFtpPipeSpecData };
+        pipeSpecs = new SoloPipeSpec[] { routerPipeSpec, routerFtpPipeSpecCtl, routerFtpPipeSpecData };
     }
 
-    // package protected methods ----------------------------------------------
+    PipelineConnector getRouterPipelineConnector()
+    {
+        return routerPipeSpec.getPipelineConnector();
+    }
 
     PipelineConnector getRouterFtpPipeSpecCtl()
     {
@@ -59,8 +65,6 @@ public class RouterImpl extends NodeBase implements Router
     {
         return routerFtpPipeSpecData.getPipelineConnector();
     }
-
-    // NodeBase methods ----------------------------------------------
 
     @Override
     protected PipeSpec[] getPipeSpecs()
