d57aa6376c73872aa819ad77927de06cdeb9c422
Dirk Morris <dmorris@untangle.com>
2014-06-11 23:02:43 +0000

    fix application names
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@37863 8ebf8b4e-06f3-0310-961f-89f74618be54

--- phish/hier/usr/lib/python2.6/reports/node/untangle_node_phish.py
+++ phish/hier/usr/lib/python2.6/reports/node/untangle_node_phish.py
@@ -4,4 +4,4 @@ import reports.node.untangle_base_spam
 
 _ = reports.i18n_helper.get_translation('untangle-node-phish').lgettext
 
-reports.engine.register_node(reports.node.untangle_base_spam.SpamBaseNode('untangle-node-phish', 'Phish', 'phish', 'Clam', _('Phish'), _('Clean'), _('Hourly Phish Rate'), _('Daily Phish Rate'), _('Top Ten Phishing Victims')))
+reports.engine.register_node(reports.node.untangle_base_spam.SpamBaseNode('untangle-node-phish', 'Phish Blocker', 'phish', 'Clam', _('Phish'), _('Clean'), _('Hourly Phish Rate'), _('Daily Phish Rate'), _('Top Ten Phishing Victims')))
