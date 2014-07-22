d1bc05db0a06393b729b6ee51c1e66308b9f08e3
Mike Hotz <mahotz@untangle.com>
2014-07-22 16:51:00 +0000

    Removed commtouch and added virusblocker and spamblocker license stuff.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@38137 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/api/com/untangle/uvm/node/License.java
+++ uvm/api/com/untangle/uvm/node/License.java
@@ -23,9 +23,8 @@ public class License implements Serializable
     public static final String BANDWIDTH = "untangle-node-bandwidth";
     public static final String BOXBACKUP = "untangle-node-boxbackup";
     public static final String BRANDING = "untangle-node-branding";
-    public static final String COMMTOUCH = "untangle-node-commtouch";
-    public static final String COMMTOUCHAV = "untangle-node-commtouchav";
-    public static final String COMMTOUCHAS = "untangle-node-commtouchas";
+    public static final String VIRUSBLOCKER = "untangle-node-virusblocker";
+    public static final String SPAMBLOCKER = "untangle-node-spamblocker";
     public static final String FAILD = "untangle-node-faild";
     public static final String IPSEC = "untangle-node-ipsec";
     public static final String POLICY = "untangle-node-policy";
