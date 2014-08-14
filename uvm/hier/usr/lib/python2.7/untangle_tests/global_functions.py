f8ea6805d25dc65a419162bfc50db61c94290d4f
John Coffin <jcoffin@gmail.com>
2014-08-14 21:15:39 +0000

    switch from mgen ro iperf
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@38417 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/lib/python2.7/untangle_tests/global_functions.py
+++ uvm/hier/usr/lib/python2.7/untangle_tests/global_functions.py
@@ -7,65 +7,67 @@ import datetime
 import remote_control
 import system_properties
 
-radiusServer = "10.111.56.71"
+iperfServer = "10.111.56.32"
 
-# http://www.nrl.navy.mil/itd/ncs/products/mgen
-def verifyMgen():
-    mGenPresent = False
-    # Check to see if mgen endpoint is reachable
-    externalClientResult = subprocess.call(["ping","-c","1",radiusServer],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    # Check to see if some other test is using mgen for UDP testing
-    isMgenNotRunning = os.system("ssh -o 'StrictHostKeyChecking=no' -i " + system_properties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + radiusServer + " \"pidof mgen >/dev/null 2>&1\"")
+def verifyIperf():
+    # https://iperf.fr/
+    iperfPresent = False
+    # Check to see if iperf endpoint is reachable
+    externalClientResult = subprocess.call(["ping","-c","1",iperfServer],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    # Check to see if some other test is using iperf for UDP testing
+    isIperfNotRunning = os.system("ssh -o 'StrictHostKeyChecking=no' -i " + systemProperties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + iperfServer + " \"pidof iperf >/dev/null 2>&1\"")
     # print "externalClientResult <%s>" % externalClientResult
-    # print "isMgenNotRunning <%s>" % isMgenNotRunning
-    if (externalClientResult == 0 and isMgenNotRunning):
-        # RADIUS server, the mgen endpoint is reachable, check other requirements
-        mgenResult = remote_control.runCommand("test -x /usr/bin/mgen")
-        # print "mgenResult <%s>" % mgenResult
-        if (mgenResult == 0):
-            # Always get new UDP traffic generator file in case it changes.
-            remote_control.runCommand("rm udp-load-ats.mgn")
-            mgnFileResult = remote_control.runCommand("wget -q http://test.untangle.com/test/udp-load-ats.mgn")
-            if (mgnFileResult == 0):
-                mGenPresent = True
-    return mGenPresent
+    # print "isIperfNotRunning <%s>" % isIperfNotRunning
+    if (externalClientResult == 0 and isIperfNotRunning):
+        # Iperf server, the iperf endpoint is reachable, check other requirements
+        iperfResult = clientControl.runCommand("test -x /usr/bin/iperf")
+        # print "iperfResult <%s>" % iperfResult
+        if (iperfResult == 0):
+            iperfPresent = True
+    return iperfPresent
 
 def getUDPSpeed():
-    # Use mgen to get UDP speed.  Returns number of packets received.
-    # start mgen receiver on radius server.
-    os.system("rm -f /tmp/mgen_recv.dat")
-    os.system("ssh -o 'StrictHostKeyChecking=no' -i " + system_properties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + radiusServer + " \"rm -f mgen_recv.dat\" >/dev/null 2>&1")
-    os.system("ssh -o 'StrictHostKeyChecking=no' -i " + system_properties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + radiusServer + " \"/home/fnsadmin/MGEN/mgen output mgen_recv.dat port 5000 >/dev/null 2>&1 &\"")
+    # Use iperf to get UDP speed.  Returns number of packets received.
+    # start iperf receiver on iperf server.
+    os.system("ssh -o 'StrictHostKeyChecking=no' -i " + systemProperties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + iperfServer + " \"rm -f iperf_recv.dat\" >/dev/null 2>&1")
+    os.system("ssh -o 'StrictHostKeyChecking=no' -i " + systemProperties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + iperfServer + " \"iperf -s -p 5000 -u > iperf_recv.dat &\"")
     # start the UDP generator on the client behind the Untangle.
-    remote_control.runCommand("mgen input /home/testshell/udp-load-ats.mgn txlog log mgen_snd.log")
+    clientControl.runCommand("iperf -c " + iperfServer + " -u -p 5000 -b 10M -t 20")
     # wait for UDP to finish
     time.sleep(25)
-    # kill mgen receiver    
-    os.system("ssh -o 'StrictHostKeyChecking=no' -i " + system_properties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + radiusServer + " \"pkill mgen\"  >/dev/null 2>&1")
-    os.system("scp -o 'StrictHostKeyChecking=no' -i " + system_properties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + radiusServer + ":mgen_recv.dat /tmp/mgen_recv.dat >/dev/null 2>&1")
-    wcResults = subprocess.Popen(["wc","-l","/tmp/mgen_recv.dat"], stdout=subprocess.PIPE).communicate()[0]
-    print "wcResults " + str(wcResults)
-    numOfPackets = wcResults.split(' ')[0]
-    return int(numOfPackets)
+    # kill iperf receiver    
+    os.system("ssh -o 'StrictHostKeyChecking=no' -i " + systemProperties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + iperfServer + " \"pkill iperf\"  >/dev/null 2>&1")
+    os.system("scp -o 'StrictHostKeyChecking=no' -i " + systemProperties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + iperfServer + ":iperf_recv.dat /tmp/iperf_recv.dat >/dev/null 2>&1")
+    f = open('/tmp/iperf_recv.dat')
+    results = f.readlines()
+    f.close()
+    for data in results:
+        if 'bits/sec' in data:
+            match = re.search(r'([0-9.]+) [KM]Bytes', data)
+            udp_speed =  match.group(1)
+            break
+    return float(udp_speed)
 
 def sendUDPPackets(targetIP):
-    # Use mgen to send UDP packets.  Returns number of packets received.
-    # start mgen receiver on client.
-    remote_control.runCommand("rm -f mgen_recv.dat")
-    remote_control.runCommand("nohup mgen output mgen_recv.dat port 5000", stdout=False, nowait=True)
-    # Create a mgen config with client host IP as the target
-    os.system("ssh -o 'StrictHostKeyChecking=no' -i " + system_properties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + radiusServer + " \"rm -f udp-load-send.mgn\" >/dev/null 2>&1")
-    os.system("ssh -o 'StrictHostKeyChecking=no' -i " + system_properties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + radiusServer + " \"sed 's/" + radiusServer + "/" + targetIP + "/' udp-load-ats.mgn > udp-load-send.mgn\" >/dev/null 2>&1")
-    # start the UDP generator on the radius server.
-    os.system("ssh -o 'StrictHostKeyChecking=no' -i " + system_properties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + radiusServer + " \"/home/fnsadmin/MGEN/mgen input /home/testshell/udp-load-send.mgn txlog log mgen_snd.log\" >/dev/null 2>&1")
+    # Use iperf to send UDP packets.  Returns number of packets received.
+    # start iperf receiver on client.
+    clientControl.runCommand("rm -f iperf_recv.dat")
+    clientControl.runCommand("iperf -s -p 5000 -u > iperf_recv.dat", False, True)
+    # start the UDP generator on the iperf server.
+    os.system("ssh -o 'StrictHostKeyChecking=no' -i " + systemProperties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + iperfServer + " \"iperf -c " + targetIP + " -u -p 5000 -b 10M -t 20\" >/dev/null 2>&1")
     # wait for UDP to finish
     time.sleep(25)
-    # kill mgen receiver    
-    remote_control.runCommand("pkill mgen")
-    wcResults = remote_control.runCommand("wc -l mgen_recv.dat", stdout=True)
-    print "wcResults " + str(wcResults)
-    numOfPackets = wcResults.split(' ')[0]
-    return int(numOfPackets)
+    # kill iperf receiver    
+    clientControl.runCommand("pkill iperf")
+    f = open('/tmp/iperf_recv.dat')
+    results = f.readlines()
+    f.close()
+    for data in results:
+        if 'bits/sec' in data:
+            match = re.search(r'([0-9.]+) [KM]Bytes', data)
+            numOfPackets =  match.group(1)
+            break
+    return float(numOfPackets)
         
 def check_events( events, num_events, *args, **kwargs):
     if events == None:
