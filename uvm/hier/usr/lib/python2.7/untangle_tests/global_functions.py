52055e04f680f8d283c55c6e2bb8c94c953aa218
Dirk Morris <dmorris@untangle.com>
2014-08-14 22:25:08 +0000

    fix systemProperties references
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@38422 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/lib/python2.7/untangle_tests/global_functions.py
+++ uvm/hier/usr/lib/python2.7/untangle_tests/global_functions.py
@@ -15,7 +15,7 @@ def verifyIperf():
     # Check to see if iperf endpoint is reachable
     externalClientResult = subprocess.call(["ping","-c","1",iperfServer],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     # Check to see if some other test is using iperf for UDP testing
-    isIperfNotRunning = os.system("ssh -o 'StrictHostKeyChecking=no' -i " + systemProperties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + iperfServer + " \"pidof iperf >/dev/null 2>&1\"")
+    isIperfNotRunning = os.system("ssh -o 'StrictHostKeyChecking=no' -i " + system_properties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + iperfServer + " \"pidof iperf >/dev/null 2>&1\"")
     # print "externalClientResult <%s>" % externalClientResult
     # print "isIperfNotRunning <%s>" % isIperfNotRunning
     if (externalClientResult == 0 and isIperfNotRunning):
@@ -29,15 +29,15 @@ def verifyIperf():
 def getUDPSpeed():
     # Use iperf to get UDP speed.  Returns number of packets received.
     # start iperf receiver on iperf server.
-    os.system("ssh -o 'StrictHostKeyChecking=no' -i " + systemProperties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + iperfServer + " \"rm -f iperf_recv.dat\" >/dev/null 2>&1")
-    os.system("ssh -o 'StrictHostKeyChecking=no' -i " + systemProperties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + iperfServer + " \"iperf -s -p 5000 -u > iperf_recv.dat &\"")
+    os.system("ssh -o 'StrictHostKeyChecking=no' -i " + system_properties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + iperfServer + " \"rm -f iperf_recv.dat\" >/dev/null 2>&1")
+    os.system("ssh -o 'StrictHostKeyChecking=no' -i " + system_properties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + iperfServer + " \"iperf -s -p 5000 -u > iperf_recv.dat &\"")
     # start the UDP generator on the client behind the Untangle.
     remote_control.runCommand("iperf -c " + iperfServer + " -u -p 5000 -b 10M -t 20")
     # wait for UDP to finish
     time.sleep(25)
     # kill iperf receiver    
-    os.system("ssh -o 'StrictHostKeyChecking=no' -i " + systemProperties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + iperfServer + " \"pkill iperf\"  >/dev/null 2>&1")
-    os.system("scp -o 'StrictHostKeyChecking=no' -i " + systemProperties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + iperfServer + ":iperf_recv.dat /tmp/iperf_recv.dat >/dev/null 2>&1")
+    os.system("ssh -o 'StrictHostKeyChecking=no' -i " + system_properties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + iperfServer + " \"pkill iperf\"  >/dev/null 2>&1")
+    os.system("scp -o 'StrictHostKeyChecking=no' -i " + system_properties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + iperfServer + ":iperf_recv.dat /tmp/iperf_recv.dat >/dev/null 2>&1")
     f = open('/tmp/iperf_recv.dat')
     results = f.readlines()
     f.close()
@@ -54,7 +54,7 @@ def sendUDPPackets(targetIP):
     remote_control.runCommand("rm -f iperf_recv.dat")
     remote_control.runCommand("iperf -s -p 5000 -u > iperf_recv.dat", False, True)
     # start the UDP generator on the iperf server.
-    os.system("ssh -o 'StrictHostKeyChecking=no' -i " + systemProperties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + iperfServer + " \"iperf -c " + targetIP + " -u -p 5000 -b 10M -t 20\" >/dev/null 2>&1")
+    os.system("ssh -o 'StrictHostKeyChecking=no' -i " + system_properties.getPrefix() + "/usr/lib/python2.7/untangle_tests/testShell.key testshell@" + iperfServer + " \"iperf -c " + targetIP + " -u -p 5000 -b 10M -t 20\" >/dev/null 2>&1")
     # wait for UDP to finish
     time.sleep(25)
     # kill iperf receiver    
