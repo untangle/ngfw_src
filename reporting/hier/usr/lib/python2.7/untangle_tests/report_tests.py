4a13f05563e97e9c167c8421a1f6543a6728192c
John Coffin <jcoffin@gmail.com>
2014-09-18 20:38:33 +0000

    Add syslog check
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@38645 8ebf8b4e-06f3-0310-961f-89f74618be54

--- reporting/hier/usr/lib/python2.7/untangle_tests/report_tests.py
+++ reporting/hier/usr/lib/python2.7/untangle_tests/report_tests.py
@@ -4,6 +4,9 @@ import sys
 import pdb
 import socket
 import subprocess
+import copy
+import re
+from datetime import datetime
 from jsonrpc import ServiceProxy
 from jsonrpc import JSONRPCException
 from uvm import Manager
@@ -14,38 +17,34 @@ import test_registry
 uvmContext = Uvm().getUvmContext()
 defaultRackId = 1
 node = None
+nodeFirewall = None
+# special box with testshell in the sudoer group  - used to connect to as client
+syslogHostIP = "10.111.56.32"  
 
 # pdb.set_trace()
 
-def setReportSettings():
+def createFirewallSingleMatcherRule( matcherType, value, blocked=True ):
+    matcherTypeStr = str(matcherType)
+    valueStr = str(value)
     return {
-        "attachmentSizeLimit": 10, 
-        "dbHost": "localhost", 
-        "dbName": "uvm", 
-        "dbPassword": "foo", 
-        "dbPort": 5432, 
-        "dbRetention": 7, 
-        "dbUser": "postgres", 
-        "emailDetail": False, 
-        "fileRetention": 90, 
-        "generateDailyReports": "any", 
-        "generateMonthlyReports": False, 
-        "generateWeeklyReports": "sunday", 
-        "generationHour": 2, 
-        "generationMinute": 0, 
-        "hostnameMap": {
+        "javaClass": "com.untangle.node.firewall.FirewallRule", 
+        "id": 1, 
+        "enabled": True, 
+        "description": "Single Matcher: " + matcherTypeStr + " = " + valueStr, 
+        "log": True, 
+        "block": blocked, 
+        "matchers": {
             "javaClass": "java.util.LinkedList", 
-            "list": []
-        }, 
-        "javaClass": "com.untangle.node.reporting.ReportingSettings", 
-        "reportingUsers": {
-            "javaClass": "java.util.LinkedList", 
-            "list": []
-        }, 
-        "syslogEnabled": false, 
-        "syslogPort": 514, 
-        "syslogProtocol": "UDP"
-    }
+            "list": [
+                {
+                    "invert": False, 
+                    "javaClass": "com.untangle.node.firewall.FirewallRuleMatcher", 
+                    "matcherType": matcherTypeStr, 
+                    "value": valueStr
+                    }
+                ]
+            }
+        };
 
 def flushEvents():
     reports = uvmContext.nodeManager().node("untangle-node-reporting")
@@ -59,11 +58,15 @@ class ReportTests(unittest2.TestCase):
         return "untangle-node-reporting"
 
     @staticmethod
+    def nodeFWName():
+        return "untangle-node-firewall"
+
+    @staticmethod
     def vendorName():
         return "Untangle"
 
     def setUp(self):
-        global node
+        global node, nodeFirewall, syslogSettings, syslogHostResult
         if node == None:
             if (uvmContext.nodeManager().isInstantiated(self.nodeName())):
                 print "Node %s already installed" % self.nodeName()
@@ -72,6 +75,15 @@ class ReportTests(unittest2.TestCase):
                 node = uvmContext.nodeManager().node(self.nodeName())
             else:
                 node = uvmContext.nodeManager().instantiate(self.nodeName(), defaultRackId)
+        if nodeFirewall == None:
+            if (uvmContext.nodeManager().isInstantiated(self.nodeFWName())):
+                print "Node %s already installed" % self.nodeFWName()
+                nodeFirewall = uvmContext.nodeManager().node(self.nodeFWName())
+            else:
+                nodeFirewall = uvmContext.nodeManager().instantiate(self.nodeFWName(), defaultRackId)
+
+            syslogSettings = node.getSettings()
+            syslogHostResult = subprocess.call(["ping","-c","1",syslogHostIP],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
            
     # verify client is online
     def test_010_clientIsOnline(self):
@@ -84,12 +96,70 @@ class ReportTests(unittest2.TestCase):
         node.setSettings(settings)
         # TODO set email address, send email, check attachment size
 
+    def test_040_remoteSyslog(self):
+        raise unittest2.SkipTest("Review changes in test")        
+        if (syslogHostResult != 0):
+            raise unittest2.SkipTest("No syslog server available")        
+        # Install firewall rule to generate syslog events
+        rules = nodeFirewall.getRules()
+        rules["list"].append(createFirewallSingleMatcherRule("SRC_ADDR",remote_control.clientIP));
+        nodeFirewall.setRules(rules);
+        rules = nodeFirewall.getRules()
+        # Get rule ID
+        for rule in rules['list']:
+            if rule['enabled'] and rule['block']:
+                targetID = rule['ruleId']
+                break
+        # Setup syslog to send events to syslog host
+        newSyslogSettings = copy.deepcopy(syslogSettings)
+        newSyslogSettings["syslogEnabled"] = True
+        newSyslogSettings["syslogEnabled"] = True
+        newSyslogSettings["syslogHost"] = syslogHostIP
+        node.setSettings(newSyslogSettings)
+        currentTime = time.strftime("%X")
+        result = remote_control.isOnline()
+        nowtime = datetime.now()
+        currentTime =  nowtime.strftime('%b %d %H:%M')
+        print "currentTime " + currentTime
+        rsyslogResult = remote_control.runCommand("sudo tail -8 /var/log/localhost/localhost.log", host=syslogHostIP, stdout=True)
+        resultList = rsyslogResult.splitlines()
+        # remove the firewall rule aet syslog back to original settings
+        node.setSettings(syslogSettings)
+        rules["list"]=[];
+        nodeFirewall.setRules(rules);
+        
+        # Get the date and time from the last record 
+        matchedClientIP = False
+        matchedBlockedStatus = False
+        matchedFirewallRule = False
+        match = re.search(r'(\w{3}\s\d\d\s\d\d:\d\d:\d\d).*', resultList[-1])
+        if (match == None):
+            print "Rsyslog did not have any date match"
+        else:
+            # print match.group(1)
+            # Check to see if the logs are in this test's timeframe. 
+            if (currentTime in match.group(1)):
+                for line in reversed(resultList):
+                    if str(match.group(1)) in line:
+                        if remote_control.clientIP in line:
+                            matchedClientIP = True
+                        if '"blocked":true' in line:
+                            matchedBlockedStatus = True
+                        if str(targetID) in line:
+                            matchedFirewallRule = True
+         
+        assert(matchedClientIP and matchedBlockedStatus and matchedFirewallRule)
+
     @staticmethod
     def finalTearDown(self):
-        global node
+        global node, nodeFirewall
         # no need to uninstall reports
         # if node != None:
         # uvmContext.nodeManager().destroy( node.getNodeSettings()["id"] )
+        node.setSettings(syslogSettings)
         node = None
+        if nodeFirewall != None:
+            uvmContext.nodeManager().destroy( nodeFirewall.getNodeSettings()["id"] )
+        nodeFirewall = None
 
 test_registry.registerNode("reporting", ReportTests)
