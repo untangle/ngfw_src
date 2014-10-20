ec2a44ef78f1ff74624e0d2b8d68b4a9c36d2017
Dirk Morris <dmorris@untangle.com>
2014-10-20 19:32:15 +0000

    increase logging during network restart
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@38833 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/impl/com/untangle/uvm/NetworkManagerImpl.java
+++ uvm/impl/com/untangle/uvm/NetworkManagerImpl.java
@@ -195,7 +195,7 @@ public class NetworkManagerImpl implements NetworkManager
         String errorStr = null;
         
         // stop interfaces
-        result = UvmContextFactory.context().execManager().exec( "ifdown -a --exclude=lo" );
+        result = UvmContextFactory.context().execManager().exec( "ifdown -a -v --exclude=lo" );
         try {
             String lines[] = result.getOutput().split("\\r?\\n");
             logger.info("ifdown -a: ");
@@ -219,7 +219,7 @@ public class NetworkManagerImpl implements NetworkManager
         }
         
         // start interfaces
-        result = UvmContextFactory.context().execManager().exec( "ifup -a --exclude=lo" );
+        result = UvmContextFactory.context().execManager().exec( "ifup -a -v --exclude=lo" );
         try {
             String lines[] = result.getOutput().split("\\r?\\n");
             logger.info("ifup -a: ");
