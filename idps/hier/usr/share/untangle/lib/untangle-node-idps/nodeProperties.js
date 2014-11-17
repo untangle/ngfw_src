d45bd4ad8ffd02dc18c6ae38ba2816e4f243441c
Chris Blaise <cblaise@untangle.com>
2014-11-17 18:17:28 +0000

    Temporary display name change to reflect that this is development mode code and not quite close to being the full IPS upgrade, even for internal testing.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@39036 8ebf8b4e-06f3-0310-961f-89f74618be54

--- idps/hier/usr/share/untangle/lib/untangle-node-idps/nodeProperties.js
+++ idps/hier/usr/share/untangle/lib/untangle-node-idps/nodeProperties.js
@@ -2,7 +2,7 @@
         "javaClass": "com.untangle.uvm.node.NodeProperties",
         "className" : "com.untangle.node.idps.IdpsNodeImpl",
         "name" : "untangle-node-idps",
-        "displayName" : "Intrusion Prevention",
+        "displayName" : "Intrusion Prevention (DEV)",
         "type" : "SERVICE",
         "viewPosition" : 120,
         "autoStart" : "false"
