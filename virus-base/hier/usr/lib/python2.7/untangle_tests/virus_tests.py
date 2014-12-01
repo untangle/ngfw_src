10286f8f7e49f43cd83fce286114f4ab4474d6a7
John Coffin <jcoffin@gmail.com>
2014-12-01 19:01:55 +0000

    Change hardcoded IP address to host lookups
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/release-11.0/work/src@39147 8ebf8b4e-06f3-0310-961f-89f74618be54

--- virus-base/hier/usr/lib/python2.7/untangle_tests/virus_tests.py
+++ virus-base/hier/usr/lib/python2.7/untangle_tests/virus_tests.py
@@ -6,6 +6,7 @@ import sys
 import os
 import subprocess
 import socket
+import re
 
 from jsonrpc import ServiceProxy
 from jsonrpc import JSONRPCException
@@ -17,8 +18,7 @@ import global_functions
 uvmContext = Uvm().getUvmContext()
 defaultRackId = 1
 node = None
-testsite = "test.untangle.com"
-testsiteIP = socket.gethostbyname(testsite)
+ip_address_testdestination  = None
 
 def flushEvents():
     reports = uvmContext.nodeManager().node("untangle-node-reporting")
@@ -47,7 +47,7 @@ class VirusTests(unittest2.TestCase):
         return "untangle"
 
     def setUp(self):
-        global node,md5StdNum
+        global node,md5StdNum,ip_address_testdestination
         if node == None:
             # download eicar and trojan files before installing virus blocker
             remote_control.runCommand("rm -f /tmp/eicar /tmp/std_022_ftpVirusBlocked_file /tmp/temp_022_ftpVirusPassSite_file")
@@ -58,6 +58,9 @@ class VirusTests(unittest2.TestCase):
             md5StdNum = remote_control.runCommand("\"md5sum /tmp/std_022_ftpVirusBlocked_file | awk '{print $1}'\"", stdout=True)
             print "md5StdNum <%s>" % md5StdNum
             assert (result == 0)
+            result = remote_control.runCommand("host test.untangle.com", stdout=True)
+            match = re.search(r'\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}', result)
+            ip_address_testdestination  = match.group()            
             if (uvmContext.nodeManager().isInstantiated(self.nodeName())):
                 print "ERROR: Node %s already installed" % self.nodeName();
                 raise unittest2.SkipTest('node %s already instantiated' % self.nodeName())
@@ -237,7 +240,7 @@ class VirusTests(unittest2.TestCase):
         result = remote_control.runCommand("chmod 775 /tmp/email_script.py")
         assert (result == 0)
         # email the file
-        result = remote_control.runCommand("/tmp/email_script.py --server=%s --from=junk@test.untangle.com --to=junk@test.untangle.com --subject='%s' --body='body' --file=/tmp/eicar" % (testsiteIP, fname))
+        result = remote_control.runCommand("/tmp/email_script.py --server=" + ip_address_testdestination + " --from=junk@test.untangle.com --to=junk@test.untangle.com --subject='%s' --body='body' --file=/tmp/eicar" % (fname))
         assert (result == 0)
         flushEvents()
         query = None;
@@ -266,7 +269,7 @@ class VirusTests(unittest2.TestCase):
         result = remote_control.runCommand("chmod 775 /tmp/email_script.py")
         assert (result == 0)
         # email the file
-        result = remote_control.runCommand("/tmp/email_script.py --server=%s --from=junk@test.untangle.com --to=junk@test.untangle.com --subject='%s' --body='body' --file=/tmp/attachment-%s" % (testsiteIP, fname, fname))
+        result = remote_control.runCommand("/tmp/email_script.py --server=" + ip_address_testdestination + " --from=junk@test.untangle.com --to=junk@test.untangle.com --subject='%s' --body='body' --file=/tmp/attachment-%s" % (fname, fname))
         assert (result == 0)
         flushEvents()
         query = None;
@@ -297,7 +300,7 @@ class VirusTests(unittest2.TestCase):
         result = remote_control.runCommand("chmod 775 /tmp/email_script.py")
         assert (result == 0)
         # email the file
-        result = remote_control.runCommand("/tmp/email_script.py --server=%s --from=junk@test.untangle.com --to=junk@test.untangle.com --subject='%s' --body='body' --file=/tmp/eicar" % (testsiteIP, fname))
+        result = remote_control.runCommand("/tmp/email_script.py --server=" + ip_address_testdestination + " --from=junk@test.untangle.com --to=junk@test.untangle.com --subject='%s' --body='body' --file=/tmp/eicar" % (fname))
         assert (result == 0)
         flushEvents()
         query = None;
