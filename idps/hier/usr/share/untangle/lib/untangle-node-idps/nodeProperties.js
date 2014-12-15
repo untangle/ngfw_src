edbc8e3baee6ea904050187096ee146ffaca5cef
Chris Blaise <cblaise@untangle.com>
2014-12-15 18:35:30 +0000

    Change "DEV" to "Beta".
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@39294 8ebf8b4e-06f3-0310-961f-89f74618be54

--- idps/hier/usr/share/untangle/lib/untangle-node-idps/nodeProperties.js
+++ idps/hier/usr/share/untangle/lib/untangle-node-idps/nodeProperties.js
@@ -2,7 +2,7 @@
         "javaClass": "com.untangle.uvm.node.NodeProperties",
         "className" : "com.untangle.node.idps.IdpsNodeImpl",
         "name" : "untangle-node-idps",
-        "displayName" : "Intrusion Prevention (DEV)",
+        "displayName" : "Intrusion Prevention (Beta)",
         "type" : "SERVICE",
         "viewPosition" : 120,
         "autoStart" : "false"
