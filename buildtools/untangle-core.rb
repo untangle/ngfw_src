8e1ac72605a22333061a24ae3c1a9a723bb4e09b
Sebastien Delafond <sdelafond@gmail.com>
2015-03-04 10:27:18 +0000

    [buildtools] Cleanups and comments (Comment: #12036)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@39770 8ebf8b4e-06f3-0310-961f-89f74618be54

--- buildtools/untangle-core.rb
+++ buildtools/untangle-core.rb
@@ -1,40 +1,44 @@
 # -*-ruby-*-
-# $Id$
 
-## Require arch-dep sub packages
-require "#{SRC_HOME}/libmvutil/package.rb"
-require "#{SRC_HOME}/libnetcap/package.rb"
-require "#{SRC_HOME}/libvector/package.rb"
-require "#{SRC_HOME}/jmvutil/package.rb"
-require "#{SRC_HOME}/jnetcap/package.rb"
-require "#{SRC_HOME}/jvector/package.rb"
-require "#{SRC_HOME}/uvm/package.rb" # FIXME
+# declare all arch-dep sub packages...
+pkgs = ['libmvutil', 'libnetcap', 'libvector',
+            'jmvutil', 'jnetcap', 'jvector']
 
+# ... and include them
+pkgs.each { |a| require "#{SRC_HOME}/#{a}/package.rb" }
+
+# files and dirs we'll manipulate
 libuvmcore_so = "#{BuildEnv::SRC.staging}/libuvmcore.so"
-dest_libuvmcore_dir = "#{BuildEnv::SRC['untangle-libuvmcore'].distDirectory}/usr/lib/uvm"
+dist_dir = BuildEnv::SRC['untangle-libuvmcore'].distDirectory
+dest_libuvmcore_dir = "#{dist_dir}/usr/lib/uvm/"
 dest_libuvmcore_so = "#{dest_libuvmcore_dir}/libuvmcore.so"
 
-archives = ['libmvutil', 'libnetcap', 'libvector', 'jmvutil', 'jnetcap', 'jvector']
+# make the .so dependent on all packages generated from arch-dep sub
+# packages, and describe how to build it
+file libuvmcore_so => pkgs.map { |a| BuildEnv::SRC[a]['archive'] } do
+  # define compiler
+  flags = "-pthread #{CCompilerEnv.defaultDebugFlags}"
+  compilerEnv = CCompilerEnv.new( {"flags" => flags} )
+  cbuilder = CBuilder.new(BuildEnv::SRC, compilerEnv)
 
-## Make the .so dependent on each archive
-archives.each do |n|
-  file libuvmcore_so => BuildEnv::SRC[n]['archive']
+  # shared lib building
+  pkgsFiles = pkgs.map { |a| BuildEnv::SRC[a]['archive'].filename }
+  cbuilder.makeSharedLibrary(pkgsFiles, libuvmcore_so, [],
+                             ['netfilter_queue','netfilter_conntrack'],
+                             [])
 end
 
-file libuvmcore_so do
-  compilerEnv = CCompilerEnv.new( { "flags" => "-pthread #{CCompilerEnv.defaultDebugFlags}" } )
-  archivesFiles = archives.map { |n| BuildEnv::SRC[n]['archive'].filename }
-
-  CBuilder.new(BuildEnv::SRC, compilerEnv).makeSharedLibrary(archivesFiles, libuvmcore_so, [],
-                                                             ['netfilter_queue','netfilter_conntrack'], [])
-end
+# associate a task to the building of that so file
 task :libuvmcore_so => libuvmcore_so
 
+# installed version of the so file
 file dest_libuvmcore_so => libuvmcore_so do
   mkdir_p(dest_libuvmcore_dir)
-  cp_r("#{BuildEnv::SRC.staging}/libuvmcore.so", dest_libuvmcore_dir, :verbose => true)
-  info "[copy    ] #{BuildEnv::SRC.staging}/libuvmcore.so #{dest_libuvmcore_dir}"
+  cp_r("#{BuildEnv::SRC.staging}/libuvmcore.so", dest_libuvmcore_dir)
+  info "[copy    ] #{libuvmcore_so} => #{dest_libuvmcore_dir}"
 end
+
+# associate a task to the installation of that so file
 task :dest_uvmcore_so => dest_libuvmcore_so
 
 # DO IT!
