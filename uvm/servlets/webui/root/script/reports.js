9bf747f78979eddce6e602d9af3e744b7c1b5eae
Vald Dumitrescu <vdumitrescu@untangle.com>
2015-04-27 17:10:41 +0000

    Implement Reports - first draft working with predefined Time reports in sitefilter
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@40118 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/servlets/webui/root/script/reports.js
+++ uvm/servlets/webui/root/script/reports.js
@@ -1,7 +1,7 @@
 Ext.define('Ung.panel.Reports', {
     extend: 'Ext.panel.Panel',
     settingsCmp: null,
-
+    layout: { type: 'vbox', align: 'stretch' },
     initComponent: function() {
         if(!this.title) {
             this.title = i18n._('Reports');
@@ -37,6 +37,8 @@ Ext.define('Ung.panel.Reports', {
         }, {
             xtype: 'container',
             name:'chartContainer',
+            flex: 1,
+            layout: 'fit',
             html: ""
         }];
         this.callParent(arguments);
@@ -44,65 +46,174 @@ Ext.define('Ung.panel.Reports', {
     },
     loadReport: function(rep) {
         this.chartContainer.removeAll();
-        var data = this.getReportingManagerNew().getDataForReportEntry(rep, null, null, -1).list;
-        console.log(rep);
-        console.log(data);
-        var chart = {xtype: 'component', html: ""};
-        if(rep.type == 'PIE_GRAPH') {
-            var dataStore = Ext.create('Ext.data.JsonStore', {
-                fields: [rep.pieGroupColumn, 'value' ],
-                data: data
-            }); 
+        this.chartContainer.add({
+            xtype: 'container',
+            margin: 10,
+            html: i18n._("Loading report data...")
+        });
+        this.getReportingManagerNew().getDataForReportEntry(Ext.bind(function(result, exception) {
+            if(Ung.Util.handleException(exception)) return;
+            if(!this.chartContainer || !this.chartContainer.isVisible()) {
+                return;
+            }
+            this.chartContainer.removeAll();
+            var data = result.list;
+            console.log(rep);
+            console.log(data);
+            var chart = {xtype: 'component', html: ""}, dataStore;
+            if(rep.type == 'PIE_GRAPH') {
+                var othersFn = function(val) {
+                    return (val==null)?i18n._("others"):val;
+                };
+                dataStore = Ext.create('Ext.data.JsonStore', {
+                    fields: [{name: rep.pieGroupColumn, convert: othersFn}, {name:'value'} ],
+                    data: data
+                }); 
 
-            chart = {
-                xtype: 'polar',
-                theme: 'default-gradients',
-                border: false,
-                width: '100%',
-                height: 500,
-                store: dataStore,
-                insetPadding: 50,
-                innerPadding: 20,
-                legend: {
-                    docked: 'bottom'
-                },
-                interactions: ['rotate', 'itemhighlight'],
-                sprites: [{
-                    type: 'text',
-                    text: rep.title,
-                    fontSize: 22,
-                    width: 100,
-                    height: 30,
-                    x: 40, // the sprite x position
-                    y: 20  // the sprite y position
-                }, {
-                    type: 'text',
-                    text: rep.description,
-                    x: 12,
-                    y: 425
-                }],
-                series: [{
-                    type: 'pie',
-                    angleField: 'value',
-                    label: {
-                        field: rep.pieGroupColumn,
-                        calloutLine: {
-                            length: 60,
-                            width: 3
-                            // specifying 'color' is also possible here
-                        }
+                chart = {
+                    xtype: 'polar',
+                    theme: 'default-gradients',
+                    border: false,
+                    width: '100%',
+                    height: '100%',
+                    store: dataStore,
+                    insetPadding: 50,
+                    innerPadding: 20,
+                    legend: {
+                        docked: 'right'
                     },
-                    highlight: true,
-                    tooltip: {
-                        trackMouse: true,
-                        renderer: function(storeItem, item) {
-                            this.setHtml(storeItem.get(rep.pieGroupColumn) + ": "+ storeItem.get('value') +" Hits");
+                    interactions: ['rotate', 'itemhighlight'],
+                    sprites: [{
+                        type: 'text',
+                        text: rep.title,
+                        fontSize: 22,
+                        width: 100,
+                        height: 30,
+                        x: 10, // the sprite x position
+                        y: 20  // the sprite y position
+                    }, {
+                        type: 'text',
+                        text: rep.description,
+                        fontSize: 12,
+                        x: 10,
+                        y: 35
+                    }],
+                    series: [{
+                        type: 'pie',
+                        angleField: 'value',
+                        label: {
+                            field: rep.pieGroupColumn,
+                            calloutLine: {
+                                length: 60,
+                                width: 3
+                                // specifying 'color' is also possible here
+                            }
+                        },
+                        highlight: true,
+                        tooltip: {
+                            trackMouse: true,
+                            renderer: function(storeItem, item) {
+                                this.setHtml(storeItem.get(rep.pieGroupColumn) + ": "+ storeItem.get('value') +" Hits");
+                            }
                         }
-                    }
-                }]
-            };
-        }
-        this.chartContainer.add(chart); 
+                    }]
+                };
+            } else if(rep.type == 'TIME_GRAPH') {
+                var axesFields = [];
+                var series = [], column;
+                var markerFx = {
+                    duration: 200,
+                    easing: 'backOut'
+                };
+                var zeroFn = function(val) {
+                    return (val==null)?0:val;
+                };
+                var timeFn = function(val) {
+                    return (val==null || val.time==null)?0:i18n.timestampFormat(val);
+                };
+                var storeFields =[{name: 'time_trunc', convert: timeFn}];
+                for(var i=0; i<rep.timeDataColumns.length; i++) {
+                    column = rep.timeDataColumns[i].split(" ").splice(-1)[0];
+                    axesFields.push(column);
+                    storeFields.push({name: column, convert: zeroFn});
+                    series.push({
+                        type: 'line',
+                        axis: 'left',
+                        title: column,
+                        xField: 'time_trunc',
+                        yField: column,
+                        marker: {
+                            type: 'square',
+                            fx: markerFx
+                        },
+                        highlightCfg: {
+                            scaling: 2
+                        },
+                        tooltip: {
+                            trackMouse: true,
+                            style: 'background: #fff',
+                            renderer: function(storeItem, item) {
+                                var title = item.series.getTitle();
+                                this.setHtml(title + ' for ' + storeItem.get('time_trunc') + ': ' + storeItem.get(item.series.getYField()) + ' Hits');
+                            }
+                        }
+                    });
+                }
+                dataStore = Ext.create('Ext.data.JsonStore', {
+                    fields: storeFields,
+                    data: data
+                });
+                
+                chart = {
+                    xtype: 'cartesian',
+                    width: '100%',
+                    height: '100%',
+                    border: false,
+                    legend: {
+                        docked: 'right'
+                    },
+                    store: dataStore,
+                    insetPadding: 40,
+                    sprites: [{
+                        type: 'text',
+                        text: rep.title,
+                        fontSize: 22,
+                        width: 100,
+                        height: 30,
+                        x: 10, // the sprite x position
+                        y: 20  // the sprite y position
+                    }, {
+                        type: 'text',
+                        text: rep.description,
+                        fontSize: 12,
+                        x: 10,
+                        y: 35
+                    }],
+                    axes: [{
+                        type: 'numeric',
+                        fields: axesFields,
+                        position: 'left',
+                        grid: true,
+                        minimum: 0,
+                        renderer: function (v) {
+                            return v + ' Hits';
+                        }
+                    }, {
+                        type: 'category',
+                        fields: 'time_trunc',
+                        position: 'bottom',
+                        grid: true,
+                        label: {
+                            rotate: {
+                                degrees: -45
+                            }
+                        }
+                    }],
+                    series: series
+                };
+            }
+            this.chartContainer.add(chart); 
+        }, this), rep, null, null, -1);
     },
     isDirty: function() {
         return false;
