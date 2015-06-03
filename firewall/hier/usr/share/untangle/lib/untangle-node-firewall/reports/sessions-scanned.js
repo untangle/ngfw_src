c561795b4a463e228884291d5cbca0969d6bfac5
Dirk Morris <dmorris@untangle.com>
2015-06-03 00:15:43 +0000

    start firewall reports
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@40409 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ firewall/hier/usr/share/untangle/lib/untangle-node-firewall/reports/sessions-scanned.js
@@ -0,0 +1,35 @@
+{
+    "uniqueId": "firewall-GlzEJqEcXv",
+    "category": "Firewall",
+    "description": "The amount of scanned, flagged, and blocked sessions over time.",
+    "displayOrder": 100,
+    "enabled": true,
+    "javaClass": "com.untangle.node.reporting.ReportEntry",
+    "orderDesc": false,
+    "units": "hits",
+    "preCompileResults": false,
+    "readOnly": true,
+    "table": "sessions",
+    "conditions": [
+        {
+            "javaClass": "com.untangle.uvm.node.SqlCondition",
+            "column": "firewall_rule_index",
+            "operator": "is",
+            "value": "not null"
+        }
+    ],
+     "timeDataColumns": [
+        "count(*) as scanned",
+        "sum(firewall_flagged::int) as flagged",
+        "sum(firewall_blocked::int) as blocked"
+    ],
+    "colors": [
+        "#396c2b",
+        "#e5e500",
+        "#8c0000"
+    ],
+    "timeDataInterval": "AUTO",
+    "timeStyle": "BAR_OVERLAP",
+    "title": "Scanned Sessions (all)",
+    "type": "TIME_GRAPH"
+}
