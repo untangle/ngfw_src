eab12ada855dbcd85e150075f2f579b4bf91e003
Mike Hotz <mahotz@untangle.com>
2015-06-17 21:04:23 +0000

    Created new report definitions.  This is my first time working on this stuff so these may need more work, but it's a start.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@40545 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ capture/hier/usr/share/untangle/lib/untangle-node-capture/reports/top-active-users.js
@@ -0,0 +1,26 @@
+{
+    "uniqueId": "captive-portal-EeDySOBc",
+    "category": "Captive Portal",
+    "description": "The top active users that logged in to Captive Portal.",
+    "displayOrder": 200,
+    "enabled": true,
+    "javaClass": "com.untangle.node.reporting.ReportEntry",
+    "orderByColumn": "value",
+    "orderDesc": true,
+    "units": "hits",
+    "pieGroupColumn": "login_name",
+    "pieSumColumn": "count(*)",
+    "readOnly": true,
+    "table": "capture_user_events",
+    "conditions": [
+        {
+            "javaClass": "com.untangle.uvm.node.SqlCondition",
+            "column": "event_info",
+            "operator": "=",
+            "value": "LOGIN"
+        }
+    ],
+    "title": "Top Active Users",
+    "type": "PIE_GRAPH"
+}
+
---
+++ capture/hier/usr/share/untangle/lib/untangle-node-capture/reports/top-blocked-clients.js
@@ -0,0 +1,26 @@
+{
+    "uniqueId": "captive-portal-IPiZ5SS7",
+    "category": "Captive Portal",
+    "description": "The top clients that were blocked by Captive Portal because they were not logged in.",
+    "displayOrder": 300,
+    "enabled": true,
+    "javaClass": "com.untangle.node.reporting.ReportEntry",
+    "orderByColumn": "value",
+    "orderDesc": true,
+    "units": "hits",
+    "pieGroupColumn": "client_addr",
+    "pieSumColumn": "count(*)",
+    "readOnly": true,
+    "table": "capture_user_events",
+    "conditions": [
+        {
+            "javaClass": "com.untangle.uvm.node.SqlCondition",
+            "column": "event_info",
+            "operator": "=",
+            "value": "FAILED"
+        }
+    ],
+    "title": "Top Blocked Clients",
+    "type": "PIE_GRAPH"
+}
+
