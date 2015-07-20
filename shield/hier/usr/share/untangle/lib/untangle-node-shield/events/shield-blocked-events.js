ebe5cb1e1fde1fc29a3915424789768430b4cbbc
Lee Graves <lgraves@untangle.com>
2015-07-20 16:19:40 +0000

    remove comma to fix json format
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@40745 8ebf8b4e-06f3-0310-961f-89f74618be54

--- shield/hier/usr/share/untangle/lib/untangle-node-shield/events/shield-blocked-events.js
+++ shield/hier/usr/share/untangle/lib/untangle-node-shield/events/shield-blocked-events.js
@@ -6,7 +6,7 @@
             "javaClass": "com.untangle.node.reporting.SqlCondition",
             "operator": "is",
             "value": "TRUE"
-        },
+        }
     ],
     "defaultColumns": ["time_stamp","username","hostname","c_client_port","s_server_addr","s_server_port","shield_blocked"],
     "description": "All sessions blocked by Shield.",
