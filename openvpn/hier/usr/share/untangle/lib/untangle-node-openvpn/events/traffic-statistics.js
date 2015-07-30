9859a8837938c759dc9799fa6b3177837661d57e
Mike Hotz <mahotz@untangle.com>
2015-07-30 18:20:37 +0000

    Added EventEntry for OpenVPN traffic stats.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@40832 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ openvpn/hier/usr/share/untangle/lib/untangle-node-openvpn/events/traffic-statistics.js
@@ -0,0 +1,11 @@
+{
+    "category": "OpenVPN",
+    "conditions": [],
+    "defaultColumns": ["time_stamp","start_time","end_time","rx_bytes","tx_bytes","pool_address","remote_port","client_name"],
+    "description": "Shows all OpenVPN connection traffic statistics events.",
+    "displayOrder": 20,
+    "javaClass": "com.untangle.node.reporting.EventEntry",
+    "table": "openvpn_stats",
+    "title": "Traffic Events",
+    "uniqueId": "openvpn-xll5yUY674"
+}
