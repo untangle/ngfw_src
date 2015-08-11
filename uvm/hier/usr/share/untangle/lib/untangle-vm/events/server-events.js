a530d4758f632dd711705165a0831c4e25c21586
Dirk Morris <dmorris@untangle.com>
2015-08-11 17:45:41 +0000

    add server_events tableConfig and event entry
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@40927 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ uvm/hier/usr/share/untangle/lib/untangle-vm/events/server-events.js
@@ -0,0 +1,11 @@
+{
+    "uniqueId": "system-TIxQ4tziAd",
+    "category": "System",
+    "conditions": [],
+    "defaultColumns": ["time_stamp","load_1","mem_free","disk_free"],
+    "description": "All system status events.",
+    "displayOrder": 10,
+    "javaClass": "com.untangle.node.reporting.EventEntry",
+    "table": "server_events",
+    "title": "Server Status Events"
+}
