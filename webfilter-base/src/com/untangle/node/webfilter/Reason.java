c8e558d5bb1460cadecc63404c601847e0594819
Dirk Morris <dmorris@untangle.com>
2015-08-13 19:53:06 +0000

    fix strings to be consistent with settings names
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@40961 8ebf8b4e-06f3-0310-961f-89f74618be54

--- webfilter-base/src/com/untangle/node/webfilter/Reason.java
+++ webfilter-base/src/com/untangle/node/webfilter/Reason.java
@@ -9,13 +9,12 @@ package com.untangle.node.webfilter;
 public enum Reason
 {
     BLOCK_CATEGORY('D', "in Categories Block list"),
-    BLOCK_URL('U', "in URLs Block list"),
-    BLOCK_EXTENSION('E', "in File Extensions Block list"),
+    BLOCK_URL('U', "in Site Block list"),
+    BLOCK_EXTENSION('E', "in File Block list"),
     BLOCK_MIME('M', "in MIME Types Block list"),
-    BLOCK_ALL('A', "blocking all traffic"), // XXX removed in 6.0
     BLOCK_IP_HOST('H', "hostname is an IP address"),
-    PASS_URL('I', "in URLs Pass list"),
-    PASS_REFERER_URL('R', "in URLs Pass list (via referer)"),
+    PASS_URL('I', "in Site Pass list"),
+    PASS_REFERER_URL('R', "referer in Site Pass list"),
     PASS_CLIENT('C', "in Clients Pass list"),
     PASS_UNBLOCK('B', "in Bypass list"),
     DEFAULT('N', "no rule applied");
