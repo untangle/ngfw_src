d4207ab7de584477cb443ce6c452fd24384d0810
Sebastien Delafond <sdelafond@gmail.com>
2015-09-09 09:19:00 +0000

    [untangle-vm] rake, trust me on this one: it's all gonna be fine eventually
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@41191 8ebf8b4e-06f3-0310-961f-89f74618be54

--- web-filter-base-lite/package.rb
+++ web-filter-base-lite/package.rb
@@ -3,7 +3,7 @@
 http = BuildEnv::SRC['untangle-casing-http']
 webfilter = BuildEnv::SRC['untangle-base-web-filter-lite']
 
-NodeBuilder.makeBase(BuildEnv::SRC, 'untangle-base-web-filter-lite', 'webfilter-base', [http['src']])
+NodeBuilder.makeBase(BuildEnv::SRC, 'untangle-base-web-filter-lite', 'web-filter-base-lite', [http['src']])
 
 deps = [webfilter['src'], http['src']]
 
