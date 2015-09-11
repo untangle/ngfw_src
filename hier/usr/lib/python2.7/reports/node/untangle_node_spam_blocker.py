db94623ca0acd5e47c2e98e21137172981387d36
Dirk Morris <dmorris@untangle.com>
2015-09-11 23:46:40 +0000

    move webcache to web-cache (Comment: #12524)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/src@41230 8ebf8b4e-06f3-0310-961f-89f74618be54

--- hier/usr/lib/python2.7/reports/node/untangle_node_spam_blocker.py
+++ hier/usr/lib/python2.7/reports/node/untangle_node_spam_blocker.py
@@ -1,7 +1,7 @@
 import uvm.i18n_helper
 import gettext
-import reports.node.untangle_base_spam
+import reports.node.untangle_base_spam_blocker
 
 _ = uvm.i18n_helper.get_translation('untangle-node-spam-blocker').lgettext
 
-reports.engine.register_node(reports.node.untangle_base_spam.SpamBaseNode('untangle-node-spam-blocker', 'Spam Blocker', 'spam_blocker', 'SpamBlocker', _('Spam'), _('Clean'), _("Hourly Spam Rate"), _("Spam Rate"), _("Top Ten Spammed")))
+reports.engine.register_node(reports.node.untangle_base_spam_blocker.SpamBaseNode('untangle-node-spam-blocker', 'Spam Blocker', 'spam_blocker', 'SpamBlocker', _('Spam'), _('Clean'), _("Hourly Spam Rate"), _("Spam Rate"), _("Top Ten Spammed")))
