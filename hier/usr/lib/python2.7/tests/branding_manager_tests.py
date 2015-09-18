d2446a331f4035554e0c05479afe34baa6ec2822
Dirk Morris <dmorris@untangle.com>
2015-09-18 18:00:24 +0000

    rename branding to branding-manager (Comment: #12524)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/src@41304 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ hier/usr/lib/python2.7/tests/branding_manager_tests.py
@@ -0,0 +1,194 @@
+import unittest2
+import os
+import re
+import urllib
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+from jsonrpc import ServiceProxy
+from jsonrpc import JSONRPCException
+from uvm import Manager
+from uvm import Uvm
+import test_registry
+import remote_control
+import system_properties
+import pdb
+
+node = None
+nodeWeb = None
+nodeSpam = None
+newCompanyName = "Some new long name"
+newURL = "https://test.untangle.com/cgi-bin/myipaddress.py"
+newContactName = "Skynet"
+newContactEmail = "skynet@untangle.com"
+
+
+uvmContext = Uvm().getUvmContext()
+defaultRackId = 1
+
+def nukeBrandingManagerSettings():
+    nodeData = {
+        "companyName": "Untangle", 
+        "companyUrl": "http://untangle.com/", 
+        "contactName": "your network administrator", 
+        "defaultLogo": True, 
+        "javaClass": "com.untangle.node.branding_manager.BrandingManagerSettings"
+    }
+    node.setSettings(nodeData)
+    
+class BrandingManagerTests(unittest2.TestCase):
+    
+    @staticmethod
+    def nodeName():
+        return "untangle-node-branding-manager"
+
+    @staticmethod
+    def nodeNameWeb():
+        return "untangle-node-web-filter"
+
+    @staticmethod
+    def nodeNameSpam():
+        return "untangle-node-spam-blocker-lite"
+
+    @staticmethod
+    def vendorName():
+        return "Untangle"
+
+    def setUp(self):
+        global nodeData, node, nodeWeb, nodeSpam
+        if node == None:
+            if (uvmContext.nodeManager().isInstantiated(self.nodeName())):
+                print "ERROR: Node %s already installed" % self.nodeName()
+                raise Exception('node %s already instantiated' % self.nodeName())
+            node = uvmContext.nodeManager().instantiate(self.nodeName(), defaultRackId)
+            nodeData = node.getSettings()
+        if nodeWeb == None:
+            if (uvmContext.nodeManager().isInstantiated(self.nodeNameWeb())):
+                print "ERROR: Node %s already installed" % self.nodeNameWeb()
+                raise Exception('node %s already instantiated' % self.nodeNameWeb())
+            nodeWeb = uvmContext.nodeManager().instantiate(self.nodeNameWeb(), defaultRackId)
+        if nodeSpam == None:
+            if (uvmContext.nodeManager().isInstantiated(self.nodeNameSpam())):
+                print "ERROR: Node %s already installed" % self.nodeNameSpam()
+                raise Exception('node %s already instantiated' % self.nodeNameSpam())
+            nodeSpam = uvmContext.nodeManager().instantiate(self.nodeNameSpam(), defaultRackId)
+
+    # verify client is online
+    def test_010_clientIsOnline(self):
+        result = remote_control.isOnline()
+        assert (result == 0)
+
+    def test_020_changeBranding(self):
+        global node, nodeWeb, nodeData
+        nodeData['companyName'] = newCompanyName;
+        nodeData['companyUrl'] = newURL;
+        nodeData['contactName'] = newContactName;
+        nodeData['contactEmail'] = newContactEmail;
+        node.setSettings(nodeData)
+        # test blockpage has all the changes
+        result = remote_control.runCommand("wget -q -O - \"$@\" www.playboy.com",stdout=True)
+
+        # Verify Title of blockpage as company name
+        myRegex = re.compile('<title>(.*?)</title>', re.IGNORECASE|re.DOTALL)
+        matchText = myRegex.search(result).group(1)
+        matchText = matchText.split("|")[0]
+        matchText = matchText.strip()
+        assert(matchText == newCompanyName)
+
+        # Verify email address is in the contact link
+        myRegex = re.compile('mailto:(.*?)\?', re.IGNORECASE|re.DOTALL)
+        matchText = myRegex.search(result).group(1)
+        matchText = matchText.strip()
+        assert(matchText == newContactEmail)
+
+        # Verify contact name is in the mailto
+        myRegex = re.compile('mailto:.*?>(.*?)<\/a>', re.IGNORECASE|re.DOTALL)
+        matchText = myRegex.search(result).group(1)
+        matchText = matchText.strip()
+        assert(matchText == newContactName)
+
+        # Verify URL is in the Logo box
+        myRegex = re.compile('<a href\=\"(.*?)\"><img alt\=\"\" src\=\"\/images\/BrandingLogo', re.IGNORECASE|re.DOTALL)
+        matchText = myRegex.search(result).group(1)
+        assert(matchText == newURL)
+       
+        # Check login page for branding
+        internalAdmin = None
+        httpAdmin = system_properties.getHttpAdminUrl()
+        # print "IP address <%s>" % internalAdmin
+        result = remote_control.runCommand("wget -q -O - \"$@\" " + httpAdmin ,stdout=True)
+        # print "page is <%s>" % result
+        # Verify Title of blockpage as company name
+        myRegex = re.compile('<title>(.*?)</title>', re.IGNORECASE|re.DOTALL)
+        matchText = myRegex.search(result).group(1)
+        matchText = matchText.split("|")[0]
+        matchText = matchText.strip()
+        matchText = matchText.strip(" Administrator Login")
+        # print "MatchText <%s>" % matchText
+        assert(matchText == newCompanyName)
+
+        # Check quarantine page
+        result = remote_control.runCommand("wget -q -O - \"$@\" " + "\"" + httpAdmin + "quarantine/\"",stdout=True)
+        # print "page is <%s>" % result
+        # Verify Title of blockpage as company name
+        myRegex = re.compile('<title>(.*?)</title>', re.IGNORECASE|re.DOTALL)
+        matchText = myRegex.search(result).group(1)
+        matchText = matchText.split("|")[0]
+        matchText = matchText.strip()
+        # print "MatchText <%s>" % matchText
+        assert(matchText == newCompanyName)
+
+    def test_021_changeBranding_bannerMessage_added(self):
+        global node, nodeWeb, nodeData
+        nodeData['companyName'] = newCompanyName;
+        nodeData['companyUrl'] = newURL;
+        nodeData['contactName'] = newContactName;
+        nodeData['contactEmail'] = newContactEmail;
+        nodeData['bannerMessage'] = "A regulation banner requirement containing a mix of text including <b>html</b> and\nmultiple\nlines"
+        node.setSettings(nodeData)
+
+        internalAdmin = None
+        httpAdmin = system_properties.getHttpAdminUrl()
+        result = remote_control.runCommand("wget -q -O - \"$@\" " + httpAdmin ,stdout=True)
+        myRegex = re.compile('.*A regulation banner requirement containing a mix of text including <b>html<\/b> and<br\/>multiple<br\/>lines.*', re.DOTALL|re.MULTILINE)
+        if re.match(myRegex,result):
+            assert(True)
+        else:
+            assert(False)
+        
+    def test_022_changeBranding_bannerMessage_removed(self):
+        global node, nodeWeb, nodeData
+        nodeData['companyName'] = newCompanyName;
+        nodeData['companyUrl'] = newURL;
+        nodeData['contactName'] = newContactName;
+        nodeData['contactEmail'] = newContactEmail;
+        nodeData['bannerMessage'] = ""
+        node.setSettings(nodeData)
+
+        internalAdmin = None
+        httpAdmin = system_properties.getHttpAdminUrl()
+        result = remote_control.runCommand("wget -q -O - \"$@\" " + httpAdmin ,stdout=True)
+        myRegex = re.compile('.*A regulation banner requirement containing a mix of text including <b>html<\/b> and<br\/>multiple<br\/>lines.*', re.DOTALL|re.MULTILINE)
+        if re.match(myRegex,result):
+            assert(False)
+        else:
+            assert(True)
+        
+    @staticmethod
+    def finalTearDown(self):
+        global node, nodeWeb, nodeSpam
+        # Restore original settings to return to initial settings
+        nukeBrandingManagerSettings()
+        if node != None:
+            uvmContext.nodeManager().destroy( node.getNodeSettings()["id"] )
+            node = None
+        if nodeWeb != None:
+            uvmContext.nodeManager().destroy( nodeWeb.getNodeSettings()["id"] )
+            nodeWeb = None
+        if nodeSpam != None:
+            uvmContext.nodeManager().destroy( nodeSpam.getNodeSettings()["id"] )
+            nodeSpam = None
+
+
+test_registry.registerNode("branding-manager", BrandingManagerTests)
---
+++ src/com/untangle/node/branding_manager/BrandingManagerApp.java
@@ -0,0 +1,287 @@
+/**
+ * $Id$
+ */
+package com.untangle.node.branding_manager;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.apache.log4j.Logger;
+import org.apache.commons.fileupload.FileItem;
+
+import com.untangle.uvm.UvmContextFactory;
+import com.untangle.uvm.vnet.NodeBase;
+import com.untangle.uvm.vnet.PipelineConnector;
+import com.untangle.uvm.node.License;
+import com.untangle.uvm.servlet.UploadHandler;
+import com.untangle.uvm.SettingsManager;
+
+public class BrandingManagerApp extends NodeBase implements com.untangle.uvm.BrandingManager
+{
+    private static final File DEFAULT_LOGO = new File("/var/www/images/Logo150x96.png");;
+    private static final File BRANDING_LOGO = new File("/var/www/images/BrandingLogo.png");
+    private static final String BRANDING_LOGO_WEB_PATH = "images/BrandingLogo.png";
+
+    private final Logger logger = Logger.getLogger(getClass());
+    private final PipelineConnector[] connectors = new PipelineConnector[] {};
+
+    private BrandingManagerSettings settings = null;
+
+    public BrandingManagerApp( com.untangle.uvm.node.NodeSettings nodeSettings, com.untangle.uvm.node.NodeProperties nodeProperties )
+    {
+        super( nodeSettings, nodeProperties );
+    }
+
+    private void readNodeSettings()
+    {
+        SettingsManager settingsManager = UvmContextFactory.context().settingsManager();
+        String nodeID = this.getNodeSettings().getId().toString();
+        String settingsFile = System.getProperty("uvm.settings.dir") + "/untangle-node-branding/settings_" + nodeID + ".js";
+        BrandingManagerSettings readSettings = null;
+
+        logger.info("Loading settings from " + settingsFile);
+
+        try {
+            readSettings =  settingsManager.load( BrandingManagerSettings.class, settingsFile);
+        } catch (Exception exn) {
+            logger.error("Could not read node settings", exn);
+        }
+
+        try {
+            if (readSettings == null) {
+                logger.warn("No settings found... initializing with defaults");
+                initializeSettings();
+            }
+            else {
+                this.settings = readSettings;
+                setFileLogo(settings.binary_getLogo());                
+            }
+        }
+        catch (Exception exn) {
+            logger.error("Could not apply node settings", exn);
+        }
+    }
+
+    private void writeNodeSettings(BrandingManagerSettings argSettings)
+    {
+    }
+
+    @Override
+    protected void preStart()
+    {
+        /**
+         * Overwrite the logo just in case it has changed on disk
+         */
+        setFileLogo(settings.binary_getLogo());
+    }
+
+    @Override
+    protected void postInit()
+    {
+        readNodeSettings();
+        setFileLogo(settings.binary_getLogo());
+        UvmContextFactory.context().servletFileManager().registerUploadHandler( new LogoUploadHandler(this) );
+    }
+
+    @Override
+    protected PipelineConnector[] getConnectors()
+    {
+        return this.connectors;
+    }
+
+    public BrandingManagerSettings getSettings()
+    {
+        /**
+         * Don't send the logo - that is handled by the upload manager
+         */
+        BrandingManagerSettings copy = new BrandingManagerSettings(this.settings);
+        boolean defaultLogo = copy.getDefaultLogo();
+        copy.binary_setLogo(null);
+        copy.setDefaultLogo(defaultLogo);
+
+        return copy;
+    }
+
+    public void setSettings(final BrandingManagerSettings newSettings)
+    {
+        /**
+         * getSettings always returns null logo
+         * since setSettings will be called using a modified version of
+         * what getSettings returns we should restore current logo
+         * before saving if getDefaultLogo is still false and logo is null
+         */
+        if (!newSettings.getDefaultLogo() && newSettings.binary_getLogo() == null)
+            newSettings.binary_setLogo(this.settings.binary_getLogo());
+
+        logger.debug("setSettings(): " + newSettings.getCompanyName());
+
+        SettingsManager settingsManager = UvmContextFactory.context().settingsManager();
+        String nodeID = this.getNodeSettings().getId().toString();
+        String settingsFile = System.getProperty("uvm.settings.dir") + "/untangle-node-branding/settings_" + nodeID +".js";
+
+        try {
+            settingsManager.save( settingsFile, newSettings );
+        } catch (Exception exn) {
+            logger.error("Could not save branding settings", exn);
+            return;
+        }
+        
+        this.settings = newSettings;
+
+        setFileLogo(settings.binary_getLogo());
+    }
+
+    @Override
+    public void initializeSettings()
+    {
+        BrandingManagerSettings settings = new BrandingManagerSettings();
+        logger.info("Initializing Settings...");
+
+        /**
+         * If OEM, initialize settings differently
+         */
+        String oemName = UvmContextFactory.context().oemManager().getOemName();
+        if (oemName == null || oemName == "Untangle") {
+            settings.setCompanyName("Untangle");
+            settings.setCompanyUrl("http://untangle.com/");
+        } else {
+            settings.setCompanyName("MyCompany");
+            settings.setCompanyUrl("http://mycompany.com/");
+        }
+
+        settings.setContactName("your network administrator");
+        settings.setContactEmail(null);
+
+        setSettings(settings);
+    }
+
+    @Override
+    public String getContactHtml()
+    {
+        return settings.grabContactHtml();
+    }
+
+    @Override
+    public String getContactEmail()
+    {
+        return settings.getContactEmail();
+    }
+
+    @Override
+    public String getContactName()
+    {
+        return settings.getContactName();
+    }
+
+    @Override
+    public String getCompanyUrl()
+    {
+        return settings.getCompanyUrl();
+    }
+
+    @Override
+    public String getCompanyName()
+    {
+        return settings.getCompanyName();
+    }
+
+    public void setLogo(byte[] logo)
+    {
+        if ( ! isLicenseValid() ) {
+            return;
+        }
+
+        BrandingManagerSettings settings = this.getSettings();
+        settings.binary_setLogo(logo);
+        settings.setDefaultLogo(false);
+        this.setSettings(settings);
+    }
+
+    private void setFileLogo(byte[] logo)
+    {
+        /* if license is invalid - revert to default logo */
+        if ( ! isLicenseValid() )
+            logo = null;
+
+        logger.debug("Writing logo file: " + BRANDING_LOGO);
+
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+
+        try {
+            fos = new FileOutputStream(BRANDING_LOGO);
+
+            if (null == logo) {
+                byte[] buf = new byte[1024];
+                fis = new FileInputStream(DEFAULT_LOGO);
+                int c;
+                while (0 <= (c = fis.read(buf))) {
+                    fos.write(buf, 0, c);
+                }
+            } else {
+                fos.write(logo);
+            }
+        } catch (IOException exn) {
+            logger.warn("could not change icon", exn);
+        } finally {
+            if (null != fis) {
+                try {
+                    fis.close();
+                } catch (IOException exn) {
+                    logger.warn("could not close", exn);
+                }
+            }
+
+            if (null != fos) {
+                try {
+                    fos.close();
+                } catch (IOException exn) {
+                    logger.warn("could not close", exn);
+                }
+            }
+        }
+    }
+
+    private boolean isLicenseValid()
+    {
+        if (UvmContextFactory.context().licenseManager().isLicenseValid(License.BRANDING_MANAGER))
+            return true;
+        if (UvmContextFactory.context().licenseManager().isLicenseValid(License.BRANDING_MANAGER_OLDNAME))
+            return true;
+        return false;
+    }
+
+    private class LogoUploadHandler implements UploadHandler
+    {
+        private BrandingManagerApp node;
+
+        LogoUploadHandler( BrandingManagerApp node )
+        {
+            this.node = node;
+        }
+
+        @Override
+        public String getName()
+        {
+            return "logo";
+        }
+
+        @Override
+        public String handleFile(FileItem fileItem, String argument) throws Exception
+        {
+            if (fileItem.getName().toLowerCase().endsWith(".gif") ||
+                fileItem.getName().toLowerCase().endsWith(".png") ||
+                fileItem.getName().toLowerCase().endsWith(".jpg") ||
+                fileItem.getName().toLowerCase().endsWith(".jpeg") ) {
+                byte[] logo=fileItem.get();
+
+                /* Use the context in order to properly handler premium vs normal. */
+                node.setLogo(logo);
+            } else {
+                throw new Exception("Branding logo must be GIF, PNG, or JPG");
+            }
+            return "Uploaded new branding logo";
+        }
+    }
+}
