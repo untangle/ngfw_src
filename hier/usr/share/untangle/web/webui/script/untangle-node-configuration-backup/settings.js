5f2a6ec9f8576ce00c037047133a2c9b01d58372
Vald Dumitrescu <vdumitrescu@untangle.com>
2015-09-28 13:56:17 +0000

    Replace <scope>.i18n with i18 in UI
    Comment: #12490
    
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/src@41432 8ebf8b4e-06f3-0310-961f-89f74618be54

--- hier/usr/share/untangle/web/webui/script/untangle-node-configuration-backup/settings.js
+++ hier/usr/share/untangle/web/webui/script/untangle-node-configuration-backup/settings.js
@@ -24,11 +24,11 @@ Ext.define('Webui.untangle-node-configuration-backup.settings', {
     getStatus: function () {
         var event = this.getLatestEvent();
         if (event == null)
-            return this.i18n._("Backup pending.");
+            return i18n._("Backup pending.");
         else if (event.success == true)
-            return this.i18n._("Successfully backed up on") + " " + i18n.timestampFormat(event.timeStamp) + "</i>";
+            return i18n._("Successfully backed up on") + " " + i18n.timestampFormat(event.timeStamp) + "</i>";
         else
-            return this.i18n._("Last backup failed on") + " <i>" + i18n.timestampFormat(event.timeStamp) + "</i>";
+            return i18n._("Last backup failed on") + " <i>" + i18n.timestampFormat(event.timeStamp) + "</i>";
     },
     saveAction: function() {
         this.closeWindow();
@@ -42,35 +42,35 @@ Ext.define('Webui.untangle-node-configuration-backup.settings', {
             Ung.Util.rpcExHandler(e);
         }
         this.panelStatus = Ext.create('Ext.panel.Panel',{
-            title: this.i18n._('Status'),
+            title: i18n._('Status'),
             name: 'Status',
             helpSource: 'configuration_backup_status',
             autoScroll: true,
             cls: 'ung-panel',
             items: [{
                 xtype: 'fieldset',
-                title: this.i18n._('Status'),
+                title: i18n._('Status'),
                 html: this.getStatus()
             },{
                 xtype: 'fieldset',
-                title: this.i18n._('Note'),
-                html: Ext.String.format(this.i18n._('Configuration Backup automatically saves your configuration settings and sends them to {0} for safe storage. If you need a backup of your configuration settings, contact {1}'),
+                title: i18n._('Note'),
+                html: Ext.String.format(i18n._('Configuration Backup automatically saves your configuration settings and sends them to {0} for safe storage. If you need a backup of your configuration settings, contact {1}'),
                         rpc.companyName, contactName)
             }, {
                 xtype: 'fieldset',
-                title: this.i18n._('Backup Now'),
+                title: i18n._('Backup Now'),
                 layout: {type: 'hbox', align: 'middle'},
                 items: [{
                     xtype: 'container',
                     margin: '0 50 0 0',
-                    html: this.i18n._('Force an immediate backup now.')
+                    html: i18n._('Force an immediate backup now.')
                 }, {
                     xtype: 'button',
-                    text: this.i18n._("Backup now"),
+                    text: i18n._("Backup now"),
                     name: 'Backup now',
                     iconCls: 'action-icon',
                     handler: Ext.bind(function(callback) {
-                        Ext.MessageBox.wait(this.i18n._("Backing up... This may take a few minutes."), i18n._("Please wait"));
+                        Ext.MessageBox.wait(i18n._("Backing up... This may take a few minutes."), i18n._("Please wait"));
                         this.getRpcNode().sendBackup(Ext.bind(function(result, exception) {
                             Ext.MessageBox.hide();
                             if(Ung.Util.handleException(exception)) return;
