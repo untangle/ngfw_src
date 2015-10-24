1a8aeabfcdaec9352803748288aa59b07b3dfb68
Dirk Morris <dmorris@untangle.com>
2015-10-24 00:15:36 +0000

    Implement sql batching when writing SQL events to DB. It will batch all events that use the same basic string SQL. This should reduce the #writes and gives a pretty large speedup.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/src@41561 8ebf8b4e-06f3-0310-961f-89f74618be54

--- src/com/untangle/node/web_filter/WebFilterQueryEvent.java
+++ src/com/untangle/node/web_filter/WebFilterQueryEvent.java
@@ -67,7 +67,7 @@ public class WebFilterQueryEvent extends LogEvent
     public void setContentLength( long contentLength ) { this.contentLength = contentLength; }
 
     @Override
-    public java.sql.PreparedStatement getDirectEventSql( java.sql.Connection conn ) throws Exception
+    public void compileStatements( java.sql.Connection conn, java.util.Map<String,java.sql.PreparedStatement> statementCache ) throws Exception
     {
         String sql =
         "INSERT INTO reports.http_query_events" + getPartitionTablePostfix() + " " +
@@ -100,7 +100,7 @@ public class WebFilterQueryEvent extends LogEvent
             "?" +
         ")";
 
-        java.sql.PreparedStatement pstmt = conn.prepareStatement( sql );
+        java.sql.PreparedStatement pstmt = getStatementFromCache( sql, statementCache, conn );        
 
         int i=0;
         pstmt.setTimestamp(++i, getTimeStamp());
@@ -125,7 +125,8 @@ public class WebFilterQueryEvent extends LogEvent
         pstmt.setString(++i, getSessionEvent().getHostname());
         pstmt.setString(++i, getTerm());
 
-        return pstmt;
+        pstmt.addBatch();
+        return;
     }
 
 
