af3d6f5538fc026c82ad4a17c0fabcf81368beac
Dirk Morris <dmorris@untangle.com>
2015-11-16 18:01:04 +0000

    add some padding
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@41716 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/share/untangle/web/script/tableConfig.js
+++ uvm/hier/usr/share/untangle/web/script/tableConfig.js
@@ -100,9 +100,15 @@ Ext.define('Ung.TableConfig', {
             if(tableConfig!=null && Ext.isArray(tableConfig.columns)) {
                 for(var i = 0; i<tableConfig.columns.length; i++) {
                     col = tableConfig.columns[i];
+                    var name = col.header;
+                    var nameLength = 50;
+                    var padding = ( nameLength - name.length > 0 ? nameLength - name.length : 0);
+                    //FIXME this doesnt really look right because variable length characters
+                    //XXX
+                    name = name + (new Array(padding).join('&nbsp;'));
                     columns.push({
                         dataIndex: col.dataIndex,
-                        header: col.header + " [" + col.dataIndex + "]"
+                        header: name + " [" + col.dataIndex + "]"
                     });
                 }
             }
