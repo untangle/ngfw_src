bd30660832c4212aed1621d1d4b3275bc3dc8c3a
Vald Dumitrescu <vdumitrescu@untangle.com>
2015-12-24 12:52:47 +0000

    Panel Status new implementation.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/src@41991 8ebf8b4e-06f3-0310-961f-89f74618be54

--- hier/usr/share/untangle/web/webui/script/untangle-node-virus-blocker/settings.js
+++ hier/usr/share/untangle/web/webui/script/untangle-node-virus-blocker/settings.js
@@ -2,6 +2,9 @@ Ext.define('Webui.untangle-node-virus-blocker.settings', {
     extend:'Webui.untangle-base-virus-blocker.settings',
     helpSourceName: 'virus_blocker',
     aboutInfo: '&copy; BitDefender 1997-2014',
+    getAppSummary: function() {
+        return i18n._("Virus Blocker protects your network from zero-day threats to newly released malware using a multi-layer advanced threat protection system.");
+    },
     initComponent: function() {
         this.callParent(arguments);
     }
