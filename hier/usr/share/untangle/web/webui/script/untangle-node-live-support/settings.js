73045580e17ca50cc1e710b410d3bb5a1acd20df
Vald Dumitrescu <vdumitrescu@untangle.com>
2015-12-24 12:52:47 +0000

    Panel Status new implementation.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/src@41991 8ebf8b4e-06f3-0310-961f-89f74618be54

--- hier/usr/share/untangle/web/webui/script/untangle-node-live-support/settings.js
+++ hier/usr/share/untangle/web/webui/script/untangle-node-live-support/settings.js
@@ -1,16 +1,16 @@
 Ext.define('Webui.untangle-node-live-support.settings', {
     extend:'Ung.NodeWin',
     hasReports: false,
-    hasDefaultAppStatus: false,
-    panelSupport: null,
     hasApply: false, // do not need save
+    getAppSummary: function() {
+        return i18n._('Live Support for easy resolution of any technical issues.');
+    },
     initComponent: function(container, position) {
-        this.buildSupport();
-        this.buildTabPanel([this.panelSupport]);
+        this.buildTabPanel([]);
         this.callParent(arguments);
     },
     // Support Panel
-    buildSupport: function() {
+    buildStatus: function() {
         var serverUID, fullVersionAndRevision;
         try {
             serverUID = rpc.jsonrpc.UvmContext.getServerUID();
@@ -18,45 +18,39 @@ Ext.define('Webui.untangle-node-live-support.settings', {
         } catch (e) {
             Ung.Util.rpcExHandler(e);
         }
-        this.panelSupport = Ext.create('Ext.panel.Panel',{
-                name: 'Support',
-                helpSource: 'live_support_support',
-                title: i18n._('Support'),
-                cls: 'ung-panel',
-                autoScroll: true,
-                defaults: {
-                    xtype: 'fieldset'
-                },
-                items: [this.buildAppStatus(), {
-                    title: i18n._('Live Support'),
-                    items: [{
-                        xtype: 'component',
-                        html: Ext.String.format(i18n._("This {0} Server is entitled to Live Support."), rpc.companyName) 
-                    }, {
-                        xtype: 'button',
-                        margin: '10 0 0 0',
-                        text: i18n._('Get Support!'),
-                        name: 'Get Support',
-                        iconCls: 'action-icon',
-                        handler: Ext.bind(function() {
-                            Ung.Main.openSupportScreen();
-                        }, this)
-                    }]
-                },{
-                    title: i18n._('Support Information'),
-                    defaults: {
-                        xtype: 'displayfield',
-                        labelWidth: 50
-                    },
-                    items: [{
-                        fieldLabel: i18n._("UID"),
-                        value: serverUID
-                    }, {
-                        fieldLabel: i18n._("Build"),
-                        value: fullVersionAndRevision
-                    }]
+        this.panelStatus = Ext.create('Ung.panel.Status', {
+            settingsCmp: this,
+            helpSource: 'live_support_support',
+            itemsToAppend: [{
+                title: i18n._('Live Support'),
+                items: [{
+                    xtype: 'component',
+                    html: Ext.String.format(i18n._("This {0} Server is entitled to Live Support."), rpc.companyName) 
+                }, {
+                    xtype: 'button',
+                    margin: '10 0 0 0',
+                    text: i18n._('Get Support!'),
+                    name: 'Get Support',
+                    iconCls: 'action-icon',
+                    handler: Ext.bind(function() {
+                        Ung.Main.openSupportScreen();
+                    }, this)
                 }]
-            });
+            },{
+                title: i18n._('Support Information'),
+                defaults: {
+                    xtype: 'displayfield',
+                    labelWidth: 50
+                },
+                items: [{
+                    fieldLabel: i18n._("UID"),
+                    value: serverUID
+                }, {
+                    fieldLabel: i18n._("Build"),
+                    value: fullVersionAndRevision
+                }]
+            }]
+        });
     },
     // save function
     saveAction: function() {
