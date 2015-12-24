5ff2d6cc03a8ad70b9f3963c12fde80d1177fdb7
John Coffin <jcoffin@gmail.com>
2015-12-24 07:50:27 +0000

    merge test-tls into mailsender.py and add SSL Inspecter test
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@41990 8ebf8b4e-06f3-0310-961f-89f74618be54

--- phish-blocker/hier/usr/lib/python2.7/tests/phish_blocker_tests.py
+++ phish-blocker/hier/usr/lib/python2.7/tests/phish_blocker_tests.py
@@ -52,8 +52,13 @@ def getLatestMailSender():
     results = remote_control.runCommand("tar -xvf mailpkg.tar")
     # print "Results from untaring mailpkg.tar <%s>" % results
 
-def sendPhishMail(mailfrom="test"):
-    results = remote_control.runCommand("python mailsender.py --from=" + mailfrom + "@example.com --to=\"qa@example.com\" ./phish-mail/ --host="+smtpServerHost+" --reconnect --series=30:0,150,100,50,25,0,180")
+def sendPhishMail(mailfrom="test", host=smtpServerHost, useTLS=False):
+    mailResult = None
+    if useTLS:
+        mailResult = remote_control.runCommand("python mailsender.py --from=" + mailfrom + "@example.com --to=qa@example.com ./phish-mail/ --host=" + host + " --reconnect --series=30:0,150,100,50,25,0,180 --starttls", stdout=False, nowait=False)
+    else:
+        mailResult = remote_control.runCommand("python mailsender.py --from=" + mailfrom + "@example.com --to=qa@example.com ./phish-mail/ --host=" + host + " --reconnect --series=30:0,150,100,50,25,0,180")
+    return mailResult
 
 class PhishBlockerTests(unittest2.TestCase):
 
@@ -70,14 +75,23 @@ class PhishBlockerTests(unittest2.TestCase):
         return "untangle-casing-smtp"
 
     @staticmethod
+    def nodeNameSSLInspector():
+        return "untangle-casing-ssl-inspector"
+
+    @staticmethod
     def initialSetUp(self):
-        global node, nodeData, nodeSP, nodeDataSP, canRelay
+        global node, nodeData, nodeSP, nodeDataSP, nodeSSL, nodeSSLData, canRelay
         if (uvmContext.nodeManager().isInstantiated(self.nodeName())):
             raise unittest2.SkipTest('node %s already instantiated' % self.nodeName())
         node = uvmContext.nodeManager().instantiate(self.nodeName(), defaultRackId)
         nodeData = node.getSettings()
         nodeSP = uvmContext.nodeManager().node(self.nodeNameSpamCase())
         nodeDataSP = nodeSP.getSmtpNodeSettings()
+        if uvmContext.nodeManager().isInstantiated(self.nodeNameSSLInspector()):
+            raise Exception('node %s already instantiated' % self.nodeNameSSLInspector())
+        nodeSSL = uvmContext.nodeManager().instantiate(self.nodeNameSSLInspector(), defaultRackId)
+        # nodeSSL.start() # leave node off. node doesn't auto-start
+        nodeSSLData = nodeSSL.getSettings()
         try:
             canRelay = sendTestmessage()
         except Exception,e:
@@ -125,12 +139,13 @@ class PhishBlockerTests(unittest2.TestCase):
         except:
             pass
                 
-        sendPhishMail("test020")
-        sendPhishMail("test021")
-        sendPhishMail("test022")
+        sendPhishMail(mailfrom="test020")
+        sendPhishMail(mailfrom="test021")
+        sendPhishMail(mailfrom="test022")
 
         events = global_functions.get_events('Phish Blocker','All Phish Events',None,1)
         assert(events != None)
+        # print events['list'][0]
         found = global_functions.check_events( events.get('list'), 5,
                                             'c_server_addr', ip_address_testuntangle,
                                             's_server_port', 25,
@@ -151,7 +166,7 @@ class PhishBlockerTests(unittest2.TestCase):
         match = re.search(r'\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}', result)
         ip_address_testuntangle = match.group()
 
-        sendPhishMail("test030")
+        sendPhishMail(mailfrom="test030")
 
         events = global_functions.get_events('Phish Blocker','All Phish Events',None,1)
         assert(events != None)
@@ -175,7 +190,7 @@ class PhishBlockerTests(unittest2.TestCase):
         match = re.search(r'\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}', result)
         ip_address_testuntangle = match.group()
 
-        sendPhishMail("test040")
+        sendPhishMail(mailfrom="test040")
 
         events = global_functions.get_events('Phish Blocker','All Phish Events',None,1)
         assert(events != None)
@@ -194,18 +209,40 @@ class PhishBlockerTests(unittest2.TestCase):
         externalClientResult = subprocess.call(["ping -c 1 " + tlsSmtpServerHost + " >/dev/null 2>&1"],shell=True,stdout=None,stderr=None)            
         if (externalClientResult != 0):
             raise unittest2.SkipTest("TLS SMTP server is unreachable, skipping TLS Allow check")
-        # Get latest TLS test command file
-        testCopyResult = subprocess.call(["scp -3 -o 'StrictHostKeyChecking=no' -i " + system_properties.getPrefix() + "/usr/lib/python2.7/tests/testShell.key testshell@" + tlsSmtpServerHost + ":/home/testshell/test-tls.py testshell@" + remote_control.clientIP + ":/home/testshell/"],shell=True,stdout=None,stderr=None)
-        assert(testCopyResult == 0)
-        tlsSMTPResult = remote_control.runCommand("python test-tls.py", stdout=False, nowait=False)
+        tlsSMTPResult = sendPhishMail(host=tlsSmtpServerHost, useTLS=True)
         # print "TLS  : " + str(tlsSMTPResult)
         assert(tlsSMTPResult == 0)
        
+    def test_060_checkTLSwSSLInspector(self):
+        wan_IP = uvmContext.networkManager().getFirstWanAddress()
+        if not global_functions.isInOfficeNetwork(wan_IP):
+            raise unittest2.SkipTest("Not on office network, skipping")
+        externalClientResult = subprocess.call(["ping -c 1 " + tlsSmtpServerHost + " >/dev/null 2>&1"],shell=True,stdout=None,stderr=None)            
+        if (externalClientResult != 0):
+            raise unittest2.SkipTest("TLS SMTP server is unreachable, skipping TLS Allow check")
+        nodeSSL.start()
+        tlsSMTPResult = sendPhishMail(mailfrom="test060", host=tlsSmtpServerHost, useTLS=True)
+        # print "TLS  : " + str(tlsSMTPResult)
+        nodeSSL.stop()
+        assert(tlsSMTPResult == 0)
+
+        events = global_functions.get_events('Phish Blocker','All Phish Events',None,1)
+        assert(events != None)
+        found = global_functions.check_events( events.get('list'), 5,
+                                            'c_server_addr', tlsSmtpServerHost,
+                                            's_server_port', 25,
+                                            'addr', 'qa@example.com',
+                                            'c_client_addr', remote_control.clientIP,
+                                            'phish_blocker_action', 'D')
+
     @staticmethod
     def finalTearDown(self):
-        global node
+        global node, nodeSSL
         if node != None:
             uvmContext.nodeManager().destroy( node.getNodeSettings()["id"] )
             node = None
+        if nodeSSL != None:
+            uvmContext.nodeManager().destroy( nodeSSL.getNodeSettings()["id"] )
+            nodeSSL = None
 
 test_registry.registerNode("phish-blocker", PhishBlockerTests)
