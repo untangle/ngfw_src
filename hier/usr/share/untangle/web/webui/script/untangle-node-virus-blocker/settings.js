ad450fb1bbff9047d810f469bde8adc115d905f1
Dirk Morris <dmorris@untangle.com>
2016-01-19 16:59:46 +0000

    fix app summary text
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/src@42147 8ebf8b4e-06f3-0310-961f-89f74618be54

--- hier/usr/share/untangle/web/webui/script/untangle-node-virus-blocker/settings.js
+++ hier/usr/share/untangle/web/webui/script/untangle-node-virus-blocker/settings.js
@@ -3,7 +3,7 @@ Ext.define('Webui.untangle-node-virus-blocker.settings', {
     helpSourceName: 'virus_blocker',
     aboutInfo: '&copy; BitDefender 1997-2014',
     getAppSummary: function() {
-        return i18n._("Virus Blocker Lite detects and blocks malware before it reaches users' desktops or mailboxes.");
+        return i18n._("Virus Blocker detects and blocks malware before it reaches users' desktops or mailboxes.");
     },
     initComponent: function() {
         this.callParent(arguments);
