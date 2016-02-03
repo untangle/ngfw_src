2646333df2da6d7e960213677aab684a012c3cbf
Dirk Morris <dmorris@untangle.com>
2016-02-03 14:45:08 +0000

    check web_filter_reason instead of category (category is now correct) (Comment: #12756)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@42342 8ebf8b4e-06f3-0310-961f-89f74618be54

--- web-filter-lite/hier/usr/share/untangle/lib/untangle-node-web-filter-lite/events/web-filter-lite-unblocked-web-events.js
+++ web-filter-lite/hier/usr/share/untangle/lib/untangle-node-web-filter-lite/events/web-filter-lite-unblocked-web-events.js
@@ -2,10 +2,10 @@
     "category": "Web Filter Lite",
     "conditions": [
         {
-            "column": "web_filter_lite_category",
+            "column": "web_filter_lite_reason",
             "javaClass": "com.untangle.node.reports.SqlCondition",
             "operator": "=",
-            "value": "unblocked"
+            "value": "B"
         }
     ],
     "defaultColumns": ["time_stamp","hostname","username","host","uri","web_filter_lite_blocked","web_filter_lite_flagged","web_filter_lite_reason","web_filter_lite_category","s_server_addr","s_server_port"],
