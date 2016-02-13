eb69a4e275bacd146a9e013de5dd650d55d3b0d4
Dirk Morris <dmorris@untangle.com>
2016-02-13 02:49:02 +0000

    change length of callout line, white it out so it isnt as obnoxious
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@42461 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/servlets/webui/root/script/dashboardWidgets.js
+++ uvm/servlets/webui/root/script/dashboardWidgets.js
@@ -1367,14 +1367,15 @@ Ext.define('Ung.dashboard.Util', {
                             var storeItem = store.getAt(index);
                             var value = store.getAt(index).get('value');
                             var percent = value/total;
-                            if ( percent > 0.2 )
+                            if ( percent >= 0.15 )
                                 return storeItem.get(entry.pieGroupColumn);
                             else
                                 return '';
                         },
                         calloutLine: {
-                            length: 10,
-                            width: 1
+                            color: '#FFFFFF',
+                            length: 30,
+                            width: 0
                         }
                     }
                 }]
