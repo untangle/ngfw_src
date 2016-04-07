9860946babc256959d624110ed07adedffd60208
Dirk Morris <dmorris@untangle.com>
2016-04-07 15:55:10 +0000

    work in progress on chromedriver (Comment: #12736)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/src@42867 8ebf8b4e-06f3-0310-961f-89f74618be54

--- src/com/untangle/node/directory_connector/GoogleAuthenticator.java
+++ src/com/untangle/node/directory_connector/GoogleAuthenticator.java
@@ -7,34 +7,82 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
-import org.apache.log4j.Logger;
+import com.untangle.uvm.UvmContextFactory;
 
+import org.apache.log4j.Logger;
+import com.google.common.collect.ImmutableMap;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+    
 public class GoogleAuthenticator
 {
     private static final Logger logger = Logger.getLogger(GoogleAuthenticator.class);
 
-    public static boolean authenticate(String username, String password)
+    public synchronized static boolean authenticate(String username, String password)
     {
-        WebDriver driver = new PhantomJSDriver();
-        Wait<WebDriver> wait = new WebDriverWait(driver, 30);
+        //  Xvfb :1 -screen 5 1024x768x8 &
+        logger.debug("Initializing WebDriver...");
+
+        String port = ":1.5";
+        Path   tmpDir;
+        String tmpDirName;
         try {
-            String baseUrl = "https://accounts.google.com/ServiceLogin?sacu=1";
+            tmpDir = Files.createTempDirectory( "tmp-chromedriver" );
+            tmpDirName = "/tmp/" + tmpDir.getFileName().toString() + "/";
+        } catch ( Exception e ) {
+            logger.warn("Failed to create temp directory.",e);
+            return false;
+        }
+        
+        // WebDriver driver = new PhantomJSDriver();
+            
+        ChromeDriverService service = new ChromeDriverService.Builder()
+            .usingDriverExecutable(new File("/usr/lib/chromium/chromedriver"))
+            .usingAnyFreePort()
+            .withEnvironment(ImmutableMap.of("DISPLAY",port))
+            .build();
+        System.setProperty("webdriver.chrome.driver", "/usr/lib/chromium/chromedriver");
+        System.setProperty("webdriver.chrome.bin", "/usr/bin/chromium");
+        System.setProperty("webdriver.chrome.logfile", "/tmp/chrome.log");
+        System.setProperty("webdriver.chrome.verboseLogging", "true");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--user-data-dir=" + tmpDirName);
+        WebDriver driver = new ChromeDriver(service, options);
+        // WebDriver driver = new ChromeDriver(service);
+
+        // System.setProperty("webdriver.firefox.bin", "/usr/bin/iceweasel");
+        // FirefoxBinary firefoxBinary = new FirefoxBinary();
+        // firefoxBinary.setEnvironmentProperty("DISPLAY", port);
+        // driver = new FirefoxDriver(firefoxBinary, null);
+            
+        logger.debug("Initializing WebDriver... done (" + port + "," + tmpDirName + ")");
+
+        try {
+            Wait<WebDriver> wait = new WebDriverWait(driver, 30);
+
+            String baseUrl = "https://accounts.google.com/ServiceLogin#identifier";
             logger.debug("[" + username + "] Authenticating...");
 
             logger.debug("[" + username + "] Loading page...");
             driver.get(baseUrl);
+
+            if ( logger.isDebugEnabled() ) takeScreenshot(driver,tmpDirName + "screenshot-0.png");
             waitForElementId(driver, wait, "next");
+            if ( logger.isDebugEnabled() ) takeScreenshot(driver,tmpDirName + "screenshot-1.png");
         
             logger.debug("[" + username + "] Sending email...");
             List<WebElement> emailElements = driver.findElements(By.name("Email"));
@@ -48,8 +96,12 @@ public class GoogleAuthenticator
                 return false;
             }
             emailElements.get(0).sendKeys(username);
+
+            if ( logger.isDebugEnabled() ) takeScreenshot(driver,tmpDirName + "screenshot-2.png");
             nextButtonElements.get(0).click();
+            if ( logger.isDebugEnabled() ) takeScreenshot(driver,tmpDirName + "screenshot-3.png");
             waitForElementName(driver, wait, "Passwd");
+            if ( logger.isDebugEnabled() ) takeScreenshot(driver,tmpDirName + "screenshot-4.png");
 
             logger.debug("[" + username + "] Sending password...");
             List<WebElement> passwdElements = driver.findElements(By.name("Passwd"));
@@ -63,11 +115,14 @@ public class GoogleAuthenticator
                 return false;
             }
             passwdElements.get(0).sendKeys(password);
+
+            if ( logger.isDebugEnabled() ) takeScreenshot(driver,tmpDirName + "screenshot-5.png");
             signInButtonElements.get(0).click();
+            if ( logger.isDebugEnabled() ) takeScreenshot(driver,tmpDirName + "screenshot-6.png");
             waitForLogin(driver, wait, "Email", "xb");
+            if ( logger.isDebugEnabled() ) takeScreenshot(driver,tmpDirName + "screenshot-7.png");
 
             //printElements(driver);
-            //takeScreenshot(driver,"/tmp/screenshot1.png");
         
             logger.debug("[" + username + "] Analyzing page...");
             boolean succeeded = wasLoginSuccessful( driver );
@@ -77,39 +132,49 @@ public class GoogleAuthenticator
                 logger.debug("[" + username + "] Login failed.");
 
             return succeeded;
-        }
-        finally {
+        } catch (Exception e) {
+            logger.warn("Exception",e);
+            return false;
+        } finally {
             driver.close();
+            if ( ! logger.isDebugEnabled() )
+                UvmContextFactory.context().execManager().exec("/bin/rm -rf " + tmpDirName);
         }
+        
     }
 
     private static boolean wasLoginSuccessful( WebDriver driver )
     {
+        List<WebElement> elements;
+
         /**
-         * If prompting for email, login must have failed
+         * If prompting for email or passwd, login must have failed
          */
-        List<WebElement> emails = driver.findElements(By.name("Email"));
-        if ( emails.size() > 0 )
+        elements = driver.findElements(By.name("Email"));
+        if ( elements.size() > 0 )
+            return false;
+        elements = driver.findElements(By.name("Passd"));
+        if ( elements.size() > 0 )
             return false;
 
         /**
-         * The XB element is only on the account page
+         * Look for elements on the account page
          */
-        List<WebElement> xbs = driver.findElements(By.id("xb"));
-        if ( xbs.size() > 0 )
+        elements = driver.findElements(By.id("xb"));
+        if ( elements.size() > 0 )
             return true;
-        /**
-         * If it says account settings, must have succeeded
-         * This may not be in english so this may not work
-         */
-        if ( driver.getPageSource().contains("Account Settings") )
+        elements = driver.findElements(By.id("roster-for-Google-Help"));
+        if ( elements.size() > 0 )
             return true;
 
         /**
-         * If it says that you should download firefox or chrome
-         * then the login succeeded
+         * Look for common phrases on the account page
+         * This will only work in english so its a last resort
          */
-        if ( driver.getPageSource().contains("Firefox") )
+        String pageSource = driver.getPageSource();
+        if ( pageSource.contains("Account Settings") )
+            return true;
+        if ( pageSource.contains("Welcome") )
             return true;
         
         return false;
@@ -168,7 +233,7 @@ public class GoogleAuthenticator
     
     private static void takeScreenshot(final WebDriver driver, String filename)
     {
-        logger.warn("Saving screenshot: " + filename);
+        logger.debug("Saving screenshot: " + filename);
         File scrFile = ((org.openqa.selenium.TakesScreenshot)driver).getScreenshotAs(org.openqa.selenium.OutputType.FILE);
         copyFile(scrFile, new File(filename));
     }
