82e4b1ab7ac8de7cab1ed98f5857ce7eb4d37aa8
Dirk Morris <dmorris@untangle.com>
2016-04-19 21:31:35 +0000

    merge EventEntry into ReportEntry. Event Entries are now just Report Entries of type EVENT_LIST
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/src@43016 8ebf8b4e-06f3-0310-961f-89f74618be54

--- hier/usr/share/untangle/lib/untangle-node-web-filter/reports/web-filter-flagged-web-events.js
+++ hier/usr/share/untangle/lib/untangle-node-web-filter/reports/web-filter-flagged-web-events.js
@@ -1,18 +1,19 @@
 {
     "category": "Web Filter",
+    "type": "EVENT_LIST",
     "conditions": [
         {
-            "column": "web_filter_blocked",
+            "column": "web_filter_flagged",
             "javaClass": "com.untangle.node.reports.SqlCondition",
             "operator": "is",
             "value": "TRUE"
         }
     ],
     "defaultColumns": ["time_stamp","hostname","username","host","uri","web_filter_blocked","web_filter_flagged","web_filter_reason","web_filter_category","s_server_addr","s_server_port"],
-    "description": "Shows all blocked web requests.",
-    "displayOrder": 12,
-    "javaClass": "com.untangle.node.reports.EventEntry",
+    "description": "Shows all flagged web requests.",
+    "displayOrder": 1011,
+    "javaClass": "com.untangle.node.reports.ReportEntry",
     "table": "http_events",
-    "title": "Blocked Web Events",
-    "uniqueId": "web-filter-EZ7FRTJGUP"
+    "title": "Flagged Web Events",
+    "uniqueId": "web-filter-P3PSUGFTIY"
 }
