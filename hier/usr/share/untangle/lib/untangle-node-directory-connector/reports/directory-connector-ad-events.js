b75730711afa5a50fcf69119883cbea66858f4c7
Dirk Morris <dmorris@untangle.com>
2016-04-19 21:31:35 +0000

    merge EventEntry into ReportEntry. Event Entries are now just Report Entries of type EVENT_LIST
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/src@43016 8ebf8b4e-06f3-0310-961f-89f74618be54

--- hier/usr/share/untangle/lib/untangle-node-directory-connector/reports/directory-connector-ad-events.js
+++ hier/usr/share/untangle/lib/untangle-node-directory-connector/reports/directory-connector-ad-events.js
@@ -1,10 +1,11 @@
 {
     "category": "Directory Connector",
+    "type": "EVENT_LIST",
     "conditions": [],
     "defaultColumns": ["time_stamp","login_name","domain","type","client_addr"],
     "description": "Events from the user notification API.",
-    "displayOrder": 10,
-    "javaClass": "com.untangle.node.reports.EventEntry",
+    "displayOrder": 1010,
+    "javaClass": "com.untangle.node.reports.ReportEntry",
     "table": "directory_connector_login_events",
     "title": "AD Events",
     "uniqueId": "directory-connector-RKL4XIEPFQ"
