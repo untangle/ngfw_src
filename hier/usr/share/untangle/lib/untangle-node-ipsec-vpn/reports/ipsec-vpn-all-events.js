2c0fc39f38687000fce6ef10478b4ade40ec0868
Dirk Morris <dmorris@untangle.com>
2016-04-19 21:31:35 +0000

    merge EventEntry into ReportEntry. Event Entries are now just Report Entries of type EVENT_LIST
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/src@43016 8ebf8b4e-06f3-0310-961f-89f74618be54

--- hier/usr/share/untangle/lib/untangle-node-ipsec-vpn/reports/ipsec-vpn-all-events.js
+++ hier/usr/share/untangle/lib/untangle-node-ipsec-vpn/reports/ipsec-vpn-all-events.js
@@ -1,10 +1,11 @@
 {
     "category": "IPsec VPN",
+    "type": "EVENT_LIST",
     "conditions": [],
     "defaultColumns": ["time_stamp","connect_stamp","goodbye_stamp","client_address","client_protocol","client_username","net_interface","rx_bytes","tx_bytes"],
     "description": "Shows all user L2TP/Xauth events.",
-    "displayOrder": 10,
-    "javaClass": "com.untangle.node.reports.EventEntry",
+    "displayOrder": 1010,
+    "javaClass": "com.untangle.node.reports.ReportEntry",
     "table": "ipsec_user_events",
     "title": "L2TP/Xauth Events",
     "uniqueId": "ipsec-vpn-CIHJZEU5OX"
