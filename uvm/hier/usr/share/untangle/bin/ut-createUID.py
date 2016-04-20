500802a6efc1161e3e6d27f6116dc48fcaaf801f
Dirk Morris <dmorris@untangle.com>
2016-04-20 02:13:12 +0000

    rewrite stable-120 to stable-120x
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/release-12.0/work/src@43026 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/share/untangle/bin/ut-createUID.py
+++ uvm/hier/usr/share/untangle/bin/ut-createUID.py
@@ -120,7 +120,7 @@ file.close()
 file = open( SOURCES_FILENAME, "w+" )
 file.write("## Auto Generated on %s\n" % datetime.datetime.now());
 file.write("## DO NOT EDIT. Changes will be overwritten.\n" + "\n");
-file.write("deb http://%s:untangle@%s/public/%s %s main non-free upstream" % (uid, UPDATE_SERVER, debian_distro, "stable-120x"_)+ "\n")
+file.write("deb http://%s:untangle@%s/public/%s %s main non-free upstream" % (uid, UPDATE_SERVER, debian_distro, "stable-120x")+ "\n")
 file.flush()
 file.close()
 
