d022ee57a3f1b53ae09bc075e0af80c23bc6abc8
Dirk Morris <dmorris@untangle.com>
2016-05-18 15:13:48 +0000

    change summary to gigabytes
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@43370 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/share/untangle/lib/untangle-vm/reports/summary.js
+++ uvm/hier/usr/share/untangle/lib/untangle-vm/reports/summary.js
@@ -6,10 +6,10 @@
     "enabled": true,
     "javaClass": "com.untangle.node.reports.ReportEntry",
     "textColumns": [
-        "round(coalesce(sum(s2p_bytes + p2s_bytes), 0)/(1024*1024)) as megabytes",
+        "round((coalesce(sum(s2p_bytes + p2s_bytes), 0)/(1024*1024*1024)),1) as gigabytes",
         "count(*) as sessions"
     ],
-    "textString": "The server has passed {0} megabytes across {1} sessions.", 
+    "textString": "The server passed {0} gigabytes and {1} sessions.", 
     "readOnly": true,
     "table": "sessions",
     "title": "Network Summary",
