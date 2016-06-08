7888e9df54f88885fe88a9f3ed7457844338ffc6
Mike Hotz <mahotz@untangle.com>
2016-06-08 21:29:28 +0000

    Display empty field for RX and TX bytes for L2TP and Xauth sessions that are still active, since we don't get traffic details until disconnect. Closes: #13005
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@43594 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/hier/usr/share/untangle/web/script/tableConfig.js
+++ uvm/hier/usr/share/untangle/web/script/tableConfig.js
@@ -2700,6 +2700,7 @@ Ext.define('Ung.TableConfig', {
                     sortable: true,
                     dataIndex: "rx_bytes",
                     renderer: function(value) {
+                        if ((value === null) || (value ==="")) return("");
                         var kb = value/1024;
                         return (Math.round( kb*10 )/10).toString() + " KB";
                     }
@@ -2709,6 +2710,7 @@ Ext.define('Ung.TableConfig', {
                     sortable: true,
                     dataIndex: "tx_bytes",
                     renderer: function(value) {
+                        if ((value === null) || (value ==="")) return("");
                         var kb = value/1024;
                         return (Math.round( kb*10 )/10).toString() + " KB";
                     }
