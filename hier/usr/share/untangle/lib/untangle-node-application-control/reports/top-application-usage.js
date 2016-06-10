eb09f607eca080254571ce07f1d14fff245352c8
Dirk Morris <dmorris@untangle.com>
2016-06-10 16:46:40 +0000

    fix syntax
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/src@43620 8ebf8b4e-06f3-0310-961f-89f74618be54

--- hier/usr/share/untangle/lib/untangle-node-application-control/reports/top-application-usage.js
+++ hier/usr/share/untangle/lib/untangle-node-application-control/reports/top-application-usage.js
@@ -14,7 +14,7 @@
     "timeDataDynamicColumn": "application_control_application",
     "timeDataDynamicLimit": "10",
     "timeDataDynamicAggregationFunction": "sum",
-    "timeDataDynamicAllowNull", true,
+    "timeDataDynamicAllowNull": true,
     "timeStyle": "AREA_STACKED",
     "title": "Top Applications Usage",
     "type": "TIME_GRAPH_DYNAMIC"
