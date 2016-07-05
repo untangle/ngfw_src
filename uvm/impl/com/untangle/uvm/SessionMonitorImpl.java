d267bb989e242397f0ea89ea4b97e8ccaa430623
Dirk Morris <dmorris@untangle.com>
2016-07-05 18:22:16 +0000

    set qos priority properly (Closes: #13040)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@43710 8ebf8b4e-06f3-0310-961f-89f74618be54

--- uvm/impl/com/untangle/uvm/SessionMonitorImpl.java
+++ uvm/impl/com/untangle/uvm/SessionMonitorImpl.java
@@ -523,7 +523,7 @@ public class SessionMonitorImpl implements SessionMonitor
                             int mark = Integer.parseInt( varval );
                             newEntry.setMark( mark ); 
                             newEntry.setBypassed( ((mark & 0x01000000) != 0) );
-                            newEntry.setPriority( (mark & 0x000F0000) >> 16 );
+                            newEntry.setQosPriority( (mark & 0x000F0000) >> 16 );
                             newEntry.setClientIntf( (mark & 0x000000FF) >> 0 );
                             newEntry.setServerIntf( (mark & 0x0000FF00) >> 8 );
                             break;
