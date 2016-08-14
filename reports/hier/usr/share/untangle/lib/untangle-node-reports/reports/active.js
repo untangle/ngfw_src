41821926a924084e9ddcda8f41399b0f93ce471f
Dirk Morris <dmorris@untangle.com>
2016-08-14 22:44:54 +0000

    minor text fixes
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@44054 8ebf8b4e-06f3-0310-961f-89f74618be54

--- reports/hier/usr/share/untangle/lib/untangle-node-reports/reports/active.js
+++ reports/hier/usr/share/untangle/lib/untangle-node-reports/reports/active.js
@@ -1,16 +1,16 @@
 {
     "uniqueId": "system-lL959lz7qu",
     "category": "System",
-    "description": "The amount of free memory over time.",
+    "description": "The highest number of active hosts.",
     "displayOrder": 150,
     "enabled": true,
     "javaClass": "com.untangle.node.reports.ReportEntry",
     "textColumns": [
         "max(active_hosts) as max_active_hosts"
     ],
-    "textString": "{0} active hosts.", 
+    "textString": "{0} hosts.", 
     "readOnly": true,
     "table": "server_events",
-    "title": "Max Active Hosts",
+    "title": "Highest Active Hosts",
     "type": "TEXT"
 }
