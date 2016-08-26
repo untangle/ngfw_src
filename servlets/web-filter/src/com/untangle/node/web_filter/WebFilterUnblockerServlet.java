d173973ea4408c110c2542b6ceb4fc4bcd79b023
Dirk Morris <dmorris@untangle.com>
2016-08-26 19:19:13 +0000

    rename tid (transform ID) to appid (application ID)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/src@44129 8ebf8b4e-06f3-0310-961f-89f74618be54

--- servlets/web-filter/src/com/untangle/node/web_filter/WebFilterUnblockerServlet.java
+++ servlets/web-filter/src/com/untangle/node/web_filter/WebFilterUnblockerServlet.java
@@ -4,40 +4,56 @@
 package com.untangle.node.web_filter;
 
 import java.io.IOException;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.untangle.uvm.UvmContextFactory;
+import com.untangle.uvm.util.I18nUtil;
 import com.untangle.uvm.node.NodeManager;
 import com.untangle.uvm.node.NodeSettings;
+import com.untangle.node.web_filter.WebFilterApp;
 
 @SuppressWarnings("serial")
 public class WebFilterUnblockerServlet extends HttpServlet
 {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException
     {
-        resp.setContentType("text/xml");
-        resp.addHeader("Cache-Control", "no-cache");
+        Map<String,String> i18n_map = UvmContextFactory.context().languageManager().getTranslations( "untangle" );
 
-        String nonce = req.getParameter("nonce");
-        String tidStr = req.getParameter("tid");
-        String password = req.getParameter("password");
-        boolean global = Boolean.parseBoolean(req.getParameter("global"));
+        response.setContentType("text/xml");
+        response.addHeader("Cache-Control", "no-cache");
+
+        String nonce = request.getParameter("nonce");
+        String password = request.getParameter("password");
+        boolean global = Boolean.parseBoolean(request.getParameter("global"));
+
+        NodeManager nm = UvmContextFactory.context().nodeManager();
+        WebFilterApp node = null;
+        if ( node == null )
+            try {node = (WebFilterApp) nm.node( Long.parseLong(request.getParameter( "tid" )) );} catch (Exception e) {}
+        if ( node == null )
+            try {node = (WebFilterApp) nm.node( Long.parseLong(request.getParameter( "appid" )) );} catch (Exception e) {}
 
         try {
-            NodeManager tman = UvmContextFactory.context().nodeManager();
-            Long nodeId = Long.parseLong(tidStr);
-            WebFilterApp node = (WebFilterApp) tman.node( nodeId );
+            if ( node == null ) { 
+                response.sendError( HttpServletResponse.SC_NOT_ACCEPTABLE, I18nUtil.tr( "App ID not found.", i18n_map ));
+                return;
+            }
+            if ( !(node instanceof WebFilter) ) {
+                response.sendError( HttpServletResponse.SC_NOT_ACCEPTABLE, I18nUtil.tr( "Invalid App ID.", i18n_map ));
+                return;
+            }
 
             if (node.unblockSite(nonce, global, password)) {
-                resp.getOutputStream().println("<success/>");
+                response.getOutputStream().println("<success/>");
             } else {
-                resp.getOutputStream().println("<failure/>");
+                response.getOutputStream().println("<failure/>");
             }
         } catch (IOException exn) {
             throw new ServletException(exn);
