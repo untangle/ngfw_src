3a9882b75fe4fad8fa8c6c63be3615cfd6b14a87
Mike Hotz <mahotz@untangle.com>
2016-08-30 18:28:03 +0000

    More and better comments in the new VirusFileManager class.  I also found and fixed an edge case bug that could cause problems in the future. Mixing calls to write(int) and write(ByteBuffer) could have caused data to be written out of order.  It's not a problem today because of the way the different protocol handlers do their writing, but I went ahead and fixed by making sure we always flush the write(int) buffer when the write(ByteBuffer) version is called.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/src@44149 8ebf8b4e-06f3-0310-961f-89f74618be54

--- virus-blocker-base/src/com/untangle/node/virus_blocker/VirusFileManager.java
+++ virus-blocker-base/src/com/untangle/node/virus_blocker/VirusFileManager.java
@@ -25,7 +25,7 @@ public class VirusFileManager extends OutputStream
     private FileInputStream fileInStream = null;
     private FileOutputStream fileOutStream = null;
     private FileChannel fileInChannel = null;
-    private ByteArrayOutputStream arrayStream = null;
+    private ByteArrayOutputStream dummyStream = null;
     private MessageDigest hashDigest = null;
     private DigestOutputStream hashStream = null;
     private LinkedList<ByteBuffer> memoryBuffer = null;
@@ -39,9 +39,9 @@ public class VirusFileManager extends OutputStream
         if (memoryMode == true) {
             logger.debug("VIRUS: Using memory buffering mode");
             memoryBuffer = new LinkedList<ByteBuffer>();
-            arrayStream = new ByteArrayOutputStream();
+            dummyStream = new ByteArrayOutputStream();
             hashDigest = MessageDigest.getInstance("MD5");
-            hashStream = new DigestOutputStream(arrayStream, hashDigest);
+            hashStream = new DigestOutputStream(dummyStream, hashDigest);
         } else {
             diskFile = File.createTempFile(filePrefix, null);
 
@@ -58,6 +58,7 @@ public class VirusFileManager extends OutputStream
 
     public int read(ByteBuffer bb)
     {
+        // in file mode we read from the disk and return
         if (memoryMode == false) {
             int retval = 0;
 
@@ -69,13 +70,13 @@ public class VirusFileManager extends OutputStream
             return (retval);
         }
 
-        // return end of file if the list is empty
+        // memory mode active so return end of file if the list is empty
         if (memoryBuffer.size() == 0) return (-1);
 
         int total = 0;
         int count = 0;
 
-        // fill the passed buffer from the memory buffer
+        // we have data so fill passed buffer until full or until we're empty
         while (bb.remaining() > 0) {
             count = filler(bb);
             if (count <= 0) break;
@@ -87,11 +88,13 @@ public class VirusFileManager extends OutputStream
 
     private int filler(ByteBuffer bb)
     {
+        // grab the first buffer from the list
         ByteBuffer local = memoryBuffer.peek();
 
         // if we run out of stuff in the list signal end of data
         if (local == null) return (0);
 
+        // figure out how much space is available in each buffer
         int arg = bb.remaining();
         int mem = (local.limit() - local.position());
 
@@ -118,21 +121,66 @@ public class VirusFileManager extends OutputStream
 
     public int write(ByteBuffer bb)
     {
+        return (_write(bb, true));
+    }
+
+    public void write(int b)
+    {
+        /*
+         * I think this is only used by the SMTP handler. We take single
+         * characters and buffer them until we have enough to do the actual
+         * write.
+         */
+
+        if (byteWriter.remaining() == 0) {
+            byteWriter.flip();
+            _write(byteWriter, false);
+            byteWriter.clear();
+        }
+
+        byteWriter.put((byte) b);
+    }
+
+    private int _write(ByteBuffer bb, boolean preFlush)
+    {
+        /*
+         * If there is anything in the byteWriter buffer and preFlush is set we
+         * handle that first so things don't end up out of order.
+         */
+
+        if (preFlush == true) flush();
+
         int pos = bb.position();
         int rem = bb.remaining();
 
         if (memoryMode == true) {
+
+            /*
+             * In memory mode we store the passed data in our linked list and we
+             * also write to our dummyStream (via hashStream) which allows
+             * MessageDigest to calculate the checksum as we stream the file. We
+             * then immediately clear the data sent to the dummy stream so it
+             * doesn't grow, since we only use it to allow MessageDigest to do
+             * its thing.
+             */
+
             ByteBuffer local = ByteBuffer.allocate(rem - pos);
             local.put(bb.array(), pos, rem);
             local.flip();
             memoryBuffer.add(local);
             try {
                 hashStream.write(bb.array(), pos, rem);
-                arrayStream.reset();
+                dummyStream.reset();
             } catch (Exception exn) {
                 logger.error("Virus scan write memory failed: ", exn);
             }
+
         } else {
+
+            /*
+             * In file mode the MessageDigest is associated directly with the
+             * actual disk file so we just do a normal write
+             */
 
             try {
                 hashStream.write(bb.array(), pos, rem);
@@ -144,17 +192,6 @@ public class VirusFileManager extends OutputStream
         bb.position(pos + rem);
 
         return (rem - pos);
-    }
-
-    public void write(int b)
-    {
-        if (byteWriter.remaining() == 0) {
-            byteWriter.flip();
-            write(byteWriter);
-            byteWriter.clear();
-        }
-
-        byteWriter.put((byte) b);
     }
 
     public void position(int location)
@@ -170,14 +207,17 @@ public class VirusFileManager extends OutputStream
 
     public void flush()
     {
+        // handle any data hanging around in the byteWriter
         if (byteWriter.position() > 0) {
             byteWriter.flip();
-            write(byteWriter);
+            _write(byteWriter, false);
             byteWriter.clear();
         }
 
+        // for memory mode we are done
         if (memoryMode == true) return;
 
+        // for file mode flush the actual file streams 
         try {
             hashStream.flush();
             fileOutStream.flush();
@@ -188,10 +228,13 @@ public class VirusFileManager extends OutputStream
 
     public void close()
     {
+        // handle any data hanging around in the byteWriter
         flush();
 
+        // for memory mode we are done
         if (memoryMode == true) return;
 
+        // for file mode we close the actual file stream
         try {
             fileOutStream.close();
         } catch (Exception exn) {
@@ -201,6 +244,9 @@ public class VirusFileManager extends OutputStream
 
     public boolean delete()
     {
+        // nothing to delete in memory mode
+        if (memoryMode == true) return (true);
+
         boolean retstat = false;
 
         try {
@@ -216,6 +262,7 @@ public class VirusFileManager extends OutputStream
     {
         int total = 0;
 
+        // calculate the total amount of data stored in our linked list
         for (int i = 0; i < memoryBuffer.size(); i++) {
             ByteBuffer local = memoryBuffer.get(i);
             total += local.remaining();
@@ -226,6 +273,7 @@ public class VirusFileManager extends OutputStream
 
     protected File getFileObject()
     {
+        // return the actual file object we are using which could be null for memory mode
         return (diskFile);
     }
 
@@ -246,8 +294,10 @@ public class VirusFileManager extends OutputStream
         String fileHash = "00000000000000000000000000000000";
         if (hashDigest == null) return (fileHash);
 
+        // handle any data hanging around in the byteWriter        
         flush();
 
+        // get the hash from the MessageDigest and close the stream
         try {
             BigInteger val = new BigInteger(1, hashDigest.digest());
             fileHash = String.format("%1$032x", val);
