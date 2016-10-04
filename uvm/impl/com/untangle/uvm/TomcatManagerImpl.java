9f3ee2b29c55e045ed98b073c4393d7d1e5f2e62
Dirk Morris <dmorris@untangle.com>
2016-10-04 16:53:24 -0700

    add new admin servlet skeleton

--- uvm/impl/com/untangle/uvm/TomcatManagerImpl.java
+++ uvm/impl/com/untangle/uvm/TomcatManagerImpl.java
@@ -113,6 +113,9 @@ public class TomcatManagerImpl implements TomcatManager
         ctx = loadServlet("/webui", "webui", true );
         ctx.setAttribute("threadRequest", threadRequest);
 
+        ctx = loadServlet("/admin", "admin", true );
+        ctx.setAttribute("threadRequest", threadRequest);
+
         ctx = loadServlet("/setup", "setup", true );
         ctx.setAttribute("threadRequest", threadRequest);
 
