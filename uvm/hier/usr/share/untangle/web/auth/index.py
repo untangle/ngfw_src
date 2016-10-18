0a18c46a2177b5eacedbd1917d583668935a440f
Dirk Morris <dmorris@untangle.com>
2016-10-18 13:05:36 -0700

    move untangle-vm auth logic into untangle-vm

---
+++ uvm/hier/usr/share/untangle/web/auth/index.py
@@ -0,0 +1,218 @@
+# $Id$
+import md5
+import cgi
+import base64
+import sys
+import re
+
+from mod_python import apache, Session, util
+from psycopg2 import connect
+
+sys.path.insert(0,'@PREFIX@/usr/lib/python%d.%d/' % sys.version_info[:2])
+import uvm_login
+
+def get_node_settings_item(a,b):
+    return None
+def get_uvm_settings_item(a,b):
+    return None
+
+try:
+    from uvm.settings_reader import get_node_settings_item
+    from uvm.settings_reader import get_uvm_settings_item
+except ImportError:
+    pass
+
+# pages -----------------------------------------------------------------------
+
+def login(req, url=None, realm='Administrator'):
+    uvm_login.setup_gettext()
+
+    options = req.get_options()
+
+    args = util.parse_qs(req.args or '')
+
+    if req.form.has_key('username') or req.form.has_key('password'):
+        is_error = True
+    else:
+        is_error = False
+
+    if req.form.has_key('username') and req.form.has_key('password'):
+        username = req.form['username']
+        password = req.form['password']
+        # debug
+        # assert False, ("User:Pass = %s %s" % (username,password))
+
+        if _valid_login(req, realm, username, password):
+            sess = Session.Session(req)
+            sess.set_timeout(uvm_login.SESSION_TIMEOUT)
+            uvm_login.save_session_user(sess, realm, username)
+            sess.save()
+            sess.unlock()
+
+            if url == None:
+                return apache.OK
+            else:
+                url = re.sub('[^A-Za-z0-9-_/.]','',url) # sanitize input
+                util.redirect(req, url, text="Login Successful")
+
+    company_name = uvm_login.get_company_name()
+    title = _("Administrator Login")
+    # some i18n company_names cause exception here, so wrap to handle this
+    # revert to "Administrator Login" if exception occurs
+    try:
+        title = cgi.escape(_("%s Administrator Login") % company_name)
+    except:
+        pass
+
+    host = cgi.escape(req.hostname)
+
+    _write_login_form(req, title, host, is_error)
+
+def logout(req, url=None, realm='Administrator'):
+    sess = Session.Session(req)
+    sess.set_timeout(uvm_login.SESSION_TIMEOUT)
+    uvm_login.delete_session_user(sess, realm)
+    sess.save()
+    sess.unlock()
+
+    if url == None:
+        return apache.OK
+    else:
+        url = re.sub('[^A-Za-z0-9-_/.]','',url) # sanitize input
+        util.redirect(req, url, text="Logout Successfull")
+
+# internal methods ------------------------------------------------------------
+
+def _valid_login(req, realm, username, password):
+    if realm == 'Administrator':
+        return _admin_valid_login(req, realm, username, password)
+    elif realm == 'Reports':
+        if _admin_valid_login(req, 'Administrator', username, password, False):
+            return True;
+        else:
+            return _reports_valid_login(req, realm, username, password)
+    else:
+        return False
+
+def _reports_valid_login(req, realm, username, password, log=True):
+    users = get_node_settings_item('untangle-node-reports','reportsUsers')
+    if users == None:
+        return False;
+    if users['list'] == None:
+        return False;
+    for user in users['list']:
+        if user['emailAddress'] != username:
+            continue;
+        pw_hash_base64 = user['passwordHashBase64']
+        pw_hash = base64.b64decode(pw_hash_base64)
+        raw_pw = pw_hash[0:len(pw_hash) - 8]
+        salt = pw_hash[len(pw_hash) - 8:]
+        if raw_pw == md5.new(password + salt).digest():
+            if log:
+                uvm_login.log_login(req, username, False, True, None)
+            return True
+        else:
+            if log:
+                uvm_login.log_login(req, username, False, False, 'P')
+            return False
+    if log:
+        uvm_login.log_login(req, username, False, False, 'U')
+    return False
+
+def _admin_valid_login(req, realm, username, password, log=True):
+    users = get_uvm_settings_item('admin','users')
+    if users == None:
+        return False;
+    if users['list'] == None:
+        return False;
+    for user in users['list']:
+        if user['username'] != username:
+            continue;
+        pw_hash_base64 = user['passwordHashBase64']
+        pw_hash = base64.b64decode(pw_hash_base64)
+        raw_pw = pw_hash[0:len(pw_hash) - 8]
+        salt = pw_hash[len(pw_hash) - 8:]
+        if raw_pw == md5.new(password + salt).digest():
+            if log:
+                uvm_login.log_login(req, username, False, True, None)
+            return True
+        else:
+            if log:
+                uvm_login.log_login(req, username, False, False, 'P')
+            return False
+    if log:
+        uvm_login.log_login(req, username, False, False, 'U')
+    return False
+
+def _write_login_form(req, title, host, is_error):
+    login_url = cgi.escape(req.unparsed_uri)
+    req.content_type = "text/html; charset=utf-8"
+    req.send_http_header()
+
+    if is_error:
+        error_msg = '%s' % cgi.escape(_('Error: Username and Password do not match'))
+    else:
+        error_msg = ''
+
+    server_str = cgi.escape(_("Server:"))
+    username_str = cgi.escape(_("Username:"))
+    password_str = cgi.escape(_("Password:"))
+    login_str = cgi.escape(_("Login"))
+
+    if not type(title) is str:
+        title = cgi.escape(title).encode("utf-8")
+    if not type(host) is str:
+        host = cgi.escape(host).encode("utf-8")
+
+    default_username = get_uvm_settings_item('admin','defaultUsername')
+    if default_username == None:
+        default_username = "admin"
+    focus_field_id = "password"
+    if default_username == "":
+        focus_field_id = "username"
+
+    banner_msg = get_node_settings_item('untangle-node-branding-manager','bannerMessage')
+    if banner_msg != None and banner_msg != "":
+        banner_msg = banner_msg.replace("\n", "<br/>")
+        banner_msg = "<p>" + banner_msg.encode('utf-8') + "</p>"
+    else:
+        banner_msg = ""
+
+    html = """\
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="initial-scale=1.0, width=device-width">
+<meta name="description" content="loginPage">
+<title>%s</title>
+<script type="text/javascript">if (top.location!=location) top.location.href=document.location.href;</script>
+<style type="text/css">
+/* <![CDATA[ */
+@import url(/images/base.css);
+/* ]]> */
+</style>
+</head>
+<body>
+
+<header>
+    <img src="/images/BrandingLogo.png">
+</header>
+
+<div class="form-login">
+    <form method="post" action="%s">
+        <h2>%s</h2>
+        <p class="server">%s</p>
+        <div class="banner">%s</div>
+        <p class="error">%s</p>
+        <input id="username" type="text" name="username" value="%s" placeholder="%s"/>
+        <input id="password" type="password" name="password" placeholder="%s"/>
+        <button type="submit">%s</button>
+    </form>
+</div>
+
+<script type="text/javascript">document.getElementById('%s').focus();</script>
+
+</body>
+</html>""" % (title, login_url, title, host, banner_msg, error_msg, default_username, username_str, password_str, login_str, focus_field_id)
+
+    req.write(html)
---
+++ uvm/hier/usr/share/untangle/web/error/index.py
@@ -0,0 +1,259 @@
+# $Id$
+
+import gettext
+import cgi
+import os
+
+from mod_python import apache
+
+sys.path.insert(0,'@PREFIX@/usr/lib/python%d.%d/' % sys.version_info[:2])
+import uvm_login
+
+gettext.bindtextdomain('untangle-apache2-config')
+gettext.textdomain('untangle-apache2-config')
+_ = gettext.gettext
+
+# pages -----------------------------------------------------------------------
+def isUvmStarting():
+    try:
+        ret = os.system("ps aux | grep -v grep | grep 'init.d/untangle-vm'")
+        if ret == 0:
+            return True
+    except:
+        pass
+    return False
+
+def status400(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Bad Request"))
+
+def status401(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Unauthorized"))
+
+def status402(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Payment Required"))
+
+def status403(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Forbidden"))
+
+def status404(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Not Found"))
+
+def status405(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Method Not Allowed"))
+
+def status406(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Not Acceptable"))
+
+def status407(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Proxy Authentication Required"))
+
+def status408(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Request Timeout"))
+
+def status409(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Conflict"))
+
+def status410(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Gone"))
+
+def status411(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Length Required"))
+
+def status412(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Precondition Failed"))
+
+def status413(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Request Entity Too Large"))
+
+def status414(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Request-URI Too Long"))
+
+def status415(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Unsupported Media Type"))
+
+def status416(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Requested Range Not Satisfiable"))
+
+def status417(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Expectation Failed"))
+
+def status500(req):
+    uvm_login.setup_gettext()
+    if isUvmStarting():
+        _write_loading_page(req)
+        return
+    _write_error_page(req, _("Internal Server Error"))
+
+def status501(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Not Implemented"))
+
+def status502(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Bad Gateway"))
+
+def status503(req):
+    uvm_login.setup_gettext()
+    if isUvmStarting():
+        _write_loading_page(req)
+        return
+    _write_error_page(req, _("Service Unavailable"))
+
+def status504(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("Gateway Timeout"))
+
+def status505(req):
+    uvm_login.setup_gettext()
+    _write_error_page(req, _("HTTP Version Not Supported"))
+
+# private methods --------------------------------------------------------------
+
+def _write_error_page(req, msg):
+    req.content_type = "text/html; charset=utf-8"
+    req.send_http_header()
+
+    us = _("Server")
+    try:
+        us = _("%s Server") % uvm_login.get_company_name()
+    except:
+        pass
+    
+    if not type(us) is str:
+        us = us.encode("utf-8")
+    if not type(msg) is str:
+        msg = msg.encode("utf-8")
+
+
+    html = """\
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+    <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+    <title>%s</title>
+    <script type="text/javascript">if (top.location!=location) top.location.href=document.location.href;</script>
+    <style type="text/css">
+    /* <![CDATA[ */
+    @import url(/images/base.css);
+    /* ]]> */
+    </style>
+    </head>
+    <body class="loginPage">
+    <div id="main" style="width: 500px; margin: 50px auto 0 auto;">
+        <form class="form-signin">
+            <center>
+                <img style="margin-bottom:10px;" src="/images/BrandingLogo.png"><br/>
+                <span class="form-signin-heading"><strong>%s</strong></span>
+             <br/>
+                <br/>
+                <span class="form-signin-heading"><font color="red"><em>%s</em></font></span>
+            </center>
+        </form>
+    </div>
+    </body>
+    </html>""" % (us,us, cgi.escape(msg))
+       
+    req.write(html)
+
+def _write_loading_page(req):
+    req.content_type = "text/html; charset=utf-8"
+    req.send_http_header()
+    msg = 'Server is starting. Please wait.'
+    
+    us = _("Server")
+    try:
+        us = _("%s Server") % uvm_login.get_company_name()
+    except:
+        pass
+    
+    if not type(us) is str:
+        us = us.encode("utf-8")
+    if not type(msg) is str:
+        msg = msg.encode("utf-8")
+
+
+    html = """\
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+    <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+    <title>%s</title>
+    <script type="text/javascript">if (top.location!=location) top.location.href=document.location.href;</script>
+    <script>
+    function poll(fn, success_callback, interval) {
+        function p() {
+            if(fn()) {
+                success_callback();
+            }
+            else {
+                setTimeout(p, interval);
+            }
+        };
+        setTimeout(p, interval);
+    }
+    var numPeriod = 0;
+    poll(
+        function() {
+            var xmlHttp = new XMLHttpRequest();
+            // load yourself and see if you get this page again
+            // 0fKAvHm1Nzi5adJpzpI3
+            xmlHttp.open( "GET", window.location.href, false ); // synchronous request
+            xmlHttp.send( null );
+            var text = xmlHttp.responseText;
+            if (text.indexOf("0fKAvHm1Nzi5adJpzpI3") > -1) {
+                var loading_message_element = document.getElementById('loading_message');
+                numPeriod = (numPeriod + 1) %% 4;
+                loading_message_element.innerHTML = "%s" + Array(numPeriod+1).join(".") + Array(4-numPeriod).join("&nbsp;");
+                return false; // got loading page still
+            } else {
+                return true; // did not get loading page
+            }
+        },
+        function() {
+            window.location.reload(true);
+        },
+        1000
+    );
+    </script>
+    <style type="text/css">
+    /* <![CDATA[ */
+    @import url(/images/base.css);
+    /* ]]> */
+    </style>
+    </head>
+    <body class="loginPage">
+    <div id="main" style="width: 500px; margin: 50px auto 0 auto;">
+        <form class="form-signin">
+            <center>
+                <img style="margin-bottom:10px;" src="/images/BrandingLogo.png"><br/>
+             <br/>
+                <br/>
+                <span class="form-signin-heading">
+                    <div id="loading_message" style="color:white; font-size:20px; font-weight:bold; text-align:center">%s&nbsp;&nbsp;&nbsp;</div>
+                </span>
+            </center>
+        </form>
+    </div>
+    </body>
+    </html>""" % (us, cgi.escape(msg), cgi.escape(msg));
+       
+    req.write(html)
+    
