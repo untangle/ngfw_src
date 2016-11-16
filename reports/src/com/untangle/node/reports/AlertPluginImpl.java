d33c89c3b2b93b3ff1256ba57e46c2836e49d5dc
Dirk Morris <dmorris@untangle.com>
2016-11-15 19:03:25 -0800

    add checking of cloud setting and license and sending of event to cloud service in alert plugin

--- reports/src/com/untangle/node/reports/AlertPluginImpl.java
+++ reports/src/com/untangle/node/reports/AlertPluginImpl.java
@@ -6,26 +6,38 @@ import java.util.HashMap;
 import java.util.Random;
 
 import org.apache.log4j.Logger;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.entity.StringEntity;
+import org.json.JSONObject;
 
 import com.untangle.uvm.Plugin;
-import com.untangle.uvm.HostTableEntry;
-import com.untangle.uvm.node.Node;
-import com.untangle.uvm.node.NodeSettings;
-import com.untangle.uvm.vnet.Affinity;
-import com.untangle.uvm.vnet.Fitting;
-import com.untangle.uvm.vnet.PipelineConnector;
-import com.untangle.uvm.vnet.NodeTCPSession;
-import com.untangle.uvm.vnet.Token;
-import com.untangle.uvm.vnet.ChunkToken;
+import com.untangle.uvm.node.License;
 import com.untangle.node.reports.AlertEvent;
+import com.untangle.uvm.logging.LogEvent;
 
 public class AlertPluginImpl implements Plugin
 {
     private static final Logger logger = Logger.getLogger( AlertPluginImpl.class );
 
-    protected PipelineConnector connector = null;
-
     private final AlertEventHookCallback alertCallback = new AlertEventHookCallback();
+    private final SettingsChangeHookCallback settingsCallback = new SettingsChangeHookCallback();
+    private final LicenseChangeHookCallback licenseCallback = new LicenseChangeHookCallback();
+
+    private String uid = null;
     
     private AlertPluginImpl() {}
     
@@ -36,12 +48,111 @@ public class AlertPluginImpl implements Plugin
     
     public final void run()
     {
-        UvmContextFactory.context().hookManager().registerCallback( com.untangle.uvm.HookManager.REPORTS_EVENT_LOGGED, this.alertCallback );
+        this.uid = UvmContextFactory.context().getServerUID();
+
+        registerCallback();
+
+        UvmContextFactory.context().hookManager().registerCallback( com.untangle.uvm.HookManager.UVM_SETTINGS_CHANGE, this.settingsCallback );
+        UvmContextFactory.context().hookManager().registerCallback( com.untangle.uvm.HookManager.LICENSE_CHANGE, this.licenseCallback );
     }
 
     public final void stop()
     {
+        unregisterCallback();
+
+        UvmContextFactory.context().hookManager().unregisterCallback( com.untangle.uvm.HookManager.UVM_SETTINGS_CHANGE, this.settingsCallback );
+        UvmContextFactory.context().hookManager().unregisterCallback( com.untangle.uvm.HookManager.LICENSE_CHANGE, this.licenseCallback );
+    }
+
+    private void registerCallback()
+    {
+        UvmContextFactory.context().hookManager().registerCallback( com.untangle.uvm.HookManager.REPORTS_EVENT_LOGGED, this.alertCallback );
+    }
+
+    private void unregisterCallback()
+    {
         UvmContextFactory.context().hookManager().unregisterCallback( com.untangle.uvm.HookManager.REPORTS_EVENT_LOGGED, this.alertCallback );
+    }
+
+    private boolean isEnabled()
+    {
+        if ( !isLicenseValid() )
+            return false;
+        if ( !isCloudEnabled() )
+            return false;
+        return true;
+    }
+
+    private boolean isLicenseValid()
+    {
+        if (UvmContextFactory.context().licenseManager().isLicenseValid(License.CLOUD_COMMAND))
+            return true;
+        return false;
+    }
+
+    private boolean isCloudEnabled()
+    {
+        try {
+            return UvmContextFactory.context().systemManager().getSettings().getCloudEnabled();
+        } catch (Exception e) {
+            logger.warn( "Unable to check cloud settings:", e );
+            return false;
+        }
+    }
+
+    private void resumeProperState()
+    {
+        boolean callbackRegistered = UvmContextFactory.context().hookManager().isRegistered( com.untangle.uvm.HookManager.REPORTS_EVENT_LOGGED, this.alertCallback );
+
+        if ( isEnabled() ) {
+            if ( callbackRegistered ) {
+                // should be enabled and is registered, nothing to do
+                return;
+            } else {
+                // should be enabled but is not registered, register now
+                registerCallback();
+                return;
+            }
+        } else {
+            if ( callbackRegistered ) {
+                // should not be registered but is registered, unregister now
+                unregisterCallback();
+                return;
+            } else {
+                // should not be enabled and is not registered, nothing to do
+                return;
+            }
+        }
+    }
+
+    private void sendEventToCloud( LogEvent event )
+    {
+        try {
+            CloseableHttpClient httpClient = HttpClients.custom().build();
+            HttpClientContext context = HttpClientContext.create();
+
+            URIBuilder builder = new URIBuilder( "https://events.untangle.com/api/v1/123/FIXME/" );
+            builder.addParameter( "uid" , this.uid );
+            String url = builder.build().toString();
+
+            HttpPost  post = new HttpPost(url);
+            post.setHeader("Content-Type", "application/x-www-form-urlencoded");
+
+            String jsonString = event.toJSONString();
+            StringEntity body = new StringEntity( jsonString );
+            post.setEntity( body );
+
+            logger.info( "sending event: " + event.toJSONString() );
+            CloseableHttpResponse response = httpClient.execute( post, context );
+
+            String responseBody = EntityUtils.toString( response.getEntity(), "UTF-8" );
+            responseBody = responseBody.trim();
+            logger.debug( "response: " + responseBody );
+
+            return;
+        } catch (Exception e) {
+            logger.warn( "send event exception: ", e );
+        }
     }
 
     private class AlertEventHookCallback implements HookCallback
@@ -56,8 +167,38 @@ public class AlertPluginImpl implements Plugin
             if ( ! (o instanceof AlertEvent) ) {
                 return;
             }
+            AlertEvent ae = (AlertEvent) o;
 
+            sendEventToCloud( ae );
             logger.warn(o);
+        }
+    }
+
+    private class LicenseChangeHookCallback implements HookCallback
+    {
+        public String getName()
+        {
+            return "alert-plugin-license-hook";
+        }
+
+        public void callback( Object o )
+        {
+            // check license
+            resumeProperState();
+        }
+    }
+
+    private class SettingsChangeHookCallback implements HookCallback
+    {
+        public String getName()
+        {
+            return "alert-plugin-settings-hook";
+        }
+
+        public void callback( Object o )
+        {
+            // check cloud setting
+            resumeProperState();
         }
     }
     
