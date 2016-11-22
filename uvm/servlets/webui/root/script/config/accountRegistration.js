b17e7ad6260335ff3491d7259ad3f4c313c0a54d
Dan Hadarau <dhadarau@untangle.com>
2016-11-22 11:55:57 +0200

    NGFW-10176, NGFW-10184 - set isRegistered flag and install recommended apps on exit

--- uvm/servlets/webui/root/script/config/accountRegistration.js
+++ uvm/servlets/webui/root/script/config/accountRegistration.js
@@ -67,7 +67,7 @@ Ext.define('Webui.config.accountRegistration', {
                 xtype: 'button',
                 baseCls: 'reg-close',
                 html: '<span style="vertical-align: middle;">' + i18n._('Skip') + '</span> <i class="material-icons">close</i>',
-                handler: 'closeWindow'
+                handler: 'skipReg'
             }]
         }, {
             layout: 'card',
@@ -517,7 +517,7 @@ Ext.define('Webui.config.accountRegistration', {
                     text: i18n._('Continue'),
                     baseCls: 'reg-btn',
                     margin: '20 0 0 0',
-                    handler: 'closeWindowReg'
+                    handler: 'finishReg'
                 }]
             }]
         }];
@@ -567,13 +567,18 @@ Ext.define('Webui.config.accountRegistrationController', {
         }
     },
 
-    closeWindow: function () {
+    skipReg: function () {
         this.getView().close();
     },
 
-    closeWindowReg: function () {
-        rpc.jsonrpc.UvmContext.setRegistered();
+    finishReg: function() {
+        rpc.jsonrpc.UvmContext.setRegistered(function(result, exception) {
+            if(Ung.Util.handleException(exception)) return;
+            rpc.isRegistered = true;
+            Ung.Main.showPostRegistrationPopup();
+        });
         this.getView().close();
+        Ext.destroy(this.getView());
     },
 
     showLogin: function () {
@@ -608,7 +613,6 @@ Ext.define('Webui.config.accountRegistrationController', {
                         me.getView().email = form.findField('email').getValue();
                         me.getView().token = response.token;
                         me.getView().down('#cards').setActiveItem(3);
-                        // me.closeWindow();
                     }
                 },
                 form.findField('email').getValue(),
