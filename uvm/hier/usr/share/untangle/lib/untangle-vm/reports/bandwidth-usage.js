8dc7e383b1fe998849f8f360aba1791faf27392f
Dirk Morris <dmorris@untangle.com>
2016-11-26 22:05:02 -0800

    sqlite experiment - work in progress

--- uvm/hier/usr/share/untangle/lib/untangle-vm/reports/bandwidth-usage.js
+++ uvm/hier/usr/share/untangle/lib/untangle-vm/reports/bandwidth-usage.js
@@ -1,14 +1,14 @@
 {
     "uniqueId": "network-StzlzfZAp8",
     "category": "Network",
-    "description": "The approximate averaged data transfer rate (total, sent, received) over time grouped by session creation time.",
+    "description": "The approximate averaged data transfer rate (total, sent, received) over time.",
     "displayOrder": 103,
     "enabled": true,
     "javaClass": "com.untangle.node.reports.ReportEntry",
     "orderDesc": false,
     "units": "KB/s",
     "readOnly": true,
-    "table": "sessions",
+    "table": "session_minutes",
     "timeDataColumns": [
         "round(coalesce(sum(s2p_bytes + p2s_bytes), 0) / (1024*60*10),1) as total",
         "round(coalesce(sum(p2s_bytes), 0) / (1024*60*10),1) as sent",
@@ -19,7 +19,7 @@
         "#0099ff",
         "#6600ff"
     ],
-    "timeDataInterval": "TENMINUTE",
+    "timeDataInterval": "MINUTE",
     "timeStyle": "AREA",
     "title": "Bandwidth Usage",
     "type": "TIME_GRAPH"
