5c30d34db6220c7c4b94d8a917baf10b8367f68e
mahotz <mahotz@untangle.com>
2016-11-28 14:45:08 -0700

    Disabled the blocked flag on default categories when creating new settings since we only monitor, no blocking. Fixed the names returned by the app to keep reporting happy. Added test case to check that a porn site is flagged as expected.

--- web-monitor/hier/usr/lib/python2.7/tests/web_monitor_tests.py
+++ web-monitor/hier/usr/lib/python2.7/tests/web_monitor_tests.py
@@ -1,146 +1,61 @@
 import unittest2
 import time
 import sys
-import re
 import datetime
-
+import string
 from jsonrpc import ServiceProxy
 from jsonrpc import JSONRPCException
 from global_functions import uvmContext
 from uvm import Manager
 from uvm import Uvm
-from tests.web_filter_base_tests import WebFilterBaseTests
 import remote_control
 import test_registry
 import global_functions
+import pprint
 
 defaultRackId = 1
+node = None
 
-def addBlockedUrl(node, url, blocked=True, flagged=True, description="description"):
-    newRule = { "blocked": blocked, "description": description, "flagged": flagged, "javaClass": "com.untangle.uvm.node.GenericRule", "string": url }
-    rules = node.getBlockedUrls()
-    rules["list"].append(newRule)
-    node.setBlockedUrls(rules)
-
-def nukeBlockedUrls(node):
-    rules = node.getBlockedUrls()
-    rules["list"] = []
-    node.setBlockedUrls(rules)
-
-#
-# Just extends the web filter tests
-#
-class WebFilterTests(WebFilterBaseTests):
+class WebMonitorTests(unittest2.TestCase):
 
     @staticmethod
     def nodeName():
-        return "untangle-node-web-filter"
+        return "untangle-node-web-monitor"
 
     @staticmethod
-    def shortNodeName():
-        return "web-filter"
+    def initialSetUp(self):
+        global node
+        if (uvmContext.nodeManager().isInstantiated(self.nodeName())):
+            raise Exception('node %s already instantiated' % self.nodeName())
+        node = uvmContext.nodeManager().instantiate(self.nodeName(), defaultRackId)
+        nodemetrics = uvmContext.metricManager().getMetrics(node.getNodeSettings()["id"])
+        self.node = node
 
-    @staticmethod
-    def eventNodeName():
-        return "web_filter"
-
-    @staticmethod
-    def displayName():
-        return "Web Filter"
+    def setUp(self):
+        pass
 
     # verify client is online
-    def test_510_clientIsOnlineHttps(self):
-        global remote_control
-        result = remote_control.runCommand("wget -q -O /dev/null -4 -t 2 --timeout=5 --no-check-certificate -o /dev/null https://test.untangle.com/")
+    def test_010_clientIsOnline(self):
+        result = remote_control.isOnline()
         assert (result == 0)
 
-    # verify that a block page is shown but unblock if correct password.
-    def test_520_unblockOptionWithPassword(self):
-        fname = sys._getframe().f_code.co_name
-        addBlockedUrl(self.node, "test.untangle.com/test/testPage2.html")
-        settings = self.node.getSettings()
-        settings["unblockMode"] = "Host"
-        settings["unblockPassword"] = "atstest"
-        settings["unblockPasswordEnabled"] = True
-        self.node.setSettings(settings)        
+    def test_020_eventlog_flaggedPorn(self):
+        result = remote_control.runCommand("wget -q -O - http://www.playboy.com/bunnies 2>&1 >/dev/null")
+        time.sleep(1);
+        events = global_functions.get_events("Web Monitor", "Flagged Web Events", None, 1)
+        assert(events != None)
+        found = global_functions.check_events( events.get('list'), 5,
+                                            "host","www.playboy.com",
+                                            "uri", "/bunnies",
+                                            'web_filter_blocked', False,
+                                            'web_filter_flagged', True )
+        assert( found )
 
-        # this test URL should be blocked but allow  
-        remote_control.runCommand("rm -f /tmp/%s.log"%fname)
-        result = remote_control.runCommand("wget -4 -t 2 --timeout=5 -a /tmp/%s.log -O /tmp/%s.out http://test.untangle.com/test/testPage2.html"%(fname,fname))
-        resultButton = remote_control.runCommand("grep -q 'unblock' /tmp/%s.out"%fname)
-        resultBlock = remote_control.runCommand("grep -q 'blockpage' /tmp/%s.out"%fname)
+    @staticmethod
+    def finalTearDown(self):
+        global node
+        if node != None:
+            uvmContext.nodeManager().destroy( node.getNodeSettings()["id"] )
+            node = None
 
-        # get the IP address of the block page 
-        ipfind = remote_control.runCommand("grep 'Location' /tmp/%s.log"%fname,stdout=True)
-        print 'ipFind %s' % ipfind
-        ip = re.findall( r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}(?:[0-9:]{0,6})', ipfind )
-        blockPageIP = ip[0]
-        # print 'Block page IP address is %s' % blockPageIP
-        blockParamaters = re.findall( r'\?(.*)\s', ipfind )
-        paramaters = blockParamaters[0]
-        # Use unblock button.
-        unBlockParameters = "global=false&"+ paramaters + "&password=atstest"
-        # print "unBlockParameters %s" % unBlockParameters
-        remote_control.runCommand("wget -q -O /dev/null --post-data=\'" + unBlockParameters + "\' http://" + blockPageIP + "/" + self.shortNodeName() + "/unblock")
-        resultUnBlock = remote_control.runCommand("wget -O - http://test.untangle.com/test/testPage2.html 2>&1 | grep -q text123")
-
-        settings = self.node.getSettings()
-        settings["unblockMode"] = "None"
-        settings["unblockPassword"] = ""
-        settings["unblockPasswordEnabled"] = False
-
-        self.node.setSettings(settings)        
-        nukeBlockedUrls(self.node)
-        print "block %s button %s unblock %s" % (resultBlock,resultButton,resultUnBlock)
-        assert (resultBlock == 0 and resultButton == 0 and resultUnBlock == 0 )
-
-    # check for block page with HTTPS request
-    def test_530_httpsPornIsBlocked(self):
-        result = remote_control.runCommand("wget -q -4 -t 2 --timeout=8 --no-check-certificate -O - https://penthouse.com/ 2>&1 | grep -q blockpage")
-        assert (result == 0)        
-
-    # Check google safe search
-    def test_540_safeSearchEnabled(self):
-        settings = self.node.getSettings()
-        settings["enforceSafeSearch"] = False
-        self.node.setSettings(settings)        
-        resultWithoutSafe = remote_control.runCommand("wget -q -O - '$@' -U 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7) Gecko/20040613 Firefox/0.8.0+)' 'http://www.google.com/search?hl=en&q=boobs&safe=off' | grep -q 'safe=off'");
-
-        settings["enforceSafeSearch"] = True
-        self.node.setSettings(settings)        
-        resultWithSafe = remote_control.runCommand("wget -q -O - '$@' -U 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7) Gecko/20040613 Firefox/0.8.0+)' 'http://www.google.com/search?hl=en&q=boobs&safe=off' | grep -q 'safe=active'");
-
-        assert( resultWithoutSafe == 0 )               
-        assert( resultWithSafe == 0 )               
-
-    # Query eventlog
-    def test_600_queryEventLog(self):
-        termTests = [{
-            "host": "www.bing.com",
-            "uri":  "/search?q=oneterm&qs=n&form=QBRE",
-            "term": "oneterm"
-        },{
-            "host": "www.bing.com",
-            "uri":  "/search?q=two+terms&qs=n&form=QBRE",
-            "term": "two terms"
-        },{
-            "host": "www.bing.com",        
-            "uri":  "/search?q=%22quoted+terms%22&qs=n&form=QBRE",
-            "term": '"quoted terms"'
-        }]
-        host = "www.bing.com"
-        uri = "/search?q=oneterm&qs=n&form=QBRE"
-        for t in termTests:
-            fname = sys._getframe().f_code.co_name
-            eventTime = datetime.datetime.now()
-            result1 = remote_control.runCommand("wget -q -O - 'http://%s%s' 2>&1 >/dev/null" % ( t["host"], t["uri"] ) ) 
-            time.sleep(1);
-
-            events = global_functions.get_events(self.displayName(),'All Query Events',None,1)
-            assert(events != None)
-            found = global_functions.check_events( events.get('list'), 5, 
-                                                "host", t["host"], 
-                                                "term", t["term"])
-            assert( found )
-
-test_registry.registerNode("web-filter", WebFilterTests)
+test_registry.registerNode("web-monitor", WebMonitorTests)
