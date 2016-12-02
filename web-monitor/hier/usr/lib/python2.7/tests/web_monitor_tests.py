bb4515943ef031bbf462dbf6b8560a4783cb589f
John Coffin <jcoffin@gmail.com>
2016-12-02 10:26:22 -0800

    Add pass list test

--- web-monitor/hier/usr/lib/python2.7/tests/web_monitor_tests.py
+++ web-monitor/hier/usr/lib/python2.7/tests/web_monitor_tests.py
@@ -16,6 +16,17 @@ import pprint
 defaultRackId = 1
 node = None
 
+def addPassedUrl(url, enabled=True, description="description"):
+    newRule =  { "enabled": enabled, "description": description, "javaClass": "com.untangle.uvm.node.GenericRule", "string": url }
+    rules = node.getPassedUrls()
+    rules["list"].append(newRule)
+    node.setPassedUrls(rules)
+
+def nukePassedUrls():
+    rules = node.getPassedUrls()
+    rules["list"] = []
+    node.setPassedUrls(rules)
+
 class WebMonitorTests(unittest2.TestCase):
 
     @staticmethod
@@ -50,6 +61,21 @@ class WebMonitorTests(unittest2.TestCase):
                                             'web_filter_blocked', False,
                                             'web_filter_flagged', True )
         assert( found )
+
+    # verify that an entry in the pass list overrides a flagged category
+    def test_030_passedUrlOverridesFlaggedCategory(self):
+        addPassedUrl("playboy.com")
+        result = remote_control.runCommand("wget -q -O - http://www.playboy.com/ 2>&1 >/dev/null")
+        time.sleep(1);
+        events = global_functions.get_events("Web Monitor", "All Web Events", None, 1)
+        assert(events != None)
+        found = global_functions.check_events( events.get('list'), 5,
+                                            "host","www.playboy.com",
+                                            "uri", "/",
+                                            'web_filter_blocked', False,
+                                            'web_filter_flagged', False )
+        assert( found )
+        nukePassedUrls()
 
     @staticmethod
     def finalTearDown(self):
