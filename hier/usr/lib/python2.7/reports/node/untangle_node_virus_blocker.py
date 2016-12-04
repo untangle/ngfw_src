28fdec5e33f72646e43ddf3d379c87c8b1bc40b9
Dirk Morris <dmorris@untangle.com>
2016-12-03 16:21:11 -0800

    change the database creation and cleanup logic to just be simple scripts instead of large object-oriented programs. remove lots of old reporting code and logic.

--- hier/usr/lib/python2.7/reports/node/untangle_node_virus_blocker.py
+++
@@ -1,3 +0,0 @@
-import reports.node.untangle_base_virus_blocker
-
-reports.engine.register_node(reports.node.untangle_base_virus_blocker.VirusBaseNode('untangle-node-virus-blocker', 'Virus Blocker', 'virus_blocker'))
