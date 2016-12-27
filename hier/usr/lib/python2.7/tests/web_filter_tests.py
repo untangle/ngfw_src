2f8d74bb9095d77f285753e2a9c326126126d836
mahotz <mahotz@untangle.com>
2016-12-27 08:24:54 -0700

    Added test for checking blocklist from the SNI handler.

--- hier/usr/lib/python2.7/tests/web_filter_tests.py
+++ hier/usr/lib/python2.7/tests/web_filter_tests.py
@@ -62,15 +62,15 @@ class WebFilterTests(WebFilterBaseTests):
         settings["unblockMode"] = "Host"
         settings["unblockPassword"] = "atstest"
         settings["unblockPasswordEnabled"] = True
-        self.node.setSettings(settings)        
+        self.node.setSettings(settings)
 
-        # this test URL should be blocked but allow  
+        # this test URL should be blocked but allow
         remote_control.runCommand("rm -f /tmp/%s.log"%fname)
         result = remote_control.runCommand("wget -4 -t 2 --timeout=5 -a /tmp/%s.log -O /tmp/%s.out http://test.untangle.com/test/testPage2.html"%(fname,fname))
         resultButton = remote_control.runCommand("grep -q 'unblock' /tmp/%s.out"%fname)
         resultBlock = remote_control.runCommand("grep -q 'blockpage' /tmp/%s.out"%fname)
 
-        # get the IP address of the block page 
+        # get the IP address of the block page
         ipfind = remote_control.runCommand("grep 'Location' /tmp/%s.log"%fname,stdout=True)
         print 'ipFind %s' % ipfind
         ip = re.findall( r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}(?:[0-9:]{0,6})', ipfind )
@@ -89,7 +89,7 @@ class WebFilterTests(WebFilterBaseTests):
         settings["unblockPassword"] = ""
         settings["unblockPasswordEnabled"] = False
 
-        self.node.setSettings(settings)        
+        self.node.setSettings(settings)
         nukeBlockedUrls(self.node)
         print "block %s button %s unblock %s" % (resultBlock,resultButton,resultUnBlock)
         assert (resultBlock == 0 and resultButton == 0 and resultUnBlock == 0 )
@@ -97,21 +97,28 @@ class WebFilterTests(WebFilterBaseTests):
     # check for block page with HTTPS request
     def test_530_httpsPornIsBlocked(self):
         result = remote_control.runCommand("wget -q -4 -t 2 --timeout=8 --no-check-certificate -O - https://penthouse.com/ 2>&1 | grep -q blockpage")
-        assert (result == 0)        
+        assert (result == 0)
 
     # Check google safe search
     def test_540_safeSearchEnabled(self):
         settings = self.node.getSettings()
         settings["enforceSafeSearch"] = False
-        self.node.setSettings(settings)        
+        self.node.setSettings(settings)
         resultWithoutSafe = remote_control.runCommand("wget -q -O - '$@' -U 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7) Gecko/20040613 Firefox/0.8.0+)' 'http://www.google.com/search?hl=en&q=boobs&safe=off' | grep -q 'safe=off'");
 
         settings["enforceSafeSearch"] = True
-        self.node.setSettings(settings)        
+        self.node.setSettings(settings)
         resultWithSafe = remote_control.runCommand("wget -q -O - '$@' -U 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7) Gecko/20040613 Firefox/0.8.0+)' 'http://www.google.com/search?hl=en&q=boobs&safe=off' | grep -q 'safe=active'");
 
-        assert( resultWithoutSafe == 0 )               
-        assert( resultWithSafe == 0 )               
+        assert( resultWithoutSafe == 0 )
+        assert( resultWithSafe == 0 )
+
+    # Check SNI block list handling
+    def test_550_httpsBlockListWithSNI(self):
+        addBlockedUrl(self.node, "playboy.com")
+        result = remote_control.runCommand("wget -q -4 -t 2 --timeout=8 --no-check-certificate -O - https://www.playboy.com/ 2>&1 | grep -q blockpage")
+        nukeBlockedUrls(self.node)
+        assert (result == 0)
 
     # Query eventlog
     def test_600_queryEventLog(self):
@@ -124,7 +131,7 @@ class WebFilterTests(WebFilterBaseTests):
             "uri":  "/search?q=two+terms&qs=n&form=QBRE",
             "term": "two terms"
         },{
-            "host": "www.bing.com",        
+            "host": "www.bing.com",
             "uri":  "/search?q=%22quoted+terms%22&qs=n&form=QBRE",
             "term": '"quoted terms"'
         }]
@@ -133,13 +140,13 @@ class WebFilterTests(WebFilterBaseTests):
         for t in termTests:
             fname = sys._getframe().f_code.co_name
             eventTime = datetime.datetime.now()
-            result1 = remote_control.runCommand("wget -q -O - 'http://%s%s' 2>&1 >/dev/null" % ( t["host"], t["uri"] ) ) 
+            result1 = remote_control.runCommand("wget -q -O - 'http://%s%s' 2>&1 >/dev/null" % ( t["host"], t["uri"] ) )
             time.sleep(1);
 
             events = global_functions.get_events(self.displayName(),'All Query Events',None,1)
             assert(events != None)
-            found = global_functions.check_events( events.get('list'), 5, 
-                                                "host", t["host"], 
+            found = global_functions.check_events( events.get('list'), 5,
+                                                "host", t["host"],
                                                 "term", t["term"])
             assert( found )
 
