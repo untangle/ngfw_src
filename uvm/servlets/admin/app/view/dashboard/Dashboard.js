24929edac34071323020846aa6b90fc5fd423dc0
Dan Hadarau <dhadarau@untangle.com>
2017-01-09 16:11:37 +0200

    ngfw-admin - wip

--- uvm/servlets/admin/app/view/dashboard/Dashboard.js
+++ uvm/servlets/admin/app/view/dashboard/Dashboard.js
@@ -33,218 +33,141 @@ Ext.define('Ung.view.dashboard.Dashboard', {
     defaults: {
         border: false
     },
+
     items: [{
-        region: 'north',
-        weight: 20,
-        border: false,
-        height: 44,
-        itemId: 'apps-topnav',
+        region: 'west',
+        title: 'Manage Widgets'.t(),
+        // weight: 30,
+        width: 300,
+        collapsible: true,
+        //border: false,
+        // shadow: false,
+        animCollapse: false,
+        // collapsed: false,
+        // collapseMode: 'mini',
+        titleCollapse: true,
+        floatable: false,
+        // cls: 'widget-manager',
+        split: false,
+        xtype: 'grid',
+        reference: 'dashboardNav',
+        // forceFit: true,
+        hideHeaders: true,
+        // disableSelection: true,
+        // trackMouseOver: false,
+
+        store: 'widgets',
+
         bodyStyle: {
-            background: '#555',
-            padding: '0 5px'
+            border: 0
         },
-        layout: {
-            type: 'hbox',
-            align: 'middle'
-        },
-        defaults: {
-            xtype: 'button',
-            baseCls: 'heading-btn'
-        },
-        items: [{
-            html: Ung.Util.iconTitle('Manage Widgets'.t(), 'settings-16'),
-            handler: 'managerHandler',
-            bind: {
-                hidden: '{managerOpen}'
+        viewConfig: {
+            plugins: {
+                ptype: 'gridviewdragdrop',
+                dragText: 'Drag and drop to reorganize'.t(),
+                dragZone: {
+                    onBeforeDrag: function (data, e) {
+                        return Ext.get(e.target).hasCls('drag-handle');
+                    }
+                }
+            },
+            stripeRows: false,
+            getRowClass: function (record) {
+                return !record.get('enabled') ? 'disabled' : '';
+            },
+            listeners: {
+                drop: 'onDrop'
             }
+        },
+        columns: [{
+            width: 14,
+            align: 'center',
+            sortable: false,
+            hideable: false,
+            resizable: false,
+            menuDisabled: true,
+            tdCls: 'drag-handle'
         }, {
-            xtype: 'component',
+            width: 30,
+            align: 'center',
+            sortable: false,
+            hideable: false,
+            resizable: false,
+            menuDisabled: true,
+            //handler: 'toggleWidgetEnabled',
+            dataIndex: 'enabled',
+            renderer: 'enableRenderer'
+        }, {
+            dataIndex: 'entryId',
+            renderer: 'widgetTitleRenderer',
             flex: 1
         }, {
-            html: 'Sessions'.t(),
-            href: '#sessions',
-            hrefTarget: '_self'
+            xtype: 'actioncolumn',
+            width: 30,
+            align: 'center',
+            sortable: false,
+            hideable: false,
+            resizable: false,
+            menuDisabled: true,
+            handler: 'removeWidget',
+            renderer: function (value, meta, record) {
+                return '<i class="material-icons" style="color: #999; font-size: 20px;">close</i>';
+            }
+        }/*, {
+            xtype: 'actioncolumn',
+            align: 'center',
+            width: 25,
+            sortable: false,
+            hideable: false,
+            resizable: false,
+            menuDisabled: true,
+            renderer: function (value, meta, record) {
+                if (record.get('type') !== 'ReportEntry') {
+                    return '';
+                }
+                return '<i style="font-size: 16px; color: #777; padding-top: 4px;" class="material-icons">settings</i>';
+            }
+        }*/],
+        listeners: {
+            itemmouseleave : 'onItemLeave',
+            cellclick: 'onItemClick'
+        },
+        tbar: [{
+            itemId: 'addWidgetBtn',
+            text: Ung.Util.iconTitle('Add'.t(), 'add_circle-16'),
+            // menu: Ext.create('Ext.menu.Menu', {
+            //     mouseLeaveDelay: 0
+            // })
+        }, '->', {
+            text: Ung.Util.iconTitle('Import'.t(), 'file_download-16'),
+            // handler: 'applyChanges'
         }, {
-            html: 'Hosts'.t(),
-            href: '#hosts',
-            hrefTarget: '_self'
-        }, {
-            html: 'Devices'.t(),
-            href: '#devices',
-            hrefTarget: '_self'
+            text: Ung.Util.iconTitle('Export'.t(), 'file_upload-16')
+            //handler: 'applyChanges'
+        }],
+        bbar: [{
+            text: Ung.Util.iconTitle('Reset'.t(), 'replay-16'),
+            handler: 'resetDashboard'
+        }, '->', {
+            text: Ung.Util.iconTitle('Apply'.t(), 'save-16'),
+            handler: 'applyChanges'
         }]
-    }, {
-        region: 'west',
-        weight: 30,
-        collapsible: true,
-        layout: {
-            type: 'vbox',
-            align: 'stretch'
-        },
-        //border: false,
-        header: false,
-        shadow: false,
-        animCollapse: false,
-        collapsed: true,
-        collapseMode: 'mini',
-        bind: {
-            collapsed: '{!managerOpen}'
-        },
-        //titleCollapse: true,
-        floatable: false,
-        cls: 'widget-manager',
-        split: {
-            size: 0
-        },
-        items: [{
-            xtype: 'container',
-            cls: 'heading',
-            height: 44,
-            border: false,
-            layout: {
-                type: 'hbox',
-                align: 'stretch',
-                pack: 'center'
-            },
-            items: [{
-                xtype: 'component',
-                flex: 1,
-                padding: 10,
-                html: 'Manage Widgets'.t()
-            }, {
-                xtype: 'button',
-                width: 44,
-                baseCls: 'manager-close-btn',
-                html: '<i class="material-icons">close</i>',
-                handler: 'managerHandler'
-            }]
-        }, {
-            xtype: 'grid',
-            reference: 'dashboardNav',
-            forceFit: true,
-            flex: 1,
-            width: 300,
-            border: false,
-            header: false,
-            hideHeaders: true,
-            // disableSelection: true,
-            // trackMouseOver: false,
-
-            store: 'widgets',
-
-            bodyStyle: {
-                border: 0
-            },
-            viewConfig: {
-                plugins: {
-                    ptype: 'gridviewdragdrop',
-                    dragText: 'Drag and drop to reorganize'.t(),
-                    dragZone: {
-                        onBeforeDrag: function (data, e) {
-                            return Ext.get(e.target).hasCls('drag-handle');
-                        }
-                    }
-                },
-                stripeRows: false,
-                getRowClass: function (record) {
-                    return !record.get('enabled') ? 'disabled' : '';
-                },
-                listeners: {
-                    drop: 'onDrop'
-                }
-            },
-            columns: [{
-                width: 14,
-                align: 'center',
-                sortable: false,
-                hideable: false,
-                resizable: false,
-                menuDisabled: true,
-                tdCls: 'drag-handle'
-            }, {
-                width: 30,
-                align: 'center',
-                sortable: false,
-                hideable: false,
-                resizable: false,
-                menuDisabled: true,
-                //handler: 'toggleWidgetEnabled',
-                dataIndex: 'enabled',
-                renderer: 'enableRenderer'
-            }, {
-                dataIndex: 'entryId',
-                renderer: 'widgetTitleRenderer'
-            }, {
-                xtype: 'actioncolumn',
-                width: 30,
-                align: 'center',
-                sortable: false,
-                hideable: false,
-                resizable: false,
-                menuDisabled: true,
-                handler: 'removeWidget',
-                renderer: function (value, meta, record) {
-                    return '<i class="material-icons" style="color: #999; font-size: 20px;">close</i>';
-                }
-            }/*, {
-                xtype: 'actioncolumn',
-                align: 'center',
-                width: 25,
-                sortable: false,
-                hideable: false,
-                resizable: false,
-                menuDisabled: true,
-                renderer: function (value, meta, record) {
-                    if (record.get('type') !== 'ReportEntry') {
-                        return '';
-                    }
-                    return '<i style="font-size: 16px; color: #777; padding-top: 4px;" class="material-icons">settings</i>';
-                }
-            }*/],
-            listeners: {
-                itemmouseleave : 'onItemLeave',
-                cellclick: 'onItemClick'
-            },
-            tbar: [{
-                itemId: 'addWidgetBtn',
-                text: Ung.Util.iconTitle('Add'.t(), 'add_circle-16'),
-                // menu: Ext.create('Ext.menu.Menu', {
-                //     mouseLeaveDelay: 0
-                // })
-            }, '->', {
-                text: Ung.Util.iconTitle('Import'.t(), 'file_download-16'),
-                // handler: 'applyChanges'
-            }, {
-                text: Ung.Util.iconTitle('Export'.t(), 'file_upload-16')
-                //handler: 'applyChanges'
-            }],
-            bbar: [{
-                text: Ung.Util.iconTitle('Reset'.t(), 'replay-16'),
-                handler: 'resetDashboard'
-            }, '->', {
-                text: Ung.Util.iconTitle('Apply'.t(), 'save-16'),
-                handler: 'applyChanges'
-            }]
-        }, /*{
-            xtype: 'component',
-            html: '<table>' +
-                    '<tr><td style="text-align: right;"><i class="material-icons" style="color: #999; font-size: 16px; vertical-align: middle;">info</i></td><td>' + 'drag widgets to sort them'.t() + '</td></tr>' +
-                    '</table>',
-            style: {
-                color: '#555',
-                fontSize: '11px',
-                background: '#efefef'
-            },
-            padding: 5,
-            border: false
-        }*/]
     }, {
         xtype: 'container',
         region: 'center',
         reference: 'dashboard',
-        baseCls: 'dashboard',
+        cls: 'dashboard',
         padding: 8,
-        scrollable: true
+        // scrollable: true,
+        // dockedItems: [{
+        //     xtype: 'toolbar',
+        //     dock: 'top',
+        //     border: false,
+        //     items: [{
+        //         xtype: 'button',
+        //         text: 'Manage Widgets'.t()
+        //     }]
+        // }]
     }],
     listeners: {
         //afterrender: 'onAfterRender',
--- uvm/servlets/admin/app/view/main/Main.js
+++ uvm/servlets/admin/app/view/main/Main.js
@@ -1,10 +1,10 @@
 Ext.define('Ung.view.main.Main', {
-    extend: 'Ext.container.Viewport',
+    extend: 'Ext.panel.Panel',
     //xtype: 'ung-main',
 
-    plugins: [
-        'viewport'
-    ],
+    // plugins: [
+    //     'viewport'
+    // ],
 
     requires: [
         'Ext.plugin.Viewport',
@@ -28,154 +28,208 @@ Ext.define('Ung.view.main.Main', {
         type: 'main'
     },
 
-    layout: 'border',
+    layout: 'card',
     border: false,
 
+    bind: {
+        activeItem: '{activeItem}'
+    },
+
     items: [{
-        region: 'north',
-        layout: { type: 'hbox', align: 'middle' },
-        border: false,
-        height: 66,
-        ui: 'navigation',
-        items: [{
-            xtype: 'container',
-            layout: { type: 'hbox', align: 'middle' },
-            defaults: {
-                xtype: 'button',
-                enableToggle: true,
-                baseCls: 'nav-item',
-                height: 30,
-                hrefTarget: '_self'
-            },
-            items: [{
-                enableToggle: false,
-                html: '<img src="' + resourcesBaseHref + '/images/BrandingLogo.png" style="height: 40px;"/>',
-                width: 100,
-                height: 40,
-                href: '#'
-            }, {
-                html: Ung.Util.iconTitle('Dashboard'.t(), 'home-16'),
-                href: '#',
-                bind: {
-                    pressed: '{isDashboard}'
-                }
-            }, {
-                html: Ung.Util.iconTitle('Apps'.t(), 'apps-16'),
-                bind: {
-                    href: '#apps/{policyId}',
-                    pressed: '{isApps}'
-                }
-            }, {
-                html: Ung.Util.iconTitle('Config'.t(), 'tune-16'),
-                href: '#config',
-                bind: {
-                    pressed: '{isConfig}'
-                }
-            }, {
-                html: Ung.Util.iconTitle('Reports'.t(), 'show_chart-16'),
-                href: '#reports',
-                bind: {
-                    //html: '{reportsEnabled}',
-                    hidden: '{!reportsEnabled}',
-                    pressed: '{isReports}'
-                }
-            }]
-        }]
+        xtype: 'ung.dashboard',
+        itemId: 'dashboard'
     }, {
-        xtype: 'container',
-        region: 'center',
-        layout: 'card',
-        itemId: 'main',
+        xtype: 'ung.apps',
+        itemId: 'apps'
+    }, {
+        xtype: 'ung.config',
+        itemId: 'config'
+    }, {
+        xtype: 'ung.sessions',
+        itemId: 'sessions'
+    }, {
+        xtype: 'ung.hosts',
+        itemId: 'hosts'
+    }, {
+        xtype: 'ung.devices',
+        itemId: 'devices'
+    }],
+
+    dockedItems: [{
+        xtype: 'toolbar',
+        ui: 'navigation',
+        dock: 'top',
         border: false,
-        bind: {
-            activeItem: '{activeItem}'
-        },
-        items: [{
-            xtype: 'ung.dashboard',
-            itemId: 'dashboard'
-        },
-        {
-            xtype: 'ung.apps',
-            itemId: 'apps'
-        }, {
-            xtype: 'ung.config',
-            itemId: 'config'
-        }, {
-            xtype: 'ung.configsettings',
-            itemId: 'configsettings'
-        }, {
-            xtype: 'ung.appsinstall',
-            itemId: 'appsinstall'
-        }, {
-            xtype: 'ung.nodesettings',
-            itemId: 'settings'
-        },
-        {
-            layout: 'border',
-            itemId: 'shd', // sessions hosts devices
+        defaults: {
+            xtype: 'button',
             border: false,
-            items: [{
-                region: 'north',
-                weight: 20,
-                border: false,
-                height: 44,
-                bodyStyle: {
-                    background: '#555',
-                    padding: '0 5px'
-                },
-                layout: {
-                    type: 'hbox',
-                    align: 'middle'
-                },
-                defaults: {
-                    xtype: 'button',
-                    enableToggle: true,
-                    baseCls: 'heading-btn',
-                    hrefTarget: '_self'
-                },
-                items: [{
-                    html: Ung.Util.iconTitle('Back to Dashboard', 'keyboard_arrow_left-16'),
-                    enableToggle: false,
-                    href: '#',
-                    hrefTarget: '_self'
-                }, {
-                    xtype: 'component',
-                    flex: 1
-                }, {
-                    html: 'Sessions'.t(),
-                    href: '#sessions',
-                    bind: {
-                        pressed: '{isSessions}'
-                    }
-                }, {
-                    html: 'Hosts'.t(),
-                    href: '#hosts',
-                    bind: {
-                        pressed: '{isHosts}'
-                    }
-                }, {
-                    html: 'Devices'.t(),
-                    href: '#devices',
-                    bind: {
-                        pressed: '{isDevices}'
-                    }
-                }]
-            }, {
-                region: 'center',
-                layout: 'card',
-                itemId: 'shdcenter',
-                items: [{
-                    xtype: 'ung.sessions',
-                    itemId: 'sessions'
-                }, {
-                    xtype: 'ung.hosts',
-                    itemId: 'hosts'
-                }, {
-                    xtype: 'ung.devices',
-                    itemId: 'devices'
-                }]
-            }]
-        }
+            enableToggle: true,
+            iconAlign: 'top',
+            hrefTarget: '_self'
+        },
+        items: [
+            { html: '<img src="' + resourcesBaseHref + '/images/BrandingLogo.png" style="height: 40px;"/>', cls: 'logo' },
+            { text: 'Dashboard'.t(), iconCls: 'fa fa-home', cls: 'upper', href: '#', bind: { pressed: '{isDashboard}' } },
+            { text: 'Apps'.t(), iconCls: 'fa fa-th', cls: 'upper', bind: { href: '#apps/{policyId}', pressed: '{isApps}' } },
+            { text: 'Config'.t(), iconCls: 'fa fa-cog', cls: 'upper', href: '#config', bind: { pressed: '{isConfig}' } },
+            { text: 'Reports'.t(), iconCls: 'fa fa-line-chart', cls: 'upper' },
+            '->',
+            { text: 'Sessions'.t(), iconCls: 'fa fa-list', href: '#sessions' },
+            { text: 'Hosts'.t(), iconCls: 'fa fa-list' },
+            { text: 'Devices'.t(), iconCls: 'fa fa-laptop' },
+            '-',
+            { text: 'Help'.t(), iconCls: 'fa fa-question-circle' },
+            { text: 'Account'.t(), iconCls: 'fa fa-user-circle' }
         ]
     }]
+
+
+    // items: [{
+    //     region: 'north',
+    //     layout: { type: 'hbox', align: 'middle' },
+    //     border: false,
+    //     height: 66,
+    //     // ui: 'navigation',
+    //     items: [{
+    //         xtype: 'container',
+    //         layout: { type: 'hbox', align: 'middle' },
+    //         defaults: {
+    //             xtype: 'button',
+    //             enableToggle: true,
+    //             baseCls: 'nav-item',
+    //             height: 30,
+    //             hrefTarget: '_self'
+    //         },
+    //         items: [{
+    //             enableToggle: false,
+    //             html: '<img src="' + resourcesBaseHref + '/images/BrandingLogo.png" style="height: 40px;"/>',
+    //             width: 100,
+    //             height: 40,
+    //             href: '#'
+    //         }, {
+    //             text: 'Dashboard',
+    //             iconCls: 'fa fa-home',
+    //             href: '#',
+    //             bind: {
+    //                 pressed: '{isDashboard}'
+    //             }
+    //         }, {
+    //             html: Ung.Util.iconTitle('Apps'.t(), 'apps-16'),
+    //             bind: {
+    //                 href: '#apps/{policyId}',
+    //                 pressed: '{isApps}'
+    //             }
+    //         }, {
+    //             html: Ung.Util.iconTitle('Config'.t(), 'tune-16'),
+    //             href: '#config',
+    //             bind: {
+    //                 pressed: '{isConfig}'
+    //             }
+    //         }, {
+    //             html: Ung.Util.iconTitle('Reports'.t(), 'show_chart-16'),
+    //             href: '#reports',
+    //             bind: {
+    //                 //html: '{reportsEnabled}',
+    //                 hidden: '{!reportsEnabled}',
+    //                 pressed: '{isReports}'
+    //             }
+    //         }]
+    //     }]
+    // }, {
+    //     xtype: 'container',
+    //     region: 'center',
+    //     layout: 'card',
+    //     itemId: 'main',
+    //     border: false,
+    //     bind: {
+    //         activeItem: '{activeItem}'
+    //     },
+    //     items: [{
+    //         xtype: 'ung.dashboard',
+    //         itemId: 'dashboard'
+    //     },
+    //     {
+    //         xtype: 'ung.apps',
+    //         itemId: 'apps'
+    //     }, {
+    //         xtype: 'ung.config',
+    //         itemId: 'config'
+    //     }, {
+    //         xtype: 'ung.configsettings',
+    //         itemId: 'configsettings'
+    //     }, {
+    //         xtype: 'ung.appsinstall',
+    //         itemId: 'appsinstall'
+    //     }, {
+    //         xtype: 'ung.nodesettings',
+    //         itemId: 'settings'
+    //     },
+    //     {
+    //         layout: 'border',
+    //         itemId: 'shd', // sessions hosts devices
+    //         border: false,
+    //         items: [{
+    //             region: 'north',
+    //             weight: 20,
+    //             border: false,
+    //             height: 44,
+    //             bodyStyle: {
+    //                 background: '#555',
+    //                 padding: '0 5px'
+    //             },
+    //             layout: {
+    //                 type: 'hbox',
+    //                 align: 'middle'
+    //             },
+    //             defaults: {
+    //                 xtype: 'button',
+    //                 enableToggle: true,
+    //                 baseCls: 'heading-btn',
+    //                 hrefTarget: '_self'
+    //             },
+    //             items: [{
+    //                 html: Ung.Util.iconTitle('Back to Dashboard', 'keyboard_arrow_left-16'),
+    //                 enableToggle: false,
+    //                 href: '#',
+    //                 hrefTarget: '_self'
+    //             }, {
+    //                 xtype: 'component',
+    //                 flex: 1
+    //             }, {
+    //                 html: 'Sessions'.t(),
+    //                 href: '#sessions',
+    //                 bind: {
+    //                     pressed: '{isSessions}'
+    //                 }
+    //             }, {
+    //                 html: 'Hosts'.t(),
+    //                 href: '#hosts',
+    //                 bind: {
+    //                     pressed: '{isHosts}'
+    //                 }
+    //             }, {
+    //                 html: 'Devices'.t(),
+    //                 href: '#devices',
+    //                 bind: {
+    //                     pressed: '{isDevices}'
+    //                 }
+    //             }]
+    //         }, {
+    //             region: 'center',
+    //             layout: 'card',
+    //             itemId: 'shdcenter',
+    //             items: [{
+    //                 xtype: 'ung.sessions',
+    //                 itemId: 'sessions'
+    //             }, {
+    //                 xtype: 'ung.hosts',
+    //                 itemId: 'hosts'
+    //             }, {
+    //                 xtype: 'ung.devices',
+    //                 itemId: 'devices'
+    //             }]
+    //         }]
+    //     }
+    //     ]
+    // }]
 });
