92870f18cade22a77bc9cd1a2d2804207aeaf1a8
Dirk Morris <dmorris@untangle.com>
2017-01-30 19:57:45 -0800

    add c_client_addr to event logs with s_server_addr NGFW-10300

--- hier/usr/share/untangle/lib/untangle-node-policy-manager/reports/policy-manager-all-events.js
+++ hier/usr/share/untangle/lib/untangle-node-policy-manager/reports/policy-manager-all-events.js
@@ -10,7 +10,7 @@
             "value": "NOT NULL"
         }
     ],
-    "defaultColumns": ["time_stamp","username","hostname","c_client_port","policy_id","policy_rule_id","s_server_addr","s_server_port"],
+    "defaultColumns": ["time_stamp","username","hostname","c_client_port","policy_id","policy_rule_id","c_client_addr","s_server_addr","s_server_port"],
     "description": "Lists all sessions with the policy manager rack that handled the session.",
     "displayOrder": 1010,
     "javaClass": "com.untangle.node.reports.ReportEntry",
