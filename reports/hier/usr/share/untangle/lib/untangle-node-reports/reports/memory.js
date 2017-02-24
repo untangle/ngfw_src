ed129b88f2ee59bbf73845391afbb1c080291dd7
Dirk Morris <dmorris@untangle.com>
2017-02-24 08:53:51 -0800

    fix sql queries to work both with sqlite and postgres

--- reports/hier/usr/share/untangle/lib/untangle-node-reports/reports/memory.js
+++ reports/hier/usr/share/untangle/lib/untangle-node-reports/reports/memory.js
@@ -10,7 +10,7 @@
     "readOnly": true,
     "table": "server_events",
     "timeDataColumns": [
-        "round(((max(mem_total)-max(mem_free))::float/max(mem_total)::float)::numeric,4)*100 as mem_usage"
+        "round( CAST( (CAST((max(mem_total)-max(mem_free)) as float) / CAST(max(mem_total) as float)) as numeric),4)*100 as mem_usage"
     ],
     "colors": [
         "#396c2b"
