6c78a23391c6e33bba4ac17d4baf867151b46297
Dirk Morris <dmorris@untangle.com>
2017-02-25 16:40:40 -0800

    add new conditions for rules that deal with the local address instead of forcing the user to choose client or server

--- uvm/hier/usr/share/untangle/lib/untangle-vm/reports/hosts-quotas.js
+++ uvm/hier/usr/share/untangle/lib/untangle-vm/reports/hosts-quotas.js
@@ -1,13 +1,13 @@
 {
+    "uniqueId": "host-viewer-bcRhs6RHUt",
     "category": "Hosts",
     "readOnly": true,
     "type": "EVENT_LIST",
     "conditions": [],
-    "defaultColumns": ["time_stamp","address","action","size","reason"],
+    "defaultColumns": ["time_stamp","entity","action","size","reason"],
     "description": "Shows when quotas are assigned or expired.",
     "displayOrder": 1011,
     "javaClass": "com.untangle.node.reports.ReportEntry",
     "table": "quotas",
-    "title": "Quota Events",
-    "uniqueId": "host-viewer-bcRhs6RHUt"
+    "title": "Quota Events"
 }
