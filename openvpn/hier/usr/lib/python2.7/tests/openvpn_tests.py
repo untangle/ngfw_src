cd320a8ecf341f3828e7cd8dac2406e1db06ba9b
Dirk Morris <dmorris@untangle.com>
2017-03-05 16:26:31 -0800

    use new function name

--- openvpn/hier/usr/lib/python2.7/tests/openvpn_tests.py
+++ openvpn/hier/usr/lib/python2.7/tests/openvpn_tests.py
@@ -259,7 +259,7 @@ class OpenVpnTests(unittest2.TestCase):
         result = os.system("wget -o /dev/null -t 1 --timeout=3 http://localhost"+clientLink+" -O /tmp/clientconfig.zip")
         assert (result == 0)
         # Copy the config file to the remote PC, unzip the files and move to the openvpn directory on the remote device
-        os.system("scp -o 'StrictHostKeyChecking=no' -i " + system_properties.getPrefix() + "/usr/lib/python2.7/tests/testShell.key /tmp/clientconfig.zip testshell@" + global_functions.vpnClientVpnIP + ":/tmp/>/dev/null 2>&1")
+        os.system("scp -o 'StrictHostKeyChecking=no' -i " + system_properties.get_prefix() + "/usr/lib/python2.7/tests/testShell.key /tmp/clientconfig.zip testshell@" + global_functions.vpnClientVpnIP + ":/tmp/>/dev/null 2>&1")
         remote_control.runCommand("sudo unzip -o /tmp/clientconfig.zip -d /tmp/", host=global_functions.vpnClientVpnIP)
         remote_control.runCommand("sudo rm -f /etc/openvpn/*.conf; sudo rm -f /etc/openvpn/*.ovpn; sudo rm -rf /etc/openvpn/keys", host=global_functions.vpnClientVpnIP)
         remote_control.runCommand("sudo mv -f /tmp/untangle-vpn/* /etc/openvpn/", host=global_functions.vpnClientVpnIP)
