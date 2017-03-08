fe9fc934dedef352370c935d61480a3b9ad5ee56
Sébastien Delafond <sdelafond@gmail.com>
2017-03-08 16:51:09 +0100

    [build] Add spam-blocker to rakefile and debian/control

--- spam-blocker/package.rb
+++ spam-blocker/package.rb
@@ -5,4 +5,4 @@ spam = BuildEnv::SRC['untangle-base-spam-blocker']
 
 deps = [smtp['src'], spam['src']]
 
-NodeBuilder.makeNode(HadesBuildEnv, 'untangle-node-spam-blocker', 'spam-blocker', deps, { 'spam-blocker-base' => spam } )
+NodeBuilder.makeNode(BuildEnv::SRC, 'untangle-node-spam-blocker', 'spam-blocker', deps, { 'spam-blocker-base' => spam } )
