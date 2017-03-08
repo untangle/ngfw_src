ba4853f79a123759c48dfd7a1dc481845dc1a11f
Sébastien Delafond <sdelafond@gmail.com>
2017-03-08 16:59:05 +0100

    [build] Add wan-balancer to rakefile and debian/control

--- wan-balancer/package.rb
+++ wan-balancer/package.rb
@@ -1,4 +1,4 @@
 # -*-ruby-*-
 
-NodeBuilder.makeNode(HadesBuildEnv, 'untangle-node-wan-balancer', 'wan-balancer', [] )
+NodeBuilder.makeNode(BuildEnv::SRC, 'untangle-node-wan-balancer', 'wan-balancer', [] )
 
