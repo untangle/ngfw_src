151b5daeb8894eedf78a78a6fdb7e2c6053c1b4c
Sébastien Delafond <sdelafond@gmail.com>
2017-03-08 16:35:07 +0100

    [build] Add branding-manager to rakefile and debian/control

--- branding-manager/package.rb
+++ branding-manager/package.rb
@@ -1,5 +1,5 @@
 # -*-ruby-*-
 
-NodeBuilder.makeNode(HadesBuildEnv, 'untangle-node-branding-manager', 'branding-manager', [])
+NodeBuilder.makeNode(BuildEnv::SRC, 'untangle-node-branding-manager', 'branding-manager', [])
 
 
