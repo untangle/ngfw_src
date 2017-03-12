23e9be277e9eeffbe9af58fb568ad43b7ec9988f
Chris Blaise <cblaise@untangle.com>
2017-03-12 16:56:46 -0400

    Preliminary Event implementation in new UI

---
+++ uvm/servlets/admin/config/events/MainModel.js
@@ -0,0 +1,17 @@
+Ext.define('Ung.config.events.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.config.events',
+
+    data: {
+        title: 'Events'.t(),
+        iconName: 'icon_config_events',
+
+        settings: null
+
+    },
+    stores: {
+        alertRules: { data: '{settings.alertRules.list}' },
+        syslogRules: { data: '{settings.syslogRules.list}' }
+    }
+});
