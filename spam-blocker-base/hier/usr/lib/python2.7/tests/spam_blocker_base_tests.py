40b6fc75705c947f73068bdb37ad337c9b1d8b17
John Coffin <jcoffin@gmail.com>
2017-03-14 11:49:06 -0700

    Change from python mail sink to public test mail server

--- spam-blocker-base/hier/usr/lib/python2.7/tests/spam_blocker_base_tests.py
+++ spam-blocker-base/hier/usr/lib/python2.7/tests/spam_blocker_base_tests.py
@@ -20,8 +20,7 @@ nodeData = None
 nodeSSL = None
 nodeSSLData = None
 canRelay = True
-canRelayTLS = True
-smtpServerHost = 'test.untangle.com'
+smtpServerHost = global_functions.testServerHost
 
 def getLatestMailSender():
     remote_control.run_command("rm -f mailpkg.tar") # remove all previous mail packages
@@ -32,11 +31,13 @@ def getLatestMailSender():
 
 def sendSpamMail(host=smtpServerHost, useTLS=False):
     mailResult = None
+    randomAddress = global_functions.random_email()
+    print "randomAddress: " + randomAddress
     if useTLS:
-        mailResult = remote_control.run_command("python mailsender.py --from=test@example.com --to=qa@example.com ./spam-mail/ --host=" + host + " --reconnect --series=30:0,150,100,50,25,0,180 --starttls", stdout=False, nowait=False)
+        mailResult = remote_control.run_command("python mailsender.py --from=atstest@test.untangle.com --to=" + randomAddress + " ./spam-mail/ --host=" + host + " --reconnect --series=30:0,150,100,50,25,0,180 --starttls", stdout=False, nowait=False)
     else:
-        mailResult = remote_control.run_command("python mailsender.py --from=test@example.com --to=qa@example.com ./spam-mail/ --host=" + host + " --reconnect --series=30:0,150,100,50,25,0,180")
-    return mailResult
+        mailResult = remote_control.run_command("python mailsender.py --from=atstest@test.untangle.com --to=" + randomAddress + " ./spam-mail/ --host=" + host + " --reconnect --series=30:0,150,100,50,25,0,180")
+    return mailResult, randomAddress
 
 def createSSLInspectRule(port="25"):
     return {
@@ -84,7 +85,7 @@ class SpamBlockerBaseTests(unittest2.TestCase):
 
     @staticmethod
     def initialSetUp(self):
-        global node, nodeData, nodeSP, nodeDataSP, nodeSSL, nodeSSLData, canRelay, canRelayTLS
+        global node, nodeData, nodeSP, nodeDataSP, nodeSSL, nodeSSLData, canRelay
         if (uvmContext.nodeManager().isInstantiated(self.nodeName())):
             raise unittest2.SkipTest('node %s already instantiated' % self.nodeName())
         node = uvmContext.nodeManager().instantiate(self.nodeName(), defaultRackId)
@@ -100,10 +101,6 @@ class SpamBlockerBaseTests(unittest2.TestCase):
             canRelay = global_functions.send_test_email(mailhost=smtpServerHost)
         except Exception,e:
             canRelay = False
-        try:
-            canRelayTLS = global_functions.send_test_email(mailhost=global_functions.tlsSmtpServerHost)
-        except Exception,e:
-            canRelayTLS = False
         getLatestMailSender()
         # flush quarantine.
         curQuarantine = nodeSP.getQuarantineMaintenenceView()
@@ -122,14 +119,14 @@ class SpamBlockerBaseTests(unittest2.TestCase):
 
     def test_020_smtpTest(self):
         if (not canRelay):
-            raise unittest2.SkipTest('Unable to relay through test.untangle.com')
+            raise unittest2.SkipTest('Unable to relay through ' + smtpServerHost)
         nodeData['smtpConfig']['scanWanMail'] = True
         nodeData['smtpConfig']['strength'] = 30
         node.setSettings(nodeData)
         # Get the IP address of test.untangle.com
-        test_untangle_IP = socket.gethostbyname("test.untangle.com")
+        test_untangle_IP = socket.gethostbyname(smtpServerHost)
 
-        sendSpamMail()
+        result, to_address = sendSpamMail()
         
         events = global_functions.get_events(self.displayName(),'Quarantined Events',None,1)
         assert( events != None )
@@ -139,102 +136,114 @@ class SpamBlockerBaseTests(unittest2.TestCase):
         found = global_functions.check_events( events.get('list'), 10,
                                                's_server_addr', test_untangle_IP,
                                                's_server_port', 25,
-                                               'addr', 'qa@example.com',
+                                               'addr', to_address,
                                                'c_client_addr', remote_control.clientIP)
         assert( found ) 
 
     def test_030_adminQuarantine(self):
         if (not canRelay):
-            raise unittest2.SkipTest('Unable to relay through test.untangle.com')
+            raise unittest2.SkipTest('Unable to relay through ' + smtpServerHost)
+        # send some spam
+        nodeData['smtpConfig']['scanWanMail'] = True
+        nodeData['smtpConfig']['strength'] = 30
+        node.setSettings(nodeData)
+        result, to_address = sendSpamMail()
         events = global_functions.get_events(self.displayName(),'Quarantined Events',None,1)
+       
         if (events == None):
             raise unittest2.SkipTest('Unable to run admin quarantine since there are no quarantine events')
-            
+        
         # Get adminstrative quarantine list of email addresses
         addressFound = False
         curQuarantine = nodeSP.getQuarantineMaintenenceView()
         curQuarantineList = curQuarantine.listInboxes()
         for checkAddress in curQuarantineList['list']:
             print checkAddress
-            if (checkAddress['address'] == 'qa@example.com') and (checkAddress['totalMails'] > 0): addressFound = True
+            if (checkAddress['address'] == to_address) and (checkAddress['totalMails'] > 0): addressFound = True
         assert(addressFound)
 
     def test_040_userQuarantine(self):
         if (not canRelay):
-            raise unittest2.SkipTest('Unable to relay through test.untangle.com')
+            raise unittest2.SkipTest('Unable to relay through ' + smtpServerHost)
+        # send some spam
+        nodeData['smtpConfig']['scanWanMail'] = True
+        nodeData['smtpConfig']['strength'] = 30
+        node.setSettings(nodeData)
+        result, to_address = sendSpamMail()
         # Get user quarantine list of email addresses
         addressFound = False
         curQuarantine = nodeSP.getQuarantineUserView()
-        curQuarantineList = curQuarantine.getInboxRecords('qa@example.com')
+        curQuarantineList = curQuarantine.getInboxRecords(to_address)
         #print curQuarantineList
         assert(len(curQuarantineList['list']) > 0)
 
     def test_050_userQuarantinePurge(self):
         if (not canRelay):
-            raise unittest2.SkipTest('Unable to relay through test.untangle.com')
+            raise unittest2.SkipTest('Unable to relay through ' + smtpServerHost)
+        # send some spam
+        nodeData['smtpConfig']['scanWanMail'] = True
+        nodeData['smtpConfig']['strength'] = 30
+        node.setSettings(nodeData)
+        result, to_address = sendSpamMail()
         # Get user quarantine list of email addresses
         addressFound = False
         curQuarantine = nodeSP.getQuarantineUserView()
 
-        curQuarantineList = curQuarantine.getInboxRecords('qa@example.com')
+        curQuarantineList = curQuarantine.getInboxRecords(to_address)
         initialLen = len(curQuarantineList['list'])
         mailId = curQuarantineList['list'][0]['mailID'];
         print mailId
-        curQuarantine.purge('qa@example.com', [mailId]);
+        curQuarantine.purge(to_address, [mailId]);
 
-        curQuarantineListAfter = curQuarantine.getInboxRecords('qa@example.com')
+        curQuarantineListAfter = curQuarantine.getInboxRecords(to_address)
         assert(len(curQuarantineListAfter['list']) == initialLen - 1);
 
     def test_060_adminQuarantineDeleteAccount(self):
         # Get adminstrative quarantine list of email addresses
+        if (not canRelay):
+            raise unittest2.SkipTest('Unable to relay through ' + smtpServerHost)
+        # send some spam
+        nodeData['smtpConfig']['scanWanMail'] = True
+        nodeData['smtpConfig']['strength'] = 30
+        node.setSettings(nodeData)
+        result, to_address = sendSpamMail()
         addressFound = False
         curQuarantine = nodeSP.getQuarantineMaintenenceView()
-        curQuarantine.deleteInbox('qa@example.com')
+        curQuarantine.deleteInbox(to_address)
         curQuarantineList = curQuarantine.listInboxes()
         for checkAddress in curQuarantineList['list']:
-            if (checkAddress['address'] == 'qa@example.com') and (checkAddress['totalMails'] > 0): addressFound = True
+            if (checkAddress['address'] == to_address) and (checkAddress['totalMails'] > 0): addressFound = True
         assert(not addressFound)
 
     def test_070_checkForSMTPHeaders(self):
-        wan_IP = uvmContext.networkManager().getFirstWanAddress()
-        # find local SMTP sender
-        fakeSmtpServerHost = "";
-        fakeSmtpServerHost, fakeSmtpdomain = global_functions.find_smtp_server(wan_IP)
-        if (fakeSmtpServerHost == None):
-            raise unittest2.SkipTest("No local SMTP server")
-        externalClientResult = subprocess.call(["ping","-c","1",fakeSmtpServerHost],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-        if (externalClientResult != 0):
-            raise unittest2.SkipTest("Fake SMTP client is unreachable, skipping smtp headers check")
+        if (not canRelay):
+            raise unittest2.SkipTest('Unable to relay through ' + smtpServerHost)
+
         nodeData['smtpConfig']['blockSuperSpam'] = False
         nodeData['smtpConfig']['scanWanMail'] = True
         nodeData['smtpConfig']['addSpamHeaders'] = True
         nodeData['smtpConfig']['msgAction'] = "MARK"
         node.setSettings(nodeData)
-        # remove previous smtp log file
-        remote_control.run_command("sudo pkill -INT python",host=fakeSmtpServerHost)
-        remote_control.run_command("sudo rm -f /tmp/test_070_checkForSMTPHeaders.log /tmp/qa@example.com.*", host=fakeSmtpServerHost)
-        # Start mail sink
-        remote_control.run_command("sudo python fakemail.py --host " + fakeSmtpServerHost + " --log /tmp/test_070_checkForSMTPHeaders.log --port 25 --path /tmp/ --background", host=fakeSmtpServerHost, stdout=False, nowait=True)
-        time.sleep(5) # the current mail sink takes a bit of time to start listening.
-        sendSpamMail(host=fakeSmtpServerHost)
+
+        result, to_address = sendSpamMail()
         # check for email file if there is no timeout
         emailFound = False
-        timeout = 60
+        timeout = 5
+        emailContext = ""
         while not emailFound and timeout > 0:
             timeout -= 1
             time.sleep(1)
             # Check to see if the delivered email file is present
-            email_file = remote_control.run_command("test -f /tmp/qa@example.com.1",host=fakeSmtpServerHost)
-            if (email_file == 0):
+            emailContext = remote_control.run_command("wget -q --timeout=5 -O - http://test.untangle.com/cgi-bin/getEmail.py?toaddress=" + to_address + " 2>&1" ,stdout=True)
+            if (emailContext != ""):
                 emailFound = True
                 
         # Either found email file or timed out so kill mail sink
-        remote_control.run_command("sudo pkill -INT python",host=fakeSmtpServerHost)
+        # remote_control.run_command("sudo pkill -INT python",host=fakeSmtpServerHost)
         nodeData['smtpConfig']['msgAction'] = "QUARANTINE"
         node.setSettings(nodeData)
         assert (timeout != 0)
         # look for added header in delivered email
-        emailContext=remote_control.run_command("cat /tmp/qa@example.com.1",host=fakeSmtpServerHost, stdout=True)
         lines = emailContext.split("\n")
         spamScore = 0
         requiredScore = 0
@@ -255,31 +264,26 @@ class SpamBlockerBaseTests(unittest2.TestCase):
         # assert(float(requiredScore) > float(spamScore))
 
     def test_080_checkAllowTLS(self):
-        wan_IP = uvmContext.networkManager().getFirstWanAddress()
-        if not global_functions.is_in_office_network(wan_IP):
-            raise unittest2.SkipTest("Not on office network, skipping")
-        if (not canRelayTLS):
-            raise unittest2.SkipTest('Unable to relay through ' + global_functions.tlsSmtpServerHost)
+        if (not canRelay):
+            raise unittest2.SkipTest('Unable to relay through ' + smtpServerHost)
         nodeData['smtpConfig']['scanWanMail'] = True
         node.setSettings(nodeData)
         # Make sure SSL Inspector is off
         nodeSSL.stop()
-        tlsSMTPResult = sendSpamMail(host=global_functions.tlsSmtpServerHost, useTLS=True)
+        tlsSMTPResult, to_address = sendSpamMail(useTLS=True)
         # print "TLS 1 : " + str(tlsSMTPResult)
         assert(tlsSMTPResult != 0)
         nodeData['smtpConfig']['allowTls'] = True
         node.setSettings(nodeData)
-        tlsSMTPResult = sendSpamMail(host=global_functions.tlsSmtpServerHost, useTLS=True)
+        tlsSMTPResult, to_address = sendSpamMail(host=global_functions.testServerHost, useTLS=True)
         # print "TLS 2 : " + str(tlsSMTPResult)
         assert(tlsSMTPResult == 0)
         
     
     def test_090_checkTLSwSSLInspector(self):
-        wan_IP = uvmContext.networkManager().getFirstWanAddress()
-        if not global_functions.is_in_office_network(wan_IP):
-            raise unittest2.SkipTest("Not on office network, skipping")
-        if (not canRelayTLS):
-            raise unittest2.SkipTest('Unable to relay through ' + global_functions.tlsSmtpServerHost)
+        if (not canRelay):
+            raise unittest2.SkipTest('Unable to relay through ' + smtpServerHost)
+        test_untangle_IP = socket.gethostbyname(smtpServerHost)
         nodeData['smtpConfig']['scanWanMail'] = True
         nodeData['smtpConfig']['allowTls'] = False
         nodeData['smtpConfig']['strength'] = 30
@@ -289,7 +293,7 @@ class SpamBlockerBaseTests(unittest2.TestCase):
         nodeSSLData['ignoreRules']['list'].insert(0,createSSLInspectRule("25"))
         nodeSSL.setSettings(nodeSSLData)
         nodeSSL.start()
-        tlsSMTPResult = sendSpamMail(host=global_functions.tlsSmtpServerHost, useTLS=True)
+        tlsSMTPResult, to_address = sendSpamMail(useTLS=True)
         # print "TLS 090 : " + str(tlsSMTPResult)
         nodeSSL.stop()
         assert(tlsSMTPResult == 0)
@@ -299,9 +303,9 @@ class SpamBlockerBaseTests(unittest2.TestCase):
 
         print events['list'][0]
         found = global_functions.check_events( events.get('list'), 5,
-                                               's_server_addr', global_functions.tlsSmtpServerHost,
+                                               's_server_addr', test_untangle_IP,
                                                's_server_port', 25,
-                                               'addr', 'qa@example.com',
+                                               'addr', to_address,
                                                'c_client_addr', remote_control.clientIP)
         assert( found ) 
             
