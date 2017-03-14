2c1298b3d34eb0178250681fa1013f92e5088928
Chris Blaise <cblaise@untangle.com>
2017-03-14 12:33:22 -0400

    Update Branding Manager to new UI API

--- uvm/servlets/admin/apps/branding-manager/Main.js
+++ uvm/servlets/admin/apps/branding-manager/Main.js
@@ -1,6 +1,7 @@
 Ext.define('Ung.apps.brandingmanager.Main', {
     extend: 'Ung.cmp.AppPanel',
     alias: 'widget.app-branding-manager',
+    controller: 'app-branding-manager',
 
     items: [
         { xtype: 'app-branding-manager-status' },
