d3b52d2882d970b2430bad2bee924c85a5ed5ed3
Sébastien Delafond <sdelafond@gmail.com>
2017-03-15 16:48:30 +0100

    [build] Re-enable jslint target, but only run it on "new" admin UI; only call jslint binary once on all the files, to limit overhead

--- buildtools/node.rb
+++ buildtools/node.rb
@@ -53,13 +53,13 @@ class NodeBuilder
       # buildEnv.hierTarget.register_dependency(cf_python)
     end
 
-    jsFiles = FileList["#{home}/#{dirName}/hier/usr/share/untangle/web/webui/**/*.js"]
-    if ( jsFiles.length > 0 ) 
-      jsFiles.each do |f|
-        jsl = JsLintTarget.new(node, [f], 'jslint', f)
-        buildEnv.jsLintTarget.register_dependency(jsl)
-      end
-    end
+    # jsFiles = FileList["#{home}/#{dirName}/hier/usr/share/untangle/web/webui/**/*.js"]
+    # if ( jsFiles.length > 0 ) 
+    #   jsFiles.each do |f|
+    #     jsl = JsLintTarget.new(node, [f], 'jslint', f)
+    #     buildEnv.jsLintTarget.register_dependency(jsl)
+    #   end
+    # end
   end
 
   ## Helper to retrieve the standard dependencies
