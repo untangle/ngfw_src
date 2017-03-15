22dcdc6c7063a51d59dd7d2981fd7ae56192eeec
Chris Blaise <cblaise@untangle.com>
2017-03-14 20:52:24 -0400

    Nw component for Google drive

---
+++ uvm/servlets/admin/app/cmp/GoogleDrive.js
@@ -0,0 +1,33 @@
+Ext.define('Ung.cmp.GoogleDrive', {
+    alias: 'widget.googledrive',
+
+    isConfigured: function () {
+        var googleDriveConfigured = false, directoryConnectorLicense, directoryConnectorNode, googleManager;
+        try{
+            directoryConnectorLicense = Rpc.directData('rpc.UvmContext.licenseManager').isLicenseValid("untangle-node-directory-connector");
+            directoryConnectorNode = Rpc.directData('rpc.UvmContext.nodeManager').node("untangle-node-directory-connector");
+            if( directoryConnectorLicense && directoryConnectorNode ){
+                googleManager = directoryConnectorNode.getGoogleManager();
+                if( googleManager && googleManager.isGoogleDriveConnected() ){
+                    googleDriveConfigured = true;
+                }
+            }
+        }catch(e){
+            Util.exceptionToast('isConfigured: Unable to determine \'' + e + '\'');
+        }
+        return googleDriveConfigured;
+    },
+    configure: function () {
+        var node = Rpc.directData('rpc.UvmContext.nodeManager').node("untangle-node-directory-connector");
+        if (node != null) {
+            // var nodeCmp = Ung.Node.getCmp(node.nodeId);
+            var nodeCmp = Ext.getCmp(node.nodeId);
+            if (nodeCmp != null) {
+                Ung.Main.target = "node.untangle-node-directory-connector.Google Connector";
+                nodeCmp.loadSettings();
+            }
+        } else {
+            Ext.MessageBox.alert("Error".t(), "Google Drive requires Directory Connector application.".t());
+        }
+    }
+});
