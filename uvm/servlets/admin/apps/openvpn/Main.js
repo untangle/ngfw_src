55560697622a6c075bfcfe937af93e27804cc03e
mahotz <mahotz@untangle.com>
2017-03-16 16:29:06 -0700

    Making progress on the OpenVPN interface. Work in progress.

--- uvm/servlets/admin/apps/openvpn/Main.js
+++ uvm/servlets/admin/apps/openvpn/Main.js
@@ -3,10 +3,22 @@ Ext.define('Ung.apps.openvpn.Main', {
     alias: 'widget.app-openvpn',
     controller: 'app-openvpn',
 
+    viewModel: {
+        stores: {
+            remoteClients: {
+                data: '{settings.remoteClients.list}'
+            },
+            remoteServers: {
+                data: '{settings.remoteServers.list}'
+            }
+        }
+    },
+
     items: [
         { xtype: 'app-openvpn-status' },
         { xtype: 'app-openvpn-server' },
-        { xtype: 'app-openvpn-client' }
+        { xtype: 'app-openvpn-client' },
+        { xtype: 'app-openvpn-advanced' }
     ]
 
 });
--- uvm/servlets/admin/apps/openvpn/MainController.js
+++ uvm/servlets/admin/apps/openvpn/MainController.js
@@ -3,6 +3,9 @@ Ext.define('Ung.apps.openvpn.MainController', {
     alias: 'controller.app-openvpn',
 
     control: {
+        '#': {
+            afterrender: 'getSettings'
+        },
         '#status': {
             beforerender: 'onStatusBeforeRender'
         }
@@ -40,6 +43,44 @@ Ext.define('Ung.apps.openvpn.MainController', {
             vm.set('remoteServers', result.list);
             console.log(result);
         });
+    },
+
+    getSettings: function () {
+        var v = this.getView(), vm = this.getViewModel();
+        v.setLoading(true);
+        v.appManager.getSettings(function (result, ex) {
+            v.setLoading(false);
+            if (ex) { Util.exceptionToast(ex); return; }
+            vm.set('settings', result);
+        });
+    },
+
+    setSettings: function () {
+        var me = this, v = this.getView(), vm = this.getViewModel();
+
+        v.query('ungrid').forEach(function (grid) {
+            var store = grid.getStore();
+            if (store.getModifiedRecords().length > 0 ||
+                store.getNewRecords().length > 0 ||
+                store.getRemovedRecords().length > 0 ||
+                store.isReordered) {
+                store.each(function (record) {
+                    if (record.get('markedForDelete')) {
+                        record.drop();
+                    }
+                });
+                store.isReordered = undefined;
+                vm.set(grid.listProperty, Ext.Array.pluck(store.getRange(), 'data'));
+            }
+        });
+
+        v.setLoading(true);
+        v.appManager.setSettings(function (result, ex) {
+            v.setLoading(false);
+            if (ex) { Util.exceptionToast(ex); return; }
+            Util.successToast('Settings saved');
+            me.getSettings();
+        }, vm.get('settings'));
     }
 
 });
