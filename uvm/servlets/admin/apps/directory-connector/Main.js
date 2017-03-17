b0a60c59a8cd02df860be92752cbd38c2d9e1efd
Chris Blaise <cblaise@untangle.com>
2017-03-16 20:10:09 -0400

    Partial Director Connector conversion: User Notification API and Active Directory

--- uvm/servlets/admin/apps/directory-connector/Main.js
+++ uvm/servlets/admin/apps/directory-connector/Main.js
@@ -1,12 +1,7 @@
 Ext.define('Ung.apps.directoryconnector.Main', {
     extend: 'Ung.cmp.AppPanel',
     alias: 'widget.app-directory-connector',
-
-    viewModel: {
-        data: {
-            appName: 'directory-connector'
-        }
-    },
+    controller: 'app-directory-connector',
 
     items: [
         { xtype: 'app-directory-connector-status' },
