025aa166049d168d2487c32d8c0436b39bd9bd24
mahotz <mahotz@untangle.com>
2017-03-21 11:19:59 -0700

    Moved the server tab grids into the Server view file. Added tab and grid tot he Client view file. Work in progress.

--- uvm/servlets/admin/apps/openvpn/view/Client.js
+++ uvm/servlets/admin/apps/openvpn/view/Client.js
@@ -9,7 +9,7 @@ Ext.define('Ung.apps.openvpn.view.Client', {
         xtype: 'tbtext',
         padding: '8 5',
         style: { fontSize: '12px', fontWeight: 600 },
-        html: 'The Client tab is used to configure servers to which OpenVPN will connect as a client.'.t()
+        html: 'The Client tab is used to configure the servers that OpenVPN will connect as a client.'.t()
     }],
 
     defaults: {
@@ -17,25 +17,10 @@ Ext.define('Ung.apps.openvpn.view.Client', {
     },
 
     items: [{
-        xtype: 'ungrid',
-        region: 'center',
-        tbar: ['@add'],
-        recordActions: ['edit','delete'],
-        listProperty: 'settings.remoteServers.list',
-
-        bind: '{remoteServers}',
-
-        columns: [{
-            header: 'Enabled'.t(),
-            dataIndex: 'enabled',
-            xtype: 'checkcolumn',
-            width: 80
-        },{
-            header: 'Server Name'.t(),
-            dataIndex: 'name',
-            width: 200,
-            flex: 1
-        }],
+        xtype: 'app-openvpn-client-tab-panel',
+        padding: '20 20 20 20',
+        width: 800,
+        height: 400
     },{
         xtype: 'displayfield',
         padding: '20 0 0 10',
@@ -51,4 +36,67 @@ Ext.define('Ung.apps.openvpn.view.Client', {
         width: 100,
         text: 'Submit'.t()
     }]
+});
+
+Ext.define('Ung.apps.openvpn.view.ClientTabs', {
+    extend: 'Ext.tab.Panel',
+    alias: 'widget.app-openvpn-client-tab-panel',
+    itemId: 'client-tab-panel',
+    viewModel: true,
+    width: 800,
+    height: 400,
+
+    items: [{
+        title: 'Remote Servers'.t(),
+        items: [
+            { xtype: 'app-openvpn-remote-servers-grid' }
+            ]
+    }]
+
+});
+
+Ext.define('Ung.apps.openvpn.view.RemoteServersGrid', {
+    extend: 'Ung.cmp.Grid',
+    alias: 'widget.app-openvpn-remote-servers-grid',
+    itemId: 'remote-servers-grid',
+
+    dockedItems: [{
+        xtype: 'toolbar',
+        dock: 'top',
+        html: 'Remote Servers'.t(),
+    }],
+
+    recordActions: ['edit', 'delete'],
+    listProperty: 'settings.remoteServers.list',
+    emptyRow: {
+        javaClass: 'com.untangle.app.openvpn.OpenVpnRemoteServer',
+        'enabled': true,
+        'name': '',
+        },
+
+    bind: '{remoteServers}',
+
+    columns: [{
+        xtype: 'checkcolumn',
+        header: 'Enabled'.t(),
+        width: 80,
+        dataIndex: 'enabled',
+        resizable: false
+    }, {
+        header: 'Server Name'.t(),
+        width: 150,
+        flex: 1,
+        dataIndex: 'name',
+    }],
+
+    editorFields: [{
+        xtype: 'checkbox',
+        bind: '{record.enabled}',
+        fieldLabel: 'Enabled'.t()
+    }, {
+        xtype: 'textfield',
+        bind: '{record.name}',
+        fieldLabel: 'Server Name'.t()
+    }]
+
 });
