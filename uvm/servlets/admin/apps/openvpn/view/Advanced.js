2c2946c52602123b730c30342832b2fccdc7d194
mahotz <mahotz@untangle.com>
2017-03-22 14:11:14 -0700

    Made OpenVpnConfigItem simpler, and changed defaultFlag to readOnly which works nicely with Ung.cmp.Grid to prevent default options from being deleted.

--- uvm/servlets/admin/apps/openvpn/view/Advanced.js
+++ uvm/servlets/admin/apps/openvpn/view/Advanced.js
@@ -36,6 +36,85 @@ Ext.define('Ung.apps.openvpn.view.Advanced', {
         fieldLabel: 'Client To Client Allowed'.t(),
         xtype: 'checkbox',
         bind: '{settings.clientToClient}'
+    },{
+        title: 'Server Configuration'.t(),
+        xtype: 'app-openvpn-config-editor-grid',
+        padding: '20 20 20 20',
+        width: 800,
+        height: 300,
+        listProperty: 'settings.serverConfiguration.list',
+        itemId: 'server-config-editor-grid',
+        bind: '{serverConfiguration}'
+    },{
+        title: 'Client Configuration'.t(),
+        xtype: 'app-openvpn-config-editor-grid',
+        padding: '20 20 20 20',
+        width: 800,
+        height: 300,
+        listProperty: 'settings.clientConfiguration.list',
+        itemId: 'client-config-editor-grid',
+        bind: '{clientConfiguration}'
+    }]
+});
+
+Ext.define('Ung.apps.openvpn.view.ConfigEditorGrid', {
+    extend: 'Ung.cmp.Grid',
+    alias: 'widget.app-openvpn-config-editor-grid',
+
+    dockedItems: [{
+        xtype: 'toolbar',
+        dock: 'top',
+        items: ['@addInline']
+    }],
+
+    recordActions: ['delete'],
+
+    emptyRow: {
+        javaClass: 'com.untangle.app.openvpn.OpenVpnConfigItem',
+        'optionName': null,
+        'optionValue': null,
+        'excludeFlag': false,
+        'readOnly': false,
+    },
+
+    listeners: {
+        cellclick: function(grid,td,cellIndex,record,tr,rowIndex,e,eOpts)  {
+            // not allowed to edit default options
+            if (record.data.readOnly) return(false);
+            }
+        },
+
+    columns: [{
+        header: 'Option Name'.t(),
+        width: 250,
+        dataIndex: 'optionName',
+        editable: false,
+        editor: {
+            xtype: 'textfield',
+            bind: '{record.optionName}',
+        },
+    }, {
+        header: 'Option Value'.t(),
+        width: 250,
+        dataIndex: 'optionValue',
+        editor: {
+            xtype: 'textfield',
+            bind: '{record.optionValue}'
+        }
+    }, {
+        header: 'Option Type'.t(),
+        width: 80,
+        dataIndex: 'readOnly',
+        resizable: false,
+        renderer: function(val) {
+            return(val ? 'default' : 'custom');
+        }
+    }, {
+        xtype: 'checkcolumn',
+        header: 'Exclude'.t(),
+        width: 80,
+        dataIndex: 'excludeFlag',
+        resizable: false,
     }]
 
 });
