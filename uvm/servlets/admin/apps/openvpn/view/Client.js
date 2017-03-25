5779beb358ea41d9a19a8805a87827d5e9509fbc
mahotz <mahotz@untangle.com>
2017-03-24 21:52:13 -0700

    The OpenVPN client upload button is working now.

--- uvm/servlets/admin/apps/openvpn/view/Client.js
+++ uvm/servlets/admin/apps/openvpn/view/Client.js
@@ -1,7 +1,7 @@
 Ext.define('Ung.apps.openvpn.view.Client', {
     extend: 'Ext.panel.Panel',
     alias: 'widget.app-openvpn-client',
-    itemId: 'client',
+    itemId: 'openvpn-client',
     title: 'Client'.t(),
     viewModel: true,
 
@@ -18,32 +18,20 @@ Ext.define('Ung.apps.openvpn.view.Client', {
 
     items: [{
         xtype: 'app-openvpn-client-tab-panel',
-        padding: '20 20 20 20',
+        padding: '20 20 0 20',
         width: 800,
         height: 400
     },{
-        xtype: 'displayfield',
-        value: '<STRONG>' + 'Configure a new Remote Server connection'.t() + '</STRONG>'
-    },{
-        xtype: 'fileuploadfield',
-        fieldLabel: 'Configuration File'.t(),
-        labelWidth: 160,
-        width: 600,
-    },{
-        xtype: 'button',
-        align: 'center',
-        width: 120,
-        height: 30,
-        padding: '0 0 0 0',
-        margin: '0 10 0 10',
-        text: 'Submit'.t()
+        xtype: 'app-openvpn-client-submit-form',
+        padding: '20 20 0 20',
+        border: false
     }]
 });
 
 Ext.define('Ung.apps.openvpn.view.ClientTabs', {
     extend: 'Ext.tab.Panel',
     alias: 'widget.app-openvpn-client-tab-panel',
-    itemId: 'client-tab-panel',
+    itemId: 'openvpn-client-tab-panel',
     viewModel: true,
     width: 800,
     height: 400,
@@ -60,7 +48,7 @@ Ext.define('Ung.apps.openvpn.view.ClientTabs', {
 Ext.define('Ung.apps.openvpn.view.RemoteServersGrid', {
     extend: 'Ung.cmp.Grid',
     alias: 'widget.app-openvpn-remote-servers-grid',
-    itemId: 'remote-servers-grid',
+    itemId: 'openvpn-remote-servers-grid',
 
     recordActions: ['delete'],
     listProperty: 'settings.remoteServers.list',
@@ -88,4 +76,58 @@ Ext.define('Ung.apps.openvpn.view.RemoteServersGrid', {
             bind: '{record.name}'
         }
     }],
+});
+
+Ext.define('Ung.apps.openvpn.view.ClientSubmitForm', {
+    extend: 'Ext.form.Panel',
+    alias: 'widget.app-openvpn-client-submit-form',
+    itemId: 'openvpn-client-submit-form',
+    flex: 0,
+    margin: '5 0 0 0',
+    border: false,
+    viewModel: true,
+
+    defaults: {
+        padding: '10 0 0 10'
+    },
+
+    items: [{
+        xtype: 'fieldset',
+        title: 'Configure a new Remote Server connection'.t(),
+        border: false,
+        items: [{
+            xtype: 'filefield',
+            name: 'uploadConfigFileName',
+            fieldLabel: 'Configuration File'.t(),
+            allowBlank: false,
+            validateOnBlur: false,
+            labelWidth: 150,
+            width: 600
+        }, {
+            xtype: 'button',
+            id: 'submitUpload',
+            text: 'Submit'.t(),
+            name: "Submit",
+            handler: function(btn) {
+                var form = Ext.ComponentQuery.query('#openvpn-client-submit-form')[0];
+                var filename = Ext.ComponentQuery.query('textfield[name=uploadConfigFileName]')[0].value;
+                if ( filename == null || filename.length == 0 ) {
+                    Ext.MessageBox.alert('Select File'.t(), 'Please choose a file to upload.'.t());
+                    return;
+                }
+                form.submit({
+                    url: "/openvpn/uploadConfig",
+                    success: Ext.bind(function( form, action, handler ) {
+                        Ext.MessageBox.alert('Success'.t(), 'The configuration has been imported.'.t());
+                        var grid = Ext.ComponentQuery.query('#openvpn-remote-servers-grid')[0];
+                        grid.store.reload();
+                        }, this),
+                    failure: Ext.bind(function( form, action ) {
+                        Ext.MessageBox.alert('Failure'.t(), 'Import failure'.t() + ": " + action.result.code);
+                    }, this)
+                });
+            }
+        }]
+    }]
+
 });
