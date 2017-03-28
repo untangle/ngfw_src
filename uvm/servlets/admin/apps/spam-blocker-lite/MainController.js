b54304b7d94a40b479e44d04ceb031f9043e6305
mahotz <mahotz@untangle.com>
2017-03-28 07:38:58 -0700

    Added Email tab for Spam Blocker Lite.

---
+++ uvm/servlets/admin/apps/spam-blocker-lite/MainController.js
@@ -0,0 +1,46 @@
+Ext.define('Ung.apps.spamblockerlite.MainController', {
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.app-spam-blocker-lite',
+
+    control: {
+        '#': {
+            afterrender: 'getSettings',
+        }
+    },
+
+    getSettings: function () {
+        var v = this.getView(), vm = this.getViewModel();
+        v.setLoading(true);
+        v.appManager.getSettings(function (result, ex) {
+            v.setLoading(false);
+            if (ex) { Util.exceptionToast(ex); return; }
+            console.log(result);
+            vm.set('settings', result);
+        });
+
+        var lastUpdate = v.appManager.getLastUpdate();
+        var lastUpdateCheck = v.appManager.getLastUpdateCheck();
+        if (lastUpdate) {
+            vm.set('lastUpdate', 'Spam Blocker Lite was last updated'.t() + ': <strong>' + Ext.util.Format.date(new Date(lastUpdate.time), 'timestamp_fmt'.t()) + '</strong>');
+        } else {
+            vm.set('lastUpdate', 'Spam Blocker Lite was last updated'.t() + ': <strong>' + 'never'.t() + '</strong>');
+        }
+        if (lastUpdateCheck) {
+            vm.set('lastUpdateCheck', 'Spam Blocker Lite last checked for updates'.t() + ': <strong>' + Ext.util.Format.date(new Date(lastUpdateCheck.time), 'timestamp_fmt'.t()) + '</strong>');
+        } else {
+            vm.set('lastUpdateCheck', 'Spam Blocker Lite last checked for updates'.t() + ': <strong>' + 'never'.t() + '</strong>');
+        }
+    },
+
+    setSettings: function () {
+        var me = this, v = this.getView(), vm = this.getViewModel();
+        v.setLoading(true);
+        v.appManager.setSettings(function (result, ex) {
+            v.setLoading(false);
+            if (ex) { Util.exceptionToast(ex); return; }
+            Util.successToast('Settings saved');
+            me.getSettings();
+        }, vm.get('settings'));
+    }
+
+});
