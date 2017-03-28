2252f9ecfda63a3bdf59166fb4def7eeefe783f0
Dirk Morris <dmorris@untangle.com>
2017-03-27 20:25:29 -0700

    fix priority renderer NGFW-10437

--- uvm/servlets/admin/apps/bandwidth-control/view/Rules.js
+++ uvm/servlets/admin/apps/bandwidth-control/view/Rules.js
@@ -120,7 +120,20 @@ Ext.define('Ung.apps.bandwidthcontrol.view.Rules', {
                     return 'Unknown action'.t();
                 }
                 switch(value.actionType) {
-                  case 'SET_PRIORITY': return 'Set Priority' + ' [' + this.priorityRenderer(value.priority) + ']';
+                  //case 'SET_PRIORITY': return 'Set Priority' + ' [' + this.priorityRenderer(value.priority) + ']';
+                  case 'SET_PRIORITY':
+                    var priostr = value.priority;
+                    switch(value.priority) {
+                      case 1: priostr = 'Very High'.t(); break;
+                      case 2: priostr = 'High'.t(); break;
+                      case 3: priostr = 'Medium'.t(); break;
+                      case 4: priostr = 'Low'.t(); break;
+                      case 5: priostr = 'Limited'.t(); break;
+                      case 6: priostr = 'Limited More'.t(); break;
+                      case 7: priostr = 'Limited Severely'.t(); break;
+                    default: priostr = 'Unknown Priority'.t() + ': ' + value.priority; break;
+                    }
+                    return 'Set Priority'.t() + ' [' + priostr + ']';
                   case 'TAG_HOST': return 'Tag Host'.t();
                   case 'APPLY_PENALTY_PRIORITY': return 'Apply Penalty Priority'.t(); // DEPRECATED
                   case 'GIVE_CLIENT_HOST_QUOTA': return 'Give Client a Quota'.t();
