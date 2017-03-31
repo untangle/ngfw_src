e072d2ce7689faea54e7369082f99036c03768a2
Dan Hadarau <dhadarau@untangle.com>
2017-03-31 18:44:12 +0300

    admin Policy Manager work in progress

--- uvm/servlets/admin/apps/policy-manager/Main.js
+++ uvm/servlets/admin/apps/policy-manager/Main.js
@@ -2,6 +2,23 @@ Ext.define('Ung.apps.policymanager.Main', {
     extend: 'Ung.cmp.AppPanel',
     alias: 'widget.app-policy-manager',
 
+    controller: 'app-policy-manager',
+
+    viewModel: {
+        data: {
+            appsData: []
+        },
+        stores: {
+            appsStore: {
+                data: '{appsData}',
+                filters: [{
+                    property: 'type',
+                    value: 'FILTER'
+                }]
+            }
+        }
+    },
+
     items: [
         { xtype: 'app-policy-manager-status' },
         { xtype: 'app-policy-manager-policies' },
