4ddabd199a1cab69a9dc22cc2041b8ead55200ea
Chris Blaise <cblaise@untangle.com>
2017-04-17 12:30:47 -0400

    NGFW-10399 Display licenses

--- uvm/servlets/admin/config/about/MainController.js
+++ uvm/servlets/admin/config/about/MainController.js
@@ -21,19 +21,31 @@ Ext.define('Ung.config.about.MainController', {
                 activeSize: rpc.hostTable.getCurrentActiveSize(),
                 maxActiveSize: rpc.hostTable.getMaxActiveSize()
             });
-            console.log(view.getViewModel());
         } catch (ex) {
 
         }
     },
 
     reloadLicenses: function () {
+        var vm = this.getViewModel();
+
         rpc.licenseManager = rpc.UvmContext.licenseManager();
         Rpc.asyncData('rpc.licenseManager.reloadLicenses', true).then(function(result) {
             Rpc.asyncData('rpc.licenseManager.getLicenses').then(function(result) {
                 vm.set('licenses', result.list);
             });
         });
+    }
+
+});
+
+Ext.define('Ung.config.about.cmp.LicenseGridController', {
+    extend: 'Ung.cmp.GridController',
+
+    alias: 'controller.unaboutlicensegrid',
+
+    dateRenderer: function(value){
+        return Util.timestampFormat(value * 1000);
     }
 
 });
