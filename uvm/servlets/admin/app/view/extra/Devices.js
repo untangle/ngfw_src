4efb8c4c99323ae3522bb60eb2346ce3cf550196
Dan Hadarau <dhadarau@untangle.com>
2017-04-18 10:33:41 +0300

    NGFW-10570 - fix Devices view

--- uvm/servlets/admin/app/view/extra/Devices.js
+++ uvm/servlets/admin/app/view/extra/Devices.js
@@ -40,88 +40,200 @@ Ext.define('Ung.view.extra.Devices', {
     },
 
     viewModel: {
-        formulas: {
-            deviceDetails: function (get) {
-                if (get('devicesgrid.selection')) {
-                    var data = get('devicesgrid.selection').getData();
-                    console.log(data);
-                    delete data._id;
-                    delete data.javaClass;
-                    return data;
-                }
-                return;
-            }
-        }
+        // formulas: {
+        //     deviceDetails: function (get) {
+        //         if (get('devicesgrid.selection')) {
+        //             var data = get('devicesgrid.selection').getData();
+        //             console.log(data);
+        //             delete data._id;
+        //             delete data.javaClass;
+        //             return data;
+        //         }
+        //         return;
+        //     }
+        // }
     },
 
     items: [{
-        xtype: 'grid',
+        xtype: 'ungrid',
         region: 'center',
         itemId: 'devicesgrid',
         reference: 'devicesgrid',
         title: 'Current Devices'.t(),
         store: 'devices',
-        forceFit: true,
-        columns: [
-            { header: 'MAC Address'.t(), dataIndex: 'macAddress' },
-            { header: 'MAC Vendor'.t(), dataIndex: 'macVendor' },
-            { header: 'Interface'.t(), dataIndex: 'lastSeenInterfaceId' },
-            { header: 'Hostname'.t(), dataIndex: 'hostname' },
-            { header: 'Hostname Last Known'.t(), dataIndex: 'hostnameLastKnown' },
-            { header: 'Username'.t(), dataIndex: 'username' },
-            { header: 'Tags'.t(), dataIndex: 'tags' },
-            { header: 'Tags String'.t(), dataIndex: 'tagsString' },
-            { header: 'HTTP'.t() + ' - ' + 'User Agent'.t(), dataIndex: 'httpUserAgent' },
-            { header: 'Last Seen Time'.t(), dataIndex: 'lastSessionTimeDate' },
-        ]
-    }, {
-        region: 'east',
-        xtype: 'propertygrid',
-        itemId: 'details',
-        editable: false,
-        width: 400,
-        split: true,
-        collapsible: false,
-        resizable: true,
-        shadow: false,
-        hidden: true,
+        forceFit: false,
 
-        cls: 'prop-grid',
+        tbar: ['@add', '->', '@import', '@export'],
+        recordActions: ['edit', 'delete'],
+        emptyRow: {
+            macAddress: '',
+            macVendor: '',
+            hostname: '',
+            interfaceId: -1,
+            lastSessionTime: 0,
+            tags: {
+                javaClass: 'java.util.LinkedList',
+                list: []
+            },
+            username: '',
+            javaClass: 'com.untangle.uvm.DeviceTableEntry'
+        },
 
-        viewConfig: {
-            stripeRows: false,
-            getRowClass: function(record) {
-                if (record.get('value') === null || record.get('value') === '') {
-                    return 'empty';
+        columns: [{
+            header: 'MAC Address'.t(),
+            dataIndex: 'macAddress',
+            width: 150,
+            filter: { type: 'string' }
+        }, {
+            header: 'MAC Vendor'.t(),
+            dataIndex: 'macVendor',
+            width: 150,
+            filter: { type: 'string' },
+            editor: {
+                xtype: 'textfield',
+                emptyText: '[no MAC Vendor]'.t()
+            }
+        }, {
+            header: 'Interface'.t(),
+            dataIndex: 'interfaceId',
+            width: 120,
+            filter: { type: 'number' },
+            renderer: function (value) {
+                if (value === null || value < 0) {
+                    return '';
                 }
-                return;
+                var intf = Util.interfacesListNamesMap()[value];
+                if (!intf) {
+                    return Ext.String.format('Interface [{0}]'.t(), value);
+                }
+                return intf + ' [' + value + ']';
             }
-        },
+        }, {
+            header: 'Hostname'.t(),
+            dataIndex: 'hostname',
+            width: 120,
+            editor: {
+                xtype: 'textfield',
+                emptyText: '[no hostname]'.t()
+            }
+        }, {
+            header: 'Device Username'.t(),
+            dataIndex: 'username',
+            width: 150,
+            editor: {
+                xtype: 'textfield',
+                emptyText: '[no device username]'.t()
+            }
+        }, {
+            header: 'HTTP'.t() + ' - ' + 'User Agent'.t(),
+            dataIndex: 'httpUserAgent',
+            width: 200,
+            flex: 1,
+            editor: {
+                xtype: 'textfield',
+                emptyText: '[no HTTP user agent]'.t()
+            }
+        }, {
+            header: 'Last Seen Time'.t(),
+            dataIndex: 'lastSessionTime',
+            width: 150,
+            renderer: function (val, metaData, record) {
+                return val === 0 || val === '' ? '' : Util.timestampFormat(val);
+            },
+            filter: {
+                type: 'date'
+            }
+        }, {
+            header: 'Tags',
+            dataIndex: 'tagsString',
+            width: 150,
+            filter: {
+                type: 'string'
+            }
+        }],
+        editorFields: [{
+            xtype: 'textfield',
+            disabled: true,
+            bind: {
+                value: '{record.macAddress}',
+                disabled: '{record.internalId >= 0}'
+            },
+            fieldLabel: 'MAC Address'.t(),
+            emptyText: '[enter MAC address]'.t(),
+            allowBlank: false,
+            vtype: 'macAddress',
+            maskRe: /[a-fA-F0-9:]/
+        }, {
+            xtype: 'textfield',
+            bind: '{record.macVendor}',
+            fieldLabel: 'MAC Vendor'.t(),
+            emptyText: '[no MAC Vendor]'.t(),
+        }, {
+            xtype: 'textfield',
+            bind: '{record.hostname}',
+            fieldLabel: 'Hostname'.t(),
+            emptyText: '[no hostname]'.t(),
+        }, {
+            xtype: 'textfield',
+            bind: '{record.username}',
+            fieldLabel: 'Username'.t(),
+            emptyText: '[no username]'.t(),
+        }, {
+            xtype: 'textfield',
+            bind: '{record.httpUserAgent}',
+            fieldLabel: 'HTTP'.t() + ' - ' + 'User Agent'.t(),
+            emptyText: '[no HTTP user agent]'.t()
+        }],
+    },
+    // {
+    //     region: 'east',
+    //     xtype: 'propertygrid',
+    //     itemId: 'details',
+    //     editable: false,
+    //     width: 400,
+    //     split: true,
+    //     collapsible: false,
+    //     resizable: true,
+    //     shadow: false,
+    //     hidden: true,
 
-        nameColumnWidth: 200,
-        bind: {
-            // title: '{devicesgrid.selection.hostname} ({devicesgrid.selection.address})',
-            source: '{deviceDetails}',
-            hidden: '{!devicesgrid.selection}'
-        },
-        sourceConfig: {
-            username:            { displayName: 'Username'.t() },
-            hostname:            { displayName: 'Hostname'.t() },
-            hostnameLastKnown:   { displayName: 'HostnameLastKnown'.t() },
-            httpUserAgent:       { displayName: 'HTTP'.t() + ' - ' + 'User Agent'.t() },
-            lastSeenInterfaceId: { displayName: 'Interface'.t() },
-            lastSessionTime:     { displayName: 'Last Seen Time'.t(), renderer: 'timestampRenderer' },
-            macAddress:          { displayName: 'MAC Address'.t() },
-            macVendor:           { displayName: 'MAC Vendor'.t() },
-            tags:                { displayName: 'Tags'.t() },
-            tagsString:          { displayName: 'Tags String'.t() },
-        },
-        listeners: {
-            beforeedit: function () {
-                return false;
-            }
-        }
-    }],
+    //     cls: 'prop-grid',
+
+    //     viewConfig: {
+    //         stripeRows: false,
+    //         getRowClass: function(record) {
+    //             if (record.get('value') === null || record.get('value') === '') {
+    //                 return 'empty';
+    //             }
+    //             return;
+    //         }
+    //     },
+
+    //     nameColumnWidth: 200,
+    //     bind: {
+    //         // title: '{devicesgrid.selection.hostname} ({devicesgrid.selection.address})',
+    //         source: '{deviceDetails}',
+    //         hidden: '{!devicesgrid.selection}'
+    //     },
+    //     sourceConfig: {
+    //         username:            { displayName: 'Username'.t() },
+    //         hostname:            { displayName: 'Hostname'.t() },
+    //         hostnameLastKnown:   { displayName: 'HostnameLastKnown'.t() },
+    //         httpUserAgent:       { displayName: 'HTTP'.t() + ' - ' + 'User Agent'.t() },
+    //         lastSeenInterfaceId: { displayName: 'Interface'.t() },
+    //         lastSessionTime:     { displayName: 'Last Seen Time'.t(), renderer: 'timestampRenderer' },
+    //         macAddress:          { displayName: 'MAC Address'.t() },
+    //         macVendor:           { displayName: 'MAC Vendor'.t() },
+    //         tags:                { displayName: 'Tags'.t() },
+    //         tagsString:          { displayName: 'Tags String'.t() },
+    //     },
+    //     listeners: {
+    //         beforeedit: function () {
+    //             return false;
+    //         }
+    //     }
+    // }
+    ],
     tbar: [{
         xtype: 'button',
         text: 'Refresh'.t(),
@@ -130,12 +242,6 @@ Ext.define('Ung.view.extra.Devices', {
         bind: {
             disabled: '{autoRefresh}'
         }
-    }, {
-        xtype: 'button',
-        text: 'Auto Refresh'.t(),
-        iconCls: 'fa fa-refresh',
-        enableToggle: true,
-        toggleHandler: 'setAutoRefresh'
     }, '-', 'Filter:'.t(), {
         xtype: 'textfield',
         checkChangeBuffer: 200
@@ -145,5 +251,10 @@ Ext.define('Ung.view.extra.Devices', {
         iconCls: 'fa fa-line-chart',
         href: '#reports/devices',
         hrefTarget: '_self'
+    }],
+    bbar: ['->', {
+        text: 'Apply'.t(),
+        iconCls: 'fa fa-floppy-o',
+        handler: 'saveDevices'
     }]
 });
