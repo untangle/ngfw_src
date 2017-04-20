2ec6fcd7acd399c7c698cdabb54a63ed520d81b2
mahotz <mahotz@untangle.com>
2017-04-20 13:21:38 -0700

    Finally got the local directory password and expiration time stuff working. I had to add a whole new date and time picker class, and I had to implement all of the base64 password logic and functions that we had in the old code. Still a work in progress but basic functionality now works.

--- uvm/servlets/admin/app/util/Util.js
+++ uvm/servlets/admin/app/util/Util.js
@@ -52,7 +52,6 @@ Ext.define('Ung.util.Util', {
         'live-support': 'Live Support provides on-demand help for any technical issues.'.t()
     },
 
-
     iconReportTitle: function (report) {
         var icon;
         switch (report.get('type')) {
@@ -275,9 +274,6 @@ Ext.define('Ung.util.Util', {
         return map;
     },
 
-
-
-
     urlValidator: function (val) {
         var res = val.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
         return res ? true : 'Url missing or in wrong format!'.t();
@@ -464,6 +460,57 @@ Ext.define('Ung.util.Util', {
         '5': 'Thursday'.t(),
         '6': 'Friday'.t(),
         '7': 'Saturday'.t()
+    },
+
+    keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+    base64encode: function(input) {
+        if (typeof(base64encode) === 'function') {
+            return base64encode(input);
+        }
+        var output = "";
+        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+        var i = 0;
+        input = Util.utf8Encode(input);
+        while (i < input.length) {
+            chr1 = input.charCodeAt(i++);
+            chr2 = input.charCodeAt(i++);
+            chr3 = input.charCodeAt(i++);
+            enc1 = chr1 >> 2;
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            enc4 = chr3 & 63;
+            if (isNaN(chr2)) {
+                enc3 = enc4 = 64;
+            } else if (isNaN(chr3)) {
+                enc4 = 64;
+            }
+            output = output +
+            Util.keyStr.charAt(enc1) + Util.keyStr.charAt(enc2) +
+            Util.keyStr.charAt(enc3) + Util.keyStr.charAt(enc4);
+        }
+        return output;
+    },
+
+    utf8Encode : function (string) {
+        string = string.replace(/\r\n/g,"\n");
+        var utftext = "";
+        for (var n = 0; n < string.length; n++) {
+            var c = string.charCodeAt(n);
+            if (c < 128) {
+                utftext += String.fromCharCode(c);
+            }
+            else if((c > 127) && (c < 2048)) {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+            else {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+        }
+        return utftext;
     }
 
 });
