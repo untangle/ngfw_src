05d36101fad135a3fe43b8d28070e77a1773e3d0
Dirk Morris <dmorris@untangle.com>
2017-04-25 16:04:51 -0700

    NGFW-10606 - remove tools options entirely - it is now redundant with the options on toolbar

--- uvm/servlets/admin/app/view/config/Config.js
+++ uvm/servlets/admin/app/view/config/Config.js
@@ -53,21 +53,5 @@ Ext.define('Ung.view.config.Config', {
                 '</a>' +
             '</tpl>',
         itemSelector: 'a'
-    }, {
-        xtype: 'dataview',
-        store: {data: [
-                { name: 'Sessions'.t(), url: 'sessions', icon: 'icon_config_sessions.png' },
-                { name: 'Hosts'.t(), url: 'hosts', icon: 'icon_config_hosts.png' },
-                { name: 'Devices'.t(), url: 'devices', icon: 'icon_config_devices.png' },
-                { name: 'Users'.t(), url: 'users', icon: 'icon_config_users.png' }
-        ]},
-        tpl: '<p class="apps-title">' + 'Tools'.t() + '</p>' +
-             '<tpl for=".">' +
-                '<a href="#{url}" class="app-item">' +
-                '<img src="' + '/skins/modern-rack/images/admin/config/{icon}" width=80 height=80/>' +
-                '<span class="app-name">{name}</span>' +
-                '</a>' +
-            '</tpl>',
-        itemSelector: 'a'
     }]
 });
