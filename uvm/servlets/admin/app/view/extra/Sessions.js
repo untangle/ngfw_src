b86894cf44620732e1810f975c5740b137a7a262
Chris Blaise <cblaise@untangle.com>
2017-05-15 14:58:13 -0400

    NGFW-10682 Rename second 'Hostname' field to 'Platform Hostname' to match field.

--- uvm/servlets/admin/app/view/extra/Sessions.js
+++ uvm/servlets/admin/app/view/extra/Sessions.js
@@ -129,11 +129,11 @@ Ext.define('Ung.view.extra.Sessions', {
             width: TableConfig.booleanFieldWidth,
             hidden: true
         }, {
-            header: 'Hostname'.t(),
+            header: 'Platform Hostname'.t(),
             dataIndex: 'platform-hostname',
             hidden: true
         }, {
-            header: 'Username'.t(),
+            header: 'Platform Username'.t(),
             dataIndex: 'platform-username',
             hidden: false
         }, {
