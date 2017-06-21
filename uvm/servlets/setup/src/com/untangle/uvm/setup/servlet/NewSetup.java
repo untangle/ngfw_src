f8abd08dcb265b9942dc451b3d4614cc14d04c86
Dan Hadarau <dhadarau@untangle.com>
2017-06-21 12:28:21 +0300

    NGFW-10709 - new Setup Wizard with latest ExtJS, MVC, MVVM

---
+++ uvm/servlets/setup/src/com/untangle/uvm/setup/servlet/NewSetup.java
@@ -0,0 +1,41 @@
+/**
+ * $Id$
+ */
+package com.untangle.uvm.setup.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import com.untangle.uvm.UvmContext;
+import com.untangle.uvm.UvmContextFactory;
+
+/**
+ * A servlet which will display the start page
+ */
+@SuppressWarnings("serial")
+public class NewSetup extends HttpServlet
+{
+    private final Logger logger = Logger.getLogger(getClass());
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        UvmContext context = UvmContextFactory.context();
+        request.setAttribute( "buildStamp", getServletConfig().getInitParameter("buildStamp") );
+        request.setAttribute( "skinName", context.skinManager().getSettings().getSkinName());
+        request.setAttribute( "extjsTheme", context.skinManager().getSkinInfo().getExtjsTheme());
+
+        String url="/WEB-INF/jsp/newsetup.jsp";
+        ServletContext sc = getServletContext();
+        RequestDispatcher rd = sc.getRequestDispatcher(url);
+        rd.forward(request, response);
+    }
+}
