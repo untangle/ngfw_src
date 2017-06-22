a7cec7bee12a76fcc6e66cfd4656ee37ac41af35
Dan Hadarau <dhadarau@untangle.com>
2017-06-22 14:16:04 +0300

    NGFW-10836 - implement graph widget download

--- uvm/servlets/admin/app/widget/Report.js
+++ uvm/servlets/admin/app/widget/Report.js
@@ -35,7 +35,7 @@ Ext.define('Ung.widget.Report', {
             html: '<h1>{entry.localizedTitle}</h1><p>{entry.localizedDescription}</p>' +
                 '<div class="actions">' +
                     '<a class="action-btn"><i class="fa fa-cog fa-lg" data-action="settings"></i></a>' +
-                    '<a class="action-btn"><i class="fa fa-download fa-lg" data-action="download"></i></a>' +
+                    '<a class="action-btn" style="display: {entry.type === "TIME_GRAPH" || entry.type === "TIME_GRAPH_DYNAMIC" || entry.type === "PIE_GRAPH" ? "auto" : "none"};"><i class="fa fa-download fa-lg" data-action="download"></i></a>' +
                     // '<a class="action-btn"><i class="fa fa-area-chart fa-lg" data-action="style"></i></a>' +
                     '<a href="#reports/{entry.url}" class="action-btn"><i class="fa fa-external-link-square fa-lg"></i></a>' +
                     '<a class="action-btn"><i class="fa fa-rotate-left fa-lg" data-action="refresh"></i></a>' +
