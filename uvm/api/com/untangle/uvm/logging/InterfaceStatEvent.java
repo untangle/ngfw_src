e81994c062cf5d619a9e13f5abe64b5705059724
Dirk Morris <dmorris@untangle.com>
2017-06-26 20:34:04 -0700

    add bytes to interface stat events
    add byte counters to interface stat events (so we can report on bytes more easily) NGFW-10839
    other test fixes

--- uvm/api/com/untangle/uvm/logging/InterfaceStatEvent.java
+++ uvm/api/com/untangle/uvm/logging/InterfaceStatEvent.java
@@ -15,11 +15,16 @@ public class InterfaceStatEvent extends LogEvent
     private int interfaceId;
     private double rxRate;
     private double txRate;
+    private double rxBytes;
+    private double txBytes;
 
     public InterfaceStatEvent() { }
 
     public int getInterfaceId() { return interfaceId; }
     public void setInterfaceId(int newValue) { this.interfaceId = newValue; }
+
+    public double getRxBytes() { return rxBytes; }
+    public void setRxBytes(double newValue) { this.rxBytes = newValue; }
 
     public double getRxRate() { return rxRate; }
     public void setRxRate(double newValue) { this.rxRate = newValue; }
@@ -27,14 +32,17 @@ public class InterfaceStatEvent extends LogEvent
     public double getTxRate() { return txRate; }
     public void setTxRate(double newValue) { this.txRate = newValue; }
 
+    public double getTxBytes() { return txBytes; }
+    public void setTxBytes(double newValue) { this.txBytes = newValue; }
+
     @Override
     public void compileStatements( java.sql.Connection conn, java.util.Map<String,java.sql.PreparedStatement> statementCache ) throws Exception
     {
         String sql =
             "INSERT INTO " + schemaPrefix() + "interface_stat_events" + getPartitionTablePostfix() + " " +
-            "(time_stamp, interface_id, rx_rate, tx_rate) " +
+            "(time_stamp, interface_id, rx_rate, rx_bytes, tx_rate, tx_bytes) " +
             " values " +
-            "( ?, ?, ?, ? )";
+            "( ?, ?, ?, ?, ?, ? )";
 
         java.sql.PreparedStatement pstmt = getStatementFromCache( sql, statementCache, conn );        
 
@@ -42,7 +50,9 @@ public class InterfaceStatEvent extends LogEvent
         pstmt.setTimestamp(++i, getTimeStamp());
         pstmt.setLong(++i, interfaceId);
         pstmt.setDouble(++i, rxRate);
+        pstmt.setDouble(++i, rxBytes);
         pstmt.setDouble(++i, txRate);
+        pstmt.setDouble(++i, txBytes);
 
         pstmt.addBatch();
         return;
