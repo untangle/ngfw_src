ef892fc295f95026be1b189021cb75503820760c
Dirk Morris <dmorris@untangle.com>
2017-06-29 22:41:32 -0700

    fix compile @minor

--- uvm/api/com/untangle/uvm/UvmContext.java
+++ uvm/api/com/untangle/uvm/UvmContext.java
@@ -168,7 +168,7 @@ public interface UvmContext
      *
      * @return the SessionMonitor
      */
-    SessionMonitor sessionMonitor();b
+    SessionMonitor sessionMonitor();
 
     /**
      * Get the <code>OemManager</code> singleton.
