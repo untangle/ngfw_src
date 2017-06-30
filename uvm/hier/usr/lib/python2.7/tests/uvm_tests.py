e20d80ea8ad77fafc21fa5fa1148c95fe02a80e4
John Coffin <jcoffin@gmail.com>
2017-06-29 20:24:13 -0700

    Add the ability to check sub-tab help reference.

--- uvm/hier/usr/lib/python2.7/tests/uvm_tests.py
+++ uvm/hier/usr/lib/python2.7/tests/uvm_tests.py
@@ -133,7 +133,8 @@ class UvmTests(unittest2.TestCase):
         
         # Check all the links in JSON
         testResults = True
-        for link in helpLinks["links"]: # 
+        for link in helpLinks["links"]:
+            subLinks = [""]
             hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
@@ -147,23 +148,30 @@ class UvmTests(unittest2.TestCase):
 
             pat = re.compile(r'''.*URL=http://wiki.*.untangle.com/(.*)">.*$''')
             version = uvmContext.getFullVersion()
-            url = "http://wiki.untangle.com/get.php?fragment=" + link['fragment'] + "&uid=0000-0000-0000-0000&version=" + version + "&webui=true&lang=en"
-            print "Checking %s = %s " % (link['fragment'], url)
-            req = urllib2.Request( url, headers=hdr) 
-            ret = urllib2.urlopen( req, context=ctx )
-            time.sleep(.1) # dont flood wiki
-            assert(ret)
-            result = ret.read()
-            assert(result)
-            patmatch = pat.match( result )
-            assert(patmatch)
-            if (patmatch.group(1)):
-                print "Result: \"%s\"" % patmatch.group(1)
-            else:
-                print "Failed to get result for %s.  Expecting: %s" % (link['fragment'],link['title'])
-                # check all help links before failing the test
-                testResults = False
-
+            if ('subcat' in link):
+                subLinks.extend(link['subcat'])
+            for subLink in subLinks:
+                if (subLink != ""):
+                    subLink = link['fragment'] + "/" + subLink
+                else:
+                    subLink = link['fragment']
+                url = "http://wiki.untangle.com/get.php?fragment=" + subLink + "&uid=0000-0000-0000-0000&version=" + version + "&webui=true&lang=en"
+                print "Checking %s = %s " % (subLink, url)
+                req = urllib2.Request( url, headers=hdr) 
+                ret = urllib2.urlopen( req, context=ctx )
+                time.sleep(.1) # dont flood wiki
+                assert(ret)
+                result = ret.read()
+                assert(result)
+                patmatch = pat.match( result )
+                assert(patmatch)
+                if (patmatch.group(1)):
+                    print "Result: \"%s\"" % patmatch.group(1)
+                else:
+                    print "Failed to get result for %s.  Expecting: %s" % (subLink,link['title'])
+                    # check all help links before failing the test
+                    testResults = False
+                    
         assert(testResults)
 
     def test_020_about_info(self):
