2d46d885ad4f644d7bd6b6eab66fb1c6b8efade7
Dirk Morris <dmorris@untangle.com>
2017-06-29 19:21:22 -0700

    add new report to show data usage by interface NGFW-10839

---
+++ uvm/hier/usr/share/untangle/lib/untangle-vm/reports/network-data.js
@@ -0,0 +1,19 @@
+{
+    "uniqueId": "network-aGUe5wYZ1x",
+    "category": "Network",
+    "description": "The data usage by interface.",
+    "displayOrder": 2,
+    "enabled": true,
+    "javaClass": "com.untangle.app.reports.ReportEntry",
+    "orderByColumn": "value",
+    "orderDesc": true,
+    "units": "bytes",
+    "pieGroupColumn": "interface_id",
+    "pieSumColumn": "sum(rx_bytes+tx_bytes)",
+    "readOnly": true,
+    "table": "interface_stat_events",
+    "seriesRenderer": "interface",
+    "title": "Data Usage (by interface)",
+    "pieStyle": "COLUMN",
+    "type": "PIE_GRAPH"
+}
