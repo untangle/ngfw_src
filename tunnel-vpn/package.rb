3db23c0c0686e01d8a8ebe11456a155eb7be1164
Dirk Morris <dmorris@untangle.com>
2017-07-07 10:21:34 -0700

    Change tunnel VPN to use standard upload servlet
    Use standard upload servlet instead of custom

--- tunnel-vpn/package.rb
+++ tunnel-vpn/package.rb
@@ -4,4 +4,3 @@ AppBuilder.makeApp(BuildEnv::SRC, 'untangle-app-tunnel-vpn', 'tunnel-vpn')
 
 tunnel_vpn = BuildEnv::SRC['untangle-app-tunnel-vpn']
 
-ServletBuilder.new(tunnel_vpn, 'com.untangle.app.tunnel_vpn.servlet','tunnel-vpn/servlets/tunnel-vpn', [], [tunnel_vpn['src']])
