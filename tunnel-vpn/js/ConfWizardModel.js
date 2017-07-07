debdc0fc32f9b8ad24b39a6d173f6fbfc99209c0
Dirk Morris <dmorris@untangle.com>
2017-07-06 22:39:41 -0700

    Work in progress on tunnel VPN wizard
    Add basic wizard for Tunnel VPN NGFW-10800
    @exclude

---
+++ tunnel-vpn/js/ConfWizardModel.js
@@ -0,0 +1,55 @@
+Ext.define('Ung.apps.bandwidthcontrol.ConfWizardModel', {
+    extend: 'Ext.app.ViewModel',
+    alias: 'viewmodel.app-tunnel-vpn-wizard',
+
+    formulas: {
+        selectedProvider: {
+            get: function () {
+                return;
+            },
+            set: function (val) {
+                var details = this.get('providerConfigs')[val];
+
+                this.set('providerName', details.providerName);
+                this.set('providerTitle', details.providerTitle);
+                this.set('providerInstructions', details.providerInstructions);
+            }
+        }
+    },
+    
+    data: {
+        selectedConf: null,
+
+        providerConfigs: {
+            Untangle: {
+                providerName: 'Untangle'.t(),
+                providerTitle: 'Upload the Untangle OpenVPN config zip',
+                providerInstructions: 'FIXME',
+            },
+            NordVPN: {
+                providerName: 'NordVPN'.t(),
+                providerTitle: 'Upload the NordVPN OpenVPN config zip'.t(),
+                providerInstructions: '<li>' + 'Log in to "My account" at nordvpn.com'.t() + '<br/>' +
+                    '<li>' + 'Click on the "Download area"'.t() + '<br/>' +
+                    '<li>' + 'Download the Linux ".OVPN configuration files" zip'.t() + '<br/>' +
+                    '<li>' + 'Upload the downloaded zip file below'.t() + '<br/>'
+            },
+            ExpressVPN: {
+                providerName: 'ExpressVPN'.t(),
+                providerTitle: 'Upload the ExpressVPN OpenVPN config zip'.t(),
+                providerInstructions: 'FIXME',
+            },
+            Custom: {
+                providerName: 'Custom'.t(),
+                providerTitle: 'Upload the Custom OpenVPN config zip'.t(),
+                providerInstructions: '<li>' + 'Upload the Custom OpenVPN Config File'.t() + '<br/>'
+            },
+        },
+
+        trafficConfig: {
+            allTraffic: false,
+        }
+    },
+
+    
+});
--- tunnel-vpn/src/com/untangle/app/tunnel_vpn/TunnelVpnTunnelSettings.java
+++ tunnel-vpn/src/com/untangle/app/tunnel_vpn/TunnelVpnTunnelSettings.java
@@ -4,12 +4,20 @@
 package com.untangle.app.tunnel_vpn;
 
 import java.net.InetAddress;
-    
+import java.util.List;
+import java.util.LinkedList;
+
+import com.untangle.uvm.app.IPMatcher;
+
 @SuppressWarnings("serial")
 public class TunnelVpnTunnelSettings implements java.io.Serializable
 {
     private boolean enabled = true;
     private String name;
+
+    private boolean allTraffic = false;
+    private List<String> tags = new LinkedList<String>();
+    private List<IPMatcher> hosts = new LinkedList<IPMatcher>();
     
     public TunnelVpnTunnelSettings() {}
 
@@ -18,4 +26,13 @@ public class TunnelVpnTunnelSettings implements java.io.Serializable
 
     public String getName() { return name; }
     public void setName(String name) { this.name = ( name == null ? null : name.replaceAll("\\s","") ); }
+
+    public boolean getAllTraffic() { return allTraffic; }
+    public void setAllTraffic(boolean newValue) { allTraffic = newValue; }
+
+    public List<String> getTags() { return tags; }
+    public void setTags(List<String> newValue) { tags = newValue; }
+
+    public List<IPMatcher> getHosts() { return hosts; }
+    public void setHosts(List<IPMatcher> newValue) { hosts = newValue; }
 }
