65fe9888b88afce3835d26a67096b4eb0c5ebdee
Chris Blaise <cblaise@untangle.com>
2017-08-01 10:30:07 -0400

    NGFW-10800 Convert sub-tab panel into single grid and override grid's add action to use wizard

--- tunnel-vpn/js/view/Tunnels.js
+++ tunnel-vpn/js/view/Tunnels.js
@@ -1,63 +1,29 @@
 Ext.define('Ung.apps.tunnel-vpn.view.Tunnels', {
-    extend: 'Ext.panel.Panel',
+    extend: 'Ung.cmp.Grid',
     alias: 'widget.app-tunnel-vpn-tunnels',
     itemId: 'tunnels',
     title: 'Tunnels'.t(),
     viewModel: true,
-    autoScroll: true,
 
-    tbar: [{
-        xtype: 'tbtext',
-        padding: '8 5',
-        style: { fontSize: '12px', fontWeight: 600 },
-        html: 'The Tunnels tab is used to configure Tunnel VPN tunnels (connections) to remote VPN services'.t()
-    }],
+    controller: 'tunnelgrid',
 
-    defaults: {
-        labelWidth: 180,
-        padding: '10 0 0 10'
-    },
-
-    items: [{
-        xtype: 'app-tunnel-vpn-tunnel-tab-panel',
-        padding: '20 20 0 20',
+    dockedItems: [{
+        xtype: 'toolbar',
+        dock: 'top',
+        items: [{
+            xtype: 'tbtext',
+            padding: '8 5',
+            style: { fontSize: '12px', fontWeight: 600 },
+            html: 'Configure Tunnel VPN tunnels (connections) to remote VPN services'.t()
+        }]
     }, {
-        xtype: 'button',
-        anchor: '100%',
-        text: 'Configured New Tunnel VPN Connection'.t(),
-        iconCls: 'fa fa-magic',
-        handler: 'runWizard'
-    }]
-});
-
-Ext.define('Ung.apps.tunnel-vpn.view.ClientTabs', {
-    extend: 'Ext.tab.Panel',
-    alias: 'widget.app-tunnel-vpn-tunnel-tab-panel',
-    itemId: 'tunnel-vpn-tunnel-tab-panel',
-    viewModel: true,
-    layout: 'fit',
-
-    items: [{
-        title: 'Tunnels'.t(),
-        items: [
-            { xtype: 'app-tunnel-vpn-tunnels-grid' }
-            ]
-    }]
-
-});
-
-Ext.define('Ung.apps.tunnel-vpn.view.TunnelsGrid', {
-    extend: 'Ung.cmp.Grid',
-    alias: 'widget.app-tunnel-vpn-tunnels-grid',
-    itemId: 'tunnel-vpn-tunnels-grid',
+        xtype: 'toolbar',
+        dock: 'top',
+        items: ['@add']
+    }],
 
     recordActions: ['delete'],
     listProperty: 'settings.tunnels.list',
-    emptyRow: {
-        javaClass: 'com.untangle.app.tunnel_vpn.TunnelVpnTunnelSettings',
-        'enabled': true,
-        'name': '',
-    },
 
     bind: '{tunnels}',
 
@@ -93,4 +59,5 @@ Ext.define('Ung.apps.tunnel-vpn.view.TunnelsGrid', {
             bind: '{record.password}'
         }
     }],
+
 });
