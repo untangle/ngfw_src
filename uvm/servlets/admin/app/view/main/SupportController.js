92e45d8e290f3c3c23095afbce3ad333ffe6bd1d
Chris Blaise <cblaise@untangle.com>
2017-08-14 10:06:05 -0400

    NGFW-10241 Pass email address and product line

--- uvm/servlets/admin/app/view/main/SupportController.js
+++ uvm/servlets/admin/app/view/main/SupportController.js
@@ -14,11 +14,13 @@ Ext.define('Ung.view.main.SupportController', {
             vm = me.getViewModel();
 
         Ext.Deferred.sequence([
-            Rpc.directPromise('rpc.systemManager.getSettings'),
+            Rpc.asyncPromise('rpc.systemManager.getSettings'),
+            Rpc.asyncPromise('rpc.adminManager.getSettings'),
         ], this).then(function (result) {
             me.getView().setLoading(false);
             vm.set({
-                systemSettings: result[0]
+                systemSettings: result[0],
+                adminSettings: result[1]
             });
 
             if(vm.get('systemSettings')['supportEnabled'] == false){
@@ -31,11 +33,17 @@ Ext.define('Ung.view.main.SupportController', {
 
     openSupportWindow: function(){
         var me = this,
-            view = this.getView();
+            view = this.getView(),
+            vm = this.getViewModel();
 
         var query = 'action=support';
         query += '&' + Util.getAbout();
         query += '&fragment=' + view.fragment.substr(1);
+        var user = vm.get('adminSettings').users.list[0];
+        if(user){
+            query += '&email=' + user.emailAddress;
+        }
+        query += '&line=ngfw';
 
         window.location.hash = view.fragment;
         window.open(Util.getStoreUrl() + '?' + query);
