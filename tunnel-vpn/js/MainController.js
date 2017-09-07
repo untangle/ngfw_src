e0b6685c2bc39f2177abc33331f6c6b4636cc0af
Dirk Morris <dmorris@untangle.com>
2017-09-07 16:49:41 -0700

    Add more instructions @minor

--- tunnel-vpn/js/MainController.js
+++ tunnel-vpn/js/MainController.js
@@ -86,7 +86,8 @@ Ext.define('Ung.apps.tunnel-vpn.MainController', {
                 providerTitle: 'Upload the PrivateInternetAccess OpenVPN config ovpn file'.t(),
                 providerInstructions: '<li>' + 'Download the appropriate ovpn file here from:'.t() + '<br/>' +
                     '<li>' + '<a href="https://www.privateinternetaccess.com/pages/openvpn-ios" target="_blank">https://www.privateinternetaccess.com/pages/openvpn-ios</a>' + '<br/>' +
-                    '<li>' + 'Upload the .ovpn file'.t() + '<br/>'
+                    '<li>' + 'Upload the .ovpn file'.t() + '<br/>' +
+                    '<li>' + 'Provide the username/password of your account'.t() + '<br/>'
             },
             CustomZip: {
                 description: 'Custom zip file'.t(),
