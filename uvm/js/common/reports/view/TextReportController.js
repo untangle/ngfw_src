3eba8a5a7e9b981363cc2ea7140799ef21ad6bbb
Dan Hadarau <dhadarau@untangle.com>
2017-09-21 15:18:59 +0300

    NGFW-11106 - fix reports display range issue caused by some timezone problem

--- uvm/js/common/reports/view/TextReportController.js
+++ uvm/js/common/reports/view/TextReportController.js
@@ -35,15 +35,14 @@ Ext.define('Ung.view.reports.TextReportController', {
         if (reps) { reps.getViewModel().set('fetching', true); }
 
         if (!me.getView().renderInReports) { // if not rendered in reports than treat as widget
-            vm.set('startDate', new Date(rpc.systemManager.getMilliseconds() - (vm.get('widget.timeframe') || 3600 * 24) * 1000));
-            vm.set('endDate', new Date(rpc.systemManager.getMilliseconds()));
+            vm.set('startDate', new Date(rpc.systemManager.getMilliseconds() - (vm.get('widget.timeframe') || 3600) * 1000 + (new Date().getTimezoneOffset() * 60000) + rpc.timeZoneOffset));
         }
 
         me.getView().setLoading(true);
         Rpc.asyncData('rpc.reportsManager.getDataForReportEntry',
             vm.get('entry').getData(), // entry
-            vm.get('startDate'), // start date
-            vm.get('tillNow') ? null : vm.get('endDate'), // end date
+            Ext.Date.add(vm.get('startDate'), Ext.Date.MINUTE, -(new Date().getTimezoneOffset() + rpc.timeZoneOffset/60000)), // start date
+            (vm.get('tillNow') || !me.getView().renderInReports) ? null : Ext.Date.add(vm.get('endDate'), Ext.Date.MINUTE, -(new Date().getTimezoneOffset() + rpc.timeZoneOffset/60000)), // end date
             vm.get('sqlFilterData'), -1) // sql filters
             .then(function(result) {
                 me.getView().setLoading(false);
