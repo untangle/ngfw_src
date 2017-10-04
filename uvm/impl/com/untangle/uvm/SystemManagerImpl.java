01bbf330f1cb8bba766ed6bbb283bd01d3aa92fc
Dirk Morris <dmorris@untangle.com>
2017-10-04 12:38:17 -0700

    remove old code

--- uvm/impl/com/untangle/uvm/SystemManagerImpl.java
+++ uvm/impl/com/untangle/uvm/SystemManagerImpl.java
@@ -371,28 +371,6 @@ public class SystemManagerImpl implements SystemManager
         return(offset);
     }
 
-    // TODO - this should be removed once we're sure the code above is good
-    public Integer OLD_getTimeZoneOffset()
-    {
-        try {
-            String tzoffsetStr = UvmContextImpl.context().execManager().execOutput("date +%:z");
-            if (tzoffsetStr == null) {
-                return 0;
-            } else {
-                String[] tzParts = tzoffsetStr.replaceAll("(\\r|\\n)", "").split(":");
-                if (tzParts.length==2) {
-                    Integer hours= Integer.valueOf(tzParts[0]);
-                    Integer tzoffset = Math.abs(hours)*3600000+Integer.valueOf(tzParts[1])*60000;
-                    return hours >= 0 ? tzoffset : -tzoffset;
-                }
-            }
-        } catch (Exception e) {
-            logger.warn("Unable to fetch version",e);
-        }
-
-        return 0;
-    }
-
     public boolean downloadUpgrades()
     {
         LinkedList<String> downloadUrls = new LinkedList<String>();
