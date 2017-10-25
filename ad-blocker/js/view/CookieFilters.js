a7844de68a1464d423adee2a44c0c889cb5965d8
Chris Blaise <cblaise@untangle.com>
2017-10-25 13:08:56 -0400

    NGFW-10866 Add empty Text

--- ad-blocker/js/view/CookieFilters.js
+++ ad-blocker/js/view/CookieFilters.js
@@ -44,6 +44,11 @@ Ext.define('Ung.apps.ad-blocker.view.CookieFilters', {
             javaClass: 'com.untangle.uvm.app.GenericRule'
         },
 
+        viewConfig: {
+            emptyText: '<p style="text-align: center; margin: 0; line-height: 2;"><i class="fa fa-info-circle fa-2x"></i> <br/>' + 'No User Defined Cookie Filters Defined'.t() + '</p>'
+        },
+
+
         columns: [
             Column.enabled, {
                 header: 'Rule'.t(),
