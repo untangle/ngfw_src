f03b7d3bc93ec5a87b90c0673151a375c372cb9b
Dirk Morris <dmorris@untangle.com>
2017-10-26 12:02:15 -0700

    remove obsolete conversion code

--- libnetcap/src/netcap_udp.c
+++ libnetcap/src/netcap_udp.c
@@ -264,8 +264,8 @@ static int _netcap_udp_sendto (int sock, void* data, size_t data_len, int flags,
     cmsg->cmsg_type  = IP_TOS;
     if ( IS_NEW_KERNEL() >= 316 ) {
         tos_len   = CMSG_LEN(sizeof(int));
-        cmsg->cmsg_len   = tos_len;
         tos_val   = pkt->tos;
+        cmsg->cmsg_len   = tos_len;
         memcpy( CMSG_DATA(cmsg), &tos_val, tos_len );
     }
     else {
