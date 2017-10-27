3d49a01f906fcd71092910a71a8caf46b828adcd
John Coffin <jcoffin@gmail.com>
2017-10-27 08:52:11 -0700

    Add  license test

--- phish-blocker/hier/usr/lib/python2.7/tests/phish_blocker_tests.py
+++ phish-blocker/hier/usr/lib/python2.7/tests/phish_blocker_tests.py
@@ -102,6 +102,9 @@ class PhishBlockerTests(unittest2.TestCase):
         result = remote_control.is_online()
         assert (result == 0)
 
+    def test_011_license_valid(self):
+        assert(uvmContext.licenseManager().isLicenseValid(self.appName()))
+
     def test_020_smtpQuarantinedPhishBlockerTest(self):
         if (not canRelay):
             raise unittest2.SkipTest('Unable to relay through' + smtpServerHost)
