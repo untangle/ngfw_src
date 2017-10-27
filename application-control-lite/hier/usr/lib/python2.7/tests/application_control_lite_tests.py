c4aa3f9061e08aedca890d56b6d5ea1cc5beb1a0
John Coffin <jcoffin@gmail.com>
2017-10-27 08:04:40 -0700

    Add license test

--- application-control-lite/hier/usr/lib/python2.7/tests/application_control_lite_tests.py
+++ application-control-lite/hier/usr/lib/python2.7/tests/application_control_lite_tests.py
@@ -65,6 +65,9 @@ class ApplicationControlLiteTests(unittest2.TestCase):
         result = remote_control.is_online()
         assert (result == 0)
 
+    def test_011_license_valid(self):
+        assert(uvmContext.licenseManager().isLicenseValid(self.appName()))
+  
     def test_020_testHttpPatternLog(self):
         nukepatterns()
         
