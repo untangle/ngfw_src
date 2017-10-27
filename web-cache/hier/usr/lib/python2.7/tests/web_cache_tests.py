43dcdf9a2d6bf4cd8a0f4d38e7b73d2216636606
John Coffin <jcoffin@gmail.com>
2017-10-27 10:05:37 -0700

    Add license test

--- web-cache/hier/usr/lib/python2.7/tests/web_cache_tests.py
+++ web-cache/hier/usr/lib/python2.7/tests/web_cache_tests.py
@@ -37,6 +37,9 @@ class WebCacheTests(unittest2.TestCase):
         result = remote_control.is_online()
         assert (result == 0)
 
+    def test_011_license_valid(self):
+        assert(uvmContext.licenseManager().isLicenseValid(self.appName()))
+
     def test_020_testBasicWebCache(self):
         if remote_control.quickTestsOnly:
             raise unittest2.SkipTest('Skipping a time consuming test')
