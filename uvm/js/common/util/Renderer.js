1b688f46aec61f8a6e7633c27f555b11a79a5102
Chris Blaise <cblaise@untangle.com>
2017-11-16 13:46:50 -0500

    NGFW-10866 If timestamp value is less than epoch, its in non-ms format so muliply by 1000

--- uvm/js/common/util/Renderer.js
+++ uvm/js/common/util/Renderer.js
@@ -92,6 +92,7 @@ Ext.define('Ung.util.Renderer', {
     timestamp: function (value) {
         if (!value) { return ''; }
         if ((typeof(value) === 'object') && value.time) { value = value.time; }
+        if(value < 2696400000){ value *= 1000; }
         var date = new Date(value);
         date.setTime(value + Renderer.timestampOffset);
         return Ext.util.Format.date(date, 'timestamp_fmt'.t());
