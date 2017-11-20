f1b3cfc965b9c8dae41f2036602d2bb12f5e78f9
Chris Blaise <cblaise@untangle.com>
2017-11-20 15:03:22 -0500

    NGFW-10866 Update grids for system mail

--- uvm/servlets/admin/config/email/view/Quarantine.js
+++ uvm/servlets/admin/config/email/view/Quarantine.js
@@ -24,7 +24,7 @@ Ext.define('Ung.config.email.view.Quarantine', {
                 }
             },
             quarantineTotalDisk: function (get) {
-                return Ext.String.format('Total Disk Space Used: {0} MB'.t(), get('inboxesTotalSize')/(1024 * 1024));
+                return Ext.String.format('Total Disk Space Used: {0}'.t(), Renderer.datasize(get('inboxesTotalSize')));
             }
         },
         stores: {
@@ -68,21 +68,24 @@ Ext.define('Ung.config.email.view.Quarantine', {
                 allowBlank: false,
                 editable: false,
                 increment: 30,
-                bind: '{digestHour}'
+                disabled: true,
+                bind: {
+                    value: '{digestHour}',
+                    disabled: '{smtpSettings.quarantineSettings.sendDailyDigests == false}'
+                }
             }, {
                 xtype: 'component',
                 margin: '10 0 0 0',
                 html: Ext.String.format('Users can also request Quarantine Digest Emails manually at this link: <b>https://{0}/quarantine/</b>'.t(), rpc.networkManager.getPublicUrl())
             }]
         }, {
-            xtype: 'grid',
+            xtype: 'ungrid',
             reference: 'inboxesGrid',
             title: 'User Quarantines'.t(),
             flex: 1,
 
-            // viewConfig: {
-            //     emptyText: '<p style="text-align: center; margin: 0; line-height: 2;"><i class="fa fa-info-circle fa-lg"></i> No Data!</p>',
-            // },
+            emptyText: 'No User Quarantines defined'.t(),
+
             selModel: {
                 selType: 'checkboxmodel'
             },
@@ -92,7 +95,8 @@ Ext.define('Ung.config.email.view.Quarantine', {
             tbar: [{
                 text: 'Purge Selected'.t(),
                 iconCls: 'fa fa-circle fa-red',
-                handler: 'purgeInboxes',
+                handler: 'externalAction',
+                action: 'purgeInboxes',
                 disabled: true,
                 bind: {
                     disabled: '{!inboxesGrid.selection}'
@@ -100,7 +104,8 @@ Ext.define('Ung.config.email.view.Quarantine', {
             }, {
                 text: 'Release Selected'.t(),
                 iconCls: 'fa fa-circle fa-green',
-                handler: 'releaseInboxes',
+                handler: 'externalAction',
+                action: 'releaseInboxes',
                 disabled: true,
                 bind: {
                     disabled: '{!inboxesGrid.selection}'
@@ -114,25 +119,28 @@ Ext.define('Ung.config.email.view.Quarantine', {
             columns: [{
                 header: 'Account Address'.t(),
                 dataIndex: 'address',
+                width: Renderer.emailWidth,
                 flex: 1
             }, {
                 header: 'Message Count'.t(),
-                width: 185,
+                width: Renderer.messageWidth,
                 align: 'right',
                 dataIndex: 'totalMails'
             }, {
-                header: 'Data Size (kB)'.t(),
-                width: 185,
+                header: 'Data Size'.t(),
+                width: Renderer.sizeWidth,
                 align: 'right',
-                dataIndex: 'totalSz'
+                renderer: Renderer.datasize,
+                dataIndex: 'totalSz',
             }, {
                 xtype: 'actioncolumn',
-                width: 80,
+                width: Renderer.actionWidth,
                 menuDisabled: true,
                 align: 'center',
-                header: 'Show Detail'.t(),
+                header: 'Detail'.t(),
                 iconCls: 'fa fa-envelope',
-                handler: 'showQuarantineDetails'
+                handler: 'externalAction',
+                action: 'showQuarantineDetails'
             }]
         }]
     }, {
@@ -161,6 +169,7 @@ Ext.define('Ung.config.email.view.Quarantine', {
             items: [{
                 xtype: 'ungrid',
                 border: false,
+                emptyText: 'No Quarantinable Addresses defined'.t(),
 
                 bind: '{qAddresses}',
 
@@ -175,15 +184,13 @@ Ext.define('Ung.config.email.view.Quarantine', {
                 columns: [{
                     header: 'Quarantinable Address'.t(),
                     flex: 1,
+                    width: Renderer.emailWidth,
                     dataIndex: 'address',
-                    renderer: function (value) {
-                        return value || '<em>click to edit</em>';
-                    },
                     editor: {
                         xtype: 'textfield',
                         emptyText: '[enter email address rule]'.t(),
                         allowBlank: false,
-                        vtype: 'email'
+                        vtype: 'emailwildcard'
                     }
                 }],
             }]
@@ -209,6 +216,7 @@ Ext.define('Ung.config.email.view.Quarantine', {
                 xtype: 'ungrid',
                 border: false,
 
+                emptyText: 'No Quarantine Forwards defined'.t(),
                 bind: '{qForwards}',
 
                 tbar: ['@addInline', '->', '@import', '@export'],
@@ -224,10 +232,7 @@ Ext.define('Ung.config.email.view.Quarantine', {
                 columns: [{
                     header: 'Distribution List Address'.t(),
                     dataIndex: 'address1',
-                    width: 200,
-                    renderer: function (value) {
-                        return value || '<em>click to edit</em>';
-                    },
+                    width: Renderer.emailWidth,
                     editor: {
                         xtype: 'textfield',
                         emptyText: 'distributionlistrecipient@example.com'.t(),
@@ -237,10 +242,8 @@ Ext.define('Ung.config.email.view.Quarantine', {
                 }, {
                     header: 'Send to Address'.t(),
                     dataIndex: 'address2',
+                    width: Renderer.emailWidth,
                     flex: 1,
-                    renderer: function (value) {
-                        return value || '<em>click to edit</em>';
-                    },
                     editor: {
                         xtype: 'textfield',
                         emptyText: 'quarantinelistowner@example.com'.t(),
