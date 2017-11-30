fa0b9f552b46f9e48d9e0d01d02199a15aef116f
Dirk Morris <dmorris@untangle.com>
2017-11-30 09:03:50 -0800

    Revert "Remove inheritance of tags device entry to host entry"
    This reverts commit 96b119c47957e66d08ad4bf149cfab911f3feb03.

--- uvm/impl/com/untangle/uvm/HostTableImpl.java
+++ uvm/impl/com/untangle/uvm/HostTableImpl.java
@@ -455,6 +455,7 @@ public class HostTableImpl implements HostTable
             entry.setHttpUserAgent( deviceEntry.getHttpUserAgent() );
         if ( deviceEntry.getMacVendor() != null )
             entry.setMacVendor( deviceEntry.getMacVendor() );
+        entry.addTags( deviceEntry.getTags() );
 
         /**
          * Update Device Entry
