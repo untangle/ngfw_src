85d474c4f4548f4a690d6efd3c5e32d64b0e731f
Dirk Morris <dmorris@untangle.com>
2017-12-19 09:24:39 -0800

    Add conversion to add new dashboard widgets
    NGFW-11299 #code-completed

--- uvm/impl/com/untangle/uvm/DashboardManagerImpl.java
+++ uvm/impl/com/untangle/uvm/DashboardManagerImpl.java
@@ -44,59 +44,23 @@ public class DashboardManagerImpl implements DashboardManager
         }
 
         /**
-         * 12.2 Conversion
+         * 13.2 Conversion
          */
-        if ( settings.getVersion() == null || settings.getVersion() < 2 ) {
-            logger.info("Migrating dashboard settings to v2...");
+        if ( settings.getVersion() == null || settings.getVersion() < 3 ) {
+            logger.info("Migrating dashboard settings to v3...");
 
-            settings.setVersion(2);
+            settings.setVersion(3);
 
-            List<DashboardWidgetSettings> widgets = settings.getWidgets();
-            for ( ListIterator<DashboardWidgetSettings> iterator = widgets.listIterator() ; iterator.hasNext() ; ) {
-                DashboardWidgetSettings widget = iterator.next();
-
-                // change Events widgets to Report widgets
-                if ( "EventEntry".equals(widget.getType()) )
-                    widget.setType("ReportEntry");
-
-                // Hardware widget is gone, replace with Resources
-                if ( "Hardware".equals(widget.getType()) ) {
-                    iterator.remove();
-                    iterator.add( new DashboardWidgetSettings("Resources") );
-                }
-                // Network widget is gone,
-                if ( "Network".equals(widget.getType()) ) {
-                    iterator.remove();
-                    iterator.add( new DashboardWidgetSettings("NetworkInformation") );
-                    iterator.add( new DashboardWidgetSettings("NetworkLayout") );
-                    iterator.add( new DashboardWidgetSettings("MapDistribution") );
-                }
-                if ( "HostsDevices".equals(widget.getType()) )
-                    iterator.remove();
-                if ( "Sessions".equals(widget.getType()) )
-                    iterator.remove();
-                if ( "Memory".equals(widget.getType()) )
-                    iterator.remove();
-                if ( "Server".equals(widget.getType()) )
-                    iterator.remove();
+            try {
+                List<DashboardWidgetSettings> widgets = settings.getWidgets();
+                widgets.add( 5, new DashboardWidgetSettings("PolicyOverview") );
+                widgets.add( new DashboardWidgetSettings("Notifications") );
+            } catch (Exception e) {
+                logger.warn("Exception converting to v3",e);
             }
 
-            DashboardWidgetSettings widgetSettings;
-
-            widgetSettings = new DashboardWidgetSettings("ReportEntry");
-            widgetSettings.setRefreshIntervalSec(60);
-            widgetSettings.setTimeframe(3600);
-            widgetSettings.setEntryId("bandwidth-control-BVOy539ahO"); // Bandwidth Control - Top Hostnames Usage
-            settings.getWidgets().add( widgetSettings);
-
-            widgetSettings = new DashboardWidgetSettings("ReportEntry");
-            widgetSettings.setRefreshIntervalSec(60);
-            widgetSettings.setTimeframe(3600);
-            widgetSettings.setEntryId("application-control-OAI5zmhxOM"); // Application Control - Top Applications Usage
-            settings.getWidgets().add( widgetSettings);
-
             this.setSettings( settings );
-            logger.info("Migrating dashboard settings to v2... done.");
+            logger.info("Migrating dashboard settings to v3... done.");
         }
 
         logger.info("Initialized DashboardManager");
@@ -203,7 +167,7 @@ public class DashboardManagerImpl implements DashboardManager
         widgets.add( new DashboardWidgetSettings("Notifications"));
 
         DashboardSettings newSettings = new DashboardSettings();
-        newSettings.setVersion(2);
+        newSettings.setVersion(3);
         newSettings.setWidgets(widgets);
         return newSettings;
     }
