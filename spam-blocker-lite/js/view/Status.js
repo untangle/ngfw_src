f2211184d218653395de8df2b4ddf773e35ce478
Chris Blaise <cblaise@untangle.com>
2017-12-21 13:33:10 -0500

    NGFW-10869 On app status pages with a sessions/metric pane, set that pane to be 1/4 of the view area.  Still looks good on desktops, looks a million times better on mobile

--- spam-blocker-lite/js/view/Status.js
+++ spam-blocker-lite/js/view/Status.js
@@ -27,10 +27,8 @@ Ext.define('Ung.apps.spamblockerlite.view.Status', {
     }, {
         region: 'west',
         border: false,
-        width: 350,
-        minWidth: 300,
+        width: Math.ceil(Ext.getBody().getViewSize().width / 4),
         split: true,
-        layout: 'border',
         items: [{
             xtype: 'appsessions',
             region: 'north',
--- web-monitor/js/view/Status.js
+++ web-monitor/js/view/Status.js
@@ -33,10 +33,8 @@ Ext.define('Ung.apps.webmonitor.view.Status', {
     }, {
         region: 'west',
         border: false,
-        width: 350,
-        minWidth: 300,
+        width: Math.ceil(Ext.getBody().getViewSize().width / 4),
         split: true,
-        layout: 'border',
         items: [{
             xtype: 'appsessions',
             region: 'north',
