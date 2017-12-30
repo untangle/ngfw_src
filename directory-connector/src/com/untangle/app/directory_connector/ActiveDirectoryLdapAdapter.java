20dacbee9631244c80102cd0268dee282b3eaaa9
Chris Blaise <cblaise@untangle.com>
2017-12-29 19:53:25 -0500

    sonarqube - Remove 11 unused imports

--- directory-connector/src/com/untangle/app/directory_connector/ActiveDirectoryLdapAdapter.java
+++ directory-connector/src/com/untangle/app/directory_connector/ActiveDirectoryLdapAdapter.java
@@ -15,11 +15,8 @@ package com.untangle.app.directory_connector;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -34,12 +31,7 @@ import javax.naming.directory.DirContext;
 import javax.naming.directory.SearchControls;
 import javax.naming.directory.SearchResult;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.log4j.PatternLayout;
-import org.apache.log4j.varia.LevelRangeFilter;
 import org.jabsorb.JSONSerializer;
 import org.jabsorb.serializer.UnmarshallException;
 
