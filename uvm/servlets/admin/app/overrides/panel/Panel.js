ec310a127dc4670bc08b573de9807a7a5989881e
Chris Blaise <cblaise@untangle.com>
2018-01-10 15:12:32 -0500

    11385 Add a cheap hack to watch for tab panel forms for config, aps, and extra.  Wait a bit after the final layout and then disable scrollable to disable the blank scrollbars.  Super cheap hack.

--- uvm/servlets/admin/app/overrides/panel/Panel.js
+++ uvm/servlets/admin/app/overrides/panel/Panel.js
@@ -1,4 +1,23 @@
 Ext.define('Ung.overrides.panel.Panel', {
     override: 'Ext.panel.Panel',
-//    scrollable: true
+    listeners:{
+        afterlayout: function(){
+            if(this.$className.startsWith('Ung.config') ||
+               this.$className.startsWith('Ung.apps') ||
+               this.$className.startsWith('Ung.view.extra')){
+                var me = this;
+                if(!me.scrollableTaskRan){
+                    if(!me.scrollableTask){
+                        me.scrollableTask = new Ext.util.DelayedTask( Ext.bind(function(){
+                            if(this.container && this.container.dom){
+                                me.setScrollable(false);
+                            }
+                            me.scrollableTaskRan = true;
+                        }, me) );
+                    }
+                    me.scrollableTask.delay( 100 );
+                }
+            }
+        }
+    }
 });
