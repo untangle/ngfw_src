de2ef645b62a3f4eaf9cc7305c6f9021c828381c
Colin Mercer <cmercer@untangle.com>
2018-02-23 18:58:36 -0700

    test_120_cert_is_in_backup
    Test to check that the server cert is in the backup (under /system/backup). 1. backup original cert 2. modify apache.pem a little 3. download backup 4. check backed-up copy has modification 5. restore original cert.

--- uvm/hier/usr/lib/python2.7/tests/uvm_tests.py
+++ uvm/hier/usr/lib/python2.7/tests/uvm_tests.py
@@ -15,6 +15,7 @@ import ipaddr
 import time
 import ssl
 import json
+import glob
 
 from jsonrpc import ServiceProxy
 from jsonrpc import JSONRPCException
@@ -370,5 +371,43 @@ class UvmTests(unittest2.TestCase):
                         output = subprocess.check_output("dig +short @" + netaddr + " " + fullName, shell=True)
                         result = output.strip()
                         assert(result == netaddr)
+    
+    def test_120_cert_is_in_backup(self):
+        """check that the Server Certificate exists in the backup"""
+
+        #copy a backup of apache.pem
+        certFilePath = "/usr/share/untangle/settings/untangle-certificates/apache.pem"
+        os.system("cp "+certFilePath+" "+certFilePath+".backup")
+
+        #Modify apache.pem a little to verify the change is in the backup
+        certFile = open(certFilePath)
+        lines = certFile.read().splitlines()
+        newline = "AAAAA" + lines[1][5:]
+        lines[1] = newline
+        open(certFilePath, "w").write('\n'.join(lines))
+
+        #Download backup
+        os.system("rm -f /tmp/untangleBackup.backup") # remove old backup file
+        result = os.system("wget -o /dev/null -t 1 --timeout=3 --post-data 'type=backup' http://localhost/admin/download -O /tmp/untangleBackup.backup")
+        assert(result == 0)
+
+        #remove any old files associated with backup
+        os.system("rm -f /tmp/files*")
+        os.system("rm -f /tmp/PUBVERSION")
+        os.system("rm -fr /tmp/usr")
+
+        #extract backup
+        os.system("tar -xf /tmp/untangleBackup.backup -C /tmp")
+        os.system("tar -xf "+glob.glob("/tmp/files*.tar.gz")[0] + " -C /tmp") #use glob since extracted file has timestamp
+
+        #Check the cert in the backup
+        newCertFilePath = "/tmp/usr/share/untangle/settings/untangle-certificates/apache.pem"
+        newCertFile = open(newCertFilePath, "r")
+        newCertFileLines = newCertFile.read().splitlines()
+        #compare original and modified certs
+        assert(newline == newCertFileLines[1])
+
+        #replace modified cert with backed-up original
+        os.system("cp "+certFilePath+".backup "+certFilePath)
 
 test_registry.registerApp("uvm", UvmTests)
