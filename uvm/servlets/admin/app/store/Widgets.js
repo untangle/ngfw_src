8c6a55830b97356c3a93974bac92c78663b4fe9e
Dan Hadarau <dhadarau@untangle.com>
2018-04-10 02:48:25 -0700

    Updated Dashboard Widgets management
    Any widget (common or report) can be removed/added multiple times.
    Added a separate management class.
    
    NGFW-11329 #code-completed

--- uvm/servlets/admin/app/store/Widgets.js
+++ uvm/servlets/admin/app/store/Widgets.js
@@ -2,5 +2,21 @@ Ext.define('Ung.store.Widgets', {
     extend: 'Ext.data.Store',
     alias: 'store.widgets',
     storeId: 'widgets',
-    model: 'Ung.model.Widget'
+    model: 'Ung.model.Widget',
+    listeners: {
+        // load: function () { },
+        // datachanged: function () { },
+        update: function (store, record, operation, modifiedFieldNames, details) {
+            if (operation === 'edit' && Ext.Array.contains(modifiedFieldNames, 'enabled')) {
+                Ext.fireEvent('updatewidget', store, record, operation, modifiedFieldNames, details);
+            }
+        },
+        remove: function (store, records) {
+            Ext.fireEvent('removewidgets', store, records);
+        },
+        add: function (store, records) {
+            Ext.fireEvent('addwidgets', store, records);
+        },
+        // refresh: function (store) { }
+    }
 });
