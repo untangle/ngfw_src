924b3cc594dd027ae491463f326b08ee323d91a2
Chris Blaise <cblaise@untangle.com>
2018-04-10 10:22:16 -0400

    NGFW-11413 Fix ordering.

--- live-support/js/Main.js
+++ live-support/js/Main.js
@@ -1,18 +1,17 @@
 Ext.define('Ung.apps.livesupport.Main', {
     extend: 'Ung.cmp.AppPanel',
     alias: 'widget.app-live-support',
-
-    items: [
-        { xtype: 'app-live-support-status' }
-    ],
-
     controller: 'app-live-support',
 
     viewModel: {
         data: {
             companyName: '',
             serverUID: '',
-            fullVersionAndRevision: '',
+            fullVersionAndRevision: ''
         }
-    }
+    },
+
+    items: [{
+        xtype: 'app-live-support-status'
+    }]
 });
