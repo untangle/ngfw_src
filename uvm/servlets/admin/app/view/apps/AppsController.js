e16d6a8b0a47bf173b93eda8c92544309caceed3
Dirk Morris <dmorris@untangle.com>
2018-04-10 23:02:58 -0700

    change default suggestion to only install hungry apps on >1.5 gigs

--- uvm/servlets/admin/app/view/apps/AppsController.js
+++ uvm/servlets/admin/app/view/apps/AppsController.js
@@ -406,7 +406,7 @@ Ext.define('Ung.view.apps.AppsController', {
                     // or if its arm still install virus blocker (in clientless mode)
                     try {
                         var memTotal = Util.bytesToMBs(Ext.getStore('stats').first().get('MemTotal'));
-                        if (memTotal && memTotal > 900) {
+                        if (memTotal && memTotal > 1400) {
                             apps.splice(2, 0, { displayName: 'Phish Blocker', name: 'phish-blocker'});
                             apps.splice(2, 0, { displayName: 'Spam Blocker', name: 'spam-blocker'});
                             apps.splice(2, 0, { displayName: 'Virus Blocker Lite', name: 'virus-blocker-lite'});
