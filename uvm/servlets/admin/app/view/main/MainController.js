4eab1584e89287559e07797932276e661a41b761
Chris Blaise <cblaise@untangle.com>
2018-04-17 21:20:29 -0400

    NGFW-11413 Check for destroyed objects, refactor RPC calls.

--- uvm/servlets/admin/app/view/main/MainController.js
+++ uvm/servlets/admin/app/view/main/MainController.js
@@ -23,19 +23,19 @@ Ext.define('Ung.view.main.MainController', {
 
     checkRegister: function () {
         var me = this;
-        if(!rpc.isRegistered) {
-            // Ext.MessageBox.wait('Determining Connectivity...'.t(), 'Please wait'.t());
-            rpc.UvmContext.isStoreAvailable(function (result, ex) {
-                if (ex) { Util.handleException(ex); }
-                // Ext.MessageBox.hide();
-
-                // If box is not online - show error message.
-                // Otherwise show registration screen
+        if(!Rpc.directData('rpc.isRegistered')) {
+            Rpc.asyncData('rpc.UvmContext.isStoreAvailable')
+            .then( function(result){
+                if(Util.isDestroyed(me)){
+                    return;
+                }
                 if (!result) {
                     me.openOffline();
                 } else {
                     me.openRegister();
                 }
+            },function(ex){
+                Util.handleException(ex);
             });
         }
     },
@@ -53,48 +53,51 @@ Ext.define('Ung.view.main.MainController', {
     checkNotifications: function () {
         var me = this;
         Rpc.asyncData('rpc.notificationManager.getNotifications')
-            .then(function (result) {
-                var btn = me.getView().down('#notificationBtn'), notificationArr = '', i;
+        .then(function (result) {
+            if(Util.isDestroyed(me)){
+                return;
+            }
+            var btn = me.getView().down('#notificationBtn'), notificationArr = '', i;
 
-                if (result != null && result.list.length > 0) {
-                    btn.show();
-                    notificationArr += '<h3>' + 'Notifications:'.t() + '</h3><ul>';
-                    for (i = 0; i < result.list.length; i += 1) {
-                        notificationArr += '<li>' + result.list[i] + '</li>';
-                    }
-                    notificationArr += '</ul>';
-                    btn.setText(result.list.length);
-                } else {
-                    btn.hide();
-                    return;
+            if (result != null && result.list.length > 0) {
+                btn.show();
+                notificationArr += '<h3>' + 'Notifications:'.t() + '</h3><ul>';
+                for (i = 0; i < result.list.length; i += 1) {
+                    notificationArr += '<li>' + result.list[i] + '</li>';
                 }
+                notificationArr += '</ul>';
+                btn.setText(result.list.length);
+            } else {
+                btn.hide();
+                return;
+            }
 
-                btn.setMenu({
-                    cls: 'notification-menu',
-                    plain: true,
-                    shadow: false,
-                    width: 300,
-                    items: [{
-                        xtype: 'component',
-                        padding: '20',
-                        style: {
-                            color: '#CCC'
-                        },
-                        autoEl: {
-                            html: notificationArr
-                        }
-                    }, {
-                        xtype: 'button',
-                        iconCls: 'fa fa-question-circle',
-                        text: 'Help with Administration Notifications'.t(),
-                        margin: '0 20 20 20',
-                        href: rpc.helpUrl + '?source=admin_notifications' + '&' + Util.getAbout(),
-                        hrefTarget: '_blank'
-                    }]
-                });
-            }, function (ex) {
-                Util.handleException(ex);
+            btn.setMenu({
+                cls: 'notification-menu',
+                plain: true,
+                shadow: false,
+                width: 300,
+                items: [{
+                    xtype: 'component',
+                    padding: '20',
+                    style: {
+                        color: '#CCC'
+                    },
+                    autoEl: {
+                        html: notificationArr
+                    }
+                }, {
+                    xtype: 'button',
+                    iconCls: 'fa fa-question-circle',
+                    text: 'Help with Administration Notifications'.t(),
+                    margin: '0 20 20 20',
+                    href: Rpc.directData('rpc.helpUrl') + '?source=admin_notifications' + '&' + Util.getAbout(),
+                    hrefTarget: '_blank'
+                }]
             });
+        }, function (ex) {
+            Util.handleException(ex);
+        });
     },
 
 
@@ -114,18 +117,18 @@ Ext.define('Ung.view.main.MainController', {
     },
 
     setLiveSupport: function() {
-        this.getViewModel().set('liveSupport', rpc.appManager.app('live-support') !== null);
+        this.getViewModel().set('liveSupport', Rpc.directData('rpc.appManager.app', 'live-support') !== null);
     },
 
     helpHandler: function (btn) {
-        var helpUrl = rpc.helpUrl + '?fragment=' + window.location.hash.substr(1) + '&' + Util.getAbout();
+        var helpUrl = Rpc.directData('rpc.helpUrl') + '?fragment=' + window.location.hash.substr(1) + '&' + Util.getAbout();
         window.open(helpUrl);
     },
 
     supportHandler: function (btn) {
         var me = this;
         // check here if support is enabled and show modal only if not, otherwise open support window
-        if (rpc.systemManager.getSettings().supportEnabled) {
+        if (Rpc.directData('rpc.systemManager.getSettings').supportEnabled) {
             me.supportLaunch();
         } else {
             me.getView().add({ xtype: 'support' }).show();
@@ -134,7 +137,7 @@ Ext.define('Ung.view.main.MainController', {
 
     supportLaunch: function () {
         var supportUrl = Util.getStoreUrl() + '?action=support&' + Util.getAbout() + '&fragment=' + window.location.hash.substr(1) + '&line=ngfw';
-        var user = rpc.adminManager.getSettings().users.list[0];
+        var user = Rpc.directData('rpc.adminManager.getSettings').users.list[0];
         if (user) {
             supportUrl += '&email=' + user.emailAddress;
         }
