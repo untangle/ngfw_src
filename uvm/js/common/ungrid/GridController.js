4a0398be1f908b6907424e52083629736440c355
Chris Blaise <cblaise@untangle.com>
2018-04-26 20:54:02 -0400

    NGFW-11680 If user somehow adds an unsupported conditional, render that name as-is instead of attempting a lookup.

--- uvm/js/common/ungrid/GridController.js
+++ uvm/js/common/ungrid/GridController.js
@@ -204,11 +204,15 @@ Ext.define('Ung.cmp.GridController', {
                 }
             }
             // for boolean conditions just add 'True' string as value
-            if (view.conditionsMap[conds[i].conditionType].type === 'boolean') {
+            if (view.conditionsMap[conds[i].conditionType] != undefined && view.conditionsMap[conds[i].conditionType].type === 'boolean') {
                 valueRenderer = ['True'.t()];
             }
 
-            resp.push(view.conditionsMap[conds[i].conditionType].displayName + '<strong>' + (conds[i].invert ? ' &nrArr; ' : ' &rArr; ') + '<span class="cond-val ' + (conds[i].invert ? 'invert' : '') + '">' + valueRenderer.join(', ') + '</span>' + '</strong>');
+            resp.push(
+                ( view.conditionsMap[conds[i].conditionType] != undefined ? view.conditionsMap[conds[i].conditionType].displayName : conds[i].conditionType ) +
+                '<strong>' +
+                (conds[i].invert ? ' &nrArr; ' : ' &rArr; ') + '<span class="cond-val ' + (conds[i].invert ? 'invert' : '') + '">' + valueRenderer.join(', ') + '</span>' +
+                '</strong>');
         }
         return resp.length > 0 ? resp.join(' &nbsp;&bull;&nbsp; ') : '<em>' + 'No conditions' + '</em>';
     },
