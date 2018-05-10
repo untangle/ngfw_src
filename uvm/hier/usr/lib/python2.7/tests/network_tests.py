f42539acdbc8a3f7a18aad15cef0db1fc2482499
John Coffin <jcoffin@gmail.com>
2018-05-10 10:11:23 -0700

    Removing dyn domain that might have an issue

--- uvm/hier/usr/lib/python2.7/tests/network_tests.py
+++ uvm/hier/usr/lib/python2.7/tests/network_tests.py
@@ -22,7 +22,7 @@ import global_functions
 
 ftp_file_name = ""
 dyn_hostname = ""
-dyn_names = ['ats.dataprotected.net', 'atsbeta.dataprotected.net', 'atsgamma.dataprotected.net', 'atsdelta.dataprotected.net']
+dyn_names = ['atsbeta.dataprotected.net', 'atsgamma.dataprotected.net', 'atsdelta.dataprotected.net']
 
 default_policy_id = 1
 orig_netsettings = None
