e159932ee944b29b6b67b8e0e72e7f628e7b1a5a
Chris Blaise <cblaise@untangle.com>
2018-06-13 16:31:51 -0400

    javadocing

--- smtp-casing/src/com/untangle/app/smtp/UnparsableCommand.java
+++ smtp-casing/src/com/untangle/app/smtp/UnparsableCommand.java
@@ -15,17 +15,30 @@ public class UnparsableCommand extends Command
 
     private ByteBuffer m_unparsedLine;
 
+    /**
+     * Initialize instance of UnparsableCommand.
+     * @param  badLine ByteBuffer of unparsable line.
+     * @return         Instance of UnparsableCommand.
+     */
     public UnparsableCommand(ByteBuffer badLine) {
         super(CommandType.UNKNOWN);
         m_unparsedLine = badLine;
     }
 
+    /**
+     * Return argument string.
+     * @return String of unparsed line.
+     */
     @Override
     public String getArgString()
     {
         return bbToString(m_unparsedLine);
     }
 
+    /**
+     * Return copy of argument string.
+     * @return Copy of unparsed line.
+     */
     @Override
     public ByteBuffer getBytes()
     {
