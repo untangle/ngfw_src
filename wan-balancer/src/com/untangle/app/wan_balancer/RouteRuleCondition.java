a205c0b35fbcdb9ffba87219a395c5eb73cd8858
mahotz <mahotz@untangle.com>
2018-06-15 13:10:23 -0700

    Add Javadoc for wan-balancer

--- wan-balancer/src/com/untangle/app/wan_balancer/RouteRuleCondition.java
+++ wan-balancer/src/com/untangle/app/wan_balancer/RouteRuleCondition.java
@@ -1,32 +1,53 @@
 /**
  * $Id$
  */
+
 package com.untangle.app.wan_balancer;
 
 import com.untangle.uvm.app.RuleCondition;
 
-
 /**
  * This is a matching criteria for a RouteRule Rule
  * Example: "Destination Port" == "80"
- *
+ * 
  * A RouteRule has a set of these to determine what traffic to match
  */
 @SuppressWarnings("serial")
 public class RouteRuleCondition extends RuleCondition
 {
-    public RouteRuleCondition( )
+    /**
+     * Constructor
+     */
+    public RouteRuleCondition()
     {
         super();
     }
 
-    public RouteRuleCondition( ConditionType matcherType, String value )
+    /**
+     * Constructor
+     * 
+     * @param matcherType
+     *        The matcher type
+     * @param value
+     *        The value
+     */
+    public RouteRuleCondition(ConditionType matcherType, String value)
     {
-        super( matcherType, value );
+        super(matcherType, value);
     }
 
-    public RouteRuleCondition( ConditionType matcherType, String value, Boolean invert )
+    /**
+     * Constructor
+     * 
+     * @param matcherType
+     *        The matcher type
+     * @param value
+     *        The value
+     * @param invert
+     *        Invert flag
+     */
+    public RouteRuleCondition(ConditionType matcherType, String value, Boolean invert)
     {
-        super( matcherType, value, invert );
+        super(matcherType, value, invert);
     }
 }
