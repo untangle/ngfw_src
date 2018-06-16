7f3b91e9131c4293ce909be661c93d2c7d8fc56e
mahotz <mahotz@untangle.com>
2018-06-15 17:05:46 -0700

    Start Javadoc for virus-blocker-base

--- virus-blocker-base/src/com/untangle/app/virus_blocker/VirusScanner.java
+++ virus-blocker-base/src/com/untangle/app/virus_blocker/VirusScanner.java
@@ -1,25 +1,39 @@
 /**
  * $Id$
  */
+
 package com.untangle.app.virus_blocker;
 
 import java.io.File;
 import java.util.Date;
 import com.untangle.uvm.vnet.AppSession;
- 
+
 import com.untangle.uvm.app.Scanner;
 
+/**
+ * Virus scanner
+ */
 public interface VirusScanner extends Scanner
 {
     /**
-     * Scans the file for viruses, producing a virus report.  Note that the contract for this
-     * requires that a report always be generated, for any problems or exceptions an
-     * "clean" report is generated (and the error/warning should be logged).
-     *
-     * @param msgFile a <code>File</code> value
-     * @return a <code>VirusScannerResult</code> value
+     * 
+     * Scans the file for viruses, producing a virus report. Note that the
+     * contract for this requires that a report always be generated, for any
+     * problems or exceptions an "clean" report is generated (and the
+     * error/warning should be logged).
+     * 
+     * @param msgFile
+     *        The file to scan
+     * @param session
+     *        The application session
+     * @return The scan result
      */
     VirusScannerResult scanFile(File msgFile, AppSession session);
 
+    /**
+     * Get the date of the last virus signature update
+     * 
+     * @return The date of the last virus signature update
+     */
     Date getLastSignatureUpdate();
 }
