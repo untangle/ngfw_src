da29806bd97579c0af326c297ca13c22f4a315d1
mahotz <mahotz@untangle.com>
2018-06-15 17:05:46 -0700

    Start Javadoc for virus-blocker-base

--- virus-blocker-base/src/com/untangle/app/virus_blocker/VirusReplacementGenerator.java
+++ virus-blocker-base/src/com/untangle/app/virus_blocker/VirusReplacementGenerator.java
@@ -1,6 +1,7 @@
-/*
+/**
  * $Id$
  */
+
 package com.untangle.app.virus_blocker;
 
 import com.untangle.app.http.ReplacementGenerator;
@@ -13,6 +14,8 @@ import com.untangle.uvm.app.AppSettings;
  */
 class VirusReplacementGenerator extends ReplacementGenerator<VirusBlockDetails>
 {
+// THIS IS FOR ECLIPSE - @formatter:off
+    
     private static final String BLOCK_TEMPLATE
         = "<HTML><HEAD>"
         + "<TITLE>403 Forbidden</TITLE>"
@@ -24,23 +27,45 @@ class VirusReplacementGenerator extends ReplacementGenerator<VirusBlockDetails>
         + "<p>Reason: %s</p>"
         + "<p>Please contact %s</p>"
         + "</BODY></HTML>";
+// THIS IS FOR ECLIPSE - @formatter:on
 
+    /**
+     * Constructor
+     * 
+     * @param tid
+     *        Application settings
+     */
     VirusReplacementGenerator(AppSettings tid)
     {
         super(tid);
     }
 
+    /**
+     * Get the replacement
+     * 
+     * @param details
+     *        The block details
+     * @return The replacement
+     */
     @Override
     protected String getReplacement(VirusBlockDetails details)
     {
         UvmContext uvm = UvmContextFactory.context();
 
-        return String.format(BLOCK_TEMPLATE, details.getVendor(),
-                             details.getHost(), details.getUri(),
-                             details.getReason(),
-                             uvm.brandingManager().getContactHtml());
+        return String.format(BLOCK_TEMPLATE, details.getVendor(), details.getHost(), details.getUri(), details.getReason(), uvm.brandingManager().getContactHtml());
     }
 
+    /**
+     * Get the redirect URL
+     * 
+     * @param nonce
+     *        The nonce
+     * @param host
+     *        The host
+     * @param appSettings
+     *        The application settings
+     * @return The redirect URL
+     */
     @Override
     protected String getRedirectUrl(String nonce, String host, AppSettings appSettings)
     {
