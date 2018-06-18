e8e0620aa0c067ee4110545afbcc56d5523daed7
mahotz <mahotz@untangle.com>
2018-06-18 15:20:14 -0700

    Add Javadoc to spam-blocker-base

--- spam-blocker-base/src/com/untangle/app/spam_blocker/ReportItem.java
+++ spam-blocker-base/src/com/untangle/app/spam_blocker/ReportItem.java
@@ -3,33 +3,54 @@
  */
 package com.untangle.app.spam_blocker;
 
+/**
+ * Spam report item implementation
+ */
 public class ReportItem
 {
     private final float score;
     private final String category;
 
-    // constructors -----------------------------------------------------------
-
+    /**
+     * Constructor
+     * 
+     * @param score
+     *        The score
+     * @param category
+     *        The category
+     */
     public ReportItem(float score, String category)
     {
         this.score = score;
         this.category = category;
     }
 
-    // accessors --------------------------------------------------------------
+    /**
+     * Get the score
+     * 
+     * @return The score
+     */
 
     public float getScore()
     {
         return score;
     }
 
+    /**
+     * Get the category
+     * 
+     * @return The category
+     */
     public String getCategory()
     {
         return category;
     }
 
-    // Object methods ---------------------------------------------------------
-
+    /**
+     * Get the string representation
+     * 
+     * @return The string representation
+     */
     public String toString()
     {
         return score + " " + category;
