69092a6d4e6d481b68e9fd2ba961e4826051bb12
Dirk Morris <dmorris@untangle.com>
2018-06-21 17:17:37 -0700

    Add JNetcap javadoc

--- jnetcap/impl/com/untangle/jnetcap/NetcapTCPSession.java
+++ jnetcap/impl/com/untangle/jnetcap/NetcapTCPSession.java
@@ -7,6 +7,9 @@ import java.net.InetAddress;
 
 import org.apache.log4j.Logger;
 
+/**
+ * NetcapTCPSession
+ */
 @SuppressWarnings("unused") //JNI
 public class NetcapTCPSession extends NetcapSession
 {
@@ -15,14 +18,38 @@ public class NetcapTCPSession extends NetcapSession
     private static final int FLAG_SERVER_FD                    = 100;
     private static final int FLAG_CLIENT_FD                    = 101;
 
+    /**
+     * NetcapTCPSession is a TCP session instance
+     * @param id
+     */
     public NetcapTCPSession( long id )
     {
         super( id );
     }
     
-    public TCPEndpoints clientSide() { return (TCPEndpoints)clientSide; }
-    public TCPEndpoints serverSide() { return (TCPEndpoints)serverSide; }
+    /**
+     * clientSide
+     * @return the client side TCPEndpoints
+     */
+    public TCPEndpoints clientSide()
+    {
+        return (TCPEndpoints)clientSide;
+    }
 
+    /**
+     * serverSide
+     * @return the server side TCPEndpoints
+     */
+    public TCPEndpoints serverSide()
+    {
+        return (TCPEndpoints)serverSide;
+    }
+
+    /**
+     * makeEndpoints makes Endpoints object for this session
+     * @param ifClient - true if is a client endpoints, false otherwise
+     * @return endpoints
+     */
     protected Endpoints makeEndpoints( boolean ifClient ) 
     {
         return new TCPSessionEndpoints( ifClient );
@@ -30,8 +57,7 @@ public class NetcapTCPSession extends NetcapSession
     
     /**
      * Complete the connection to a client, this may throw an exception if
-     * the connection is not completed succesfully.</p>
-     * @param flags - Flags for the client complete.
+     * the connection is not completed succesfully.
      */
     public void clientComplete()
     {
@@ -39,8 +65,7 @@ public class NetcapTCPSession extends NetcapSession
     }
 
     /**
-     * Reset the connection to the client.</p>
-     * @param flags - Flags for the client reset.
+     * Reset the connection to the client.
      */
     public void clientReset()
     {
@@ -67,7 +92,8 @@ public class NetcapTCPSession extends NetcapSession
     }
 
     /**
-     * Send an ICMP Destination unreachable message with the specified code 
+     * Send an ICMP Destination unreachable message with the specified code
+     * @param code
      */
     public void clientSendIcmpDestUnreach( byte code )
     {
@@ -82,14 +108,23 @@ public class NetcapTCPSession extends NetcapSession
         clientForwardReject( pointer.value() );
     }
     
-    /* Deprecated (intf must be specified before completion) (made non-public) */
-    /* Complete the connection, every other function should call this after setup */
+    /**
+     * Deprecated (intf must be specified before completion) (made non-public) 
+     * Complete the connection, every other function should call this after setup
+     */
     private void   serverComplete( )
     {
         serverComplete( pointer.value() );
     }
 
-    /* Connect to the server with new settings on the traffic side */
+    /**
+     * Connect to the server with new settings on the traffic side
+     * @param clientAddress 
+     * @param clientPort 
+     * @param serverAddress 
+     * @param serverPort 
+     * @param intf 
+     */
     public void   serverComplete( InetAddress clientAddress, int clientPort, InetAddress serverAddress, int serverPort, int intf )
     {
         if ( clientAddress == null ) clientAddress = serverSide.client().host();
@@ -104,32 +139,113 @@ public class NetcapTCPSession extends NetcapSession
         serverComplete( );
     }
     
+    /**
+     * setServerEndpoint
+     * @param sessionPointer 
+     * @param clientAddress 
+     * @param clientPort 
+     * @param serverAddress 
+     * @param serverPort 
+     * @param intf 
+     * @return 0 if success
+     */
     private static native int setServerEndpoint ( long sessionPointer, long clientAddress, int clientPort, long serverAddress, int serverPort, int intf );
 
+    /**
+     * clientComplete
+     * @param sessionPointer 
+     */
     private static native void clientComplete           ( long sessionPointer );
+
+    /**
+     * clientReset
+     * @param sessionPointer 
+     */
     private static native void clientReset              ( long sessionPointer );
+
+    /**
+     * clientDrop
+     * @param sessionPointer 
+     */
     private static native void clientDrop               ( long sessionPointer );
+
+    /**
+     * clientSendIcmp
+     * @param sessionPointer 
+     */
     private static native void clientSendIcmp           ( long sessionPointer );
+
+    /**
+     * clientForwardReject
+     * @param sessionPointer 
+     */
     private static native void clientForwardReject      ( long sessionPointer );
+
+    /**
+     * clientSendIcmpDestUnreach
+     * @param sessionPointer 
+     * @param code 
+     */
     private static native void clientSendIcmpDestUnreach( long sessionPointer, byte code );    
+
+    /**
+     * serverComplete
+     * @param sessionPointer 
+     */
     private static native void serverComplete           ( long sessionPointer );
+
+    /**
+     * close
+     * @param sessionPointer 
+     * @param ifClient 
+     */
     private static native void close                    ( long sessionPointer, boolean ifClient );
     
+    /**
+     * read
+     * @param sessionPointer 
+     * @param ifClient 
+     * @param data 
+     * @return number of bytes
+     */
     private static native int read( long sessionPointer, boolean ifClient, byte[] data );
+
+    /**
+     * write
+     * @param sessionPointer 
+     * @param ifClient 
+     * @param data 
+     * @return number of bytes
+     */
     private static native int write( long sessionPointer, boolean ifClient, byte[] data );
 
     /**
-     * Set the blocking mode for one of the file descriptors.  This will throw an error if it fails.
+     * Set the blocking mode for one of the file descriptors.  This
+     * will throw an error if it fails.
+     * @param sessionPointer 
+     * @param ifClient 
+     * @param mode 
      */
     private static native void blocking( long sessionPointer, boolean ifClient, boolean mode );
 
+    /**
+     * TCPSessionEndpoints
+     */
     protected class TCPSessionEndpoints extends SessionEndpoints implements TCPEndpoints
     {
+        /**
+         * TCPSessionEndpoints
+         * @param isClientSide - true if for the client side, false if for server side
+         */
         public TCPSessionEndpoints( boolean isClientSide )
         {
             super( isClientSide );
         }
                 
+        /**
+         * fd - gets the file descriptor
+         * @return the FD
+         */
         public int fd()
         {
             if ( isClientSide )
@@ -139,23 +255,40 @@ public class NetcapTCPSession extends NetcapSession
         }
         
         /**
-         * Set the blocking mode for one of the file descriptors.  This will throw an if it fails.
+         * Set the blocking mode for one of the file descriptors.
+         * This will throw an Exception if it fails.
+         * @param mode 
          */
         public void blocking( boolean mode )
         {
             NetcapTCPSession.blocking( pointer.value(), isClientSide, mode );
         }
 
+        /**
+         * read
+         * @param data
+         * @return number of bytes read
+         */
         public int read( byte[] data )
         {
             return NetcapTCPSession.read( pointer.value(), isClientSide, data );
         }
         
+        /**
+         * write
+         * @param data
+         * @return number of bytes written
+         */
         public int write( byte[] data )
         {
             return NetcapTCPSession.write( pointer.value(), isClientSide, data );
         }
 
+        /**
+         * write
+         * @param data 
+         * @return number of bytes written
+         */
         public int write( String data )
         {
             return write( data.getBytes());
@@ -163,7 +296,8 @@ public class NetcapTCPSession extends NetcapSession
 
         /**
          * Close a file descriptor associated with one of the sides.  This will
-         * throw an error if it is unable to close the file desscriptor */
+         * throw an error if it is unable to close the file desscriptor
+         */
         public void close()
         {
             NetcapTCPSession.close( pointer.value(), isClientSide );
