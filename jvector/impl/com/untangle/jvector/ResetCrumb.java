8d726d32bdc692c7b44b3bf39f4a3ff8cce6ab90
Dirk Morris <dmorris@untangle.com>
2018-06-21 17:59:30 -0700

    Add JVector javadoc

--- jvector/impl/com/untangle/jvector/ResetCrumb.java
+++ jvector/impl/com/untangle/jvector/ResetCrumb.java
@@ -3,35 +3,52 @@
  */
 package com.untangle.jvector;
 
+/**
+ * ResetCrumb represents a reset event (for TCPSource/TCPSink)
+ */
 public class ResetCrumb extends Crumb
 {
     private static final ResetCrumb ACKED     = new ResetCrumb();
     private static final ResetCrumb NOT_ACKED = new ResetCrumb();
-
+    
+    /**
+     * ResetCrumb
+     */
     private ResetCrumb()
     {
     }
     
+    /**
+     * raze - noop
+     */
     public void raze()
     {
     }
 
     /**
-     * Determine whether or not this is a reset crumb from an ACKED session
-     * or from an unacked session.
+     * acked
+     * Determine whether or not this is a reset crumb from an ACKED
+     * session or from an unacked session.
+     * @return
      */
     public boolean acked()
     {
         return ( this == ACKED ) ? true : false;
     }
 
+    /**
+     * type
+     * @return
+     */
     public int type()
     { 
         return TYPE_RESET;
     }
 
     /**
+     * getInstance
      * Get the acked reset crumb.
+     * @return
      */
     public static ResetCrumb getInstance()
     {
@@ -39,13 +56,20 @@ public class ResetCrumb extends Crumb
     }
 
     /**
-     * Get either the acked or non-acked reset crumb 
+     * getInstance
+     * Get either the acked or non-acked reset crumb
+     * @param acked
+     * @return
      */
     public static ResetCrumb getInstance( boolean acked )
     {
         return ( acked ) ? ACKED : NOT_ACKED;
     }
 
+    /**
+     * getInstanceNotAcked
+     * @return
+     */
     public static ResetCrumb getInstanceNotAcked() 
     {
         return NOT_ACKED;
