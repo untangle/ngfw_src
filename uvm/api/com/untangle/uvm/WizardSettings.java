014d6821affcae179f600c02e9d92f0676b1b069
Dirk Morris <dmorris@untangle.com>
2018-07-04 08:33:19 -0700

    Add a passwordRequired special option to wizard settings

--- uvm/api/com/untangle/uvm/WizardSettings.java
+++ uvm/api/com/untangle/uvm/WizardSettings.java
@@ -15,7 +15,19 @@ import org.json.JSONString;
 public class WizardSettings implements Serializable, JSONString
 {
     private boolean wizardComplete = false;
+    /**
+     * if passwordRequired == true, then a password will be required
+     * on the *even* if the wizard has not been completed.
+     * if passwordRequired == false, then the user will be automatically logged
+     * into the setup wizard the first time
+     */
+    private boolean passwordRequired = false;
     private String completedStep;
+
+    /**
+     * XXX DEPRECATED
+     * Can be removed after 14.0
+     */
     private String[] steps = new String[] {
         "Welcome",
         "ServerSettings",
@@ -25,7 +37,7 @@ public class WizardSettings implements Serializable, JSONString
         "AutoUpgrades",
         "Complete"
     };
-    
+
     public WizardSettings() { }
 
     public String toJSONString()
@@ -36,9 +48,12 @@ public class WizardSettings implements Serializable, JSONString
 
     public String[] getSteps() { return steps; }
     public void setSteps( String[] newValue ) { this.steps = newValue; }
-    
+
     public boolean getWizardComplete() { return wizardComplete; }
     public void setWizardComplete( boolean newValue ) { this.wizardComplete = newValue; }
+
+    public boolean getPasswordRequired() { return passwordRequired; }
+    public void setPasswordRequired( boolean newValue ) { this.passwordRequired = newValue; }
     
     public String getCompletedStep() { return this.completedStep; }
     public void setCompletedStep( String newValue ) { this.completedStep = newValue; }
