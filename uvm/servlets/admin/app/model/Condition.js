9980596a7feae350a77676dd905c6453d4710bc5
Chris Blaise <cblaise@untangle.com>
2018-07-10 16:13:25 -0400

    NGFW-11967 Remove default value for condition javaClass value.  Properly propogate from masterGrid definition.

--- uvm/servlets/admin/app/model/Condition.js
+++ uvm/servlets/admin/app/model/Condition.js
@@ -1,7 +1,7 @@
 Ext.define ('Ung.model.Condition', {
     extend: 'Ext.data.Model' ,
     fields: [
-        { name: 'javaClass', type: 'string',  defaultValue: 'com.untangle.uvm.network.FilterRuleCondition' },
+        { name: 'javaClass', type: 'string'},
         { name: 'conditionType', type: 'string' },
         { name: 'invert', type: 'boolean', defaultValue: false },
         { name: 'value', type: 'auto', defaultValue: '' }
