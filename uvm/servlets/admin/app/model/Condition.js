eeb12541d91b2af26348b55840f262d28fbcdd51
Chris Blaise <cblaise@untangle.com>
2018-07-10 15:58:15 -0400

    NGFW-11967 Set default javaClass for most rules.

--- uvm/servlets/admin/app/model/Condition.js
+++ uvm/servlets/admin/app/model/Condition.js
@@ -1,9 +1,9 @@
 Ext.define ('Ung.model.Condition', {
     extend: 'Ext.data.Model' ,
     fields: [
+        { name: 'javaClass', type: 'string',  defaultValue: 'com.untangle.uvm.network.FilterRuleCondition' },
         { name: 'conditionType', type: 'string' },
         { name: 'invert', type: 'boolean', defaultValue: false },
-        { name: 'javaClass', type: 'string' },
         { name: 'value', type: 'auto', defaultValue: '' }
     ],
     proxy: {
