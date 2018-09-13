5a617fff869533553d8f41c29c7f5b1f039e27fc
Dirk Morris <dmorris@untangle.com>
2018-09-13 09:50:39 -0700

    Cleanup virtual detect script NGFW-11907 #code-completed

--- uvm/hier/usr/share/untangle/bin/ut-virtual-detect.py
+++ uvm/hier/usr/share/untangle/bin/ut-virtual-detect.py
@@ -5,15 +5,34 @@ import sys
 
 Debug = False
 
+def get_virtual_type():
+    # Unknown.  Let the UI interpret this as "custom"
+    # which could be baremetal or another VM.
+    virtual_type = ""
+
+    # There are other possibilities but these are the systems we currently 
+    # have available for testing.
+    if os.path.exists("/sys/devices/virtual/dmi/id/product_serial"):
+        file = open("/sys/devices/virtual/dmi/id/product_serial")
+        product_serial = file.read().lower()
+        file.close()
+
+        if product_serial[0:3] == "ec2":
+            # Amazon EC2
+            virtual_type = "Amazon EC2"
+        elif product_serial[0:6] == "vmware":
+            # Some VMware product such as ESX or Workstation.
+            virtual_type = "VMware"
+    elif os.path.exists("/proc/xen"):
+        virtual_type = "Xen"
+
+    return virtual_type;
+
 def main(argv):
     """
     Main
     """
     global Debug
-
-    # Unknown.  Let the UI interpret this as "custom"
-    # which could be baremetal or another VM.
-    virtual_type = ""
 
     try:
         opts, args = getopt.getopt(argv, "hs:d", ["help", "debug"] )
@@ -25,20 +44,12 @@ def main(argv):
         if opt in ( "-d", "--debug"):
             Debug = True
 
-    # There are other possibilities but these are the systems we currently 
-    # have available for testing.
-    file = open("/sys/devices/virtual/dmi/id/product_serial")
-    product_serial = file.read().lower()
-    file.close()
-
-    if product_serial[0:3] == "ec2":
-        # Amazon EC2
-        virtual_type = "Amazon EC2"
-    elif product_serial[0:6] == "vmware":
-        # Some VMware product such as ESX or Workstation.
-        virtual_type = "VMware"
-
-    print(virtual_type);
+    virtual_type = ""
+    try:
+        virtual_type = get_virtual_type()
+    except:
+        pass
+    print(virtual_type)
 
 if __name__ == "__main__":
     main( sys.argv[1:] )
