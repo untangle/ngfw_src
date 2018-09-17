dabad3082ac0cc4c4203cb65eb43c8ec5f965074
Chris Blaise <cblaise@untangle.com>
2018-09-17 10:43:10 -0400

    NGFW-11875 Fix path.

--- intrusion-prevention/hier/usr/lib/python2.7/dist-packages/intrusion_prevention/intrusion_prevention_defaults.py
+++ intrusion-prevention/hier/usr/lib/python2.7/dist-packages/intrusion_prevention/intrusion_prevention_defaults.py
@@ -8,8 +8,7 @@ class IntrusionPreventionDefaults:
     """
     Profile defaults
     """
-    ## !!!! WOAH
-    file_name = "/usr/share/untangle-suricata-config/dev/templates/defaults.js"
+    file_name = "/usr/share/untangle-suricata-config/current/templates/defaults.js"
     reserved_id_regex = re.compile(r'^reserved_default_')
 
     def __init__(self):
