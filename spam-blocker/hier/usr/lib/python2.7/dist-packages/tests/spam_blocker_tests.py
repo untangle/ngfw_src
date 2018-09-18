5f25db88986b0ae8536cc8b9a03f861b47831af0
John Coffin <jcoffin@gmail.com>
2018-09-17 22:37:02 -0700

    Check if we can relay

--- spam-blocker/hier/usr/lib/python2.7/dist-packages/tests/spam_blocker_tests.py
+++ spam-blocker/hier/usr/lib/python2.7/dist-packages/tests/spam_blocker_tests.py
@@ -38,6 +38,8 @@ class SpamBlockerTests(SpamBlockerBaseTests):
 
     # verify MAIL_SHELL is scoring. Relies on test_20_smtpTest
     def test_021_check_for_mailshell(self):
+        if (not self.canRelay):
+            raise unittest2.SkipTest('Unable to relay through ' + global_functions.TEST_SERVER_HOST)
         events = global_functions.get_events(self.displayName(),'Quarantined Events',None,8)
         if events != None:
             assert( events.get('list') != None )
