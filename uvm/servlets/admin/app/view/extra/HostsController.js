eeb6e9b028e20b55e3260b0d973e79e5577ef44a
Chris Blaise <cblaise@untangle.com>
2018-10-25 10:39:23 -0700

    For instances where clearFilters() is used when saving store, pass argument to suppress event.

--- uvm/servlets/admin/app/view/extra/HostsController.js
+++ uvm/servlets/admin/app/view/extra/HostsController.js
@@ -114,7 +114,7 @@ Ext.define('Ung.view.extra.HostsController', {
             var store = grid.getStore();
 
             var filters = store.getFilters().clone();
-            store.clearFilter();
+            store.clearFilter(true);
 
             if (store.getModifiedRecords().length > 0 ||
                 store.getNewRecords().length > 0 ||
