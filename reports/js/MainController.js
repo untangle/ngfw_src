37e3aa864a0b420609c866da76b8fd7ffd4114c5
Chris Blaise <cblaise@untangle.com>
2018-12-30 13:59:01 -0800

    sonarqube: Fix 2 unused assignment issues

--- reports/js/MainController.js
+++ reports/js/MainController.js
@@ -253,7 +253,7 @@ Ext.define('Ung.apps.reports.cmp.EmailTemplatesGridController', {
     },
 
     sendReport: function(unk1, unk2, unk3, event, unk5, record){
-        var v = this.getView(), vm = this.getViewModel();
+        var v = this.getView();
         var dialog = v.add({
             xtype: 'app-reports-sendfixedreport',
             title: 'Send Email Report'.t(),
