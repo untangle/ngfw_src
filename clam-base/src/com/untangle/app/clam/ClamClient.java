58c18d9631c74b84081780d2fc9558908850b6bf
Chris Blaise <cblaise@untangle.com>
2018-12-30 08:05:21 -0800

    sonarqube: Fix 1 try-with-resources blocker

--- clam-base/src/com/untangle/app/clam/ClamClient.java
+++ clam-base/src/com/untangle/app/clam/ClamClient.java
@@ -116,6 +116,7 @@ public class ClamClient extends VirusClient
             }
 
             BufferedOutputStream bufMsgOutputStream = msgcSocket.getBufferedOutputStream();
+            FileInputStream fInputStream = null;
             try {
                 // send message (on msg socket)
                 //
@@ -123,7 +124,7 @@ public class ClamClient extends VirusClient
                 // we don't exit until we've checked the main socket
                 // - clamd will always have a result to report
 
-                FileInputStream fInputStream = new FileInputStream(cContext.getMsgFile());
+                fInputStream = new FileInputStream(cContext.getMsgFile());
                 rBuf = new byte[READ_SZ];
 
                 int rLen;
@@ -131,8 +132,6 @@ public class ClamClient extends VirusClient
                     bufMsgOutputStream.write(rBuf, 0, rLen);
                     bufMsgOutputStream.flush();
                 }
-                fInputStream.close();
-                fInputStream = null;
                 rBuf = null;
             } catch (SocketException e) {
                 // thrown during read block
@@ -145,6 +144,16 @@ public class ClamClient extends VirusClient
             } catch (Exception e) {
                 clogger.warn(dbgName + ", clamc msg failed", e);
                 // fall through and check clamd result
+            } finally {
+                if (fInputStream != null){
+                    try{
+                        fInputStream.close();
+                        fInputStream = null;
+                    } catch (Exception e) {
+                        clogger.warn(dbgName + ", unable to close msg file", e);
+
+                    }
+                }
             }
 
             bufMsgOutputStream = null;
