16878eb097a8b50e7dc9d73946ac9ada04899870
Dirk Morris <dmorris@untangle.com>
2019-01-06 19:16:16 +0000

    Add autoimporting of tests

--- uvm/hier/usr/lib/python3.5/dist-packages/tests/__init__.py
+++ uvm/hier/usr/lib/python3.5/dist-packages/tests/__init__.py
@@ -1,11 +1,6 @@
-"""__init__"""
+"""x"""
 import tests.global_functions
 import tests.remote_control
 import tests.test_registry
 
-from tests.environment_tests import TestEnvironmentTests
-from tests.network_tests import NetworkTests
-from tests.uvm_tests import UvmTests
 
-import os, pkgutil
-__all__ = list(module for _, module, _ in pkgutil.iter_modules([os.path.dirname(__file__)]))
