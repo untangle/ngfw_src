ab5c25736d0e67f98f9f6a18370e87d6e70f7357
Chris Blaise <cblaise@untangle.com>
2019-04-17 06:11:44 -0700

    NGFW-12388 Call updated clear cache method.

--- web-filter/src/com/untangle/app/web_filter/WebFilterApp.java
+++ web-filter/src/com/untangle/app/web_filter/WebFilterApp.java
@@ -62,14 +62,11 @@ public class WebFilterApp extends WebFilterBase
 
     /**
      * Clears the decision engine cache
-     * 
-     * @param expireAll
-     *        Expire all flag
      */
-    // public void clearCache(boolean expireAll)
-    // {
-    //     this.engine.clearCache(expireAll);
-    // }
+    public void clearCache()
+    {
+        this.engine.clearCache();
+    }
 
     /**
      * Called to lookup a specific site
