fe396738b1e1c46ea376613148b0cfc3708385e6
Chris Blaise <cblaise@untangle.com>
2019-05-23 12:10:21 -0700

    NGFW-12447 Change remaining table fields/columns to use renderers instead of converters.

--- uvm/js/common/util/Renderer.js
+++ uvm/js/common/util/Renderer.js
@@ -940,6 +940,156 @@ Ext.define('Ung.util.Renderer', {
                 ( value in Renderer.countryMap ) ? Renderer.countryMap[value] : Renderer.countryMap['default'],
                 value
         );
-    }
+    },
+
+    quotaActionMap: {
+        1: 'Given'.t(),
+        2: 'Exceeded'.t(),
+        default: 'Unknown'.t()
+    },
+    quotaAction: function( value ){
+        if(Ext.isEmpty(value)) {
+            return '';
+        }
+        return Ext.String.format(
+                Renderer.mapValueFormat,
+                ( value in Renderer.quotaActionMap ) ? Renderer.quotaActionMap[value] : Renderer.quotaActionMap['default'],
+                value
+        );
+    },
+
+    captivePortalEventInfoMap: {
+        LOGIN: 'Login Success'.t(),
+        FAILED: 'Login Failure'.t(),
+        TIMEOUT: 'Session Timeout'.t(),
+        INACTIVE: 'Idle Timeout'.t(),
+        USER_LOGOUT: 'User Logout'.t(),
+        ADMIN_LOGOUT: 'Admin Logout'.t(),
+        HOST_CHANGE: 'Host Change Logout'.t(),
+        default: 'Unknown'.t()
+    },
+    captivePortalEventInfo: function( value ){
+        if(Ext.isEmpty(value)) {
+            return '';
+        }
+        return ( value in Renderer.captivePortalEventInfoMap ) ? Renderer.captivePortalEventInfoMap[value] : Renderer.captivePortalEventInfoMap['default'];
+    },
+
+    authTypeMap: {
+        NONE: 'None'.t(),
+        LOCAL_DIRECTORY: 'Local Directory'.t(),
+        ACTIVE_DIRECTORY: 'Active Directory'.t(),
+        RADIUS: 'RADIUS'.t(),
+        GOOGLE: 'Google Account'.t(),
+        FACEBOOK: 'Facebook Account'.t(),
+        MICROSOFT: 'Microsoft Account'.t(),
+        CUSTOM: 'Custom'.t(),
+        default: 'Unknown'.t()
+    },
+    authType: function( value ){
+        if(Ext.isEmpty(value)) {
+            return '';
+        }
+        return ( value in Renderer.authTypeMap ) ? Renderer.authTypeMap[value] : Renderer.authTypeMap['default'];
+    },
+
+    configurationBackupSuccessMap:{
+        true: 'success'.t(),
+        default: 'failed'.t()
+    },
+    configurationBackupSuccess: function( value ){
+        return ( value in Renderer.configurationBackupSuccessMap ) ? Renderer.configurationBackupSuccessMap[value] : Renderer.configurationBackupSuccessMap['default'];
+    },
+
+    loginFailureReasonMap : {
+        U:'invalid username'.t(),
+        P: 'invalid password'.t(),
+        default: ''
+
+    },
+    loginFailureReason: function( value ){
+        if(Ext.isEmpty(value)) {
+            return '';
+        }
+        return ( value in Renderer.loginFailureReasonMap ) ? Renderer.loginFailureReasonMap[value] : Renderer.loginFailureReasonMap['default'];
+    },
+
+    loginSuccess: function( value ){
+        return value ?  'success'.t() : 'failed'.t();
+    },
+
+    loginFrom: function( value ){
+        return value ?  'local'.t() : 'remote'.t();
+    },
+
+    directoryConnectorActionMap: {
+        I: 'login'.t(),
+        U: 'update'.t(),
+        O: 'logout'.t(),
+        A: 'authenticate'.t(),
+        default: 'unknown'.t()
+
+    },
+    directoryConnectorAction: function( value ){
+        if(Ext.isEmpty(value)) {
+            return '';
+        }
+        return ( value in Renderer.directoryConnectorActionMap ) ? Renderer.directoryConnectorActionMap[value] : Renderer.directoryConnectorActionMap['default'];
+    },
+
+    directoryConnectorActionSourceMap: {
+        W: 'client'.t(),
+        A: 'active directory'.t(),
+        R: 'radius'.t(),
+        T: 'test'.t(),
+        default: 'unknown'.t()
+
+    },
+    directoryConnectorActionSource: function( value ){
+        if(Ext.isEmpty(value)) {
+            return '';
+        }
+        return ( value in Renderer.directoryConnectorActionSourceMap ) ? Renderer.directoryConnectorActionSourceMap[value] : Renderer.directoryConnectorActionSourceMap['default'];
+    },
+
+    adBlockerActionMap:{
+        B: 'block'.t(),
+        default: 'pass'.t()
+    },
+    adBlockerAction: function( value ){
+        if(Ext.isEmpty(value)) {
+            return '';
+        }
+        return ( value in Renderer.adBlockerActionMap ) ? Renderer.adBlockerActionMap[value] : Renderer.adBlockerActionMap['default'];
+    },
+
+    bandwidthControlRule: function( value ){
+        return Ext.isEmpty(value) ? 'none'.t() : value;
+    },
+
+    emailActionMap: {
+        P: 'pass message'.t(),
+        M: 'mark message'.t(),
+        D: 'drop message'.t(),
+        B: 'block message'.t(),
+        Q: 'quarantine message'.t(),
+        S: 'pass safelist message'.t(),
+        Z: 'pass oversize message'.t(),
+        O: 'pass outbound message'.t(),
+        F: 'block message (scan failure)'.t(),
+        G: 'pass message (scan failure)'.t(),
+        Y: 'block message (greylist)'.t(),
+        default:  'unknown action'.t()
+    },
+    emailAction: function(value){
+        if(Ext.isEmpty(value)) {
+            return '';
+        }
+        return Ext.String.format(
+                Renderer.mapValueFormat,
+                ( value in Renderer.emailActionMap ) ? Renderer.emailActionMap[value] : Renderer.emailActionMap['default'],
+                value
+        );
+    },
 
 });
