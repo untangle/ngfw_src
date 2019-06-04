0a442ddee764ef1893a7f572a7089fd7f87fd072
Dan Hadarau <dhadarau@untangle.com>
2019-06-04 12:04:41 +0300

    Fix JS error on report refresh
    NGFW-12474 #code-completed

--- uvm/js/common/reports/view/EventReport.js
+++ uvm/js/common/reports/view/EventReport.js
@@ -88,8 +88,8 @@ Ext.define('Ung.view.reports.EventReport', {
                 if(Util.isDestroyed(me, view)){
                     return;
                 }
-                if (!entry || entry.get('type') !== 'EVENT_LIST') { 
-                    return; 
+                if (!entry || entry.get('type') !== 'EVENT_LIST') {
+                    return;
                 }
 
                 // if rendered as widget, add to dashboard queue
@@ -179,7 +179,7 @@ Ext.define('Ung.view.reports.EventReport', {
         },
 
         onRefresh: function(){
-            if(this.tableConfig.refresh){
+            if(this.tableConfig && this.tableConfig.refresh){
                 this.tableConfig.refresh();
             }
         },
@@ -229,8 +229,8 @@ Ext.define('Ung.view.reports.EventReport', {
                         return;
                     }
                     me.getView().setLoading(false);
-                    if (reps) { 
-                        reps.getViewModel().set('fetching', false); 
+                    if (reps) {
+                        reps.getViewModel().set('fetching', false);
                     }
                     me.loadResultSet(result);
                 })
@@ -238,12 +238,12 @@ Ext.define('Ung.view.reports.EventReport', {
                     if(Util.isDestroyed(me)){
                         return;
                     }
-                    if (cb) { 
-                        cb(); 
+                    if (cb) {
+                        cb();
                     }
                     me.getView().setLoading(false);
-                    if (reps) { 
-                        reps.getViewModel().set('fetching', false); 
+                    if (reps) {
+                        reps.getViewModel().set('fetching', false);
                     }
                 });
         },
