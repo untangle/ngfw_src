a903bed3c2cd73707d053bed12a2bede1be2ad22
Chris Blaise <cblaise@untangle.com>
2019-07-22 10:47:12 -0400

    NGFW-12476 Add build depency for webroot-base

--- web-filter-base/package.rb
+++ web-filter-base/package.rb
@@ -1,11 +1,12 @@
 # -*-ruby-*-
 
 http = BuildEnv::SRC['untangle-app-http']
+webroot = BuildEnv::SRC['untangle-app-webroot']
 web_filter = BuildEnv::SRC['untangle-base-web-filter']
 
-AppBuilder.makeBase(BuildEnv::SRC, 'untangle-base-web-filter', 'web-filter-base', [http['src']])
+AppBuilder.makeBase(BuildEnv::SRC, 'untangle-base-web-filter', 'web-filter-base', [http['src'], webroot['src']])
 
-deps = [web_filter['src'], http['src']]
+deps = [web_filter['src'], http['src'], webroot['src']]
 
 ServletBuilder.new(web_filter, 'com.untangle.app.web_filter.jsp', "./web-filter-base/servlets/web-filter", [], deps, [], [BuildEnv::SERVLET_COMMON])
 
