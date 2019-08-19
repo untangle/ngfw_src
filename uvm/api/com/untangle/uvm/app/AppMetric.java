87f11f02e6c615a8610fcc7a0635849c3bc80e26
Chris Blaise <cblaise@untangle.com>
2019-08-19 15:35:27 -0400

    NGFW-12541 Expand metrics to include a new type for calculatng time over a counter.  Optional displayUnits field.  Optionally allow some statics to only be available in expert mode.

--- uvm/api/com/untangle/uvm/app/AppMetric.java
+++ uvm/api/com/untangle/uvm/app/AppMetric.java
@@ -13,10 +13,18 @@ import org.json.JSONString;
 @SuppressWarnings("serial")
 public class AppMetric implements Serializable, JSONString
 {
+    public enum Type { COUNTER, AVG_TIME };
     private String name;
     private String displayName;
+    private String displayUnits;
     private Long value;
-    
+    private Type type;
+
+    private Long timeValue;
+    private Long counterValue;
+
+    private boolean expert = false;
+
     public AppMetric() { }
 
     public AppMetric(String name, String displayName)
@@ -24,6 +32,7 @@ public class AppMetric implements Serializable, JSONString
         this.name = name;
         this.displayName = displayName;
         this.value = 0L;
+        this.type = Type.COUNTER;
     }
 
     public AppMetric(String name, String displayName, Long value)
@@ -31,6 +40,17 @@ public class AppMetric implements Serializable, JSONString
         this.name = name;
         this.displayName = displayName;
         this.value = value;
+        this.type = Type.COUNTER;
+    }
+
+    public AppMetric(String name, String displayName, Long value, Type type, String displayUnits, boolean expert)
+    {
+        this.name = name;
+        this.displayName = displayName;
+        this.value = value;
+        this.type = type;
+        this.displayUnits = displayUnits;
+        this.expert = expert;
     }
     
     public String getName() { return name; }
@@ -38,15 +58,38 @@ public class AppMetric implements Serializable, JSONString
 
     public String getDisplayName() { return displayName; }
     public void setDisplayName(String displayName) { this.displayName = displayName; }
-    
-    public Long getValue() { return value; }
+
+    public String getDisplayUnits() { return displayUnits; }
+    public void setDisplayUnits(String displayUnits) { this.displayUnits = displayUnits; }
+
+    public Long getValue() { return value;}
     public void setValue( Long value ) { this.value = value; }
+
+    public Type getType() { return type; }
+    public void setType(Type name) { this.type = type; }
+
+    public Long getTimeValue() { return timeValue; }
+    public void setTimeValue(Long timeValue) { this.timeValue = timeValue; }
+
+    public Long getCounterValue() { return counterValue; }
+    public void setCounterValue(Long counterValue) { this.counterValue = counterValue; }
+
+    public boolean getExpert() { return expert; }
+    public void setExpertValue(boolean expertValue) { this.expert = expertValue; }
+
+    public Long calculate(){
+        if(this.type == Type.AVG_TIME){
+            this.setValue(this.timeValue / this.counterValue);
+        }
+
+        return value;
+    }
 
     public String toString()
     {
         return "AppMetric[" + name + "] = " + value;
     }
-    
+
     public String toJSONString()
     {
         org.json.JSONObject jO = new org.json.JSONObject(this);
