811c2529da67e77dbfe730824f52692d03dadc20
Chris Blaise <cblaise@untangle.com>
2019-08-26 09:03:51 -0400

    NGFW-12541 Add new method to get local networks.

--- uvm/api/com/untangle/uvm/NetworkManager.java
+++ uvm/api/com/untangle/uvm/NetworkManager.java
@@ -59,4 +59,6 @@ public interface NetworkManager
     String getFullyQualifiedHostname();
 
     int getNextFreeInterfaceId(NetworkSettings netSettings, int minimum);
+
+    List<IPMaskedAddress> getLocalNetworks();
 }
