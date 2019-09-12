a407b48925daa5980fa88306d3c6c584b93d6037
Chris Blaise <cblaise@untangle.com>
2019-09-12 13:28:41 -0400

    NGFW-12576 Create separate session attachment interface thayt the base session and request interfaces extend.  This allows methods that want to look at attachments to look at an object of either class.

--- uvm/api/com/untangle/uvm/vnet/NewSessionRequest.java
+++ uvm/api/com/untangle/uvm/vnet/NewSessionRequest.java
@@ -6,7 +6,7 @@ package com.untangle.uvm.vnet;
 /**
  * Base interface for all new session requests.
  */
-public interface NewSessionRequest 
+public interface NewSessionRequest extends SessionAttachments
 {
     /**
      * <code>id</code> returns the session's unique identifier, a positive integer >= 1.
@@ -25,59 +25,4 @@ public interface NewSessionRequest
      */
     PipelineConnector pipelineConnector();
 
-    /**
-     * Attaches the given object to this session request.
-     *
-     * <p> An attached object may later be retrieved via the {@link #attachment
-     * attachment} method of the session itself.  (The attached object is automatically
-     * attached to the session, assuming the request is allowed).
-     * Only one object may be attached at a time; invoking
-     * this method causes any previous attachment to be discarded.  The current
-     * attachment may be discarded by attaching <tt>null</tt>.  </p>
-     *
-     * @param  ob
-     *         The object to be attached; may be <tt>null</tt>
-     *
-     * @return  The previously-attached object, if any,
-     *          otherwise <tt>null</tt>
-     */
-    Object attach(Object ob);
-    Object attach(String key, Object ob);
-
-    /**
-     * Retrieves the current attachment.  </p>
-     *
-     * @return  The object currently attached to this session,
-     *          or <tt>null</tt> if there is no attachment
-     */
-    Object attachment();
-    Object attachment(String key);
-
-    /**
-     * Attaches the given object to this session
-     * This is visible and modifiable by all Apps
-     *
-     * <p> An attached object may later be retrieved via the {@link
-     * #attachment attachment} method.  Only one object may be
-     * attached at a time for a given key; invoking this method
-     * causes any previous attachment to be discarded.  The
-     * current attachment may be discarded by attaching <tt>null</tt>.
-     *
-     * @param key The string key; may be <tt>null</tt>
-     * @param ob The object to be attached; may be <tt>null</tt>
-     *
-     * @return The previously-attached object, if any, otherwise
-     *          <tt>null</tt>
-     */
-    Object globalAttach(String key, Object ob);
-
-    /**
-     * Retrieves the current attachment.
-     *
-     * @param key The string key; may be <tt>null</tt>
-     * 
-     * @return The object currently attached to this session, or
-     *          <tt>null</tt> if there is no attachment
-     */
-    Object globalAttachment(String key);
 }
