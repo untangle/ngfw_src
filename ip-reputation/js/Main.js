1070cfded3b5a9cbc8de9485bc3c75a9ffe76c2a
Chris Blaise <cblaise@untangle.com>
2019-09-19 08:57:37 -0400

    NGFW-12577 Use new ip reputation common library

--- ip-reputation/js/Main.js
+++ ip-reputation/js/Main.js
@@ -1,3 +1,4 @@
+Ext.syncRequire('Ung.common.ipreputation');
 Ext.define('Ung.apps.ipreputation.Main', {
     extend: 'Ung.cmp.AppPanel',
     alias: 'widget.app-ip-reputation',
@@ -13,65 +14,5 @@ Ext.define('Ung.apps.ipreputation.Main', {
         { xtype: 'app-ip-reputation-status' },
         { xtype: 'app-ip-reputation-reputation' },
         { xtype: 'app-ip-reputation-pass' }
-    ],
-
-    statics: {
-        threatLevels: Ext.create('Ext.data.ArrayStore', {
-            fields: [ 'color', 'rangeBegin', 'rangeEnd', 'description', 'details' ],
-            sorters: [{
-                property: 'rangeBegin',
-                direction: 'ASC'
-            }],
-            data: [
-            [ 'e51313', 0,  20, 'High Risk'.t(), 'These are high risk IP addresses. There is a high predictive risk that these IPs will deliver attacks - such as malicious payloads, DoS attacks, or others - to your infrastructure and endpoints.'.t() ],
-            [ 'e8580c', 21, 40, 'Suspicious'.t(), 'These are suspicious IPs. There is a higher than average predictive risk that these IPs will deliver attacks to your infrastructure and endpoints.'.t() ],
-            [ 'ff9a22', 41, 60, 'Moderate Risk'.t(), 'These are generally benign IPs, but have exhibited some potential risk characteristics. There is some predictive risk that these IPs will deliver attacks to our infrastructure and endpoints.'.t() ],
-            [ '68af68', 61, 80, 'Low Risk'.t(), 'These are benign IPs and rarely exhibit characteristics that expose your infrastructure and endpoints to security risks. There is a low predictive risk of attack.'.t() ],
-            [ '408740', 81, 100, 'Trustworthy'.t(), 'These are clean IPs that have not been tied to a security risk. There is a very low predictive risk that your infrastructure and endpoints will be exposed to attack.'.t() ]
-            ]
-        }),
-
-        threaLevelsRenderer: function(value, meta, record){
-            return Ext.String.format("{0} ({1})".t(), record.get('description'), record.get('details'));
-        },
-
-        threats: Ext.create('Ext.data.ArrayStore', {
-            fields: [ 'bit', 'description', 'details' ],
-            sorters: [{
-                property: 'bit',
-                direction: 'ASC'
-            }],
-            data: [
-                ["0", 'Any'.t(), 'Any threat'.t()],
-                ["1", 'Spam Sources'.t(), 'IP addresses from rogue SMTP server connection attempts, anomalous mail activity detected on network sensors, and IPs correlated with known email and forum spammers.'.t() ],
-                ["2", 'Windows Exploits'.t(), 'IP addresses participating in distribution of malware or malware attacks, protocol exploits, recorded login attempts, information stealing and other advanced or nefarious shell code, rootkits, worms or virus-related activity.'.t() ],
-                ["3", 'Web Attacks'.t(), 'IP addresses using cross-site scripting, iFrame injection, SQL injection, cross-domain injection, domain password brute force attacks to target vulnerabilities on a web server, and malicious automated web activity.'.t()],
-                ["4", 'Botnets'.t(), 'IP addresses acting as botnet Command and Control (C&C) centers, infected zombie machines controlled by the C&C servers, botnet hosts distributing malware, cobalt beacons, suspicious fast flux activity, and ransomware reported C&C.'.t() ],
-                ["5", 'Scanners'.t(), 'IP addresses involved in unauthorized reconnaissance activities such as probing, host scanning, port scanning, and brute force login attempts, as well as repetitive and suspicious programmatic behavior observed in regular time intervals.'.t() ],
-                ["6", 'Denal of Service'.t(), 'Scanners category includes all reconnaissance such as probes, host scan, domain scan and password brute force attack.'.t() ],
-                ["7", 'Reputation'.t(), 'IP addresses with a low Reputation Index score. IP addresses in this category have a higher than average risk of hosting and/or distributing malicious content or files.'.t() ],
-                ["8", 'Phishing'.t(), 'IP addresses that are hosting phishing sites or initiating suspicious entrapment activities through malicious ads, web email POST forms, and click fraud.'.t() ],
-                ["9", 'Proxy'.t(), 'IP addresses offering anonymization services, as well as those known to have been associated with open web proxy, VPN, or suspicious proxy chains.'.t() ],
-                ["12", 'Mobile Threats'.t(), 'IP addresses associated with malicious mobile application distribution or with attacks initiating from a mobile device.'.t() ],
-                ["14", 'Tor Proxy'.t(), 'IP addresses associated with Tor Proxy activity such as exit nodes or suspicious IPs associated with Tor activity.'.t() ],
-            ]
-        }),
-
-        threatsRenderer: function(value, meta, record){
-            return record.get('description');
-        },
-
-        actions: Ext.create('Ext.data.ArrayStore', {
-            fields: [ 'value', 'description'],
-            sorters: [{
-                property: 'value',
-                direction: 'ASC'
-            }],
-            data: [
-                ["block", 'Block'.t()],
-                ["pass", 'Pass'.t()]
-            ]
-        }),
-    }
-
+    ]
 });
