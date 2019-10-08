51f038325e7a99b71509016df8a198787eda65a3
Chris Blaise <cblaise@untangle.com>
2019-10-08 13:11:02 -0400

    NGFW-12609 Add http dependency.

--- ip-reputation/package.rb
+++ ip-reputation/package.rb
@@ -1,4 +1,11 @@
 # -*-ruby-*-
+http = BuildEnv::SRC['untangle-app-http']
 webroot = BuildEnv::SRC['untangle-app-webroot']
+ip_reputation = BuildEnv::SRC['untangle-app-ip-reputation']
 
-AppBuilder.makeApp(BuildEnv::SRC, 'untangle-app-ip-reputation', 'ip-reputation', [webroot['src']])
+AppBuilder.makeApp(BuildEnv::SRC, 'untangle-app-ip-reputation', 'ip-reputation', [http['src'], webroot['src']])
+
+deps = [ip_reputation['src'], http['src'], webroot['src']]
+
+ServletBuilder.new(ip_reputation, 'com.untangle.app.ip_reputation.jsp', "./ip-reputation/servlets/ip-reputation", [], deps, [], [BuildEnv::SERVLET_COMMON])
+
