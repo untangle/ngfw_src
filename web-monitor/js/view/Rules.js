329dcda96a919d21196f19594fa37af409e99c13
Chris Blaise <cblaise@untangle.com>
2019-10-09 12:00:18 -0400

    NGFW-12634 Rename conditions

--- web-monitor/js/view/Rules.js
+++ web-monitor/js/view/Rules.js
@@ -132,10 +132,10 @@ Ext.define('Ung.apps.webmonitor.view.Rules', {
             'CLIENT_COUNTRY',
             'SERVER_COUNTRY',
 
-            'IP_REPUTATION_SRC_REPUTATION',
-            'IP_REPUTATION_SRC_THREATMASK',
-            'IP_REPUTATION_DST_REPUTATION',
-            'IP_REPUTATION_DST_THREATMASK'
+            'THREAT_PREVENTION_SRC_REPUTATION',
+            'THREAT_PREVENTION_SRC_THREATMASK',
+            'THREAT_PREVENTION_DST_REPUTATION',
+            'THREAT_PREVENTION_DST_THREATMASK'
         ]),
         Field.flagged,
     ]
