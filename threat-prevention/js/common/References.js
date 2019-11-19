638afdd9b32ccf95770da57f6527dfec3e30d145
Chris Blaise <cblaise@untangle.com>
2019-11-19 13:17:07 -0500

    NGFW-12712 Reverse threat reputation bar.

--- threat-prevention/js/common/References.js
+++ threat-prevention/js/common/References.js
@@ -6,14 +6,14 @@ Ext.define('Ung.common.threatprevention.references', {
         fields: [ 'color', 'rangeBegin', 'rangeEnd', 'description', 'details' ],
         sorters: [{
             property: 'rangeBegin',
-            direction: 'ASC'
+            direction: 'DESC'
         }],
         data: [
-        [ 'e51313', 0,  20, 'High Risk'.t(), 'These are high risk IP addresses. There is a high predictive risk that these IPs will deliver attacks - such as malicious payloads, DoS attacks, or others - to your infrastructure and endpoints.'.t() ],
-        [ 'e8580c', 21, 40, 'Suspicious'.t(), 'These are suspicious IPs. There is a higher than average predictive risk that these IPs will deliver attacks to your infrastructure and endpoints.'.t() ],
-        [ 'ff9a22', 41, 60, 'Moderate Risk'.t(), 'These are generally benign IPs, but have exhibited some potential risk characteristics. There is some predictive risk that these IPs will deliver attacks to our infrastructure and endpoints.'.t() ],
+        [ '408740', 81, 100, 'Trustworthy'.t(), 'These are clean IPs that have not been tied to a security risk. There is a very low predictive risk that your infrastructure and endpoints will be exposed to attack.'.t() ],
         [ '68af68', 61, 80, 'Low Risk'.t(), 'These are benign IPs and rarely exhibit characteristics that expose your infrastructure and endpoints to security risks. There is a low predictive risk of attack.'.t() ],
-        [ '408740', 81, 100, 'Trustworthy'.t(), 'These are clean IPs that have not been tied to a security risk. There is a very low predictive risk that your infrastructure and endpoints will be exposed to attack.'.t() ]
+        [ 'ff9a22', 41, 60, 'Moderate Risk'.t(), 'These are generally benign IPs, but have exhibited some potential risk characteristics. There is some predictive risk that these IPs will deliver attacks to our infrastructure and endpoints.'.t() ],
+        [ 'e8580c', 21, 40, 'Suspicious'.t(), 'These are suspicious IPs. There is a higher than average predictive risk that these IPs will deliver attacks to your infrastructure and endpoints.'.t() ],
+        [ 'e51313', 0,  20, 'High Risk'.t(), 'These are high risk IP addresses. There is a high predictive risk that these IPs will deliver attacks - such as malicious payloads, DoS attacks, or others - to your infrastructure and endpoints.'.t() ],
         ]
     }),
 
@@ -47,7 +47,7 @@ Ext.define('Ung.common.threatprevention.references', {
             ["3", 'Web Attacks'.t(), 'IP addresses using cross-site scripting, iFrame injection, SQL injection, cross-domain injection, domain password brute force attacks to target vulnerabilities on a web server, and malicious automated web activity.'.t()],
             ["4", 'Botnets'.t(), 'IP addresses acting as botnet Command and Control (C&C) centers, infected zombie machines controlled by the C&C servers, botnet hosts distributing malware, cobalt beacons, suspicious fast flux activity, and ransomware reported C&C.'.t() ],
             ["5", 'Scanners'.t(), 'IP addresses involved in unauthorized reconnaissance activities such as probing, host scanning, port scanning, and brute force login attempts, as well as repetitive and suspicious programmatic behavior observed in regular time intervals.'.t() ],
-            ["6", 'Denal of Service'.t(), 'Scanners category includes all reconnaissance such as probes, host scan, domain scan and password brute force attack.'.t() ],
+            ["6", 'Denial of Service'.t(), 'Scanners category includes all reconnaissance such as probes, host scan, domain scan and password brute force attack.'.t() ],
             ["7", 'Reputation'.t(), 'IP addresses with a low Reputation Index score. IP addresses in this category have a higher than average risk of hosting and/or distributing malicious content or files.'.t() ],
             ["8", 'Phishing'.t(), 'IP addresses that are hosting phishing sites or initiating suspicious entrapment activities through malicious ads, web email POST forms, and click fraud.'.t() ],
             ["9", 'Proxy'.t(), 'IP addresses offering anonymization services, as well as those known to have been associated with open web proxy, VPN, or suspicious proxy chains.'.t() ],
