512ca2e31982eda7c40bf226e080cb596b660983
John Sommerville <jsommerville@untangle.com>
2020-01-15 15:09:13 -0700

    fix model typings, add comments

--- threat-prevention/js/MainModel.js
+++ threat-prevention/js/MainModel.js
@@ -19,19 +19,20 @@ Ext.define('Ung.apps.threatprevention.MainModel', {
         }
     }
 });
-
+/**
+ * ThreatLookupInfo is a model to represent the threat info that should be displayed on the Lookup Threat Tab
+ */
 Ext.define('Ung.apps.threatprevention.ThreatLookupInfo', {
     extend: 'Ext.data.Model',
     fields: [
          {name: 'inputVal', type: 'string'},
          {name: 'address', type: 'string'},
-         {name: 'score', type: 'string'},
-         {name: 'level', type: 'string'},
-         {name: 'levelDetails', type: 'string'},
-         {name: 'popularity', type: 'string'},
+         {name: 'score', type: 'int'},
+         {name: 'level', type: 'int'},
+         {name: 'popularity', type: 'int'},
          {name: 'threathistory', type: 'string'},
          {name: 'country', type: 'string'},
-         {name: 'age', type: 'string'}
+         {name: 'age', type: 'int'}
     ],
 
     hasMany: [{
@@ -44,6 +45,9 @@ Ext.define('Ung.apps.threatprevention.ThreatLookupInfo', {
 
 });
 
+/**
+ * ThreatCategories model contains confidence% and category ID of a threat that shows up in the Threat History or in the Threat Lookup Info
+ */
 Ext.define('Ung.apps.threatprevention.ThreatCategories', {
     extend: 'Ext.data.Model',
     fields: [
@@ -52,6 +56,9 @@ Ext.define('Ung.apps.threatprevention.ThreatCategories', {
     ]
 });
 
+/**
+ * ThreatHistory contains a timestamp and any categories associated with historical data about the threat
+ */
 Ext.define('Ung.apps.threatprevention.ThreatHistory', {
     extend: 'Ext.data.Model',
     fields: [
