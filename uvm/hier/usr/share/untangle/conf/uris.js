1e7044d4ce12f7008f068acc993294ce13d21c7d
Chris Blaise <cblaise@untangle.com>
2020-01-20 11:52:55 -0500

    NGFW-12808 New uri translation manager

---
+++ uvm/hier/usr/share/untangle/conf/uris.js
@@ -0,0 +1,40 @@
+{
+    "javaClass": "com.untangle.uvm.UriManagerSettings",
+    "uriTranslations": {
+        "javaClass": "java.util.LinkedList",
+        "list": [
+            {
+                "uri": "https://ids.untangle.com/suricatasignatures.tar.gz",
+                "javaClass": "com.untangle.uvm.UriTranslation"
+            },{
+                "uri": "https://labs.untangle.com/",
+                "javaClass": "com.untangle.uvm.UriTranslation"
+            },{
+                "uri": "relay.untangle.com",
+                "javaClass": "com.untangle.uvm.UriTranslation"
+            },{
+                "uri": "https://telemetry.untangle.com",
+                "javaClass": "com.untangle.uvm.UriTranslation"
+            },{
+                "uri": "https://updates.untangle.com",
+                "javaClass": "com.untangle.uvm.UriTranslation"
+            },{
+                "uri": "https://cmd.untangle.com",
+                "javaClass": "com.untangle.uvm.UriTranslation"
+            },{
+                "uri": "https://license.untangle.com",
+                "javaClass": "com.untangle.uvm.UriTranslation"
+            },{
+                "uri": "https://classify.untangle.com",
+                "javaClass": "com.untangle.uvm.UriTranslation"
+            },{
+                "uri": "https://bd.untangle.com",
+                "javaClass": "com.untangle.uvm.UriTranslation"
+            },{
+                "uri": "https://backup.untangle.com",
+                "javaClass": "com.untangle.uvm.UriTranslation"
+            }
+        ]
+    },
+    "version": 1
+}
