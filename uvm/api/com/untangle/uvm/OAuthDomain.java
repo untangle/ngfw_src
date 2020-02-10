034de0ea6f9b2f1af8bf573f71659d418cd98210
Chris Blaise <cblaise@untangle.com>
2020-02-10 11:09:57 -0500

    NGFW-12808 Add new authentication manager to process oauth conf with callback hook for uri settings.

---
+++ uvm/api/com/untangle/uvm/OAuthDomain.java
@@ -0,0 +1,15 @@
+/**
+ * $Id$
+ */
+
+package com.untangle.uvm;
+
+/**
+  * Holds the details of an OAuth domain
+*/
+class OAuthDomain
+{
+    String provider;
+    String match;
+    String name;
+}
