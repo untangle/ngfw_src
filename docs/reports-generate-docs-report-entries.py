07b8aad33e30ed49b9c47799acf4ad11852783c2
Chris Blaise <cblaise@untangle.com>
2020-02-19 10:18:40 -0500

    NGFW-12842 Look at json extensions.

--- docs/reports-generate-docs-report-entries.py
+++ docs/reports-generate-docs-report-entries.py
@@ -9,7 +9,7 @@ import traceback
 dict = {}
 
 # read the report entries and put in dictionary
-p = subprocess.Popen(["sh","-c","/usr/bin/find /usr/share/untangle/lib -path '*/reports/*.js' -print"], stdout=subprocess.PIPE)
+p = subprocess.Popen(["sh","-c","/usr/bin/find /usr/share/untangle/lib -path '*/reports/*.json' -print"], stdout=subprocess.PIPE)
 for line in iter(p.stdout.readline, ''):
     if 'appProperties.json' in line: continue
     filename = line.strip()
