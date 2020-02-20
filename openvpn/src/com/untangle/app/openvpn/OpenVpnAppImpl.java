2e39a8440ceb6cb5d2e4ac4393ae9432be85c236
John Sommerville <jsommerville@untangle.com>
2020-02-20 08:42:38 -0700

    Remove the validation to verify client/server keys that should be matching

--- openvpn/src/com/untangle/app/openvpn/OpenVpnAppImpl.java
+++ openvpn/src/com/untangle/app/openvpn/OpenVpnAppImpl.java
@@ -764,25 +764,6 @@ public class OpenVpnAppImpl extends AppBase
                 }
             }
         }
-
-        /**
-         * Compare kvps that should be matching between client and server configuration settings
-         * verify they match, or throw an error
-         */
-        List<String> matchedSettings = new ArrayList<>(Arrays.asList("proto", "tls-auth", "cipher", "compress"));
-        List<OpenVpnConfigItem> clientSettings = newSettings.getClientConfiguration(matchedSettings);
-        List<OpenVpnConfigItem> serverSettings = newSettings.getServerConfiguration(matchedSettings);
-
-        for (OpenVpnConfigItem serverCfgItem : serverSettings) {
-            for (OpenVpnConfigItem clientCfgItem : clientSettings) {
-                if (serverCfgItem.getOptionName().equals(clientCfgItem.getOptionName())) {
-                    if (!java.util.Objects.equals(serverCfgItem.getOptionValue(), clientCfgItem.getOptionValue())) {
-                        throw new RuntimeException(I18nUtil.marktr("Invalid Settings") + ": " + I18nUtil.marktr("OpenVPN Configuration Option:") + " " + serverCfgItem.getOptionName() + " " + I18nUtil.marktr("does not match between server configuration:") + " " + serverCfgItem.getOptionValue()+ " "  + I18nUtil.marktr("and client configuration:") + " " + clientCfgItem.getOptionValue());
-                    }
-                }
-            }
-        }
-
     }
 
     /**
