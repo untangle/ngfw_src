32f8061505f0e8480b88abd235bd2a5cd2537556
mahotz <mahotz@untangle.com>
2020-03-16 11:51:18 -0700

    Fix Last Handhake display
    The Wireguard Status now uses the timestamp renderer and filter
    so the Last Handshake column displays useful information.

--- wireguard-vpn/js/view/Status.js
+++ wireguard-vpn/js/view/Status.js
@@ -93,7 +93,9 @@ Ext.define('Ung.apps.wireguard-vpn.view.Status', {
                 }, {
                     header: 'Last Handshake'.t(),
                     dataIndex: 'latest-handshake',
-                    width: Renderer.timestampWidth
+                    width: Renderer.timestampWidth,
+                    renderer: Renderer.timestamp,
+                    filter: Renderer.timestampFilter
                 }, {
                     header: 'Keepalive'.t(),
                     dataIndex: 'persistent-keepalive',
