d2a0e68c3fce048b1502289d26c8b3337ea488b7
Sébastien Delafond <sdelafond@gmail.com>
2020-04-01 10:37:35 +0200

    uvm/python: for both python2.7 & python3, clean up i18n_helper and allow relative import of uvm
    Comment: NGFW-12976

--- uvm/hier/usr/lib/python3/dist-packages/uvm/__init__.py
+++ uvm/hier/usr/lib/python3/dist-packages/uvm/__init__.py
@@ -1,3 +1,8 @@
+import sys
+
+if "@PREFIX@" != '':
+    sys.path.insert(0, '@PREFIX@/usr/lib/python3/dist-packages')
+
 from uvm.app_manager import AppManager
 from uvm.system_manager import SystemManager
 from uvm.logging_manager import LoggingManager
