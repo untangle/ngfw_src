62fa520a89191122a3a46ff0bb3265df9b18f3d8
mahotz <mahotz@untangle.com>
2020-04-09 16:57:30 -0700

    Enable authentication logging in the freeradius config
    We now create and manage our client config directly in the
    radiusd.conf file and also turn on auth request logging which we'll
    need for reporting purposes.

--- uvm/impl/com/untangle/uvm/LocalDirectoryImpl.java
+++ uvm/impl/com/untangle/uvm/LocalDirectoryImpl.java
@@ -37,7 +37,7 @@ public class LocalDirectoryImpl implements LocalDirectory
 
     private final static String FREERADIUS_LOCAL_SECRETS = "/etc/freeradius/3.0/mods-config/files/untangle.local";
     private final static String FREERADIUS_AUTHORIZE = "/etc/freeradius/3.0/mods-config/files/authorize";
-    private final static String FREERADIUS_CLIENTS = "/etc/freeradius/3.0/clients.conf";
+    private final static String FREERADIUS_RADIUSD = "/etc/freeradius/3.0/radiusd.conf";
 
     private final static String UNCHANGED_PASSWORD = "***UNCHANGED***";
     private final static String FILE_DISCLAIMER = "# This file is created and maintained by the Untangle Local Directory.\n" + "# If you modify this file manually, your changes will be overwritten!\n\n";
@@ -478,8 +478,8 @@ public class LocalDirectoryImpl implements LocalDirectory
         }
 
         /*
-         * Create the authorize file with the package defaults and a line to
-         * include our local credentials.
+         * Create the authorize file with the freeradius package defaults and a
+         * line to include our local credentials.
          */
         try {
             fw = new FileWriter(FREERADIUS_AUTHORIZE, false);
@@ -505,28 +505,95 @@ public class LocalDirectoryImpl implements LocalDirectory
         }
 
         /*
-         * Create the clients.conf
+         * Create the radiusd.conf file with most of the freeradius package
+         * defaults. The main change here is we create our client definition
+         * inline here rather than including and maintaining a separate
+         * clients.conf as would be the case with the default configuration. We
+         * also enable the logging stuff for reporting purposes.
          */
         try {
-            fw = new FileWriter(FREERADIUS_CLIENTS, false);
+            fw = new FileWriter(FREERADIUS_RADIUSD, false);
             fw.write(FILE_DISCLAIMER);
             if (systemSettings.getRadiusServerEnabled()) {
+                fw.write("prefix = /usr\n");
+                fw.write("exec_prefix = /usr\n");
+                fw.write("sysconfdir = /etc\n");
+                fw.write("localstatedir = /var\n");
+                fw.write("sbindir = ${exec_prefix}/sbin\n");
+                fw.write("logdir = /var/log/freeradius\n");
+                fw.write("raddbdir = /etc/freeradius/3.0\n");
+                fw.write("radacctdir = ${logdir}/radacct\n");
+                fw.write("name = freeradius\n");
+                fw.write("confdir = ${raddbdir}\n");
+                fw.write("modconfdir = ${confdir}/mods-config\n");
+                fw.write("certdir = ${confdir}/certs\n");
+                fw.write("cadir = ${confdir}/certs\n");
+                fw.write("run_dir = ${localstatedir}/run/${name}\n");
+                fw.write("db_dir = ${raddbdir}\n");
+                fw.write("libdir = /usr/lib/freeradius\n");
+                fw.write("pidfile = ${run_dir}/${name}.pid\n");
+                fw.write("correct_escapes = true\n");
+                fw.write("max_request_time = 30\n");
+                fw.write("cleanup_delay = 5\n");
+                fw.write("max_requests = 16384\n");
+                fw.write("hostname_lookups = no\n");
+                fw.write("log {\n");
+                fw.write("\tdestination = files\n");
+                fw.write("\tcolourise = yes\n");
+                fw.write("\tfile = ${logdir}/radius.log\n");
+                fw.write("\tsyslog_facility = daemon\n");
+                fw.write("\tstripped_names = no\n");
+                fw.write("\tauth = yes\n");
+                fw.write("\tauth_badpass = yes\n");
+                fw.write("\tauth_goodpass = yes\n");
+                fw.write("\tmsg_goodpass = \"UT_RADIUS_GOOD\"\n");
+                fw.write("\tmsg_badpass = \"UT_RADIUS_FAIL\"\n");
+                fw.write("\tmsg_denied = \"Access Denied\"\n");
+                fw.write("}\n");
+                fw.write("checkrad = ${sbindir}/checkrad\n");
+                fw.write("security {\n");
+                fw.write("\tuser = freerad\n");
+                fw.write("\tgroup = freerad\n");
+                fw.write("\tallow_core_dumps = no\n");
+                fw.write("\tmax_attributes = 200\n");
+                fw.write("\treject_delay = 1\n");
+                fw.write("\tstatus_server = yes\n");
+                fw.write("}\n");
+                fw.write("proxy_requests = yes\n");
+                fw.write("$INCLUDE proxy.conf\n");
                 fw.write("client untangle {\n");
                 fw.write("\tipaddr = 0.0.0.0/0\n");
                 fw.write("\tproto = *\n");
                 fw.write("\tsecret = " + systemSettings.getRadiusServerSecret() + "\n");
                 fw.write("}\n");
+                fw.write("thread pool {\n");
+                fw.write("\tstart_servers = 5\n");
+                fw.write("\tmax_servers = 32\n");
+                fw.write("\tmin_spare_servers = 3\n");
+                fw.write("\tmax_spare_servers = 10\n");
+                fw.write("\tmax_requests_per_server = 0\n");
+                fw.write("\tauto_limit_acct = no\n");
+                fw.write("}\n");
+                fw.write("modules {\n");
+                fw.write("$INCLUDE mods-enabled/\n");
+                fw.write("}\n");
+                fw.write("instantiate {\n");
+                fw.write("}\n");
+                fw.write("policy {\n");
+                fw.write("$INCLUDE policy.d/\n");
+                fw.write("}\n");
+                fw.write("$INCLUDE sites-enabled/\n");
             }
             fw.flush();
             fw.close();
         } catch (Exception exn) {
-            logger.error("Exception creating RADIUS clients file", exn);
+            logger.error("Exception creating RADIUS radiusd file", exn);
         } finally {
             if (fw != null) {
                 try {
                     fw.close();
                 } catch (IOException ex) {
-                    logger.error("Exception closing RADIUS clients file", ex);
+                    logger.error("Exception closing RADIUS radiusd file", ex);
                 }
             }
         }
