121b4bc480df005bc24bf70233d5fe0dd44df58a
John Sommerville <jsommerville@untangle.com>
2020-04-09 12:57:42 -0600

    Revert "buildtools: Add new tomcat Jars, remove tomcat log4j reference"
    This reverts commit d6fa1d4973c9339486e3dd16815873b836504f0f.

--- buildtools/jars.rb
+++ buildtools/jars.rb
@@ -47,7 +47,6 @@ class Jars
     const_set(:TomcatCommon, [ 'tomcat-embed-jasper.jar',
                                'tomcat-embed-core.jar',
                                'tomcat-embed-el.jar',
-                               'annotations-api.jar',
                             ].map do |n|
                 Jars.downloadTarget("apache-tomcat-8.5.53-embed/#{n}")
               end)
@@ -57,8 +56,12 @@ class Jars
                 Jars.downloadTarget("apache-tomcat-8.5.53-embed/#{n}")
               end)
 
+    const_set(:TomcatLogging, ['tomcat-embed-logging-log4j.jar',
+                         ].map do |n|
+                Jars.downloadTarget("apache-tomcat-8.5.53-embed/#{n}")
+             end)
 
-    const_set(:TomcatEmb, TomcatCommon + TomcatServer + [Jars.downloadTarget('httpcomponents-client-4.5.1/lib/commons-logging-1.2.jar')])
+    const_set(:TomcatEmb, TomcatCommon + TomcatServer + [Jars.downloadTarget('httpcomponents-client-4.5.1/lib/commons-logging-1.2.jar')] +  TomcatLogging)
 
     ## Miscellaneous Jars
     const_set(:JavaMail, [ Jars.downloadTarget('javamail-1.3.3_01/mail.jar') ])
