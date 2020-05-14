feedd365c71caf3f144a5f62ff0e0cfb0dbfcdd1
Chris Blaise <cblaise@untangle.com>
2020-05-14 16:34:38 -0400

    NGFW-12962 New remote configuration popup for remote client QR/configuration

---
+++ wireguard-vpn/js/RemoteConfigController.js
@@ -0,0 +1,44 @@
+Ext.define('Ung.apps.wireguard-vpn.RemoteconfigController', {
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.app-wireguard-vpn-remote-config',
+
+    control: {
+        '#': {
+            afterrender: 'afterrender'
+        }
+    },
+
+    afterrender: function(view){
+        var me = this,
+            vm = me.getViewModel(),
+            appManager = view.up('app-wireguard-vpn').appManager,
+            publicKey = view.record.get('publicKey');
+
+        view.setLoading(true);
+        Ext.Deferred.sequence([
+            Rpc.asyncPromise(appManager, 'createRemoteQrCode', publicKey),
+            Rpc.asyncPromise(appManager, 'getRemoteConfig', publicKey)
+        ], this).then( function(result){
+            if(Util.isDestroyed(view)){
+                return;
+            }
+
+            view.down('[itemId=qrcode]').setSrc('data:image/png;base64,' + result[0]);
+            view.down('[itemId=file]').setHtml('<pre>' + result[1] + '</pre>');
+
+            view.setLoading(false);
+        },function(ex){
+            if(!Util.isDestroyed(view)){
+                view.setLoading(false);
+            }
+            Util.handleException(ex);
+        });
+
+        vm.set('type', 'qrcode');
+    },
+
+    closeWindow: function (button) {
+        button.up('window').close();
+    },
+
+});
