a6e5fc94c4bc9d0dd23d037739f2aa9f894ca28a
John Sommerville <jsommerville@untangle.com>
2020-05-28 15:14:53 -0600

    remove dummy param from generateCertificateAuthority UI

--- uvm/servlets/admin/config/administration/MainController.js
+++ uvm/servlets/admin/config/administration/MainController.js
@@ -418,7 +418,7 @@ Ext.define('Ung.config.administration.MainController', {
 
         if (certMode === 'ROOT') {
             me.certDialog.setLoading(true);
-            Rpc.asyncData('rpc.UvmContext.certificateManager.generateCertificateAuthority', certSubject, altNames)
+            Rpc.asyncData('rpc.UvmContext.certificateManager.generateCertificateAuthority', certSubject)
             .then(function (result) {
                 if(Util.isDestroyed(me)){
                     return;
