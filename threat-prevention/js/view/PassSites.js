8b24c844e282d3e5c14ae8f311d1b6104b96c0cb
Chris Blaise <cblaise@untangle.com>
2020-06-17 14:09:12 -0400

    NGFW-12972 New Pass Sites tab.

---
+++ threat-prevention/js/view/PassSites.js
@@ -0,0 +1,74 @@
+Ext.define('Ung.apps.threatprevention.view.PassSites', {
+    extend: 'Ung.cmp.Grid',
+    alias: 'widget.app-threat-prevention-pass-sites',
+    itemId: 'pass-sites',
+    title: 'Pass Sites'.t(),
+    scrollable: true,
+
+    dockedItems: [{
+        xtype: 'toolbar',
+        dock: 'top',
+        items: ['@add', '->', '@import', '@export']
+    }],
+
+    recordActions: ['edit', 'delete', 'reorder'],
+
+    listProperty: 'settings.passSites.list',
+
+    emptyText: 'No Pass Sites defined'.t(),
+
+    emptyRow: {
+        string: '',
+        enabled: true,
+        description: '',
+        javaClass: 'com.untangle.uvm.app.GenericRule'
+    },
+
+    bind: '{passSites}',
+
+    columns: [{
+        header: 'Site'.t(),
+        width: Renderer.uriWidth,
+        flex: 1,
+        dataIndex: 'string',
+        editor: {
+            xtype: 'textfield',
+            emptyText: '[enter site]'.t(),
+            allowBlank: false
+        }
+    }, {
+        xtype: 'checkcolumn',
+        width: Renderer.booleanWidth,
+        header: 'Pass'.t(),
+        dataIndex: 'enabled',
+        resizable: false
+    }, {
+        header: 'Description'.t(),
+        width: Renderer.messageWidth,
+        flex: 2,
+        dataIndex: 'description',
+        editor: {
+            xtype: 'textfield',
+            emptyText: '[no description]'.t()
+        }
+    }],
+    editorFields: [{
+        xtype: 'textfield',
+        bind: '{record.string}',
+        fieldLabel: 'Site'.t(),
+        emptyText: '[enter site]'.t(),
+        allowBlank: false,
+        width: 400
+    }, {
+        xtype: 'checkbox',
+        bind: '{record.enabled}',
+        fieldLabel: 'Pass'.t()
+    }, {
+        xtype: 'textarea',
+        bind: '{record.description}',
+        fieldLabel: 'Description'.t(),
+        emptyText: '[no description]'.t(),
+        width: 400,
+        height: 60
+    }]
+});
