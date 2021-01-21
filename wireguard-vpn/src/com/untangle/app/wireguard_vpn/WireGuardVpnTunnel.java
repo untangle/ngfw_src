2ad9a7a813aadb1095229981141e3f71f766c165
Tiffany Kalin <tkalin@untangle.com>
2021-01-21 13:12:02 -0700

    NGFW-13305: Add in conversion for 16.3 and below

--- wireguard-vpn/src/com/untangle/app/wireguard_vpn/WireGuardVpnTunnel.java
+++ wireguard-vpn/src/com/untangle/app/wireguard_vpn/WireGuardVpnTunnel.java
@@ -27,7 +27,8 @@ public class WireGuardVpnTunnel implements Serializable, JSONString
     private String publicKey = "";
     private String privateKey = "";
     private Boolean endpointDynamic = true;
-    private String endpointHostname;
+    private InetAddress endpointAddress = null;
+    private String endpointHostname = "";
     private Integer endpointPort = 51820;
     private InetAddress peerAddress;
     private String networks = "";
@@ -53,6 +54,9 @@ public class WireGuardVpnTunnel implements Serializable, JSONString
 
     public Boolean getEndpointDynamic() { return endpointDynamic; }
     public void setEndpointDynamic( Boolean newValue ) { this.endpointDynamic = newValue; }
+
+    public InetAddress getEndpointAddress() { return endpointAddress; }
+    public void setEndpointAddress( InetAddress newValue ) { this.endpointAddress = newValue; }
 
     public String getEndpointHostname() { return endpointHostname; }
     public void setEndpointHostname( String newValue ) { this.endpointHostname = newValue; }
