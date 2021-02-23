5b2b744d7343909ca1559f0e74ca33abb710bb50
Sébastien Delafond <sdelafond@gmail.com>
2021-02-23 17:04:52 +0100

    pytest: conftest refactored, and now also allows --quick-tests-only
    Comment: NGFW-13490

--- uvm/hier/usr/lib/python3/dist-packages/tests/conftest.py
+++ uvm/hier/usr/lib/python3/dist-packages/tests/conftest.py
@@ -3,20 +3,45 @@ import pytest
 
 import runtests
 
-# these need to be transformed into command-line switches as well
+# FIXME: these need to be transformed into command-line switches as well
 runtests.remote_control.external_interface = 1
 runtests.remote_control.interface = 2
 
-def pytest_addoption(parser):
-    parser.addoption("--runtests-host", type=foo, default="192.168.122.122", help="client IP")
-    parser.addoption("--skip-instantiated", type=bar, default=True, help="skip tests if app is already instantiated")
 
-def foo(host):
-    runtests.remote_control.client_ip = host
-    return host
+def is_enabled(value):
+    return value in ['true', 'True', '1', 't', 'y', 'yes']
 
-def bar(skip_instantiated):
-    runtests.skip_instantiated = skip_instantiated in ['true', 'True', '1', 't', 'y', 'yes']
+
+def set_client_ip(value):
+    runtests.remote_control.client_ip = value
+    return runtests.remote_control.client_ip
+
+
+def set_skip_instantiated(value):
+    runtests.skip_instantiated = is_enabled(value)
     return runtests.skip_instantiated
+
+
+def set_quick_tests_only(value):
+    runtests.quick_tests_only = is_enabled(value)
+    return runtests.quick_tests_only
+
+
+def pytest_addoption(parser):
+    parser.addoption("--runtests-host",
+                     type=set_client_ip,
+                     default="192.168.122.122",
+                     help="client IP")
+
+    parser.addoption("--skip-instantiated",
+                     type=set_skip_instantiated,
+                     default=True,
+                     help="skip tests if app is already instantiated")
+
+    parser.addoption("--quick-tests-only",
+                     type=set_quick_tests_only,
+                     default=False,
+                     help="only run quick tests")
+
 
 runtests.test_start_time = datetime.datetime.now()
