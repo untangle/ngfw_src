f4410dac04d2b29db1274cea51d01258e55f31bd
Chris Blaise <cblaise@untangle.com>
2021-03-31 17:56:45 -0400

    NGFW-13685 Look at setup flag and if remote, go directly to wizard.  Otherwise, still go to language selection.

--- uvm/servlets/setup/src/com/untangle/uvm/setup/servlet/Welcome.java
+++ uvm/servlets/setup/src/com/untangle/uvm/setup/servlet/Welcome.java
@@ -19,7 +19,8 @@ import com.untangle.uvm.UvmContextFactory;
 public class Welcome extends HttpServlet
 {
     private static final String ADMIN_URL = "/admin/index.do";
-    private static final String SETUP_URL = "/setup/language.do";
+    private static final String LANGUAGE_URL = "/setup/language.do";
+    private static final String SETUP_URL = "/setup/setup.do";
         
     /**
      * doGet - handle GET requests
@@ -34,7 +35,15 @@ public class Welcome extends HttpServlet
         String url = ADMIN_URL;
 
         /* If the setup wizard is not complete send them to the setup page. */
-        if ( !UvmContextFactory.context().isWizardComplete() ) url = SETUP_URL;
+        if ( !UvmContextFactory.context().isWizardComplete() ){
+            if ( UvmContextFactory.context().isRemoteSetup() ){
+                // For remote setup, jump right into the wizard.
+                url = SETUP_URL;
+            }else{
+                // For local setup, select language first.
+                url = LANGUAGE_URL;
+            }
+        }
 
         if (request.getParameter("console") != null && request.getParameter("console").equals("1")) {
             url = url + "?console=1";
