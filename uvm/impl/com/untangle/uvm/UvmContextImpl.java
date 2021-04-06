567d69549183712d10536c2de2c133624795c635
Chris Blaise <cblaise@untangle.com>
2021-04-06 15:22:42 -0400

    NGFW-13683 Remove app install at startup.

--- uvm/impl/com/untangle/uvm/UvmContextImpl.java
+++ uvm/impl/com/untangle/uvm/UvmContextImpl.java
@@ -1484,14 +1484,6 @@ public class UvmContextImpl extends UvmContextBase implements UvmContext
         UvmContextFactory.context().hookManager().registerCallback( HookManager.APPLICATION_DESTROY, appDestroyHook );
         // Pull if it already added.
         this.reportsApp = (Reporting) this.appManager().app("reports");
-
-        // NGFW-13588 The firstRunFlag will be true when we initialize our
-        // settings the very first time the uvm is started. We use the flag
-        // to automatically install the recommended apps but not if we
-        // detect the DEV or ATS environments.
-        if ( (firstRunFlag) && (! isDevel()) && (! isAts()) ) {
-            appManager.doAutoInstall();
-        }
     }
 
     /**
@@ -1790,4 +1782,5 @@ public class UvmContextImpl extends UvmContextBase implements UvmContext
             }
         }
     }
+
 }
