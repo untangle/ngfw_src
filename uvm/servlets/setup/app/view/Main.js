bba787676adf5ed975e7861ed93244bb26a2fa7b
Chris Blaise <cblaise@untangle.com>
2021-04-12 19:05:16 -0400

    NGFW-13704 Updated create account link as per eliz's suggestion.

--- uvm/servlets/setup/app/view/Main.js
+++ uvm/servlets/setup/app/view/Main.js
@@ -111,7 +111,7 @@ Ext.define('Ung.Setup.Main', {
                         text: '<div style="color:white;">' + 'Create Account'.t() + '</div>',
                         baseCls: 'command-center-create-button',
                         handler: function(){
-                            window.location = rpc.remoteUrl + "/login/create-account?redirectUrl="+encodeURI("/appliances/add/" + rpc.serverUID);
+                            window.location = rpc.remoteUrl + "/login/create-account/add-appliance/" + rpc.serverUID;
                         }
                     }]
                 },{
