aee2ce29193188f59a697c1e892c8b2ede86361b
John Phillps <jphillips@arista.com>
2022-06-22 10:43:43 -0600

    Refactor a bit to test MD5 reports logins and add common code.

--- uvm/hier/usr/lib/python3/dist-packages/unit_tests/test_web_auth_index.py
+++ uvm/hier/usr/lib/python3/dist-packages/unit_tests/test_web_auth_index.py
@@ -66,3 +66,40 @@ def test_valid_login_md5(monkeypatch):
     assert not login_tools.valid_login(None, 'Administrator', 'admin',
                                        'wrongPassword')
     
+
+
+
+
+def test_logins_reports_md5(monkeypatch):
+    password = 'passw0rd'
+    salt = b'\x0f' * 8
+    md5_hash_bytes = hashlib.md5(password.encode('utf-8') + salt).digest()
+    password_hash = base64.b64encode(md5_hash_bytes + salt).decode('utf-8')
+
+    reports_settings = {
+        'reports': {'reportsUsers':
+                  {'list':[
+                      {'username': 'reporter1',
+                       'emailAddress': 'reporter1',
+                       'passwordHashBase64': password_hash}
+                      ]
+                   }
+                  }
+    }
+    def mock_get_app_settings_item(app, item):
+        return reports_settings[app][item]
+    def mock_get_uvm_settings_item(key, subkey):
+        return None
+
+
+    monkeypatch.setattr(login_tools, 'get_app_settings_item',
+                        mock_get_app_settings_item)
+    monkeypatch.setattr(login_tools, 'get_uvm_settings_item',
+                        mock_get_uvm_settings_item)
+
+    assert login_tools.valid_login(None, 'Reports', 'reporter1',
+                                   password)
+
+    assert not login_tools.valid_login(None, 'Reports', 'reporter1',
+                                       'wrongPassword')
+    
