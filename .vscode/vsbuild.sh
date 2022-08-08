48595857e17e1aff4e0f4b1885cef94c7e851169
Chris Blaise <cblaise@untangle.com>
2022-08-08 13:00:23 -0400

    Build for both local and remote targets

--- .vscode/vsbuild.sh
+++ .vscode/vsbuild.sh
@@ -1,40 +1,100 @@
 #!/bin/bash
+DEV_ENVIRONMENT=local
+TARGET=
+PORT=22
+RAKE_LOG=rake.log
+
+while getopts "t:p:
+e:r:" flag; do
+    case "${flag}" in
+        t) TARGET=${OPTARG} ;;
+        p) PORT=${OPTARG} ;;
+        e) DEV_ENVIRONMENT=${OPTARG} ;;
+        r) RAKE_LOG=${OPTARG} ;;
+    esac
+done
+shift $((OPTIND-1))
+
+echo "TARGET=$TARGET"
+echo "DEV_ENVIRONMENT=$DEV_ENVIRONMENT"
+echo "RAKE_LOG=$RAKE_LOG"
+
+if [ "$DEV_ENVIRONMENT" == "remote" ]; then
+    ##
+    ## Build in docker DEV_ENVIRONMENT
+    ##
+    VERBOSE=1 NO_CLEAN=1 RAKE_LOG=$RAKE_LOG DEV_ENVIRONMENT=$DEV_ENVIRONMENT docker-compose -f docker-compose.build.yml up dev-build
+elif [ "$DEV_ENVIRONMENT" == "remote"]; then
+    ##
+    ## Compile ngfw and restart uvm if neccessary.
+    ##
+    rm -f $RAKE_LOG
+    rake |& tee $RAKE_LOG
+else
+    echo "Unknown DEV_ENVIRONMENT=$DEV_ENVIRONMENT"
+    exit
+fi
+
 ##
-## Compile ngfw and restart uvm if neccessary.
+## Review output of build
 ##
-RAKEOUTPUT=/tmp/rakeoutput.txt
-rm -f $RAKEOUTPUT
-rake |& tee $RAKEOUTPUT
-
-if [ $(grep -c "missing documentation" $RAKEOUTPUT) -gt 0 ] ; then 
+if [ $(grep -c "missing documentation" $RAKE_LOG) -gt 0 ] ; then 
     exit
 fi
 
-if [ $(grep -c error $RAKEOUTPUT) -gt 0 ] ; then 
+if [ $(grep -c error $RAKE_LOG) -gt 0 ] ; then 
     exit
 fi
 
-if [ $(grep -c "jslint failed" $RAKEOUTPUT) -gt 0 ] ; then 
+if [ $(grep -c "jslint failed" $RAKE_LOG) -gt 0 ] ; then 
     exit
 fi
 
+##
+## If java modiified, restart uvm
+##
 RESTART=0
-if [ $(grep -c javac $RAKEOUTPUT) -gt 0 ] ; then 
+if [ -f $RAKE_LOG ] && [ $(grep -c javac $RAKE_LOG) -gt 0 ]; then 
     RESTART=1
-    ./dist/etc/init.d/untangle-vm restart
+    if [ "$DEV_ENVIRONMENT" == "local" ]; then
+        ./dist/etc/init.d/untangle-vm restart
+    fi
 fi
 
-if [ -f .vscode/remote-hosts ]; then
-    while read address; do
-        [[ $address = \#* ]] && continue
-        echo "Synchronzing begin with $address"
-        rsync -a dist/usr/share/untangle/lib root@$address:/usr/share/untangle
-        rsync -a dist/usr/share/untangle/bin root@$address:/usr/share/untangle
-        rsync -a dist/usr/share/untangle/web root@$address:/usr/share/untangle
+# Break target down by commas into an array.
+TARGET_ADDRESSES=()
+while IFS=',' read -ra ADDRESSES; do
+    for address in "${ADDRESSES[@]}"; do
+        TARGET_ADDRESSES+=($address)
+    done
+done <<< "$TARGET"
 
-        if [ $RESTART -eq 1 ] ; then
-            ssh root@$address "/etc/init.d/untangle-vm restart"
-        fi
-        echo "Synchronzing end with $address"
-    done < .vscode/remote-hosts
-fi
+for target_address in "${TARGET_ADDRESSES[@]}"; do
+    echo "Copying to $target_address..."
+
+    ssh-copy-id -p $PORT root@$target_address
+    rsync -a dist/usr/lib root@$address:/usr
+    rsync -a dist/usr/share/untangle/lib root@$address:/usr/share/untangle
+    rsync -a dist/usr/share/untangle/bin root@$address:/usr/share/untangle
+    rsync -a dist/usr/share/untangle/web root@$address:/usr/share/untangle
+
+    if [ $RESTART -eq 1 ] ; then
+        ssh root@$address "/etc/init.d/untangle-vm restart"
+    fi
+
+done
+
+# if [ -f .vscode/remote-hosts ]; then
+#     while read address; do
+#         [[ $address = \#* ]] && continue
+#         echo "Synchronzing begin with $address"
+#         rsync -a dist/usr/share/untangle/lib root@$address:/usr/share/untangle
+#         rsync -a dist/usr/share/untangle/bin root@$address:/usr/share/untangle
+#         rsync -a dist/usr/share/untangle/web root@$address:/usr/share/untangle
+
+#         if [ $RESTART -eq 1 ] ; then
+#             ssh root@$address "/etc/init.d/untangle-vm restart"
+#         fi
+#         echo "Synchronzing end with $address"
+#     done < .vscode/remote-hosts
+# fi
