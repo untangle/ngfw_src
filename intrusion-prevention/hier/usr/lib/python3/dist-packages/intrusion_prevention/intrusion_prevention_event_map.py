ecdc70d8c1481cba28658a44521a2705dd72ceda
Chris Blaise <cblaise@untangle.com>
2022-08-08 08:46:18 -0400

    NGFW-14143 Add new field for signature protocol.  Format file for readability.

--- intrusion-prevention/hier/usr/lib/python3/dist-packages/intrusion_prevention/intrusion_prevention_event_map.py
+++ intrusion-prevention/hier/usr/lib/python3/dist-packages/intrusion_prevention/intrusion_prevention_event_map.py
@@ -51,6 +51,7 @@ class IntrusionPreventionEventMap:
                 "category": signature.category,
                 "msg": msg,
                 "classtype": signature.options["classtype"],
+                "protocol": signature.protocol,
             })
 
     def save(self):
@@ -58,5 +59,5 @@ class IntrusionPreventionEventMap:
         Save event map
         """
         settings_file = open(self.file_name, "w")
-        json.dump(self.settings, settings_file)
+        json.dump(self.settings, settings_file, indent=4)
         settings_file.close()
