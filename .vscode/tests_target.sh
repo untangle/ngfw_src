175377fa0ad890e50453e2f251d145304c4e85e0
Chris Blaise <cblaise@untangle.com>
2022-08-12 10:58:57 -0400

    Add test scripts.

---
+++ .vscode/tests_target.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+##
+## Installer script run from NGW to setup target testshell client.
+##
+CLIENT_TARGET=$1
+
+##
+## Build SSH command base we will use to communicate with client
+##
+SOURCE_KEY=/usr/lib/runtests/test_shell.key
+SSH_COMMAND="ssh -i $SOURCE_KEY -o StrictHostKeyChecking=no -o ConnectTimeout=300 -o ConnectionAttempts=15 root@$CLIENT_TARGET"
+
+echo "SSH_COMAND=$SSH_COMMAND"
+
+eval $SSH_COMMAND "ls"
+if [ $? -ne 0 ] ; then
+    # Unable to access via key
+    echo "Unable to access $CLIENT_TARGET with $ACTIVE_KEY"
+    exit
+fi
+
+SETUP_TESTSHELL_SCRIPT=/root/setup_testshell.sh
+SETUP_TESTSHELL_URI=http://test.untangle.com/test/setup_testshell.sh
+
+## Install required Debian packages
+eval '$SSH_COMMAND "apt-get update && apt-get -y install wget sudo resolvconf"'
+## Get testshell script and run
+eval '$SSH_COMMAND "cd /root; wget -O $SETUP_TESTSHELL_SCRIPT $SETUP_TESTSHELL_URI "'
+eval '$SSH_COMMAND "chmod 775 $SETUP_TESTSHELL_SCRIPT"'
+eval '$SSH_COMMAND "echo y | $SETUP_TESTSHELL_SCRIPT"'
+## Add testshell to sudoers
+eval '$SSH_COMMAND "echo \"testshell ALL=(ALL) NOPASSWD: ALL\" >> /etc/sudoers"'
+
