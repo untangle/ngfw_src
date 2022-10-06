2c52b1072aabede9d810c19f3c0e63ffeadc5a16
Chris Blaise <cblaise@untangle.com>
2022-10-06 14:26:14 -0400

    NGFW-14202 Restore 16.5.x backups.

--- uvm/hier/usr/share/untangle/bin/ut-restore.sh
+++ uvm/hier/usr/share/untangle/bin/ut-restore.sh
@@ -12,7 +12,7 @@ TARBALL_FILE=""
 VERSION_FILE=""
 # To support multiple versions, separate with pipe character like:
 #ACCEPTED_PREVIOUS_VERSION="15.1|16.0"
-ACCEPTED_PREVIOUS_VERSION="16.4"
+ACCEPTED_PREVIOUS_VERSION="16.5"
 
 function debug() {
   if [ "true" == $VERBOSE ]; then
