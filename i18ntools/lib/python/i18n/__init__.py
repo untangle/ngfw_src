53da6dd1bf1f4243757f6e22ab1f5b0154b86284
Chris Blaise <cblaise@untangle.com>
2022-10-14 12:03:59 -0400

    NGFW-14174 New classes to support status file and server generation

--- i18ntools/lib/python/i18n/__init__.py
+++ i18ntools/lib/python/i18n/__init__.py
@@ -7,4 +7,7 @@ from i18n.ngfw import Ngfw
 from i18n.po_record import PoRecord
 from i18n.po_file import PoFile
 from i18n.pot_file import PotFile
+from i18n.server_content import ServerContent
+from i18n.status_record import StatusRecord
+from i18n.status_file import StatusFile
 from i18n.utility import Utility
---
+++ i18ntools/lib/python/i18n/status_file.py
@@ -0,0 +1,71 @@
+"""
+Status file management
+"""
+import copy
+import json
+import os
+
+from i18n.utility import Utility
+
+class StatusFile:
+    """
+    Output file for server.
+    Downloaded and consumed by NGFW to determine available languages
+    """
+    url_base = None
+    url_translate_base = None
+    last_author = "translations@translations.untangle.com"
+    default_last_author = "FULL NAME <EMAIL@ADDRESS>"
+
+    status = {
+        "stats": {
+        }
+    }
+    path = "/status"
+    extension = "json"
+
+    def __init__(self, source_id="official", path=None):
+        """
+        Init
+        """
+        if path != None:
+            self.file_name = f"{path}{source_id}.{self.extension}"
+        else:
+            self.build_file_name(source_id)
+
+    def build_file_name(self, source="official"):
+        """
+        Create filename from source
+        """
+        self.file_name = f"{Utility.get_base_path()}{self.path}/{source}.{self.extension}"
+
+    def exists(self):
+        return os.path.isfile(self.file_name)
+
+    def add_record(self, language_id, status_record):
+        """
+        """
+        self.status["stats"][language_id] = copy.deepcopy(status_record.get_status())
+
+    def make_directories(self, file_name=None):
+        """
+        Create directory path
+        """
+        if file_name == None:
+            file_name = self.file_name
+        rindex = file_name.rfind("/")
+        if rindex != -1:
+            path = file_name[0:rindex]
+            if os.path.isdir(path) == False:
+                os.makedirs(path)
+
+    def save(self):
+        """
+        Save file
+        """
+        self.make_directories()
+
+        print(self.file_name)
+        with open(self.file_name, 'w', encoding='utf-8') as f:
+            json.dump(self.status, f, ensure_ascii=False, indent=4)
+
