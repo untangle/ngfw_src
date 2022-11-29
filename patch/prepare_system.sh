0b0ddde0e06e49f39422baa1f8d74f536f635604
Chris Blaise <cblaise@untangle.com>
2022-11-29 12:37:45 -0500

    New patch/updated creation scripts documented at https://intranet.untangle.com/pages/viewpage.action?pageId=10912002

---
+++ patch/prepare_system.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+##
+## Prepare for patching
+##
+ORIG_PATH=/root/orig
+SOURCES_FILE_NAME=/etc/apt/sources.list.d/untangle.list
+
+## Point apt to packages
+## deb http://302e-9d2e-5023-5e62:untangle@updates.untangle.com/public/bullseye stable-166 main non-free
+##
+BACKUP_SOURCES_FILE_NAME=/root/untangle.list
+if [ ! -f $BACKUP_SOURCES_FILE_NAME ] ; then
+	cp -a $SOURCES_FILE_NAME $BACKUP_SOURCES_FILE_NAME
+fi
+
+VERSION=$(cat /usr/share/untangle/lib/untangle-libuvm-api/PUBVERSION)
+
+sed -i $SOURCES_FILE_NAME \
+    -e 's/updates.untangle.com/package-server.untangle.int/' \
+    -e "s/stable-[[:digit:]]\+/ngfw-release-$VERSION/"
+
+##
+## Backup for comparison.
+##
+if [ -d $ORIG_PATH ] ; then
+	# Generally do not unconditionally update.
+	# If we really need patch dependencies, resolve manually
+	echo "$ORIG_PATH already exists"
+	exit
+fi
+
+mkdir -p /root/orig
+rsync -av /usr /root/orig
