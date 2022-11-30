9b987a9a6785d0b2e8673f88226e69b39d2b9159
Chris Blaise <cblaise@untangle.com>
2022-11-30 14:17:02 -0500

    NGFW-13238 In bullseye there is no whitespace after uplink.id, so perform more exact interface match by expanding the interfaceId matching part of the fwmark.

--- wan-balancer/hier/usr/share/untangle/bin/wan-balancer-sync-settings.py
+++ wan-balancer/hier/usr/share/untangle/bin/wan-balancer-sync-settings.py
@@ -212,7 +212,7 @@ def write_route_file( file, verbosity=0 ):
             interfaceId = int(intf.get('interfaceId'))
             weight = int(settings.get('weights')[intf.get('interfaceId') - 1])
             if ( weight != 0 ):
-                file.write("if [ ! -z  \"`ip rule ls | grep fwmark | egrep 'uplink.%i\s' 2>/dev/null`\" ] ; then " % (interfaceId) + "\n")
+                file.write("if [ ! -z  \"`ip rule ls | egrep 'fwmark 0x%i00' | egrep 'uplink.%i' 2>/dev/null`\" ] ; then " % (interfaceId, interfaceId) + "\n")
                 file.write("    ROUTE_STR=\"$ROUTE_STR `ip route show table uplink.%i | sed -e 's/default/nexthop/'` weight %i\"" % (interfaceId, weight) + "\n")
                 file.write("fi" + "\n")
     file.write("\n");
