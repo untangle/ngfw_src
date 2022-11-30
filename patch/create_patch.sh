7248cd55ca67e1a7b18671fa6e9021604dddf2a4
Chris Blaise <cblaise@untangle.com>
2022-11-30 11:30:48 -0500

    Make VERSION argument be required and warn if matches current system.

--- patch/create_patch.sh
+++ patch/create_patch.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-TARBALL=$1
-PATCH_NAME=$2
+VERSION=$1
+TARBALL=$2
+PATCH_NAME=$3
+
+if [ "$VERSION" = "" ] ; then
+    echo "Version is required, in full format like 16.6.0"
+    exit
+elif [ "$VERSION" = $(cat /usr/share/untangle/lib/untangle-libuvm-api/VERSION) ] ; then
+    echo "WARNING: $VERSION matches current system; it may need to be one less?"
+fi
 
 if [ "$TARBALL" = "" ] ; then
     TARBALL=patch.tgz
@@ -9,8 +17,6 @@ fi
 if [ "$PATCH_NAME" = "" ] ; then
     PATCH_NAME=patch.sh
 fi
-
-VERSION=$(cat /usr/share/untangle/lib/untangle-libuvm-api/VERSION)
 
 cat << SCRIPT > $PATCH_NAME
 if [ \`cat /usr/share/untangle/lib/untangle-libuvm-api/VERSION\` != '$VERSION' ] ; then 
@@ -31,3 +37,5 @@ base64 $TARBALL >> $PATCH_NAME
 echo "ENCODED_PATCH" >> $PATCH_NAME
 
 chmod oug+x $PATCH_NAME
+
+echo "Created $PATCH_NAME for VERSION=$VERSION"
