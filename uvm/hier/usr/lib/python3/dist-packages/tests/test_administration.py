0df7bf3e766c92216d0fab2a41d4059a59610eb3
acooke-arista <acooke@arista.com>
2023-01-31 11:10:09 -0700

    Added skips to work with old versions

--- uvm/hier/usr/lib/python3/dist-packages/tests/test_administration.py
+++ uvm/hier/usr/lib/python3/dist-packages/tests/test_administration.py
@@ -1,4 +1,5 @@
 import pytest
+import unittest
 
 from tests.common import NGFWTestCase
 import tests.global_functions as global_functions
@@ -23,18 +24,28 @@ class AdministrationTests(NGFWTestCase):
         root_cert_info = cert_manager.getRootCertificateInformation()
         
         # Checks for CN=[Arista Site], O=Arista, and L=Santa Clara
-        cert_subjects = root_cert_info['certSubject'].split(", ")
+        cert_subjects = root_cert_info["certSubject"].split(", ")
         cn_found, o_found, l_found = False, False, False
+        skip_str = "Untangle certs allowed on old versions. Skipping test."
         for subject in cert_subjects:
             if "CN=" in subject:
                 cn_found = True
-                assert("www.edge.arista.com" in subject)
+                if "www.untangle.com" in subject:
+                    raise unittest.SkipTest(skip_str)
+                else:
+                    assert("www.edge.arista.com" in subject)
             if "O=" in subject:
                 o_found = True
-                assert("Arista" in subject)
+                if "Untangle" in subject:
+                    raise unittest.SkipTest(skip_str)
+                else:
+                    assert("Arista" in subject)
             if "L=" in subject:
                 l_found = True
-                assert("Santa Clara" in subject)
+                if "Sunnyvale" in subject:
+                    raise unittest.SkipTest(skip_str)
+                else:
+                    assert("Santa Clara" in subject)
         assert(cn_found and o_found and l_found)
     
 test_registry.register_module("administration-tests", AdministrationTests)
