73eb60192e6e575a3cc4e4d1ef5e25e970764516
Chris Blaise <cblaise@untangle.com>
2023-04-03 18:01:48 -0400

    NGFW-14346 Improve logging by including script name.  Bring down ip rule.

--- tunnel-vpn/hier/usr/share/untangle/bin/tunnel-vpn-down.sh
+++ tunnel-vpn/hier/usr/share/untangle/bin/tunnel-vpn-down.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
-
+script_name=$0
 exec >> /var/log/uvm/tunnel.log 2>&1
 
-echo "`date`: dev:${dev} local:${ifconfig_local} remote:${ifconfig_remote} gateway:${route_vpn_gateway}"
+# It would be nice to match priorities to ids, but some apps like
+# strongswan force priorities that would conflict.
+priority_floor=1000
+
+echo "`date`: ${script_name}: dev:${dev} local:${ifconfig_local} remote:${ifconfig_remote} gateway:${route_vpn_gateway}"
 
 if [ -z "${dev}" ] ; then
-    echo "Missing dev!"
+    echo "${script_name}: missing device!"
     exit 1
 fi
 
 interface_id="`echo ${dev} | sed -e 's/[a-zA-Z]//g'`"
 if [ -z "${interface_id}" ] ; then
-    echo "Missing ID!"
+    echo "${script_name}: missing id!"
     exit 1
 fi
+
+priority=$(($interface_id + $priority_floor))
+table_name=tunnel.$interface_id
+ip rule del priority $priority lookup $table_name
 
 # table can not exist at this point - hide errors
 ip -4 route flush table "uplink.${interface_id}" >/dev/null 2>&1
