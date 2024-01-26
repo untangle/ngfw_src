1723e17ad4c7c2e4632537deee54c73847a93ca2
Chris Blaise <cblaise@untangle.com>
2024-01-26 10:27:42 -0500

    NGFW-14493 Change previous version to 17.0

--- uvm/hier/usr/share/untangle/bin/ut-restore.sh
+++ uvm/hier/usr/share/untangle/bin/ut-restore.sh
@@ -12,7 +12,7 @@ TARBALL_FILE=""
 VERSION_FILE=""
 # To support multiple versions, separate with pipe character like:
 #ACCEPTED_PREVIOUS_VERSION="15.1|16.0"
-ACCEPTED_PREVIOUS_VERSION="16.6"
+ACCEPTED_PREVIOUS_VERSION="17.0"
 
 function debug() {
   if [ "true" == $VERBOSE ]; then
