26cd29aeed57bdac59735b689bad4e6b5aa2fb78
Mayur Dhande <mayur.dhande@arista.com>
2024-02-01 19:34:01 +0530

    NGFW-14380 Allowing specific characters for file type in virus blocker apps

--- virus-blocker/js/view/Advanced.js
+++ virus-blocker/js/view/Advanced.js
@@ -105,6 +105,8 @@ Ext.define('Ung.apps.virusblocker.view.Advanced', {
                 fieldLabel: 'File Type'.t(),
                 emptyText: '[enter file type]'.t(),
                 allowBlank: false,
+                regex: RegExp("[\\?\\^\\w~!@#$-]+"),
+                regexText: 'File type can start with an alphanumeric character and any one of those special characters ? ^ _ ~ ! @ # and $ only',
                 width: 400
             }, {
                 xtype: 'checkbox',
