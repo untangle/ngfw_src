9684c60d14ca58403a41529abd73a4545256174b
Shriram-Patil-Arista <shriram.patil@arista.com>
2024-03-14 15:03:19 +0530

    tags search fix

--- uvm/js/common/ungrid/GridFilterController.js
+++ uvm/js/common/ungrid/GridFilterController.js
@@ -133,10 +133,13 @@ Ext.define('Ung.cmp.GridFilterController', {
             var str = [], filtered = false;
 
             Ext.Array.each(cols, function (col) {
-                var val = item.get(col.dataIndex);
+                var val = item.get(col.text !== 'Tags'.t() ? col.dataIndex : "tags");
+                if(val && col.text === 'Tags'.t()){
+                    val = Converter.tagsFilterText(val);
+                }
                 if (!val) { return; }
-                var tableConfigColumn = TableConfig.getTableColumn(null, col.dataIndex);
-                str.push(typeof val === 'object' ? Renderer.timestamp(val) : ( tableConfigColumn && tableConfigColumn['renderer'] ? tableConfigColumn['renderer'](val): val.toString()) ) ;
+                var tableConfigColumn = TableConfig.getTableColumn(null, col.text !== 'Tags'.t() ? col.dataIndex : "tags");
+                str.push(typeof val === 'object' ? Renderer.timestampUnixRenderer(val) : ( tableConfigColumn && tableConfigColumn['renderer'] ? tableConfigColumn['renderer'](val): val.toString()) ) ;
             });
             if (regex.test(str.join('|'))) { filtered = true; }
 
--- uvm/js/common/util/Converter.js
+++ uvm/js/common/util/Converter.js
@@ -121,5 +121,16 @@ Ext.define('Ung.util.Converter', {
     // },
 
     // not implemented, see old Renderer
-    webRule: function () {}
+    webRule: function () {},
+
+    tagsFilterText: function(tags){
+        if(tags.list && tags.list.length > 0){
+            var tagsList = [];
+            tagsList = Ext.Array.map(tags.list, function (tag){
+                return tag.name.trim() ? tag.name.trim() : "";
+            });
+            return tagsList.length > 0 ? tagsList.join(" ") : "";
+        }
+        return "";
+    } 
 });
