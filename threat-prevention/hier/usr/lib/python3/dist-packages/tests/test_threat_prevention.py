1431c4006d731eedd93e2d2674301398ffa714f0
Chris Blaise <cblaise@untangle.com>
2024-06-05 16:39:34 -0400

    NGFW-14642 New tests to verify exceptions are not caused from sni extraction

--- threat-prevention/hier/usr/lib/python3/dist-packages/tests/test_threat_prevention.py
+++ threat-prevention/hier/usr/lib/python3/dist-packages/tests/test_threat_prevention.py
@@ -1,6 +1,9 @@
 """threat_prevention tests"""
 import datetime
 import pytest
+import runtests
+import subprocess
+import unittest
 import sys
 
 from tests.common import NGFWTestCase
@@ -248,6 +251,41 @@ class ThreatpreventionTests(NGFWTestCase):
                                             self.eventAppName() + '_blocked', True,
                                             self.eventAppName() + '_flagged', True )
         assert( found )
+
+    def test_551_https_with_sni_packet_split(self):
+        """ Verify no exceptions with split Hello TLS packets"""
+        if runtests.quick_tests_only:
+            raise unittest.SkipTest('Skipping a time consuming test')
+
+        sni_domain = "docs.google.com"
+        app_id = self._app.getAppSettings()["id"]
+        log_file = f"/var/log/uvm/app-{app_id}.log"
+
+        count = 0
+        exceptions = 0
+
+        # Not the entire packet, but a little bit beyond the position of the SNI server record.
+        max_index=1600
+        # Hit as many places as we could be out of bounds with split
+        packet_split_iteration = 2
+
+        for index in range(2, max_index,packet_split_iteration):
+            last_log_line = subprocess.check_output(f"wc -l {log_file} | cut -d' ' -f1", shell=True).decode("utf-8").strip()
+            last_log_line = int(last_log_line) + 1
+            result = remote_control.run_command(f"./https_client.py -i {index}")
+
+            log_invalid_exception = subprocess.check_output(f"awk 'NR >= {last_log_line} && /WARN  Exception calling extractSNIhostname/{{ print NR, $0 }}' {log_file}", shell=True).decode("utf-8")
+            print(log_invalid_exception)
+            for log in log_invalid_exception.split("\n"):
+                if len(log) == 0:
+                    continue
+                exceptions += 1
+                break
+
+            count += 1
+
+        print(f"count={count}, exceptions={exceptions}")
+        assert exceptions == 0, "exceptions"
 
     @classmethod
     def final_extra_tear_down(cls):
