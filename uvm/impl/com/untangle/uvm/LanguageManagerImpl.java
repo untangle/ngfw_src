dc7bbe6f41f906f3a2d8a204f00df7f8e87faaa1
Mayur Dhande <mayur.dhande@arista.com>
2024-06-29 00:54:12 +0530

    NGFW-14679 fixed compile errors

--- uvm/impl/com/untangle/uvm/LanguageManagerImpl.java
+++ uvm/impl/com/untangle/uvm/LanguageManagerImpl.java
@@ -38,6 +38,7 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.core5.util.Timeout;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.json.JSONException;
