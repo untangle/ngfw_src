4be63df788ea060e3b21ca22b2c9c6c210e712e3
Mayur Dhande <mayur.dhande@arista.com>
2024-10-02 01:36:59 +0530

    NGFW-14847 fixed drive helper script that fixes oauth token api

--- uvm/hier/usr/share/untangle/bin/ut-google-drive-helper.sh
+++ uvm/hier/usr/share/untangle/bin/ut-google-drive-helper.sh
@@ -5,7 +5,8 @@
 
 get_json_value() {
   local json_key=$1
-  python3 -m simplejson.tool $2 | grep "$json_key" | awk '{print $2}' | sed 's/\"//g'
+  # Remove , space \n and \t
+  python3 -m simplejson.tool $2 | grep "$json_key" | awk '{print $2}' | sed 's/[",[:space:]]//g' | tr -d '\t' | tr -d '\n'
 }
 
 refreshToken()
