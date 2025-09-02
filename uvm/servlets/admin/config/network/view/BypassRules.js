e4a77ba33518cc856da94f81b416d4b6fc8d4b65
DhirajM09 <dhirajkumar.mahajan@arista.com>
2025-09-02 09:48:29 +0530

    ngfw-15298 Iframe changes for Bypass and Filter Rules

--- uvm/servlets/admin/config/network/view/BypassRules.js
+++ uvm/servlets/admin/config/network/view/BypassRules.js
@@ -6,88 +6,16 @@ Ext.define('Ung.config.network.view.BypassRules', {
     scrollable: true,
     withValidation: false,
     title: 'Bypass Rules'.t(),
+    layout: 'border',
 
-    layout: 'fit',
+    listeners: {
+        activate: function (panel) {
+            var target = panel.down('#iframeHolder');
+            Util.attachIframeToTarget(target, '/console/settings/network/bypass', false);
+        }
+    },
 
-    tbar: [{
-        xtype: 'tbtext',
-        padding: '8 5',
-        style: { fontSize: '12px' },
-        html: 'Bypass Rules control what traffic is scanned by the applications. Bypassed traffic skips application processing. The rules are evaluated in order. Sessions that meet no rule are not bypassed.'.t()
-    }],
-
-    items: [{
-        xtype: 'ungrid',
-        itemId: 'bypass-rules-grid',
-        srcAddrIsLanCheck: true,
-        controller: 'unconfigbypassrulesgridcontroller',
-
-        dockedItems: [{
-            xtype: 'container',
-            padding: '8 5',
-            style: { fontSize: '12px', background: '#DADADA'},
-            html: '<i class="fa fa-exclamation-triangle" style="color: red;"></i> ' + 'One or more rules have the condition of the source address a LAN address.'.t(),
-            hidden: true,
-            bind: {
-                hidden: '{!warnBypassRuleSrcAddrIsLan}'
-            }
-        }],
-
-        tbar: ['@add', '->', '@import', '@export'],
-        recordActions: ['edit', 'copy', 'delete', 'reorder'],
-        copyId: 'ruleId',  
-        copyAppendField: 'description',
-
-        emptyText: 'No Bypass Rules defined'.t(),
-
-        listProperty: 'settings.bypassRules.list',
-
-        importValidationJavaClass: true,
-
-        importValidationForComboBox: true,
-
-        emptyRow: {
-            ruleId: -1,
-            enabled: true,
-            bypass: true,
-            javaClass: 'com.untangle.uvm.network.BypassRule',
-            conditions: {
-                javaClass: 'java.util.LinkedList',
-                list: []
-            },
-            description: ''
-        },
-
-        bind: '{bypassRules}',
-
-        columns: [
-            Column.ruleId,
-            Column.enabled,
-            Column.description,
-            Column.conditions,
-        {
-            header: 'Bypass'.t(),
-            xtype: 'checkcolumn',
-            dataIndex: 'bypass',
-            width: Renderer.booleanWidth
-        }],
-        editorXtype: 'ung.cmp.unconfigbypassrulesrecordeditor',
-        editorFields: [
-            Field.enableRule(),
-            Field.description,
-            Field.conditions(
-                'com.untangle.uvm.network.BypassRuleCondition',[
-                "DST_ADDR",
-                "DST_PORT",
-                "DST_INTF",
-                "SRC_ADDR",
-                "SRC_PORT",
-                "SRC_INTF",
-                "PROTOCOL",
-                "CLIENT_TAGGED",
-                "SERVER_TAGGED"
-            ]),
-           Field.bypass
-        ]
-    }]
+    items: [
+        Field.iframeHolder
+    ]
 });
