b290572eea214c6b4d97bf07256be6791fd55b67
AristaKB <khushbu.bibay@arista.com>
2025-10-15 19:32:06 +0530

    ngfw-15373: Added Iframe changes for DBL and made it Non-free

--- dynamic-blocklists/js/Main.js
+++ dynamic-blocklists/js/Main.js
@@ -1,7 +1,6 @@
 Ext.define('Ung.apps.dynamic-blocklists.Main', {
     extend: 'Ung.cmp.AppPanel',
     alias: 'widget.app-dynamic-blocklists',
-    controller: 'app-dynamic-blocklists',
     viewModel: {
         stores: {
         },
@@ -11,8 +10,6 @@ Ext.define('Ung.apps.dynamic-blocklists.Main', {
         }
     },
     items: [
-        { xtype: 'app-dynamic-blocklists-status' },
         { xtype: 'app-dynamic-blocklists-configuration' }
-
     ]
 });
