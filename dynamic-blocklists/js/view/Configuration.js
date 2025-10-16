ec068ac6bf37a0c11e705df1515321a55854ab18
AristaKB <155290371+AristaKB@users.noreply.github.com>
2025-10-16 09:39:20 +0530

    ngfw-15373: Added Iframe changes for DBL and made it Non-free (#1075)

--- dynamic-blocklists/js/view/Configuration.js
+++ dynamic-blocklists/js/view/Configuration.js
@@ -13,53 +13,20 @@ Ext.define('Ung.apps.dynamic-blocklists.view.Configuration', {
 
         layout: {
             type: 'fit',
-            // type: 'vbox',
             align: 'stretch'
         },
         scrollable: false,  
-        // scrollable: 'y',
         items: [
-        //     {
-        //     xtype: 'component',
-        //     cls: 'app-desc',
-        //     html: '<img src="/icons/apps/dynamic-lists.svg" width="80" height="80"/>' +
-        //         '<h3>Dynamic Lists</h3>' +
-        //         '<p>' + 'Dynamic Lists provides functionality to block IPS from URL Lists.'.t() + '</p>'
-        // }, 
-        // {
-        //     xtype: 'applicense',
-        //     hidden: true,
-        //     bind: {
-        //         hidden: '{!license || !license.trial}'
-        //     }
-        // }, 
-        // {
-        //     xtype: 'appstate',
-        // },
          {
             xtype: 'component',
             html: '',
             flex: 1, // Makes the iframe fill the available space
             listeners: {
                 afterrender: function (view) {
-                    view.setHtml('<iframe src="/vue/DynamicBlockLists" style="width: 100%; height: 100%; border: 1px solid red;"></iframe>');
+                    view.setHtml('<iframe src="/console/settings/services/dynamic-blocklist" style="width: 100%; height: 100%; border: none;"></iframe>');
                 }
             }
         }]
     }, 
-        // {
-        //     region: 'west',
-        //     border: false,
-        //     width: Math.ceil(Ext.getBody().getViewSize().width / 4),
-        //     split: true,
-        //     layout: 'fit',
-        //     items: [{
-        //         xtype: 'appmetrics',
-        //     }],
-        //     bbar: [{
-        //         xtype: 'appremove',
-        //         width: '100%'
-        //     }]
-        // }
 ]
 });
