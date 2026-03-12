e4d012b736760e441f0c7ead247bce83a7a7bda0
DhirajM09 <dhirajkumar.mahajan@arista.com>
2026-03-12 17:53:26 +0530

    ngfw-15640 Added setupVueMessageHandlers utility and iframe in application control lite Main.js

--- application-control-lite/js/Main.js
+++ application-control-lite/js/Main.js
@@ -4,17 +4,43 @@ Ext.define('Ung.apps.applicationcontrollite.Main', {
     controller: 'app-application-control-lite',
 
     viewModel: {
+        data: {
+            title: 'Application Control Lite'.t(),
+            iconName: 'application-control-lite',
+            vueMigrated: true
+        },
+        // stores: {
+        //     signatureList: {
+        //         data: '{settings.patterns.list}'
+        //     }
+        // }
+    },
 
-        stores: {
-            signatureList: {
-                data: '{settings.patterns.list}'
-            }
+    // items: [
+    //     { xtype: 'app-application-control-lite-status' },
+    //     { xtype: 'app-application-control-lite-signatures' }
+    // ]
+
+    listeners: {
+        activate: function (panel) {
+            var vm = panel.getViewModel();
+            var policyId = vm.get('policyId');
+            var target = panel.down('#iframeHolder');
+            Util.attachIframeToTarget(target, '/console/apps/' + policyId + '/application-control-lite', false);
+
+            Util.setupVueMessageHandlers(panel, {
+                appName: 'application-control-lite',
+                enableRemoveHandler: true
+            });
+        },
+
+        destroy: function (panel) {
+            Util.cleanupVueMessageHandlers(panel);
         }
     },
 
     items: [
-        { xtype: 'app-application-control-lite-status' },
-        { xtype: 'app-application-control-lite-signatures' }
+        Field.iframeHolder
     ]
 
 });
