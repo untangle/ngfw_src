5db6583dcafb3dbb2448af26a5c2239cdf0e795c
DhirajM09 <154422821+DhirajM09@users.noreply.github.com>
2026-04-15 16:30:18 +0530

    ngfw-15710 Added Iframe changes with remove app listeners for all vue migrated apps and services (#1174)

--- application-control-lite/js/Main.js
+++ application-control-lite/js/Main.js
@@ -1,20 +1,33 @@
 Ext.define('Ung.apps.applicationcontrollite.Main', {
     extend: 'Ung.cmp.AppPanel',
     alias: 'widget.app-application-control-lite',
-    controller: 'app-application-control-lite',
 
     viewModel: {
+        data: {
+            title: 'Application Control Lite'.t(),
+            iconName: 'application-control-lite',
+            vueMigrated: true
+        },
+    },
 
-        stores: {
-            signatureList: {
-                data: '{settings.patterns.list}'
-            }
+    listeners: {
+        activate: function (panel) {
+            var target = panel.down('#iframeHolder');
+            Util.attachIframeToTarget(target, '/console/settings/services/application-control-lite', false);
+
+            Util.setupVueMessageHandlers(panel, {
+                appName: 'application-control-lite',
+                enableRemoveHandler: true
+            });
+        },
+
+        destroy: function (panel) {
+            Util.cleanupVueMessageHandlers(panel);
         }
     },
 
     items: [
-        { xtype: 'app-application-control-lite-status' },
-        { xtype: 'app-application-control-lite-signatures' }
+        Field.iframeHolder
     ]
 
 });
--- branding-manager/js/Main.js
+++ branding-manager/js/Main.js
@@ -1,18 +1,28 @@
 Ext.define('Ung.apps.brandingmanager.Main', {
     extend: 'Ung.cmp.AppPanel',
     alias: 'widget.app-branding-manager',
-    controller: 'app-branding-manager',
 
     viewModel: {
         data: {
+            title: 'Branding Manager'.t(),
+            iconName: 'branding-manager',
             vueMigrated: true
-        }
+        },
     },
 
     listeners: {
         activate: function (panel) {
             var target = panel.down('#iframeHolder');
             Util.attachIframeToTarget(target, '/console/settings/services/branding-manager', false);
+
+            Util.setupVueMessageHandlers(panel, {
+                appName: 'branding-manager',
+                enableRemoveHandler: true
+            });
+        },
+
+        destroy: function (panel) {
+            Util.cleanupVueMessageHandlers(panel);
         }
     },
 
