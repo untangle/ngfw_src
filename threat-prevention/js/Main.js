3837836b9db2b0329aac48f5e6d54413af4e7bdb
DhirajM09 <dhirajkumar.mahajan@arista.com>
2026-08-28 12:31:48 +0530

    ngfw-15926 Threat prevention Iframe changes

--- threat-prevention/js/Main.js
+++ threat-prevention/js/Main.js
@@ -2,18 +2,35 @@ Ext.syncRequire('Ung.common.threatprevention');
 Ext.define('Ung.apps.threatprevention.Main', {
     extend: 'Ung.cmp.AppPanel',
     alias: 'widget.app-threat-prevention',
-    controller: 'app-threat-prevention',
 
     viewModel: {
-        type: 'app-threat-prevention'
+        type: 'app-threat-prevention',
+        data: {
+            title: 'Threat Prevention'.t(),
+            iconName: 'threat-prevention',
+            vueMigrated: true
+        },
+    },
+
+    listeners: {
+        activate: function (panel) {
+            var vm = panel.getViewModel();
+            var policyId = vm.get('policyId');
+            var target = panel.down('#iframeHolder');
+            Util.attachIframeToTarget(target, '/console/apps/' + policyId + '/threat-prevention', false);
+
+            Util.setupVueMessageHandlers(panel, {
+                appName: 'threat-prevention',
+                enableRemoveHandler: true
+            });
+        },
+
+        destroy: function (panel) {
+            Util.cleanupVueMessageHandlers(panel);
+        }
     },
 
     items: [
-        { xtype: 'app-threat-prevention-status' },
-        { xtype: 'app-threat-prevention-threats' },
-        { xtype: 'app-threat-prevention-pass-sites' },
-        { xtype: 'app-threat-prevention-rules' },
-        { xtype: 'app-threat-prevention-threatlookup' },
-        { xtype: 'app-threat-prevention-advanced' }
+        Field.iframeHolder
     ]
 });
